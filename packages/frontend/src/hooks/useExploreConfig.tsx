c8c39e1f96a3288b4f376cdcd57c89adaec4b3d9
oliverlaslett <olaslett@gmail.com>
2021-05-27 14:43:57 +0100

    add confirmation when user loses work

--- packages/frontend/src/hooks/useExploreConfig.tsx
+++ packages/frontend/src/hooks/useExploreConfig.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {useHistory, useLocation, useParams} from "react-router-dom";
 
+type SidebarPanel = 'base' | 'explores'
+
 type ContextProps = {
     activeTableName: string | undefined,
     setActiveTableName: (tableName: string) => void,
     activeFields: Set<string>,
     toggleActiveField: (fieldName: string) => void,
+    sidebarPanel: SidebarPanel,
+    setSidebarPanel: (panelName: SidebarPanel) => void,
 }
 const context = React.createContext<ContextProps>({
     activeTableName: undefined,
     setActiveTableName: () => {},
     activeFields: new Set<string>(),
     toggleActiveField: () => {},
+    sidebarPanel: 'base',
+    setSidebarPanel: () => {},
 });
 
 export const ExploreConfigContext: React.FC = ({ children }) => {
@@ -22,7 +28,15 @@ export const ExploreConfigContext: React.FC = ({ children }) => {
     // Currently active table
     const activeTableName = pathParams.tableId
     const setActiveTableName = (tableName: string) => {
-        history.push(`/tables/${tableName}`)
+        const newParams = new URLSearchParams(searchParams)
+        newParams.set('sidebar', 'explores')
+        if (tableName !== activeTableName) {
+            newParams.delete('fields')
+        }
+        history.push({
+            pathname: `/tables/${tableName}`,
+            search: newParams.toString(),
+        })
     }
 
     // Currently active fields
@@ -34,6 +48,17 @@ export const ExploreConfigContext: React.FC = ({ children }) => {
             newParams.delete('fields')
         else
             newParams.set('fields', Array.from(fields).join(','))
+        history.push({
+            pathname: history.location.pathname,
+            search: newParams.toString(),
+        })
+    }
+
+    // Sidebar state
+    const sidebarPanel: SidebarPanel = (searchParams.get('sidebar') || 'base') === 'explores' ? 'explores' : 'base'
+    const setSidebarPanel = (panelName: SidebarPanel) => {
+        const newParams = new URLSearchParams(searchParams)
+        newParams.set('sidebar', panelName)
         history.push({
             pathname: history.location.pathname,
             search: newParams.toString(),
@@ -52,6 +77,8 @@ export const ExploreConfigContext: React.FC = ({ children }) => {
         setActiveTableName,
         activeFields,
         toggleActiveField,
+        sidebarPanel,
+        setSidebarPanel,
     }
 
     return (
@@ -60,12 +87,20 @@ export const ExploreConfigContext: React.FC = ({ children }) => {
 }
 
 export const useExploreConfig = () => {
-    const { activeTableName, setActiveTableName, activeFields, toggleActiveField } = React.useContext(context)
+    const {
+        activeTableName,
+        setActiveTableName,
+        activeFields,
+        toggleActiveField,
+        sidebarPanel,
+        setSidebarPanel } = React.useContext(context)
 
     return {
         activeTableName,
         setActiveTableName,
         activeFields,
         toggleActiveField,
+        sidebarPanel,
+        setSidebarPanel,
     }
 }
