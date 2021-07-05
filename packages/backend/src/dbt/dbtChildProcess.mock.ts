ba8c1cf1cf1f7539833fef14dcd05adb6d7f3a81
Jose Rego <jose@lightdash.com>
2021-07-05 15:29:39 +0100

    fix dbt child process event handler

---
+++ packages/backend/src/dbt/dbtChildProcess.mock.ts
@@ -0,0 +1,54 @@
+const successLog = {
+    message: 'Send requests to ',
+};
+
+const infoLog = {
+    levelname: 'INFO',
+    message: 'log1',
+};
+
+const errorLog = {
+    levelname: 'ERROR',
+    message: 'log1',
+};
+
+const childProcessBaseMock = {
+    kill: jest.fn(),
+    on: jest.fn(),
+    stdout: {
+        on: jest.fn(),
+    },
+};
+
+export const childProcessWithExitEvent = {
+    ...childProcessBaseMock,
+    on: (_: string, callback: () => void) => callback(),
+};
+
+export const childProcessWithSuccessEvent = {
+    ...childProcessBaseMock,
+    stdout: {
+        on: (_: string, callback: (data: string) => void) =>
+            callback(JSON.stringify(successLog)),
+    },
+};
+
+export const childProcessWithDoubleLogEvent = {
+    ...childProcessBaseMock,
+    stdout: {
+        on: (_: string, callback: (data: string) => void) =>
+            callback(
+                `${JSON.stringify(infoLog)}\n${JSON.stringify(
+                    errorLog,
+                )}\n${JSON.stringify(successLog)}`,
+            ),
+    },
+};
+
+export const childProcessWithUnexpectedEvent = {
+    ...childProcessBaseMock,
+    stdout: {
+        on: (_: string, callback: (data: string) => void) =>
+            callback('not a json object'),
+    },
+};
