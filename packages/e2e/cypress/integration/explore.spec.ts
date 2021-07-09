2c72109d46837fc3b8181575da7d3fe036097a22
Jose Rego <jose@lightdash.com>
2021-07-09 14:03:41 +0100

    with e2e testss

---
+++ packages/e2e/cypress/integration/explore.spec.ts
@@ -0,0 +1,25 @@
+describe('Smoke Test', () => {
+    it('Query orders', () => {
+        cy.visit('/');
+
+        // Close query dev tools
+        cy.get('body').then((body) => {
+            if (body.find('.ReactQueryDevtools').length > 0) {
+                cy.get('.ReactQueryDevtools')
+                    .findByRole('button', { name: /Close/i })
+                    .click();
+            }
+        });
+        cy.findByText('Orders').click();
+        cy.findByText('First name').click();
+        cy.findByText('Total orders').click();
+        cy.get('th').findByText('First name').click();
+        cy.findAllByRole('button', { name: /Run query/i })
+            .first()
+            .click();
+        cy.findByText('Loading results', { timeout: 10000 }).should(
+            'not.exist',
+        );
+        cy.get('td').first().should('have.text', 'Aaron');
+    });
+});
