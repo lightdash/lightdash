179ea45cde45a330227c86fd7b56f4ad720d742c
Jose Rego <jose@lightdash.com>
2021-07-12 14:20:53 +0100

    add dbt seed full refresh

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
+dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
 dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
 yarn install
 yarn common-build
--- examples/full-jaffle-shop-demo/entrypoint.sh
+++ examples/full-jaffle-shop-demo/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
+dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
 dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
 exec "$@"
