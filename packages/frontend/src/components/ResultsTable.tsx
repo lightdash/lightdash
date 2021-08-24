b633df0517c68916bf514fbef857f613cd9fcc5c
Jose Rego <jose@lightdash.com>
2021-08-24 17:10:53 +0100

    reorder table columns

--- packages/frontend/src/components/ResultsTable.tsx
+++ packages/frontend/src/components/ResultsTable.tsx
@@ -1,4 +1,9 @@
-import { usePagination, useTable as useReactTable } from 'react-table';
+import {
+    HeaderGroup,
+    useColumnOrder,
+    usePagination,
+    useTable as useReactTable,
+} from 'react-table';
 import {
     Button,
     Colors,
@@ -8,8 +13,16 @@ import {
     Spinner,
     Tag,
 } from '@blueprintjs/core';
+import {
+    DragDropContext,
+    Draggable,
+    DraggableProvided,
+    DraggableProvidedDraggableProps,
+    DraggableStateSnapshot,
+    Droppable,
+} from 'react-beautiful-dnd';
 import { ApiError, ApiQueryResults, DimensionType } from 'common';
-import React from 'react';
+import React, { FC, useEffect } from 'react';
 import { CSVLink } from 'react-csv';
 import { UseQueryResult } from 'react-query';
 import { useColumns } from '../hooks/useColumns';
@@ -68,80 +81,15 @@ const EmptyStateNoRows = () => (
     />
 );
 
-type ResultsTableProps = {
-    queryResults: UseQueryResult<ApiQueryResults, ApiError>;
-};
-export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
-    const columns = useColumns();
-    const {
-        state: { tableName: activeTableName },
-    } = useExplorer();
-    const activeExplore = useTable();
-    const safeData = React.useMemo(
-        () => (queryResults.status === 'success' ? queryResults.data.rows : []),
-        [queryResults.status, queryResults.data],
-    );
-
-    const tableInstance = useReactTable(
-        {
-            columns,
-            data: safeData,
-            initialState: {
-                pageIndex: 0,
-                pageSize: 25,
-            },
-        },
-        usePagination,
-    );
-
-    const getColumnStyle = (isDimension: boolean) => ({
-        style: {
-            backgroundColor: isDimension
-                ? hexToRGB(Colors.BLUE1, 0.2)
-                : hexToRGB(Colors.ORANGE1, 0.2),
-        },
-    });
-
-    const getRowStyle = (rowIndex: number, isDimension: boolean) => ({
-        style: {
-            backgroundColor: rowIndex % 2 ? undefined : Colors.LIGHT_GRAY4,
-            textAlign: isDimension ? ('left' as 'left') : ('right' as 'right'),
-        },
-    });
-
-    const getHeaderStyle = (isDimension: boolean) => ({
-        style: {
-            textAlign: isDimension ? ('left' as 'left') : ('right' as 'right'),
-        },
-    });
-
-    if (activeExplore.isLoading) return <EmptyStateExploreLoading />;
-
-    if (tableInstance.columns.length === 0) return <EmptyStateNoColumns />;
-
-    const getSortIndicator = (
-        isDimension: boolean,
-        dimensionType: DimensionType,
-        desc: boolean,
-        sortIndex: number,
-        isMultiSort: boolean,
-    ) => {
-        const style = { paddingLeft: '5px' };
-        if (isDimension && dimensionType === 'string')
-            return (
-                <>
-                    {isMultiSort && (
-                        <Tag minimal style={style}>
-                            {sortIndex + 1}
-                        </Tag>
-                    )}
-                    {desc ? (
-                        <Icon style={style} icon="sort-alphabetical-desc" />
-                    ) : (
-                        <Icon style={style} icon="sort-alphabetical" />
-                    )}
-                </>
-            );
+const getSortIndicator = (
+    isDimension: boolean,
+    dimensionType: DimensionType,
+    desc: boolean,
+    sortIndex: number,
+    isMultiSort: boolean,
+) => {
+    const style = { paddingLeft: '5px' };
+    if (isDimension && dimensionType === 'string')
         return (
             <>
                 {isMultiSort && (
@@ -150,13 +98,136 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                     </Tag>
                 )}
                 {desc ? (
-                    <Icon style={style} icon="sort-numerical-desc" />
+                    <Icon style={style} icon="sort-alphabetical-desc" />
                 ) : (
-                    <Icon style={style} icon="sort-numerical" />
+                    <Icon style={style} icon="sort-alphabetical" />
                 )}
             </>
         );
-    };
+    return (
+        <>
+            {isMultiSort && (
+                <Tag minimal style={style}>
+                    {sortIndex + 1}
+                </Tag>
+            )}
+            {desc ? (
+                <Icon style={style} icon="sort-numerical-desc" />
+            ) : (
+                <Icon style={style} icon="sort-numerical" />
+            )}
+        </>
+    );
+};
+
+const getColumnStyle = (isDimension: boolean) => ({
+    style: {
+        backgroundColor: isDimension
+            ? hexToRGB(Colors.BLUE1, 0.2)
+            : hexToRGB(Colors.ORANGE1, 0.2),
+    },
+});
+
+const getRowStyle = (rowIndex: number, isDimension: boolean) => ({
+    style: {
+        backgroundColor: rowIndex % 2 ? undefined : Colors.LIGHT_GRAY4,
+        textAlign: isDimension ? ('left' as 'left') : ('right' as 'right'),
+    },
+});
+
+const getItemStyle = (
+    { isDragging, isDropAnimating }: DraggableStateSnapshot,
+    draggableStyle: DraggableProvidedDraggableProps['style'],
+): object => ({
+    ...draggableStyle,
+    userSelect: 'none',
+    ...(!isDragging && { transform: 'translate(0,0)' }),
+    ...(isDropAnimating && { transitionDuration: '0.001s' }),
+});
+
+interface ItemProps {
+    column?: HeaderGroup;
+    provided: DraggableProvided;
+    snapshot: DraggableStateSnapshot;
+}
+
+const Item: FC<ItemProps> = ({
+    column,
+    provided: { draggableProps, innerRef, dragHandleProps },
+    snapshot,
+}) => (
+    <div
+        {...draggableProps}
+        {...dragHandleProps}
+        ref={innerRef}
+        style={{
+            ...getItemStyle(snapshot, draggableProps.style),
+        }}
+    >
+        {column?.render('Header')}
+        {column?.isSorted &&
+            getSortIndicator(
+                column.isDimension,
+                column.dimensionType,
+                column.isSortedDesc || false,
+                column.sortedIndex,
+                column.isMultiSort,
+            )}
+    </div>
+);
+
+type ResultsTableProps = {
+    queryResults: UseQueryResult<ApiQueryResults, ApiError>;
+};
+export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
+    const dataColumns = useColumns();
+    const {
+        state: { tableName: activeTableName, columnOrder: explorerColumnOrder },
+        actions: { setColumnOrder: setExplorerColumnOrder },
+    } = useExplorer();
+    const activeExplore = useTable();
+    const safeData = React.useMemo(
+        () => (queryResults.status === 'success' ? queryResults.data.rows : []),
+        [queryResults.status, queryResults.data],
+    );
+    const currentColOrder = React.useRef<Array<string>>([]);
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        page,
+        rows,
+        prepareRow,
+        allColumns,
+        setColumnOrder,
+        columns,
+        pageCount,
+        nextPage,
+        canNextPage,
+        previousPage,
+        canPreviousPage,
+        state: { pageIndex },
+    } = useReactTable(
+        {
+            columns: dataColumns,
+            data: safeData,
+            initialState: {
+                pageIndex: 0,
+                pageSize: 25,
+                columnOrder: explorerColumnOrder,
+            },
+        },
+        usePagination,
+        useColumnOrder,
+    );
+
+    useEffect(() => {
+        setColumnOrder(explorerColumnOrder);
+    }, [setColumnOrder, explorerColumnOrder]);
+
+    if (activeExplore.isLoading) return <EmptyStateExploreLoading />;
+
+    if (columns.length === 0) return <EmptyStateNoColumns />;
 
     return (
         <div
@@ -173,12 +244,11 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                 <HTMLTable
                     bordered
                     condensed
-                    {...tableInstance.getTableProps()}
+                    {...getTableProps()}
                     style={{ width: '100%' }}
                 >
-                    {tableInstance.headerGroups.map((headerGroup, idx) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <colgroup key={idx}>
+                    {headerGroups.map((headerGroup) => (
+                        <colgroup key={headerGroup.id}>
                             {headerGroup.headers.map((column) => (
                                 <col
                                     {...column.getHeaderProps([
@@ -189,32 +259,107 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                         </colgroup>
                     ))}
                     <thead>
-                        {tableInstance.headerGroups.map((headerGroup) => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map((column) => (
-                                    <th
-                                        {...column.getHeaderProps([
-                                            column.getSortByToggleProps(),
-                                            getHeaderStyle(column.isDimension),
-                                        ])}
-                                    >
-                                        {column.render('Header')}
-                                        {column.isSorted &&
-                                            getSortIndicator(
-                                                column.isDimension,
-                                                column.dimensionType,
-                                                column.isSortedDesc || false,
-                                                column.sortedIndex,
-                                                column.isMultiSort,
+                        {headerGroups.map((headerGroup) => (
+                            <DragDropContext
+                                onDragStart={() => {
+                                    currentColOrder.current = allColumns.map(
+                                        (o: any) => o.id,
+                                    );
+                                }}
+                                onDragUpdate={(dragUpdateObj) => {
+                                    const colOrder = [
+                                        ...currentColOrder.current,
+                                    ];
+                                    const sIndex = dragUpdateObj.source.index;
+                                    const dIndex =
+                                        dragUpdateObj.destination &&
+                                        dragUpdateObj.destination.index;
+
+                                    if (typeof dIndex === 'number') {
+                                        colOrder.splice(sIndex, 1);
+                                        colOrder.splice(
+                                            dIndex,
+                                            0,
+                                            dragUpdateObj.draggableId,
+                                        );
+                                        setExplorerColumnOrder(colOrder);
+                                    }
+                                }}
+                                onDragEnd={() => undefined}
+                            >
+                                <Droppable
+                                    droppableId="droppable"
+                                    direction="horizontal"
+                                    renderClone={(
+                                        provided,
+                                        snapshot,
+                                        rubric,
+                                    ) => {
+                                        const column = headerGroup.headers.find(
+                                            ({ id }) =>
+                                                id === rubric.draggableId,
+                                        );
+                                        return (
+                                            <Item
+                                                column={column}
+                                                provided={provided}
+                                                snapshot={snapshot}
+                                            />
+                                        );
+                                    }}
+                                >
+                                    {(droppableProvided) => (
+                                        <tr
+                                            ref={droppableProvided.innerRef}
+                                            {...headerGroup.getHeaderGroupProps()}
+                                            {...droppableProvided.droppableProps}
+                                        >
+                                            {headerGroup.headers.map(
+                                                (column, index) => (
+                                                    <th
+                                                        {...column.getHeaderProps(
+                                                            [
+                                                                column.getSortByToggleProps(),
+                                                            ],
+                                                        )}
+                                                    >
+                                                        <Draggable
+                                                            key={column.id}
+                                                            draggableId={
+                                                                column.id
+                                                            }
+                                                            index={index}
+                                                        >
+                                                            {(
+                                                                provided,
+                                                                snapshot,
+                                                            ) => (
+                                                                <Item
+                                                                    column={
+                                                                        column
+                                                                    }
+                                                                    provided={
+                                                                        provided
+                                                                    }
+                                                                    snapshot={
+                                                                        snapshot
+                                                                    }
+                                                                />
+                                                            )}
+                                                        </Draggable>
+                                                    </th>
+                                                ),
                                             )}
-                                    </th>
-                                ))}
-                            </tr>
+                                        </tr>
+                                    )}
+                                </Droppable>
+                            </DragDropContext>
                         ))}
                     </thead>
-                    <tbody {...tableInstance.getTableBodyProps()}>
-                        {tableInstance.page.map((row) => {
-                            tableInstance.prepareRow(row);
+
+                    <tbody {...getTableBodyProps()}>
+                        {page.map((row) => {
+                            prepareRow(row);
                             return (
                                 <tr {...row.getRowProps()}>
                                     {row.cells.map((cell) => (
@@ -259,12 +404,12 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                 }}
             >
                 <div>
-                    {tableInstance.rows.length > 0 ? (
+                    {rows.length > 0 ? (
                         <CSVLink
                             role="button"
                             tabIndex={0}
                             className="bp3-button"
-                            data={tableInstance.rows.map((row) => row.values)}
+                            data={rows.map((row) => row.values)}
                             filename={`lightdash-${
                                 activeTableName || 'export'
                             }-${new Date().toISOString().slice(0, 10)}.csv`}
@@ -275,7 +420,7 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                         </CSVLink>
                     ) : null}
                 </div>
-                {tableInstance.pageCount > 1 && (
+                {pageCount > 1 && (
                     <div
                         style={{
                             display: 'flex',
@@ -284,11 +429,8 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                             alignItems: 'center',
                         }}
                     >
-                        {tableInstance.canPreviousPage && (
-                            <Button
-                                icon="arrow-left"
-                                onClick={tableInstance.previousPage}
-                            />
+                        {canPreviousPage && (
+                            <Button icon="arrow-left" onClick={previousPage} />
                         )}
                         <span
                             style={{
@@ -296,14 +438,10 @@ export const ResultsTable = ({ queryResults }: ResultsTableProps) => {
                                 paddingLeft: '5px',
                             }}
                         >
-                            Page {tableInstance.state.pageIndex + 1} of{' '}
-                            {tableInstance.pageCount}
+                            Page {pageIndex + 1} of {pageCount}
                         </span>
-                        {tableInstance.canNextPage && (
-                            <Button
-                                icon="arrow-right"
-                                onClick={tableInstance.nextPage}
-                            />
+                        {canNextPage && (
+                            <Button icon="arrow-right" onClick={nextPage} />
                         )}
                     </div>
                 )}
