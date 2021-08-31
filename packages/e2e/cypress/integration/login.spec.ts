de03f35f8e5079bfb98476fb8a737ff71e4706b3
Andreia Freitas <andgofreitas@gmail.com>
2021-08-31 18:29:18 +0100

    format login.spec.ts code

--- packages/e2e/cypress/integration/login.spec.ts
+++ packages/e2e/cypress/integration/login.spec.ts
@@ -1,25 +1,28 @@
 import { USER_SEED } from 'common';
 
 describe('Login', () => {
-
     it('Should login successfully =', () => {
         cy.visit('/login');
         cy.get('[data-cy="email"]').type(USER_SEED.email);
         cy.get('[ data-cy="password"]').type(USER_SEED.password);
         cy.get('[data-cy="login-button"]').click();
-        cy.url().should('include', '/tables')
+        cy.url().should('include', '/tables');
     });
     it('Should display error message when credentials are invalid', () => {
         cy.visit('/login');
-        cy.get('[data-cy="email"]').type("test-email");
-        cy.get('[ data-cy="password"]').type("test-password");
+        cy.get('[data-cy="email"]').type('test-email');
+        cy.get('[ data-cy="password"]').type('test-password');
         cy.get('[data-cy="login-button"]').click();
-        cy.findByText('Email and password not recognized.').should('be.visible')
+        cy.findByText('Email and password not recognized.').should(
+            'be.visible',
+        );
     });
     it('Should display error message when one field is empty', () => {
         cy.visit('/login');
-        cy.get('[data-cy="email"]').type("test-email");
+        cy.get('[data-cy="email"]').type('test-email');
         cy.get('[data-cy="login-button"]').click();
-        cy.findByText('Required fields: email and password').should('be.visible')
+        cy.findByText('Required fields: email and password').should(
+            'be.visible',
+        );
     });
 });
