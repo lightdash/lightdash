ffecb0449aa4caf89d9df20140943f62584dc40b
oliverlaslett <olaslett@gmail.com>
2021-09-20 11:28:50 +0100

    WIP backend for mulitple projects

--- packages/backend/src/services/ProjectService.ts
+++ packages/backend/src/services/ProjectService.ts
@@ -3,6 +3,8 @@ import {
     DbtProjectConfig,
     Explore,
     ExploreError,
+    isExploreError,
+    MetricQuery,
     Project,
     SessionUser,
     WarehouseCredentials,
@@ -13,6 +15,9 @@ import { ProjectAdapter } from '../types';
 import { ProjectModel } from '../models/ProjectModel';
 import { DbtLocalProjectAdapter } from '../projectAdapters/dbtLocalProjectAdapter';
 import { analytics } from '../analytics/client';
+import { errorHandler, NotExistsError } from '../errors';
+import { compileMetricQuery } from '../queryCompiler';
+import { buildQuery } from '../queryBuilder';
 
 type ProjectServiceDependencies = {
     lightdashConfig: LightdashConfig;
@@ -22,18 +27,51 @@ type ProjectServiceDependencies = {
 // Will return project with a working adapter, credentials.
 // Interface does not care about profiles.yml vs. warehouse credentials etc.
 export class ProjectService {
+    // TODO: CONFIG SHOULD BE FROM PROJECT MODEL
     projectConfig: DbtProjectConfig;
 
+    // TODO: This is replaced with projectAdapters (below)
     projectAdapter: ProjectAdapter;
 
+    // TODO: This should exist on project model
     warehouseCredentials: WarehouseCredentials | undefined;
 
     projectModel: ProjectModel;
+
+    cachedExplores: Record<string, Promise<(Explore | ExploreError)[]>>;
+
+    projectLoading: Record<string, boolean>;
+
+    projectAdapters: Record<string, ProjectAdapter>;
 
     constructor({ lightdashConfig, projectModel }: ProjectServiceDependencies) {
         [this.projectConfig] = lightdashConfig.projects;
         this.projectModel = projectModel;
         this.projectAdapter = projectAdapterFromConfig(this.projectConfig);
+        this.projectAdapters = {};
+        this.projectLoading = {};
+        this.cachedExplores = {};
+    }
+
+    async getProjectStatus(
+        projectUuid: string,
+        user: SessionUser,
+    ): Promise<'loading' | 'error' | 'ready'> {
+        // check access
+        const { [projectUuid]: isLoading = false } = this.projectLoading;
+        if (isLoading) {
+            return 'loading';
+        }
+        const explore = this.cachedExplores[projectUuid];
+        if (explore === undefined) {
+            return 'error';
+        }
+        try {
+            await explore;
+        } catch (e) {
+            return 'error';
+        }
+        return 'ready';
     }
 
     async getProject(projectUuid: string, user: SessionUser): Promise<Project> {
@@ -61,20 +99,98 @@ export class ProjectService {
         return project;
     }
 
-    async runQuery(sql: string): Promise<Record<string, any>> {
-        return this.projectAdapter.runQuery(sql);
+    // TODO: THIS DOES NOT WORK
+    async startAdapter(projectUuid: string): Promise<ProjectAdapter> {
+        const adapter = this.projectAdapters[projectUuid];
+        if (adapter !== undefined) {
+            return adapter;
+        }
+        return projectAdapterFromConfig(this.projectConfig);
     }
 
-    async compileAllExplores(): Promise<(Explore | ExploreError)[]> {
-        if (this.projectAdapter instanceof DbtLocalProjectAdapter) {
-            const project =
-                await this.projectModel.getDefaultWithSensitiveFields();
+    async runQuery(
+        user: SessionUser,
+        metricQuery: MetricQuery,
+        projectUuid: string,
+        exploreName: string,
+    ): Promise<Record<string, any>> {
+        const explore = await this.getExplore(user, projectUuid, exploreName);
+        const compiledMetricQuery = compileMetricQuery(metricQuery);
+        const sql = buildQuery({ explore, compiledMetricQuery });
+        const adapter = await this.startAdapter(projectUuid);
+        const rows = await adapter.runQuery(sql);
+        return rows;
+    }
+
+    async compileAllExplores(
+        projectUuid: string,
+    ): Promise<(Explore | ExploreError)[]> {
+        const adapter = await this.startAdapter(projectUuid);
+        if (adapter instanceof DbtLocalProjectAdapter) {
+            const project = await this.projectModel.getWithSensitiveFields(
+                projectUuid,
+            );
             if (project.warehouseConnection) {
-                await this.projectAdapter.updateProfile(
-                    project.warehouseConnection,
-                );
+                await adapter.updateProfile(project.warehouseConnection);
             }
         }
-        return this.projectAdapter.compileAllExplores();
+        return adapter.compileAllExplores();
+    }
+
+    async refreshAllTables(user: SessionUser, projectUuid: string) {
+        this.projectLoading[projectUuid] = true;
+        this.cachedExplores[projectUuid] = this.compileAllExplores(projectUuid);
+        try {
+            await this.cachedExplores[projectUuid];
+            analytics.track({
+                event: 'project.compiled',
+                userId: user.userUuid,
+                anonymousId: '', // TODO: remove anon id - not needed
+                properties: {
+                    projectType: this.projectConfig.type,
+                },
+            });
+        } catch (e) {
+            const errorResponse = errorHandler(e);
+            analytics.track({
+                event: 'project.error',
+                userId: user.userUuid,
+                properties: {
+                    name: errorResponse.name,
+                    statusCode: errorResponse.statusCode,
+                    projectType: this.projectConfig.type,
+                },
+            });
+            throw errorResponse;
+        } finally {
+            this.projectLoading[projectUuid] = false;
+        }
+        return this.cachedExplores[projectUuid];
+    }
+
+    async getAllExplores(
+        user: SessionUser,
+        projectUuid: string,
+    ): Promise<(Explore | ExploreError)[]> {
+        const explores = this.cachedExplores[projectUuid];
+        if (explores === undefined) {
+            return this.refreshAllTables(user, projectUuid);
+        }
+        return explores;
+    }
+
+    async getExplore(
+        user: SessionUser,
+        projectUuid: string,
+        exploreName: string,
+    ): Promise<Explore> {
+        const explores = await this.getAllExplores(user, projectUuid);
+        const explore = explores.find((t) => t.name === exploreName);
+        if (explore === undefined || isExploreError(explore)) {
+            throw new NotExistsError(
+                `Explore "${exploreName}" does not exist.`,
+            );
+        }
+        return explore;
     }
 }
