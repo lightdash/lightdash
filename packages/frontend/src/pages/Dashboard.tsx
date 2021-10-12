e99056696198e5045e3c5d77ec651276a62cc1e6
jb <jb.rougeot@lightdash.com>
2021-10-12 14:52:06 +0100

    change Dashboard to Dashboards

--- packages/frontend/src/pages/Dashboard.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Dashboard = () => <>Dashboard</>;
-
-export default Dashboard;
--- packages/frontend/src/types/Events.ts
+++ packages/frontend/src/types/Events.ts
@@ -21,7 +21,7 @@ export enum PageName {
     INVITE_MANAGEMENT_SETTINGS = 'invite_management_settings',
     ABOUT_LIGHTDASH = 'about_lightdash',
     CREATE_PROJECT = 'create_project',
-    DASHBOARD = 'view_dashboard',
+    DASHBOARDS = 'view_dashboards',
 }
 
 export enum CategoryName {
