231c464ffe5dddef2c6ec41bc572442469869b1c
jb <jb.rougeot@lightdash.com>
2021-10-21 08:25:56 +0100

    Add saved_dashboard seed rename seed so that they are run in alphabetical order

---
+++ packages/backend/src/database/seeds/development/3.saved_dashboard.ts
@@ -0,0 +1,34 @@
+import { Knex } from 'knex';
+import { DashboardTileTypes } from 'common';
+import { DashboardModel } from '../../../models/DashboardModel';
+import { getSpace, getSpaceWithQueries } from '../../entities/spaces';
+
+export async function seed(knex: Knex): Promise<void> {
+    // delete existing dashboards
+    await knex('dashboards').del();
+
+    // Get the project id
+    const [{ project_uuid: projectUuid }] = await knex('projects')
+        .select('*')
+        .limit(1);
+
+    // get Space
+    const { space_uuid: spaceUuid } = await getSpace(knex, projectUuid);
+    const dashboardModel = new DashboardModel({
+        database: knex,
+    });
+
+    const { queries } = await getSpaceWithQueries(projectUuid);
+
+    await dashboardModel.create(spaceUuid, {
+        name: 'Jaffle dashboard',
+        tiles: queries.map(({ uuid: savedChartUuid }, i) => ({
+                x: i % 2 === 0 ? 0 : 5,
+                y: Math.floor(i / 2) * 3,
+                w: i > 0 && i % 2 === 0 ? 10 : 5,
+                h: 3,
+                type: DashboardTileTypes.SAVED_CHART,
+                properties: { savedChartUuid },
+            })),
+    });
+}
