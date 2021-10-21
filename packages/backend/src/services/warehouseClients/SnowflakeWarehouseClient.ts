0b24032eb011eefecaa7d905ec39cf15b219eb9d
Jose Rego <ZeRego@users.noreply.github.com>
2021-10-21 16:31:30 +0100

    feat(#642): add snowflake warehouse adapter
    * feat(#642): add snowflake warehouse adapter
    
    * fix: create and destroy snowflake connection of each query run

---
+++ packages/backend/src/services/warehouseClients/SnowflakeWarehouseClient.ts
@@ -0,0 +1,69 @@
+import { CreateSnowflakeCredentials } from 'common';
+import { createConnection, Connection, ConnectionOptions } from 'snowflake-sdk';
+import { WarehouseConnectionError, WarehouseQueryError } from '../../errors';
+import { QueryRunner } from '../../types';
+
+export default class SnowflakeWarehouseClient implements QueryRunner {
+    connectionOptions: ConnectionOptions;
+
+    constructor(credentials: CreateSnowflakeCredentials) {
+        this.connectionOptions = {
+            account: credentials.account,
+            username: credentials.user,
+            password: credentials.password,
+            database: credentials.database,
+            schema: credentials.schema,
+            warehouse: credentials.warehouse,
+            role: credentials.role,
+            clientSessionKeepAlive: credentials.clientSessionKeepAlive,
+        };
+    }
+
+    async runQuery(sqlText: string): Promise<Record<string, any>[]> {
+        let connection: Connection;
+        try {
+            connection = createConnection(this.connectionOptions);
+            connection.connect((err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+        } catch (e) {
+            throw new WarehouseConnectionError(e.message);
+        }
+
+        try {
+            return await new Promise((resolve, reject) => {
+                connection.execute({
+                    sqlText,
+                    complete: (err, stmt, data) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        if (data) {
+                            resolve(data);
+                        } else {
+                            reject(
+                                new WarehouseQueryError(
+                                    'Query result is undefined',
+                                ),
+                            );
+                        }
+                    },
+                });
+            });
+        } catch (e) {
+            throw new WarehouseQueryError(e.message);
+        } finally {
+            connection.destroy((err) => {
+                if (err) {
+                    throw new WarehouseConnectionError(err.message);
+                }
+            });
+        }
+    }
+
+    async test(): Promise<void> {
+        await this.runQuery('SELECT 1');
+    }
+}
--- packages/backend/src/services/warehouseClients/warehouseClientFromCredentials.ts
+++ packages/backend/src/services/warehouseClients/warehouseClientFromCredentials.ts
@@ -1,5 +1,6 @@
 import { CreateWarehouseCredentials, WarehouseTypes } from 'common';
 import { QueryRunner } from '../../types';
+import SnowflakeWarehouseClient from './SnowflakeWarehouseClient';
 import PostgresWarehouseClient from './PostgresWarehouseClient';
 import BigqueryWarehouseClient from './BigqueryWarehouseClient';
 
@@ -7,6 +8,8 @@ export const warehouseClientFromCredentials = (
     credentials: CreateWarehouseCredentials,
 ): QueryRunner | undefined => {
     switch (credentials.type) {
+        case WarehouseTypes.SNOWFLAKE:
+            return new SnowflakeWarehouseClient(credentials);
         case WarehouseTypes.POSTGRES:
             return new PostgresWarehouseClient(credentials);
         case WarehouseTypes.BIGQUERY:
