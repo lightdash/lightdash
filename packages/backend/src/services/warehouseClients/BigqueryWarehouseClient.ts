00836689cd533b82dcbd793d4169de6a7cb061ad
Jose Rego <jose@lightdash.com>
2021-10-22 11:46:37 +0100

    fix(#665): map bigquery date/time results

--- packages/backend/src/services/warehouseClients/BigqueryWarehouseClient.ts
+++ packages/backend/src/services/warehouseClients/BigqueryWarehouseClient.ts
@@ -1,7 +1,28 @@
 import { CreateBigqueryCredentials } from 'common';
-import { BigQuery } from '@google-cloud/bigquery';
+import {
+    BigQuery,
+    BigQueryDate,
+    BigQueryDatetime,
+    BigQueryTime,
+    BigQueryTimestamp,
+} from '@google-cloud/bigquery';
 import { WarehouseConnectionError, WarehouseQueryError } from '../../errors';
 import { QueryRunner } from '../../types';
+
+// START workaround for https://github.com/lightdash/lightdash/issues/665. Please find better solution :pray:
+function toJSON() {
+    // @ts-ignore
+    return this.value;
+}
+// @ts-ignore
+BigQueryDate.prototype.toJSON = toJSON;
+// @ts-ignore
+BigQueryTimestamp.prototype.toJSON = toJSON;
+// @ts-ignore
+BigQueryDatetime.prototype.toJSON = toJSON;
+// @ts-ignore
+BigQueryTime.prototype.BigQueryTime = toJSON;
+// END workaround
 
 export default class BigqueryWarehouseClient implements QueryRunner {
     client: BigQuery;
