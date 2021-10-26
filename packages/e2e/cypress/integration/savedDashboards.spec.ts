3970b9dc520fe04d52ede8fa79b9714bf1df46d3
Jose Rego <jose@lightdash.com>
2021-10-26 13:27:14 +0100

    amend e2e tests

--- packages/e2e/cypress/integration/savedDashboards.spec.ts
+++ packages/e2e/cypress/integration/savedDashboards.spec.ts
@@ -14,27 +14,7 @@ describe('Dashboard List', () => {
         cy.visit('/');
         cy.get('[data-cy=browse]').trigger('mouseover');
         cy.findByRole('button', { name: 'Dashboards' }).click();
-        cy.get('h5').should('have.text', 'Jaffle dashboard');
-    });
-
-    it('Should update dashboards', () => {
-        cy.visit('/');
-        cy.get('[data-cy=browse]').trigger('mouseover');
-        cy.findByRole('button', { name: 'Dashboards' }).click();
-        // click on rename
-        cy.findByRole('button', { name: 'edit Rename' }).click();
-        cy.findByLabelText('Name (required)').should(
-            'have.value',
-            'Jaffle dashboard',
-        );
-        cy.findByLabelText('Name (required)').focus().clear();
-        cy.findByLabelText('Name (required)').type('updated dashboard');
-        cy.findByLabelText('Description (optional)').type('description');
-        // click on save
-        cy.findByRole('button', { name: 'Save' }).click();
-
-        // verify dashboard name has been updated in the list
-        cy.findByText('updated dashboard').should('exist');
+        cy.findByText('Jaffle dashboard').should('exist');
     });
 
     it('Should delete dashboards', () => {
@@ -53,8 +33,30 @@ describe('Dashboard List', () => {
         cy.get('[data-cy=browse]').trigger('mouseover');
         cy.findByRole('button', { name: 'Dashboards' }).click();
         cy.findByRole('button', { name: 'New dashboard' }).click();
-        cy.findByLabelText('Name (required)').type('Jaffle dashboard');
+
+        cy.url().should(
+            'match',
+            new RegExp(
+                /.*\/projects\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\/dashboards\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/,
+            ),
+        );
+        cy.findByText('Untitled dashboard').should('exist');
+    });
+
+    it('Should update dashboards', () => {
+        cy.visit('/');
+        cy.get('[data-cy=browse]').trigger('mouseover');
+        cy.findByRole('button', { name: 'Dashboards' }).click();
+        // click on rename
+        cy.findByRole('button', { name: 'edit Rename' }).click();
+        cy.findByLabelText('Name (required)').clear().type('Jaffle dashboard');
+        cy.findByLabelText('Description (optional)')
+            .clear()
+            .type('description');
         // click on save
-        cy.findByRole('button', { name: 'Create' }).click();
+        cy.findByRole('button', { name: 'Save' }).click();
+
+        // verify dashboard name has been updated in the list
+        cy.findByText('Jaffle dashboard').should('exist');
     });
 });
