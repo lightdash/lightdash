57e51b797fada6efcf2e835a22c20359d839fa87
Jose Rego <ZeRego@users.noreply.github.com>
2021-11-01 16:56:57 +0000

    feat(#686): get catalog from Postgres warehouse (#716)
    * wip: get schema from warehouse
    
    * get warehouse schema for necessary dataset and tables
    
    * add unit tests
    
    * only get necessary columns + unit tests
    
    * add unit tests
    
    * refactor: snowflake schema
    
    * refactor: clean artifacts
    
    * wip: get types from warehouse
    
    * wip: unit tests
    
    * finish unit tests
    
    * add more data types
    
    * fix: typo
    
    * exclude false positives

---
+++ packages/backend/src/services/warehouseClients/PostgresWarehouseClient.mock.ts
@@ -0,0 +1,58 @@
+import { CreatePostgresCredentials, WarehouseTypes } from 'common';
+import { config } from './WarehouseClient.mock';
+import { PostgresTypes } from './PostgresWarehouseClient';
+
+export const credentials: CreatePostgresCredentials = {
+    type: WarehouseTypes.POSTGRES,
+    host: '',
+    user: '',
+    password: '',
+    dbname: '',
+    schema: '',
+    threads: 1,
+    port: 5432,
+};
+
+const columnBase = {
+    table_catalog: config[0].database,
+    table_schema: config[0].schema,
+    table_name: config[0].table,
+};
+
+export const columns: Record<string, any>[] = [
+    {
+        ...columnBase,
+        column_name: 'myStringColumn',
+        data_type: PostgresTypes.TEXT,
+    },
+    {
+        ...columnBase,
+        column_name: 'myNumberColumn',
+        data_type: PostgresTypes.NUMERIC,
+    },
+    {
+        ...columnBase,
+        column_name: 'myDateColumn',
+        data_type: PostgresTypes.DATE,
+    },
+    {
+        ...columnBase,
+        column_name: 'myTimestampColumn',
+        data_type: PostgresTypes.TIMESTAMP,
+    },
+    {
+        ...columnBase,
+        column_name: 'myBooleanColumn',
+        data_type: PostgresTypes.BOOLEAN,
+    },
+    {
+        ...columnBase,
+        column_name: 'myArrayColumn',
+        data_type: PostgresTypes.JSONB,
+    },
+    {
+        ...columnBase,
+        column_name: 'myObjectColumn',
+        data_type: PostgresTypes.JSON,
+    },
+];
