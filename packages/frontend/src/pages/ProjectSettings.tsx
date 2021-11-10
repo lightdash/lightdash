9d1511961c72ca272aca4365e4507dd239d792b0
Jose Rego <ZeRego@users.noreply.github.com>
2021-11-10 16:14:34 +0000

    refactor: create generic page components + redesign project settings … (#791)
    * refactor: create generic page components + redesign project settings page
    
    * refactor: add title + generic link button
    
    * style: increate divider margin
    
    * refactor: rename component

--- packages/frontend/src/pages/ProjectSettings.tsx
+++ packages/frontend/src/pages/ProjectSettings.tsx
@@ -1,12 +1,28 @@
-import React, { FC } from 'react';
-import { Divider, H1, NonIdealState, Spinner } from '@blueprintjs/core';
-import { useParams } from 'react-router-dom';
+import React, { FC, useMemo } from 'react';
+import {
+    Menu,
+    MenuDivider,
+    NonIdealState,
+    Spinner,
+    Divider,
+    H3,
+} from '@blueprintjs/core';
+import { Redirect, Route, Switch, useParams } from 'react-router-dom';
 import { useProject } from '../hooks/useProject';
 import { UpdateProjectConnection } from '../components/ProjectConnection';
+import PageBase from '../components/common/Page/PageBase';
+import Sidebar from '../components/common/Page/Sidebar';
+import Content from '../components/common/Page/Content';
+import RouterMenuItem from '../components/common/RouterMenuItem';
 
 const ProjectSettings: FC = () => {
     const { projectUuid } = useParams<{ projectUuid: string }>();
     const { isLoading, data, error } = useProject(projectUuid);
+
+    const basePath = useMemo(
+        () => `/projects/${projectUuid}/settings`,
+        [projectUuid],
+    );
 
     if (error) {
         return (
@@ -26,22 +42,30 @@ const ProjectSettings: FC = () => {
             </div>
         );
     }
-
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '800px',
-                margin: '20px auto',
-            }}
-        >
-            <H1 style={{ margin: 0, flex: 1 }}>
-                Edit project connection: {data.name}
-            </H1>
-            <Divider style={{ margin: '20px 0' }} />
-            <UpdateProjectConnection projectUuid={projectUuid} />
-        </div>
+        <PageBase>
+            <Sidebar title="Project settings">
+                <Menu>
+                    <RouterMenuItem
+                        text="Project connections"
+                        exact
+                        to={basePath}
+                    />
+                </Menu>
+            </Sidebar>
+            <Content>
+                <Switch>
+                    <Route exact path={basePath}>
+                        <H3 style={{ marginTop: 10, marginBottom: 0 }}>
+                            Edit your project connections
+                        </H3>
+                        <Divider style={{ margin: '20px 0' }} />
+                        <UpdateProjectConnection projectUuid={projectUuid} />
+                    </Route>
+                    <Redirect to={basePath} />
+                </Switch>
+            </Content>
+        </PageBase>
     );
 };
 
