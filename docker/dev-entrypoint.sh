bf2336447ccb650280cdfe4325aa7804d335e3ec
oliverlaslett <olaslett@gmail.com>
2021-11-11 18:53:56 +0000

    sleep inifinity in entrypoint

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Setup symlink for github codespaces mount
+ln -s /workspaces/lightdash /usr/app
+
 # Setup dbt jaffle shop project
 dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
 dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
@@ -15,4 +18,7 @@ yarn workspace backend migrate
 yarn workspace backend seed
 
 # Run dev enviroment
-exec yarn dev
+yarn dev &
+
+# Keep container live
+exec sleep infinity
