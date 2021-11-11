b20420d65e290e061295348d8033c41142a29ba6
oliverlaslett <olaslett@gmail.com>
2021-11-11 19:03:09 +0000

    pro mode

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Setup symlink for github codespaces mount
-ln -s /workspaces/lightdash /usr/app
-
 # Setup dbt jaffle shop project
 dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
 dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
@@ -18,7 +15,4 @@ yarn workspace backend migrate
 yarn workspace backend seed
 
 # Run dev enviroment
-yarn dev &
-
-# Keep container live
-exec sleep infinity
+exec yarn dev
