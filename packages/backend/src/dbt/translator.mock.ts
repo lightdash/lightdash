ca198e573bc5b6584887520d0294b35656c57729
Oliver Laslett <olaslett@gmail.com>
2021-11-11 13:47:22 +0000

    refactor: split translator for dbt models (#793)
    * refactor: split translator for dbt models
    
    Co-authored-by: Jose Rego <jose@lightdash.com>

--- packages/backend/src/dbt/translator.mock.ts
+++ packages/backend/src/dbt/translator.mock.ts
@@ -1,9 +1,28 @@
-import { DbtModelColumn, DbtModelNode, DimensionType } from 'common';
+import {
+    DbtModelColumn,
+    DbtModelNode,
+    DimensionType,
+    FieldType,
+    MetricType,
+    Table,
+} from 'common';
 import { WarehouseCatalog } from '../types';
 
 const column: DbtModelColumn = {
     name: 'myColumnName',
     meta: {},
+};
+
+const COLUMN_WITH_METRIC: Record<string, DbtModelColumn> = {
+    user_id: {
+        name: 'user_id',
+        data_type: DimensionType.STRING,
+        meta: {
+            metrics: {
+                user_count: { type: MetricType.COUNT_DISTINCT },
+            },
+        },
+    },
 };
 
 export const model: DbtModelNode = {
@@ -20,6 +39,42 @@ export const model: DbtModelNode = {
     depends_on: { nodes: [] },
     root_path: 'root_path',
     patch_path: null,
+};
+
+export const MODEL_WITH_METRIC: DbtModelNode = {
+    ...model,
+    description: 'my test table',
+    columns: COLUMN_WITH_METRIC,
+};
+
+export const LIGHTDASH_TABLE_WITH_METRIC: Omit<Table, 'lineageGraph'> = {
+    name: MODEL_WITH_METRIC.name,
+    database: MODEL_WITH_METRIC.database,
+    schema: MODEL_WITH_METRIC.schema,
+    sqlTable: MODEL_WITH_METRIC.relation_name,
+    description: MODEL_WITH_METRIC.description,
+    dimensions: {
+        user_id: {
+            fieldType: FieldType.DIMENSION,
+            description: undefined,
+            type: DimensionType.STRING,
+            sql: '${TABLE}.user_id',
+            name: 'user_id',
+            table: MODEL_WITH_METRIC.name,
+            source: undefined,
+        },
+    },
+    metrics: {
+        user_count: {
+            fieldType: FieldType.METRIC,
+            type: MetricType.COUNT_DISTINCT,
+            sql: '${TABLE}.user_id',
+            name: 'user_count',
+            table: MODEL_WITH_METRIC.name,
+            description: 'Count distinct of User id',
+            source: undefined,
+        },
+    },
 };
 
 export const warehouseSchema: WarehouseCatalog = {
