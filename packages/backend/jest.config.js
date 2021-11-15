c516103ff896f4802d03c55c240ef0d2d6cf0241
Jose Rego <jose@lightdash.com>
2021-11-15 16:07:12 +0000

    chore: ignore dist folder when running tests

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  automock: false,
-  setupFiles: [
-    "./setupJest.ts"
-  ]
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    automock: false,
+    setupFiles: ['./setupJest.ts'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
