579e8fca0ea7e1cffbdaa7a4d647eb3f0e8d383a
Oliver Laslett <olaslett@gmail.com>
2021-11-26 12:43:27 +0000

    add tests

--- packages/backend/src/queryBuilder.test.ts
+++ packages/backend/src/queryBuilder.test.ts
@@ -1,5 +1,11 @@
 import { buildQuery } from './queryBuilder';
-import { EXPLORE, METRIC_QUERY, METRIC_QUERY_SQL } from './queryBuilder.mock';
+import {
+    EXPLORE,
+    METRIC_QUERY,
+    METRIC_QUERY_SQL,
+    METRIC_QUERY_TWO_TABLES,
+    METRIC_QUERY_TWO_TABLES_SQL,
+} from './queryBuilder.mock';
 
 test('Should build simple metric query', () => {
     expect(
@@ -8,4 +14,13 @@ test('Should build simple metric query', () => {
             compiledMetricQuery: METRIC_QUERY,
         }).query,
     ).toStrictEqual(METRIC_QUERY_SQL);
+});
+
+test('Should build metric query across two tables', () => {
+    expect(
+        buildQuery({
+            explore: EXPLORE,
+            compiledMetricQuery: METRIC_QUERY_TWO_TABLES,
+        }).query,
+    ).toStrictEqual(METRIC_QUERY_TWO_TABLES_SQL);
 });
