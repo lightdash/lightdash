b4eb42ec568d4663c7fd5023ca7b89ea57c6867d
Jose Rego <jose@lightdash.com>
2021-12-10 18:05:59 +0000

    tests: add onboarding unit tests

--- packages/backend/src/services/OrganizationService/OrganizationService.test.ts
+++ packages/backend/src/services/OrganizationService/OrganizationService.test.ts
@@ -18,25 +18,51 @@ jest.mock('../../config/lightdashConfig', () => ({
         mode: LightdashMode.DEFAULT,
     },
 }));
+const userModel = {
+    getAllByOrganization: jest.fn(async () => orgUsers),
+} as any as UserModel;
+const inviteLinkModel = {
+    hasActiveInvites: jest.fn(async () => true),
+} as any as InviteLinkModel;
 
 describe('DashboardService', () => {
     const service = new OrganizationService({
         organizationModel: {} as OrganizationModel,
-        userModel: {
-            getAllByOrganization: jest.fn(async () => orgUsers),
-        } as any as UserModel,
+        userModel,
         projectModel: {} as ProjectModel,
         onboardingModel: {} as OnboardingModel,
-        inviteLinkModel: {
-            hasActiveInvites: jest.fn(async () => true),
-        } as any as InviteLinkModel,
+        inviteLinkModel,
     });
     afterEach(() => {
         jest.clearAllMocks();
     });
     describe('hasInvitedUser', () => {
-        test('should return true if there are no users but has an open invite', async () => {
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+        test('should return true if there are users and has an open invite', async () => {
             expect(await service.hasInvitedUser(user)).toEqual(true);
+        });
+        test('should return true if there are users and no open invite', async () => {
+            (
+                inviteLinkModel.hasActiveInvites as jest.Mock
+            ).mockImplementationOnce(async () => false);
+            expect(await service.hasInvitedUser(user)).toEqual(true);
+        });
+        test('should return true if there are no users and has an open invite', async () => {
+            (
+                userModel.getAllByOrganization as jest.Mock
+            ).mockImplementationOnce(async () => []);
+            expect(await service.hasInvitedUser(user)).toEqual(true);
+        });
+        test('should return false if there are no users and no open invite', async () => {
+            (
+                inviteLinkModel.hasActiveInvites as jest.Mock
+            ).mockImplementationOnce(async () => false);
+            (
+                userModel.getAllByOrganization as jest.Mock
+            ).mockImplementationOnce(async () => []);
+            expect(await service.hasInvitedUser(user)).toEqual(false);
         });
     });
 });
