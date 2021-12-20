8ab134e67bb06ed0e0152ca5d506723869e4c989
Jose Rego <jose@lightdash.com>
2021-12-20 14:01:38 +0000

    test: mock config file

--- packages/backend/setupJest.ts
+++ packages/backend/setupJest.ts
@@ -1,6 +1,14 @@
+import { LightdashMode } from 'common';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { LightdashAnalytics } from './src/analytics/LightdashAnalytics';
+
 enableFetchMocks();
+
+jest.mock('./src/config/lightdashConfig', () => ({
+    lightdashConfig: {
+        mode: LightdashMode.DEFAULT,
+    },
+}));
 
 jest.mock('./src/analytics/client.ts', () => ({
     analytics: new LightdashAnalytics('notrack', 'notrack', {
