cd0b1c95672d4700e773a707b1f24122e1d1f895
Oliver Laslett <olaslett@gmail.com>
2022-01-14 17:40:54 +0000

    fix(#1127): fix variable type parsing in snowflake (#1144)
    * fix(#1127): fix variable type parsing in snowflake

--- packages/backend/src/services/warehouseClients/SnowflakeWarehouseClient.test.ts
+++ packages/backend/src/services/warehouseClients/SnowflakeWarehouseClient.test.ts
@@ -1,5 +1,8 @@
+import { DimensionType } from 'common';
 import { createConnection } from 'snowflake-sdk';
-import SnowflakeWarehouseClient from './SnowflakeWarehouseClient';
+import SnowflakeWarehouseClient, {
+    mapFieldType,
+} from './SnowflakeWarehouseClient';
 import { columns, credentials } from './SnowflakeWarehouseClient.mock';
 import {
     config,
@@ -35,5 +38,14 @@ describe('SnowflakeWarehouseClient', () => {
         expect(await warehouse.getCatalog(config)).toEqual(
             expectedWarehouseSchema,
         );
+    });
+});
+
+describe('SnowflakeTypeParsing', () => {
+    it('expect NUMBER(x,x) to be a number', () => {
+        expect(mapFieldType('NUMBER(12,10)')).toEqual(DimensionType.NUMBER);
+    });
+    it('expect VARCHAR(x) to be a string', () => {
+        expect(mapFieldType('VARCHAR(12)')).toEqual(DimensionType.STRING);
     });
 });
