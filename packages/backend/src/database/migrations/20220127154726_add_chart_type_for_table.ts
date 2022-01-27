0f3b1e598223867471e452d6e0b6cd07724fa3b0
oliverlaslett <olaslett@gmail.com>
2022-01-27 22:07:39 +0000

    add new chart type to database

---
+++ packages/backend/src/database/migrations/20220127154726_add_chart_type_for_table.ts
@@ -0,0 +1,9 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex('chart_types').insert({ chart_type: 'table' });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex('chart_types').delete().where('chart_type', 'table');
+}
