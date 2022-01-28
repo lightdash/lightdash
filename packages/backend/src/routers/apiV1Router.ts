4f38f5f8c6268f98e5bcfd9e81fd3236f6f95c3b
Oliver Laslett <olaslett@gmail.com>
2022-01-28 12:10:11 +0000

    refactor: move saved charts api to router (#1250)

--- packages/backend/src/routers/apiV1Router.ts
+++ packages/backend/src/routers/apiV1Router.ts
@@ -5,11 +5,9 @@ import {
     getGoogleLogin,
     getGoogleLoginFailure,
     getGoogleLoginSuccess,
-    isAuthenticated,
     unauthorisedInDemo,
 } from '../controllers/authentication';
 import { userModel } from '../models/models';
-import { SavedQueriesModel } from '../models/savedQueries';
 import { UserModel } from '../models/UserModel';
 import { healthService, userService } from '../services/services';
 import { sanitizeEmailParam, sanitizeStringParam } from '../utils';
@@ -18,6 +16,7 @@ import { inviteLinksRouter } from './inviteLinksRouter';
 import { organizationRouter } from './organizationRouter';
 import { passwordResetLinksRouter } from './passwordResetLinksRouter';
 import { projectRouter } from './projectRouter';
+import { savedChartRouter } from './savedChartRouter';
 import { userRouter } from './userRouter';
 
 export const apiV1Router = express.Router();
@@ -111,77 +110,7 @@ apiV1Router.get('/logout', (req, res, next) => {
     });
 });
 
-apiV1Router.get(
-    '/saved/:savedQueryUuid',
-    isAuthenticated,
-    async (req, res, next) => {
-        SavedQueriesModel.getById(req.params.savedQueryUuid)
-            .then((results) => {
-                res.json({
-                    status: 'ok',
-                    results,
-                });
-            })
-            .catch(next);
-    },
-);
-
-apiV1Router.delete(
-    '/saved/:savedQueryUuid',
-    isAuthenticated,
-    unauthorisedInDemo,
-    async (req, res, next) => {
-        SavedQueriesModel.delete(req.user!, req.params.savedQueryUuid)
-            .then(() => {
-                res.json({
-                    status: 'ok',
-                    results: undefined,
-                });
-            })
-            .catch(next);
-    },
-);
-
-apiV1Router.patch(
-    '/saved/:savedQueryUuid',
-    isAuthenticated,
-    unauthorisedInDemo,
-    async (req, res, next) => {
-        SavedQueriesModel.update(
-            req.user!,
-            req.params.savedQueryUuid,
-            req.body.savedQuery,
-        )
-            .then((results) => {
-                res.json({
-                    status: 'ok',
-                    results,
-                });
-            })
-            .catch(next);
-    },
-);
-
-apiV1Router.post(
-    '/saved/:savedQueryUuid/version',
-    isAuthenticated,
-    unauthorisedInDemo,
-    async (req, res, next) => {
-        SavedQueriesModel.addVersion(
-            req.user!,
-            req.params.savedQueryUuid,
-            req.body.savedQuery,
-        )
-            .then((results) => {
-                res.json({
-                    status: 'ok',
-                    results,
-                });
-            })
-            .catch(next);
-    },
-);
-
+apiV1Router.use('/saved', savedChartRouter);
 apiV1Router.use('/invite-links', inviteLinksRouter);
 apiV1Router.use('/org', organizationRouter);
 apiV1Router.use('/user', userRouter);
---
+++ packages/backend/src/routers/savedChartRouter.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import {
+    isAuthenticated,
+    unauthorisedInDemo,
+} from '../controllers/authentication';
+import { SavedQueriesModel } from '../models/savedQueries';
+
+export const savedChartRouter = express.Router();
+
+savedChartRouter.get(
+    '/:savedQueryUuid',
+    isAuthenticated,
+    async (req, res, next) => {
+        SavedQueriesModel.getById(req.params.savedQueryUuid)
+            .then((results) => {
+                res.json({
+                    status: 'ok',
+                    results,
+                });
+            })
+            .catch(next);
+    },
+);
+
+savedChartRouter.delete(
+    '/:savedQueryUuid',
+    isAuthenticated,
+    unauthorisedInDemo,
+    async (req, res, next) => {
+        SavedQueriesModel.delete(req.user!, req.params.savedQueryUuid)
+            .then(() => {
+                res.json({
+                    status: 'ok',
+                    results: undefined,
+                });
+            })
+            .catch(next);
+    },
+);
+
+savedChartRouter.patch(
+    '/:savedQueryUuid',
+    isAuthenticated,
+    unauthorisedInDemo,
+    async (req, res, next) => {
+        SavedQueriesModel.update(
+            req.user!,
+            req.params.savedQueryUuid,
+            req.body.savedQuery,
+        )
+            .then((results) => {
+                res.json({
+                    status: 'ok',
+                    results,
+                });
+            })
+            .catch(next);
+    },
+);
+
+savedChartRouter.post(
+    '/:savedQueryUuid/version',
+    isAuthenticated,
+    unauthorisedInDemo,
+    async (req, res, next) => {
+        SavedQueriesModel.addVersion(
+            req.user!,
+            req.params.savedQueryUuid,
+            req.body.savedQuery,
+        )
+            .then((results) => {
+                res.json({
+                    status: 'ok',
+                    results,
+                });
+            })
+            .catch(next);
+    },
+);
