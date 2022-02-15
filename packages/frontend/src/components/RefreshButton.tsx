c7eb3301a418cf52d9cc5a0a18c626f92b2fc610
oliverlaslett <olaslett@gmail.com>
2022-02-15 19:29:50 +0000

    feat: add cmd+enter hotkey to RefreshButton

--- packages/frontend/src/components/RefreshButton.tsx
+++ packages/frontend/src/components/RefreshButton.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useHotkeys } from '@blueprintjs/core';
+import React, { useMemo } from 'react';
 import useDefaultSortField from '../hooks/useDefaultSortField';
 import { useQueryResults } from '../hooks/useQueryResults';
 import { useServerStatus } from '../hooks/useServerStatus';
@@ -16,17 +17,34 @@ export const RefreshButton = () => {
     const { isFetching, remove } = useQueryResults();
     const { track } = useTracking();
     const defaultSort = useDefaultSortField();
+    const onClick = async () => {
+        remove();
+        syncState(sorts.length === 0 ? defaultSort : undefined);
+        track({
+            name: EventName.RUN_QUERY_BUTTON_CLICKED,
+        });
+    };
+    const hotkeys = useMemo(
+        () => [
+            {
+                combo: 'cmd+enter',
+                group: 'Explorer',
+                label: 'Run query',
+                allowInInput: true,
+                onKeyDown: onClick,
+                global: true,
+                preventDefault: true,
+                stopPropagation: true,
+            },
+        ],
+        [onClick],
+    );
+    useHotkeys(hotkeys);
     return (
         <BigButton
             intent="primary"
             style={{ width: 150, marginRight: '10px' }}
-            onClick={async () => {
-                remove();
-                syncState(sorts.length === 0 ? defaultSort : undefined);
-                track({
-                    name: EventName.RUN_QUERY_BUTTON_CLICKED,
-                });
-            }}
+            onClick={onClick}
             disabled={!isValidQuery}
             loading={isFetching || status.data === 'loading'}
         >
