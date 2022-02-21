a817e9385086c37a4fccf1cf8996868fd8d69812
Nathalia Buitrago Jurado <31137824+nathaliabj@users.noreply.github.com>
2022-02-21 04:07:35 -0500

    feat: add column displaying the row number [skip ci] (#1423)
    * feat: add column displaying the row number
    
    * fix: update row number with index instead of id

---
+++ packages/frontend/src/components/ResultsTable/ResultsTable.styles.tsx
@@ -0,0 +1,67 @@
+import { Colors } from '@blueprintjs/core';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100%;
+    padding: 0.714em;
+    min-height: 21.429em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const TableOuterContainer = styled.div`
+    display: block;
+    max-width: 100% !important;
+`;
+
+export const TableInnerContainer = styled.div`
+    display: flex;
+    max-width: 100% !important;
+    flex-direction: row;
+`;
+
+export const TableContainer = styled.div`
+    flex: 1;
+    overflow-x: auto;
+`;
+
+export const RowNumberColumn = styled.col`
+    background: ${Colors.WHITE};
+`;
+
+export const RowNumberHeader = styled.th`
+    font-weight: bold;
+    border-top: 0.071em solid rgb(16 22 26 / 15%);
+    border-left: 0.071em solid rgb(16 22 26 / 15%);
+`;
+
+export const RowNumber = styled.td`
+    background: ${Colors.WHITE};
+    border-left: 0.071em solid rgb(16 22 26 / 15%);
+`;
+
+export const TableActionContainer = styled.div`
+    display: flex;
+    background-color: hexToRGB(Colors.GRAY4, 0.2);
+    box-shadow: inset 0.071em 0 0 0 rgb(16 22 26 / 15%);
+`;
+
+export const TableFooter = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 0.714em;
+`;
+
+export const PaginationWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+export const PageCount = styled.span`
+    padding: 0 0.357em 0 0.357em;
+`;
