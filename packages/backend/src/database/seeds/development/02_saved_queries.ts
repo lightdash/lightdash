2ebe18e7720889ad1d59a6ec65256f5c3a5b3cd7
Nathalia Buitrago Jurado <31137824+nathaliabj@users.noreply.github.com>
2022-03-08 10:01:22 -0500

    chore: update demo project (#1517)
    * chore: update saved charts
    
    * chore: add markdown tiles
    
    * chore add mkdown tiles
    
    * chore: update tiles position and sizing, add filters
    
    * chore: blockquote added

--- packages/backend/src/database/seeds/development/02_saved_queries.ts
+++ packages/backend/src/database/seeds/development/02_saved_queries.ts
@@ -55,31 +55,37 @@ export async function seed(knex: Knex): Promise<void> {
     });
 
     await savedChartModel.create(SEED_PROJECT.project_uuid, {
-        name: "What's the average spend per customer?",
-        tableName: 'orders',
+        name: `What's our total revenue to date?`,
+        tableName: 'payments',
         metricQuery: {
-            dimensions: ['customers_customer_id'],
-            metrics: ['orders_avg_amount'],
+            dimensions: ['orders_status'],
+            metrics: ['payments_total_revenue'],
             filters: {},
             limit: 500,
-            sorts: [{ fieldId: 'orders_avg_amount', descending: true }],
-            tableCalculations: [],
+            sorts: [
+                {
+                    fieldId: 'payments_total_revenue',
+                    descending: true,
+                },
+            ],
+            tableCalculations: [
+                {
+                    name: 'total_revenue',
+                    displayName: 'total revenue',
+                    sql: 'SUM(${payments.total_revenue}) OVER(PARTITION BY NULL)',
+                },
+            ],
         },
         chartConfig: {
-            type: ChartType.CARTESIAN,
-            config: {
-                series: [
-                    {
-                        flipAxes: true,
-                        type: CartesianSeriesType.BAR,
-                        xField: 'customers_customer_id',
-                        yField: 'orders_avg_amount',
-                    },
-                ],
-            },
+            type: ChartType.BIG_NUMBER,
+            config: undefined,
         },
         tableConfig: {
-            columnOrder: ['customers_customer_id', 'orders_avg_amount'],
+            columnOrder: [
+                'orders_status',
+                'payments_total_revenue',
+                'total_revenue',
+            ],
         },
     });
 
@@ -127,6 +133,73 @@ export async function seed(knex: Knex): Promise<void> {
                 'orders_order_date',
                 'orders_unique_order_count',
                 'cumulative_order_count',
+            ],
+        },
+    });
+
+    await savedChartModel.create(SEED_PROJECT.project_uuid, {
+        name: "What's the average spend per customer?",
+        tableName: 'orders',
+        metricQuery: {
+            dimensions: ['customers_customer_id'],
+            metrics: ['orders_avg_amount'],
+            filters: {},
+            limit: 500,
+            sorts: [{ fieldId: 'orders_avg_amount', descending: true }],
+            tableCalculations: [],
+        },
+        chartConfig: {
+            type: ChartType.CARTESIAN,
+            config: {
+                series: [
+                    {
+                        flipAxes: true,
+                        type: CartesianSeriesType.BAR,
+                        xField: 'customers_customer_id',
+                        yField: 'orders_avg_amount',
+                    },
+                ],
+            },
+        },
+        tableConfig: {
+            columnOrder: ['customers_customer_id', 'orders_avg_amount'],
+        },
+    });
+
+    await savedChartModel.create(SEED_PROJECT.project_uuid, {
+        name: 'Which customers have not recently ordered an item?',
+        tableName: 'payments',
+        metricQuery: {
+            dimensions: [
+                'customers_customer_id',
+                'customers_days_since_last_order',
+                'customers_days_between_created_and_first_order',
+            ],
+            metrics: [
+                'payments_total_revenue',
+                'payments_unique_payment_count',
+            ],
+            filters: {},
+            limit: 500,
+            sorts: [
+                {
+                    fieldId: 'customers_days_since_last_order',
+                    descending: false,
+                },
+            ],
+            tableCalculations: [],
+        },
+        chartConfig: {
+            type: ChartType.TABLE,
+            config: undefined,
+        },
+        tableConfig: {
+            columnOrder: [
+                'customers_customer_id',
+                'customers_days_since_last_order',
+                'customers_days_between_created_and_first_order',
+                'payments_total_revenue',
+                'payments_unique_payment_count',
             ],
         },
     });
--- packages/backend/src/database/seeds/development/03_saved_dashboards.ts
+++ packages/backend/src/database/seeds/development/03_saved_dashboards.ts
@@ -3,6 +3,7 @@ import {
     DashboardLoomTile,
     DashboardMarkdownTile,
     DashboardTileTypes,
+    FilterOperator,
     SEED_PROJECT,
 } from 'common';
 import { Knex } from 'knex';
@@ -14,10 +15,16 @@ const markdownSample = `### Lightdash is an open source analytics for your dbt p
 
 We're kind of like Looker, but without the price tag. 
 
-At Lightdash, our mission is simple: we want to enable everybody in your company to answer their own questions using data.
+#### At Lightdash, our mission is simple: 
+
+> we want to enable everybody in your company to answer their own questions using data.
 
 Have any questions? Feel free to send us a message at support@lightdash.com. Or, just talk to the bot on this page - he may not have the answers to life's great questions, but he'll get you in touch with the right people.
 `;
+
+const markdownRevenue = `Charts related to our Jaffle Shop's revenue.`;
+
+const markdownOrders = `Details about our Jaffle orders and customer activity.`;
 
 export async function seed(knex: Knex): Promise<void> {
     // delete existing dashboards
@@ -57,23 +64,117 @@ export async function seed(knex: Knex): Promise<void> {
         },
     };
 
-    const chartTiles = queries.map<DashboardChartTile>(
-        ({ uuid: savedChartUuid }, i) => ({
-            uuid: uuidv4(),
-            x: i % 2 === 0 ? 0 : 6,
-            y: Math.floor(i / 2) * 3 + 3,
-            w: i === 0 || i % 3 === 0 ? 12 : 6,
-            h: 3,
-            type: DashboardTileTypes.SAVED_CHART,
-            properties: { savedChartUuid },
-        }),
-    );
+    const markdownRevenueTile: DashboardMarkdownTile = {
+        uuid: uuidv4(),
+        x: 0,
+        y: 3,
+        w: 12,
+        h: 1,
+        type: DashboardTileTypes.MARKDOWN,
+        properties: {
+            title: '💸 Revenue',
+            content: markdownRevenue,
+        },
+    };
+
+    const barChart: DashboardChartTile = {
+        uuid: uuidv4(),
+        x: 0,
+        y: 4,
+        w: 8,
+        h: 3,
+        type: DashboardTileTypes.SAVED_CHART,
+        properties: { savedChartUuid: queries[0].uuid },
+    };
+
+    const bigNumberTile: DashboardChartTile = {
+        uuid: uuidv4(),
+        x: 8,
+        y: 4,
+        w: 4,
+        h: 3,
+        type: DashboardTileTypes.SAVED_CHART,
+        properties: { savedChartUuid: queries[1].uuid },
+    };
+
+    const lineChartTile: DashboardChartTile = {
+        uuid: uuidv4(),
+        x: 0,
+        y: 8,
+        w: 6,
+        h: 3,
+        type: DashboardTileTypes.SAVED_CHART,
+        properties: { savedChartUuid: queries[2].uuid },
+    };
+
+    const barTile: DashboardChartTile = {
+        uuid: uuidv4(),
+        x: 6,
+        y: 8,
+        w: 6,
+        h: 3,
+        type: DashboardTileTypes.SAVED_CHART,
+        properties: { savedChartUuid: queries[3].uuid },
+    };
+
+    const tableTile: DashboardChartTile = {
+        uuid: uuidv4(),
+        x: 0,
+        y: 11,
+        w: 12,
+        h: 3,
+        type: DashboardTileTypes.SAVED_CHART,
+        properties: { savedChartUuid: queries[4].uuid },
+    };
+
+    const markdownOrdersTile: DashboardMarkdownTile = {
+        uuid: uuidv4(),
+        x: 0,
+        y: 7,
+        w: 12,
+        h: 1,
+        type: DashboardTileTypes.MARKDOWN,
+        properties: {
+            title: '📨 Orders',
+            content: markdownOrders,
+        },
+    };
 
     await dashboardModel.create(spaceUuid, {
         name: 'Jaffle dashboard',
-        tiles: [loomTile, markdownTile, ...chartTiles],
+        tiles: [
+            loomTile,
+            markdownTile,
+            markdownRevenueTile,
+            barChart,
+            bigNumberTile,
+            markdownOrdersTile,
+            lineChartTile,
+            barTile,
+            tableTile,
+        ],
         filters: {
-            dimensions: [],
+            dimensions: [
+                {
+                    id: 'e7df7c5a-1070-439a-8300-125fe5f9b1af',
+                    target: {
+                        fieldId: 'orders_is_completed',
+                        tableName: 'orders',
+                    },
+                    values: [true],
+                    operator: FilterOperator.EQUALS,
+                },
+                {
+                    id: '6d28a3a5-c01f-46d8-9f6b-74a9ab1efd99',
+                    target: {
+                        fieldId: 'orders_order_date_year',
+                        tableName: 'orders',
+                    },
+                    values: [5],
+                    operator: FilterOperator.IN_THE_PAST,
+                    settings: { completed: true, unitOfTime: 'years' },
+                },
+            ],
             metrics: [],
         },
     });
