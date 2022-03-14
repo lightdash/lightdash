a3fc425d294190e489a7d2385da9608d7ac12747
Jose Rego <jose@lightdash.com>
2022-03-14 11:37:02 +0000

    styles: amend table input styles

--- packages/frontend/src/components/ReactHookForm/MultiKeyValuePairsInput.styles.ts
+++ packages/frontend/src/components/ReactHookForm/MultiKeyValuePairsInput.styles.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 export const MultiKeyValuePairRow = styled.div`
     display: flex;
-    flex-direction: row;
     align-items: flex-start;
-    justify-content: stretch;
-    width: 100%;
     gap: 5px;
-    padding-right: 3px;
+
+    .bp3-form-group {
+        margin-bottom: 0;
+        flex: 1;
+    }
 `;
--- packages/frontend/src/components/ReactHookForm/MultiKeyValuePairsInput.tsx
+++ packages/frontend/src/components/ReactHookForm/MultiKeyValuePairsInput.tsx
@@ -1,21 +1,14 @@
 import { Button, FormGroup } from '@blueprintjs/core';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 import Input from './Input';
-import {
-    AddKeyValuePairRow,
-    MultiKeyValuePairRow,
-} from './MultiKeyValuePairsInput.styles';
+import { MultiKeyValuePairRow } from './MultiKeyValuePairsInput.styles';
 
 type Props = {
     name: string;
     label: string;
 };
 export const MultiKeyValuePairsInput = ({ name, label }: Props) => {
-    const {
-        control,
-        register,
-        formState: { errors },
-    } = useFormContext();
+    const { control } = useFormContext();
     const { fields, remove, append } = useFieldArray({ name, control });
     return (
         <FormGroup className="input-wrapper" label={label}>
