bb9ea3ca2192bbb71adb992473a9246d14a0e347
oliverlaslett <olaslett@gmail.com>
2022-03-22 20:55:54 +0000

    WIP: type changes

--- packages/common/src/types/savedCharts.ts
+++ packages/common/src/types/savedCharts.ts
@@ -23,6 +23,30 @@ export enum CartesianSeriesType {
 }
 
 export type Series = {
+    encode: {
+        x: string; // `explore.field.pivot.values`
+        y: string; // `explore.field.pivot.values`
+    };
+    type: CartesianSeriesType;
+    name?: string;
+    color?: string;
+    label?: {
+        show?: boolean;
+        position?: 'left' | 'top' | 'right' | 'bottom';
+    };
+};
+
+export type EChartsConfig = {
+    series: Series[];
+    xAxis?: Axis[];
+    yAxis?: Axis[];
+};
+
+type Axis = {
+    name?: string;
+};
+
+type PrevSeries = {
     xField: string;
     yField: string;
     type: CartesianSeriesType;
@@ -34,16 +58,43 @@ export type Series = {
         position?: 'left' | 'top' | 'right' | 'bottom';
     };
 };
-
-type Axis = {
-    name?: string;
-};
-
-export type CartesianChart = {
-    series: Series[];
+type Prev = {
+    series: PrevSeries[];
     xAxes?: Axis[];
     yAxes?: Axis[];
 };
+
+const configMap = (prev: Prev): CartesianChart => ({
+    layout: {
+        xField: prev.series[0].xField,
+        yField: prev.series.map((s) => s.yField),
+        flipAxes: prev.series[0].flipAxes,
+    },
+    eChartsConfig: {
+        series: prev.series.map((s) => ({
+            encode: {
+                x: s.xField,
+                y: s.yField,
+            },
+            type: s.type,
+            name: s.name,
+            color: s.color,
+            label: s.label,
+        })),
+        xAxis: prev.xAxes,
+        yAxis: prev.yAxes,
+    },
+});
+
+export type CartesianChart = {
+    layout: {
+        xField: string;
+        yField: string[];
+        flipAxes?: boolean | undefined;
+    };
+    eChartsConfig: EChartsConfig;
+};
+
 export type CartesianChartConfig = {
     type: ChartType.CARTESIAN;
     config: CartesianChart;
