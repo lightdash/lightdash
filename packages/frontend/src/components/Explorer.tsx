6740bee3cdd3dd6402ba7e56eb0d8026c6c9a746
Javier Rengel Jiménez <javier+github@rengel.me>
2022-03-24 11:01:19 +0000

    fix: dont collapse chart on save

--- packages/frontend/src/components/Explorer.tsx
+++ packages/frontend/src/components/Explorer.tsx
@@ -99,7 +99,7 @@ export const Explorer: FC<Props> = ({ savedQueryUuid }) => {
     const [resultsIsOpen, setResultsIsOpen] = useState<boolean>(true);
     const [sqlIsOpen, setSqlIsOpen] = useState<boolean>(false);
     const [vizIsOpen, setVizisOpen] = useState<boolean>(
-        !!savedQueryUuid && !location.state?.fromExplorer,
+        !!savedQueryUuid && location.state?.fromExplorer !== undefined,
     );
     const totalActiveFilters: number = countTotalFilterRules(filters);
     const [activeVizTab, setActiveVizTab] = useState<ChartType>(
@@ -193,6 +193,14 @@ export const Explorer: FC<Props> = ({ savedQueryUuid }) => {
             setActiveVizTab(data.chartConfig.type);
         }
     }, [data]);
+    console.log('PATATA savedQueryUuid', savedQueryUuid, !!savedQueryUuid);
+    console.log(
+        'PATATA location',
+        location.state?.fromExplorer,
+        !location.state?.fromExplorer,
+        location.state?.fromExplorer === undefined,
+    );
+    console.log('PATATA vizIsOpen', vizIsOpen);
 
     return (
         <>
