2fda8ebc759d3c4afa363e3b19095dafa6928935
Javier Rengel Jiménez <javier+github@rengel.me>
2022-03-30 13:30:37 +0000

    fix: replace _text with text array on migration

--- packages/backend/src/database/migrations/20220330140655_alter_organisation_add_chart_colours.ts
+++ packages/backend/src/database/migrations/20220330140655_alter_organisation_add_chart_colours.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(`ALTER TABLE organizations
-        ADD COLUMN chart_colors _text ;`);
+        ADD COLUMN chart_colors TEXT[] ;`);
 }
 
 export async function down(knex: Knex): Promise<void> {
