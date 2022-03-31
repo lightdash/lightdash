0b400719c4a20da5c1b7ff4b2caaafd3fead34ed
nathaliabj <nathalia.buitragoj@gmail.com>
2022-03-31 11:57:42 -0500

    fix: remove hook

--- packages/frontend/src/components/ChartConfigPanel/ChartConfigTabs.tsx
+++ packages/frontend/src/components/ChartConfigPanel/ChartConfigTabs.tsx
@@ -13,7 +13,6 @@ import {
     TableCalculation,
 } from 'common';
 import React, { FC, useCallback, useMemo, useState } from 'react';
-import { ActiveSeriesProvider } from '../../providers/ActiveSeries';
 import { useVisualizationContext } from '../LightdashVisualization/VisualizationProvider';
 import { InputWrapper, Wrapper } from './ChartConfigPanel.styles';
 import FieldLayoutOptions from './FieldLayoutOptions';
@@ -110,18 +109,14 @@ const ChartConfigTabs: FC = () => {
                     title="Series"
                     panel={
                         pivotDimension ? (
-                            <ActiveSeriesProvider>
-                                <GroupedSeriesConfiguration
-                                    items={items}
-                                    layout={dirtyLayout}
-                                    series={dirtyEchartsConfig?.series}
-                                    getSeriesColor={getSeriesColor}
-                                    updateSingleSeries={updateSingleSeries}
-                                    updateAllGroupedSeries={
-                                        updateAllGroupedSeries
-                                    }
-                                />
-                            </ActiveSeriesProvider>
+                            <GroupedSeriesConfiguration
+                                items={items}
+                                layout={dirtyLayout}
+                                series={dirtyEchartsConfig?.series}
+                                getSeriesColor={getSeriesColor}
+                                updateSingleSeries={updateSingleSeries}
+                                updateAllGroupedSeries={updateAllGroupedSeries}
+                            />
                         ) : (
                             <BasicSeriesConfiguration
                                 items={items}
