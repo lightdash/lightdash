cae4561972eae872f2cf3b4ec5d1a670f3d54388
Rephus <rephus@gmail.com>
2022-04-11 09:55:20 +0200

    feat/add-new-chart-to-dashboard

--- packages/frontend/src/components/SavedDashboards/AddTilesToDashboardModal.tsx
+++ packages/frontend/src/components/SavedDashboards/AddTilesToDashboardModal.tsx
@@ -1,118 +1,198 @@
 import {
+    Button,
+    Classes,
+    Dialog,
+    FormGroup,
+    HTMLSelect,
+    InputGroup,
+    Intent,
+} from '@blueprintjs/core';
+import {
     DashboardTileTypes,
     getDefaultChartTileSize,
     SavedChart,
 } from 'common';
-import React, { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { v4 as uuid4 } from 'uuid';
 import {
+    useCreateMutation,
     useDashboardQuery,
     useUpdateDashboard,
 } from '../../hooks/dashboard/useDashboard';
 import { useDashboards } from '../../hooks/dashboard/useDashboards';
-import ActionModal, {
-    ActionModalProps,
-    ActionTypeModal,
-} from '../common/modal/ActionModal';
-import SelectField from '../ReactHookForm/Select';
 
-const Form = ({
-    isDisabled,
-}: Pick<
-    ActionModalProps<{ dashboardUuid: string }>,
-    'useActionModalState' | 'isDisabled'
->) => {
-    const { projectUuid } = useParams<{ projectUuid: string }>();
-    const { data, isLoading } = useDashboards(projectUuid);
-    return (
-        <>
-            <SelectField
-                key={data?.[0]?.uuid} // Note: force re-render when data load
-                name="dashboardUuid"
-                label="Select a dashboard"
-                options={(data || []).map(({ uuid, name }) => ({
-                    value: uuid,
-                    label: name,
-                }))}
-                rules={{
-                    required: 'Required field',
-                }}
-                defaultValue={data?.[0]?.uuid}
-                disabled={isDisabled || isLoading}
-            />
-        </>
-    );
-};
+interface AddTilesToDashboardModalProps {
+    isOpen: boolean;
+
+    savedChart: SavedChart;
+    onClose?: () => void;
+}
 
 const useUpdateMutation = (id?: string) => {
     const hook = useUpdateDashboard(id || '', true);
     if (id) {
         return hook;
     }
-    return { mutate: undefined, isIdle: undefined, isSuccess: undefined };
+    return { mutate: undefined };
 };
 
-interface Props {
-    savedChart: SavedChart;
-    onClose?: () => void;
-}
-
-const AddTilesToDashboardModal: FC<Props> = ({ savedChart, onClose }) => {
-    const [selectedDashboardUuid, setSelectedDashboardUuid] =
-        useState<string>();
-    const { data } = useDashboardQuery(selectedDashboardUuid);
-    const { mutate, isSuccess, isIdle } = useUpdateMutation(
-        selectedDashboardUuid,
-    );
-    const [actionState, setActionState] = useState<{
-        actionType: number;
-    }>({
-        actionType: ActionTypeModal.UPDATE,
-    });
-    const [completedMutation, setCompletedMutation] = useState(false);
-
-    const onSubmitForm = (properties: { dashboardUuid: string }) => {
-        setCompletedMutation(false);
-        setSelectedDashboardUuid(properties.dashboardUuid);
-    };
+const AddTilesToDashboardModal: FC<AddTilesToDashboardModalProps> = ({
+    isOpen,
+    savedChart,
+    onClose,
+}) => {
+    const { projectUuid } = useParams<{ projectUuid: string }>();
+    const useCreate = useCreateMutation(projectUuid, true);
+    const { mutate: mutateCreate } = useCreate;
+    const { data: dashboards, isLoading } = useDashboards(projectUuid);
+    const [selectedDashboard, setSelectedDashboard] = useState<string>();
+    const { data: dashboardData } = useDashboardQuery(selectedDashboard);
+    const { mutate: updateMutation } = useUpdateMutation(selectedDashboard);
+    const [name, setName] = useState<string>('');
+    const [showNewDashboardInput, setShowNewDashboardInput] =
+        useState<boolean>(false);
 
     useEffect(() => {
-        if (data && mutate && isIdle) {
-            mutate({
-                tiles: [
-                    ...data.tiles,
-                    {
-                        uuid: uuid4(),
-                        type: DashboardTileTypes.SAVED_CHART,
-                        properties: {
-                            savedChartUuid: savedChart.uuid,
-                        },
-                        ...getDefaultChartTileSize(savedChart.chartConfig.type),
-                    },
-                ],
-                filters: data.filters,
-            });
+        if (dashboards && !dashboardData && !isLoading) {
+            if (dashboards.length > 0) setSelectedDashboard(dashboards[0].uuid);
         }
-    }, [data, mutate, isIdle, savedChart]);
-
-    useEffect(() => {
-        if (isSuccess) {
-            setCompletedMutation(true);
+        // If no dashboards, we always show the ""
+        if (
+            dashboards &&
+            dashboards.length == 0 &&
+            !isLoading &&
+            !showNewDashboardInput
+        ) {
+            setShowNewDashboardInput(true);
         }
-    }, [isSuccess]);
+    }, [dashboards, isLoading]);
 
     return (
-        <ActionModal
-            title="Add chart to dashboard"
-            confirmButtonLabel="Add"
-            useActionModalState={[actionState, setActionState]}
-            isDisabled={false}
-            onSubmitForm={onSubmitForm}
-            completedMutation={completedMutation}
-            ModalContent={Form}
+        <Dialog
+            isOpen={isOpen}
             onClose={onClose}
-        />
+            lazy
+            title="Add chart to dashboard"
+        >
+            <form>
+                <div className={Classes.DIALOG_BODY}>
+                    {!showNewDashboardInput && (
+                        <>
+                            <p>
+                                <b>Select a dashboard</b>
+                            </p>
+                            <HTMLSelect
+                                id="select-dashboard"
+                                fill={true}
+                                value={selectedDashboard}
+                                style={{ marginBottom: 15 }}
+                                onChange={(e) =>
+                                    setSelectedDashboard(e.currentTarget.value)
+                                }
+                                options={
+                                    dashboards
+                                        ? dashboards.map((dashboard) => ({
+                                              value: dashboard.uuid,
+                                              label: dashboard.name,
+                                          }))
+                                        : []
+                                }
+                            />
+                            <div
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => setShowNewDashboardInput(true)}
+                            >
+                                <b style={{ color: '#267dbf' }}>
+                                    {' '}
+                                    + Create new
+                                </b>
+                            </div>
+                        </>
+                    )}
+
+                    {showNewDashboardInput && (
+                        <FormGroup
+                            label="Name"
+                            labelFor="chart-name"
+                            style={{ fontWeight: 'bold' }}
+                        >
+                            <InputGroup
+                                id="chart-name"
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                placeholder="eg. KPI dashboard"
+                            />
+                        </FormGroup>
+                    )}
+                </div>
+                <div className={Classes.DIALOG_FOOTER}>
+                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                        <Button
+                            onClick={() => {
+                                if (onClose) onClose();
+                                setShowNewDashboardInput(false);
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            data-cy="submit-base-modal"
+                            intent={Intent.SUCCESS}
+                            text={'Add'}
+                            type="submit"
+                            onClick={(e) => {
+                                if (showNewDashboardInput) {
+                                    mutateCreate({
+                                        name,
+                                        tiles: [
+                                            {
+                                                uuid: uuid4(),
+                                                type: DashboardTileTypes.SAVED_CHART,
+                                                properties: {
+                                                    savedChartUuid:
+                                                        savedChart.uuid,
+                                                },
+                                                ...getDefaultChartTileSize(
+                                                    savedChart.chartConfig.type,
+                                                ),
+                                            },
+                                        ],
+                                    });
+                                } else if (dashboardData && updateMutation) {
+                                    updateMutation({
+                                        name: dashboardData.name,
+                                        filters: dashboardData.filters,
+                                        tiles: [
+                                            ...dashboardData.tiles,
+                                            {
+                                                uuid: uuid4(),
+                                                type: DashboardTileTypes.SAVED_CHART,
+                                                properties: {
+                                                    savedChartUuid:
+                                                        savedChart.uuid,
+                                                },
+                                                ...getDefaultChartTileSize(
+                                                    savedChart.chartConfig.type,
+                                                ),
+                                            },
+                                        ],
+                                    });
+                                }
+
+                                if (onClose) onClose();
+                                setShowNewDashboardInput(false);
+                                setName('');
+
+                                e.preventDefault();
+                            }}
+                            disabled={showNewDashboardInput && name === ''}
+                        />
+                    </div>
+                </div>
+            </form>
+        </Dialog>
     );
 };
 
