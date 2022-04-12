c9ceb32b24c8aaca959376e1cf15ffc4b3c0698b
nathaliabj <nathalia.buitragoj@gmail.com>
2022-04-12 17:28:16 +0100

    feat: duplicate dashboard and clean up

--- packages/frontend/src/hooks/useDuplicate/index.tsx
+++ packages/frontend/src/hooks/useDuplicate/index.tsx
@@ -1,41 +1,37 @@
 import { useCallback } from 'react';
+import {
+    useDashboardQuery,
+    useDuplicateDashboardMutation,
+} from '../dashboard/useDashboard';
 import { useDuplicateMutation, useSavedQuery } from '../useSavedQuery';
 
 const useDuplicate = (itemId: string) => {
-    const isChart = true;
-
+    // Chart data
     const { data: chartToDuplicate } = useSavedQuery({ id: itemId });
-    // const { data: dashboardToDuplicate } = useDashboardQuery(itemId);
-
     const { mutate: duplicateChart, data: duplicatedChart } =
         useDuplicateMutation(itemId);
-    // const { mutate: duplicateDashboard } =
-    //     useDuplicateDashboardMutation(itemId);
 
-    const onDuplicateChart = useCallback(() => {
+    // Dashboard data
+    const { data: dashboardToDuplicate } = useDashboardQuery(itemId);
+    const { mutate: duplicateDashboard } =
+        useDuplicateDashboardMutation(itemId);
+
+    const onDuplicate = useCallback(() => {
+        // duplicate chart
         if (chartToDuplicate) {
             const { projectUuid, uuid, updatedAt, ...chartDuplicate } =
                 chartToDuplicate;
             duplicateChart(chartDuplicate);
         }
-    }, [chartToDuplicate]);
+        // duplicate dashboard
+        if (dashboardToDuplicate) {
+            const { projectUuid, uuid, updatedAt, ...DashboardDuplicate } =
+                dashboardToDuplicate;
+            duplicateDashboard(DashboardDuplicate);
+        }
+    }, [chartToDuplicate, dashboardToDuplicate]);
 
-    // const onDuplicateDashboard = useCallback(() => {
-    //     if (dashboardToDuplicate) {
-    //         const {
-    //             projectUuid: idToForget,
-    //             uuid,
-    //             updatedAt,
-    //             ...chartDuplicate
-    //         } = duplicateDashboard;
-    //         duplicateChart({
-    //             ...duplicateDashboard,
-    //             name: `${duplicateDashboard.name} (copy)`,
-    //         });
-    //     }
-    // }, [dashboardToDuplicate]);
-
-    return { onDuplicateChart, duplicatedChart };
+    return { onDuplicate, duplicatedChart };
 };
 
 export default useDuplicate;
