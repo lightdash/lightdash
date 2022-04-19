78f45fe560cd813b71263ee445255d48d76dcc11
Nathalia Buitrago Jurado <31137824+nathaliabj@users.noreply.github.com>
2022-04-19 15:44:25 +0100

    feat: add tracking to notifications button (#1808)

--- packages/frontend/src/components/NavBar/HelpMenu/index.tsx
+++ packages/frontend/src/components/NavBar/HelpMenu/index.tsx
@@ -6,6 +6,10 @@ import {
 } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 import React, { FC, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useApp } from '../../../providers/AppProvider';
+import { useTracking } from '../../../providers/TrackingProvider';
+import { EventName } from '../../../types/Events';
 import {
     ButtonWrapper,
     HelpItem,
@@ -19,10 +23,43 @@ import {
 } from './HelpMenu.styles';
 
 const HelpMenu: FC = () => {
+    const { track } = useTracking();
+    const { user } = useApp();
+    const { projectUuid } = useParams<{ projectUuid: string }>();
+
+    const trackNotifications = {
+        user_id: user.data?.userUuid,
+        project_id: projectUuid,
+        organization_id: user.data?.organizationUuid,
+    };
+
     useEffect(() => {
         (window as any).Headway.init({
             selector: '#headway-badge',
             account: '7L3Bzx',
+            callbacks: {
+                onShowWidget: () => {
+                    track({
+                        name: EventName.NOTIFICATIONS_CLICKED,
+                        properties: { ...trackNotifications },
+                    });
+                },
+                onShowDetails: (changelog: any) => {
+                    track({
+                        name: EventName.NOTIFICATIONS_ITEM_CLICKED,
+                        properties: {
+                            ...trackNotifications,
+                            item: changelog.title,
+                        },
+                    });
+                },
+                onReadMore: () => {
+                    track({
+                        name: EventName.NOTIFICATIONS_CLICKED,
+                        properties: { ...trackNotifications },
+                    });
+                },
+            },
         });
     }, []);
 
