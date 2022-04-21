a81f4cec6295f336b9bdcd1016c610156441f86f
Rephus <rephus@gmail.com>
2022-04-21 11:14:39 +0200

    test: add test for chart config

--- packages/e2e/cypress/integration/explore.spec.ts
+++ packages/e2e/cypress/integration/explore.spec.ts
@@ -50,6 +50,66 @@ describe('Explore', () => {
         cy.findByText('Success! Chart was saved.');
     });
 
+    it('Should change chart config type', () => {
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables`);
+
+        cy.findByText('Orders').click();
+        cy.findByText('First name').click();
+        cy.findByText('Unique order count').click();
+        cy.get('th b').first().should('have.text', 'First name').click();
+        cy.get('td', { timeout: 10000 }).eq(1).should('have.text', 'Aaron');
+
+        cy.findByText('Charts').prev().click(); // open chart
+
+        cy.findByText('Customers First name');
+
+        cy.findByText('Column chart').click(); // Change chart type
+        cy.findByText('Bar chart').click();
+
+        cy.findByText('Customers First name');
+
+        cy.findByText('Bar chart').click();
+        cy.findByText('Line chart').click();
+
+        cy.findByText('Customers First name');
+
+        cy.findByText('Line chart').click();
+        cy.findByText('Scatter chart').click();
+
+        cy.findByText('Customers First name');
+
+        cy.findByText('Scatter chart').click();
+        cy.get('[name="Table"]').click();
+
+        cy.findByText('Customers first name'); // Different label ¿?
+
+        cy.get('.bp3-icon-panel-table').first().parent().click();
+        cy.findByText('Big number').click();
+
+        cy.findByText('Orders Unique order count'); // Different label ¿?
+    });
+
+    it('Should change chart config layout', () => {
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables`);
+
+        cy.findByText('Orders').click();
+        cy.findByText('First name').click();
+        cy.findByText('Unique order count').click();
+        cy.get('th b').first().should('have.text', 'First name').click();
+        cy.get('td', { timeout: 10000 }).eq(1).should('have.text', 'Aaron');
+
+        cy.findByText('Charts').prev().click(); // open chart
+
+        cy.findByText('Customers First name');
+        cy.get('g').children('text').should('have.length.lessThan', 30); // without labels
+
+        cy.findByText('Configure').click();
+        cy.findByText('Series').click();
+        cy.findByText('Value labels');
+        cy.get('option[value="top"]').parent().select('top');
+
+        cy.get('g').children('text').should('have.length.greaterThan', 30); // with labels
+    });
     // TODO test `save chart` from chart list at home
     // TODO test `save chart` from dashboard on `edit chart`
     // TODO test `save chart` from dashboard on `explore here`
