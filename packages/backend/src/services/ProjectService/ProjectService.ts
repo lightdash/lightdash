ef787f0be646b24bab3085b27bed06d67b823395
Rephus <rephus@gmail.com>
2022-04-25 14:18:55 +0200

    feat: add job status

--- packages/backend/src/services/ProjectService/ProjectService.ts
+++ packages/backend/src/services/ProjectService/ProjectService.ts
@@ -23,6 +23,7 @@ import {
     TableSelectionType,
     UpdateProject,
 } from 'common';
+import { v4 as uuidv4 } from 'uuid';
 import { analytics } from '../../analytics/client';
 import {
     AuthorizationError,
@@ -426,20 +427,27 @@ export class ProjectService {
     async getAllExplores(
         user: SessionUser,
         projectUuid: string,
-        jobId: string,
+        jobUuid?: string,
         forceRefresh: boolean = false,
     ): Promise<(Explore | ExploreError)[]> {
         const cachedExplores = await this.projectModel.getExploresFromCache(
             projectUuid,
         );
         if (cachedExplores.length === 0 || forceRefresh) {
-            this.projectModel.tryWithProjectLock(projectUuid, () => {
-                const explores = this.refreshAllTables(user, projectUuid);
-                explores.then((ex) => {
-                    this.projectModel.saveExploresToCache(projectUuid, ex);
-                });
-                return explores;
-            });
+            this.projectModel.tryWithProjectLock(
+                projectUuid,
+                jobUuid || uuidv4(),
+                async () => {
+                    const explores = await this.refreshAllTables(
+                        user,
+                        projectUuid,
+                    );
+                    await this.projectModel.saveExploresToCache(
+                        projectUuid,
+                        explores,
+                    );
+                },
+            );
         }
         return cachedExplores;
     }
