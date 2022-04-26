f312263340b3177396ee5dc517a2e585c61efea6
Rephus <rephus@gmail.com>
2022-04-26 16:29:04 +0200

    use enums

--- packages/common/src/types/job.ts
+++ packages/common/src/types/job.ts
@@ -1,9 +1,16 @@
+export const enum JobStatusType {
+    STARTED = 'STARTED',
+    DONE = 'DONE',
+    RUNNING = 'RUNNING',
+    ERROR = 'ERROR',
+}
+
 export type Job = {
     jobUuid: string;
     projectUuid: string;
     createdAt: Date;
     updatedAt: Date;
-    jobStatus: string;
+    jobStatus: JobStatusType;
     jobResults?: string;
     steps: [];
 };
