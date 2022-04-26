b21d928a8b57e5397999f06c21c868c1bcae4b37
Rephus <rephus@gmail.com>
2022-04-26 10:53:58 +0200

    chore: new delete dashboard

--- packages/e2e/cypress/integration/savedDashboards.spec.ts
+++ packages/e2e/cypress/integration/savedDashboards.spec.ts
@@ -17,18 +17,6 @@ describe('Dashboard List', () => {
         cy.findByText('Jaffle dashboard').should('exist');
     });
 
-    it('Should delete dashboards', () => {
-        cy.visit('/');
-        cy.findByRole('button', { name: 'Browse' }).click();
-        cy.findByRole('button', { name: 'Dashboards' }).click();
-        // click on delete
-        cy.get('[data-icon=more]').click();
-        cy.findByRole('button', { name: 'Delete' }).click();
-        // click on delete in the popup
-        cy.findByText('Delete').click();
-        cy.findByText('No results available');
-    });
-
     it('Should create a new dashboard', () => {
         cy.visit('/');
         cy.findByRole('button', { name: 'Browse' }).click();
@@ -47,13 +35,25 @@ describe('Dashboard List', () => {
         cy.findByRole('button', { name: 'Browse' }).click();
         cy.findByRole('button', { name: 'Dashboards' }).click();
         // click on rename
-        cy.get('[data-icon=more]').click();
+        cy.contains('Untitled dashboard').children().eq(1).click();
         cy.findByRole('button', { name: 'Rename' }).click();
-        cy.findByLabelText('Name *').clear().type('Jaffle dashboard');
+        cy.findByLabelText('Name *').clear().type('e2e dashboard');
         // click on save
         cy.findByRole('button', { name: 'Save' }).click();
 
         // verify dashboard name has been updated in the list
-        cy.findByText('Jaffle dashboard').should('exist');
+        cy.findByText('e2e dashboard').should('exist');
+    });
+
+    it('Should delete dashboards', () => {
+        cy.visit('/');
+        cy.findByRole('button', { name: 'Browse' }).click();
+        cy.findByRole('button', { name: 'Dashboards' }).click();
+        // click on delete
+        cy.contains('e2e dashboard').children().eq(1).click();
+        cy.findByRole('button', { name: 'Delete' }).click();
+        // click on delete in the popup
+        cy.findByText('Delete').click();
+        cy.findByText('Jaffle dashboard'); // still exists
     });
 });
