96f481bb8b8ff74383b2fd6e2c62005327969775
Jose Rego <jose@lightdash.com>
2022-04-29 16:00:17 +0100

    chore: dont identify demo app user in intercom

--- packages/frontend/src/components/Intercom.tsx
+++ packages/frontend/src/components/Intercom.tsx
@@ -1,30 +1,33 @@
+import { LightdashMode } from 'common';
 import React, { useEffect } from 'react';
 import { useIntercom } from 'react-use-intercom';
 import { useApp } from '../providers/AppProvider';
 
 export const Intercom: React.FC = () => {
-    const { user } = useApp();
+    const { user, health } = useApp();
     const { update } = useIntercom();
 
     useEffect(() => {
-        update(
-            user.data
-                ? {
-                      userId: user.data.userUuid,
-                      name: `${user.data.firstName} ${user.data.lastName}`,
-                      email: user.data.email,
-                      company: {
-                          companyId: user.data.organizationUuid,
-                          name: user.data.organizationName,
-                      },
-                      customAttributes: {
-                          role: user.data.role,
-                          is_setup_complete: user.data.isSetupComplete,
-                      },
-                  }
-                : undefined,
-        );
-    }, [update, user]);
+        if (health.data?.mode && health.data.mode !== LightdashMode.DEMO) {
+            update(
+                user.data
+                    ? {
+                          userId: user.data.userUuid,
+                          name: `${user.data.firstName} ${user.data.lastName}`,
+                          email: user.data.email,
+                          company: {
+                              companyId: user.data.organizationUuid,
+                              name: user.data.organizationName,
+                          },
+                          customAttributes: {
+                              role: user.data.role,
+                              is_setup_complete: user.data.isSetupComplete,
+                          },
+                      }
+                    : undefined,
+            );
+        }
+    }, [update, health, user]);
 
     return null;
 };
