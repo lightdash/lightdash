f47471fdc38b4fbf444d4470f823f11d35f51846
Rephus <rephus@gmail.com>
2022-05-04 10:41:19 +0200

    update lint

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
 
-const { program } = require('commander')
+const { program } = require('commander');
 const { exec } = require('child_process');
 const { version: VERSION } = require('../package.json');
 
-
-program
-    .command('version')
-    .description('output the version number')
-    .action(version)
-program
-    .command('[dbt_command]', { isDefault: true })
-    .description('runs dbt')
-    .action(dbt)
-
-
-
-function dbt () {
-
-    const args = process.argv.slice(2).join(' ')
+function dbt() {
+    const args = process.argv.slice(2).join(' ');
 
     exec(`dbt ${args}`, (err: string, stdout: string, stderr: string) => {
         console.log(`${stdout}`);
         console.error(`${stderr}`);
-      });
+    });
 }
 
-function version () {
-     console.log(`lightdash-cli version: ${VERSION}`)
+function version() {
+    console.log(`lightdash version: ${VERSION}`);
 }
-program.parse()
+
+program
+    .command('version')
+    .description('output the version number')
+    .action(version);
+program
+    .command('[dbt_command]', { isDefault: true })
+    .description('runs dbt')
+    .action(dbt);
+
+program.parse();
