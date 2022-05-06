446da4e388d46cddfd95f26e7852642fdef4fbec
Jose Rego <ZeRego@users.noreply.github.com>
2022-05-06 13:10:15 +0100

    fix: share explore state (#2007)

--- packages/frontend/src/hooks/useExplorerRoute.ts
+++ packages/frontend/src/hooks/useExplorerRoute.ts
@@ -1,4 +1,4 @@
-import { ChartType, countTotalFilterRules } from 'common';
+import { ChartType, CreateSavedChartVersion } from 'common';
 import { useEffect, useMemo } from 'react';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
 import { useApp } from '../providers/AppProvider';
@@ -7,98 +7,80 @@ import {
     useExplorer,
 } from '../providers/ExplorerProvider';
 
+const getExplorerUrlFromCreateSavedChartVersion = (
+    projectUuid: string,
+    createSavedChart: CreateSavedChartVersion,
+): { pathname: string; search: string } => {
+    const newParams = new URLSearchParams();
+    newParams.set(
+        'create_saved_chart_version',
+        JSON.stringify(createSavedChart),
+    );
+    return {
+        pathname: `/projects/${projectUuid}/tables/${createSavedChart.tableName}`,
+        search: newParams.toString(),
+    };
+};
+
+const parseExplorerSearchParams = (search: string): CreateSavedChartVersion => {
+    const searchParams = new URLSearchParams(search);
+    const chartConfigSearchParam = searchParams.get(
+        'create_saved_chart_version',
+    );
+    return chartConfigSearchParam
+        ? JSON.parse(chartConfigSearchParam)
+        : {
+              tableName: '',
+              metricQuery: {
+                  dimensions: [],
+                  metrics: [],
+                  filters: {},
+                  sorts: [],
+                  limit: 500,
+                  tableCalculations: [],
+                  additionalMetrics: [],
+              },
+              pivotConfig: undefined,
+              tableConfig: {
+                  columnOrder: [],
+              },
+              chartConfig: {
+                  type: ChartType.CARTESIAN,
+                  config: { layout: {}, eChartsConfig: {} },
+              },
+          };
+};
+
 export const useExplorerRoute = () => {
     const history = useHistory();
-    const pathParams =
-        useParams<{ projectUuid: string; tableId: string | undefined }>();
+    const pathParams = useParams<{
+        projectUuid: string;
+        tableId: string | undefined;
+    }>();
     const {
-        state: {
-            unsavedChartVersion: {
-                tableName,
-                tableConfig: { columnOrder: stateColumnOrder },
-            },
-        },
+        state: { unsavedChartVersion },
         queryResults: { data: queryResultsData },
         actions: { reset, setTableName },
     } = useExplorer();
 
     // Update url params based on pristine state
     useEffect(() => {
-        if (queryResultsData?.metricQuery && tableName) {
-            const newParams = new URLSearchParams();
-            if (queryResultsData.metricQuery.dimensions.length === 0) {
-                newParams.delete('dimensions');
-            } else {
-                newParams.set(
-                    'dimensions',
-                    queryResultsData.metricQuery.dimensions.join(','),
-                );
-            }
-            if (queryResultsData.metricQuery.metrics.length === 0) {
-                newParams.delete('metrics');
-            } else {
-                newParams.set(
-                    'metrics',
-                    queryResultsData.metricQuery.metrics.join(','),
-                );
-            }
-            if (queryResultsData.metricQuery.sorts.length === 0) {
-                newParams.delete('sort');
-            } else {
-                newParams.set(
-                    'sort',
-                    JSON.stringify(queryResultsData.metricQuery.sorts),
-                );
-            }
-            if (
-                countTotalFilterRules(queryResultsData.metricQuery.filters) ===
-                0
-            ) {
-                newParams.delete('filters');
-            } else {
-                newParams.set(
-                    'filters',
-                    JSON.stringify(queryResultsData.metricQuery.filters),
-                );
-            }
-            newParams.set('limit', `${queryResultsData.metricQuery.limit}`);
-            if (stateColumnOrder.length === 0) {
-                newParams.delete('column_order');
-            } else {
-                newParams.set('column_order', stateColumnOrder.join(','));
-            }
-            if (queryResultsData.metricQuery.tableCalculations.length === 0) {
-                newParams.delete('table_calculations');
-            } else {
-                newParams.set(
-                    'table_calculations',
-                    JSON.stringify(
-                        queryResultsData.metricQuery.tableCalculations,
-                    ),
-                );
-            }
-
-            if (queryResultsData.metricQuery.additionalMetrics?.length === 0) {
-                newParams.delete('additional_metrics');
-            } else {
-                newParams.set(
-                    'additional_metrics',
-                    JSON.stringify(
-                        queryResultsData.metricQuery.additionalMetrics,
-                    ),
-                );
-            }
-            history.replace({
-                pathname: `/projects/${pathParams.projectUuid}/tables/${tableName}`,
-                search: newParams.toString(),
-            });
+        if (queryResultsData?.metricQuery) {
+            history.replace(
+                getExplorerUrlFromCreateSavedChartVersion(
+                    pathParams.projectUuid,
+                    {
+                        ...unsavedChartVersion,
+                        metricQuery: queryResultsData.metricQuery,
+                    },
+                ),
+            );
         }
     }, [
         queryResultsData,
-        tableName,
-        stateColumnOrder,
         history,
         pathParams.projectUuid,
+        unsavedChartVersion,
     ]);
 
     useEffect(() => {
@@ -113,64 +95,17 @@ export const useExplorerRoute = () => {
 export const useExplorerUrlState = (): ExplorerReduceState | undefined => {
     const { showToastError } = useApp();
     const { search } = useLocation();
-    const pathParams =
-        useParams<{ projectUuid: string; tableId: string | undefined }>();
+    const pathParams = useParams<{
+        projectUuid: string;
+        tableId: string | undefined;
+    }>();
 
     return useMemo(() => {
         if (pathParams.tableId) {
             try {
-                const searchParams = new URLSearchParams(search);
-                const dimensions =
-                    searchParams.get('dimensions')?.split(',') || [];
-                const metrics = searchParams.get('metrics')?.split(',') || [];
-                const sortSearchParam = searchParams.get('sort');
-                const sorts = !sortSearchParam
-                    ? []
-                    : JSON.parse(sortSearchParam);
-                const filterSearchParam = searchParams.get('filters');
-                const filters = !filterSearchParam
-                    ? []
-                    : JSON.parse(filterSearchParam);
-                const limitSearchParam = searchParams.get('limit');
-                const limit =
-                    limitSearchParam &&
-                    !Number.isNaN(parseInt(limitSearchParam, 10))
-                        ? parseInt(limitSearchParam, 10)
-                        : 500;
-                const columnOrder =
-                    searchParams.get('column_order')?.split(',') || [];
-                const tableCalculationsSearchParam =
-                    searchParams.get('table_calculations');
-                const tableCalculations = !tableCalculationsSearchParam
-                    ? []
-                    : JSON.parse(tableCalculationsSearchParam);
-                const additionalMetricsParam =
-                    searchParams.get('additional_metrics');
-                const additionalMetrics = !additionalMetricsParam
-                    ? []
-                    : JSON.parse(additionalMetricsParam);
                 return {
                     shouldFetchResults: true,
-                    unsavedChartVersion: {
-                        tableName: pathParams.tableId,
-                        metricQuery: {
-                            dimensions,
-                            metrics,
-                            filters,
-                            sorts,
-                            limit,
-                            tableCalculations,
-                            additionalMetrics,
-                        },
-                        pivotConfig: undefined,
-                        tableConfig: {
-                            columnOrder,
-                        },
-                        chartConfig: {
-                            type: ChartType.CARTESIAN,
-                            config: { layout: {}, eChartsConfig: {} },
-                        },
-                    },
+                    unsavedChartVersion: parseExplorerSearchParams(search),
                 };
             } catch (e: any) {
                 showToastError({ title: 'Error parsing url', subtitle: e });
