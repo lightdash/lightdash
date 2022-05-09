78258f4ebd8baa7ccd4c0aaa8edc37f9590c5f3f
Jose Rego <jose@lightdash.com>
2022-05-09 15:25:15 +0100

    fix: selete y axis with same name

--- packages/frontend/src/components/ChartConfigPanel/FieldLayoutOptions.tsx
+++ packages/frontend/src/components/ChartConfigPanel/FieldLayoutOptions.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@blueprintjs/core';
 import { Field, getItemId, TableCalculation } from 'common';
-import React, { FC, useMemo } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import FieldAutoComplete from '../common/Filters/FieldAutoComplete';
 import SimpleButton from '../common/SimpleButton';
 import { useVisualizationContext } from '../LightdashVisualization/VisualizationProvider';
@@ -45,9 +45,12 @@ const FieldLayoutOptions: FC<Props> = ({ items }) => {
     // Y axis logic
     const yFields = dirtyLayout?.yField || [];
 
-    const yActiveField = (field: string) => {
-        return items.find((item) => field.includes(item.name));
-    };
+    const yActiveField = useCallback(
+        (field: string) => {
+            return items.find((item) => getItemId(item) === field);
+        },
+        [items],
+    );
 
     const availableYFields = useMemo(() => {
         return items.filter(
