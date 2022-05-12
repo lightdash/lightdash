4b842cab6c083df4817d255902e717eb23f1add6
nathaliabj <nathalia.buitragoj@gmail.com>
2022-05-12 20:01:33 +0100

    fix: view mode button alignment

--- packages/frontend/src/components/ExploreFromHereButton/ExploreFromHere.styles.tsx
+++ packages/frontend/src/components/ExploreFromHereButton/ExploreFromHere.styles.tsx
@@ -4,4 +4,5 @@ import { BigButton } from '../common/BigButton';
 export const ExploreFromHerePrimary = styled(BigButton)`
     width: 175px;
     height: 40px;
+    margin-left: auto !important;
 `;
