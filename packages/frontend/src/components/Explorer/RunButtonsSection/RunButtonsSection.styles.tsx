02842131d76a5cb5e4b937e91d1def5f98d2d5c4
nathaliabj <nathalia.buitragoj@gmail.com>
2022-05-12 19:02:10 +0100

    fix: content alignment

--- packages/frontend/src/components/Explorer/RunButtonsSection/RunButtonsSection.styles.tsx
+++ packages/frontend/src/components/Explorer/RunButtonsSection/RunButtonsSection.styles.tsx
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 export const Wrapper = styled.div<{ $isViewMode: boolean }>`
     height: 60px;
     display: flex;
-    justify-content: ${($isViewMode) =>
-        !$isViewMode ? 'space-between' : 'flex-end'};
+    justify-content: space-between;
     align-items: center;
     button {
         margin: 0;
     }
+
+    ${({ $isViewMode }) =>
+        $isViewMode &&
+        `
+        justify-content: flex-end;
+  `}
 `;
