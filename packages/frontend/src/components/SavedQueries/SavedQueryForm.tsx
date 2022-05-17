e2415fa2ac555296b476f52da6c420fe7e29a70c
Nathalia Buitrago Jurado <31137824+nathaliabj@users.noreply.github.com>
2022-05-17 13:14:11 +0100

    feat: show descriptions on saved charts page (#2130)
    * feat: add tooltip
    
    * feat: add info and new update modal to saved queries
    
    * feat: have description default value
    
    * fix: add check to handle when it is a dashboard

--- packages/frontend/src/components/SavedQueries/SavedQueryForm.tsx
+++ packages/frontend/src/components/SavedQueries/SavedQueryForm.tsx
@@ -1,5 +1,7 @@
+import { Classes } from '@blueprintjs/core';
 import { SpaceQuery } from 'common';
 import React from 'react';
+import { useSavedQuery } from '../../hooks/useSavedQuery';
 import { ActionModalProps, ActionTypeModal } from '../common/modal/ActionModal';
 import Input from '../ReactHookForm/Input';
 
@@ -11,17 +13,32 @@ const SavedQueryForm = ({
     'useActionModalState' | 'isDisabled'
 >) => {
     const [{ actionType, data }] = useActionModalState;
+    const { data: savedChart } = useSavedQuery({
+        id: data?.uuid,
+    });
+
     return (
         <>
             {actionType === ActionTypeModal.UPDATE && (
-                <Input
-                    label="Enter a memorable name for your chart"
-                    name="name"
-                    placeholder="eg. How many weekly active users do we have?"
-                    disabled={isDisabled}
-                    rules={{ required: true }}
-                    defaultValue={data?.name}
-                />
+                <div className={Classes.DIALOG_BODY}>
+                    <Input
+                        label="Enter a memorable name for your chart"
+                        name="name"
+                        placeholder="eg. How many weekly active users do we have?"
+                        disabled={isDisabled}
+                        rules={{
+                            required: 'Required field',
+                        }}
+                        defaultValue={data?.name}
+                    />
+                    <Input
+                        label="Chart description"
+                        name="description"
+                        placeholder="A few words to give your team some context"
+                        disabled={isDisabled}
+                        defaultValue={savedChart?.description}
+                    />
+                </div>
             )}
             {actionType === ActionTypeModal.DELETE && (
                 <p>Are you sure you want to delete this chart ?</p>
---
+++ packages/frontend/src/components/common/ActionCard/ActionCard.styles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const RightButtons = styled.div`
+    display: flex;
+    column-gap: 8px;
+`;
