bddb22c2a55a563fb55165145efbc94b4c896bb4
Rephus <rephus@gmail.com>
2022-05-24 11:33:27 +0200

    feat: cli postinstall message

---
+++ packages/cli/src/postinstall.js
@@ -0,0 +1,12 @@
+// This script will be executed after npm install
+chalk = require('chalk')
+
+const title = chalk.bold.yellowBright;
+const success = chalk.green;
+const { bold } = chalk;
+
+console.log(`
+${title('⚡lightdash')} CLI installed ${success('successfully')}
+
+Run ${bold('lightdash --help')} to get a detailed list of all commands
+`)
