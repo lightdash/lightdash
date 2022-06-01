c64283d96abd379b4364a0c5a7f6f23cc20ed96a
Javier Rengel Jiménez <rephus@gmail.com>
2022-06-01 15:33:01 +0200

    fix: use utc on echart tooltips (#2271)
    * fix: use utc on echart tooltips
    
    * new user on sqlrunner
    
    * order by customer_id
    
    * rename var

--- packages/e2e/cypress/integration/sqlrunner.spec.ts
+++ packages/e2e/cypress/integration/sqlrunner.spec.ts
@@ -1,5 +1,10 @@
 import { SEED_PROJECT } from '@lightdash/common';
 
+const firstCustomer = {
+    firstName: 'Michael',
+    lastName: 'P.',
+    created: '2017-01-30T06:00:00.000Z',
+};
 describe('Dashboard', () => {
     before(() => {
         // @ts-ignore
@@ -19,15 +24,17 @@ describe('Dashboard', () => {
         const customersTable = `"jaffle"."customers"`;
 
         cy.get('.ace_content')
-            .type(`SELECT * FROM ${customersTable} LIMIT 1`)
+            .type(
+                `SELECT * FROM ${customersTable} order by customer_id LIMIT 1`,
+            )
             .type('{ctrl}{enter}');
 
         const find = [
             '1 results',
-            '2017-01-30T06:00:00.000Z',
             'first_name',
-            'Michael',
-            'P.',
+            firstCustomer.created,
+            firstCustomer.firstName,
+            firstCustomer.lastName,
         ];
         find.forEach((text) => cy.findByText(text));
     });
@@ -35,15 +42,17 @@ describe('Dashboard', () => {
     it('Should autocomplete customer table', () => {
         cy.visit(`/projects/${SEED_PROJECT.project_uuid}/sqlRunner`);
 
-        cy.get('.ace_content').type(`SELECT * FROM cu\n`).type('{ctrl}{enter}');
+        cy.get('.ace_content')
+            .type(`SELECT * FROM cu\n order by customer_id`)
+            .type('{ctrl}{enter}');
 
         const find = [
             '100 results',
-            '2017-01-30T06:00:00.000Z',
             'first_name',
-            'Michael',
-            'P.',
-            'Shawn',
+            firstCustomer.created,
+            firstCustomer.firstName,
+            firstCustomer.lastName,
+            'Shawn', // customer_id = 2
         ];
         find.forEach((text) => cy.findAllByText(text));
     });
