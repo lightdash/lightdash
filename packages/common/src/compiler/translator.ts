c68d9c9ac4312885d23a8b9f48da5dc2c83691e3
Jose Rego <jose@lightdash.com>
2022-06-08 14:30:26 +0100

    chore: simplify code

--- packages/common/src/compiler/translator.ts
+++ packages/common/src/compiler/translator.ts
@@ -249,14 +249,12 @@ export const convertTable = (
     const duplicatedNames = Object.keys(allMetrics).filter((metric) =>
         Object.keys(dimensions).includes(metric),
     );
-    if (duplicatedNames.length > 1) {
-        throw new ParseError(
-            `Found multiple metrics and a dimensions with the same name: ${duplicatedNames}`,
-        );
-    } else if (duplicatedNames.length === 1) {
-        throw new ParseError(
-            `Found a metric and a dimension with the same name: ${duplicatedNames[0]}`,
-        );
+    if (duplicatedNames.length > 0) {
+        const message =
+            duplicatedNames.length > 1
+                ? 'Found multiple metrics and a dimensions with the same name:'
+                : 'Found a metric and a dimension with the same name:';
+        throw new ParseError(`${message} ${duplicatedNames}`);
     }
 
     return {
