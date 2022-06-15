cba730d4b8be7f47a0e57badb217654af7c704bf
Rephus <rephus@gmail.com>
2022-06-15 11:47:24 +0200

    update tests

--- packages/common/src/authorization/organizationMemberAbility.mock.ts
+++ packages/common/src/authorization/organizationMemberAbility.mock.ts
@@ -3,22 +3,26 @@ import {
     OrganizationMemberRole,
 } from '../types/organizationMemberProfile';
 
-export const ORGANIZATION_VIEWER: OrganizationMemberProfile = {
+export const ORGANIZATION_MEMBER: OrganizationMemberProfile = {
     userUuid: '123',
     organizationUuid: '456',
-    role: OrganizationMemberRole.VIEWER,
+    role: OrganizationMemberRole.MEMBER,
     firstName: 'jane',
     lastName: 'jackson',
     email: 'jane@gmail.com',
     isActive: true,
 };
 
+export const ORGANIZATION_VIEWER: OrganizationMemberProfile = {
+    ...ORGANIZATION_MEMBER,
+    role: OrganizationMemberRole.VIEWER,
+};
 export const ORGANIZATION_EDITOR: OrganizationMemberProfile = {
-    ...ORGANIZATION_VIEWER,
+    ...ORGANIZATION_MEMBER,
     role: OrganizationMemberRole.EDITOR,
 };
 
 export const ORGANIZATION_ADMIN: OrganizationMemberProfile = {
-    ...ORGANIZATION_VIEWER,
+    ...ORGANIZATION_MEMBER,
     role: OrganizationMemberRole.ADMIN,
 };
