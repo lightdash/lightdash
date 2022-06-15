e0513a80d7ccb9649131d585a5681fef05db23b2
nathaliabj <nathalia.buitragoj@gmail.com>
2022-06-15 12:18:40 +0200

    fix: fix styling of google button, and migrate to SC the social logins component

--- packages/frontend/src/components/common/GoogleLoginButton/index.tsx
+++ packages/frontend/src/components/common/GoogleLoginButton/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useFlashMessages } from '../../../hooks/useFlashMessages';
 import { useApp } from '../../../providers/AppProvider';
-import { GoogleLoginWrapper } from './GoogleLoginButton.styles';
+import {
+    GoogleLoginWrapper,
+    GoogleLogo,
+    LinkContent,
+} from './GoogleLoginButton.styles';
 
 export const GoogleLoginButton: React.FC<{ inviteCode?: string }> = ({
     inviteCode,
@@ -30,7 +34,13 @@ export const GoogleLoginButton: React.FC<{ inviteCode?: string }> = ({
                     : ''
             }`}
         >
-            Sign in with Google
+            <LinkContent>
+                <GoogleLogo
+                    src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="
+                    alt="Google logo"
+                />
+                Sign in with Google
+            </LinkContent>
         </GoogleLoginWrapper>
     );
 };
