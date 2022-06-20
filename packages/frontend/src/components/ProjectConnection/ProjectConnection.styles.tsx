03906a49b87c6fe638804701f9fc8bf5cc1bc3e7
nathaliabj <nathalia.buitragoj@gmail.com>
2022-06-20 16:47:21 +0200

    fix: button bar not sticking to the bottom

--- packages/frontend/src/components/ProjectConnection/ProjectConnection.styles.tsx
+++ packages/frontend/src/components/ProjectConnection/ProjectConnection.styles.tsx
@@ -5,6 +5,9 @@ import Form from '../ReactHookForm/Form';
 
 export const FormContainer = styled(Form)`
     width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const WarehouseLogo = styled.img`
@@ -19,18 +22,19 @@ export const CompileProjectWrapper = styled.div`
     background: ${Colors.WHITE};
     border-top: 1px solid ${Colors.LIGHT_GRAY1};
     bottom: 0;
+    margin-top: auto;
 `;
 
 export const FormWrapper = styled.div`
     width: 800px;
-    margin: auto;
+    margin: 0 auto;
 `;
 
 export const CompileProjectButton = styled(Button)`
     height: 40px;
     width: 215px;
     float: right;
-    margin-top: 15px;
+    margin: 15px 0;
 `;
 
 export const AdvancedButtonWrapper = styled.div`
--- packages/frontend/src/pages/ProjectSettings.styles.tsx
+++ packages/frontend/src/pages/ProjectSettings.styles.tsx
@@ -5,6 +5,7 @@ export const UpdateProjectWrapper = styled.div`
     display: flex;
     flex-direction: column;
     flex: 1;
+    overflow-y: scroll;
 `;
 
 export const UpdateHeaderWrapper = styled.div`
@@ -20,4 +21,13 @@ export const Title = styled(H3)<{ marginBottom?: boolean }>`
         css`
             margin: 0 0 20px;
         `}
+`;
+
+export const ProjectConnectionContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    max-width: 100vw;
+    height: calc(100vh - 50px) !important;
 `;
