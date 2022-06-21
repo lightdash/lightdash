6cad61e8cb8648466e9e751f8d56dcd23bcdec3b
Jose Rego <jose@lightdash.com>
2022-06-21 10:20:43 +0100

    chore: define useAbilityContext hook

--- packages/frontend/src/components/common/Authorization/index.tsx
+++ packages/frontend/src/components/common/Authorization/index.tsx
@@ -1,6 +1,16 @@
-import {Ability} from '@casl/ability';
-import {createContextualCan} from '@casl/react';
-import {createContext} from 'react';
+import { Ability } from '@casl/ability';
+import { createContextualCan } from '@casl/react';
+import { createContext, useContext } from 'react';
 
 export const AbilityContext = createContext(new Ability());
 export const Can = createContextualCan(AbilityContext.Consumer);
+
+export function useAbilityContext() {
+    const context = useContext(AbilityContext);
+    if (context === undefined) {
+        throw new Error(
+            'useAbilityContext must be used within a AbilityContext.Provider',
+        );
+    }
+    return context;
+}
