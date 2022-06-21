7a7718005f2af4cca1ace92cb95a577de08dc209
Jose Rego <jose@lightdash.com>
2022-06-21 10:04:35 +0100

    fix: add project route

--- packages/frontend/src/components/AppRoute.tsx
+++ packages/frontend/src/components/AppRoute.tsx
@@ -2,36 +2,27 @@ import { NonIdealState } from '@blueprintjs/core';
 import React, { ComponentProps, FC } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useOrganisation } from '../hooks/organisation/useOrganisation';
-import { useProjects } from '../hooks/useProjects';
 import { useApp } from '../providers/AppProvider';
 import PageSpinner from './PageSpinner';
 
 const AppRoute: FC<ComponentProps<typeof Route>> = ({ children, ...rest }) => {
     const { health } = useApp();
     const orgRequest = useOrganisation();
-    const projectsRequest = useProjects();
 
     return (
         <Route
             {...rest}
             render={({ location }) => {
-                if (
-                    health.isLoading ||
-                    orgRequest.isLoading ||
-                    projectsRequest.isLoading
-                ) {
+                if (health.isLoading || orgRequest.isLoading) {
                     return <PageSpinner />;
                 }
 
-                if (orgRequest.error || projectsRequest.error || health.error) {
+                if (orgRequest.error || health.error) {
                     return (
                         <div style={{ marginTop: '20px' }}>
                             <NonIdealState
                                 title="Unexpected error"
-                                description={
-                                    (orgRequest.error || projectsRequest.error)
-                                        ?.error.message
-                                }
+                                description={orgRequest.error?.error.message}
                             />
                         </div>
                     );
@@ -46,13 +37,6 @@ const AppRoute: FC<ComponentProps<typeof Route>> = ({ children, ...rest }) => {
                             }}
                         />
                     );
-                }
-
-                if (
-                    !projectsRequest.data ||
-                    projectsRequest.data?.length <= 0
-                ) {
-                    return <Redirect to={`/no-access`} />;
                 }
 
                 return children;
---
+++ packages/frontend/src/components/ProjectRoute.tsx
@@ -0,0 +1,73 @@
+import { NonIdealState } from '@blueprintjs/core';
+import { subject } from '@casl/ability';
+import React, { ComponentProps, FC } from 'react';
+import { Redirect, Route, useParams } from 'react-router-dom';
+import { useProjects } from '../hooks/useProjects';
+import { useApp } from '../providers/AppProvider';
+import { Can } from './common/Authorization';
+import PageSpinner from './PageSpinner';
+
+const ProjectRoute: FC<ComponentProps<typeof Route>> = ({
+    children,
+    ...rest
+}) => {
+    const params = useParams<{ projectUuid: string | undefined }>();
+    const { user } = useApp();
+    const projectsRequest = useProjects();
+
+    return (
+        <Route
+            {...rest}
+            render={() => {
+                if (projectsRequest.isLoading) {
+                    return <PageSpinner />;
+                }
+
+                if (projectsRequest.error) {
+                    return (
+                        <div style={{ marginTop: '20px' }}>
+                            <NonIdealState
+                                title="Unexpected error"
+                                description={
+                                    projectsRequest.error.error.message
+                                }
+                            />
+                        </div>
+                    );
+                }
+
+                if (
+                    !projectsRequest.data ||
+                    projectsRequest.data?.length <= 0
+                ) {
+                    return <Redirect to={`/no-access`} />;
+                }
+
+                return (
+                    <Can
+                        I="view"
+                        this={subject('Project', {
+                            organizationUuid: user.data?.organizationUuid,
+                            projectUuid: params.projectUuid,
+                        })}
+                        passThrough
+                    >
+                        {(isAllowed) => {
+                            console.log('isAllowed', isAllowed, {
+                                organizationUuid: user.data?.organizationUuid,
+                                projectUuid: params.projectUuid,
+                            });
+                            return isAllowed ? (
+                                children
+                            ) : (
+                                <Redirect to={`/no-project-access`} />
+                            );
+                        }}
+                    </Can>
+                );
+            }}
+        />
+    );
+};
+
+export default ProjectRoute;
