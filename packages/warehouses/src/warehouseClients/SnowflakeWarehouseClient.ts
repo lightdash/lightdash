21551871f4d089b1fae96210d6cad847f5d4f38b
Jose Rego <jose@lightdash.com>
2022-06-22 14:31:48 +0100

    fix: parse snowflake date

--- packages/warehouses/src/warehouseClients/SnowflakeWarehouseClient.ts
+++ packages/warehouses/src/warehouseClients/SnowflakeWarehouseClient.ts
@@ -85,6 +85,24 @@ export const mapFieldType = (type: string): DimensionType => {
     }
 };
 
+const parseCell = (cell: any) => {
+    if (cell instanceof Date) {
+        return new Date(cell);
+    }
+
+    return cell;
+};
+
+const parseRows = (rows: Record<string, any>[]) =>
+    rows.map((row) =>
+        Object.fromEntries(
+            Object.entries(row).map(([name, value]) => [
+                name,
+                parseCell(value),
+            ]),
+        ),
+    );
+
 export class SnowflakeWarehouseClient implements WarehouseClient {
     connectionOptions: ConnectionOptions;
 
@@ -117,7 +135,6 @@ export class SnowflakeWarehouseClient implements WarehouseClient {
             }>((resolve, reject) => {
                 connection.execute({
                     sqlText,
-                    fetchAsString: ['Date'],
                     complete: (err, stmt, data) => {
                         if (err) {
                             reject(err);
@@ -134,7 +151,7 @@ export class SnowflakeWarehouseClient implements WarehouseClient {
                                 }),
                                 {},
                             );
-                            resolve({ fields, rows: data });
+                            resolve({ fields, rows: parseRows(data) });
                         } else {
                             reject(
                                 new WarehouseQueryError(
