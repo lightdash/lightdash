0ffa076514a91c6a9700ac5510b12e07759b02f1
Rephus <rephus@gmail.com>
2022-07-20 10:24:46 +0200

    fix tests

--- packages/backend/src/services/ProjectService/ProjectService.mock.ts
+++ packages/backend/src/services/ProjectService/ProjectService.mock.ts
@@ -25,6 +25,7 @@ import {
     TableSelectionType,
 } from '@lightdash/common';
 import { LightdashConfig } from '../../config/parseConfig';
+import { projectUuid } from '../../models/ProjectModel/ProjectModel.mock';
 import { ProjectAdapter } from '../../types';
 
 export const user: SessionUser = {
@@ -242,6 +243,7 @@ export const spacesWithSavedCharts: Space[] = [
                 updatedAt: new Date(),
             },
         ],
+        projectUuid,
     },
 ];
 
@@ -251,6 +253,7 @@ export const spacesWithNoSavedCharts: Space[] = [
         name: 'sapce',
         uuid: 'uuid',
         queries: [],
+        projectUuid,
     },
 ];
 
