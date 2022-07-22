f03a77ba21f04b0b7597e56c505dfaa77610427f
Javier Rengel Jiménez <rephus@gmail.com>
2022-07-22 17:36:42 +0200

    feat: navigate-spaces-navbar (#2762)
    * feat: navigate-spaces-navbar
    
    * Hide navbar

--- packages/frontend/src/components/NavBar/BrowseMenu/index.tsx
+++ packages/frontend/src/components/NavBar/BrowseMenu/index.tsx
@@ -7,44 +7,56 @@ import {
 } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 import React, { FC } from 'react';
+import { useSpaces } from '../../../hooks/useSpaces';
 import NavLink from '../../NavLink';
 import { FirstItem, MenuWrapper, SpinnerWrapper } from '../NavBar.styles';
 
 interface Props {
-    projectId: string | undefined;
+    projectId: string;
 }
 
-const BrowseMenu: FC<Props> = ({ projectId }) => (
-    <>
-        <Popover2
-            interactionKind={PopoverInteractionKind.CLICK}
-            content={
-                !projectId ? (
-                    <SpinnerWrapper>
-                        <Spinner size={20} />
-                    </SpinnerWrapper>
-                ) : (
-                    <MenuWrapper>
-                        <NavLink to={`/projects/${projectId}/dashboards`}>
-                            <FirstItem
-                                role="button"
-                                text="Dashboards"
-                                icon="control"
-                            />
-                        </NavLink>
-                        <NavLink
-                            to={`/projects/${projectId}/saved`}
-                            style={{ marginBottom: 5 }}
-                        >
-                            <MenuItem icon="chart" text="Saved charts" />
-                        </NavLink>
-                    </MenuWrapper>
-                )
-            }
-            position={Position.BOTTOM_LEFT}
-        >
-            <Button minimal icon="search" text="Browse" />
-        </Popover2>
-    </>
-);
+const BrowseMenu: FC<Props> = ({ projectId }) => {
+    const { data: spaces, isLoading } = useSpaces(projectId);
+    return (
+        <>
+            <Popover2
+                interactionKind={PopoverInteractionKind.CLICK}
+                content={
+                    !projectId || isLoading ? (
+                        <SpinnerWrapper>
+                            <Spinner size={20} />
+                        </SpinnerWrapper>
+                    ) : (
+                        <MenuWrapper>
+                            <NavLink to={`/projects/${projectId}/dashboards`}>
+                                <FirstItem
+                                    role="button"
+                                    text="Dashboards"
+                                    icon="control"
+                                />
+                            </NavLink>
+                            <NavLink to={`/projects/${projectId}/saved`}>
+                                <MenuItem icon="chart" text="Saved charts" />
+                            </NavLink>
+                            {/*<MenuDivider />
+                            SPACE_TODO {spaces?.map((space) => (
+                                <NavLink
+                                    to={`/projects/${projectId}/spaces/${space.uuid}`}
+                                >
+                                    <MenuItem
+                                        icon="folder-close"
+                                        text={space.name}
+                                    />
+                                </NavLink>
+                            ))}*/}
+                        </MenuWrapper>
+                    )
+                }
+                position={Position.BOTTOM_LEFT}
+            >
+                <Button minimal icon="search" text="Browse" />
+            </Popover2>
+        </>
+    );
+};
 export default BrowseMenu;
