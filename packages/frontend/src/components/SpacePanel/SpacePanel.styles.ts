e7867f9c22319a01c8bbb82e7b291fb12099970b
Rephus <rephus@gmail.com>
2022-08-01 12:13:48 +0200

    font size title dashboards and charts

--- packages/frontend/src/components/SpacePanel/SpacePanel.styles.ts
+++ packages/frontend/src/components/SpacePanel/SpacePanel.styles.ts
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 
 export const Title = styled(H3)`
     text-align: left;
+    font-size: 22px !important;
 `;
 
 export const SpacePanelWrapper = styled.div`
     .bp4-card:last-child {
         margin-top: 20px;
+    }
+
+    h3 {
+        font-size: 18px;
     }
 `;
