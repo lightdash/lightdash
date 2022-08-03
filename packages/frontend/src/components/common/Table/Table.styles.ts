3ff6d20c3d3159152c0bd0385c65facc23cff011
Javier Rengel Jiménez <rephus@gmail.com>
2022-08-03 16:25:47 +0200

    fix: fix column name header get cut off (#2833)
    * fix: fix column name header get cut off
    
    * support up to 3 rows
    
    * 4

--- packages/frontend/src/components/common/Table/Table.styles.ts
+++ packages/frontend/src/components/common/Table/Table.styles.ts
@@ -10,7 +10,7 @@ export const TableContainer = styled.div`
 `;
 export const TableScrollableWrapper = styled.div`
     overflow: auto;
-    min-height: 30px;
+    min-height: 90px;
 `;
 
 export const Table = styled(HTMLTable)`
