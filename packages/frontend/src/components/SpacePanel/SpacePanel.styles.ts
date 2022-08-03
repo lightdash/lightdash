870d728211b51c0809621372aebfd696302fb940
Javier Rengel Jiménez <rephus@gmail.com>
2022-08-03 10:26:14 +0200

    style: Update breadcrumbs style (#2834)

--- packages/frontend/src/components/SpacePanel/SpacePanel.styles.ts
+++ packages/frontend/src/components/SpacePanel/SpacePanel.styles.ts
@@ -1,26 +1,30 @@
-import { Colors, H3, H4, Icon } from '@blueprintjs/core';
+import { Colors, H4, Icon } from '@blueprintjs/core';
 import styled from 'styled-components';
-
-export const Title = styled(H3)`
-    margin-top: 10px;
-    margin-bottom: 30px;
-    text-align: left;
-    font-size: 22px !important;
-`;
 
 export const SpacePanelWrapper = styled.div`
     .bp4-card:last-child {
         margin-top: 20px;
     }
 
-    .home-breadcrumb {
-        color: ${Colors.BLUE3};
-    }
     h3 {
         font-size: 18px;
     }
 `;
 
+export const BreadcrumbsWrapper = styled.div`
+    .bp4-breadcrumb {
+        font-size: 22px;
+        font-weight: 500;
+    }
+    .home-breadcrumb {
+        color: #5c7080;
+    }
+    padding: 11px 0px 31px 0px;
+
+    .bp4-breadcrumbs > li::after {
+        margin-top: 5px;
+    }
+`;
 export const EmptyStateWrapper = styled.div`
     max-width: 250px;
 `;
