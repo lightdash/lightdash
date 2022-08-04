a0515d1efa240e1c2c76a2d7f1feefbdb2f24fe4
Jose Rego <jose@lightdash.com>
2022-08-04 16:47:37 +0100

    chore: simplify highlighted text componet

--- packages/frontend/src/components/common/HighlightedText/index.tsx
+++ packages/frontend/src/components/common/HighlightedText/index.tsx
@@ -1,46 +1,34 @@
 import React, { FC } from 'react';
 
-function escapeRegExpChars(text: string) {
-    //eslint-disable-next-line
-    return text.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
-}
-
 type Props = {
     text: string;
     query: string;
     highlightElement: FC;
 };
 
-const HighlightedText: FC<Props> = ({ text, query, highlightElement }) => {
+export const HighlightedText: FC<Props> = ({
+    text = '',
+    query = '',
+    highlightElement,
+}) => {
     const Highlight = highlightElement;
-    let lastIndex = 0;
-    const words = query
-        .split(/\s+/)
-        .filter((word) => word.length > 0)
-        .map(escapeRegExpChars);
-    if (words.length === 0) {
-        return <>text</>;
+    if (!query.trim()) {
+        return <span>{text}</span>;
     }
-    const regexp = new RegExp(words.join('|'), 'gi');
-    const tokens: React.ReactNode[] = [];
-    while (true) {
-        const match = regexp.exec(text);
-        if (!match) {
-            break;
-        }
-        const length = match[0].length;
-        const before = text.slice(lastIndex, regexp.lastIndex - length);
-        if (before.length > 0) {
-            tokens.push(before);
-        }
-        lastIndex = regexp.lastIndex;
-        tokens.push(<Highlight key={lastIndex}>{match[0]}</Highlight>);
-    }
-    const rest = text.slice(lastIndex);
-    if (rest.length > 0) {
-        tokens.push(rest);
-    }
-    return <>tokens</>;
+    const regex = new RegExp(`(${query})`, 'gi');
+    const parts = text.split(regex);
+
+    return (
+        <span>
+            {parts.filter(String).map((part, i) => {
+                return regex.test(part) ? (
+                    <Highlight key={i}>{part}</Highlight>
+                ) : (
+                    <span key={i}>{part}</span>
+                );
+            })}
+        </span>
+    );
 };
 
 export default HighlightedText;
