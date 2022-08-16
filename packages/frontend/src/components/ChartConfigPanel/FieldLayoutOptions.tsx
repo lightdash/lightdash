7e7202f599d221c6ee5c8749902148b0164f7bae
Jose Rego <jose@lightdash.com>
2022-08-16 18:20:03 +0100

    chore: remove placeholders

--- packages/frontend/src/components/ChartConfigPanel/FieldLayoutOptions.tsx
+++ packages/frontend/src/components/ChartConfigPanel/FieldLayoutOptions.tsx
@@ -103,11 +103,6 @@ const FieldLayoutOptions: FC<Props> = ({ items }) => {
                 <AxisFieldDropdown>
                     <FieldAutoComplete
                         fields={items}
-                        placeholder={
-                            !!dirtyLayout?.xField && !xAxisField
-                                ? `${dirtyLayout?.xField} not available`
-                                : undefined
-                        }
                         activeField={xAxisField}
                         onChange={(item) => {
                             setXField(getItemId(item));
@@ -127,11 +122,6 @@ const FieldLayoutOptions: FC<Props> = ({ items }) => {
                     return (
                         <AxisFieldDropdown key={`${field}-y-axis`}>
                             <FieldAutoComplete
-                                placeholder={
-                                    !!field && !activeField
-                                        ? `${field} not available`
-                                        : undefined
-                                }
                                 fields={
                                     activeField
                                         ? [activeField, ...availableYFields]
@@ -171,11 +161,7 @@ const FieldLayoutOptions: FC<Props> = ({ items }) => {
                 <AxisFieldDropdown>
                     <FieldAutoComplete
                         fields={availableDimensions}
-                        placeholder={
-                            !!pivotDimension && !groupSelectedField
-                                ? `${pivotDimension} not available`
-                                : 'Select a field to group by'
-                        }
+                        placeholder="Select a field to group by"
                         activeField={groupSelectedField}
                         onChange={(item) => {
                             setPivotDimensions([getItemId(item)]);
