fd15155b68d5e67bc430d2e5c7abac8356520041
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-08-17 12:37:25 +0400

    chore: add frontend proxy to the backend

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig({
     },
     server: {
         port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
     },
     plugins: [
         reactRefresh(),
