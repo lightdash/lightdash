1d9e1a636e353d6e7e5fef502763f1793c9d9096
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-08-17 12:38:48 +0400

    chore: use react plugin to fix jsx importing React automatically

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgrPlugin from 'vite-plugin-svgr';
 
@@ -24,6 +25,7 @@ export default defineConfig({
         },
     },
     plugins: [
+        react(),
         reactRefresh(),
         svgrPlugin({
             svgrOptions: {
