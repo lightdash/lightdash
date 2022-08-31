6648b27abb4f5df509f3b21e2f054c2ac5524bed
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-08-31 14:23:42 +0000

    feat: add feedback card on upload success

--- packages/frontend/src/components/ReactHookForm/FileInput.tsx
+++ packages/frontend/src/components/ReactHookForm/FileInput.tsx
@@ -1,4 +1,4 @@
-import { FileInput as File } from '@blueprintjs/core';
+import { Card, Elevation, FileInput as File } from '@blueprintjs/core';
 import React, { ComponentProps, FC, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import InputWrapper, { InputWrapperProps } from './InputWrapper';
@@ -16,41 +16,60 @@ const FileInput: FC<FileFieldProps> = ({
     const [fileName, setFileName] = useState<string>();
     const { setError } = useFormContext();
     return (
-        <InputWrapper
-            {...rest}
-            render={(props, { field: { onChange, onBlur, name } }) => (
-                <File
-                    fill
-                    hasSelection={!!fileName}
-                    text={fileName}
-                    inputProps={{
-                        accept: acceptedTypes,
+        <>
+            <InputWrapper
+                {...rest}
+                render={(props, { field: { onChange, onBlur, name } }) => (
+                    <File
+                        fill
+                        hasSelection={!!fileName}
+                        text={fileName}
+                        inputProps={{
+                            accept: acceptedTypes,
+                        }}
+                        onInputChange={(e) => {
+                            const file = (e.target as HTMLInputElement)
+                                .files?.[0];
+                            if (file) {
+                                const fileReader = new FileReader();
+                                fileReader.onload = (event) => {
+                                    const contents = event.target?.result;
+                                    if (typeof contents === 'string') {
+                                        setFileName(file.name);
+                                        onChange(JSON.parse(contents));
+                                    } else {
+                                        setError(name, {
+                                            message: 'Unexpected file content',
+                                            type: 'unexpected_file_content',
+                                        });
+                                    }
+                                };
+                                fileReader.readAsText(file);
+                            }
+                        }}
+                        onBlur={onBlur}
+                        {...props}
+                        {...fileInputProps}
+                    />
+                )}
+            />
+            {fileName && (
+                <Card
+                    style={{
+                        backgroundColor: '#B1ECB5', // light green
+                        textAlign: 'center',
+                        padding: 10,
                     }}
-                    onInputChange={(e) => {
-                        const file = (e.target as HTMLInputElement).files?.[0];
-                        if (file) {
-                            const fileReader = new FileReader();
-                            fileReader.onload = (event) => {
-                                const contents = event.target?.result;
-                                if (typeof contents === 'string') {
-                                    setFileName(file.name);
-                                    onChange(JSON.parse(contents));
-                                } else {
-                                    setError(name, {
-                                        message: 'Unexpected file content',
-                                        type: 'unexpected_file_content',
-                                    });
-                                }
-                            };
-                            fileReader.readAsText(file);
-                        }
-                    }}
-                    onBlur={onBlur}
-                    {...props}
-                    {...fileInputProps}
-                />
+                    elevation={Elevation.ONE}
+                >
+                    <span>
+                        Your file{' '}
+                        <span style={{ fontWeight: 'bold' }}>{fileName}</span>{' '}
+                        was uploaded successfully!
+                    </span>
+                </Card>
             )}
-        />
+        </>
     );
 };
 
