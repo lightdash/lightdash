8b5ae7e61f4f181f5c1b446745d0a7d51a3812df
Rephus <rephus@gmail.com>
2022-08-31 09:20:49 +0200

    github , gitlab

--- packages/frontend/src/components/ProjectConnection/DbtForms/GitlabForm.tsx
+++ packages/frontend/src/components/ProjectConnection/DbtForms/GitlabForm.tsx
@@ -18,7 +18,26 @@ const GitlabForm: FC<{ disabled: boolean }> = ({ disabled }) => {
             <PasswordInput
                 name="dbt.personal_access_token"
                 label="Personal access token"
-                documentationUrl="https://docs.lightdash.com/get-started/setup-lightdash/connect-project#personal-access-token-2"
+                labelHelp={
+                    <>
+                        <p>
+                            This is used to access your repo. See the{' '}
+                            <a
+                                target="_blank"
+                                href="https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html"
+                                rel="noreferrer"
+                            >
+                                instructions for creating a personal access
+                                token here
+                            </a>
+                            .
+                        </p>
+                        <p>
+                            Select read_repository scope when you're creating
+                            the token.
+                        </p>
+                    </>
+                }
                 rules={{
                     required: requireSecrets ? 'Required field' : undefined,
                 }}
@@ -30,7 +49,12 @@ const GitlabForm: FC<{ disabled: boolean }> = ({ disabled }) => {
             <Input
                 name="dbt.repository"
                 label="Repository"
-                documentationUrl="https://docs.lightdash.com/get-started/setup-lightdash/connect-project#repository-1"
+                labelHelp={
+                    <p>
+                        This should be in the format <b>my-org/my-repo</b>. e.g.{' '}
+                        <b>lightdash/lightdash-analytics</b>
+                    </p>
+                }
                 rules={{
                     required: 'Required field',
                     validate: {
@@ -44,7 +68,19 @@ const GitlabForm: FC<{ disabled: boolean }> = ({ disabled }) => {
             <Input
                 name="dbt.branch"
                 label="Branch"
-                documentationUrl="https://docs.lightdash.com/get-started/setup-lightdash/connect-project#branch-1"
+                labelHelp={
+                    <>
+                        <p>
+                            This is the branch in your Gitlab repo that
+                            Lightdash should sync to. e.g. <b>main</b>,{' '}
+                            <b>master</b> or <b>dev</b>
+                        </p>
+                        <p>
+                            By default, we've set this to <b>main</b> but you
+                            can change it to whatever you'd like.
+                        </p>
+                    </>
+                }
                 rules={{
                     required: 'Required field',
                     validate: {
@@ -57,7 +93,33 @@ const GitlabForm: FC<{ disabled: boolean }> = ({ disabled }) => {
             <Input
                 name="dbt.project_sub_path"
                 label="Project directory path"
-                documentationUrl="https://docs.lightdash.com/get-started/setup-lightdash/connect-project#project-directory-path-1"
+                labelHelp={
+                    <>
+                        <p>
+                            This is the folder where your <b>dbt_project.yml</b>{' '}
+                            file is found in the GitLab repository you entered
+                            above.
+                        </p>
+                        <p>
+                            If your <b>dbt_project.yml</b> file is in the main
+                            folder of your repo (e.g.{' '}
+                            <b>lightdash/lightdash-analytics/dbt_project.yml</b>
+                            ), then you don't need to change anything in here.
+                            You can just leave the default value we've put in.
+                        </p>
+
+                        <p>
+                            If your dbt project is in a sub-folder in your repo
+                            (e.g.{' '}
+                            <b>
+                                lightdash/lightdash-analytics/dbt/dbt_project.yml
+                            </b>
+                            ), then you'll need to include the path to the
+                            sub-folder where your dbt project is (e.g.
+                            <b>/dbt</b>).
+                        </p>
+                    </>
+                }
                 rules={{
                     required: 'Required field',
                     validate: {
