3fe4ae0c3ec6312cc09a362f5084ca05831fe9ac
Mohamed Muhsin <62111075+muhsinkamil@users.noreply.github.com>
2022-09-02 22:17:02 +0530

    feat: add feedback card on upload success (#3121)
    * feat: add feedback card on upload success
    
    * feat: replace card with callout for feedback

--- packages/frontend/src/components/ReactHookForm/FileInput.tsx
+++ packages/frontend/src/components/ReactHookForm/FileInput.tsx
@@ -1,4 +1,4 @@
-import { FileInput as File } from '@blueprintjs/core';
+import { Callout, FileInput as File } from '@blueprintjs/core';
 import React, { ComponentProps, FC, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import InputWrapper, { InputWrapperProps } from './InputWrapper';
@@ -16,41 +16,49 @@ const FileInput: FC<FileFieldProps> = ({
     const [fileName, setFileName] = useState<string>();
     const { setError } = useFormContext();
     return (
-        <InputWrapper
-            {...rest}
-            render={(props, { field: { onChange, onBlur, name } }) => (
-                <File
-                    fill
-                    hasSelection={!!fileName}
-                    text={fileName}
-                    inputProps={{
-                        accept: acceptedTypes,
-                    }}
-                    onInputChange={(e) => {
-                        const file = (e.target as HTMLInputElement).files?.[0];
-                        if (file) {
-                            const fileReader = new FileReader();
-                            fileReader.onload = (event) => {
-                                const contents = event.target?.result;
-                                if (typeof contents === 'string') {
-                                    setFileName(file.name);
-                                    onChange(JSON.parse(contents));
-                                } else {
-                                    setError(name, {
-                                        message: 'Unexpected file content',
-                                        type: 'unexpected_file_content',
-                                    });
-                                }
-                            };
-                            fileReader.readAsText(file);
-                        }
-                    }}
-                    onBlur={onBlur}
-                    {...props}
-                    {...fileInputProps}
-                />
+        <>
+            <InputWrapper
+                {...rest}
+                render={(props, { field: { onChange, onBlur, name } }) => (
+                    <File
+                        fill
+                        hasSelection={!!fileName}
+                        text={fileName}
+                        inputProps={{
+                            accept: acceptedTypes,
+                        }}
+                        onInputChange={(e) => {
+                            const file = (e.target as HTMLInputElement)
+                                .files?.[0];
+                            if (file) {
+                                const fileReader = new FileReader();
+                                fileReader.onload = (event) => {
+                                    const contents = event.target?.result;
+                                    if (typeof contents === 'string') {
+                                        setFileName(file.name);
+                                        onChange(JSON.parse(contents));
+                                    } else {
+                                        setError(name, {
+                                            message: 'Unexpected file content',
+                                            type: 'unexpected_file_content',
+                                        });
+                                    }
+                                };
+                                fileReader.readAsText(file);
+                            }
+                        }}
+                        onBlur={onBlur}
+                        {...props}
+                        {...fileInputProps}
+                    />
+                )}
+            />
+            {fileName && (
+                <Callout intent="success">
+                    Your file <b>{fileName}</b> was uploaded successfully!
+                </Callout>
             )}
-        />
+        </>
     );
 };
 
