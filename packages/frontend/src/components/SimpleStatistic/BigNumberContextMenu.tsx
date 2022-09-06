2627662c9accd789f47a3212c1a9b2186fe2318c
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-09-06 14:55:15 +0400

    feat: change bignumber context menu to open on left-click

--- packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
+++ packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
@@ -1,25 +1,19 @@
-import { Menu, Portal } from '@blueprintjs/core';
-import {
-    MenuItem2,
-    Popover2,
-    Popover2TargetProps,
-} from '@blueprintjs/popover2';
+import { Menu, Position } from '@blueprintjs/core';
+import { MenuItem2, Popover2, Popover2Props } from '@blueprintjs/popover2';
 import { fieldId, getFields, ResultRow } from '@lightdash/common';
 import { FC, useCallback } from 'react';
 import { useExplore } from '../../hooks/useExplore';
 import { useVisualizationContext } from '../LightdashVisualization/VisualizationProvider';
 import { useUnderlyingDataContext } from '../UnderlyingData/UnderlyingDataProvider';
 
-export const BigNumberContextMenu: FC<{
-    isOpen: boolean;
-    onClose: () => void;
-    position: {
-        left: number;
-        top: number;
-    };
-}> = ({ isOpen, onClose, position }) => {
-    const { resultsData, bigNumberConfig } = useVisualizationContext();
+interface BigNumberContextMenuProps {
+    renderTarget: Popover2Props['renderTarget'];
+}
 
+export const BigNumberContextMenu: FC<BigNumberContextMenuProps> = ({
+    renderTarget,
+}) => {
+    const { resultsData, bigNumberConfig } = useVisualizationContext();
     const { viewData, tableName } = useUnderlyingDataContext();
     const { data: explore } = useExplore(tableName);
 
@@ -41,44 +35,24 @@ export const BigNumberContextMenu: FC<{
             viewData(value, meta, row);
         }
     }, [explore, resultsData, bigNumberConfig, viewData]);
-    const contextMenuRenderTarget = useCallback(
-        ({ ref }: Popover2TargetProps) => (
-            <Portal>
-                <div style={{ position: 'absolute', ...position }} ref={ref} />
-            </Portal>
-        ),
-        [position],
-    );
 
-    const cancelContextMenu = useCallback(
-        (e: React.SyntheticEvent<HTMLDivElement>) => e.preventDefault(),
-        [],
-    );
     return (
         <Popover2
+            lazy
+            minimal
+            position={Position.BOTTOM}
+            renderTarget={renderTarget}
             content={
-                <div onContextMenu={cancelContextMenu}>
-                    <Menu>
-                        <MenuItem2
-                            text={`View underlying data`}
-                            icon={'layers'}
-                            onClick={() => {
-                                viewUnderlyingData();
-                            }}
-                        />
-                    </Menu>
-                </div>
+                <Menu>
+                    <MenuItem2
+                        text="View underlying data"
+                        icon="layers"
+                        onClick={() => {
+                            viewUnderlyingData();
+                        }}
+                    />
+                </Menu>
             }
-            enforceFocus={false}
-            hasBackdrop={true}
-            isOpen={isOpen}
-            minimal={true}
-            onClose={onClose}
-            placement="right-start"
-            positioningStrategy="fixed"
-            rootBoundary={'viewport'}
-            renderTarget={contextMenuRenderTarget}
-            transitionDuration={100}
         />
     );
 };
