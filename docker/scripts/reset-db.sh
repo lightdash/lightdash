85160c1c13ba5881f86941d15ba65a4e2cdf926b
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-09-08 14:01:29 +0400

    fix: allow empty PG passwords

--- docker/scripts/reset-db.sh
+++ docker/scripts/reset-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -z "$PGPASSWORD" ]; then 
+if [ ! -v PGPASSWORD ]; then
    echo "Can't find ENV variables (PGPASSWORD), have you loaded them with 'source docker/.env' ?"
     exit 1
 fi
@@ -9,9 +9,9 @@ fi
 # delete public schema on db
 PGPASSWORD=$PGPASSWORD psql -p $PGPORT -h $PGHOST -d $PGDATABASE -U $PGUSER -c 'drop schema public cascade; create schema public;'
 
-# migrate 
+# migrate
 yarn workspace backend migrate
-# seed 
+# seed
 yarn workspace backend seed
 
 echo "All done"
