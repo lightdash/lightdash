ca0e50f0704611ba38efb4127047bf483e69380e
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-09-11 21:32:06 +0400

    * chore: bring vite * chore: disable some eslint rules

---
+++ packages/frontend/vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [svgr(), react()],
+    build: {
+        outDir: 'build',
+    },
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
+    },
+    clearScreen: false,
+});
