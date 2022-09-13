143b6bdcd7e02c590891db00957d750193e85e69
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-09-13 15:10:48 +0400

    style: update charts page to use ResourceList

--- packages/frontend/src/components/SavedQueries/SavedQueriesContent.tsx
+++ packages/frontend/src/components/SavedQueries/SavedQueriesContent.tsx
@@ -1,46 +1,76 @@
-import { SpaceQuery } from '@lightdash/common';
-import React, { ReactNode } from 'react';
+import { Button } from '@blueprintjs/core';
+import { Breadcrumbs2 } from '@blueprintjs/popover2';
+import { LightdashMode, SpaceQuery } from '@lightdash/common';
+import { useHistory } from 'react-router-dom';
+import { useApp } from '../../providers/AppProvider';
 import {
-    useDeleteMutation,
-    useUpdateMutation,
-} from '../../hooks/useSavedQuery';
-import ActionCardList from '../common/ActionCardList';
-import SavedQueryForm from './SavedQueryForm';
+    PageBreadcrumbsWrapper,
+    PageContentWrapper,
+    PageHeader,
+} from '../common/Page/Page.styles';
+import ResourceList from '../common/ResourceList';
 
 type SavedQueriesContentProps = {
     savedQueries: SpaceQuery[];
     projectUuid: string;
-    headerAction?: ReactNode;
-    emptyBody?: ReactNode;
     title: string;
 };
 
 const SavedQueriesContent = ({
     savedQueries,
     projectUuid,
-    headerAction,
-    emptyBody,
     title,
 }: SavedQueriesContentProps) => {
+    const history = useHistory();
+    const { user, health } = useApp();
+    const isDemo = health.data?.mode === LightdashMode.DEMO;
+
     const orderedCharts = savedQueries.sort(
         (a, b) =>
             new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
     );
+
     return (
-        <ActionCardList
-            title={title}
-            useUpdate={useUpdateMutation}
-            useDelete={useDeleteMutation()}
-            dataList={orderedCharts}
-            getURL={(savedQuery: SpaceQuery) => {
-                const { uuid } = savedQuery;
-                return `/projects/${projectUuid}/saved/${uuid}`;
-            }}
-            ModalContent={SavedQueryForm}
-            headerAction={headerAction}
-            emptyBody={emptyBody}
-            isChart
-        />
+        <PageContentWrapper>
+            <PageHeader>
+                <PageBreadcrumbsWrapper>
+                    <Breadcrumbs2
+                        items={[
+                            {
+                                href: '/home',
+                                text: 'Home',
+                                className: 'home-breadcrumb',
+                                onClick: (e) => {
+                                    history.push('/home');
+                                },
+                            },
+                            { text: title },
+                        ]}
+                    />
+                </PageBreadcrumbsWrapper>
+
+                {user.data?.ability?.can('manage', 'Dashboard') && !isDemo && (
+                    <Button
+                        text="Create chart"
+                        icon="plus"
+                        onClick={() => {
+                            // createDashboard({
+                            //     name: DEFAULT_DASHBOARD_NAME,
+                            //     tiles: [],
+                            // })
+                        }}
+                        intent="primary"
+                    />
+                )}
+            </PageHeader>
+
+            <ResourceList
+                resourceIcon="chart"
+                resourceType="saved_chart"
+                resourceList={orderedCharts}
+                getURL={({ uuid }) => `/projects/${projectUuid}/saved/${uuid}`}
+            />
+        </PageContentWrapper>
     );
 };
 
--- packages/frontend/src/components/common/Page/Page.styles.ts
+++ packages/frontend/src/components/common/Page/Page.styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const PageContentWrapper = styled.div`
+    .bp4-card:not(:last-child) {
+        margin-bottom: 20px;
+    }
+
     h3 {
         font-size: 18px;
     }
