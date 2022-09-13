b0cdc49c2877a8402d5e51a3de1fc8fc082a34a9
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-09-13 15:01:42 +0400

    style: update dashboards to use ResourceList

--- packages/frontend/src/components/common/Page/Page.styles.ts
+++ packages/frontend/src/components/common/Page/Page.styles.ts
@@ -1,23 +1,19 @@
 import styled from 'styled-components';
 
-export const SpacePanelWrapper = styled.div`
-    .bp4-card:last-child {
-        margin-top: 20px;
-    }
-
+export const PageContentWrapper = styled.div`
     h3 {
         font-size: 18px;
     }
 `;
 
-export const SpacePanelHeader = styled.div`
+export const PageHeader = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 11px 0px 31px 0px;
 `;
 
-export const BreadcrumbsWrapper = styled.div`
+export const PageBreadcrumbsWrapper = styled.div`
     .bp4-breadcrumb {
         font-size: 22px;
         font-weight: 500;
