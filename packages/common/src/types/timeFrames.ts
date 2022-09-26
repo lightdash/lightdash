dba3f400017d262ec58873174c2e962c8f15e917
Jose Rego <jose@lightdash.com>
2022-09-26 11:25:37 +0100

    wip: date parts

--- packages/common/src/types/timeFrames.ts
+++ packages/common/src/types/timeFrames.ts
@@ -10,4 +10,40 @@ export enum TimeFrames {
     MINUTE = 'MINUTE',
     SECOND = 'SECOND',
     MILLISECOND = 'MILLISECOND',
+    MONTH_NUM = 'MONTH_NUM',
+    MONTH_NAME = 'MONTH_NAME',
 }
+
+/*
+## Dates
+
+RAW
+MILLISECOND
+SECOND
+MINUTE
+HOUR
+DAY
+WEEK
+MONTH
+QUARTER
+YEAR
+
+## Numbers
+
+day_of_week_index |	2  (0 = Monday, 6 = Sunday)
+day_of_month_num | 20
+month_num |	9
+quarter_num	| 2
+year_num | 2017
+
+## Strings
+
+day_of_week_name | Wednesday
+month_name | September
+quarter_name |	Q2
+
+???? these don't seem to fit the naming convention
+year_and_quarter | 2018-Q2
+year_and_month | 2018-05
+
+ */
--- packages/common/src/utils/timeFrames.ts
+++ packages/common/src/utils/timeFrames.ts
@@ -1,41 +1,175 @@
+import { SupportedDbtAdapter } from '../types/dbt';
+import { ParseError } from '../types/errors';
 import { DimensionType } from '../types/field';
 import { TimeFrames } from '../types/timeFrames';
 
 type TimeFrameConfig = {
     getDimensionType: (fallback: DimensionType) => DimensionType;
+    getSql: (
+        adapterType: SupportedDbtAdapter,
+        timeInterval: TimeFrames,
+        originalSql: string,
+        type: DimensionType,
+    ) => string;
+};
+
+const wrapSqlWithTruncateFn = (
+    adapterType: SupportedDbtAdapter,
+    timeInterval: TimeFrames,
+    originalSql: string,
+    type: DimensionType,
+) => {
+    switch (adapterType) {
+        case SupportedDbtAdapter.BIGQUERY:
+            if (type === DimensionType.TIMESTAMP) {
+                return `DATETIME_TRUNC(${originalSql}, ${timeInterval.toUpperCase()})`;
+            }
+            return `DATE_TRUNC(${originalSql}, ${timeInterval.toUpperCase()})`;
+        case SupportedDbtAdapter.SNOWFLAKE:
+        case SupportedDbtAdapter.REDSHIFT:
+        case SupportedDbtAdapter.POSTGRES:
+        case SupportedDbtAdapter.DATABRICKS:
+            return `DATE_TRUNC('${timeInterval.toUpperCase()}', ${originalSql})`;
+        default:
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const never: never = adapterType;
+            throw new ParseError(`Cannot recognise warehouse ${adapterType}`);
+    }
+};
+
+const wrapSqlWithDatePartFn = (
+    adapterType: SupportedDbtAdapter,
+    datePart: string,
+    originalSql: string,
+) => {
+    switch (adapterType) {
+        case SupportedDbtAdapter.BIGQUERY:
+            return `EXTRACT(${datePart} FROM ${originalSql})`;
+        case SupportedDbtAdapter.SNOWFLAKE:
+        case SupportedDbtAdapter.REDSHIFT:
+        case SupportedDbtAdapter.POSTGRES:
+        case SupportedDbtAdapter.DATABRICKS:
+            return `DATE_PART('${datePart}', ${originalSql})`;
+        default:
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const never: never = adapterType;
+            throw new ParseError(`Cannot recognise warehouse ${adapterType}`);
+    }
+};
+
+const wrapSqlWithFormatExpressionFn = (
+    adapterType: SupportedDbtAdapter,
+    formatExpression: string,
+    originalSql: string,
+    type: DimensionType,
+) => {
+    switch (adapterType) {
+        case SupportedDbtAdapter.BIGQUERY: // https://cloud.google.com/bigquery/docs/reference/standard-sql/format-elements#format_elements_date_time
+            if (type === DimensionType.TIMESTAMP) {
+                return `FORMAT_DATETIME(${formatExpression}, ${originalSql})`;
+            }
+            return `FORMAT_DATE(${formatExpression}, ${originalSql})`;
+        case SupportedDbtAdapter.SNOWFLAKE: // https://docs.snowflake.com/en/sql-reference/functions/to_char.html
+            return `TO_CHAR(${formatExpression}, ${originalSql})`;
+        case SupportedDbtAdapter.REDSHIFT:
+        case SupportedDbtAdapter.POSTGRES: // https://www.postgresql.org/docs/current/functions-formatting.html
+            return `TO_CHAR(${originalSql}, '${formatExpression}')`;
+        case SupportedDbtAdapter.DATABRICKS: // https://docs.databricks.com/spark/latest/spark-sql/language-manual/functions/date_format.html
+            return `DATE_FORMAT(${originalSql}, '${formatExpression}')`;
+        default:
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const never: never = adapterType;
+            throw new ParseError(`Cannot recognise warehouse ${adapterType}`);
+    }
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const timeFrameConfigs: Record<TimeFrames, TimeFrameConfig> = {
     RAW: {
         getDimensionType: (fallback) => fallback,
+        getSql: (_adapterType, _timeInterval, originalSql) => originalSql,
     },
     MILLISECOND: {
         getDimensionType: () => DimensionType.TIMESTAMP,
+        getSql: wrapSqlWithTruncateFn,
     },
     SECOND: {
         getDimensionType: () => DimensionType.TIMESTAMP,
+        getSql: wrapSqlWithTruncateFn,
     },
     MINUTE: {
         getDimensionType: () => DimensionType.TIMESTAMP,
+        getSql: wrapSqlWithTruncateFn,
     },
     HOUR: {
         getDimensionType: () => DimensionType.TIMESTAMP,
+        getSql: wrapSqlWithTruncateFn,
     },
     DAY: {
         getDimensionType: () => DimensionType.DATE,
+        getSql: wrapSqlWithTruncateFn,
     },
     WEEK: {
         getDimensionType: () => DimensionType.DATE,
+        getSql: wrapSqlWithTruncateFn,
     },
     MONTH: {
         getDimensionType: () => DimensionType.DATE,
+        getSql: wrapSqlWithTruncateFn,
     },
     QUARTER: {
         getDimensionType: () => DimensionType.DATE,
+        getSql: wrapSqlWithTruncateFn,
     },
     YEAR: {
         getDimensionType: () => DimensionType.DATE,
+        getSql: wrapSqlWithTruncateFn,
+    },
+    MONTH_NUM: {
+        getDimensionType: () => DimensionType.NUMBER,
+        getSql: (
+            adapterType: SupportedDbtAdapter,
+            timeInterval: TimeFrames,
+            originalSql: string,
+        ) => wrapSqlWithDatePartFn(adapterType, 'MONTH', originalSql),
+    },
+    MONTH_NAME: {
+        getDimensionType: () => DimensionType.STRING,
+        getSql: (
+            adapterType: SupportedDbtAdapter,
+            timeInterval: TimeFrames,
+            originalSql: string,
+            type: DimensionType,
+        ) => {
+            let datePart;
+            switch (adapterType) {
+                case SupportedDbtAdapter.BIGQUERY:
+                    datePart = '%B';
+                    break;
+                case SupportedDbtAdapter.SNOWFLAKE:
+                    datePart = 'MMMM';
+                    break;
+                case SupportedDbtAdapter.REDSHIFT:
+                case SupportedDbtAdapter.POSTGRES:
+                    datePart = 'Month';
+                    break;
+                case SupportedDbtAdapter.DATABRICKS:
+                    datePart = 'MMMM';
+                    break;
+                default:
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const never: never = adapterType;
+                    throw new ParseError(
+                        `Cannot recognise warehouse ${adapterType}`,
+                    );
+            }
+            return wrapSqlWithFormatExpressionFn(
+                adapterType,
+                datePart,
+                originalSql,
+                type,
+            );
+        },
     },
 };
 
@@ -46,6 +180,7 @@ export const getDefaultTimeFrames = (type: DimensionType) =>
               TimeFrames.DAY,
               TimeFrames.WEEK,
               TimeFrames.MONTH,
+              TimeFrames.QUARTER,
               TimeFrames.YEAR,
           ]
         : [TimeFrames.DAY, TimeFrames.WEEK, TimeFrames.MONTH, TimeFrames.YEAR];
