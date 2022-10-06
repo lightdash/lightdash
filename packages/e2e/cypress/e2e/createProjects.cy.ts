4deb5a6b64c8f6260228843e905ded9bdfe59c5c
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-10-06 22:55:53 +0400

    test: fix e2e in createProjects

--- packages/e2e/cypress/e2e/createProjects.cy.ts
+++ packages/e2e/cypress/e2e/createProjects.cy.ts
@@ -18,6 +18,7 @@ const warehouseConfig = {
         token: Cypress.env('DATABRICKS_TOKEN'),
         httpPath: Cypress.env('DATABRICKS_PATH'),
         schema: 'jaffle',
+        database: 'SPARK',
     },
     snowflake: {
         account: Cypress.env('SNOWFLAKE_ACCOUNT'),
@@ -30,7 +31,9 @@ const warehouseConfig = {
     },
 };
 
-const configurePostgresWarehouse = (config) => {
+const configurePostgresWarehouse = (
+    config: typeof warehouseConfig['postgresSQL'],
+) => {
     cy.get('[name="warehouse.host"]').type(config.host, { log: false });
     cy.get('[name="warehouse.user"]').type(config.user, { log: false });
     cy.get('[name="warehouse.password"]').type(config.password, { log: false });
@@ -47,7 +50,9 @@ const configurePostgresWarehouse = (config) => {
     cy.get('[name="warehouse.schema"]').type(config.schema);
 };
 
-const configureBigqueryWarehouse = (config) => {
+const configureBigqueryWarehouse = (
+    config: typeof warehouseConfig['bigQuery'],
+) => {
     cy.get('[name="warehouse.project"]').type(config.project, { log: false });
     cy.get('[name="warehouse.location"]').type(config.location, { log: false });
     cy.get('[type="file"]').attachFile(warehouseConfig.bigQuery.keyFile);
@@ -58,7 +63,9 @@ const configureBigqueryWarehouse = (config) => {
     cy.get('[name="warehouse.dataset"]').type(config.dataset);
 };
 
-const configureDatabricksWarehouse = (config) => {
+const configureDatabricksWarehouse = (
+    config: typeof warehouseConfig['databricks'],
+) => {
     cy.get('[name="warehouse.serverHostName"]').type(config.host, {
         log: false,
     });
@@ -66,6 +73,7 @@ const configureDatabricksWarehouse = (config) => {
     cy.get('[name="warehouse.personalAccessToken"]').type(config.token, {
         log: false,
     });
+    cy.get('[name="warehouse.dbname"]').type(config.database, { log: false });
 
     // DBT
     cy.get('[name="dbt.type"]').select('dbt local server');
@@ -73,7 +81,9 @@ const configureDatabricksWarehouse = (config) => {
     cy.get('[name="warehouse.database"]').type(config.schema);
 };
 
-const configureSnowflakeWarehouse = (config) => {
+const configureSnowflakeWarehouse = (
+    config: typeof warehouseConfig['snowflake'],
+) => {
     cy.get('[name="warehouse.account"]').type(config.account, { log: false });
     cy.get('[name="warehouse.user"]').type(config.user, { log: false });
     cy.get('[name="warehouse.password"]').type(config.password, { log: false });
