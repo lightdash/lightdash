7103e8e824a07ff68f7d028f5219c0ce7d690988
Jose Rego <jose@lightdash.com>
2022-10-10 17:46:11 +0100

    chore: chunk manually to prevent out of memory error

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -19,6 +19,13 @@ export default defineConfig({
         commonjsOptions: {
             exclude: ['lightdash/common'],
         },
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
     },
     resolve: {
         alias: {
