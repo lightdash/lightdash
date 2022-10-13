5159f0bdd94e2ea11276c9e88d07f730ad8ba508
Javier Rengel <rephus@gmail.com>
2022-10-13 10:54:57 +0200

    update copy

--- packages/frontend/src/components/ProjectConnection/DbtForms/DbtNoneForm.tsx
+++ packages/frontend/src/components/ProjectConnection/DbtForms/DbtNoneForm.tsx
@@ -4,8 +4,9 @@ import React, { FC } from 'react';
 const DbtNoneForm: FC = () => (
     <>
         <Callout intent="warning" style={{ marginBottom: 20 }}>
-            This project was created from CLI, refresh dbt by doing `lightdash
-            deploy`
+            This project was created from the CLI. If you want to refresh dbt,
+            you need to run <b>lightdash deploy</b> from your command line or
+            update your dbt connection type here.
         </Callout>
     </>
 );
