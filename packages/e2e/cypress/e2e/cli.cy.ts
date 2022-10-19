20c481f2f8bb1edcfa1f27c692a911c53eb8cb35
Rephus <rephus@gmail.com>
2022-10-19 17:06:41 +0200

    Fix preview name

--- packages/e2e/cypress/e2e/cli.cy.ts
+++ packages/e2e/cypress/e2e/cli.cy.ts
@@ -1,40 +1,20 @@
 const lightdashUrl = Cypress.config('baseUrl');
 const projectDir = `../../examples/full-jaffle-shop-demo/dbt`;
 const profilesDir = `../../examples/full-jaffle-shop-demo/profiles`;
-const rootDir = `../../`;
 const cliCommand = `lightdash`;
-const { exec } = require('child_process');
 
 describe('CLI', () => {
+    const previewName = `e2e preview ${new Date().getTime()}`;
+
     it('Should test lightdash command help', () => {
         cy.exec(`${cliCommand} help`)
             .its('stdout')
             .should('contain', 'Developer tools for dbt and Lightdash.');
     });
-
-    it('test pwd', () => {
-        cy.exec(`pwd`).its('stdout').should('contain', 'throw error');
-    });
-    it('test ls', () => {
-        cy.exec(`ls`).its('stdout').should('contain', 'throw error');
-    });
-    it('test ls projectdir', () => {
-        cy.exec(`ls ${projectDir}`)
+    it('Should get version', () => {
+        cy.exec(`${cliCommand} --version`)
             .its('stdout')
-            .should('contain', 'throw error');
-    });
-
-    it('test ls /', () => {
-        cy.exec(`ls /`).its('stdout').should('contain', 'throw error');
-    });
-    it('test ls ../../', () => {
-        cy.exec(`ls ${rootDir}`).its('stdout').should('contain', 'throw error');
-    });
-
-    it('test ls /__w/lightdash/lightdash', () => {
-        cy.exec(`ls /__w/lightdash/lightdash`)
-            .its('stdout')
-            .should('contain', 'throw error');
+            .should('contain', '0.');
     });
 
     it('Should run DBT first', () => {
@@ -55,26 +35,6 @@ describe('CLI', () => {
             .should('contain', 'Completed successfully');
     });
 
-    it('Should run DBT exec', () => {
-        exec(
-            `dbt run --project-dir ${projectDir} --profiles-dir ${profilesDir}`,
-            {
-                env: {
-                    CI: true,
-                    PGHOST: Cypress.env('PGHOST') || 'localhost',
-                    PGPORT: 5432,
-                    PGUSER: 'postgres',
-                    PGPASSWORD: Cypress.env('PGPASSWORD') || 'password',
-                    PGDATABASE: 'postgres',
-                },
-            },
-            (error, stdout, stderr) => {
-                cy.log('err', error);
-                cy.log('stdout', stdout);
-                cy.log('stderr', stderr);
-            },
-        );
-    });
     it('Should lightdash generate', () => {
         cy.exec(
             `${cliCommand} generate  -y --project-dir ${projectDir} --profiles-dir ${profilesDir}`,
@@ -92,27 +52,6 @@ describe('CLI', () => {
         )
             .its('stderr')
             .should('contain', 'Done 🕶');
-    });
-
-    it('Should lightdash generate with exec', () => {
-        exec(
-            `lightdash generate  -y --project-dir ${projectDir} --profiles-dir ${profilesDir}`,
-            {
-                env: {
-                    CI: true,
-                    PGHOST: Cypress.env('PGHOST') || 'localhost',
-                    PGPORT: 5432,
-                    PGUSER: 'postgres',
-                    PGPASSWORD: Cypress.env('PGPASSWORD') || 'password',
-                    PGDATABASE: 'postgres',
-                },
-            },
-            (error, stdout, stderr) => {
-                cy.log('err', error);
-                cy.log('stdout', stdout);
-                cy.log('stderr', stderr);
-            },
-        );
     });
 
     it('Should lightdash compile', () => {
@@ -174,7 +113,6 @@ describe('CLI', () => {
     it('Should start-preview', () => {
         cy.login();
         cy.getApiToken().then((apiToken) => {
-            const previewName = 'e2e preview';
             cy.exec(
                 `${cliCommand} start-preview --project-dir ${projectDir} --profiles-dir ${profilesDir} --name "${previewName}"`,
                 {
@@ -198,7 +136,6 @@ describe('CLI', () => {
     it('Should stop-preview', () => {
         cy.login();
         cy.getApiToken().then((apiToken) => {
-            const previewName = 'e2e preview';
             cy.exec(`${cliCommand} stop-preview --name "${previewName}"`, {
                 failOnNonZeroExit: false,
                 env: {
