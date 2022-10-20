6169c5ff31f7e168f7403f551c6afa02db84877b
Rephus <rephus@gmail.com>
2022-10-20 15:55:00 +0200

    feat: cli delete old columns on generate

--- packages/cli/src/dbt/models.ts
+++ packages/cli/src/dbt/models.ts
@@ -212,9 +212,36 @@ export const findAndUpdateModelYaml = async ({
         const newColumns = generatedModel.columns.filter(
             (c) => !existingColumnNames.includes(c.name),
         );
+        const deletedColumnNames = existingColumnNames.filter(
+            (c) => !generatedModel.columns.map(gc=> gc.name).includes(c),
+        );
+        let updatedColumns = [...existingColumnsUpdated, ...newColumns]
+        if (deletedColumnNames.length > 0) {
+            if (process.env.CI !== 'true') {
+                {
+                    spinner.stop();
+                const answers = await inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'isConfirm',
+                        message: `
+These columns in your .yml file no longer exist in your warehouse:
+${deletedColumnNames.map(name => `- ${name} \n`).join('')}
+Would you like to remove them from your .yml file? `
+                           ,
+                    },
+                ]);
+                spinner.start();
+
+                if (answers.isConfirm) {
+                     updatedColumns = updatedColumns.filter(column => !deletedColumnNames.includes(column.name))
+                } 
+            }
+            }
+        }
         const updatedModel = {
             ...existingModel,
-            columns: [...existingColumnsUpdated, ...newColumns],
+            columns: updatedColumns,
         };
         const updatedYml: YamlSchema = {
             ...match.doc,
