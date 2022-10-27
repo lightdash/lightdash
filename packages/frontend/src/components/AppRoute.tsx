3bfd7e3c9c5a902a9b8d218adaf2ef408d8f98da
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-10-27 12:17:42 +0400

    fix: check for permissions and redirect to no-project-access

--- packages/frontend/src/components/AppRoute.tsx
+++ packages/frontend/src/components/AppRoute.tsx
@@ -1,45 +1,65 @@
 import { NonIdealState } from '@blueprintjs/core';
-import React, { ComponentProps, FC } from 'react';
+import { subject } from '@casl/ability';
+import { ComponentProps, FC } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useOrganisation } from '../hooks/organisation/useOrganisation';
 import { useApp } from '../providers/AppProvider';
 import PageSpinner from './PageSpinner';
 
 const AppRoute: FC<ComponentProps<typeof Route>> = ({ children, ...rest }) => {
-    const { health } = useApp();
-    const orgRequest = useOrganisation();
+    const {
+        health: {
+            data: health,
+            isLoading: isLoadingHealth,
+            error: healthError,
+        },
+        user: { data: user, isLoading: isLoadingUser, error: userError },
+    } = useApp();
+    const {
+        data: organization,
+        error: organizationError,
+        isLoading: isLoadingOrganization,
+    } = useOrganisation();
+
+    if (isLoadingHealth || isLoadingUser || isLoadingOrganization) {
+        return <PageSpinner />;
+    }
+
+    if (healthError || userError || organizationError) {
+        return (
+            <div style={{ marginTop: '20px' }}>
+                <NonIdealState
+                    title="Unexpected error"
+                    description={
+                        healthError?.error.message ||
+                        userError?.error.message ||
+                        organizationError?.error.message
+                    }
+                />
+            </div>
+        );
+    }
+
+    if (!health || !user || !organization) return null;
+
+    const canUserCreateProject = user.ability.can(
+        'create',
+        subject('Project', {
+            organizationUuid: organization.organizationUuid,
+        }),
+    );
 
     return (
         <Route
             {...rest}
-            render={({ location }) => {
-                if (health.isLoading || orgRequest.isLoading) {
-                    return <PageSpinner />;
+            render={() => {
+                if (organization.needsProject && canUserCreateProject) {
+                    return <Redirect to="/createProject" />;
+                } else if (organization.needsProject && !canUserCreateProject) {
+                    return <Redirect to="/no-project-access" />;
+                } else {
+                    return children;
                 }
-
-                if (orgRequest.error || health.error) {
-                    return (
-                        <div style={{ marginTop: '20px' }}>
-                            <NonIdealState
-                                title="Unexpected error"
-                                description={orgRequest.error?.error.message}
-                            />
-                        </div>
-                    );
-                }
-
-                if (orgRequest.data?.needsProject) {
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: '/createProject',
-                                state: { from: location },
-                            }}
-                        />
-                    );
-                }
-
-                return children;
             }}
         />
     );
