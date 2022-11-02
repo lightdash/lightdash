b5710b471ee55b356f76e57e47485f8b20426220
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-11-02 14:15:54 +0400

    chore: update viz card body, remove wrappers

--- packages/frontend/src/components/Explorer/VisualizationCard/VisualizationCardBody.tsx
+++ packages/frontend/src/components/Explorer/VisualizationCard/VisualizationCardBody.tsx
@@ -1,5 +1,6 @@
-import { Collapse } from '@blueprintjs/core';
-import { FC, memo, useMemo } from 'react';
+import { Collapse, CollapseProps } from '@blueprintjs/core';
+import { FC, memo } from 'react';
+import styled from 'styled-components';
 import { EChartSeries } from '../../../hooks/echarts/useEcharts';
 import {
     ExplorerSection,
@@ -7,8 +8,8 @@ import {
 } from '../../../providers/ExplorerProvider';
 import LightdashVisualization from '../../LightdashVisualization';
 import { EchartSeriesClickEvent } from '../../SimpleChart';
+import { StyledCollapse } from '../Explorer.styles';
 import { SeriesContextMenu } from './SeriesContextMenu';
-import { VisualizationCardContentWrapper } from './VisualizationCard.styles';
 
 export type EchartsClickEvent = {
     event: EchartSeriesClickEvent;
@@ -22,23 +23,24 @@ const VisualizationCardBody: FC<{
     const expandedSections = useExplorerContext(
         (context) => context.state.expandedSections,
     );
-    const vizIsOpen = useMemo(
-        () => expandedSections.includes(ExplorerSection.VISUALIZATION),
-        [expandedSections],
-    );
+
+    const isOpen = expandedSections.includes(ExplorerSection.VISUALIZATION);
+    const isOnlyExpandedCard = isOpen && expandedSections.length === 1;
 
     return (
-        <Collapse className="explorer-chart" isOpen={vizIsOpen}>
-            <VisualizationCardContentWrapper className="cohere-block">
-                <LightdashVisualization />
+        <StyledCollapse
+            data-testid="visualization-card-body"
+            isOpen={isOpen}
+            $flexGrow={isOnlyExpandedCard ? 1 : undefined}
+        >
+            <LightdashVisualization className="cohere-block" />
 
-                <SeriesContextMenu
-                    echartSeriesClickEvent={echartsClickEvent?.event}
-                    dimensions={echartsClickEvent?.dimensions}
-                    series={echartsClickEvent?.series}
-                />
-            </VisualizationCardContentWrapper>
-        </Collapse>
+            <SeriesContextMenu
+                echartSeriesClickEvent={echartsClickEvent?.event}
+                dimensions={echartsClickEvent?.dimensions}
+                series={echartsClickEvent?.series}
+            />
+        </StyledCollapse>
     );
 });
 
