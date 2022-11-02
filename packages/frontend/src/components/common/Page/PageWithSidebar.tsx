06fca1ebe3d6884330f5a11277ccf131c9ababb4
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-11-03 00:07:45 +0400

    style: page with sidebar should stretch items by default

--- packages/frontend/src/components/common/Page/PageWithSidebar.tsx
+++ packages/frontend/src/components/common/Page/PageWithSidebar.tsx
@@ -1,12 +1,12 @@
 import { Colors } from '@blueprintjs/core';
 import styled from 'styled-components';
 
-const PageWithSidebar = styled('div')`
+const PageWithSidebar = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: stretch;
-    align-items: flex-start;
+    align-items: stretch;
     background: ${Colors.LIGHT_GRAY4};
 `;
 
