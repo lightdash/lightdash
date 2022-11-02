4a66706c44782ee5bb07b607b85d579a7c7f198a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-11-02 14:12:43 +0400

    style: extract collapse and card with flex grow in Explore.styles

--- packages/frontend/src/components/Explorer/Explorer.styles.ts
+++ packages/frontend/src/components/Explorer/Explorer.styles.ts
@@ -1,7 +1,31 @@
+import { Card, Collapse } from '@blueprintjs/core';
 import styled from 'styled-components';
 
 export const ExplorerWrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
+    flex: 1;
+`;
+
+interface StyledCollapseProps {
+    isOpen?: boolean;
+    $flexGrow?: number;
+}
+
+export const StyledCollapse = styled(Collapse)<StyledCollapseProps>`
+    flex-grow: ${({ $flexGrow }) => $flexGrow || 'unset'};
+    min-height: ${({ isOpen }) => (isOpen ? '300px' : 'unset')};
+
+    .bp4-collapse-body {
+        height: 100%;
+    }
+`;
+
+export const ExploreCard = styled(Card)<{ $flexGrow?: number }>`
+    padding: 5px;
+    overflow-y: hidden;
+    display: flex;
+    flex-direction: column;
+    flex-grow: ${({ $flexGrow }) => $flexGrow || 'unset'};
 `;
