95e9a4d0f24c18f017d6332c9229b08fe82df174
Javier Rengel Jiménez <rephus@gmail.com>
2022-11-07 16:07:33 +0100

    feat: remove magic metrics (#3708)
    * feat: remove magic metrics
    
    * remove extra method
    
    * dbt master

--- packages/frontend/src/components/Explorer/ExplorePanel/index.tsx
+++ packages/frontend/src/components/Explorer/ExplorePanel/index.tsx
@@ -1,36 +1,12 @@
 import { MenuDivider } from '@blueprintjs/core';
 import { MenuItem2, Tooltip2 } from '@blueprintjs/popover2';
-import {
-    AdditionalMetric,
-    CompiledTable,
-    extractEntityNameFromIdColumn,
-    MetricType,
-} from '@lightdash/common';
-import React, { FC, memo, useEffect } from 'react';
+
+import React, { FC, memo } from 'react';
 import { useExplore } from '../../../hooks/useExplore';
 import { useExplorerContext } from '../../../providers/ExplorerProvider';
 import { StyledBreadcrumb } from '../ExploreSideBar/ExploreSideBar.styles';
 import ExploreTree from '../ExploreTree';
 import { LoadingStateWrapper, TableDivider } from './ExplorePanel.styles';
-
-const getTableMagicMetrics = (
-    table: CompiledTable,
-): Record<string, AdditionalMetric> =>
-    Object.values(table.dimensions).reduce((previous, dimension) => {
-        const entityName = extractEntityNameFromIdColumn(dimension.name);
-        if (entityName === null) {
-            return previous;
-        }
-        const magicMetric: AdditionalMetric = {
-            name: `${dimension.name}_count_distinct`,
-            label: `Count distinct of ${dimension.label}`,
-            description: `Count distinct of ${dimension.label} on the table ${dimension.tableLabel}. Lightdash has created this metric automatically.`,
-            table: dimension.table,
-            sql: dimension.sql,
-            type: MetricType.COUNT_DISTINCT,
-        };
-        return { ...previous, [magicMetric.name]: magicMetric };
-    }, {});
 
 const SideBarLoadingState = () => (
     <LoadingStateWrapper large>
@@ -61,31 +37,8 @@ const ExplorePanel: FC<ExplorePanelProps> = memo(({ onBack }) => {
     const toggleActiveField = useExplorerContext(
         (context) => context.actions.toggleActiveField,
     );
-    const setMagicMetrics = useExplorerContext(
-        (context) => context.actions.setMagicMetrics,
-    );
 
     const { data, status } = useExplore(activeTableName);
-
-    useEffect(() => {
-        if (data) {
-            setMagicMetrics(
-                Object.values(data.tables).reduce<AdditionalMetric[]>(
-                    (sum, table) => {
-                        const hasMetrics =
-                            Object.values(table.metrics).length > 0;
-                        return hasMetrics
-                            ? [...sum]
-                            : [
-                                  ...sum,
-                                  ...Object.values(getTableMagicMetrics(table)),
-                              ];
-                    },
-                    [],
-                ),
-            );
-        }
-    }, [data, setMagicMetrics]);
 
     if (status === 'loading') {
         return <SideBarLoadingState />;
