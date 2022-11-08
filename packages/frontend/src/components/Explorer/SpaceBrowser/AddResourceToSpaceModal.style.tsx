4582cb8bc12ce370e379763280bde40539972824
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-11-08 12:29:38 +0400

    fix: add proper disabled styles for add to space item (#3714)

--- packages/frontend/src/components/Explorer/SpaceBrowser/AddResourceToSpaceModal.style.tsx
+++ packages/frontend/src/components/Explorer/SpaceBrowser/AddResourceToSpaceModal.style.tsx
@@ -1,8 +1,12 @@
 import { Colors } from '@blueprintjs/core';
 import styled from 'styled-components';
 
-export const SpaceLabel = styled.div`
-    color: ${Colors.GRAY2};
+interface SpaceLabelProps {
+    disabled?: boolean;
+}
+
+export const SpaceLabel = styled.div<SpaceLabelProps>`
+    color: ${({ disabled }) => (disabled ? Colors.GRAY4 : Colors.GRAY2)};
 
     font-size: 12px;
     line-height: 18px;
@@ -10,5 +14,10 @@ export const SpaceLabel = styled.div`
     .bp4-icon {
         margin-right: 6px;
         margin-bottom: 2px;
+
+        svg {
+            ${({ disabled }) =>
+                disabled ? `fill: ${Colors.LIGHT_GRAY1};` : ''}
+        }
     }
 `;
