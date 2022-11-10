5703d15d4e6594626b70c276e8a01742fefde85e
Rephus <rephus@gmail.com>
2022-11-10 12:17:58 +0100

    ensure unique name

--- packages/frontend/src/components/TableCalculationModels/TableCalculationModal.tsx
+++ packages/frontend/src/components/TableCalculationModels/TableCalculationModal.tsx
@@ -71,6 +71,25 @@ const TableCalculationModal: FC<Props> = ({
         },
     });
 
+    const getUniqueTableCalculationName = (name: string): string => {
+        const snakeName = snakeCaseName(name);
+        const suffixes = Array.from(Array(100).keys());
+        const getCalcName = (suffix: number) =>
+            suffix === 0 ? snakeName : `${snakeName}_${suffix}`;
+
+        const validSuffix = suffixes.find(
+            (suffix) =>
+                tableCalculations.findIndex(
+                    ({ name: tableCalcName }) =>
+                        tableCalcName === getCalcName(suffix),
+                ) === -1,
+        );
+        if (validSuffix === undefined) {
+            throw new Error(`Table calculation ID "${name}" already exists.`);
+        }
+        return getCalcName(validSuffix);
+    };
+
     return (
         <TableCalculationDialog
             isOpen={isOpen}
@@ -99,7 +118,7 @@ const TableCalculationModal: FC<Props> = ({
                     const { name, sql } = data;
                     try {
                         onSave({
-                            name: snakeCaseName(name),
+                            name: getUniqueTableCalculationName(name),
                             displayName: name,
                             sql,
                         });
