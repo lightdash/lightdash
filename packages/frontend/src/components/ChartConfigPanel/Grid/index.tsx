a847e89a624d5c833f7f347d2a12e7545636e25b
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-11-15 14:19:07 +0400

    chore: update inputs for margin config (#3776)
    * chore: update helptext for margin config
    
    * feat: update chart configuration inputs
    
    * fix: label text
    
    * fix: placeholder units and cleanup/improve code
    
    * fix: remove useless state
    
    * fix: import

--- packages/frontend/src/components/ChartConfigPanel/Grid/index.tsx
+++ packages/frontend/src/components/ChartConfigPanel/Grid/index.tsx
@@ -1,55 +1,130 @@
+import { Button, FormGroup, InputGroup } from '@blueprintjs/core';
 import { EchartsGrid } from '@lightdash/common';
-import React, { FC } from 'react';
-import { useForm } from 'react-hook-form';
+import startCase from 'lodash/startCase';
+import { FC, useMemo } from 'react';
 import { useVisualizationContext } from '../../LightdashVisualization/VisualizationProvider';
-import Form from '../../ReactHookForm/Form';
-import Input from '../../ReactHookForm/Input';
-import { SectionRow, SectionTitle } from './Grid.styles';
+import { SectionRow } from './Grid.styles';
 
-export const defaultGrid: EchartsGrid = {
+export const defaultGrid = {
     containLabel: true,
     left: '5%', // small padding
     right: '5%', // small padding
-    top: '70', // pixels from top (makes room for legend)
-    bottom: '30', // pixels from bottom (makes room for x-axis)
+    top: '70px', // pixels from top (makes room for legend)
+    bottom: '30px', // pixels from bottom (makes room for x-axis)
+};
+
+const positions = ['top', 'bottom', 'left', 'right'] as const;
+
+enum Units {
+    Pixels = 'px',
+    Percentage = '%',
+}
+
+const getNextUnit = (current?: Units): Units | undefined => {
+    if (!current) return;
+
+    const units = Object.values(Units);
+    const currentIndex = units.indexOf(current);
+    return units.concat(units[0])[currentIndex + 1];
+};
+
+const getValueAndUnit = (valueWithUnit?: string): [string?, Units?] => {
+    if (!valueWithUnit || valueWithUnit === '') return [];
+
+    const unit =
+        Object.values(Units).find((u) => valueWithUnit.endsWith(u)) ||
+        Units.Pixels;
+
+    const value = valueWithUnit.replace(unit, '');
+    return [value, unit];
 };
 
 const GridPanel: FC = () => {
     const {
         cartesianConfig: { dirtyEchartsConfig, setGrid },
     } = useVisualizationContext();
-    const methods = useForm<EchartsGrid>({
-        mode: 'onBlur',
-        defaultValues: { ...defaultGrid, ...dirtyEchartsConfig?.grid },
-    });
+
+    const config = useMemo<EchartsGrid>(
+        () => ({
+            ...defaultGrid,
+            ...dirtyEchartsConfig?.grid,
+        }),
+        [dirtyEchartsConfig?.grid],
+    );
+
+    const handleUpdate = (
+        position: typeof positions[number],
+        value: string,
+        unit: Units = Units.Pixels,
+    ) => {
+        const newValue = value && value !== '' ? `${value}${unit}` : undefined;
+
+        const newState = { ...config, [position]: newValue };
+        setGrid(newState);
+        return newState;
+    };
+
+    const handleUpdateUnit = (
+        key: typeof positions[number],
+        nextUnit: Units = Units.Pixels,
+    ) => {
+        const originalValue = config[key];
+        const [value] = getValueAndUnit(originalValue);
+
+        if (!value || value === '') return;
+
+        handleUpdate(key, value, nextUnit);
+    };
 
     return (
-        <Form
-            name="grid"
-            methods={methods}
-            onSubmit={() => undefined}
-            onBlur={methods.handleSubmit(setGrid)}
-        >
-            <SectionTitle>Margins</SectionTitle>
-            <SectionRow>
-                <Input name="top" label="Top" placeholder={defaultGrid.top} />
-                <Input
-                    name="bottom"
-                    label="Bottom"
-                    placeholder={defaultGrid.bottom}
-                />
-                <Input
-                    name="left"
-                    label="Left"
-                    placeholder={defaultGrid.left}
-                />
-                <Input
-                    name="right"
-                    label="Right"
-                    placeholder={defaultGrid.right}
-                />
-            </SectionRow>
-        </Form>
+        <SectionRow>
+            {positions.map((position) => {
+                const [value, unit] = getValueAndUnit(config[position]);
+                const [placeholder, placeholderUnit] = getValueAndUnit(
+                    defaultGrid[position],
+                );
+
+                const nextUnit = getNextUnit(unit);
+
+                return (
+                    <FormGroup
+                        key={position}
+                        label={startCase(position)}
+                        labelFor={`${position}-input`}
+                    >
+                        <InputGroup
+                            type="number"
+                            id={`${position}-input`}
+                            name={position}
+                            placeholder={placeholder}
+                            value={value || ''}
+                            onChange={(e) =>
+                                handleUpdate(
+                                    position,
+                                    e.target.value,
+                                    value ? unit : placeholderUnit,
+                                )
+                            }
+                            rightElement={
+                                <Button
+                                    minimal
+                                    small
+                                    disabled={!value}
+                                    onClick={() =>
+                                        handleUpdateUnit(
+                                            position,
+                                            value ? nextUnit : placeholderUnit,
+                                        )
+                                    }
+                                >
+                                    {unit || placeholderUnit}
+                                </Button>
+                            }
+                        />
+                    </FormGroup>
+                );
+            })}
+        </SectionRow>
     );
 };
 
