e010a4c4c85c052cae2c4d94de5d5496fec45f6d
Rephus <rephus@gmail.com>
2022-11-21 17:14:29 +0100

    site_url

--- packages/backend/src/clients/Slack/Slackbot.ts
+++ packages/backend/src/clients/Slack/Slackbot.ts
@@ -5,50 +5,41 @@ import {
     getInstallation,
 } from './SlackStorage';
 
-const { FileInstallationStore } = require('@slack/oauth');
+// TODO https://github.com/slackapi/bolt-js/issues/904 fix slack bot compatibility with typescript
 const { App, ExpressReceiver } = require('@slack/bolt');
-// import {App} from '@slack/bolt' //TODO fix import
 
-// Docs: https://slack.dev/bolt-js/concepts#authenticating-oauth
-
-const receiverOptions = {
+const slackOptions = {
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     clientId: process.env.SLACK_CLIENT_ID,
-    // appToken: process.env.SLACK_APP_TOKEN,
-
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     stateSecret: process.env.SLACK_STATE_SECRET,
     router: apiV1Router,
-    // port: 4000,
-    // port: 3000, // use same express port
-    scopes: ['links:read', 'links:write', 'channels:read'],
+    scopes: ['links:read', 'links:write'],
     installationStore: {
         storeInstallation: createInstallation,
         fetchInstallation: getInstallation,
         deleteInstallation,
     },
-    // installationStore: new FileInstallationStore(), // TODO replace with DB storage
-    redirectUri: 'http://localhost:3000/api/v1/slack/oauth_redirect',
+    redirectUri: `${process.env.SITE_URL}/api/v1/slack/oauth_redirect`,
     installerOptions: {
         directInstall: true,
         redirectUriPath: '/slack/oauth_redirect',
     },
 };
-// export const app = new App(receiverOptions );
 
-export const receiver = new ExpressReceiver(receiverOptions);
+export const receiver = new ExpressReceiver(slackOptions);
 
 apiV1Router.get('/slack/install/:organizationUuid', async (req, res, next) => {
     try {
         const options = {
-            redirectUri: receiverOptions.redirectUri,
-            scopes: receiverOptions.scopes,
+            redirectUri: slackOptions.redirectUri,
+            scopes: slackOptions.scopes,
             metadata: { organizationUuid: req.params.organizationUuid },
         };
         await receiver.installer.handleInstallPath(
             req,
             res,
-            receiverOptions.installerOptions,
+            slackOptions.installerOptions,
             options,
         );
     } catch (error) {
@@ -57,9 +48,8 @@ apiV1Router.get('/slack/install/:organizationUuid', async (req, res, next) => {
 });
 
 const app = new App({
-    // receiver,
-    ...receiverOptions,
-    port: 4000,
+    ...slackOptions,
+    port: process.env.SLACK_PORT || 4000,
     socketMode: true,
     appToken: process.env.SLACK_APP_TOKEN,
 });
@@ -70,23 +60,11 @@ const unfurl = (event: any, client: any) => {
         const imgUrl =
             'https://user-images.githubusercontent.com/1983672/203070424-c6051437-f92d-448b-827e-0c4aec58867a.png';
         const dashboardName = 'Public dashboard';
-        const dashboardUrl =
-            'http://localhost:3000/projects/3675b69e-8324-4110-bdca-059031aa8da3/dashboards/2844e926-a868-4dfe-b41c-6d7cc74e1b24/edit';
+        const dashboardUrl = `${process.env.SITE_URL}/projects/3675b69e-8324-4110-bdca-059031aa8da3/dashboards/2844e926-a868-4dfe-b41c-6d7cc74e1b24/edit`;
         return {
             [url]: {
                 blocks: [
                     {
-                        /* type: 'section',
-                        text: {
-                            type: 'mrkdwn',
-                            text: `public dashboard`,
-                        },
-                        accessory: {
-                            type: 'image',
-                            image_url: imgUrl,
-                            alt_text: 'Image unfurled',
-                        }, */
-
                         type: 'image',
                         title: {
                             type: 'plain_text',
@@ -116,28 +94,18 @@ const unfurl = (event: any, client: any) => {
             },
         };
     }, {});
-    console.info('unfurls', unfurls);
     client.chat
         .unfurl({ ts: event.message_ts, channel: event.channel, unfurls })
         .catch(console.error);
 };
 
 app.event('link_shared', (message: any) => {
-    const { event, client } = message; // message.links ? message : message.event; // depending on the api, the message is different
-    // console.debug('link_shared event', event);
-    // console.debug('client event', client);
-
+    const { event, client } = message;
     unfurl(event, client);
-});
-
-app.message(':wave:', async (event: any) => {
-    const { message, say } = event;
-    await say(`Hello, <@${message.user}>`);
 });
 
 export const startSlackBot = async () => {
     await receiver.start();
-
     await app.start();
 
     console.debug('⚡️ Bolt app is running!');
