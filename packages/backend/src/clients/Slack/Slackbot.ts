fa27dccfec4978efe607ebdd043c5eee411f923e
Rephus <rephus@gmail.com>
2022-11-25 15:01:17 +0100

    screenshot

--- packages/backend/src/clients/Slack/Slackbot.ts
+++ packages/backend/src/clients/Slack/Slackbot.ts
@@ -5,6 +5,8 @@ import {
     getInstallation,
 } from './SlackStorage';
 
+const puppeteer = require('puppeteer');
+
 // TODO https://github.com/slackapi/bolt-js/issues/904 fix slack bot compatibility with typescript
 const { App, ExpressReceiver, LogLevel } = require('@slack/bolt');
 
@@ -52,22 +54,89 @@ apiV1Router.get('/slack/install/:organizationUuid', async (req, res, next) => {
     }
 });
 
+const fetchDashboardScreenshot = async (
+    url: string,
+): Promise<Buffer | string> => {
+    let browser;
+
+    try {
+        const browserWSEndpoint = `ws://${process.env.HEADLESS_BROWSER_HOST}:${process.env.HEADLESS_BROWSER_PORT}`;
+        console.debug(`Headless chrome endpoint: ${browserWSEndpoint}`);
+        browser = await puppeteer.connect({
+            browserWSEndpoint,
+        });
+
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1024,
+            height: 768, // hardcoded
+        });
+        // await page.setExtraHTTPHeaders({ cookie: req.headers.cookie || '' }); // copy cookie
+
+        const blockedUrls = [
+            'headwayapp.co',
+            'rudderlabs.com',
+            'analytics.lightdash.com',
+            'cohere.so',
+            'intercom.io',
+        ];
+        await page.setRequestInterception(true);
+        page.on('request', (request: any) => {
+            const requestUrl = request.url();
+            if (blockedUrls.includes(requestUrl)) {
+                request.abort();
+                return;
+            }
+
+            request.continue();
+        });
+        const hostname =
+            process.env.NODE_ENV === 'development'
+                ? 'lightdash-dev'
+                : 'lightdash';
+
+        const testUrl = `http://${hostname}:${process.env.PORT || 3000}/login`;
+        console.debug(`Fetching headless chrome URL: ${testUrl}`);
+        await page.goto(testUrl, {
+            timeout: 100000,
+            waitUntil: 'networkidle0',
+        });
+
+        const imageBuffer: Promise<Buffer | string> = await page.screenshot({
+            path: 'screenshot.png',
+            clip: { x: 0, y: 0, width: 1024, height: 768 },
+        });
+
+        return await imageBuffer;
+    } catch (e) {
+        console.error(`Unable to fetch screenshots from headless chromeo ${e}`);
+        return e;
+    } finally {
+        if (browser) await browser.close();
+    }
+};
+
 const unfurl = (event: any, client: any) => {
-    const unfurls = event.links.reduce((acc: any, l: any) => {
+    const unfurls = event.links.reduce(async (acc: any, l: any) => {
         const { url } = l;
 
-        return {
-            [url]: {
-                blocks: [
-                    {
-                        type: 'header',
-                        text: {
-                            type: 'plain_text',
-                            text: 'Lightdash URL unfurls are not yet available',
+        try {
+            const screenshot = await fetchDashboardScreenshot(url);
+            // client.files.upload()
+            return {
+                ...acc,
+                [url]: {
+                    blocks: [
+                        {
+                            type: 'header',
+                            text: {
+                                type: 'plain_text',
+                                text: 'Lightdash URL unfurls are not yet available',
+                            },
                         },
-                    },
 
-                    /* {
+                        /* {
                         type: 'image',
                         title: {
                             type: 'plain_text',
@@ -93,10 +162,14 @@ const unfurl = (event: any, client: any) => {
                             value: 'view_alternate_1',
                         },
                     }, */
-                ],
-            },
-        };
+                    ],
+                },
+            };
+        } catch (e) {
+            return acc;
+        }
     }, {});
+
     client.chat
         .unfurl({ ts: event.message_ts, channel: event.channel, unfurls })
         .catch(console.error);
