89114c3d80194716a677b3099e849dcf8dfa53be
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-12-05 14:04:20 +0400

    style: improve label

--- packages/frontend/src/components/DashboardFilter/FilterSearch/FilterSearch.styles.tsx
+++ packages/frontend/src/components/DashboardFilter/FilterSearch/FilterSearch.styles.tsx
@@ -8,3 +8,7 @@ export const FilterModalContainer = styled.div<FilterModalContainerProps>`
     padding: 20px;
     width: ${({ $wide }) => ($wide ? '500px' : '350px')};
 `;
+
+export const BolderLabel = styled.span`
+    font-weight: 500;
+`;
