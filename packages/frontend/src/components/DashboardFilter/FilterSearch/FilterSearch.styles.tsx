18046ff390f9d89259a8ba3e17c53c157cad4a98
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-12-05 13:59:17 +0400

    chore: cleanup

--- packages/frontend/src/components/DashboardFilter/FilterSearch/FilterSearch.styles.tsx
+++ packages/frontend/src/components/DashboardFilter/FilterSearch/FilterSearch.styles.tsx
@@ -1,5 +1,3 @@
-import { Colors } from '@blueprintjs/core';
-import { MenuItem2 } from '@blueprintjs/popover2';
 import styled from 'styled-components';
 
 interface FilterModalContainerProps {
@@ -9,37 +7,4 @@ interface FilterModalContainerProps {
 export const FilterModalContainer = styled.div<FilterModalContainerProps>`
     padding: 20px;
     width: ${({ $wide }) => ($wide ? '500px' : '350px')};
-`;
-
-export const TileContainer = styled.div`
-    width: 500px;
-`;
-
-export const InputWrapper = styled.div`
-    width: 20.5em;
-`;
-
-export const DimensionLabel = styled(MenuItem2)`
-    margin: 0;
-    width: 100%;
-    border-radius: 0;
-
-    span {
-        width: 100%;
-        text-align: left;
-    }
-
-    :active,
-    :focus {
-        outline: none;
-    }
-`;
-
-export const DimensionItem = styled.span`
-    :hover {
-        ${DimensionLabel} {
-            background: ${Colors.BLUE3};
-            color: ${Colors.WHITE};
-        }
-    }
 `;
