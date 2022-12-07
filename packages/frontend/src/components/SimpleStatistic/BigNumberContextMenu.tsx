835954538e2af56711fd8a45315f0a0c4ab6fd29
Jose Rego <ZeRego@users.noreply.github.com>
2022-12-07 13:55:48 +0000

    fix: show underlying data for table calculation in big number visualization (#3919)

--- packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
+++ packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
@@ -1,6 +1,6 @@
 import { Menu, Position } from '@blueprintjs/core';
 import { MenuItem2, Popover2, Popover2Props } from '@blueprintjs/popover2';
-import { fieldId, getFields, ResultRow } from '@lightdash/common';
+import { ResultRow } from '@lightdash/common';
 import { FC, useCallback } from 'react';
 import { useExplore } from '../../hooks/useExplore';
 import { useVisualizationContext } from '../LightdashVisualization/VisualizationProvider';
@@ -22,13 +22,9 @@ export const BigNumberContextMenu: FC<BigNumberContextMenuProps> = ({
             explore !== undefined &&
             bigNumberConfig.selectedField !== undefined
         ) {
-            const fields = getFields(explore);
-
-            const selectedField = fields.find(
-                (dimension) =>
-                    fieldId(dimension) === bigNumberConfig.selectedField,
-            );
-            const meta = { item: selectedField };
+            const meta = {
+                item: bigNumberConfig.getField(bigNumberConfig.selectedField),
+            };
 
             const row: ResultRow = resultsData?.rows?.[0] || {};
             const value = row[bigNumberConfig.selectedField]?.value;
