b71ac68ebf9f3337d8af6d83d8902e4ef7f6e278
Rephus <rephus@gmail.com>
2022-12-19 08:39:05 +0100

    add v7

--- packages/cli/src/dbt/validation.ts
+++ packages/cli/src/dbt/validation.ts
@@ -12,9 +12,10 @@ import {
 import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
 import { AnyValidateFunction } from 'ajv/dist/types';
+import dbtManifestSchema from '../manifestv7.json';
 import lightdashDbtSchema from '../schema.json';
 
-const ajv = new Ajv({ schemas: [lightdashDbtSchema] });
+const ajv = new Ajv({ schemas: [lightdashDbtSchema, dbtManifestSchema] });
 addFormats(ajv);
 
 const getModelValidator = () => {
