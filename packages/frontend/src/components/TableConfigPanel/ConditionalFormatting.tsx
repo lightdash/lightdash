d7d9a7c416fb9a21ee34585d0217cf4a2c976352
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-04 19:05:36 +0400

    wip: use filter provider to input conditional formatting rules

--- packages/frontend/src/components/TableConfigPanel/ConditionalFormatting.tsx
+++ packages/frontend/src/components/TableConfigPanel/ConditionalFormatting.tsx
@@ -1,7 +1,19 @@
-import { CompiledField } from '@lightdash/common';
+import { FormGroup, HTMLSelect } from '@blueprintjs/core';
+import {
+    CompiledField,
+    createFilterRuleFromField,
+    FilterOperator,
+    FilterRule,
+    FilterType,
+} from '@lightdash/common';
 import { FC, useState } from 'react';
 import ColorInput from '../common/ColorInput';
+import { FilterTypeConfig } from '../common/Filters/configs';
 import FieldAutoComplete from '../common/Filters/FieldAutoComplete';
+import {
+    FieldsWithSuggestions,
+    FiltersProvider,
+} from '../common/Filters/FiltersProvider';
 
 interface ConditionalFormattingProps {
     numericFields: CompiledField[];
@@ -10,30 +22,73 @@ interface ConditionalFormattingProps {
 const ConditionalFormatting: FC<ConditionalFormattingProps> = ({
     numericFields,
 }) => {
-    const [activeField, setActiveField] = useState<CompiledField>();
+    const [field, setField] = useState<CompiledField>();
     const [color, setColor] = useState<string>();
+    const [filter, setFilter] = useState<FilterRule>();
+
+    const handleChangeField = (newField: CompiledField) => {
+        setField(newField);
+        setFilter(createFilterRuleFromField(newField));
+    };
+
+    const handleChangeFilterOperator = (newOperator: FilterOperator) => {
+        if (!filter) return;
+        setFilter({ ...filter, operator: newOperator });
+    };
+
+    const config = FilterTypeConfig.number;
 
     return (
-        <>
-            <FieldAutoComplete
-                id="numeric-field-autocomplete"
-                fields={numericFields}
-                activeField={activeField}
-                onChange={setActiveField}
-                popoverProps={{
-                    lazy: true,
-                    matchTargetWidth: true,
-                }}
-            />
-
-            {activeField && (
-                <ColorInput
-                    placeholder="Enter hex color"
-                    value={color}
-                    onChange={(e) => setColor(e.target.value)}
+        <FiltersProvider projectUuid="BLAH." fieldsMap={{}}>
+            <FormGroup label="Select field">
+                <FieldAutoComplete
+                    id="numeric-field-autocomplete"
+                    fields={numericFields}
+                    activeField={field}
+                    onChange={handleChangeField}
+                    popoverProps={{
+                        lazy: true,
+                        matchTargetWidth: true,
+                    }}
                 />
+            </FormGroup>
+
+            {field && filter && (
+                <>
+                    <FormGroup label="Set color">
+                        <ColorInput
+                            placeholder="Enter hex color"
+                            value={color}
+                            onChange={(e) => setColor(e.target.value)}
+                        />
+                    </FormGroup>
+
+                    <FormGroup label="Value">
+                        <HTMLSelect
+                            fill
+                            onChange={(e) =>
+                                handleChangeFilterOperator(
+                                    e.target.value as FilterOperator,
+                                )
+                            }
+                            options={config.operatorOptions}
+                            value={filter?.operator}
+                        />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <config.inputs
+                            filterType={FilterType.NUMBER}
+                            field={field}
+                            filterRule={filter}
+                            onChange={(newFilterRule) =>
+                                setFilter(newFilterRule)
+                            }
+                        />
+                    </FormGroup>
+                </>
             )}
-        </>
+        </FiltersProvider>
     );
 };
 export default ConditionalFormatting;
