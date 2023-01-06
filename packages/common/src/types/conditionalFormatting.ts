93b386450777c11f167209761bc79ba6ecc3c8b8
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-06 19:27:54 +0400

    chore: improve typings

--- packages/common/src/types/conditionalFormatting.ts
+++ packages/common/src/types/conditionalFormatting.ts
@@ -1,71 +1,58 @@
 import { v4 as uuidv4 } from 'uuid';
 import assertUnreachable from '../utils/assertUnreachable';
-import { getFilterRuleWithDefaultValue } from '../utils/filters';
-import { CompiledField, fieldId } from './field';
-import { FieldTarget, FilterOperator, FilterRule } from './filter';
+import { ConditionalRule } from './conditionalRule';
+import { FieldTarget, FilterOperator } from './filter';
 
-export type ConditionalFormattingRule = FilterRule<
-    FilterOperator,
-    FieldTarget,
-    number,
-    never
->;
+export type ConditionalFormattingRule = ConditionalRule<FilterOperator, number>;
 
 export interface ConditionalFormattingConfig {
-    field: CompiledField;
+    target: FieldTarget | null;
     rules: ConditionalFormattingRule[];
     color: string;
 }
 
-export const createConditionalFormatingRule = (
-    field: CompiledField,
-): ConditionalFormattingRule =>
-    getFilterRuleWithDefaultValue(
-        field,
-        {
-            id: uuidv4(),
-            target: { fieldId: fieldId(field) },
-            operator: FilterOperator.EQUALS,
-        },
-        [],
-    );
+export const createConditionalFormatingRule =
+    (): ConditionalFormattingRule => ({
+        operator: FilterOperator.EQUALS,
+        values: [],
+    });
 
 export const hasConditionalFormatting = (
     rules: ConditionalFormattingConfig['rules'],
     value: number,
 ) =>
-    rules.every((rule) =>
-        rule.values && rule.values.length > 0
-            ? rule.values.some((conditionValue) => {
-                  const { operator } = rule;
+    rules.every((rule) => {
+        if (!rule.values || rule.values.length === 0) {
+            return false;
+        }
 
-                  switch (operator) {
-                      case FilterOperator.NULL:
-                          return value === null;
-                      case FilterOperator.NOT_NULL:
-                          return value !== conditionValue;
-                      case FilterOperator.EQUALS:
-                          return value === conditionValue;
-                      case FilterOperator.NOT_EQUALS:
-                          return value !== conditionValue;
-                      case FilterOperator.LESS_THAN:
-                          return value < conditionValue;
-                      case FilterOperator.GREATER_THAN:
-                          return value > conditionValue;
-                      case FilterOperator.STARTS_WITH:
-                      case FilterOperator.INCLUDE:
-                      case FilterOperator.NOT_INCLUDE:
-                      case FilterOperator.LESS_THAN_OR_EQUAL:
-                      case FilterOperator.GREATER_THAN_OR_EQUAL:
-                      case FilterOperator.IN_THE_PAST:
-                      case FilterOperator.IN_THE_CURRENT:
-                          throw new Error('Not implemented');
-                      default:
-                          return assertUnreachable(
-                              operator,
-                              'Unknown operator',
-                          );
-                  }
-              })
-            : false,
-    );
+        return rule.values.some((conditionValue) => {
+            switch (rule.operator) {
+                case FilterOperator.NULL:
+                    return value === null;
+                case FilterOperator.NOT_NULL:
+                    return value !== conditionValue;
+                case FilterOperator.EQUALS:
+                    return value === conditionValue;
+                case FilterOperator.NOT_EQUALS:
+                    return value !== conditionValue;
+                case FilterOperator.LESS_THAN:
+                    return value < conditionValue;
+                case FilterOperator.GREATER_THAN:
+                    return value > conditionValue;
+                case FilterOperator.STARTS_WITH:
+                case FilterOperator.INCLUDE:
+                case FilterOperator.NOT_INCLUDE:
+                case FilterOperator.LESS_THAN_OR_EQUAL:
+                case FilterOperator.GREATER_THAN_OR_EQUAL:
+                case FilterOperator.IN_THE_PAST:
+                case FilterOperator.IN_THE_CURRENT:
+                    throw new Error('Not implemented');
+                default:
+                    return assertUnreachable(
+                        rule.operator,
+                        'Unknown operator for conditional formatting',
+                    );
+            }
+        });
+    });
