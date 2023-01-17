b03032330e66a71d6430090ec22a8ee4eadf1821
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-17 18:19:21 +0400

    feat: conditional formatting multiple conditions (#4164)
    * chore: rename fn name
    
    * style: improve rule styles
    
    * wip: conditional formatting rule component
    
    * style: improve spacing
    
    * feat: expand rules only if its the only one or adding
    
    * style: adjust spacing

--- packages/frontend/src/components/TableConfigPanel/ConditionalFormatting.styles.ts
+++ packages/frontend/src/components/TableConfigPanel/ConditionalFormatting.styles.ts
@@ -4,32 +4,52 @@ import styled from 'styled-components';
 export const ConditionalFormattingListWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 8px;
+    gap 10px;
 
-    .bp4-form-group:last-child {
+    .bp4-form-group {
         margin: 0;
     }
 `;
 
-export const ConditionalFormattingWrapper = styled.div``;
+export const ConditionalFormattingWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
 
 export const ConditionalFormattingConfigWrapper = styled.div`
-    margin-top: 4px;
-    padding: 12px 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 8px 10px;
     background-color: ${Colors.LIGHT_GRAY5};
     border-radius: 5px;
-
-    .bp4-form-group:last-child {
-        margin: 0;
-    }
 `;
 
-export const StyledCloseButton = styled(Button)`
-    margin-left: auto;
-`;
-
-export const ConditionalRuleHeader = styled.div`
+export const ConditionalFormattingGroupHeader = styled.div`
     display: flex;
-    gap: 6px;
     align-items: center;
+`;
+
+export const ConditionalFormattingRuleGroupHeader = styled(
+    ConditionalFormattingGroupHeader,
+)``;
+
+export const ConditionalFormattingGroupTitle = styled.div`
+    font-size: 13px;
+    color: ${Colors.DARK_GRAY3};
+    font-weight: 600;
+`;
+
+export const ConditionalFormattingRuleAndLabel = styled.div`
+    padding: 0 10px;
+    font-size: 11px;
+    color: ${Colors.DARK_GRAY5};
+    font-weight: 600;
+`;
+
+export const ConditionalFormattingRuleWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
 `;
