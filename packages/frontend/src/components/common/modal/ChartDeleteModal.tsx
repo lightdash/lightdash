79fff9749791a7f5e7d43c534a4b794dd101545f
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-25 19:30:14 +0400

    feat: add chart delete modal

---
+++ packages/frontend/src/components/common/modal/ChartDeleteModal.tsx
@@ -0,0 +1,87 @@
+import {
+    Button,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogProps,
+} from '@blueprintjs/core';
+import { FC } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDashboardsContainingChart } from '../../../hooks/dashboard/useDashboards';
+import { useDeleteMutation, useSavedQuery } from '../../../hooks/useSavedQuery';
+
+interface ChartDeleteModalProps extends DialogProps {
+    uuid: string;
+    onConfirm?: () => void;
+}
+
+const ChartDeleteModal: FC<ChartDeleteModalProps> = ({
+    uuid,
+    onConfirm,
+    ...modalProps
+}) => {
+    const { projectUuid } = useParams<{ projectUuid: string }>();
+    const { data: chart, isLoading } = useSavedQuery({ id: uuid });
+    const { data: relatedDashboards, isLoading: isLoadingRelatedDashboards } =
+        useDashboardsContainingChart(projectUuid, uuid);
+    const { mutateAsync: deleteChart, isLoading: isDeletingChart } =
+        useDeleteMutation();
+
+    if (
+        isLoading ||
+        isLoadingRelatedDashboards ||
+        !chart ||
+        !relatedDashboards
+    ) {
+        return null;
+    }
+
+    const handleConfirm = async () => {
+        await deleteChart(uuid);
+        onConfirm?.();
+    };
+
+    return (
+        <Dialog {...modalProps} title="Delete Chart" icon="trash">
+            <DialogBody>
+                <p>
+                    Are you sure you want to delete the chart{' '}
+                    <b>"{chart.name}"</b>?
+                </p>
+
+                <b>
+                    This action will remove a chart tile from{' '}
+                    {relatedDashboards.length} dashboard
+                    {relatedDashboards.length > 1 ? 's' : ''}:
+                </b>
+
+                <ul>
+                    {relatedDashboards.map((dashboard) => (
+                        <li key={dashboard.uuid}>
+                            <Link
+                                target="_blank"
+                                to={`/projects/${projectUuid}/dashboards/${dashboard.uuid}`}
+                            >
+                                {dashboard.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </DialogBody>
+
+            <DialogFooter
+                actions={
+                    <Button
+                        loading={isDeletingChart}
+                        intent="danger"
+                        onClick={handleConfirm}
+                    >
+                        Delete
+                    </Button>
+                }
+            />
+        </Dialog>
+    );
+};
+
+export default ChartDeleteModal;
