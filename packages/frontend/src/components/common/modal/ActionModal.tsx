10546f635435d4a2bf92164b520deb4f7fd26f65
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-26 04:12:18 +0400

    chore: more cleanups

--- packages/frontend/src/components/common/modal/ActionModal.tsx
+++ packages/frontend/src/components/common/modal/ActionModal.tsx
@@ -1,7 +1,3 @@
-import { IconName, Intent } from '@blueprintjs/core';
-import { Dispatch, FC, SetStateAction } from 'react';
-import { UseFormReturn } from 'react-hook-form';
-
 export enum ActionTypeModal {
     CLOSE,
     UPDATE,
@@ -10,23 +6,3 @@ export enum ActionTypeModal {
     MOVE_TO_SPACE,
     CREATE_SPACE,
 }
-
-export type ActionModalProps<T> = {
-    title: string;
-    icon?: IconName;
-    confirmButtonLabel: string;
-    confirmButtonIntent?: Intent;
-    useActionModalState: [
-        { actionType: number; data?: T },
-        Dispatch<SetStateAction<{ actionType: number; data?: T }>>,
-    ];
-    ModalContent: FC<
-        Pick<ActionModalProps<T>, 'useActionModalState' | 'isDisabled'>
-    >;
-    isDisabled: boolean;
-    onSubmitForm: (data: T) => void;
-    completedMutation: boolean;
-    setFormValues?: (data: any, methods: UseFormReturn<any, object>) => void;
-    onClose?: () => void;
-    errorMessage?: string;
-};
