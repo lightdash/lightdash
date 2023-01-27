9e3bbf8a2fd1c8043b28215e4dde4873ff36c956
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-01-27 13:27:12 +0400

    style: tweaks

--- packages/frontend/src/components/common/ResourceList/ResourceIcon.tsx
+++ packages/frontend/src/components/common/ResourceList/ResourceIcon.tsx
@@ -23,52 +23,52 @@ const ResourceIcon: FC<ResourceIconProps> = ({ resource, resourceType }) => {
     switch (resourceType) {
         case 'dashboard':
             return (
-                <ResourceIconBox color={Colors.BLUE5}>
-                    <IconLayoutDashboard color={Colors.BLUE3} />
+                <ResourceIconBox color={Colors.GREEN3}>
+                    <IconLayoutDashboard color={Colors.GREEN3} size={20} />
                 </ResourceIconBox>
             );
         case 'chart':
             switch ((resource as SpaceQuery).chartType) {
                 case ChartKind.LINE:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconChartLine color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconChartLine color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.BAR:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconChartBar color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconChartBar color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.SCATTER:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconChartDots color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconChartDots color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.AREA:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconChartArea color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconChartArea color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.MIXED:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconChartAreaLine color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconChartAreaLine color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.TABLE:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <IconTable color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <IconTable color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 case ChartKind.BIG_NUMBER:
                     return (
-                        <ResourceIconBox color={Colors.GREEN3}>
-                            <Icon123 color={Colors.GREEN3} />
+                        <ResourceIconBox color={Colors.BLUE3}>
+                            <Icon123 color={Colors.BLUE3} size={20} />
                         </ResourceIconBox>
                     );
                 default:
