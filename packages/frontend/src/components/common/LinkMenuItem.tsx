897458213af1041e8aca98ae04ce7045636acf7c
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-02-02 17:38:48 +0400

    fix: fix explore from here link in dashboard menu

---
+++ packages/frontend/src/components/common/LinkMenuItem.tsx
@@ -0,0 +1,41 @@
+import { MenuItem2, MenuItem2Props } from '@blueprintjs/popover2';
+import React, { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import { EventData, useTracking } from '../../providers/TrackingProvider';
+
+export interface LinkMenuItemProps extends MenuItem2Props {
+    href: string;
+    trackingEvent?: EventData;
+    target?: React.HTMLAttributeAnchorTarget;
+}
+
+const LinkMenuItem: FC<LinkMenuItemProps> = ({
+    href,
+    target,
+    trackingEvent,
+    onClick,
+    ...rest
+}) => {
+    const history = useHistory();
+    const { track } = useTracking();
+
+    return (
+        <MenuItem2
+            {...rest}
+            href={href}
+            target={target}
+            onClick={(e) => {
+                if (!e.ctrlKey && !e.metaKey && target !== '_blank') {
+                    e.preventDefault();
+                    history.push(href);
+                }
+
+                onClick?.(e);
+
+                if (trackingEvent) track(trackingEvent);
+            }}
+        />
+    );
+};
+
+export default LinkMenuItem;
