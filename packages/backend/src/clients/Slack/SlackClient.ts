8cc2bace65b32749a5c5b011ed8b8eec4e1c7dfd
Rephus <rephus@gmail.com>
2023-02-02 17:22:52 +0100

    unfurl slack image notification

--- packages/backend/src/clients/Slack/SlackClient.ts
+++ packages/backend/src/clients/Slack/SlackClient.ts
@@ -1,14 +1,24 @@
 import { App, LogLevel } from '@slack/bolt';
+import { nanoid } from 'nanoid';
 
 import { LightdashConfig } from '../../config/parseConfig';
 import Logger from '../../logger';
+import { DashboardModel } from '../../models/DashboardModel/DashboardModel';
+import { SavedChartModel } from '../../models/SavedChartModel';
 import { SlackAuthenticationModel } from '../../models/SlackAuthenticationModel';
+import {
+    LightdashPage,
+    Unfurl,
+} from '../../services/UnfurlService/UnfurlService';
 
 import { slackOptions } from './SlackOptions';
+import { unfurlChartAndDashboard } from './SlackUnfurl';
 
 type SlackClientDependencies = {
     slackAuthenticationModel: SlackAuthenticationModel;
     lightdashConfig: LightdashConfig;
+    savedChartModel: SavedChartModel;
+    dashboardModel: DashboardModel;
 };
 
 export class SlackClient {
@@ -16,14 +26,22 @@ export class SlackClient {
 
     lightdashConfig: LightdashConfig;
 
+    savedChartModel: SavedChartModel;
+
     slackApp: App | undefined;
+
+    dashboardModel: DashboardModel;
 
     constructor({
         slackAuthenticationModel,
         lightdashConfig,
+        savedChartModel,
+        dashboardModel,
     }: SlackClientDependencies) {
         this.lightdashConfig = lightdashConfig;
         this.slackAuthenticationModel = slackAuthenticationModel;
+        this.savedChartModel = savedChartModel;
+        this.dashboardModel = dashboardModel;
         this.start();
     }
 
@@ -44,14 +62,107 @@ export class SlackClient {
                     appToken: this.lightdashConfig.slack.appToken,
                 });
             } catch (e: unknown) {
-                Logger.error(`Unable to start Slack app ${e}`);
+                Logger.error(`Unable to start Slack client ${e}`);
             }
         } else {
-            Logger.warn(`Missing "SLACK_APP_TOKEN", Slack App will not run`);
+            Logger.warn(
+                `Missing "SLACK_APP_TOKEN", Slack client will not work`,
+            );
         }
     }
 
     async sendNotification(notification: {
+        organizationUuid: string;
+        userUuid: string;
+        chartUuid?: string;
+        dashboardUuid?: string;
+        channel: string;
+    }): Promise<void> {
+        if (this.slackApp === undefined) {
+            throw new Error('Slack app is not configured');
+        }
+        try {
+            /* TODO  analytics.track({
+                event: 'share_slack.unfurl',
+                userId: eventUserId,
+                properties: {},
+            });
+            */
+            const { userUuid, chartUuid, channel } = notification;
+
+            if (chartUuid === undefined) {
+                Logger.error(`Undefined chart ${chartUuid}`);
+                return;
+            }
+            const chart = await this.savedChartModel.get(chartUuid);
+
+            const imageId = `slack-image-${nanoid()}`;
+
+            const url = `${this.lightdashConfig.siteUrl}/projects/${chart.projectUuid}/saved/${chartUuid}`;
+            const imageUrl = `https://lightdash-cloud-file-storage-us.storage.googleapis.com/slack-image-NOp7S9b-qz4I3VQ4pL-QC`; /* await unfurlService.unfurlImage(
+                    url,
+                    LightdashPage.CHART,
+                    imageId,
+                    userUuid,
+                ); */
+
+            if (imageUrl) {
+                const installation =
+                    await this.slackAuthenticationModel.getInstallationFromOrganizationUuid(
+                        notification.organizationUuid,
+                    );
+                const unfurl: Unfurl = {
+                    title: chart.name,
+                    description: chart.description,
+                    imageUrl,
+                    pageType: LightdashPage.CHART,
+                };
+                const blocks = unfurlChartAndDashboard(url, unfurl, true);
+                this.slackApp.client.chat
+                    .postMessage({
+                        token: installation?.token,
+                        channel,
+                        blocks,
+                        text: chart.name,
+                    })
+                    .catch((e: any) => {
+                        /* analytics.track({
+                            event: 'share_slack.unfurl_error',
+                            userId: event.user,
+                            properties: {
+                                error: `${e}`,
+                            },
+                        }); */
+                        Logger.error(
+                            `Unable to postmessage on slack with blocks ${JSON.stringify(
+                                blocks,
+                            )}: ${JSON.stringify(e)}`,
+                        );
+                    });
+
+                /* analytics.track({
+                        event: 'share_slack.unfurl_completed',
+                        userId: eventUserId,
+                        properties: { pageType: details.pageType },
+                    }); */
+            } else {
+                // TODO track error here and retry
+            }
+        } catch (e) {
+            /* analytics.track({
+                event: 'share_slack.unfurl_error',
+                userId: eventUserId,
+
+                properties: {
+                    error: `${e}`,
+                },
+            });
+
+            notifySlackError(e, l.url, client, event); */
+        }
+    }
+
+    async sendText(message: {
         organizationUuid: string;
         text: string;
         channel: string;
@@ -60,16 +171,18 @@ export class SlackClient {
             throw new Error('Slack app is not configured');
         }
 
+        const { organizationUuid, text, channel } = message;
+
         const installation =
             await this.slackAuthenticationModel.getInstallationFromOrganizationUuid(
-                notification.organizationUuid,
+                organizationUuid,
             );
 
         this.slackApp.client.chat
             .postMessage({
                 token: installation?.token,
-                channel: notification.channel,
-                text: notification.text,
+                channel,
+                text,
             })
             .catch((e: any) => {
                 /* analytics.track({
