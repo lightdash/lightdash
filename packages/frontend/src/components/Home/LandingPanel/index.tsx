d2eb0ae5263f1f736dc6f49784b8df222f7eb7ea
collins <collinsogbuzuru@gmail.com>
2023-02-09 08:44:21 +0100

    fix: safari rendering bolt icon differently

--- packages/frontend/src/components/Home/LandingPanel/index.tsx
+++ packages/frontend/src/components/Home/LandingPanel/index.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import BoltIcon from '../icon/boltIcon';
+
 import {
     Intro,
     LandingHeaderWrapper,
@@ -17,9 +19,8 @@ const LandingPanel: FC<Props> = ({ userName, projectUuid }) => {
         <LandingHeaderWrapper>
             <WelcomeText>
                 <Title>
-                    {`Welcome${
-                        userName ? ', ' + userName : ' to Lightdash'
-                    }! ⚡`}
+                    {`Welcome${userName ? ', ' + userName : ' to Lightdash'}!`}
+                    <BoltIcon />
                 </Title>
 
                 <Intro>
