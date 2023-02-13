73b28a4ddcdda751e504c9178c2997e7014bdebc
Rephus <rephus@gmail.com>
2023-02-13 15:45:32 +0100

    fix format

--- packages/backend/src/services/ProjectService/formatRows.js
+++ packages/backend/src/services/ProjectService/formatRows.js
@@ -5,8 +5,7 @@ const { workerData, parentPort } = require('worker_threads');
 const { formatRows } = require('@lightdash/common');
 
 function formatRowsWorker() {
-
-    const formattedRows = formatRows(workerData.rows, workerData.itemMap)
+    const formattedRows = formatRows(workerData.rows, workerData.itemMap);
     parentPort.postMessage(formattedRows);
 }
 
