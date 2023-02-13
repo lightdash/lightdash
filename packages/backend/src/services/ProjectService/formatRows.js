4566ae994c69535efd127f90f62182b6d2f73cb6
Javier Rengel Jiménez <rephus@gmail.com>
2023-02-13 16:09:09 +0100

    chore: format rows worker (#4440)
    * chore: format rows worker
    
    * disable lint
    
    * format file
    
    * better typing, add context
    
    * refactor formatrows
    
    * fix format

---
+++ packages/backend/src/services/ProjectService/formatRows.js
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+const { workerData, parentPort } = require('worker_threads');
+
+const { formatRows } = require('@lightdash/common');
+
+function formatRowsWorker() {
+    const formattedRows = formatRows(workerData.rows, workerData.itemMap);
+    parentPort.postMessage(formattedRows);
+}
+
+formatRowsWorker();
