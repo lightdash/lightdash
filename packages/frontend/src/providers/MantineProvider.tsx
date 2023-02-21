1728a67101ec19a96e9ffa29dcc20f49956023df
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-02-22 01:36:55 +0400

    wip

--- packages/frontend/src/providers/MantineProvider.tsx
+++ packages/frontend/src/providers/MantineProvider.tsx
@@ -6,17 +6,20 @@ import {
 import { FC } from 'react';
 
 const themeOverride: MantineThemeOverride = {
+    black: Colors.DARK_GRAY1,
+
     fontFamily:
         'BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Open Sans, Helvetica Neue, blueprint-icons-16, sans-serif',
-    fontSizes: {
-        xs: 11,
-        sm: 12,
-        md: 14,
-        lg: 16,
-        xl: 20,
-    },
-    black: Colors.DARK_GRAY1,
-    lineHeight: 1.2858142857,
+
+    // TODO: temporary fix
+    // lineHeight: 1.2858142857,
+    // fontSizes: {
+    //     xs: 11,
+    //     sm: 12,
+    //     md: 14,
+    //     lg: 16,
+    //     xl: 20,
+    // },
 };
 
 const MantineProvider: FC = ({ children }) => {
