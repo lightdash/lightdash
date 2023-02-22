7062181a8e4ffd28dde3d0f98b998f9a2309d6c7
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-02-22 13:35:31 +0400

    chore: add mantine theme with blueprint defaults

--- packages/frontend/src/providers/MantineProvider.tsx
+++ packages/frontend/src/providers/MantineProvider.tsx
@@ -8,18 +8,65 @@ import { FC } from 'react';
 const themeOverride: MantineThemeOverride = {
     black: Colors.DARK_GRAY1,
 
-    fontFamily:
-        'BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Open Sans, Helvetica Neue, blueprint-icons-16, sans-serif',
+    fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Open Sans',
+        'Helvetica Neue',
+        'blueprint-icons-16',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'sans-serif',
+    ].join(', '),
 
-    // TODO: temporary fix
-    // lineHeight: 1.2858142857,
-    // fontSizes: {
-    //     xs: 11,
-    //     sm: 12,
-    //     md: 14,
-    //     lg: 16,
-    //     xl: 20,
-    // },
+    lineHeight: 1.2858142857,
+
+    globalStyles: () => ({
+        body: {
+            textTransform: 'none',
+            fontSize: '14px',
+        },
+
+        p: {
+            marginBottom: '10px',
+            marginTop: 0,
+        },
+
+        small: {
+            fontSize: '12px',
+        },
+
+        b: {
+            fontWeight: 500,
+        },
+
+        strong: {
+            fontWeight: 600,
+        },
+
+        a: {
+            color: Colors.BLUE2,
+            textDecoration: 'none',
+            ':hover': {
+                color: Colors.BLUE2,
+                cursor: 'pointer',
+                textDecoration: 'underline',
+            },
+        },
+
+        ':focus': {
+            outline: 'rgba(45, 114, 210, 0.6) solid 2px',
+            outlineOffset: '2px',
+            '-moz-outline-radius': '6px',
+        },
+    }),
 };
 
 const MantineProvider: FC = ({ children }) => {
@@ -27,6 +74,8 @@ const MantineProvider: FC = ({ children }) => {
         <MantineProviderBase
             withGlobalStyles
             withNormalizeCSS
+            inherit
+            withCSSVariables
             theme={themeOverride}
         >
             {children}
