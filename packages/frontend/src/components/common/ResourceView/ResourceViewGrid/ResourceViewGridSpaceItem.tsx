aae525705b0c1ea7ae85b517fe9c11e68e91b19a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-02-23 19:58:49 +0400

    style: resource view grid space item

--- packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridSpaceItem.tsx
+++ packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridSpaceItem.tsx
@@ -1,5 +1,11 @@
-import { Box, Divider, Flex, Group, Paper, Text } from '@mantine/core';
-import { IconChartBar, IconLayoutDashboard } from '@tabler/icons-react';
+import { Colors } from '@blueprintjs/core';
+import { Box, Flex, Group, Paper, Stack, Text } from '@mantine/core';
+import {
+    IconChartBar,
+    IconLayoutDashboard,
+    IconLock,
+    IconUsers,
+} from '@tabler/icons-react';
 import { FC } from 'react';
 import ResourceIcon from '../ResourceIcon';
 import { ResourceViewSpaceItem } from '../resourceTypeUtils';
@@ -17,35 +23,52 @@ const ResourceViewGridSpaceItem: FC<ResourceViewGridSpaceItemProps> = ({
 }) => {
     return (
         <Paper p={0} withBorder>
-            <Group p="md" align="center" spacing="xs" noWrap>
-                <Box>
+            <Group p="md" align="center" spacing="md" noWrap>
+                <Box sx={{ flexGrow: 0, flexShrink: 0 }}>
                     <ResourceIcon item={item} />
                 </Box>
 
-                <Text lineClamp={2} fz="sm" fw={600}>
-                    {item.data.name}
-                </Text>
+                <Stack spacing={4} sx={{ flexGrow: 1, flexShrink: 1 }}>
+                    <Text lineClamp={1} fz="sm" fw={600}>
+                        {item.data.name}
+                    </Text>
+
+                    <Group spacing="sm">
+                        <Flex align="center" gap={2}>
+                            {!item.data.isPrivate ? (
+                                <IconLock color={Colors.GRAY2} size={14} />
+                            ) : (
+                                <IconUsers color={Colors.GRAY2} size={14} />
+                            )}
+
+                            <Text size={14} color="gray.6" fz="xs">
+                                {item.data.isPrivate ? 'Private' : 'Restricted'}
+                            </Text>
+                        </Flex>
+
+                        <Flex align="center" gap={2}>
+                            <IconLayoutDashboard
+                                color={Colors.GRAY2}
+                                size={14}
+                            />
+
+                            <Text size={14} color="gray.6" fz="xs">
+                                {item.data.dashboards.length}
+                            </Text>
+                        </Flex>
+
+                        <Flex align="center" gap={2}>
+                            <IconChartBar color={Colors.GRAY2} size={14} />
+
+                            <Text size={14} color="gray.6" fz="xs">
+                                {item.data.queries.length}
+                            </Text>
+                        </Flex>
+                    </Group>
+                </Stack>
+
+                <Box sx={{ flexGrow: 0, flexShrink: 0 }}>{renderActions()}</Box>
             </Group>
-
-            <Divider color="gray.3" />
-
-            <Flex pl="md" pr="xs" py={4} justify="space-between" align="center">
-                <Group spacing="sm">
-                    <Flex align="center" gap={2}>
-                        <IconLayoutDashboard size={14} />
-
-                        <Text size={14}>{item.data.dashboards.length}</Text>
-                    </Flex>
-
-                    <Flex align="center" gap={2}>
-                        <IconChartBar size={14} />
-
-                        <Text size={14}>{item.data.queries.length}</Text>
-                    </Flex>
-                </Group>
-
-                <Box>{renderActions()}</Box>
-            </Flex>
         </Paper>
     );
 };
