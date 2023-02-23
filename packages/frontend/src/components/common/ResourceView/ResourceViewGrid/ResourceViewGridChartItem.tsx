14182e2eac9392f51b517002a426424a4e9c472c
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-02-23 16:40:54 +0400

    style: resource view grid items WIP

--- packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridChartItem.tsx
+++ packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridChartItem.tsx
@@ -1,12 +1,4 @@
-import {
-    Box,
-    Card,
-    Divider,
-    Flex,
-    Group,
-    Text,
-    UnstyledButton,
-} from '@mantine/core';
+import { Box, Divider, Flex, Group, Paper, Text } from '@mantine/core';
 import { IconEye } from '@tabler/icons-react';
 import { FC } from 'react';
 import ResourceIcon from '../ResourceIcon';
@@ -24,34 +16,26 @@ const ResourceViewGridChartItem: FC<ResourceViewGridChartItemProps> = ({
     renderActions,
 }) => {
     return (
-        <UnstyledButton>
-            <Card p={0} withBorder>
-                <Group p="md" align="center" spacing="xs" noWrap>
-                    <Box>
-                        <ResourceIcon item={item} />
-                    </Box>
-                    <Text lineClamp={2} fz="sm" fw={600}>
-                        {item.data.name}
-                    </Text>
-                </Group>
+        <Paper p={0} withBorder>
+            <Group p="md" align="center" spacing="xs" noWrap grow>
+                <Box>
+                    <ResourceIcon item={item} />
+                </Box>
+                <Text lineClamp={2} fz="sm" fw={600}>
+                    {item.data.name}
+                </Text>
+            </Group>
 
-                <Divider color="gray.3" />
+            <Divider color="gray.3" />
 
-                <Flex
-                    pl="md"
-                    pr="xs"
-                    py={4}
-                    justify="space-between"
-                    align="center"
-                >
-                    <Flex align="center" gap={2}>
-                        <IconEye size={14} />
-                        <Text size={14}>{item.data.views} views</Text>
-                    </Flex>
-                    <Box>{renderActions()}</Box>
+            <Flex pl="md" pr="xs" py={4} justify="space-between" align="center">
+                <Flex align="center" gap={2}>
+                    <IconEye size={14} />
+                    <Text size={14}>{item.data.views} views</Text>
                 </Flex>
-            </Card>
-        </UnstyledButton>
+                <Box>{renderActions()}</Box>
+            </Flex>
+        </Paper>
     );
 };
 
--- packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridSpaceItem.tsx
+++ packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridSpaceItem.tsx
@@ -1,14 +1,4 @@
-import { Colors, Icon } from '@blueprintjs/core';
-import {
-    Anchor,
-    Box,
-    Card,
-    Divider,
-    Flex,
-    Group,
-    Text,
-    UnstyledButton,
-} from '@mantine/core';
+import { Box, Divider, Flex, Group, Paper, Text } from '@mantine/core';
 import { IconChartBar, IconLayoutDashboard } from '@tabler/icons-react';
 import { FC } from 'react';
 import ResourceIcon from '../ResourceIcon';
@@ -26,42 +16,34 @@ const ResourceViewGridSpaceItem: FC<ResourceViewGridSpaceItemProps> = ({
     renderActions,
 }) => {
     return (
-        <UnstyledButton>
-            <Card p={0} withBorder>
-                <Group p="md" align="center" spacing="xs" noWrap>
-                    <Box>
-                        <ResourceIcon item={item} />
-                    </Box>
-                    <Text lineClamp={2} fz="sm" fw={600}>
-                        {item.data.name}
-                    </Text>
+        <Paper p={0} withBorder>
+            <Group p="md" align="center" spacing="xs" noWrap grow>
+                <Box>
+                    <ResourceIcon item={item} />
+                </Box>
+                <Text lineClamp={2} fz="sm" fw={600}>
+                    {item.data.name}
+                </Text>
+            </Group>
+
+            <Divider color="gray.3" />
+
+            <Flex pl="md" pr="xs" py={4} justify="space-between" align="center">
+                <Group spacing="sm">
+                    <Flex align="center" gap={2}>
+                        <IconLayoutDashboard size={14} />
+                        <Text size={14}>{item.data.dashboards.length}</Text>
+                    </Flex>
+
+                    <Flex align="center" gap={2}>
+                        <IconChartBar size={14} />
+                        <Text size={14}>{item.data.queries.length}</Text>
+                    </Flex>
                 </Group>
 
-                <Divider color="gray.3" />
-
-                <Flex
-                    pl="md"
-                    pr="xs"
-                    py={4}
-                    justify="space-between"
-                    align="center"
-                >
-                    <Group spacing="sm">
-                        <Flex align="center" gap={2}>
-                            <IconLayoutDashboard size={14} />
-                            <Text size={14}>{item.data.dashboards.length}</Text>
-                        </Flex>
-
-                        <Flex align="center" gap={2}>
-                            <IconChartBar size={14} />
-                            <Text size={14}>{item.data.queries.length}</Text>
-                        </Flex>
-                    </Group>
-
-                    <Box>{renderActions()}</Box>
-                </Flex>
-            </Card>
-        </UnstyledButton>
+                <Box>{renderActions()}</Box>
+            </Flex>
+        </Paper>
     );
 };
 
