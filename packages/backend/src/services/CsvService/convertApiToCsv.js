1e8a1b4da494a5f75482360c530a45f3ef84d14b
Rephus <rephus@gmail.com>
2023-02-23 16:22:52 +0100

    format

--- packages/backend/src/services/CsvService/convertApiToCsv.js
+++ packages/backend/src/services/CsvService/convertApiToCsv.js
@@ -5,12 +5,14 @@ const { workerData, parentPort } = require('worker_threads');
 const { convertApiToCsv } = require('./CsvService');
 
 function formatRowsWorker() {
-   
-    convertApiToCsv(workerData.fieldIds, workerData.rows, workerData.onlyRaw, workerData.itemMap).then(
-        (csv) => {
-            parentPort.postMessage(csv);
-        }
-    );
+    convertApiToCsv(
+        workerData.fieldIds,
+        workerData.rows,
+        workerData.onlyRaw,
+        workerData.itemMap,
+    ).then((csv) => {
+        parentPort.postMessage(csv);
+    });
 }
 
 formatRowsWorker();
--- packages/backend/src/services/CsvService/convertSqlToCsv.js
+++ packages/backend/src/services/CsvService/convertSqlToCsv.js
@@ -5,12 +5,9 @@ const { workerData, parentPort } = require('worker_threads');
 const { convertSqlToCsv } = require('./CsvService');
 
 function formatRowsWorker() {
-   
-    convertSqlToCsv(workerData.results).then(
-        (csv) => {
-            parentPort.postMessage(csv);
-        }
-    );
+    convertSqlToCsv(workerData.results).then((csv) => {
+        parentPort.postMessage(csv);
+    });
 }
 
 formatRowsWorker();
