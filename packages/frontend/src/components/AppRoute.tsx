a76daa3a679a9ccd1b76e3d447f97a3a118a22f9
Jose Rego <jose@lightdash.com>
2023-03-09 23:38:49 +0000

    fix: loading verify page

--- packages/frontend/src/components/AppRoute.tsx
+++ packages/frontend/src/components/AppRoute.tsx
@@ -1,27 +1,20 @@
-import { LightdashMode } from '@lightdash/common';
 import React, { ComponentProps, FC } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useOrganisation } from '../hooks/organisation/useOrganisation';
-import { useEmailStatus } from '../hooks/useEmailVerification';
 import { useApp } from '../providers/AppProvider';
 import ErrorState from './common/ErrorState';
 import PageSpinner from './PageSpinner';
 
 const AppRoute: FC<ComponentProps<typeof Route>> = ({ children, ...rest }) => {
-    const { health, user } = useApp();
-    const emailStatus = useEmailStatus();
-    const isEmailServerConfigured = health.data?.hasEmailClient;
+    const { health } = useApp();
+
     const orgRequest = useOrganisation();
 
     return (
         <Route
             {...rest}
             render={({ location }) => {
-                if (
-                    health.isLoading ||
-                    orgRequest.isLoading ||
-                    emailStatus.isLoading
-                ) {
+                if (health.isLoading || orgRequest.isLoading) {
                     return <PageSpinner />;
                 }
 
@@ -31,22 +24,6 @@ const AppRoute: FC<ComponentProps<typeof Route>> = ({ children, ...rest }) => {
                             error={
                                 orgRequest.error?.error || health.error?.error
                             }
-                        />
-                    );
-                }
-
-                if (
-                    health.data?.mode !== LightdashMode.PR &&
-                    !emailStatus.data?.isVerified &&
-                    isEmailServerConfigured &&
-                    !user.data?.isSetupComplete
-                ) {
-                    return (
-                        <Redirect
-                            to={{
-                                pathname: '/verify-email',
-                                state: { from: location },
-                            }}
                         />
                     );
                 }
