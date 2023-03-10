d596efb8aaeebb97ad664d8fb9ada3c4508c6845
Jose Rego <jose@lightdash.com>
2023-03-10 17:24:39 +0000

    styles: style page

--- packages/frontend/src/pages/JoinOrganization.tsx
+++ packages/frontend/src/pages/JoinOrganization.tsx
@@ -1,8 +1,11 @@
-import { Button } from '@blueprintjs/core';
+import { Card, Colors } from '@blueprintjs/core';
 import { LightdashMode } from '@lightdash/common';
+import { Avatar, Box, Text } from '@mantine/core';
+import { IconChevronRight } from '@tabler/icons-react';
 import React, { FC, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { useHistory } from 'react-router-dom';
+import { BigButton } from '../components/common/BigButton';
 import Page from '../components/common/Page/Page';
 import PageSpinner from '../components/PageSpinner';
 import { useOrganisationCreateMutation } from '../hooks/organisation/useOrganisationCreateMutation';
@@ -10,7 +13,14 @@ import useAllowedOrganizations from '../hooks/user/useAllowedOrganizations';
 import { useJoinOrganizationMutation } from '../hooks/user/useJoinOrganizationMutation';
 import { useApp } from '../providers/AppProvider';
 import LightdashLogo from '../svgs/lightdash-black.svg';
-import { CardWrapper, FormWrapper, Logo, LogoWrapper } from './SignUp.styles';
+import {
+    CardWrapper,
+    FormWrapper,
+    Logo,
+    LogoWrapper,
+    Subtitle,
+    Title,
+} from './SignUp.styles';
 
 export const JoinOrganizationPage: FC = () => {
     const { health, user } = useApp();
@@ -59,6 +69,8 @@ export const JoinOrganizationPage: FC = () => {
         return <PageSpinner />;
     }
 
+    const disabled = isCreatingOrg || isJoiningOrg;
+
     return (
         <Page isFullHeight>
             <Helmet>
@@ -69,23 +81,48 @@ export const JoinOrganizationPage: FC = () => {
             </LogoWrapper>
             <FormWrapper>
                 <CardWrapper elevation={2}>
+                    <Title>Join a workspace</Title>
+                    <Subtitle>
+                        The workspaces below are open to anyone with a{' '}
+                        <b>@{(user.data?.email || '').split('@')[1]}</b> domain.
+                        Select a workspace to join, or create a new one!
+                    </Subtitle>
                     {allowedOrgs?.map((org) => (
-                        <p key={org.organizationUuid}>
-                            {org.name} {org.organizationUuid} {org.membersCount}{' '}
-                            <Button
-                                disabled={isJoiningOrg}
-                                onClick={() => joinOrg(org.organizationUuid)}
-                            >
-                                Join
-                            </Button>
-                        </p>
+                        <Card
+                            key={org.organizationUuid}
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                marginBottom: 22,
+                                padding: 10,
+                            }}
+                            interactive
+                            elevation={0}
+                            onClick={() => joinOrg(org.organizationUuid)}
+                        >
+                            <Avatar size="md" radius="xl" color={'gray'}>
+                                {org.name[0]?.toUpperCase()}
+                            </Avatar>
+                            <Box style={{ flex: 1, marginLeft: 12 }}>
+                                <Text truncate fw={600}>
+                                    {org.name}
+                                </Text>
+                                <Text fz="xs" c="gray">
+                                    {org.membersCount} members
+                                </Text>
+                            </Box>
+                            <IconChevronRight size={24} color={Colors.GRAY3} />
+                        </Card>
                     ))}
-                    <Button
-                        disabled={isCreatingOrg}
+                    <BigButton
+                        fill
+                        intent={'primary'}
+                        disabled={disabled}
+                        loading={isCreatingOrg}
                         onClick={() => createOrg({ name: '' })}
                     >
                         Create a new one
-                    </Button>
+                    </BigButton>
                 </CardWrapper>
             </FormWrapper>
         </Page>
