2efd07fa43f11c08751f41bae16bd95c3815c0dc
Yasmine <yasmine@lightdash.com>
2023-03-12 13:01:50 +0100

    feat: organisation settings allowed email domains panel (#4752)
    * feat: organisation settings allowed email domains panel
    
    * fix: multiselect when there are projects and input when no projects
    
    * fix: projects select
    
    * fix: reset default values
    
    ---------
    
    Co-authored-by: Jose Rego <jose@lightdash.com>

---
+++ packages/frontend/src/components/UserSettings/AllowedDomainsPanel/index.tsx
@@ -0,0 +1,156 @@
+import { Button, Intent } from '@blueprintjs/core';
+import { OrganizationMemberRole, ProjectType } from '@lightdash/common';
+import React, { FC, useEffect, useMemo } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+    useAllowedEmailDomains,
+    useUpdateAllowedEmailDomains,
+} from '../../../hooks/organisation/useAllowedDomains';
+import { useProjects } from '../../../hooks/useProjects';
+import { isValidEmailDomain } from '../../../utils/fieldValidators';
+import Form from '../../ReactHookForm/Form';
+import MultiSelect from '../../ReactHookForm/MultiSelect';
+import Select2 from '../../ReactHookForm/Select2';
+import TagInput from '../../ReactHookForm/TagInput';
+import { Description } from '../DeleteOrganisationPanel/DeleteOrganisationPanel.styles';
+import { FormWrapper } from '../OrganisationPanel/OrganisationPanel.styles';
+
+type FormData = {
+    emailDomains: string[];
+    role: OrganizationMemberRole;
+    projects: { value: string; label: string }[];
+};
+
+const roleOptions = [
+    {
+        value: OrganizationMemberRole.VIEWER,
+        label: 'Organisation Viewer',
+        subLabel: (
+            <Description>
+                Has view access across all projects in the org
+            </Description>
+        ),
+    },
+    {
+        value: OrganizationMemberRole.MEMBER,
+        label: 'Organisation Member',
+        subLabel: (
+            <Description>Has view access to selected projects only</Description>
+        ),
+    },
+];
+
+const AllowedDomainsPanel: FC = () => {
+    const methods = useForm<FormData>({
+        mode: 'onSubmit',
+        defaultValues: {
+            emailDomains: [],
+            role: OrganizationMemberRole.VIEWER,
+            projects: [],
+        },
+    });
+    const { data: projects, isLoading: isLoadingProjects } = useProjects();
+    const { data, isLoading: emailDomainsLoading } = useAllowedEmailDomains();
+    const { mutate, isLoading: updateEmailDomainsLoading } =
+        useUpdateAllowedEmailDomains();
+    const isLoading =
+        updateEmailDomainsLoading || emailDomainsLoading || isLoadingProjects;
+
+    const projectOptions = useMemo(() => {
+        return (projects || [])
+            .filter(({ type }) => type !== ProjectType.PREVIEW)
+            .map((item) => ({
+                value: item.projectUuid,
+                label: item.name,
+            }));
+    }, [projects]);
+
+    const selectedEmailDomains = methods.watch('emailDomains', []);
+    const selectedRole = methods.watch('role', OrganizationMemberRole.VIEWER);
+
+    useEffect(() => {
+        if (data) {
+            methods.setValue('emailDomains', data.emailDomains);
+            methods.setValue('role', data.role);
+            methods.setValue(
+                'projects',
+                projectOptions.filter(({ value }) =>
+                    data.projectUuids.includes(value),
+                ),
+            );
+        }
+    }, [data, methods, projectOptions]);
+
+    const handleUpdate = (values: FormData) => {
+        const role =
+            values.emailDomains.length > 0
+                ? values.role
+                : OrganizationMemberRole.VIEWER;
+        const projectUuids =
+            role === OrganizationMemberRole.MEMBER
+                ? values.projects.map(({ value }) => value)
+                : [];
+        mutate({
+            emailDomains: values.emailDomains,
+            role,
+            projectUuids,
+        });
+    };
+
+    return (
+        <FormWrapper>
+            <Form
+                name="login"
+                methods={methods}
+                onSubmit={handleUpdate}
+                disableSubmitOnEnter
+                isLoading={isLoading}
+            >
+                <TagInput
+                    label="Allowed email domains"
+                    name="emailDomains"
+                    placeholder="E.g. lightdash.com"
+                    disabled={isLoading}
+                    defaultValue={data?.emailDomains || []}
+                    rules={{
+                        validate: {
+                            isValidEmailDomain:
+                                isValidEmailDomain('Email domains'),
+                        },
+                    }}
+                />
+                {selectedEmailDomains.length > 0 && (
+                    <>
+                        <Select2
+                            label="Default role"
+                            name="role"
+                            placeholder="Organisation viewer"
+                            disabled={isLoading}
+                            items={roleOptions}
+                            defaultValue="viewer"
+                        />
+                        {projectOptions.length > 0 &&
+                            selectedRole === OrganizationMemberRole.MEMBER && (
+                                <MultiSelect
+                                    label="Project Viewer Access"
+                                    name="projects"
+                                    items={projectOptions}
+                                    placeholder="Select projects"
+                                />
+                            )}
+                    </>
+                )}
+                <div style={{ flex: 1 }} />
+                <Button
+                    style={{ alignSelf: 'flex-end', marginTop: 20 }}
+                    intent={Intent.PRIMARY}
+                    text="Update"
+                    loading={isLoading}
+                    type="submit"
+                />
+            </Form>
+        </FormWrapper>
+    );
+};
+
+export default AllowedDomainsPanel;
