2d67e8a6c87bf004827499494bd9672a290cf618
jnfrati <nicofrati@gmail.com>
2023-03-17 12:03:49 -0300

    feat(frontend): add tooltip to ResourceViewGrid[Chart|Dashboard]Item

--- packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridChartItem.tsx
+++ packages/frontend/src/components/common/ResourceView/ResourceViewGrid/ResourceViewGridChartItem.tsx
@@ -5,6 +5,7 @@ import {
     Group,
     Paper,
     Text,
+    Tooltip,
     useMantineTheme,
 } from '@mantine/core';
 import { useDisclosure, useHover } from '@mantine/hooks';
@@ -30,66 +31,87 @@ const ResourceViewGridChartItem: FC<ResourceViewGridChartItemProps> = ({
     const theme = useMantineTheme();
 
     return (
-        <Paper
-            ref={ref}
-            component={Flex}
-            direction="column"
-            p={0}
-            withBorder
-            bg={hovered ? theme.fn.rgba(theme.colors.gray[0], 0.5) : undefined}
-            h="100%"
+        <Tooltip
+            opened={opened || hovered}
+            label={item.data.description}
+            withArrow
+            position="top"
+            styles={{
+                tooltip: { backgroundColor: theme.colors.dark[6] },
+                arrow: { backgroundColor: theme.colors.dark[6] },
+            }}
         >
-            <Group
-                p="md"
-                align="center"
-                spacing="md"
-                noWrap
-                sx={{
-                    flexGrow: 1,
-                    borderBottomWidth: 1,
-                    borderBottomStyle: 'solid',
-                    borderBottomColor: theme.colors.gray[3],
-                }}
+            <Paper
+                ref={ref}
+                component={Flex}
+                direction="column"
+                p={0}
+                withBorder
+                bg={
+                    hovered
+                        ? theme.fn.rgba(theme.colors.gray[0], 0.5)
+                        : undefined
+                }
+                h="100%"
             >
-                <ResourceIcon item={item} />
-
-                <Text lineClamp={2} fz="sm" fw={600}>
-                    {item.data.name}
-                </Text>
-            </Group>
-
-            <Flex pl="md" pr="xs" h={32} justify="space-between" align="center">
-                <Flex align="center" gap={4}>
-                    <IconEye color={theme.colors.gray[6]} size={14} />
-
-                    <Text size={14} color="gray.6" fz="xs">
-                        {item.data.views} views
-                    </Text>
-                </Flex>
-
-                <Box
+                <Group
+                    p="md"
+                    align="center"
+                    spacing="md"
+                    noWrap
                     sx={{
-                        flexGrow: 0,
-                        flexShrink: 0,
-                        transition: 'opacity 0.2s',
-                        opacity: hovered || opened ? 1 : 0,
-                    }}
-                    component="div"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        e.preventDefault();
+                        flexGrow: 1,
+                        borderBottomWidth: 1,
+                        borderBottomStyle: 'solid',
+                        borderBottomColor: theme.colors.gray[3],
                     }}
                 >
-                    <ResourceViewActionMenu
-                        item={item}
-                        isOpen={opened}
-                        onOpen={handlers.open}
-                        onClose={handlers.close}
-                        onAction={onAction}
-                    />
-                </Box>
-            </Flex>
-        </Paper>
+                    <ResourceIcon item={item} />
+
+                    <Text lineClamp={2} fz="sm" fw={600}>
+                        {item.data.name}
+                    </Text>
+                </Group>
+
+                <Flex
+                    pl="md"
+                    pr="xs"
+                    h={32}
+                    justify="space-between"
+                    align="center"
+                >
+                    <Flex align="center" gap={4}>
+                        <IconEye color={theme.colors.gray[6]} size={14} />
+
+                        <Text size={14} color="gray.6" fz="xs">
+                            {item.data.views} views
+                        </Text>
+                    </Flex>
+
+                    <Box
+                        sx={{
+                            flexGrow: 0,
+                            flexShrink: 0,
+                            transition: 'opacity 0.2s',
+                            opacity: hovered || opened ? 1 : 0,
+                        }}
+                        component="div"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                        }}
+                    >
+                        <ResourceViewActionMenu
+                            item={item}
+                            isOpen={opened}
+                            onOpen={handlers.open}
+                            onClose={handlers.close}
+                            onAction={onAction}
+                        />
+                    </Box>
+                </Flex>
+            </Paper>
+        </Tooltip>
     );
 };
 
