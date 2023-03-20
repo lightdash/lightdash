5c526f81d2293b75bae043bf260679514112b01d
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-03-20 22:19:35 +0400

    style: mantine icon

---
+++ packages/frontend/src/components/common/MantineIcon/index.tsx
@@ -0,0 +1,37 @@
+import {
+    MantineColor,
+    MantineNumberSize,
+    useMantineTheme,
+} from '@mantine/core';
+import { TablerIconsProps } from '@tabler/icons-react';
+import { FC } from 'react';
+
+interface MantineIconProps extends TablerIconsProps {
+    icon: (props: TablerIconsProps) => JSX.Element;
+    size?: MantineNumberSize;
+    stroke?: MantineNumberSize;
+    color?: MantineColor;
+    fill?: MantineColor;
+}
+
+const MantineIcon: FC<MantineIconProps> = ({
+    icon: TablerIcon,
+    size,
+    stroke,
+    color,
+    fill,
+    ...rest
+}) => {
+    const theme = useMantineTheme();
+
+    const mantineOverridedProps = {
+        size: typeof size === 'string' ? theme.spacing[size] : size,
+        stroke: typeof stroke === 'string' ? theme.spacing[stroke] : stroke,
+        color: color ? theme.fn.themeColor(color) : undefined,
+        fill: fill ? theme.fn.themeColor(fill) : undefined,
+    };
+
+    return <TablerIcon {...mantineOverridedProps} {...rest} />;
+};
+
+export default MantineIcon;
