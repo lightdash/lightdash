cc289a5caaea4d0f92c738601b5189b6c618cab0
yasmineabdelkefi <yasmine@lightdash.com>
2023-03-22 16:11:36 +0100

    fix: updating e2e tests to match new Mantine components

--- packages/e2e/cypress/e2e/login.cy.ts
+++ packages/e2e/cypress/e2e/login.cy.ts
@@ -10,28 +10,28 @@ describe('Login', () => {
     it('Should login successfully', () => {
         cy.logout();
         cy.visit('/login');
-        cy.findByLabelText('Email address *').type(
+        cy.findByPlaceholderText('Your email address').type(
             SEED_ORG_1_ADMIN_EMAIL.email,
         );
-        cy.findByLabelText('Password *').type(
+        cy.findByPlaceholderText('Your password').type(
             SEED_ORG_1_ADMIN_PASSWORD.password,
         );
-        cy.get('[data-cy="login-button"]').click();
+        cy.get('form').findByText('Sign in').click();
         cy.url().should('include', '/home');
     });
     it('Should display error message when credentials are invalid', () => {
         cy.logout();
         cy.visit('/login');
-        cy.findByLabelText('Email address *').type('test-email');
-        cy.findByLabelText('Password *').type('test-password');
-        cy.get('[data-cy="login-button"]').click();
+        cy.findByPlaceholderText('Your email address').type('test-email');
+        cy.findByPlaceholderText('Your password').type('test-password');
+        cy.get('form').findByText('Sign in').click();
         cy.findByText('Email and password not recognized').should('be.visible');
     });
     it('Should display error message when one field is empty', () => {
         cy.logout();
         cy.visit('/login');
-        cy.findByLabelText('Email address *').type('test-email');
-        cy.get('[data-cy="login-button"]').click();
+        cy.findByPlaceholderText('Your email address').type('test-email');
+        cy.get('form').findByText('Sign in').click();
         cy.findByText('Required field').should('be.visible');
     });
 });
