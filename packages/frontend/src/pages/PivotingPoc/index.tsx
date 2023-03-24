23a9fffa04c2845d4092793609666c775c4cd8d6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-03-24 15:33:21 +0400

    chore: add more samples

--- packages/frontend/src/pages/PivotingPoc/index.tsx
+++ packages/frontend/src/pages/PivotingPoc/index.tsx
@@ -1,5 +1,5 @@
 import { FieldType, PivotData } from '@lightdash/common';
-import { createStyles, Table } from '@mantine/core';
+import { createStyles, Divider, Stack, Table, Title } from '@mantine/core';
 import { lastIndexOf } from 'lodash-es';
 import { FC, useMemo } from 'react';
 import { DIMENSIONS_DUMMY_DATA } from './dummy_data/dimensions';
@@ -9,7 +9,6 @@ const range = (end: number) => {
     return Array.from({ length: end }, (_, i) => i);
 };
 
-// without pivoting
 const test1: PivotData = {
     headerValueTypes: [
         {
@@ -75,6 +74,54 @@ const test1: PivotData = {
         ['coupon', 20, 4, 24.5, 2, 25.67, 3, 9, 2, 12, 2],
         ['credit_card', 14.3, 9, 23.73, 10, 20.62, 12, 19, 10, 18.91, 10],
         ['gift_card', 13.5, 2, 29, 4, 12, 2, 15.5, 2, 25.5, 2],
+    ],
+
+    rowTotals: [],
+    columnTotals: [],
+};
+
+const test2: PivotData = {
+    headerValueTypes: [
+        {
+            type: FieldType.METRIC,
+        },
+    ],
+    headerValues: [['Average Order Size', 'Unique Orders Count']],
+
+    columnTypes: [
+        {
+            type: FieldType.DIMENSION,
+            fieldId: 'orders_status',
+        },
+        {
+            type: FieldType.DIMENSION,
+            fieldId: 'payments_payment_method',
+        },
+        { type: 'value' },
+        { type: 'value' },
+    ],
+
+    rows: [
+        ['completed', 'bank_transfer', 14.86, 7],
+        ['completed', 'coupon', 20, 4],
+        ['completed', 'credit_card', 14.3, 9],
+        ['completed', 'gift_card', 13.5, 2],
+        ['placed', 'bank_transfer', 9.25, 8],
+        ['placed', 'coupon', 24.5, 2],
+        ['placed', 'credit_card', 23.73, 10],
+        ['placed', 'gift_card', 29, 4],
+        ['return_pending', 'bank_transfer', 27.25, 4],
+        ['return_pending', 'coupon', 25.67, 3],
+        ['return_pending', 'credit_card', 20.62, 12],
+        ['return_pending', 'gift_card', 12, 2],
+        ['returned', 'bank_transfer', 13.71, 7],
+        ['returned', 'coupon', 9, 2],
+        ['returned', 'credit_card', 19, 10],
+        ['returned', 'gift_card', 15.5, 2],
+        ['shipped', 'bank_transfer', 19, 7],
+        ['shipped', 'coupon', 12, 2],
+        ['shipped', 'credit_card', 18.91, 10],
+        ['shipped', 'gift_card', 25.5, 2],
     ],
 
     rowTotals: [],
@@ -165,9 +212,17 @@ const RenderTable: FC<{ data: PivotData }> = ({ data }) => {
 
 const PivotingPOC = () => {
     return (
-        <>
-            <RenderTable data={test1} />
-        </>
+        <Stack spacing="lg">
+            <Stack spacing="sm">
+                <Title order={3}>Test 1</Title>
+                <RenderTable data={test1} />
+            </Stack>
+
+            <Stack spacing="sm">
+                <Title order={3}>Test 2</Title>
+                <RenderTable data={test2} />
+            </Stack>
+        </Stack>
     );
 };
 
