477bf26aef6e6e6999b21f487e19cccaf9d113a3
oliverlaslett <olaslett@gmail.com>
2023-03-28 10:11:25 +0100

    add tests

--- packages/frontend/src/hooks/pivotTable/pivotQueryResults.mock.ts
+++ packages/frontend/src/hooks/pivotTable/pivotQueryResults.mock.ts
@@ -1,6 +1,6 @@
 import { MetricQuery, ResultRow } from '@lightdash/common';
 
-export const METRIC_QUERY: Pick<
+export const METRIC_QUERY_2DIM_2METRIC: Pick<
     MetricQuery,
     'metrics' | 'dimensions' | 'tableCalculations' | 'additionalMetrics'
 > = {
@@ -9,7 +9,7 @@ export const METRIC_QUERY: Pick<
     tableCalculations: [],
 };
 
-export const RESULT_ROWS: ResultRow[] = [
+export const RESULT_ROWS_2DIM_2METRIC: ResultRow[] = [
     {
         page: { value: { raw: '/home', formatted: '/home' } },
         site: { value: { raw: 'blog', formatted: 'Blog' } },
@@ -39,5 +39,32 @@ export const RESULT_ROWS: ResultRow[] = [
         site: { value: { raw: 'docs', formatted: 'Docs' } },
         views: { value: { raw: 2, formatted: '2.0' } },
         devices: { value: { raw: 13, formatted: '13.0' } },
+    },
+];
+
+export const METRIC_QUERY_1DIM_2METRIC: Pick<
+    MetricQuery,
+    'metrics' | 'dimensions' | 'tableCalculations' | 'additionalMetrics'
+> = {
+    metrics: ['views', 'devices'],
+    dimensions: ['page'],
+    tableCalculations: [],
+};
+
+export const RESULT_ROWS_1DIM_2METRIC: ResultRow[] = [
+    {
+        page: { value: { raw: '/home', formatted: '/home' } },
+        views: { value: { raw: 6, formatted: '6.0' } },
+        devices: { value: { raw: 7, formatted: '7.0' } },
+    },
+    {
+        page: { value: { raw: '/about', formatted: '/about' } },
+        views: { value: { raw: 12, formatted: '12.0' } },
+        devices: { value: { raw: 0, formatted: '0.0' } },
+    },
+    {
+        page: { value: { raw: '/first-post', formatted: '/first-post' } },
+        views: { value: { raw: 11, formatted: '11.0' } },
+        devices: { value: { raw: 1, formatted: '1.0' } },
     },
 ];
