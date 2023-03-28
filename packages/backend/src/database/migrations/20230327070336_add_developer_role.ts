98774b4cdb713b9e88e8bb7142d2e6a27b74e704
Rephus <rephus@gmail.com>
2023-03-28 16:05:03 +0200

    migrate edtiros to developers

--- packages/backend/src/database/migrations/20230327070336_add_developer_role.ts
+++ packages/backend/src/database/migrations/20230327070336_add_developer_role.ts
@@ -1,8 +1,12 @@
 import { Knex } from 'knex';
 
+const organizationMembershipsTableName = 'organization_memberships';
 const organizationMembershipRolesTableName = 'organization_membership_roles';
 const projectMembershipRolesTableName = 'project_membership_roles';
+const projectMembershipsTableName = 'organization_memberships';
+
 const developerRole = 'developer';
+const editorRole = 'editor';
 
 export async function up(knex: Knex): Promise<void> {
     await knex(projectMembershipRolesTableName).insert({
@@ -11,9 +15,23 @@ export async function up(knex: Knex): Promise<void> {
     await knex(organizationMembershipRolesTableName).insert({
         role: developerRole,
     });
+
+    await knex(projectMembershipsTableName)
+        .update('role', developerRole)
+        .where('role', editorRole);
+    await knex(organizationMembershipsTableName)
+        .update('role', developerRole)
+        .where('role', editorRole);
 }
 
 export async function down(knex: Knex): Promise<void> {
+    await knex(projectMembershipsTableName)
+        .update('role', editorRole)
+        .where('role', developerRole);
+    await knex(organizationMembershipsTableName)
+        .update('role', editorRole)
+        .where('role', developerRole);
+
     await knex(projectMembershipRolesTableName)
         .where('role', developerRole)
         .delete();
