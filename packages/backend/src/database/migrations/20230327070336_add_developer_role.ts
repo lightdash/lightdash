10226dabf74e99e6b6acdd742b6a1608b71f1bbe
Rephus <rephus@gmail.com>
2023-03-28 18:03:38 +0200

    split migrations

--- packages/backend/src/database/migrations/20230327070336_add_developer_role.ts
+++ packages/backend/src/database/migrations/20230327070336_add_developer_role.ts
@@ -1,9 +1,7 @@
 import { Knex } from 'knex';
 
-const organizationMembershipsTableName = 'organization_memberships';
 const organizationMembershipRolesTableName = 'organization_membership_roles';
 const projectMembershipRolesTableName = 'project_membership_roles';
-const projectMembershipsTableName = 'organization_memberships';
 
 const developerRole = 'developer';
 const editorRole = 'editor';
@@ -15,23 +13,9 @@ export async function up(knex: Knex): Promise<void> {
     await knex(organizationMembershipRolesTableName).insert({
         role: developerRole,
     });
-
-    await knex(projectMembershipsTableName)
-        .update('role', developerRole)
-        .where('role', editorRole);
-    await knex(organizationMembershipsTableName)
-        .update('role', developerRole)
-        .where('role', editorRole);
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex(projectMembershipsTableName)
-        .update('role', editorRole)
-        .where('role', developerRole);
-    await knex(organizationMembershipsTableName)
-        .update('role', editorRole)
-        .where('role', developerRole);
-
     await knex(projectMembershipRolesTableName)
         .where('role', developerRole)
         .delete();
