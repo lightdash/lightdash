8a804d88dcd1e733cb1615d3f58b525c0730c241
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-06 04:27:57 +0400

    fix: minor

--- packages/frontend/src/components/ExportCSV/downloadCsv.ts
+++ packages/frontend/src/components/ExportCSV/downloadCsv.ts
@@ -1,10 +1,8 @@
-export default function downloadCSV(url: string, filename?: string) {
+export default function downloadCSV(url: string, filename: string = '') {
     // Create a temporary anchor element
     const anchor = document.createElement('a');
     anchor.href = url;
-    if (filename) {
-        anchor.download = filename;
-    }
+    anchor.download = filename;
     anchor.style.display = 'none';
 
     // Add the anchor to the DOM, trigger the click event, and remove it
