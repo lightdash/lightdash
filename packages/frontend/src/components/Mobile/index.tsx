8b984c76e7bdc1f102da8f6ba007e934b96a5171
Lokeswaran Aruljothi <74011196+lokeswaran-aj@users.noreply.github.com>
2023-04-13 22:23:34 +0530

    fix: Update the app screen when you're on mobile (#5054)
    * fix(client): update the logo to black version
    
    * fix(client): update text styles
    
    * fix(client): add button linking to home
    
    * fix(client): add link to community text
    
    * fix: removing hardcoded values for themed spacings, migrating to Mantine
    
    ---------
    
    Co-authored-by: yasmineabdelkefi <yasmine@lightdash.com>

--- packages/frontend/src/components/Mobile/index.tsx
+++ packages/frontend/src/components/Mobile/index.tsx
@@ -1,35 +1,41 @@
-import { AnchorButton } from '@blueprintjs/core';
-import React from 'react';
-import {
-    Content,
-    DarkLogo,
-    Icon,
-    MobileFooter,
-    MobileViewWrapper,
-    Text,
-} from './Mobile.styles';
+import { Anchor, Box, Image, Stack, Text, Title } from '@mantine/core';
+import LightdashLogo from '../../svgs/lightdash-black.svg';
+import MantineLinkButton from '../common/MantineLinkButton';
 
 const MobileView = () => (
-    <MobileViewWrapper>
-        <Content>
-            <DarkLogo title="Home" />
-            <Icon>&#128586;</Icon>
-            <Text>Lightdash currently works best on bigger screens.</Text>
-            <Text>Sign in on a laptop or desktop to get started!</Text>
-        </Content>
-
-        <MobileFooter>
-            <AnchorButton
-                href="https://github.com/lightdash/lightdash/discussions"
-                target="_blank"
-                minimal
-                icon="chat"
-                style={{ margin: '0' }}
+    <Box w="100vw" h="100vh" sx={{ background: '#ebf1f5' }}>
+        <Stack align="center" spacing="xl" justify="start" p="5xl">
+            <Image src={LightdashLogo} alt="lightdash logo" maw="8xl" my="lg" />
+            <Box
+                component="span"
+                sx={{
+                    fontSize: '2.5rem',
+                    display: 'block',
+                }}
             >
-                Join the conversation!
-            </AnchorButton>
-        </MobileFooter>
-    </MobileViewWrapper>
+                &#128586;
+            </Box>
+            <Title ta="center" order={4}>
+                Lightdash currently works best on bigger screens.
+            </Title>
+            <Text ta="center" color="gray.6">
+                Sign in on a laptop or desktop to get started! In the meantime:
+            </Text>
+            <MantineLinkButton
+                href="https://www.lightdash.com/"
+                target="_blank"
+            >
+                Check out our website
+            </MantineLinkButton>
+            <Anchor
+                href="https://join.slack.com/t/lightdash-community/shared_invite/zt-16q953ork-NZr1qdEqxSwB17E2ckUe7A"
+                target="_blank"
+                span
+            >
+                ...or join our community!
+            </Anchor>
+        </Stack>
+    </Box>
 );
 
 export default MobileView;
