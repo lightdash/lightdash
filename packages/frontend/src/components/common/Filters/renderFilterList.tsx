9092f3dba82f9ec5e283ca2e500f92fd7e7c4316
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-14 13:38:28 +0400

    feat: add dashboard filter dropdown group header stickiness (#5088)
    * feat: add dashboard filter dropdown group header stickyness
    
    * style: minor improvements
    
    * chore: remove debug index
    
    * chore: add padding constant

--- packages/frontend/src/components/common/Filters/renderFilterList.tsx
+++ packages/frontend/src/components/common/Filters/renderFilterList.tsx
@@ -1,13 +1,55 @@
-import { Colors, Menu, MenuDivider } from '@blueprintjs/core';
+import { Colors, Divider, Menu } from '@blueprintjs/core';
 import { ItemListRendererProps } from '@blueprintjs/select';
 import { Field, getItemTableName, TableCalculation } from '@lightdash/common';
-import React from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
 
 const TableName = styled.span`
     font-weight: 600;
     color: ${Colors.GRAY2};
 `;
+
+type MenuDividerProps = {
+    $isFirst: boolean;
+};
+
+const Y_PADDING = 5;
+const STICK_HEADER_WITHOUT_DIVIDER = -Y_PADDING;
+const STICK_HEADER_WITH_DIVIDER = -Y_PADDING - 1;
+
+const MenuDivider = styled.li<MenuDividerProps>`
+    position: sticky;
+
+    top: ${({ $isFirst }) =>
+        $isFirst ? STICK_HEADER_WITHOUT_DIVIDER : STICK_HEADER_WITH_DIVIDER}px;
+
+    z-index: 1;
+    background: ${Colors.WHITE};
+    margin: 0;
+    padding: ${({ $isFirst }) => ($isFirst ? Y_PADDING : 0)}px 7px
+        ${Y_PADDING}px 7px;
+    margin-top: ${({ $isFirst }) => ($isFirst ? 0 : Y_PADDING)}px;
+`;
+
+const StyledDivider = styled(Divider)`
+    margin: 0;
+    margin-bottom: 5px;
+`;
+
+type StickyMenuDividerProps = {
+    index: number;
+    title: string;
+};
+
+const StickyMenuDivider: FC<StickyMenuDividerProps> = ({ index, title }) => {
+    return (
+        <MenuDivider $isFirst={index === 0}>
+            {index !== 0 && <StyledDivider />}
+
+            <TableName>{title}</TableName>
+        </MenuDivider>
+    );
+};
 
 const renderFilterList = <T extends Field | TableCalculation>({
     items,
@@ -16,35 +58,33 @@ const renderFilterList = <T extends Field | TableCalculation>({
     renderItem,
 }: ItemListRendererProps<T>) => {
     const getGroupedItems = (filteredItems: typeof items) => {
-        return filteredItems.reduce<
-            {
-                group: string;
-                index: number;
-                items: typeof items;
-                key: number;
-            }[]
-        >((acc, item, index) => {
-            const group = getItemTableName(item);
+        return filteredItems.reduce<{ group: string; items: typeof items }[]>(
+            (acc, item) => {
+                const group = getItemTableName(item);
 
-            const lastGroup = acc.at(-1);
-            if (lastGroup && lastGroup.group === group) {
-                lastGroup.items.push(item);
-            } else {
-                acc.push({ group, index, items: [item], key: index });
-            }
-            return acc;
-        }, []);
+                const lastGroup = acc.at(-1);
+                if (lastGroup && lastGroup.group === group) {
+                    lastGroup.items.push(item);
+                } else {
+                    acc.push({ group, items: [item] });
+                }
+                return acc;
+            },
+            [],
+        );
     };
 
     return (
         <Menu role="listbox" ulRef={itemsParentRef}>
-            {getGroupedItems(items).map((groupedItem) => (
-                <React.Fragment key={groupedItem.key}>
-                    <MenuDivider
-                        title={<TableName>{groupedItem.group}</TableName>}
+            {getGroupedItems(items).map((groupedItem, index) => (
+                <React.Fragment key={index}>
+                    <StickyMenuDivider
+                        index={index}
+                        title={groupedItem.group}
                     />
-                    {groupedItem.items.map((item, index) =>
-                        renderItem(item, groupedItem.index + index),
+
+                    {groupedItem.items.map((item, itemIndex) =>
+                        renderItem(item, index + itemIndex),
                     )}
                 </React.Fragment>
             ))}
