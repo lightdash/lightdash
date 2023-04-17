39bd13c0b2f9623f1c093b6d1364fef2116333c7
Joao Viana <joao@lightdash.com>
2023-04-17 16:52:16 +0100

    fix: redirect user to active or first default project uuid

--- packages/frontend/src/pages/Projects.tsx
+++ packages/frontend/src/pages/Projects.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import { FC } from 'react';
+import { Redirect } from 'react-router-dom';
 import ErrorState from '../components/common/ErrorState';
 import PageSpinner from '../components/PageSpinner';
-import { getLastProject, useProjects } from '../hooks/useProjects';
+import { useActiveProjectUuid } from '../hooks/useProject';
+import { useProjects } from '../hooks/useProjects';
 
 export const Projects: FC = () => {
-    const params = useParams<{ projectUuid: string | undefined }>();
     const { isLoading, data, error } = useProjects();
+    const activeProjectUuid = useActiveProjectUuid();
+
     if (isLoading) {
         return <PageSpinner />;
     }
@@ -17,18 +19,5 @@ export const Projects: FC = () => {
         return <Redirect to="/no-access" />;
     }
 
-    const availableProjectUuids = data.map(({ projectUuid }) => projectUuid);
-
-    const find = (projectUuid: string | undefined) => {
-        return projectUuid && availableProjectUuids.includes(projectUuid)
-            ? projectUuid
-            : undefined;
-    };
-    const lastProject = getLastProject();
-    const projectUuid =
-        find(params.projectUuid) ||
-        find(lastProject) ||
-        availableProjectUuids[0];
-
-    return <Redirect to={`/projects/${projectUuid}/home`} />;
+    return <Redirect to={`/projects/${activeProjectUuid}/home`} />;
 };
