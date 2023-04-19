e5209a4cdf55a0eb5a104afeef4d791033ba213a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-19 15:25:03 +0400

    fix: bignumber underlying data

--- packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
+++ packages/frontend/src/components/SimpleStatistic/BigNumberContextMenu.tsx
@@ -1,17 +1,17 @@
 import { Menu, Position } from '@blueprintjs/core';
 import { MenuItem2, Popover2, Popover2Props } from '@blueprintjs/popover2';
 import { subject } from '@casl/ability';
-import { Can } from '../common/Authorization';
-
-import { ResultRow } from '@lightdash/common';
+import mapValues from 'lodash-es/mapValues';
 import { FC, useCallback, useMemo } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useParams } from 'react-router-dom';
+
 import useToaster from '../../hooks/toaster/useToaster';
 import { useExplore } from '../../hooks/useExplore';
 import { useApp } from '../../providers/AppProvider';
 import { useTracking } from '../../providers/TrackingProvider';
 import { EventName } from '../../types/Events';
+import { Can } from '../common/Authorization';
 import { useVisualizationContext } from '../LightdashVisualization/VisualizationProvider';
 import DrillDownMenuItem from '../MetricQueryData/DrillDownMenuItem';
 import {
@@ -43,7 +43,7 @@ export const BigNumberContextMenu: FC<BigNumberContextMenuProps> = ({
     );
 
     const row: UnderlyingValueMap = useMemo(() => {
-        return resultsData?.rows?.[0].value?.value || {};
+        return mapValues(resultsData?.rows?.[0], (r) => r.value) ?? {};
     }, [resultsData]);
 
     const value = useMemo(() => {
