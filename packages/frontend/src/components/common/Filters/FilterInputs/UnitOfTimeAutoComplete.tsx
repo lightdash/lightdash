08a15a07fcba3b28de977d8dd99618664174df24
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-24 22:03:39 +0400

    chore: migrate UnitOfTimeAutoComplete

--- packages/frontend/src/components/common/Filters/FilterInputs/UnitOfTimeAutoComplete.tsx
+++ packages/frontend/src/components/common/Filters/FilterInputs/UnitOfTimeAutoComplete.tsx
@@ -1,186 +1,116 @@
-import { Button } from '@blueprintjs/core';
-import { MenuItem2, Popover2Props } from '@blueprintjs/popover2';
-import { ItemRenderer, Select2 } from '@blueprintjs/select';
 import { UnitOfTime } from '@lightdash/common';
-import { FC } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { Select } from '@mantine/core';
+import { FC, useMemo } from 'react';
 
 type UnitOfTimeOption = {
+    value: string;
     label: string;
     unitOfTime: UnitOfTime;
     completed: boolean;
-};
-
-type UnitOfTimeAutoCompleteProps = {
-    isTimestamp: boolean;
-    showCompletedOptions: boolean;
-    showOptionsInPlural: boolean;
 };
 
 const getUnitOfTimeLabel = (
     unitOfTime: UnitOfTime,
     isPlural: boolean,
     isCompleted: boolean,
-) =>
-    `${isCompleted ? 'completed ' : ''}${
+) => {
+    return `${isCompleted ? 'completed ' : ''}${
         isPlural ? unitOfTime : unitOfTime.substring(0, unitOfTime.length - 1)
     }`;
-const UnitOfTimeOptions = ({
-    isTimestamp,
-    showCompletedOptions,
-    showOptionsInPlural,
-}: UnitOfTimeAutoCompleteProps) => {
-    const dateIndex = Object.keys(UnitOfTime).indexOf(UnitOfTime.days);
-
-    // Filter unitTimes before Days if we are filtering Dates only
-    const unitsOfTime = isTimestamp
-        ? Object.values(UnitOfTime)
-        : Object.values(UnitOfTime).slice(dateIndex);
-
-    return unitsOfTime
-        .reverse()
-        .reduce<UnitOfTimeOption[]>((sum, unitOfTime) => {
-            const newOptions = [
-                ...sum,
-                {
-                    label: getUnitOfTimeLabel(
-                        unitOfTime,
-                        showOptionsInPlural,
-                        false,
-                    ),
-                    unitOfTime,
-                    completed: false,
-                },
-            ];
-
-            if (showCompletedOptions) {
-                newOptions.push({
-                    label: getUnitOfTimeLabel(
-                        unitOfTime,
-                        showOptionsInPlural,
-                        true,
-                    ),
-                    unitOfTime,
-                    completed: true,
-                });
-            }
-            return newOptions;
-        }, []);
 };
 
-const FieldSuggest = Select2.ofType<UnitOfTimeOption>();
-
-const AutocompleteMaxHeight = createGlobalStyle`
-  .autocomplete-max-height {
-    max-height: 400px;
-    overflow-y: auto;
-  }
-`;
-
-const renderItem: ItemRenderer<UnitOfTimeOption> = (
-    field,
-    { modifiers, handleClick },
-) => {
-    if (!modifiers.matchesPredicate) {
-        return null;
-    }
-    return (
-        <MenuItem2
-            active={modifiers.active}
-            key={`${field.completed}_${field.unitOfTime}`}
-            text={field.label}
-            onClick={handleClick}
-            shouldDismissPopover={false}
-        />
-    );
+const getUnitOfTimeValue = (unitOfTime: UnitOfTime, isCompleted: boolean) => {
+    return `${isCompleted ? 'completed-' : ''}${unitOfTime}`;
 };
 
 type Props = {
+    value: UnitOfTime;
+    disabled?: boolean;
     isTimestamp: boolean;
-    unitOfTime: UnitOfTime;
     showOptionsInPlural?: boolean;
     showCompletedOptions?: boolean;
     completed: boolean;
-    onChange: (value: UnitOfTimeOption) => void;
     onClosed?: () => void;
-    popoverProps?: Popover2Props;
-    disabled?: boolean;
+    onChange: (
+        value: Pick<UnitOfTimeOption, 'unitOfTime' | 'completed'>,
+    ) => void;
 };
 
 const UnitOfTimeAutoComplete: FC<Props> = ({
     isTimestamp,
-    unitOfTime,
+    completed,
+    value,
     showOptionsInPlural = true,
     showCompletedOptions = true,
-    completed,
     onChange,
     onClosed,
-    popoverProps,
     disabled,
-}) => (
-    <>
-        <AutocompleteMaxHeight />
-        <FieldSuggest
-            className={disabled ? 'disabled-filter' : ''}
-            disabled={disabled}
-            items={UnitOfTimeOptions({
-                isTimestamp,
-                showCompletedOptions,
-                showOptionsInPlural,
-            })}
-            itemsEqual={(value, other) =>
-                value.unitOfTime === other.unitOfTime &&
-                value.completed === other.completed
-            }
-            popoverProps={{
-                fill: true,
-                minimal: true,
-                onClosed,
-                popoverClassName: 'autocomplete-max-height',
-                ...popoverProps,
-            }}
-            itemRenderer={renderItem}
-            activeItem={{
-                label: getUnitOfTimeLabel(
-                    unitOfTime,
-                    showOptionsInPlural,
-                    completed,
-                ),
-                unitOfTime,
-                completed,
-            }}
-            noResults={<MenuItem2 disabled text="No results." />}
-            onItemSelect={onChange}
-            itemPredicate={(
-                query: string,
-                field: UnitOfTimeOption,
-                index?: undefined | number,
-                exactMatch?: undefined | false | true,
-            ) => {
-                if (exactMatch) {
-                    return query.toLowerCase() === field.label.toLowerCase();
+}) => {
+    const data = useMemo(() => {
+        const dateIndex = Object.keys(UnitOfTime).indexOf(UnitOfTime.days);
+
+        // Filter unitTimes before Days if we are filtering Dates only
+        const unitsOfTime = isTimestamp
+            ? Object.values(UnitOfTime)
+            : Object.values(UnitOfTime).slice(dateIndex);
+
+        return unitsOfTime
+            .reverse()
+            .reduce<UnitOfTimeOption[]>((sum, unitOfTime) => {
+                const newOptions = [
+                    ...sum,
+                    {
+                        value: getUnitOfTimeValue(unitOfTime, false),
+                        label: getUnitOfTimeLabel(
+                            unitOfTime,
+                            showOptionsInPlural,
+                            false,
+                        ),
+                        unitOfTime,
+                        completed: false,
+                    },
+                ];
+
+                if (showCompletedOptions) {
+                    newOptions.push({
+                        value: getUnitOfTimeValue(unitOfTime, true),
+                        label: getUnitOfTimeLabel(
+                            unitOfTime,
+                            showOptionsInPlural,
+                            true,
+                        ),
+                        unitOfTime,
+                        completed: true,
+                    });
                 }
-                return field.label.toLowerCase().includes(query.toLowerCase());
+                return newOptions;
+            }, []);
+    }, [isTimestamp, showCompletedOptions, showOptionsInPlural]);
+
+    return (
+        <Select
+            disabled={disabled}
+            searchable
+            nothingFound="No results..."
+            data={data}
+            value={getUnitOfTimeValue(value, completed)}
+            onChange={(newValue) => {
+                const unitOfTime = data.find(
+                    (u) =>
+                        getUnitOfTimeValue(u.unitOfTime, u.completed) ===
+                        newValue,
+                );
+
+                if (!unitOfTime) return;
+
+                onChange({
+                    unitOfTime: unitOfTime.unitOfTime,
+                    completed: unitOfTime.completed,
+                });
             }}
-        >
-            <Button
-                className={disabled ? 'disabled-filter' : ''}
-                disabled={disabled}
-                rightIcon="caret-down"
-                text={getUnitOfTimeLabel(
-                    unitOfTime,
-                    showOptionsInPlural,
-                    completed,
-                )}
-                fill
-                style={{
-                    display: 'inline-flex',
-                    justifyContent: 'space-between',
-                    whiteSpace: 'nowrap',
-                }}
-            />
-        </FieldSuggest>
-    </>
-);
+            onDropdownClose={onClosed}
+        />
+    );
+};
 
 export default UnitOfTimeAutoComplete;
