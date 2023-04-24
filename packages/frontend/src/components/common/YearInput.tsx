b7af1a94a167407e26875c2f334613329cddce0f
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-24 19:06:08 +0400

    chore: migrate year input

--- packages/frontend/src/components/common/YearInput.tsx
+++ packages/frontend/src/components/common/YearInput.tsx
@@ -1,6 +1,6 @@
-import { NumericInput } from '@blueprintjs/core';
+import { NumberInput } from '@mantine/core';
 import moment from 'moment';
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 type Props = {
     value: Date;
@@ -12,27 +12,13 @@ const YearInput: FC<Props> = ({ value, onChange, disabled }) => {
     const utcYearValue = moment(value).year();
 
     return (
-        <NumericInput
-            className={disabled ? 'disabled-filter' : ''}
+        <NumberInput
             disabled={disabled}
-            fill
-            max={9999}
             min={1000}
-            minLength={4}
-            maxLength={4}
+            max={9999}
             defaultValue={utcYearValue}
-            onValueChange={(year) => {
-                if (year > 1000 && year < 9999) {
-                    onChange(moment(value).year(year).toDate());
-                }
-            }}
-            onBlur={(e) => {
-                let year = parseInt(e.currentTarget.value, 10);
-                if (year < 1000) {
-                    year = 1000;
-                } else if (year > 9999) {
-                    year = 9999;
-                }
+            onChange={(year) => {
+                if (year === '') return;
                 onChange(moment(value).year(year).toDate());
             }}
         />
