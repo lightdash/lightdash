830e99a73ac09ea21db7737442e9b2979cef451d
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-24 19:26:32 +0400

    chore: migrate FieldLabel

--- packages/frontend/src/components/common/Filters/FieldLabel.tsx
+++ packages/frontend/src/components/common/Filters/FieldLabel.tsx
@@ -5,27 +5,24 @@ import {
     isField,
     TableCalculation,
 } from '@lightdash/common';
+import { Text } from '@mantine/core';
 import { FC } from 'react';
-import styled from 'styled-components';
 
 interface FieldLabelProps {
     item: Field | TableCalculation | AdditionalMetric;
 }
 
-const BolderText = styled.span`
-    font-weight: 600;
-`;
-
 const FieldLabel: FC<FieldLabelProps> = ({ item }) => {
     return (
-        <span>
+        <Text component="span">
             {isField(item) ? `${item.tableLabel} ` : ''}
-            <BolderText>
+
+            <Text component="span" fw={600}>
                 {isField(item) || isAdditionalMetric(item)
                     ? item.label
                     : item.displayName}
-            </BolderText>
-        </span>
+            </Text>
+        </Text>
     );
 };
 
