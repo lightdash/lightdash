79af28376342d4c89a5715f4cf2425b836d05a89
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-24 18:07:42 +0400

    fix: e2e test with sql runner and csv download (#5174)
    * fix: e2e test with sql runner and csv download
    
    * chore: add timeout
    
    * capture and retry
    
    * fix page timeout
    
    * fix popover button on results
    
    * run all
    
    * with icon export
    
    ---------
    
    Co-authored-by: Rephus <rephus@gmail.com>

--- packages/e2e/cypress/e2e/downloadCsv.cy.ts
+++ packages/e2e/cypress/e2e/downloadCsv.cy.ts
@@ -68,111 +68,141 @@ describe('Download CSV on SQL Runner', () => {
 describe('Download CSV on Dashboards', () => {
     beforeEach(() => {
         cy.login();
-        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/dashboards`);
-    });
 
-    it('Should download a CSV from dashboard', () => {
-        const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/payments/downloadCsv`;
-        cy.intercept({
-            method: 'POST',
-            url: downloadUrl,
-        }).as('apiDownloadCsv');
+        cy.on('url:changed', (newUrl) => {
+            if (newUrl.includes('.csv')) {
+                window.location.href = '/';
+            }
+        });
 
-        // wiat for the dashboard to load
-        cy.findByText('Loading dashboards').should('not.exist');
-
-        cy.contains('a', 'Jaffle dashboard').click();
-
-        cy.findAllByText('Loading chart').should('have.length', 0); // Finish loading
-
-        cy.findAllByText('No chart available').should('have.length', 0);
-        cy.findAllByText('No data available').should('have.length', 0);
-
-        cy.get('thead th').should('have.length', 6); // Table chart
-        cy.contains('Days since').trigger('mouseenter');
-
-        cy.get('[icon="more"]').click();
-        cy.findByText('Export CSV').click();
-        cy.get('button').contains('Export CSV').click();
-
-        cy.wait('@apiDownloadCsv').then((interception) => {
-            expect(interception?.response?.statusCode).to.eq(200);
-
-            expect(interception?.response?.body.results).to.have.property(
-                'jobId',
-            );
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/dashboards`, {
+            timeout: 60000,
         });
     });
+
+    it(
+        'Should download a CSV from dashboard',
+        { retries: 3, pageLoadTimeout: 1000 },
+        () => {
+            const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/payments/downloadCsv`;
+            cy.intercept({
+                method: 'POST',
+                url: downloadUrl,
+            }).as('apiDownloadCsv');
+
+            // wiat for the dashboard to load
+            cy.findByText('Loading dashboards').should('not.exist');
+
+            cy.contains('a', 'Jaffle dashboard').click();
+
+            cy.findAllByText('Loading chart').should('have.length', 0); // Finish loading
+
+            cy.findAllByText('No chart available').should('have.length', 0);
+            cy.findAllByText('No data available').should('have.length', 0);
+
+            cy.get('thead th').should('have.length', 6); // Table chart
+            cy.contains('Days since').trigger('mouseenter');
+
+            cy.get('[icon="more"]').click();
+            cy.findByText('Export CSV').click();
+            cy.get('button').contains('Export CSV').click();
+
+            cy.wait('@apiDownloadCsv').then((interception) => {
+                expect(interception?.response?.statusCode).to.eq(200);
+
+                expect(interception?.response?.body.results).to.have.property(
+                    'jobId',
+                );
+            });
+        },
+    );
 });
 
 describe('Download CSV on Explore', () => {
     beforeEach(() => {
         cy.login();
-        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables`);
-    });
 
-    it('Should download CSV from results on Explore', () => {
-        const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/orders/downloadCsv`;
-        cy.intercept({
-            method: 'POST',
-            url: downloadUrl,
-        }).as('apiDownloadCsv');
-
-        // choose table and select fields
-        cy.findByText('Orders').click();
-        cy.findByText('First name').click();
-        cy.findByText('Unique order count').click();
-
-        // run query
-        cy.get('button').contains('Run query').click();
-
-        // wait for the chart to finish loading
-        cy.findByText('Loading chart').should('not.exist');
-
-        cy.findByText('Export CSV').click();
-        cy.get('.bp4-popover2').contains('button', 'Export CSV').click();
-
-        cy.wait('@apiDownloadCsv').then((interception) => {
-            expect(interception?.response?.statusCode).to.eq(200);
-            expect(interception?.response?.body.results).to.have.property(
-                'jobId',
-            );
+        cy.on('url:changed', (newUrl) => {
+            if (newUrl.includes('.csv')) {
+                window.location.href = '/';
+            }
+        });
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables`, {
+            timeout: 60000,
         });
     });
-    it('Should download CSV from table chart on Explore', () => {
-        const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/orders/downloadCsv`;
-        cy.intercept({
-            method: 'POST',
-            url: downloadUrl,
-        }).as('apiDownloadCsv');
-        // choose table and select fields
-        cy.findByText('Orders').click();
-        cy.findByText('First name').click();
-        cy.findByText('Unique order count').click();
 
-        // run query
-        cy.get('button').contains('Run query').click();
+    it(
+        'Should download CSV from results on Explore',
+        { retries: 3, pageLoadTimeout: 1000 },
+        () => {
+            const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/orders/downloadCsv`;
+            cy.intercept({
+                method: 'POST',
+                url: downloadUrl,
+            }).as('apiDownloadCsv');
+            // choose table and select fields
+            cy.findByText('Orders').click();
+            cy.findByText('First name').click();
+            cy.findByText('Unique order count').click();
 
-        // open chart
-        cy.findByText('Charts').parent().findByRole('button').click();
-        // Close results
-        cy.findByText('Results').parent().findByRole('button').click();
-        // wait for the chart to finish loading
-        cy.findByText('Loading chart').should('not.exist');
+            // run query
+            cy.get('button').contains('Run query').click();
 
-        // open chart menu and change chart type to Table
-        cy.get('button').contains('Bar chart').click();
-        cy.get('[role="menuitem"]').contains('Table').click();
+            // wait for the chart to finish loading
+            cy.findByText('Loading results').should('not.exist');
 
-        cy.findByText('Export CSV').click();
-        cy.get('.bp4-popover2').contains('button', 'Export CSV').click();
+            cy.findByText('Export CSV').click();
+            cy.get('[icon="export"]').click();
 
-        cy.wait('@apiDownloadCsv').then((interception) => {
-            expect(interception?.response?.statusCode).to.eq(200);
-
-            expect(interception?.response?.body.results).to.have.property(
-                'jobId',
+            cy.wait('@apiDownloadCsv', { timeout: 3000 }).then(
+                (interception) => {
+                    expect(interception?.response?.statusCode).to.eq(200);
+                    expect(
+                        interception?.response?.body.results,
+                    ).to.have.property('jobId');
+                },
             );
-        });
-    });
+        },
+    );
+    it(
+        'Should download CSV from table chart on Explore',
+        { retries: 3, pageLoadTimeout: 1000 },
+        () => {
+            const downloadUrl = `/api/v1/projects/${SEED_PROJECT.project_uuid}/explores/orders/downloadCsv`;
+            cy.intercept({
+                method: 'POST',
+                url: downloadUrl,
+            }).as('apiDownloadCsv');
+            // choose table and select fields
+            cy.findByText('Orders').click();
+            cy.findByText('First name').click();
+            cy.findByText('Unique order count').click();
+
+            // run query
+            cy.get('button').contains('Run query').click();
+
+            // open chart
+            cy.findByText('Charts').parent().findByRole('button').click();
+            // Close results
+            cy.findByText('Results').parent().findByRole('button').click();
+            // wait for the chart to finish loading
+            cy.findByText('Loading chart').should('not.exist');
+
+            // open chart menu and change chart type to Table
+            cy.get('button').contains('Bar chart').click();
+            cy.get('[role="menuitem"]').contains('Table').click();
+
+            cy.findByText('Export CSV').click();
+            cy.get('.bp4-popover2').contains('button', 'Export CSV').click();
+
+            cy.wait('@apiDownloadCsv').then((interception) => {
+                expect(interception?.response?.statusCode).to.eq(200);
+
+                expect(interception?.response?.body.results).to.have.property(
+                    'jobId',
+                );
+            });
+        },
+    );
 });
