2add17a3b4501d5dd0f78098f92be95d18e95d78
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-24 19:25:56 +0400

    chore: migrate FieldAutoComplete to mantine

--- packages/frontend/src/components/common/Filters/FieldAutoComplete.tsx
+++ packages/frontend/src/components/common/Filters/FieldAutoComplete.tsx
@@ -1,26 +1,14 @@
-import { MenuItem2, Popover2Props } from '@blueprintjs/popover2';
-import { Suggest2 } from '@blueprintjs/select';
 import {
     Field,
     getItemId,
     getItemLabel,
+    getItemLabelWithoutTableName,
+    isField,
     TableCalculation,
 } from '@lightdash/common';
-import { useMemo } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { Group, Select, SelectItemProps, Text } from '@mantine/core';
+import { forwardRef, useMemo } from 'react';
 import FieldIcon from './FieldIcon';
-import {
-    renderFilterItem,
-    renderFilterItemWithoutTableName,
-} from './renderFilterItem';
-import renderFilterList from './renderFilterList';
-
-const AutocompleteMaxHeight = createGlobalStyle`
-  .autocomplete-max-height {
-    max-height: 400px;
-    overflow-y: auto;
-  }
-`;
 
 type FieldAutoCompleteProps<T> = {
     id?: string;
@@ -28,93 +16,78 @@ type FieldAutoCompleteProps<T> = {
     disabled?: boolean;
     autoFocus?: boolean;
     activeField?: T;
-    inactiveFieldIds?: string[];
     placeholder?: string;
-    fields: Array<T>;
+    fields: T[];
     onChange: (value: T) => void;
     onClosed?: () => void;
-    popoverProps?: Popover2Props;
-    inputProps?: React.ComponentProps<typeof Suggest2>['inputProps'];
     hasGrouping?: boolean;
 };
 
+type ItemProps = SelectItemProps & {
+    value: string;
+    label: string;
+    item: Field | TableCalculation;
+};
+
+const SelectItem = forwardRef<HTMLDivElement, ItemProps>(
+    ({ label, item, ...others }: ItemProps, ref) => (
+        <Group noWrap ref={ref} {...others}>
+            <FieldIcon item={item} />
+            <Text>{label}</Text>
+        </Group>
+    ),
+);
+
 const FieldAutoComplete = <T extends Field | TableCalculation>({
-    disabled,
-    autoFocus,
-    activeField,
-    fields,
     id,
     name,
+    disabled,
+    autoFocus,
+    placeholder,
+    activeField,
+    fields,
+    hasGrouping = true,
     onChange,
     onClosed,
-    placeholder,
-    popoverProps,
-    inputProps,
-    hasGrouping = false,
 }: FieldAutoCompleteProps<T>) => {
-    const sortedFields = useMemo(() => {
-        return fields.sort((a, b) =>
-            getItemLabel(a).localeCompare(getItemLabel(b)),
-        );
-    }, [fields]);
+    const itemData = useMemo(() => {
+        return fields
+            .sort((a, b) => getItemLabel(a).localeCompare(getItemLabel(b)))
+            .map<ItemProps>((field) => ({
+                item: field,
+                value: getItemId(field),
+                label: hasGrouping
+                    ? getItemLabelWithoutTableName(field)
+                    : getItemLabel(field),
+                group:
+                    hasGrouping && isField(field)
+                        ? field.table.toUpperCase()
+                        : undefined,
+            }));
+    }, [hasGrouping, fields]);
 
     return (
-        <>
-            <AutocompleteMaxHeight />
-            <Suggest2<T>
-                fill
-                className={disabled ? 'disabled-filter' : ''}
-                disabled={disabled}
-                inputProps={{
-                    id,
-                    name,
-                    autoFocus,
-                    placeholder: placeholder || 'Search field...',
-                    leftElement: activeField && (
-                        <FieldIcon
-                            item={activeField}
-                            size={16}
-                            style={{ margin: '7px 8px' }}
-                        />
-                    ),
-                    ...inputProps,
-                }}
-                items={sortedFields}
-                itemsEqual={(value, other) => {
-                    return getItemId(value) === getItemId(other);
-                }}
-                inputValueRenderer={(item) => {
-                    if (!activeField) {
-                        return '';
-                    }
-                    return getItemLabel(item);
-                }}
-                popoverProps={{
-                    minimal: true,
-                    onClosed,
-                    popoverClassName: 'autocomplete-max-height',
-                    captureDismiss: true,
-                    ...popoverProps,
-                }}
-                itemRenderer={
-                    hasGrouping
-                        ? renderFilterItemWithoutTableName
-                        : renderFilterItem
-                }
-                itemListRenderer={hasGrouping ? renderFilterList : undefined}
-                activeItem={activeField}
-                selectedItem={activeField}
-                noResults={<MenuItem2 disabled text="No results." />}
-                onItemSelect={onChange}
-                itemPredicate={(query, item, _index, exactMatch) => {
-                    const label = getItemLabel(item);
-                    if (exactMatch) {
-                        return query.toLowerCase() === label.toLowerCase();
-                    }
-                    return label.toLowerCase().includes(query.toLowerCase());
-                }}
-            />
-        </>
+        <Select
+            // input props
+            id={id}
+            name={name}
+            disabled={disabled}
+            autoFocus={autoFocus}
+            placeholder={placeholder || 'Search field...'}
+            // component props
+            searchable
+            nothingFound="No results."
+            data={itemData}
+            value={activeField && getItemId(activeField)}
+            itemComponent={SelectItem}
+            icon={activeField && <FieldIcon item={activeField} />}
+            // events
+            onChange={(value) => {
+                const field = fields.find((f) => getItemId(f) === value);
+                if (field) onChange(field);
+            }}
+            onDropdownClose={onClosed}
+        />
     );
 };
 
