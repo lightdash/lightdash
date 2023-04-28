f9cdfc1730847488507b4008e571dfdae6f89b67
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-04-28 19:07:17 +0400

    chore: fix incorrect change

--- packages/frontend/src/components/Explorer/ExploreTree/TableTree/TableTreeSections.tsx
+++ packages/frontend/src/components/Explorer/ExploreTree/TableTree/TableTreeSections.tsx
@@ -48,123 +48,110 @@ const TableTreeSections: FC<Props> = ({
         <>
             {isSearching &&
             getSearchResults(dimensions, searchQuery).size === 0 ? null : (
-                <>
-                    <Group h="xxl">
-                        <Text fw={600} color="blue.9">
-                            Dimensions
-                        </Text>
-                    </Group>
+                <Group h="xxl">
+                    <Text fw={600} color="blue.9">
+                        Dimensions
+                    </Text>
+                </Group>
+            )}
 
-                    {Object.keys(table.dimensions).length <= 0 ? (
-                        <Center>
-                            <Text color="dimmed">
-                                No dimensions defined in your dbt project
-                            </Text>
-                        </Center>
-                    ) : (
-                        <TreeProvider
-                            orderFieldsBy={table.orderFieldsBy}
-                            searchQuery={searchQuery}
-                            itemsMap={dimensions}
-                            selectedItems={selectedItems}
-                            onItemClick={(key) =>
-                                onSelectedNodeChange(key, true)
-                            }
-                        >
-                            <TreeRoot />
-                        </TreeProvider>
-                    )}
-                </>
+            {Object.keys(table.dimensions).length <= 0 ? (
+                <Center>
+                    <Text color="dimmed">
+                        No dimensions defined in your dbt project
+                    </Text>
+                </Center>
+            ) : (
+                <TreeProvider
+                    orderFieldsBy={table.orderFieldsBy}
+                    searchQuery={searchQuery}
+                    itemsMap={dimensions}
+                    selectedItems={selectedItems}
+                    onItemClick={(key) => onSelectedNodeChange(key, true)}
+                >
+                    <TreeRoot />
+                </TreeProvider>
             )}
 
             {isSearching &&
             getSearchResults(metrics, searchQuery).size === 0 ? null : (
-                <>
-                    <Group position="apart" h="xxl" pr="sm">
-                        <Text fw={600} color="yellow.9">
-                            Metrics
-                        </Text>
+                <Group position="apart" h="xxl" pr="sm">
+                    <Text fw={600} color="yellow.9">
+                        Metrics
+                    </Text>
 
-                        {hasNoMetrics ? (
-                            <DocumentationHelpButton
-                                href="https://docs.lightdash.com/guides/how-to-create-metrics"
-                                tooltipProps={{
-                                    label: (
-                                        <>
-                                            No metrics defined in your dbt
-                                            project.
-                                            <br />
-                                            Click to{' '}
-                                            <Text component="span" fw={600}>
-                                                view docs
-                                            </Text>{' '}
-                                            and learn how to add a metric to
-                                            your project.
-                                        </>
-                                    ),
-                                    multiline: true,
-                                }}
-                            />
-                        ) : null}
-                    </Group>
-
-                    {hasNoMetrics ? null : (
-                        <TreeProvider
-                            orderFieldsBy={table.orderFieldsBy}
-                            searchQuery={searchQuery}
-                            itemsMap={metrics}
-                            selectedItems={selectedItems}
-                            onItemClick={(key) =>
-                                onSelectedNodeChange(key, false)
-                            }
-                        >
-                            <TreeRoot />
-                        </TreeProvider>
-                    )}
-                </>
-            )}
-
-            {isSearching &&
-            getSearchResults(customMetrics, searchQuery).size === 0 ? null : (
-                <>
-                    <Group position="apart" h="xxl" pr="sm">
-                        <Text fw={600} color="yellow.9">
-                            Custom metrics
-                        </Text>
-
+                    {hasNoMetrics ? (
                         <DocumentationHelpButton
-                            href="https://docs.lightdash.com/guides/how-to-create-metrics#-adding-custom-metrics-in-the-explore-view"
+                            href="https://docs.lightdash.com/guides/how-to-create-metrics"
                             tooltipProps={{
                                 label: (
                                     <>
-                                        Add custom metrics by hovering over the
-                                        dimension of your choice & selecting the
-                                        three-dot Action Menu.{' '}
+                                        No metrics defined in your dbt project.
+                                        <br />
+                                        Click to{' '}
                                         <Text component="span" fw={600}>
-                                            Click to view docs.
-                                        </Text>
+                                            view docs
+                                        </Text>{' '}
+                                        and learn how to add a metric to your
+                                        project.
                                     </>
                                 ),
                                 multiline: true,
                             }}
                         />
-                    </Group>
-
-                    {hasNoMetrics || additionalMetrics.length > 0 ? (
-                        <TreeProvider
-                            orderFieldsBy={table.orderFieldsBy}
-                            searchQuery={searchQuery}
-                            itemsMap={customMetrics}
-                            selectedItems={selectedItems}
-                            onItemClick={(key) =>
-                                onSelectedNodeChange(key, false)
-                            }
-                        >
-                            <TreeRoot />
-                        </TreeProvider>
                     ) : null}
-                </>
+                </Group>
             )}
+
+            {hasNoMetrics ? null : (
+                <TreeProvider
+                    orderFieldsBy={table.orderFieldsBy}
+                    searchQuery={searchQuery}
+                    itemsMap={metrics}
+                    selectedItems={selectedItems}
+                    onItemClick={(key) => onSelectedNodeChange(key, false)}
+                >
+                    <TreeRoot />
+                </TreeProvider>
+            )}
+
+            {isSearching &&
+            getSearchResults(customMetrics, searchQuery).size === 0 ? null : (
+                <Group position="apart" h="xxl" pr="sm">
+                    <Text fw={600} color="yellow.9">
+                        Custom metrics
+                    </Text>
+
+                    <DocumentationHelpButton
+                        href="https://docs.lightdash.com/guides/how-to-create-metrics#-adding-custom-metrics-in-the-explore-view"
+                        tooltipProps={{
+                            label: (
+                                <>
+                                    Add custom metrics by hovering over the
+                                    dimension of your choice & selecting the
+                                    three-dot Action Menu.{' '}
+                                    <Text component="span" fw={600}>
+                                        Click to view docs.
+                                    </Text>
+                                </>
+                            ),
+                            multiline: true,
+                        }}
+                    />
+                </Group>
+            )}
+
+            {hasNoMetrics || additionalMetrics.length > 0 ? (
+                <TreeProvider
+                    orderFieldsBy={table.orderFieldsBy}
+                    searchQuery={searchQuery}
+                    itemsMap={customMetrics}
+                    selectedItems={selectedItems}
+                    onItemClick={(key) => onSelectedNodeChange(key, false)}
+                >
+                    <TreeRoot />
+                </TreeProvider>
+            ) : null}
         </>
     );
 };
