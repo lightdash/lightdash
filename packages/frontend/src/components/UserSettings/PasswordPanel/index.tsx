12c59eac40d9cb99ad02f187735ca8232a931156
Joao Viana <joao@lightdash.com>
2023-05-02 16:50:19 +0100

    feat: migrate password settings to mantine

--- packages/frontend/src/components/UserSettings/PasswordPanel/index.tsx
+++ packages/frontend/src/components/UserSettings/PasswordPanel/index.tsx
@@ -1,12 +1,11 @@
-import { Button, Intent } from '@blueprintjs/core';
 import { ApiError } from '@lightdash/common';
-import React, { FC, useEffect, useState } from 'react';
+import { Button, Flex, PasswordInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { FC, useEffect } from 'react';
 import { useMutation } from 'react-query';
 import { lightdashApi } from '../../../api';
-import useToaster from '../../../hooks/toaster/useToaster';
 import useUserHasPassword from '../../../hooks/user/usePassword';
 import { useErrorLogs } from '../../../providers/ErrorLogsProvider';
-import PasswordInput from '../../PasswordInput';
 
 const updateUserPasswordQuery = async (data: {
     password: string;
@@ -20,12 +19,19 @@ const updateUserPasswordQuery = async (data: {
 
 const PasswordPanel: FC = () => {
     const { data: hasPassword } = useUserHasPassword();
-    const { showToastError } = useToaster();
     const { showError } = useErrorLogs();
-    const [password, setPassword] = useState<string>();
-    const [newPassword, setNewPassword] = useState<string>();
+    const form = useForm({
+        initialValues: {
+            currentPassword: '',
+            newPassword: '',
+        },
+    });
 
-    const { isLoading, error, mutate } = useMutation<
+    const {
+        isLoading,
+        error,
+        mutate: updateUserPassword,
+    } = useMutation<
         undefined,
         ApiError,
         { password: string; newPassword: string }
@@ -46,60 +52,48 @@ const PasswordPanel: FC = () => {
         }
     }, [error, showError]);
 
-    const handleUpdate = () => {
-        if (hasPassword && password && newPassword) {
-            mutate({
-                password,
-                newPassword,
-            });
-        } else if (!hasPassword && newPassword) {
-            mutate({
-                password: '',
-                newPassword,
-            });
-        } else {
-            showToastError({
-                title: 'Required fields: password and new password',
-                timeout: 3000,
-            });
-        }
-    };
+    const onSubmit = form.onSubmit(({ currentPassword, newPassword }) => {
+        updateUserPassword({
+            password: currentPassword,
+            newPassword,
+        });
+    });
 
     return (
-        <div
-            style={{
-                height: 'fit-content',
-                display: 'flex',
-                flexDirection: 'column',
-            }}
-        >
-            {hasPassword && (
+        <Flex dir="column" sx={{ height: 'fit-content' }}>
+            <form onSubmit={onSubmit} style={{ width: '100%' }}>
+                {hasPassword && (
+                    <PasswordInput
+                        label="Current password"
+                        placeholder="Enter your password..."
+                        required
+                        disabled={isLoading}
+                        {...form.getInputProps('currentPassword')}
+                    />
+                )}
                 <PasswordInput
-                    label="Current password"
-                    placeholder="Enter your password..."
+                    label="New password"
+                    placeholder="Enter your new password..."
                     required
                     disabled={isLoading}
-                    value={password}
-                    onChange={setPassword}
+                    mt="md"
+                    {...form.getInputProps('newPassword')}
                 />
-            )}
-            <PasswordInput
-                label="New password"
-                placeholder="Enter your new password..."
-                required
-                disabled={isLoading}
-                value={newPassword}
-                onChange={setNewPassword}
-            />
-            <div style={{ flex: 1 }} />
-            <Button
-                style={{ alignSelf: 'flex-end', marginTop: 20 }}
-                intent={Intent.PRIMARY}
-                text="Update"
-                onClick={handleUpdate}
-                loading={isLoading}
-            />
-        </div>
+
+                <Button
+                    mt="md"
+                    type="submit"
+                    ml="auto"
+                    display="flex"
+                    sx={{
+                        alignSelf: 'flex-end',
+                    }}
+                    loading={isLoading}
+                >
+                    Update{' '}
+                </Button>
+            </form>
+        </Flex>
     );
 };
 
