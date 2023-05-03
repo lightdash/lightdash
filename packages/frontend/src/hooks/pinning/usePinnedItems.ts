5184d9099f2f72ee61cbf3b469f36deaf76a7cca
yasmineabdelkefi <yasmine@lightdash.com>
2023-05-03 12:07:13 +0200

    feat: pinned items reorder hook added

--- packages/frontend/src/hooks/pinning/usePinnedItems.ts
+++ packages/frontend/src/hooks/pinning/usePinnedItems.ts
@@ -1,6 +1,7 @@
 import { ApiError, ApiPinnedItems, PinnedItems } from '@lightdash/common';
-import { useQuery, useQueryClient } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { lightdashApi } from '../../api';
+import useToaster from '../toaster/useToaster';
 
 const getPinnedItems = async (projectUuid: string, pinnedlistUuid: string) =>
     lightdashApi<PinnedItems>({
@@ -8,6 +9,18 @@ const getPinnedItems = async (projectUuid: string, pinnedlistUuid: string) =>
         method: 'GET',
         body: undefined,
     });
+
+const updatePinnedItemsOrder = async (
+    projectUuid: string,
+    pinnedListUuid: string,
+    pinnedItems: PinnedItems,
+) => {
+    return lightdashApi<PinnedItems>({
+        url: `/projects/${projectUuid}/pinned-lists/${pinnedListUuid}/items/order`,
+        method: 'PATCH',
+        body: JSON.stringify(pinnedItems),
+    });
+};
 
 export const usePinnedItems = (
     projectUuid: string,
@@ -18,3 +31,29 @@ export const usePinnedItems = (
         queryFn: () => getPinnedItems(projectUuid, pinnedlistUuid || ''),
         enabled: !!pinnedlistUuid,
     });
+
+export const usePinnedItemsOrder = (
+    projectUuid: string,
+    pinnedlistUuid: string,
+    pinnedItems: PinnedItems,
+) => {
+    const queryClient = useQueryClient();
+    const { showToastSuccess, showToastError } = useToaster();
+    return useMutation<PinnedItems, ApiError, PinnedItems>(
+        () => updatePinnedItemsOrder(projectUuid, pinnedlistUuid, pinnedItems),
+        {
+            onSuccess: (data) => {
+                queryClient.setQueryData(
+                    ['pinned_items', projectUuid, pinnedlistUuid],
+                    data,
+                );
+                showToastSuccess({ title: 'Pinned items order updated' });
+            },
+            onError: (error) => {
+                showToastError({
+                    title: `Pinned items order update failed. Error: ${error}.`,
+                });
+            },
+        },
+    );
+};
