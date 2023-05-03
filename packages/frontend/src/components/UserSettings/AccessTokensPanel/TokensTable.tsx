da7a79b25711e9663790161ef6c4f7117d02520e
Joao Viana <joao@lightdash.com>
2023-05-03 15:52:20 +0100

    refactor: extract TokensTable into own comp

---
+++ packages/frontend/src/components/UserSettings/AccessTokensPanel/TokensTable.tsx
@@ -0,0 +1,124 @@
+import { Button, Classes, Dialog } from '@blueprintjs/core';
+import { ApiPersonalAccessTokenResponse, formatDate } from '@lightdash/common';
+import {
+    Button as MantineButton,
+    createStyles,
+    Paper,
+    Table,
+} from '@mantine/core';
+import { FC, useState } from 'react';
+import {
+    useAccessToken,
+    useDeleteAccessToken,
+} from '../../../hooks/useAccessToken';
+
+const useTableStyles = createStyles((theme) => ({
+    root: {
+        '& thead tr': {
+            backgroundColor: theme.colors.gray[0],
+        },
+
+        '& thead tr th': {
+            color: theme.colors.gray[6],
+            fontWeight: 600,
+        },
+
+        '& thead tr th, & tbody tr td': {
+            padding: '12px 20px',
+        },
+
+        '& tr td:last-child': {
+            textAlign: 'right',
+        },
+
+        '&[data-hover] tbody tr': theme.fn.hover({
+            cursor: 'pointer',
+            backgroundColor: theme.fn.rgba(theme.colors.gray[0], 0.5),
+        }),
+    },
+}));
+
+const TokenItem: FC<{
+    token: ApiPersonalAccessTokenResponse;
+}> = ({ token }) => {
+    const { description, expiresAt, uuid } = token;
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+    const { mutate, isLoading: isDeleting } = useDeleteAccessToken();
+
+    return (
+        <>
+            <tr>
+                <td>{description}</td>
+                <td>
+                    {expiresAt ? formatDate(expiresAt) : 'No expiration date'}
+                </td>
+                <td>
+                    <MantineButton
+                        variant="outline"
+                        size="xs"
+                        color="red"
+                        onClick={() => setIsDeleteDialogOpen(true)}
+                    >
+                        Delete
+                    </MantineButton>
+                </td>
+            </tr>
+            <Dialog
+                isOpen={isDeleteDialogOpen}
+                icon="delete"
+                onClose={() => !isDeleting && setIsDeleteDialogOpen(false)}
+                title={`Delete token ${description}`}
+                lazy
+            >
+                <div className={Classes.DIALOG_BODY}>
+                    <p>
+                        Are you sure ? This will permanently delete the
+                        <b> {description} </b> token.
+                    </p>
+                </div>
+                <div className={Classes.DIALOG_FOOTER}>
+                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                        <Button
+                            disabled={isDeleting}
+                            onClick={() => setIsDeleteDialogOpen(false)}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            disabled={isDeleting}
+                            intent="danger"
+                            onClick={() => {
+                                mutate(uuid || '');
+                            }}
+                        >
+                            Delete
+                        </Button>
+                    </div>
+                </div>
+            </Dialog>
+        </>
+    );
+};
+
+export const TokensTable = () => {
+    const { data } = useAccessToken();
+    const { classes } = useTableStyles();
+    return (
+        <Paper withBorder>
+            <Table className={classes.root} highlightOnHover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Expiration date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((token) => (
+                        <TokenItem key={token.uuid} token={token} />
+                    ))}
+                </tbody>
+            </Table>
+        </Paper>
+    );
+};
--- packages/frontend/src/components/UserSettings/AccessTokensPanel/index.tsx
+++ packages/frontend/src/components/UserSettings/AccessTokensPanel/index.tsx
@@ -1,106 +1,17 @@
-import {
-    Button,
-    ButtonGroup,
-    Classes,
-    Dialog,
-    Intent,
-} from '@blueprintjs/core';
-import { ApiPersonalAccessTokenResponse, formatDate } from '@lightdash/common';
-import { Button as MantineButton, Flex } from '@mantine/core';
+import { Button } from '@blueprintjs/core';
+import { Flex, Title } from '@mantine/core';
 import { IconKey } from '@tabler/icons-react';
 import { FC, useState } from 'react';
-import {
-    useAccessToken,
-    useDeleteAccessToken,
-} from '../../../hooks/useAccessToken';
+import { useAccessToken } from '../../../hooks/useAccessToken';
 import { EmptyState } from '../../common/EmptyState';
 import MantineIcon from '../../common/MantineIcon';
 import CreateTokenPanel from '../CreateTokenPanel';
-import {
-    AccessTokenInfo,
-    AccessTokenLabel,
-    AccessTokenWrapper,
-    ExpireAtLabel,
-    HeaderActions,
-    ItemContent,
-    PanelTitle,
-} from './AccessTokens.styles';
-
-const TokenListItem: FC<{
-    token: ApiPersonalAccessTokenResponse;
-}> = ({ token }) => {
-    const { description, expiresAt, uuid } = token;
-    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-    const { mutate, isLoading: isDeleting } = useDeleteAccessToken();
-    return (
-        <AccessTokenWrapper elevation={0}>
-            <ItemContent>
-                <AccessTokenInfo>
-                    <AccessTokenLabel
-                        className={Classes.TEXT_OVERFLOW_ELLIPSIS}
-                    >
-                        {description}
-                    </AccessTokenLabel>
-                    <ExpireAtLabel intent={expiresAt ? 'warning' : 'none'}>
-                        {expiresAt
-                            ? `Expires on ${formatDate(expiresAt)}`
-                            : 'No expiration date'}
-                    </ExpireAtLabel>
-                </AccessTokenInfo>
-                <ButtonGroup>
-                    <Button
-                        icon="delete"
-                        outlined
-                        text="Delete"
-                        intent={Intent.DANGER}
-                        style={{ marginLeft: 10 }}
-                        onClick={() => setIsDeleteDialogOpen(true)}
-                    />
-                </ButtonGroup>
-            </ItemContent>
-            <Dialog
-                isOpen={isDeleteDialogOpen}
-                icon="delete"
-                onClose={() =>
-                    !isDeleting ? setIsDeleteDialogOpen(false) : undefined
-                }
-                title={`Delete token ${description}`}
-                lazy
-            >
-                <div className={Classes.DIALOG_BODY}>
-                    <p>
-                        Are you sure ? This will permanently delete the
-                        <b> {description} </b> token.
-                    </p>
-                </div>
-                <div className={Classes.DIALOG_FOOTER}>
-                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                        <Button
-                            disabled={isDeleting}
-                            onClick={() => setIsDeleteDialogOpen(false)}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            disabled={isDeleting}
-                            intent="danger"
-                            onClick={() => {
-                                mutate(uuid || '');
-                            }}
-                        >
-                            Delete
-                        </Button>
-                    </div>
-                </div>
-            </Dialog>
-        </AccessTokenWrapper>
-    );
-};
+import { TokensTable } from './TokensTable';
 
 const AccessTokensPanel: FC = () => {
     const { data } = useAccessToken();
     const [createTokenPanel, setCreateInvitesPanel] = useState(false);
-    const hasAvailableTokens = data && data?.length > 0;
+    const hasAvailableTokens = data && data.length > 0;
     if (createTokenPanel) {
         return (
             <CreateTokenPanel
@@ -109,30 +20,9 @@ const AccessTokensPanel: FC = () => {
         );
     }
 
-    return (
-        <Flex
-            h="100%"
-            direction="column"
-            justify={hasAvailableTokens ? 'auto' : 'center'}
-            align={hasAvailableTokens ? 'auto' : 'center'}
-        >
-            {hasAvailableTokens && (
-                <HeaderActions>
-                    <PanelTitle>Personal access tokens</PanelTitle>
-                    <Button
-                        intent="primary"
-                        onClick={() => setCreateInvitesPanel(true)}
-                        text="Generate new token"
-                    />
-                </HeaderActions>
-            )}
-            {hasAvailableTokens ? (
-                <div>
-                    {data?.map((token) => (
-                        <TokenListItem key={token.uuid} token={token} />
-                    ))}
-                </div>
-            ) : (
+    if (!hasAvailableTokens) {
+        return (
+            <Flex h="100%" direction="column" justify="center" align="center">
                 <EmptyState
                     icon={
                         <MantineIcon
@@ -145,12 +35,25 @@ const AccessTokensPanel: FC = () => {
                     title="No tokens"
                     description="You haven't generated any tokens yet!, generate your first token"
                 >
-                    <MantineButton onClick={() => setCreateInvitesPanel(true)}>
+                    <Button onClick={() => setCreateInvitesPanel(true)}>
                         Generate token
-                    </MantineButton>
+                    </Button>
                 </EmptyState>
-            )}
-        </Flex>
+            </Flex>
+        );
+    }
+
+    return (
+        <>
+            <Flex justify="space-between" align="center" mb="lg">
+                <Title order={5}>Personal access tokens</Title>
+                <Button onClick={() => setCreateInvitesPanel(true)}>
+                    Generate new token
+                </Button>
+            </Flex>
+
+            <TokensTable />
+        </>
     );
 };
 
