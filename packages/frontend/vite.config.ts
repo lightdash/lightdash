9b8681022e91d04deaa149617bec2713fc90a887
Rephus <rephus@gmail.com>
2023-05-05 08:49:34 +0200

    feat: add source maps

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
         // sourcemap: true,
         target: 'es2015',
         minify: true,
+        sourcemap: true, 
         rollupOptions: {
             output: {
                 manualChunks: mapManualChunks({
