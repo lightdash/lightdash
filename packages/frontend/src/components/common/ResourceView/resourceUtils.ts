4991a0609c19384fda21d9e37aa5ece5882c4568
Joao Viana <joao@lightdash.com>
2023-05-09 12:14:27 +0100

    fix: get item project uuid if project uuid is falsy

--- packages/frontend/src/components/common/ResourceView/resourceUtils.ts
+++ packages/frontend/src/components/common/ResourceView/resourceUtils.ts
@@ -50,7 +50,9 @@ export const getResourceUrl = (projectUuid: string, item: ResourceViewItem) => {
         case ResourceViewItemType.CHART:
             return `/projects/${projectUuid}/saved/${item.data.uuid}`;
         case ResourceViewItemType.SPACE:
-            return `/projects/${projectUuid}/spaces/${item.data.uuid}`;
+            return `/projects/${projectUuid || item.data.projectUuid}/spaces/${
+                item.data.uuid
+            }`;
         default:
             return assertUnreachable(item, `Can't get URL for ${itemType}`);
     }
