130946a244b2b087716e4d7e955f8ba1ffc1d3fd
João Viana <joao@lightdash.com>
2023-05-09 08:55:45 +0100

    feat: migrate appearance view with mantine (#5365)
    * feat: migrate appearance view with mantine
    
    * refactor: use mantines useForm hook
    
    * refactor: fix reverse object and allow validation
    
    * feat: improve invalid color message format
    
    * chore: update useeffect deps
    
    * feat: add default echart color swatches
    
    * style: wrap content with stack and spacing
    
    * feat: validate color onBlur
    
    * style: apply marginBottom to color input label
    
    * refactor: move regex definition to upper scope
    
    * refactor: increase fields index by 1
    
    * refactor: move getColorFormFields to upper scope

--- packages/frontend/src/components/UserSettings/AppearancePanel/index.tsx
+++ packages/frontend/src/components/UserSettings/AppearancePanel/index.tsx
@@ -1,29 +1,52 @@
-import { Intent, Spinner } from '@blueprintjs/core';
+import { Spinner } from '@blueprintjs/core';
 import { subject } from '@casl/ability';
 import { ECHARTS_DEFAULT_COLORS } from '@lightdash/common';
-import { FC, useCallback, useEffect, useState } from 'react';
-
+import { Button, ColorInput, SimpleGrid, Stack, Title } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { FC, useEffect } from 'react';
 import { useOrganization } from '../../../hooks/organization/useOrganization';
 import { useOrganizationUpdateMutation } from '../../../hooks/organization/useOrganizationUpdateMutation';
-import { InputWrapper } from '../../ChartConfigPanel/ChartConfigPanel.styles';
 import { Can, useAbilityContext } from '../../common/Authorization';
-import ColorInput from '../../common/ColorInput';
-import {
-    AppearancePanelWrapper,
-    ColorPalette,
-    SaveButton,
-    Title,
-} from './AppearancePanel.styles';
+
+const IS_HEX_CODE_COLOR_REGEX = /^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$/;
+
+const getColorFormFields = (colors: string[]) =>
+    colors.reduce(
+        (acc, color, index) => ({ ...acc, [`color${index + 1}`]: color }),
+        {},
+    );
 
 const AppearancePanel: FC = () => {
     const ability = useAbilityContext();
     const { isLoading: isOrgLoading, data } = useOrganization();
     const updateMutation = useOrganizationUpdateMutation();
-    let [colors, setColors] = useState<string[]>(
-        data?.chartColors || ECHARTS_DEFAULT_COLORS.slice(0, 8),
-    );
 
-    const update = useCallback(() => {
+    const form = useForm({
+        initialValues: getColorFormFields(ECHARTS_DEFAULT_COLORS.slice(0, 8)),
+        validate: Object.keys(
+            getColorFormFields(ECHARTS_DEFAULT_COLORS.slice(0, 8)),
+        ).reduce(
+            (acc, key) => ({
+                [key]: (value: string) =>
+                    !IS_HEX_CODE_COLOR_REGEX.test(value)
+                        ? 'Invalid color, ensure it is in hex format (e.g. #ff000 or #fff)'
+                        : null,
+                ...acc,
+            }),
+            {},
+        ),
+    });
+
+    useEffect(() => {
+        if (data?.chartColors) {
+            form.setValues(getColorFormFields(data.chartColors));
+            form.resetDirty(getColorFormFields(data.chartColors));
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data?.chartColors]);
+
+    const handleOnSubmit = form.onSubmit((newColors) => {
         if (data) {
             const {
                 needsProject: _needsProject,
@@ -32,62 +55,61 @@ const AppearancePanel: FC = () => {
             } = data;
             updateMutation.mutate({
                 ...params,
-                chartColors: colors,
+                chartColors: Object.values(newColors),
             });
         }
-    }, [colors, data, updateMutation]);
-
-    useEffect(() => {
-        if (data?.chartColors) {
-            setColors(data.chartColors);
-        }
-    }, [data]);
+    });
 
     if (isOrgLoading) {
         return <Spinner />;
     }
 
     return (
-        <AppearancePanelWrapper>
-            <Title>Default chart colors</Title>
-            <ColorPalette>
-                {colors.map((color, index) => (
-                    <InputWrapper key={index} label={`Color ${index + 1}`}>
-                        <ColorInput
-                            placeholder="Enter hex color"
-                            value={color}
-                            disabled={ability.cannot(
-                                'update',
-                                subject('Organization', {
-                                    organizationUuid: data?.organizationUuid,
-                                }),
-                            )}
-                            onChange={(e) => {
-                                setColors(
-                                    colors.map((c, i) =>
-                                        index === i ? e.target.value : c,
-                                    ),
-                                );
-                            }}
-                        />
-                    </InputWrapper>
-                ))}
-            </ColorPalette>
-            <div style={{ flex: 1 }} />
-            <Can
-                I={'update'}
-                this={subject('Organization', {
-                    organizationUuid: data?.organizationUuid,
-                })}
-            >
-                <SaveButton
-                    intent={Intent.PRIMARY}
-                    text="Save changes"
-                    loading={updateMutation.isLoading}
-                    onClick={update}
-                />
-            </Can>
-        </AppearancePanelWrapper>
+        <Stack spacing="md">
+            <Title order={5}>Default chart colors</Title>
+            <form onSubmit={handleOnSubmit}>
+                <Stack spacing="md">
+                    <SimpleGrid cols={2}>
+                        {Object.values(form.values).map((_color, index) => (
+                            <ColorInput
+                                key={index}
+                                width="100%"
+                                placeholder="Enter hex color"
+                                label={`Color ${index + 1}`}
+                                swatches={ECHARTS_DEFAULT_COLORS.slice(0, 8)}
+                                disabled={ability.cannot(
+                                    'update',
+                                    subject('Organization', {
+                                        organizationUuid:
+                                            data?.organizationUuid,
+                                    }),
+                                )}
+                                {...form.getInputProps(`color${index + 1}`)}
+                                onBlur={() => {
+                                    form.validateField(`color${index + 1}`);
+                                }}
+                            />
+                        ))}
+                    </SimpleGrid>
+
+                    <Can
+                        I={'update'}
+                        this={subject('Organization', {
+                            organizationUuid: data?.organizationUuid,
+                        })}
+                    >
+                        <Button
+                            type="submit"
+                            loading={updateMutation.isLoading}
+                            ml="auto"
+                            display="block"
+                        >
+                            Save changes
+                        </Button>
+                    </Can>
+                </Stack>
+            </form>
+        </Stack>
     );
 };
 
