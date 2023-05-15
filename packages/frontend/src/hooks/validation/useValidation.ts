3b6387f961da976e81c48e94dc0d0de9f2895640
Joao Viana <joao@lightdash.com>
2023-05-15 14:00:50 +0100

    feat: order validation array by type

--- packages/frontend/src/hooks/validation/useValidation.ts
+++ packages/frontend/src/hooks/validation/useValidation.ts
@@ -2,6 +2,28 @@ import { ApiError, ValidationResponse } from '@lightdash/common';
 import { useQuery } from 'react-query';
 import { lightdashApi } from '../../api';
 
+// Sorts validation responses by type, order is as follows: table, chart, dashboard validation errors
+const sortByType = (a: ValidationResponse, b: ValidationResponse) => {
+    // Table type
+    if (a.chartUuid === undefined && a.dashboardUuid === undefined) {
+        if (b.chartUuid === undefined && b.dashboardUuid === undefined) {
+            return 0;
+        } else {
+            return -1;
+        }
+    } else if (a.chartUuid !== undefined && b.chartUuid === undefined) {
+        return -1;
+    } else if (a.dashboardUuid !== undefined && b.dashboardUuid === undefined) {
+        if (a.chartUuid === undefined) {
+            return 1;
+        } else {
+            return -1;
+        }
+    } else {
+        return 1;
+    }
+};
+
 const getValidation = async (
     projectUuid: string,
 ): Promise<ValidationResponse[]> =>
@@ -14,6 +36,7 @@ const getValidation = async (
 export const useValidation = (projectUuid: string) => {
     return useQuery<ValidationResponse[], ApiError>({
         queryKey: 'validation',
-        queryFn: () => getValidation(projectUuid),
+        queryFn: () =>
+            getValidation(projectUuid).then((res) => res.sort(sortByType)),
     });
 };
