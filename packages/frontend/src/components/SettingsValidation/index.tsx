4a2db94ab23420d13058844c4fb515021b3c1a02
Joao Viana <joao@lightdash.com>
2023-05-15 12:14:10 +0100

    feat: add initial icon next to name

--- packages/frontend/src/components/SettingsValidation/index.tsx
+++ packages/frontend/src/components/SettingsValidation/index.tsx
@@ -1,7 +1,14 @@
-import { clsx, Paper, Table } from '@mantine/core';
+import { ValidationResponse } from '@lightdash/common';
+import { Flex, Paper, Table, Text } from '@mantine/core';
+import {
+    IconChartBar,
+    IconLayoutDashboard,
+    IconTable,
+} from '@tabler/icons-react';
 import { FC } from 'react';
 import { useTableStyles } from '../../hooks/styles/useTableStyles';
 import { useValidation } from '../../hooks/validation/useValidation';
+import { IconBox } from '../common/ResourceView/ResourceIcon';
 
 export const SettingsValidation: FC<{ projectUuid: string }> = ({
     projectUuid,
@@ -9,14 +16,23 @@ export const SettingsValidation: FC<{ projectUuid: string }> = ({
     const { data, isSuccess } = useValidation(projectUuid);
     const { classes } = useTableStyles();
 
+    const Icon = ({
+        validationError,
+    }: {
+        validationError: ValidationResponse;
+    }) => {
+        if (validationError.chartUuid)
+            return <IconBox icon={IconChartBar} color="blue.8" />;
+        if (validationError.dashboardUuid)
+            return <IconBox icon={IconLayoutDashboard} color="green.8" />;
+        return <IconBox icon={IconTable} color="blue.8" />;
+    };
+
     return (
         <>
             {isSuccess && (
                 <Paper withBorder sx={{ overflow: 'hidden' }}>
-                    <Table
-                        className={clsx(classes.root, classes.alignLastTdRight)}
-                        highlightOnHover
-                    >
+                    <Table className={classes.root} highlightOnHover>
                         <thead>
                             <tr>
                                 <th>Name</th>
@@ -25,20 +41,30 @@ export const SettingsValidation: FC<{ projectUuid: string }> = ({
                             </tr>
                         </thead>
                         <tbody>
-                            {data?.map(
-                                ({ name, error, ...validationError }) => (
-                                    <tr key={name}>
-                                        <td>{name}</td>
-                                        <td>{error}</td>
-                                        <td>
-                                            {validationError.lastUpdatedAt &&
-                                            validationError.lastUpdatedBy
-                                                ? `${validationError.lastUpdatedAt} by ${validationError.lastUpdatedBy}`
-                                                : 'N/A'}
-                                        </td>
-                                    </tr>
-                                ),
-                            )}
+                            {data?.map((validationError) => (
+                                <tr key={validationError.name}>
+                                    <td>
+                                        <Flex gap="sm">
+                                            <Icon
+                                                validationError={
+                                                    validationError
+                                                }
+                                            />
+
+                                            <Text fw={500}>
+                                                {validationError.name}
+                                            </Text>
+                                        </Flex>
+                                    </td>
+                                    <td>{validationError.error}</td>
+                                    <td>
+                                        {validationError.lastUpdatedAt &&
+                                        validationError.lastUpdatedBy
+                                            ? `${validationError.lastUpdatedAt} by ${validationError.lastUpdatedBy}`
+                                            : 'N/A'}
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </Table>
                 </Paper>
