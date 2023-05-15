018d45b9c0c5887620a668e1e18d6ae065ca1978
Joao Viana <joao@lightdash.com>
2023-05-15 15:39:50 +0100

    feat: add validation mutation

--- packages/frontend/src/hooks/validation/useValidation.ts
+++ packages/frontend/src/hooks/validation/useValidation.ts
@@ -1,6 +1,9 @@
 import { ApiError, ValidationResponse } from '@lightdash/common';
-import { useQuery } from 'react-query';
+import { useCallback } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { lightdashApi } from '../../api';
+import { useErrorLogs } from '../../providers/ErrorLogsProvider';
+import useToaster from '../toaster/useToaster';
 import { sortAlphabetically, sortByType } from './utils';
 
 const getValidation = async (
@@ -12,12 +15,47 @@ const getValidation = async (
         body: undefined,
     });
 
-export const useValidation = (projectUuid: string) => {
-    return useQuery<ValidationResponse[], ApiError>({
+export const useValidation = (projectUuid: string) =>
+    useQuery<ValidationResponse[], ApiError>({
         queryKey: 'validation',
         queryFn: () =>
             getValidation(projectUuid).then((res) =>
                 res.sort(sortAlphabetically).sort(sortByType),
             ),
+    });
+
+const updateValidation = async (
+    projectUuid: string,
+): Promise<ValidationResponse[]> =>
+    lightdashApi<ValidationResponse[]>({
+        url: `/projects/${projectUuid}/validate`,
+        method: 'POST',
+        body: undefined,
+    });
+
+export const useValidationMutation = (projectUuid: string) => {
+    const queryClient = useQueryClient();
+    const { showError } = useErrorLogs();
+    const { showToastSuccess } = useToaster();
+
+    return useMutation<ValidationResponse[], ApiError>({
+        mutationKey: ['validation', projectUuid],
+        mutationFn: () => updateValidation(projectUuid),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['validation'] });
+            showToastSuccess({
+                title: 'Success! Validation complete.',
+            });
+        },
+        onError: useCallback(
+            (error) => {
+                const [title, ...rest] = error.error.message.split('\n');
+                showError({
+                    title,
+                    body: rest.join('\n'),
+                });
+            },
+            [showError],
+        ),
     });
 };
