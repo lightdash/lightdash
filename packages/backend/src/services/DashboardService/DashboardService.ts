693cd19ff088fecca660887c2c115842731e923a
Rephus <rephus@gmail.com>
2023-05-16 08:26:39 +0200

    fix tests

--- packages/backend/src/services/DashboardService/DashboardService.ts
+++ packages/backend/src/services/DashboardService/DashboardService.ts
@@ -165,7 +165,13 @@ export class DashboardService {
             throw new ForbiddenError();
         }
         if (
-            user.ability.cannot('manage', subject('Project', dashboard)) &&
+            user.ability.cannot(
+                'manage',
+                subject('Project', {
+                    organizationUuid: dashboard.organizationUuid,
+                    projectUuid: dashboard.projectUuid,
+                }),
+            ) &&
             !(await this.hasDashboardSpaceAccess(
                 dashboard.spaceUuid,
                 user.userUuid,
@@ -315,7 +321,10 @@ export class DashboardService {
         if (
             user.ability.cannot(
                 'manage',
-                subject('Project', existingDashboard),
+                subject('Project', {
+                    organizationUuid: existingDashboard.organizationUuid,
+                    projectUuid: existingDashboard.projectUuid,
+                }),
             ) &&
             !(await this.hasDashboardSpaceAccess(
                 existingDashboard.spaceUuid,
