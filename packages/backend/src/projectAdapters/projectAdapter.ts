04ca8671a83323fd2800d6f799897c0f3cee082b
Rephus <rephus@gmail.com>
2023-05-18 16:31:22 +0200

    rollback local dir

--- packages/backend/src/projectAdapters/projectAdapter.ts
+++ packages/backend/src/projectAdapters/projectAdapter.ts
@@ -31,9 +31,7 @@ export const projectAdapterFromConfig = async (
         case DbtProjectType.DBT:
             return new DbtLocalCredentialsProjectAdapter({
                 warehouseClient,
-                projectDir:
-                    config.project_dir ||
-                    '/home/rephus/work/lightdash/examples/full-jaffle-shop-demo/dbt',
+                projectDir: config.project_dir || '/usr/app/dbt',
                 warehouseCredentials,
                 targetName: config.target,
                 environment: config.environment,
