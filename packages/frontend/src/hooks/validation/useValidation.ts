6cfc92266f20e0b55f168b2283b2a353dd4e47fd
Joao Viana <joao@lightdash.com>
2023-05-18 11:30:45 +0100

    feat: add local storage management on valid

--- packages/frontend/src/hooks/validation/useValidation.ts
+++ packages/frontend/src/hooks/validation/useValidation.ts
@@ -3,6 +3,7 @@ import {
     ApiJobScheduledResponse,
     ValidationResponse,
 } from '@lightdash/common';
+import { useLocalStorage } from '@mantine/hooks';
 import { useCallback } from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { lightdashApi } from '../../api';
@@ -34,10 +35,15 @@ const updateValidation = async (
         body: undefined,
     });
 
+export const LAST_VALIDATION_TIMESTAMP_KEY = 'lastValidationTimestamp';
+
 export const useValidationMutation = (
     projectUuid: string,
     onComplete: () => void,
 ) => {
+    const [, setLastValidationTimestamp] = useLocalStorage({
+        key: LAST_VALIDATION_TIMESTAMP_KEY,
+    });
     const queryClient = useQueryClient();
     const { showError } = useErrorLogs();
     const { showToastSuccess } = useToaster();
@@ -50,6 +56,7 @@ export const useValidationMutation = (
             pollJobStatus(data.jobId)
                 .then(() => {
                     onComplete();
+                    setLastValidationTimestamp(Date.now().toString());
                     // Invalidate validation to get latest results
                     queryClient.invalidateQueries({ queryKey: ['validation'] });
                     showToastSuccess({ title: 'Validation completed' });
