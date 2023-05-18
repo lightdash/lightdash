a836d877f0a18529b778064201de96dee6d38842
Rephus <rephus@gmail.com>
2023-05-18 10:58:37 +0200

    test: fix spaces-and-content e2e tests

--- packages/e2e/cypress/e2e/api/pinning.cy.ts
+++ packages/e2e/cypress/e2e/api/pinning.cy.ts
@@ -10,7 +10,9 @@ describe('Lightdash pinning endpoints', () => {
         const projectUuid = SEED_PROJECT.project_uuid;
         cy.request(`${apiUrl}/projects/${projectUuid}/spaces-and-content`).then(
             (projectResponse) => {
-                const savedChart = projectResponse.body.results[0].queries[0];
+                const savedChart = projectResponse.body.results.find(
+                    (space) => space.queries.length > 0,
+                ).queries[0];
 
                 // change once
                 cy.request(
