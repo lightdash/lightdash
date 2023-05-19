1e9924b48d136141fc51786a27c116eceab93d46
Jose Rego <ZeRego@users.noreply.github.com>
2023-05-19 15:17:39 +0100

    fix: env_var() default value (#5548)

--- packages/cli/src/dbt/templating.ts
+++ packages/cli/src/dbt/templating.ts
@@ -6,7 +6,7 @@ nunjucksEnv.addFilter('as_number', (str) => parseFloat(str));
 
 // jinja global functions
 const nunjucksContext = {
-    env_var: (key: string) => process.env[key],
+    env_var: (key: string, fallback?: string) => process.env[key] ?? fallback,
     var: (key: string) =>
         JSON.parse(process.argv[process.argv.indexOf('--vars') + 1])[key],
 };
