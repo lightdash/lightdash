a3dc9e7c3de5c4af73e13e24141dd1c621379966
Joao Viana <joao@lightdash.com>
2023-05-19 13:48:06 +0100

    chore: remove useValidation call in notification comp

--- packages/frontend/src/components/NavBar/NotificationsMenu/ValidationErrorNotification.tsx
+++ packages/frontend/src/components/NavBar/NotificationsMenu/ValidationErrorNotification.tsx
@@ -1,9 +1,9 @@
-import { Menu, Text } from '@mantine/core';
+import { ValidationResponse } from '@lightdash/common';
+import { Text } from '@mantine/core';
 import { IconAlertCircle } from '@tabler/icons-react';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { useTimeAgo } from '../../../hooks/useTimeAgo';
-import { useValidation } from '../../../hooks/validation/useValidation';
 import LargeMenuItem from '../../common/LargeMenuItem';
 
 const ValidationErrorNotificationDescription: FC<{
@@ -20,25 +20,21 @@ const ValidationErrorNotificationDescription: FC<{
     );
 };
 
-export const ValidationErrorNotification: FC<{ projectUuid: string }> = ({
-    projectUuid,
-}) => {
-    const { data: validationData } = useValidation(projectUuid);
-    return validationData && validationData.length > 0 ? (
-        <LargeMenuItem
-            component={Link}
-            to={`/generalSettings/projectManagement/${projectUuid}/validator`}
-            title="Validation Errors"
-            description={
-                <ValidationErrorNotificationDescription
-                    lastValidatedAt={validationData[0].createdAt}
-                    numberOfErrors={validationData.length}
-                />
-            }
-            icon={IconAlertCircle}
-            iconProps={{ color: 'red' }}
-        />
-    ) : (
-        <Menu.Item>No notifications</Menu.Item>
-    );
-};
+export const ValidationErrorNotification: FC<{
+    projectUuid: string;
+    validationData: ValidationResponse[];
+}> = ({ projectUuid, validationData }) => (
+    <LargeMenuItem
+        component={Link}
+        to={`/generalSettings/projectManagement/${projectUuid}/validator`}
+        title="Validation Errors"
+        description={
+            <ValidationErrorNotificationDescription
+                lastValidatedAt={validationData[0].createdAt}
+                numberOfErrors={validationData.length}
+            />
+        }
+        icon={IconAlertCircle}
+        iconProps={{ color: 'red' }}
+    />
+);
