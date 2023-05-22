e9987e98bf12bfcf7a1f3547734148d750be1916
Jose Rego <ZeRego@users.noreply.github.com>
2023-05-22 16:04:49 +0100

    fix: conditional formatting with empty color (#5575)

--- packages/frontend/src/utils/colorUtils.ts
+++ packages/frontend/src/utils/colorUtils.ts
@@ -1,6 +1,13 @@
 import Color from 'colorjs.io';
 
+const IS_HEX_CODE_COLOR_REGEX = /^#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$/;
+export const isHexCodeColor = (color: string): boolean => {
+    return IS_HEX_CODE_COLOR_REGEX.test(color);
+};
 export const readableColor = (backgroundColor: string) => {
+    if (!isHexCodeColor(backgroundColor)) {
+        return 'black';
+    }
     const onWhite = Math.abs(Color.contrastAPCA('white', backgroundColor));
     const onBlack = Math.abs(Color.contrastAPCA('black', backgroundColor));
     return onWhite > onBlack ? 'white' : 'black';
