233e65d36c1829d2a13cfc72e1a54397f9c2662c
oliverlaslett <olaslett@gmail.com>
2023-05-30 14:23:33 +0100

    feat: configure everything with envvars

--- packages/backend/src/otel.ts
+++ packages/backend/src/otel.ts
@@ -1,68 +1,60 @@
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { gcpDetector } from '@opentelemetry/resource-detector-gcp';
 import { Resource } from '@opentelemetry/resources';
-import {
-    ConsoleMetricExporter,
-    PeriodicExportingMetricReader,
-} from '@opentelemetry/sdk-metrics';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { core, NodeSDK } from '@opentelemetry/sdk-node';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
-if (['console', 'otlp'].includes(process.env.OTEL_TRACES_EXPORTER || '')) {
-    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+// Default to no telemetry
+process.env.OTEL_SDK_DISABLED = process.env.OTEL_SDK_DISABLED || 'true';
 
-    const sdk = new NodeSDK({
-        // Exporters configure where and how data is exported.
-        traceExporter:
-            process.env.OTEL_TRACES_EXPORTER === 'console'
-                ? new ConsoleSpanExporter()
-                : new OTLPTraceExporter({
-                      url:
-                          process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT ||
-                          undefined,
-                  }),
-        metricReader: new PeriodicExportingMetricReader({
-            exporter:
-                process.env.OTEL_TRACES_EXPORTER === 'console'
-                    ? new ConsoleMetricExporter()
-                    : new OTLPMetricExporter(),
-        }),
+// Environment variable configuration
+// OTEL_TRACES_EXPORTER=otlp
+// OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
+// OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
+// OTEL_RESOURCE_ATTRIBUTES=service.name=lightdash,service.version=1.0.0
 
-        // Instrumentations enable tracing/profiling of specific libraries.
-        // Auto instrumentation includes http,express,knex,pg,winston
-        instrumentations: [
-            getNodeAutoInstrumentations({
-                '@opentelemetry/instrumentation-http': {
-                    ignoreIncomingRequestHook(req) {
-                        // Ignore liveness checks
-                        const isLivenessProbe =
-                            !!req?.url?.match(/^\/api\/v1\/livez$/);
-                        return isLivenessProbe;
-                    },
-                },
-            }),
-        ],
-
-        // Resource detection automatically sets resource attributes.
-        autoDetectResources: true,
-        resource: new Resource({
-            [SemanticResourceAttributes.SERVICE_NAME]: 'lightdash',
-        }),
-        resourceDetectors: [gcpDetector],
-    });
-
-    // Start the SDK and gracefully shutdown
-    sdk.start();
-    process.on('SIGTERM', () => {
-        sdk.shutdown()
-            .then(() => console.log('OpenTelemetry SDK has been shutdown'))
-            .catch((error) =>
-                console.log('Error shutting down OpenTelemetry SDK', error),
-            )
-            .finally(() => process.exit(0));
-    });
+if (process.env.OTEL_SDK_DISABLED !== 'true') {
+    diag.setLogger(new DiagConsoleLogger(), core.getEnv().OTEL_LOG_LEVEL);
 }
+
+const sdk = new NodeSDK({
+    // Exporters are auto configured based on environment variables
+
+    // Instrumentations enable tracing/profiling of specific libraries.
+    // Auto instrumentation includes http,express,knex,pg,winston
+    instrumentations: [
+        getNodeAutoInstrumentations({
+            '@opentelemetry/instrumentation-http': {
+                ignoreIncomingRequestHook(req) {
+                    // Ignore liveness checks
+                    const isLivenessProbe =
+                        !!req?.url?.match(/^\/api\/v1\/livez$/);
+                    return isLivenessProbe;
+                },
+            },
+        }),
+    ],
+
+    // Resource detection automatically sets resource attributes.
+    autoDetectResources: true,
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: 'lightdash',
+    }),
+    resourceDetectors: [gcpDetector],
+});
+
+// Start the SDK and gracefully shutdown
+try {
+    sdk.start();
+    diag.info('OpenTelemetry SDK started successfully');
+} catch (e) {
+    diag.error('Error starting OpenTelemetry SDK. No telemetry exporting', e);
+}
+process.on('SIGTERM', () => {
+    sdk.shutdown()
+        .then(() => console.log('OpenTelemetry SDK has been shutdown'))
+        .catch((error) =>
+            console.log('Error shutting down OpenTelemetry SDK', error),
+        );
+});
