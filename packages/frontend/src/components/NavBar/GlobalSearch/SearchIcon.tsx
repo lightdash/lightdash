03c8eb6d6355c51683aa8c84f8bb3f635898411e
Joao Viana <joao@lightdash.com>
2023-05-30 09:09:27 +0100

    refactor: remove unnecessary color prop

--- packages/frontend/src/components/NavBar/GlobalSearch/SearchIcon.tsx
+++ packages/frontend/src/components/NavBar/GlobalSearch/SearchIcon.tsx
@@ -8,52 +8,37 @@ import {
     IconTable,
 } from '@tabler/icons-react';
 import { FC } from 'react';
-import { MantineIconProps } from '../../common/MantineIcon';
 import { getChartIcon, IconBox } from '../../common/ResourceView/ResourceIcon';
 import { SearchItem } from './hooks';
 
-type SearchIconProps = Omit<MantineIconProps, 'icon'> & {
+type SearchIconProps = {
     searchItem: SearchItem;
 };
 
-export const SearchIcon: FC<SearchIconProps> = ({
-    searchItem,
-    ...iconProps
-}) => {
+export const SearchIcon: FC<SearchIconProps> = ({ searchItem }) => {
     switch (searchItem.type) {
         case 'field':
             return (
                 <IconBox
-                    color="black"
+                    color="gray.7"
                     icon={
                         searchItem.typeLabel.toLowerCase() === 'dimension'
                             ? IconAbc
                             : Icon123
                     }
-                    {...iconProps}
                 />
             );
         case 'dashboard':
-            return (
-                <IconBox
-                    icon={IconLayoutDashboard}
-                    color="green.8"
-                    {...iconProps}
-                />
-            );
+            return <IconBox icon={IconLayoutDashboard} color="green.8" />;
         case 'saved_chart':
             return getChartIcon(
                 (searchItem.item as SavedChartSearchResult)?.chartType,
             );
         case 'space':
-            return (
-                <IconBox icon={IconFolder} color="violet.8" {...iconProps} />
-            );
+            return <IconBox icon={IconFolder} color="violet.8" />;
         case 'table':
-            return <IconBox icon={IconTable} color="blue.8" {...iconProps} />;
+            return <IconBox icon={IconTable} color="blue.8" />;
         case 'page':
-            return (
-                <IconBox icon={IconAppWindow} color="black" {...iconProps} />
-            );
+            return <IconBox icon={IconAppWindow} color="gray.7" />;
     }
 };
