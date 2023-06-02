6afd82f087da2a587cb7ca3325c1e5384f1754fd
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-06-02 16:40:47 +0400

    fix: revert

--- packages/backend/src/projectAdapters/projectAdapter.ts
+++ packages/backend/src/projectAdapters/projectAdapter.ts
@@ -31,9 +31,7 @@ export const projectAdapterFromConfig = async (
         case DbtProjectType.DBT:
             return new DbtLocalCredentialsProjectAdapter({
                 warehouseClient,
-                projectDir:
-                    config.project_dir ||
-                    '/Users/irakli/code/lightdash/examples/full-jaffle-shop-demo/dbt',
+                projectDir: config.project_dir || '/usr/app/dbt',
                 warehouseCredentials,
                 targetName: config.target,
                 environment: config.environment,
