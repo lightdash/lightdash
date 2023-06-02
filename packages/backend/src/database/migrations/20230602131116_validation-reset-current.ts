36e6429ff2cfa6f955a19d88c5c1a26ecf8be58f
Joao Viana <joao@lightdash.com>
2023-06-02 14:17:29 +0100

    feat: add new migration to reset validation

---
+++ packages/backend/src/database/migrations/20230602131116_validation-reset-current.ts
@@ -0,0 +1,46 @@
+import { Knex } from 'knex';
+
+const ValidationTableName = 'validations';
+
+export async function up(knex: Knex): Promise<void> {
+    if (!(await knex.schema.hasTable(ValidationTableName))) {
+        await knex.schema.createTable(ValidationTableName, (table) => {
+            table.specificType(
+                'validation_id',
+                'INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY',
+            );
+            table
+                .timestamp('created_at', { useTz: false })
+                .notNullable()
+                .defaultTo(knex.fn.now());
+            table
+                .uuid('saved_chart_uuid')
+                .references('saved_query_uuid')
+                .inTable('saved_queries')
+                .onDelete('CASCADE');
+            table
+                .uuid('dashboard_uuid')
+                .references('dashboard_uuid')
+                .inTable('dashboards')
+                .onDelete('CASCADE');
+            table.string('error').notNullable();
+            table
+                .uuid('project_uuid')
+                .references('project_uuid')
+                .inTable('projects')
+                .onDelete('CASCADE');
+            table.string('error_type').nullable();
+            table.string('chart_name').nullable();
+            table.string('field_name').nullable();
+            table.string('model_name').nullable();
+
+            table.index(['project_uuid']);
+        });
+    } else {
+        await knex.schema.dropTableIfExists(ValidationTableName);
+    }
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists(ValidationTableName);
+}
