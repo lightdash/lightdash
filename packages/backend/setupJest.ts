13f05a22d81886572488a3c89d8837851b2e4f92
oliverlaslett <olaslett@gmail.com>
2023-06-05 10:19:52 +0100

    fix mocked config

--- packages/backend/setupJest.ts
+++ packages/backend/setupJest.ts
@@ -1,6 +1,7 @@
 import { LightdashMode } from '@lightdash/common';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { LightdashAnalytics } from './src/analytics/LightdashAnalytics';
+import { LightdashConfig } from './src/config/parseConfig';
 
 enableFetchMocks();
 
@@ -8,6 +9,11 @@ jest.mock('./src/config/lightdashConfig', () => ({
     lightdashConfig: {
         mode: LightdashMode.DEFAULT,
         database: {},
+        logging: {
+            level: 'debug',
+            outputs: 'console',
+            format: 'pretty',
+        },
     },
 }));
 
