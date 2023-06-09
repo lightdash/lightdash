e7c5a10cb24bca446ff5f304edcda9f1969b297d
Elton Okawa <elton.okawa@gmail.com>
2023-06-09 10:47:15 -0300

    fix: memoize sql runner button

--- packages/frontend/src/components/Explorer/SqlCard/OpenInSqlRunnerButton.tsx
+++ packages/frontend/src/components/Explorer/SqlCard/OpenInSqlRunnerButton.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import { useCompliedSql } from '../../../hooks/useCompiledSql';
 import LinkButton from '../../common/LinkButton';
 
@@ -6,23 +6,23 @@ interface OpenInSqlRunnerButtonProps {
     projectUuid: string;
 }
 
-const OpenInSqlRunnerButton: FC<OpenInSqlRunnerButtonProps> = ({
-    projectUuid,
-}) => {
-    const { data, isLoading, error } = useCompliedSql();
-    const searchParams = new URLSearchParams({
-        sql_runner: JSON.stringify({ sql: data ?? '{}' }),
-    });
+const OpenInSqlRunnerButton: FC<OpenInSqlRunnerButtonProps> = memo(
+    ({ projectUuid }) => {
+        const { data, isLoading, error } = useCompliedSql();
+        const searchParams = new URLSearchParams({
+            sql_runner: JSON.stringify({ sql: data ?? '{}' }),
+        });
 
-    return (
-        <LinkButton
-            href={`/projects/${projectUuid}/sqlRunner?${searchParams.toString()}`}
-            text="Open in SQL Runner"
-            icon="console"
-            disabled={isLoading || !!error}
-            minimal
-        />
-    );
-};
+        return (
+            <LinkButton
+                href={`/projects/${projectUuid}/sqlRunner?${searchParams.toString()}`}
+                text="Open in SQL Runner"
+                icon="console"
+                disabled={isLoading || !!error}
+                minimal
+            />
+        );
+    },
+);
 
 export default OpenInSqlRunnerButton;
