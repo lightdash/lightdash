d7b81929d38d590f7b6ef958a9bdd37e00a1f9d8
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2023-06-11 15:45:57 +0530

    fix: remove unwanted footer text for unsupported
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

--- packages/frontend/src/components/ProjectConnection/ProjectConnectFlow/UnsupportedWarehouse.tsx
+++ packages/frontend/src/components/ProjectConnection/ProjectConnectFlow/UnsupportedWarehouse.tsx
@@ -4,7 +4,9 @@ import { FC } from 'react';
 import { FloatingBackButton } from '../../../pages/CreateProject.styles';
 import { EventName } from '../../../types/Events';
 import LinkButton from '../../common/LinkButton';
-import { ConnectWarehouseWrapper, Wrapper } from './ProjectConnectFlow.styles';
+import { ProjectCreationCard } from '../../common/Settings/SettingsCard';
+import InviteExpertFooter from './InviteExpertFooter';
+import { Wrapper } from './ProjectConnectFlow.styles';
 
 interface UnsupportedWarehouseProps {
     onBack: () => void;
@@ -19,7 +21,7 @@ const UnsupportedWarehouse: FC<UnsupportedWarehouseProps> = ({ onBack }) => {
                 onClick={onBack}
             />
 
-            <ConnectWarehouseWrapper>
+            <ProjectCreationCard>
                 <NonIdealState
                     icon="error"
                     title={
@@ -61,7 +63,7 @@ const UnsupportedWarehouse: FC<UnsupportedWarehouseProps> = ({ onBack }) => {
                         </LinkButton>
                     }
                 />
-            </ConnectWarehouseWrapper>
+            </ProjectCreationCard>
         </Wrapper>
     );
 };
