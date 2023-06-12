d6b7f6f3ce23e1f8a3b000e70fdcc0dc4743a47c
Jose Rego <jose@lightdash.com>
2023-06-12 16:38:26 +0200

    feat: use mantine icon

--- packages/frontend/src/components/Explorer/SqlCard/OpenInSqlRunnerButton.tsx
+++ packages/frontend/src/components/Explorer/SqlCard/OpenInSqlRunnerButton.tsx
@@ -1,6 +1,8 @@
-import { FC, memo } from 'react';
+import { IconTerminal2 } from '@tabler/icons-react';
+import React, { FC, memo } from 'react';
 import { useCompiledSql } from '../../../hooks/useCompiledSql';
 import LinkButton from '../../common/LinkButton';
+import MantineIcon from '../../common/MantineIcon';
 
 interface OpenInSqlRunnerButtonProps {
     projectUuid: string;
@@ -10,14 +12,14 @@ const OpenInSqlRunnerButton: FC<OpenInSqlRunnerButtonProps> = memo(
     ({ projectUuid }) => {
         const { data, isLoading, error } = useCompiledSql();
         const searchParams = new URLSearchParams({
-            sql_runner: JSON.stringify({ sql: data ?? '{}' }),
+            sql_runner: JSON.stringify({ sql: data ?? '' }),
         });
 
         return (
             <LinkButton
                 href={`/projects/${projectUuid}/sqlRunner?${searchParams.toString()}`}
                 text="Open in SQL Runner"
-                icon="console"
+                icon={<MantineIcon icon={IconTerminal2} />}
                 disabled={isLoading || !!error}
                 minimal
             />
