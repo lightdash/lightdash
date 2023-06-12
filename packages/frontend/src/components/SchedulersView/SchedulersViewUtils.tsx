e2d21b6d9f8487907c6e06a30182535f3d969495
yasmineabdelkefi <yasmine@lightdash.com>
2023-06-12 11:51:27 +0200

    fix: changing to YYYY/MM/DD

--- packages/frontend/src/components/SchedulersView/SchedulersViewUtils.tsx
+++ packages/frontend/src/components/SchedulersView/SchedulersViewUtils.tsx
@@ -13,6 +13,7 @@ import {
     IconPhoto,
     IconProgress,
 } from '@tabler/icons-react';
+import moment from 'moment';
 import React from 'react';
 import MantineIcon from '../common/MantineIcon';
 import { IconBox } from '../common/ResourceIcon';
@@ -124,14 +125,8 @@ export const getItemLink = (item: SchedulerItem, projectUuid: string) => {
         : `/projects/${projectUuid}/dashboards/${item.dashboardUuid}/view`;
 };
 
-export const formatTime = (date: Date) => {
-    return new Date(date).toLocaleString('en-GB', {
-        timeZone: 'UTC',
-        dateStyle: 'short',
-        timeStyle: 'short',
-        hour12: true,
-    });
-};
+export const formatTime = (date: Date) =>
+    moment(date).format('YYYY/MM/DD HH:mm A');
 
 export const camelCaseToFlat = (str: string | undefined) =>
     str ? str.replace(/([A-Z])/g, ' $1').toLowerCase() : undefined;
