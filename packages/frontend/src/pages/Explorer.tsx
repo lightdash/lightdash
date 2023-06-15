cfde4ccfbbc94f12810586dac31d8d26e8cb5a3e
Elton Okawa <elton.okawa@gmail.com>
2023-06-14 21:20:18 -0300

    feat: add clear query shortcut

--- packages/frontend/src/pages/Explorer.tsx
+++ packages/frontend/src/pages/Explorer.tsx
@@ -2,6 +2,7 @@ import { subject } from '@casl/ability';
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { useHotkeys } from '@mantine/hooks';
 import Page from '../components/common/Page/Page';
 import Explorer from '../components/Explorer';
 import ExploreSideBar from '../components/Explorer/ExploreSideBar/index';
@@ -24,6 +25,10 @@ const ExplorerWithUrlParams = memo(() => {
         (context) => context.state.unsavedChartVersion.tableName,
     );
     const { data } = useExplore(tableId);
+
+    const clearQuery = useExplorerContext((context) => context.actions.clear);
+    useHotkeys([['mod + alt + k', clearQuery]]);
+
     return (
         <Page
             title={data ? data?.label : 'Tables'}
