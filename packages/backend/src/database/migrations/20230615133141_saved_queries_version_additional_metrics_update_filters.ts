57f61fee70a7bd8f109fe57897920184ebc893ef
Joao Viana <joao@lightdash.com>
2023-06-16 13:23:25 +0100

    feat: update migration file to add uuids when null

--- packages/backend/src/database/migrations/20230615133141_saved_queries_version_additional_metrics_update_filters.ts
+++ packages/backend/src/database/migrations/20230615133141_saved_queries_version_additional_metrics_update_filters.ts
@@ -1,4 +1,5 @@
 import { Knex } from 'knex';
+import { v4 as uuidv4 } from 'uuid';
 
 const SavedChartAdditionalMetricsTableName =
     'saved_queries_version_additional_metrics';
@@ -7,10 +8,25 @@ export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable(
         SavedChartAdditionalMetricsTableName,
         (tableBuilder) => {
-            tableBuilder.text('filters').nullable();
+            tableBuilder.jsonb('filters').nullable();
             tableBuilder.string('base_dimension_name').nullable();
-            tableBuilder.string('id').nullable();
+            tableBuilder
+                .uuid('uuid')
+                .notNullable()
+                .defaultTo(knex.raw('uuid_generate_v4()'))
+                .unique();
         },
+    );
+
+    const rows = await knex(SavedChartAdditionalMetricsTableName).whereNull(
+        'uuid',
+    );
+    await Promise.all(
+        rows.map((row) =>
+            knex(SavedChartAdditionalMetricsTableName)
+                .where('uuid', row.uuid)
+                .update({ uuid: uuidv4() }),
+        ),
     );
 }
 
@@ -18,7 +34,7 @@ export async function down(knex: Knex): Promise<void> {
     await knex.schema.alterTable(
         SavedChartAdditionalMetricsTableName,
         (tableBuilder) => {
-            tableBuilder.dropColumns('filters', 'base_dimension_name', 'id');
+            tableBuilder.dropColumns('filters', 'base_dimension_name', 'uuid');
         },
     );
 }
