f6cc0d4891617904c087a99faa1217489c6741f5
Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>
2023-06-19 14:33:22 +0900

    fix: add the path parameter `projectUuid` to `DeleteValidationDismiss`
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -5784,6 +5784,12 @@ export function RegisterRoutes(app: express.Router) {
             next: any,
         ) {
             const args = {
+                projectUuid: {
+                    in: 'path',
+                    name: 'projectUuid',
+                    required: true,
+                    dataType: 'string',
+                },
                 validationId: {
                     in: 'path',
                     name: 'validationId',
