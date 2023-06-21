9b4cac6814836a8dfaae79e74132369bce8d076a
Joao Viana <joao@lightdash.com>
2023-06-21 09:57:59 +0100

    fix: process 5 rows at a time

--- packages/backend/src/database/migrations/20230619163121_cached_explores_rename_fieldid_to_fieldref.ts
+++ packages/backend/src/database/migrations/20230619163121_cached_explores_rename_fieldid_to_fieldref.ts
@@ -3,90 +3,111 @@ import { Knex } from 'knex';
 
 const CachedExploresTableName = 'cached_explores';
 const exploresColumnName = 'explores';
+// NOTE: Process 5 rows at a time to avoid memory issues
+const CHUNK_SIZE = 5;
 
 export async function up(knex: Knex): Promise<void> {
-    const rows = await knex(CachedExploresTableName).select();
+    let offset = 0;
+    let rows;
+    do {
+        // eslint-disable-next-line no-await-in-loop
+        rows = await knex(CachedExploresTableName)
+            .select()
+            .limit(CHUNK_SIZE)
+            .offset(offset);
 
-    const updates = rows.map(async (row) => {
-        const parsedExplores = row[exploresColumnName];
+        const updates = rows.map(async (row) => {
+            const parsedExplores = row[exploresColumnName];
 
-        parsedExplores.forEach((explore: Explore) => {
-            if (explore && explore.tables) {
-                Object.values(explore.tables).forEach((table) => {
-                    if (table && table.metrics) {
-                        Object.values(table.metrics).forEach((metric) => {
-                            if (Array.isArray(metric.filters)) {
-                                // eslint-disable-next-line no-param-reassign
-                                metric.filters = metric.filters.map(
-                                    (filter) => ({
-                                        ...filter,
-                                        target: {
-                                            // @ts-expect-error migration from old fieldId to new fieldRef
-                                            fieldRef: filter.target.fieldId,
-                                        },
-                                    }),
-                                );
-                            }
-                        });
-                    }
-                });
-            }
+            parsedExplores.forEach((explore: Explore) => {
+                if (explore && explore.tables) {
+                    Object.values(explore.tables).forEach((table) => {
+                        if (table && table.metrics) {
+                            Object.values(table.metrics).forEach((metric) => {
+                                if (Array.isArray(metric.filters)) {
+                                    // eslint-disable-next-line no-param-reassign
+                                    metric.filters = metric.filters.map(
+                                        (filter) => ({
+                                            ...filter,
+                                            target: {
+                                                fieldRef:
+                                                    // @ts-expect-error migration from old fieldId to new fieldRef
+                                                    filter.target.fieldId,
+                                            },
+                                        }),
+                                    );
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+
+            const updatedExplores = JSON.stringify(parsedExplores);
+
+            await knex(CachedExploresTableName)
+                .where({ project_uuid: row.project_uuid })
+                .update({ [exploresColumnName]: updatedExplores });
         });
+        // eslint-disable-next-line no-await-in-loop
+        await Promise.all(updates);
 
-        const updatedExplores = JSON.stringify(parsedExplores);
-
-        await knex(CachedExploresTableName)
-            .where({ project_uuid: row.project_uuid })
-            .update({ [exploresColumnName]: updatedExplores });
-    });
-
-    await Promise.all(updates);
+        offset += CHUNK_SIZE;
+    } while (rows.length === CHUNK_SIZE);
 }
 
 export async function down(knex: Knex): Promise<void> {
-    const rows = await knex(CachedExploresTableName).select();
+    let offset = 0;
+    let rows;
+    do {
+        // eslint-disable-next-line no-await-in-loop
+        rows = await knex(CachedExploresTableName)
+            .select()
+            .limit(CHUNK_SIZE)
+            .offset(offset);
 
-    const updates = rows.map(async (row) => {
-        const parsedExplores = row[exploresColumnName];
+        const updates = rows.map(async (row) => {
+            const parsedExplores = row[exploresColumnName];
 
-        parsedExplores.forEach((explore: Explore) => {
-            if (explore && explore.tables) {
-                Object.values(explore.tables).forEach((table) => {
-                    if (table && table.metrics) {
-                        Object.values(table.metrics).forEach((metric) => {
-                            if (Array.isArray(metric.filters)) {
-                                // @ts-expect-error migration from new fieldRef to old fieldId
-                                // eslint-disable-next-line no-param-reassign
-                                metric.filters = metric.filters.map(
-                                    (filter) => {
-                                        if (
-                                            filter.target &&
-                                            filter.target.fieldRef
-                                        ) {
-                                            return {
-                                                ...filter,
-                                                target: {
-                                                    fieldId:
-                                                        filter.target.fieldRef,
-                                                },
-                                            };
-                                        }
-                                        return filter;
-                                    },
-                                );
-                            }
-                        });
-                    }
-                });
-            }
+            parsedExplores.forEach((explore: Explore) => {
+                if (explore && explore.tables) {
+                    Object.values(explore.tables).forEach((table) => {
+                        if (table && table.metrics) {
+                            Object.values(table.metrics).forEach((metric) => {
+                                if (Array.isArray(metric.filters)) {
+                                    // @ts-expect-error migration from new fieldRef to old fieldId
+                                    // eslint-disable-next-line no-param-reassign
+                                    metric.filters = metric.filters.map(
+                                        (filter) => {
+                                            if (filter.target.fieldRef) {
+                                                return {
+                                                    ...filter,
+                                                    target: {
+                                                        fieldId:
+                                                            filter.target
+                                                                .fieldRef,
+                                                    },
+                                                };
+                                            }
+                                            return filter;
+                                        },
+                                    );
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+
+            const updatedExplores = JSON.stringify(parsedExplores);
+
+            await knex(CachedExploresTableName)
+                .where({ project_uuid: row.project_uuid })
+                .update({ [exploresColumnName]: updatedExplores });
         });
+        // eslint-disable-next-line no-await-in-loop
+        await Promise.all(updates);
 
-        const updatedExplores = JSON.stringify(parsedExplores);
-
-        await knex(CachedExploresTableName)
-            .where({ project_uuid: row.project_uuid })
-            .update({ [exploresColumnName]: updatedExplores });
-    });
-
-    await Promise.all(updates);
+        offset += CHUNK_SIZE;
+    } while (rows.length === CHUNK_SIZE);
 }
