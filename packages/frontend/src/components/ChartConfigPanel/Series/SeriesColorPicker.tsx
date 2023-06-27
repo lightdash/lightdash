df27dcb0c6651fa8f674defcb82cfe356bdecae0
Joao Viana <joao@lightdash.com>
2023-06-27 21:51:12 +0100

    fix: fix z-index with series colours

--- packages/frontend/src/components/ChartConfigPanel/Series/SeriesColorPicker.tsx
+++ packages/frontend/src/components/ChartConfigPanel/Series/SeriesColorPicker.tsx
@@ -1,12 +1,10 @@
-import { Colors, Icon } from '@blueprintjs/core';
-import { Popover2 } from '@blueprintjs/popover2';
 import { ECHARTS_DEFAULT_COLORS } from '@lightdash/common';
+import { ActionIcon, Center, Popover } from '@mantine/core';
+import { IconDropletFilled } from '@tabler/icons-react';
 import { FC } from 'react';
 import { BlockPicker, ColorResult } from 'react-color';
-import { useToggle } from 'react-use';
-
 import { useOrganization } from '../../../hooks/organization/useOrganization';
-import { ColorButton, ColorButtonInner } from './Series.styles';
+import MantineIcon from '../../common/MantineIcon';
 
 type Props = {
     color?: string;
@@ -14,14 +12,38 @@ type Props = {
 };
 
 const SeriesColorPicker: FC<Props> = ({ color, onChange }) => {
-    const [isOpen, toggle] = useToggle(false);
     const { data } = useOrganization();
 
     const colors = data?.chartColors || ECHARTS_DEFAULT_COLORS;
     return (
-        <Popover2
-            isOpen={isOpen}
-            content={
+        <Popover
+            closeOnClickOutside
+            width={200}
+            position="bottom"
+            withArrow
+            shadow="md"
+        >
+            <Popover.Target>
+                <ActionIcon
+                    bg={color}
+                    sx={{
+                        ':hover': {
+                            backgroundColor: color,
+                        },
+                    }}
+                >
+                    <Center>
+                        {!color && (
+                            <MantineIcon
+                                size={16}
+                                color="gray.5"
+                                icon={IconDropletFilled}
+                            />
+                        )}
+                    </Center>
+                </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown>
                 <BlockPicker
                     color={color}
                     colors={colors}
@@ -29,26 +51,8 @@ const SeriesColorPicker: FC<Props> = ({ color, onChange }) => {
                         onChange(result.hex);
                     }}
                 />
-            }
-            position="bottom"
-            hasBackdrop
-            backdropProps={{
-                onClick: (e) => {
-                    e.stopPropagation();
-                    toggle(false);
-                },
-            }}
-        >
-            <ColorButton onClick={toggle}>
-                <ColorButtonInner
-                    style={{
-                        backgroundColor: color,
-                    }}
-                >
-                    {!color && <Icon icon="tint" color={Colors.GRAY3} />}
-                </ColorButtonInner>
-            </ColorButton>
-        </Popover2>
+            </Popover.Dropdown>
+        </Popover>
     );
 };
 
