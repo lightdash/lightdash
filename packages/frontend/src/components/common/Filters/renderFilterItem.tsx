da131a6ba247cf8d8a483762783e7ee65d3ef909
Joao Viana <joao@lightdash.com>
2023-06-30 10:15:12 +0100

    fix: revert "feat: display item description popover on renderFilterItem (#6077)"
    This reverts commit 61dfbee24ba8cffa97055702184e5d8fc04992e1.

--- packages/frontend/src/components/common/Filters/renderFilterItem.tsx
+++ packages/frontend/src/components/common/Filters/renderFilterItem.tsx
@@ -4,41 +4,10 @@ import {
     Field,
     getItemId,
     getItemLabelWithoutTableName,
-    isField,
     TableCalculation,
 } from '@lightdash/common';
-import { Tooltip } from '@mantine/core';
-import { forwardRef, ReactNode } from 'react';
 import FieldIcon from './FieldIcon';
 import FieldLabel from './FieldLabel';
-
-const FilterItem = forwardRef<
-    HTMLDivElement,
-    {
-        item: Field | TableCalculation;
-        active: boolean;
-        disabled: boolean;
-        text: ReactNode;
-        handleFocus: (() => void) | undefined;
-        handleClick: React.MouseEventHandler<HTMLElement>;
-    }
->(({ item, active, disabled, handleClick, handleFocus, text }, ref) => {
-    return (
-        <div ref={ref}>
-            <MenuItem2
-                key={getItemId(item)}
-                roleStructure="listoption"
-                shouldDismissPopover={false}
-                active={active}
-                disabled={disabled}
-                icon={<FieldIcon item={item} />}
-                onClick={handleClick}
-                onFocus={handleFocus}
-                text={text}
-            />
-        </div>
-    );
-});
 
 export const renderFilterItem: ItemRenderer<Field | TableCalculation> = (
     item,
@@ -48,20 +17,17 @@ export const renderFilterItem: ItemRenderer<Field | TableCalculation> = (
         return null;
     }
     return (
-        <Tooltip
-            withinPortal
-            label={isField(item) ? item.description : ''}
-            disabled={!isField(item) || (isField(item) && !item.description)}
-        >
-            <FilterItem
-                item={item}
-                active={modifiers.active}
-                disabled={modifiers.disabled}
-                text={<FieldLabel item={item} />}
-                handleClick={handleClick}
-                handleFocus={handleFocus}
-            />
-        </Tooltip>
+        <MenuItem2
+            key={getItemId(item)}
+            roleStructure="listoption"
+            shouldDismissPopover={false}
+            active={modifiers.active}
+            disabled={modifiers.disabled}
+            icon={<FieldIcon item={item} />}
+            text={<FieldLabel item={item} />}
+            onClick={handleClick}
+            onFocus={handleFocus}
+        />
     );
 };
 
@@ -72,19 +38,16 @@ export const renderFilterItemWithoutTableName: ItemRenderer<
         return null;
     }
     return (
-        <Tooltip
-            label={isField(item) ? item.description : ''}
-            withinPortal
-            disabled={!isField(item) || (isField(item) && !item.description)}
-        >
-            <FilterItem
-                item={item}
-                active={modifiers.active}
-                disabled={modifiers.disabled}
-                text={getItemLabelWithoutTableName(item)}
-                handleClick={handleClick}
-                handleFocus={handleFocus}
-            />
-        </Tooltip>
+        <MenuItem2
+            key={getItemId(item)}
+            roleStructure="listoption"
+            shouldDismissPopover={false}
+            active={modifiers.active}
+            disabled={modifiers.disabled}
+            icon={<FieldIcon item={item} />}
+            text={getItemLabelWithoutTableName(item)}
+            onClick={handleClick}
+            onFocus={handleFocus}
+        />
     );
 };
