92463c56521c3e23e06fd5a94c1892e927a741bf
Joao Viana <joao@lightdash.com>
2023-06-30 13:58:36 +0100

    feat: change from tooltip to popover

--- packages/frontend/src/components/Explorer/SavedChartsHeader/SavedChartInfoTooltip.tsx
+++ packages/frontend/src/components/Explorer/SavedChartsHeader/SavedChartInfoTooltip.tsx
@@ -1,9 +1,8 @@
 import { SavedChart } from '@lightdash/common';
-import { Anchor, List, Space, Text, Tooltip } from '@mantine/core';
+import { ActionIcon, Anchor, List, Popover, Space, Text } from '@mantine/core';
 import { IconInfoCircle } from '@tabler/icons-react';
 import { FC } from 'react';
 import { useDashboardsContainingChart } from '../../../hooks/dashboard/useDashboards';
-import { useTooltipControlOpen } from '../../../hooks/tooltip/useTooltipControlOpen';
 import MantineIcon from '../../common/MantineIcon';
 
 type Props = { savedChart: SavedChart; projectUuid: string };
@@ -12,30 +11,27 @@ export const SavedChartInfoTooltip: FC<Props> = ({
     savedChart,
     projectUuid,
 }) => {
-    const {
-        tooltipProps: { sx, isOpen, handleMouseEnter, handleMouseLeave },
-        tooltipLabelProps: { handleLabelMouseEnter, handleLabelMouseLeave },
-    } = useTooltipControlOpen();
     const { data: relatedDashboards } = useDashboardsContainingChart(
         projectUuid,
         savedChart.uuid,
     );
 
     return (
-        <Tooltip
-            sx={sx}
-            opened={isOpen}
-            p="sm"
-            maw={250}
+        <Popover
+            offset={-2}
+            width={200}
             position="bottom"
-            offset={-1}
-            multiline
+            withArrow
+            shadow="md"
             withinPortal
-            label={
-                <div
-                    onMouseEnter={handleLabelMouseEnter}
-                    onMouseLeave={handleLabelMouseLeave}
-                >
+        >
+            <Popover.Target>
+                <ActionIcon>
+                    <MantineIcon icon={IconInfoCircle} color="gray.6" />
+                </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown>
+                <div>
                     {savedChart.description && (
                         <>
                             <Text fz="xs" fw={600} color="gray.6">
@@ -45,26 +41,16 @@ export const SavedChartInfoTooltip: FC<Props> = ({
                         </>
                     )}
                     <>
-                        {savedChart.description && <Space h={4} />}
+                        {savedChart.description && <Space h={8} />}
                         <Text fw={600} fz="xs" color="gray.6">
                             Used in {relatedDashboards?.length ?? 0} dashboard
                             {relatedDashboards?.length === 1 ? '' : 's'}
                         </Text>
                         {!!relatedDashboards?.length && (
-                            <List
-                                size="xs"
-                                styles={{
-                                    item: {
-                                        '::marker': {
-                                            color: 'white',
-                                        },
-                                    },
-                                }}
-                            >
+                            <List size="xs">
                                 {relatedDashboards.map(({ uuid, name }) => (
                                     <List.Item key={uuid}>
                                         <Anchor
-                                            color="white"
                                             href={`/projects/${projectUuid}/dashboards/${uuid}/view`}
                                         >
                                             {name}
@@ -75,14 +61,7 @@ export const SavedChartInfoTooltip: FC<Props> = ({
                         )}
                     </>
                 </div>
-            }
-        >
-            <MantineIcon
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                icon={IconInfoCircle}
-                color="gray.6"
-            />
-        </Tooltip>
+            </Popover.Dropdown>
+        </Popover>
     );
 };
