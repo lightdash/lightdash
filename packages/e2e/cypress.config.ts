8264c8278e29f9a9d7947d8eca9f1daa0a5f96d6
Jose Rego <jose@lightdash.com>
2023-07-03 13:55:48 +0100

    chore: amend import

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'cypress';
-import cypressSplit from 'cypress-split';
+import { cypressSplit } from 'cypress-split';
 
 export default defineConfig({
     viewportWidth: 1080,
