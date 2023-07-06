0cd7002f8f71e4bc5ac1c2322632e9026df8e4ac
Jay Anand <jay015341@gmail.com>
2023-07-07 01:56:04 +0530

    Update edit tile content

--- packages/frontend/src/components/DashboardTiles/TileForms/ChartTileForm.tsx
+++ packages/frontend/src/components/DashboardTiles/TileForms/ChartTileForm.tsx
@@ -1,24 +1,22 @@
+import { Select } from '@mantine/core';
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { useChartSummaries } from '../../../hooks/useChartSummaries';
-import SelectField from '../../ReactHookForm/Select';
 
 const ChartTileForm: FC = () => {
     const { projectUuid } = useParams<{ projectUuid: string }>();
     const { data, isLoading } = useChartSummaries(projectUuid);
     const allSavedCharts = data || [];
+
     return (
-        <SelectField
-            key={allSavedCharts[0]?.uuid} // Note: force re-render when saved queries load
-            name="savedChartUuid"
+        <Select
+            id="savedChartUuid"
             label="Select a saved chart"
-            options={allSavedCharts.map(({ uuid, name }) => ({
+            data={allSavedCharts.map(({ uuid, name }) => ({
                 value: uuid,
                 label: name,
             }))}
-            rules={{
-                required: 'Required field',
-            }}
+            required
             defaultValue={allSavedCharts[0]?.uuid}
             disabled={isLoading}
         />
