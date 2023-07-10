ecadc9cd940c96e8c7b27b52171b44dceb01789e
Jose Rego <jose@lightdash.com>
2023-07-10 16:24:58 +0100

    chore: static lightdash schemas

--- packages/common/src/dbt/validation.ts
+++ packages/common/src/dbt/validation.ts
@@ -7,132 +7,30 @@ import {
     DbtModelNode,
     DbtRawModelNode,
 } from '../types/dbt';
-import { ParseError, UnexpectedServerError } from '../types/errors';
-import assertUnreachable from '../utils/assertUnreachable';
+import { ParseError } from '../types/errors';
 import lightdashMetadataSchema from './schemas/lightdashMetadata.json';
+import lightdashSchemaV8 from './schemas/lightdashV8.json';
+import lightdashSchemaV9 from './schemas/lightdashV9.json';
 import dbtManifestSchemaV8 from './schemas/manifestV8.json';
 import dbtManifestSchemaV9 from './schemas/manifestV9.json';
 
-const LightdashMetadataSchemaRef =
-    'https://schemas.lightdash.com/dbt/manifest.json';
-
-const getDbtManifestWithLightdashMetadata = (dbtSchemaRef: string) => ({
-    definitions: {
-        LightdashNodeConfig: {
-            allOf: [
-                {
-                    $ref: `${dbtSchemaRef}#/definitions/NodeConfig`,
-                },
-                {
-                    type: 'object',
-                    properties: {
-                        meta: {
-                            $ref: '#/definitions/LightdashModelMetadata',
-                        },
-                    },
-                },
-            ],
-        },
-        LightdashColumnInfo: {
-            allOf: [
-                {
-                    $ref: `${dbtSchemaRef}#/definitions/ColumnInfo`,
-                },
-                {
-                    type: 'object',
-                    properties: {
-                        meta: {
-                            $ref: '#/definitions/LightdashColumnMetadata',
-                        },
-                    },
-                },
-            ],
-        },
-        LightdashCompiledModelNode: {
-            allOf: [
-                {
-                    $ref: `${dbtSchemaRef}#/definitions/ModelNode`,
-                },
-                {
-                    type: 'object',
-                    properties: {
-                        meta: {
-                            $ref: '#/definitions/LightdashModelMetadata',
-                        },
-                        config: {
-                            $ref: '#/definitions/LightdashNodeConfig',
-                        },
-                    },
-                },
-            ],
-        },
-    },
-    $schema: 'http://json-schema.org/draft-07/schema#',
-    $id: LightdashMetadataSchemaRef,
+const ajv = new Ajv({
+    schemas: [
+        dbtManifestSchemaV8,
+        dbtManifestSchemaV9,
+        lightdashMetadataSchema,
+        lightdashSchemaV8,
+        lightdashSchemaV9,
+    ],
 });
 
-const getValidatorDefinition = (manifestVersion: DbtManifestVersion) => {
-    let dbtManifestSchema;
-    switch (manifestVersion) {
-        case DbtManifestVersion.V8:
-            dbtManifestSchema = dbtManifestSchemaV8;
-            break;
-        case DbtManifestVersion.V9:
-            dbtManifestSchema = dbtManifestSchemaV9;
-            break;
-        default:
-            return assertUnreachable(
-                manifestVersion,
-                new UnexpectedServerError(
-                    `Missing dbt manifest version "${manifestVersion}" in validation.`,
-                ),
-            );
-    }
-
-    const ajv = new Ajv({
-        schemas: [
-            getDbtManifestWithLightdashMetadata(dbtManifestSchema.$id),
-            lightdashMetadataSchema,
-            dbtManifestSchema,
-        ],
-    });
-
-    addFormats(ajv);
-
-    return ajv;
-};
-
-const getValidator = <T>(ajv: Ajv, schemaRef: string) => {
-    const validator = ajv.getSchema<T>(schemaRef);
-    if (validator === undefined) {
-        throw new ParseError(
-            `Could not find schema with reference: ${schemaRef}`,
-        );
-    }
-    return validator;
-};
-
-const formatAjvErrors = (validator: AnyValidateFunction): string =>
-    (validator.errors || [])
-        .map((err) => `Field at "${err.instancePath}" ${err.message}`)
-        .join('\n');
+addFormats(ajv);
 
 export class ManifestValidator {
-    private readonly dbtMetricValidator: ValidateFunction<DbtMetric>;
-
-    private readonly modelValidator: ValidateFunction<DbtModelNode>;
+    private readonly schemaId: string;
 
     constructor(manifestVersion: DbtManifestVersion) {
-        const ajv = getValidatorDefinition(manifestVersion);
-        this.dbtMetricValidator = getValidator<DbtMetric>(
-            ajv,
-            `${LightdashMetadataSchemaRef}#/definitions/LightdashMetric`,
-        );
-
-        this.modelValidator = getValidator<DbtModelNode>(
-            ajv,
-            `${LightdashMetadataSchemaRef}#/definitions/LightdashCompiledModelNode`,
-        );
+        this.schemaId = `https://schemas.lightdash.com/lightdash/${manifestVersion}.json`;
     }
 
     static isValid = (
@@ -141,18 +39,41 @@ export class ManifestValidator {
     ): [true, undefined] | [false, string] => {
         const isValid = validator(data);
         if (!isValid) {
-            return [false, formatAjvErrors(validator)];
+            return [false, ManifestValidator.formatAjvErrors(validator)];
         }
         return [true, undefined];
     };
 
+    static formatAjvErrors = (validator: AnyValidateFunction): string =>
+        (validator.errors || [])
+            .map((err) => `Field at "${err.instancePath}" ${err.message}`)
+            .join('\n');
+
+    static getValidator = <T>(schemaRef: string) => {
+        const validator = ajv.getSchema<T>(schemaRef);
+        if (validator === undefined) {
+            throw new ParseError(
+                `Could not find schema with reference: ${schemaRef}`,
+            );
+        }
+        return validator;
+    };
+
     isModelValid = (
         model: DbtRawModelNode,
-    ): [true, undefined] | [false, string] =>
-        ManifestValidator.isValid(this.modelValidator, model);
+    ): [true, undefined] | [false, string] => {
+        const validator = ManifestValidator.getValidator<DbtModelNode>(
+            `${this.schemaId}#/definitions/LightdashCompiledModelNode`,
+        );
+        return ManifestValidator.isValid(validator, model);
+    };
 
     isDbtMetricValid = (
         metric: DbtMetric,
-    ): [true, undefined] | [false, string] =>
-        ManifestValidator.isValid(this.dbtMetricValidator, metric);
+    ): [true, undefined] | [false, string] => {
+        const validator = ManifestValidator.getValidator<DbtMetric>(
+            `${this.schemaId}#/definitions/LightdashMetric`,
+        );
+        return ManifestValidator.isValid(validator, metric);
+    };
 }
