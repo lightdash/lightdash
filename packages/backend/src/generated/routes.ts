7d43f51e708c9ab3a1a691401aaa93d163a38d9e
Yasmine <yasmine@lightdash.com>
2023-07-18 10:28:06 +0200

    feat: create chart at tile creation (#6331)
    * feat: create tile at chart creation
    
    * fix: missing arg error
    
    * fix: unit tests
    
    * fix: creating tile after chart creation
    
    * feat: e2e test added
    
    * fix: create tile with created chart uuid
    
    * fix: remove newChartData from dashboard tile type
    
    * tests: add test
    
    * chore: fix format
    
    ---------
    
    Co-authored-by: Jose Rego <jose@lightdash.com>

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -1986,6 +1986,11 @@ const models: TsoaRoute.Models = {
         },
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    Format: {
+        dataType: 'refEnum',
+        enums: ['km', 'mi', 'usd', 'gbp', 'eur', 'id', 'percent'],
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     ConditionalOperator: {
         dataType: 'refEnum',
         enums: [
@@ -2037,7 +2042,7 @@ const models: TsoaRoute.Models = {
             hidden: { dataType: 'boolean' },
             round: { dataType: 'double' },
             compact: { ref: 'CompactOrAlias' },
-            format: { dataType: 'string' },
+            format: { ref: 'Format' },
             table: { dataType: 'string', required: true },
             name: { dataType: 'string', required: true },
             index: { dataType: 'double' },
