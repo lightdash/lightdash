f53fc47748efe6711b63e4ab80dd5e9f8c05bdd0
magnew <mcagnew@gmail.com>
2023-07-20 17:46:34 +0200

    feat: update table column config to use mantine (#6418)
    * feat: update table column config to use mantine
    
    * add todo comment

--- packages/frontend/src/components/VisualizationConfigs/TableConfigPanel/ColumnConfiguration.tsx
+++ packages/frontend/src/components/VisualizationConfigs/TableConfigPanel/ColumnConfiguration.tsx
@@ -1,13 +1,13 @@
-import { Button, Classes, InputGroup } from '@blueprintjs/core';
-import { Tooltip2 } from '@blueprintjs/popover2';
 import { isDimension } from '@lightdash/common';
+import { ActionIcon, Box, Group, TextInput, Tooltip } from '@mantine/core';
 import {
     IconEye,
     IconEyeOff,
     IconLock,
     IconLockOpen,
 } from '@tabler/icons-react';
-import React from 'react';
+import React, { useState } from 'react';
+import MantineIcon from '../../common/MantineIcon';
 import { useVisualizationContext } from '../../LightdashVisualization/VisualizationProvider';
 
 const ColumnConfiguration: React.FC<{ fieldId: string }> = ({ fieldId }) => {
@@ -23,90 +23,109 @@ const ColumnConfiguration: React.FC<{ fieldId: string }> = ({ fieldId }) => {
         },
     } = useVisualizationContext();
 
+    const [isShowTooltipVisible, setShowTooltipVisible] = useState(false);
+    const [isFreezeTooltipVisible, setFreezeTooltipVisible] = useState(false);
+
     const field = getField(fieldId);
     const isPivotingDimension = pivotDimensions?.includes(fieldId);
     const disableHidingDimensions = pivotDimensions && isDimension(field);
 
     return (
-        <InputGroup
-            key={fieldId}
-            fill
-            disabled={!isColumnVisible(fieldId) && !disableHidingDimensions}
-            defaultValue={getFieldLabelOverride(fieldId)}
-            placeholder={getFieldLabelDefault(fieldId)}
-            onBlur={(e) => {
-                updateColumnProperty(fieldId, {
-                    name: e.currentTarget.value,
-                });
-            }}
-            rightElement={
-                <>
-                    <Tooltip2
-                        position="top-right"
-                        content={
-                            isPivotingDimension
-                                ? "Can't hide pivot dimensions"
-                                : disableHidingDimensions
-                                ? 'Cannot hide dimensions when pivoting'
-                                : isColumnVisible(fieldId)
-                                ? 'Hide column'
-                                : 'Show column'
+        <Group spacing="xs" noWrap style={{ width: '100%' }}>
+            <TextInput
+                sx={{ flexGrow: 1 }}
+                key={fieldId}
+                disabled={!isColumnVisible(fieldId) && !disableHidingDimensions}
+                defaultValue={getFieldLabelOverride(fieldId)}
+                placeholder={getFieldLabelDefault(fieldId)}
+                onBlur={(e) => {
+                    updateColumnProperty(fieldId, {
+                        name: e.currentTarget.value,
+                    });
+                }}
+            />
+            <Tooltip
+                position="top"
+                opened={isShowTooltipVisible}
+                label={
+                    isPivotingDimension
+                        ? "Can't hide pivot dimensions"
+                        : disableHidingDimensions
+                        ? 'Cannot hide dimensions when pivoting'
+                        : isColumnVisible(fieldId)
+                        ? 'Hide column'
+                        : 'Show column'
+                }
+            >
+                <Box
+                    onMouseEnter={() => setShowTooltipVisible(true)}
+                    onMouseLeave={() => setShowTooltipVisible(false)}
+                >
+                    <ActionIcon
+                        disabled={
+                            disableHidingDimensions ||
+                            pivotDimensions?.includes(fieldId)
                         }
+                        variant="light"
+                        onClick={() => {
+                            // TODO: render perf issues on this page seem to be
+                            // causing the tooltips to stay open. This click causes a
+                            // lot of re-rendering and it's easy to move the mouse away
+                            // before getting mouse events back, so the tooltip
+                            // stays open. We could try to solve that wholistically,
+                            // but for now work around it by managing the tooltip
+                            // and closing it when the button is clicked.
+                            setShowTooltipVisible(false);
+                            if (!disableHidingDimensions) {
+                                updateColumnProperty(fieldId, {
+                                    visible: !isColumnVisible(fieldId),
+                                });
+                            }
+                        }}
                     >
-                        <Button
-                            className={
-                                disableHidingDimensions ||
-                                pivotDimensions?.includes(fieldId)
-                                    ? Classes.DISABLED
-                                    : undefined
-                            }
-                            active={!isColumnVisible(fieldId)}
-                            icon={
-                                isColumnVisible(fieldId) ? (
-                                    <IconEyeOff size={18} />
-                                ) : (
-                                    <IconEye size={18} />
-                                )
-                            }
-                            onClick={() => {
-                                if (!disableHidingDimensions) {
-                                    updateColumnProperty(fieldId, {
-                                        visible: !isColumnVisible(fieldId),
-                                    });
-                                }
-                            }}
-                        />
-                    </Tooltip2>
+                        {isColumnVisible(fieldId) ? (
+                            <MantineIcon icon={IconEyeOff} />
+                        ) : (
+                            <MantineIcon icon={IconEye} />
+                        )}
+                    </ActionIcon>
+                </Box>
+            </Tooltip>
 
-                    {!pivotDimensions ? (
-                        <Tooltip2
-                            position="top-right"
-                            content={
-                                isColumnFrozen(fieldId)
-                                    ? 'Unfreeze column'
-                                    : 'Freeze column'
-                            }
+            {!pivotDimensions ? (
+                <Tooltip
+                    position="top"
+                    opened={isFreezeTooltipVisible}
+                    label={
+                        isColumnFrozen(fieldId)
+                            ? 'Unfreeze column'
+                            : 'Freeze column'
+                    }
+                >
+                    <Box
+                        onMouseEnter={() => setFreezeTooltipVisible(true)}
+                        onMouseLeave={() => setFreezeTooltipVisible(false)}
+                    >
+                        <ActionIcon
+                            variant="light"
+                            onClick={() => {
+                                // Close the tooltip. See comment above.
+                                setFreezeTooltipVisible(false);
+                                updateColumnProperty(fieldId, {
+                                    frozen: !isColumnFrozen(fieldId),
+                                });
+                            }}
                         >
-                            <Button
-                                active={isColumnFrozen(fieldId)}
-                                icon={
-                                    isColumnFrozen(fieldId) ? (
-                                        <IconLock size={18} />
-                                    ) : (
-                                        <IconLockOpen size={18} />
-                                    )
-                                }
-                                onClick={() => {
-                                    updateColumnProperty(fieldId, {
-                                        frozen: !isColumnFrozen(fieldId),
-                                    });
-                                }}
-                            />
-                        </Tooltip2>
-                    ) : null}
-                </>
-            }
-        />
+                            {isColumnFrozen(fieldId) ? (
+                                <MantineIcon icon={IconLock} />
+                            ) : (
+                                <MantineIcon icon={IconLockOpen} />
+                            )}
+                        </ActionIcon>
+                    </Box>
+                </Tooltip>
+            ) : null}
+        </Group>
     );
 };
 
