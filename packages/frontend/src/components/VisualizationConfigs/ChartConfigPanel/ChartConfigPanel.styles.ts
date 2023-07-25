bf029ce2f567246d39462bc252557d8b45b4d059
magnew <mcagnew@gmail.com>
2023-07-25 18:59:36 +0200

    feat: migrate chart config axes tab to mantine (#6472)
    * move axes options out of tabs file
    
    * update axes options to mantine
    
    * PR feedback

--- packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/ChartConfigPanel.styles.ts
+++ packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/ChartConfigPanel.styles.ts
@@ -1,43 +1,5 @@
-import { Colors, FormGroup, Switch } from '@blueprintjs/core';
+import { Colors } from '@blueprintjs/core';
 import styled from 'styled-components';
-
-export const InputWrapper = styled(FormGroup)`
-    margin: 1.357em 0 0;
-
-    & label.bp4-label {
-        display: inline-flex;
-        gap: 0.214em;
-        color: ${Colors.DARK_GRAY1};
-        font-weight: 600;
-    }
-`;
-
-export const MinMaxContainer = styled.div`
-    margin-top: 10px;
-`;
-
-export const MinMaxWrapper = styled.div`
-    display: grid;
-    grid-template-columns: auto auto;
-    gap: 30px;
-`;
-
-export const MinMaxInput = styled(InputWrapper)`
-    margin-top: 0;
-`;
-
-export const AutoRangeSwitch = styled(Switch)`
-    width: fit-content !important;
-`;
-
-export const GridSettings = styled.div`
-    display: flex;
-    flex-direction: row;
-
-    .bp4-label {
-        margin-right: 5px;
-    }
-`;
 
 export const SectionTitle = styled.p`
     color: ${Colors.DARK_GRAY1};
