5462b6410583c3e995309b80ec1d96883faf0659
Joao Viana <joao@lightdash.com>
2023-08-01 12:02:46 +0100

    refactor: move util to its own file

--- packages/frontend/src/components/common/Filters/FilterInputs/DefaultFilterInputs.tsx
+++ packages/frontend/src/components/common/Filters/FilterInputs/DefaultFilterInputs.tsx
@@ -11,6 +11,7 @@ import {
 import isString from 'lodash-es/isString';
 import React from 'react';
 import { useFiltersContext } from '../FiltersProvider';
+import { getPlaceholderByFilterTypeAndOperator } from '../utils/getPlaceholderByFilterTypeAndOperator';
 import MultiAutoComplete from './AutoComplete/MultiAutoComplete';
 import { StyledNumericInput } from './NumericInput.styles';
 
@@ -21,74 +22,6 @@ export type FilterInputsProps<T extends ConditionalRule> = {
     onChange: (value: T) => void;
     popoverProps?: Popover2Props;
     disabled?: boolean;
-};
-
-const getPlaceholderByFilterTypeAndOperator = (
-    filterType: FilterType,
-    filterOperator: FilterOperator,
-) => {
-    switch (filterType) {
-        case FilterType.NUMBER:
-            switch (filterOperator) {
-                case FilterOperator.EQUALS:
-                case FilterOperator.NOT_EQUALS:
-                    return 'Enter value(s)';
-                case FilterOperator.LESS_THAN:
-                case FilterOperator.GREATER_THAN:
-                    return 'Enter value';
-                case FilterOperator.NULL:
-                case FilterOperator.NOT_NULL:
-                default:
-                    return '';
-            }
-        case FilterType.STRING:
-            switch (filterOperator) {
-                case FilterOperator.EQUALS:
-                case FilterOperator.NOT_EQUALS:
-                    return 'Start typing to filter results';
-                case FilterOperator.STARTS_WITH:
-                case FilterOperator.ENDS_WITH:
-                case FilterOperator.INCLUDE:
-                case FilterOperator.NOT_INCLUDE:
-                    return 'Enter value';
-                case FilterOperator.NULL:
-                case FilterOperator.NOT_NULL:
-                default:
-                    return '';
-            }
-        case FilterType.DATE:
-            switch (filterOperator) {
-                case FilterOperator.EQUALS:
-                case FilterOperator.NOT_EQUALS:
-                case FilterOperator.LESS_THAN:
-                case FilterOperator.LESS_THAN_OR_EQUAL:
-                case FilterOperator.GREATER_THAN:
-                case FilterOperator.GREATER_THAN_OR_EQUAL:
-                    return 'Select a date';
-                case FilterOperator.IN_THE_PAST:
-                case FilterOperator.NOT_IN_THE_PAST:
-                case FilterOperator.IN_THE_NEXT:
-                case FilterOperator.IN_THE_CURRENT:
-                    return 'Select a time period';
-                case FilterOperator.IN_BETWEEN:
-                    return 'Start date End date';
-                case FilterOperator.NULL:
-                case FilterOperator.NOT_NULL:
-                default:
-                    return '';
-            }
-        case FilterType.BOOLEAN:
-            switch (filterOperator) {
-                case FilterOperator.EQUALS:
-                    return 'True or False';
-                case FilterOperator.NULL:
-                case FilterOperator.NOT_NULL:
-                default:
-                    return '';
-            }
-        default:
-            return '';
-    }
 };
 
 const DefaultFilterInputs = <T extends ConditionalRule>({
---
+++ packages/frontend/src/components/common/Filters/utils/getPlaceholderByFilterTypeAndOperator.ts
@@ -0,0 +1,69 @@
+import { FilterOperator, FilterType } from '@lightdash/common';
+
+export const getPlaceholderByFilterTypeAndOperator = (
+    filterType: FilterType,
+    filterOperator: FilterOperator,
+) => {
+    switch (filterType) {
+        case FilterType.NUMBER:
+            switch (filterOperator) {
+                case FilterOperator.EQUALS:
+                case FilterOperator.NOT_EQUALS:
+                    return 'Enter value(s)';
+                case FilterOperator.LESS_THAN:
+                case FilterOperator.GREATER_THAN:
+                    return 'Enter value';
+                case FilterOperator.NULL:
+                case FilterOperator.NOT_NULL:
+                default:
+                    return '';
+            }
+        case FilterType.STRING:
+            switch (filterOperator) {
+                case FilterOperator.EQUALS:
+                case FilterOperator.NOT_EQUALS:
+                    return 'Start typing to filter results';
+                case FilterOperator.STARTS_WITH:
+                case FilterOperator.ENDS_WITH:
+                case FilterOperator.INCLUDE:
+                case FilterOperator.NOT_INCLUDE:
+                    return 'Enter value';
+                case FilterOperator.NULL:
+                case FilterOperator.NOT_NULL:
+                default:
+                    return '';
+            }
+        case FilterType.DATE:
+            switch (filterOperator) {
+                case FilterOperator.EQUALS:
+                case FilterOperator.NOT_EQUALS:
+                case FilterOperator.LESS_THAN:
+                case FilterOperator.LESS_THAN_OR_EQUAL:
+                case FilterOperator.GREATER_THAN:
+                case FilterOperator.GREATER_THAN_OR_EQUAL:
+                    return 'Select a date';
+                case FilterOperator.IN_THE_PAST:
+                case FilterOperator.NOT_IN_THE_PAST:
+                case FilterOperator.IN_THE_NEXT:
+                case FilterOperator.IN_THE_CURRENT:
+                    return 'Select a time period';
+                case FilterOperator.IN_BETWEEN:
+                    return 'Start date End date';
+                case FilterOperator.NULL:
+                case FilterOperator.NOT_NULL:
+                default:
+                    return '';
+            }
+        case FilterType.BOOLEAN:
+            switch (filterOperator) {
+                case FilterOperator.EQUALS:
+                    return 'True or False';
+                case FilterOperator.NULL:
+                case FilterOperator.NOT_NULL:
+                default:
+                    return '';
+            }
+        default:
+            return '';
+    }
+};
