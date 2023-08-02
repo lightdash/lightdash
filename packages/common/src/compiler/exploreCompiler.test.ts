7a9c71d47908079e595a0b514f92e3f064ea4533
Javier Rengel <rephus@gmail.com>
2023-08-02 15:50:37 +0200

    fix: fix lightdash variable prefix

--- packages/common/src/compiler/exploreCompiler.test.ts
+++ packages/common/src/compiler/exploreCompiler.test.ts
@@ -233,4 +233,26 @@ describe('Parse dimension reference', () => {
             [],
         );
     });
+
+    test('should parse references with lightdash prefix', () => {
+        expect(
+            parseAllReferences('${lightdash_dimension} == 1', 'table'),
+        ).toStrictEqual([
+            { refName: 'lightdash_dimension}', refTable: 'table' },
+        ]);
+        expect(
+            parseAllReferences(
+                '${lightdash_table.dimension}',
+                'lightdash_table',
+            ),
+        ).toStrictEqual([
+            { refName: 'dimension}', refTable: 'lightdash_table' },
+        ]);
+        expect(
+            parseAllReferences('${ld_dimension} == 1', 'table'),
+        ).toStrictEqual([{ refName: 'ld_dimension}', refTable: 'table' }]);
+        expect(
+            parseAllReferences('${ld_table.ld_dimension} == 1', 'ld_table'),
+        ).toStrictEqual([{ refName: 'ld_dimension}', refTable: 'ld_table' }]);
+    });
 });
