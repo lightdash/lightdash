388ede2b092d09563b2c63fccde7dea0cfb5deea
Joao Viana <joao@lightdash.com>
2023-08-02 11:09:35 +0100

    chore: disable monthAndYearInput when filter is disabled

--- packages/frontend/src/components/common/MonthAndYearInput.tsx
+++ packages/frontend/src/components/common/MonthAndYearInput.tsx
@@ -6,6 +6,7 @@ import YearInput from './YearInput';
 type Props = {
     value: Date;
     onChange: (value: Date) => void;
+    isRuleDisabled?: boolean;
 } & Pick<HTMLSelectProps, 'disabled' | 'placeholder'>;
 
 const months = moment.months();
@@ -15,28 +16,35 @@ const MonthAndYearInput: FC<Props> = ({
     onChange,
     disabled,
     placeholder,
+    isRuleDisabled,
 }) => {
     const utcMonthValue = moment(value).month();
     return (
         <>
-            <HTMLSelect
-                className={disabled ? 'disabled-filter' : ''}
-                disabled={disabled}
-                fill={false}
-                style={{ width: 150 }}
+            {isRuleDisabled ? null : (
+                <HTMLSelect
+                    className={disabled ? 'disabled-filter' : ''}
+                    disabled={disabled}
+                    fill={false}
+                    style={{ width: 150 }}
+                    onChange={(e) =>
+                        onChange(
+                            moment(value).month(e.currentTarget.value).toDate(),
+                        )
+                    }
+                    options={months.map((label, index) => ({
+                        value: index,
+                        label,
+                    }))}
+                    value={utcMonthValue}
+                />
+            )}
+            <YearInput
                 placeholder={placeholder}
-                onChange={(e) =>
-                    onChange(
-                        moment(value).month(e.currentTarget.value).toDate(),
-                    )
-                }
-                options={months.map((label, index) => ({
-                    value: index,
-                    label,
-                }))}
-                value={utcMonthValue}
+                disabled={disabled}
+                value={isRuleDisabled ? null : value}
+                onChange={onChange}
             />
-            <YearInput disabled={disabled} value={value} onChange={onChange} />
         </>
     );
 };
