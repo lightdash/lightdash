5d36b38de554d1d280f724d545a75ca0e07b58eb
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-08-03 17:05:20 +0400

    fix: e2e dates "should use dashboard month filter"

--- packages/e2e/cypress/e2e/dates.cy.ts
+++ packages/e2e/cypress/e2e/dates.cy.ts
@@ -90,9 +90,9 @@ describe('Date tests', () => {
         cy.findByPlaceholderText('Search field...').type(
             'order date month{enter}',
         );
-        cy.contains('button', now.format('MMMM YYYY')).click();
+        cy.contains('button', 'Select a date').click();
         cy.findByRole('dialog').within(() => {
-            cy.contains('button', now.format('YYYY')).click();
+            cy.contains('button', new Date().getFullYear()).click();
             cy.get('button').find('[data-previous="true"]').click();
             cy.contains('button', 2018).click();
             cy.contains('button', 'Feb').click();
