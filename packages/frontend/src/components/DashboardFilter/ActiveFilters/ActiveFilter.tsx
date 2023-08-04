1dba7cffbaca0b3e4f9ab02282c8f0082efa65af
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-08-04 20:54:22 +0400

    wip: dashboard filters simpler view

--- packages/frontend/src/components/DashboardFilter/ActiveFilters/ActiveFilter.tsx
+++ packages/frontend/src/components/DashboardFilter/ActiveFilters/ActiveFilter.tsx
@@ -1,6 +1,7 @@
-import { Popover2, Tooltip2 } from '@blueprintjs/popover2';
 import { DashboardFilterRule, FilterableField } from '@lightdash/common';
-import { FC, useState } from 'react';
+import { Popover, Tooltip } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { FC, useCallback, useState } from 'react';
 import { useDashboardContext } from '../../../providers/DashboardProvider';
 import {
     getConditionalRuleLabel,
@@ -14,7 +15,6 @@ type Props = {
     isEditMode: boolean;
     field: FilterableField;
     filterRule: DashboardFilterRule;
-    onClick?: () => void;
     onRemove: () => void;
     onUpdate: (value: DashboardFilterRule) => void;
 };
@@ -23,12 +23,21 @@ const ActiveFilter: FC<Props> = ({
     isEditMode,
     field,
     filterRule,
-    onClick,
     onRemove,
     onUpdate,
 }) => {
     const { dashboardTiles, allFilterableFields, filterableFieldsByTileUuid } =
         useDashboardContext();
+
+    const [isPopoverOpen, { close: closePopover, open: openPopover }] =
+        useDisclosure();
+    const [_, { close: closeSubPopover, open: openSubPopover }] =
+        useDisclosure();
+
+    const handleClose = useCallback(() => {
+        closeSubPopover();
+        closePopover();
+    }, [closeSubPopover, closePopover]);
 
     const [selectedTabId, setSelectedTabId] = useState<FilterTabs>();
 
@@ -44,50 +53,71 @@ const ActiveFilter: FC<Props> = ({
     );
 
     return (
-        <Popover2
-            lazy
-            placement="bottom-start"
-            content={
-                <FilterModalContainer $wide={selectedTabId === 'tiles'}>
-                    <FilterConfiguration
-                        isEditMode={isEditMode}
-                        tiles={dashboardTiles}
-                        selectedTabId={selectedTabId}
-                        onTabChange={setSelectedTabId}
-                        field={field}
-                        availableTileFilters={filterableFieldsByTileUuid}
-                        filterRule={filterRule}
-                        onSave={onUpdate}
-                    />
-                </FilterModalContainer>
-            }
+        <Popover
+            position="bottom-start"
+            withArrow
+            shadow="md"
+            opened={isPopoverOpen}
+            onClose={handleClose}
         >
-            <TagContainer interactive onRemove={onRemove} onClick={onClick}>
-                <Tooltip2
-                    interactionKind="hover"
-                    placement="bottom-start"
-                    content={
+            <Popover.Target>
+                <Tooltip
+                    withinPortal
+                    position="top-start"
+                    label={
                         filterRuleTables.length === 0
                             ? `Table: ${filterRuleTables[0]}`
                             : `Tables: ${filterRuleTables.join(', ')}`
                     }
                 >
-                    <>
-                        {filterRule.label || filterRuleLabels.field}:{' '}
-                        {filterRule.disabled ? (
-                            <>is any value</>
-                        ) : (
-                            <>
-                                {filterRuleLabels.operator}{' '}
-                                <FilterValues>
-                                    {filterRuleLabels.value}
-                                </FilterValues>
-                            </>
-                        )}
-                    </>
-                </Tooltip2>
-            </TagContainer>
-        </Popover2>
+                    <div>
+                        <TagContainer
+                            interactive
+                            onRemove={onRemove}
+                            onClick={openPopover}
+                        >
+                            {filterRule.label || filterRuleLabels.field}:{' '}
+                            {filterRule.disabled ? (
+                                <>is any value</>
+                            ) : (
+                                <>
+                                    {filterRuleLabels.operator}{' '}
+                                    <FilterValues>
+                                        {filterRuleLabels.value}
+                                    </FilterValues>
+                                </>
+                            )}
+                        </TagContainer>
+                    </div>
+                </Tooltip>
+            </Popover.Target>
+
+            {/* FIXME: remove p={0} once we remove Blueprint popover from FilterSearch */}
+            <Popover.Dropdown p={0}>
+                <FilterModalContainer $wide={selectedTabId === 'tiles'}>
+                    <FilterConfiguration
+                        isActiveFilter
+                        isEditMode={isEditMode}
+                        tiles={dashboardTiles}
+                        selectedTabId={selectedTabId}
+                        field={field}
+                        availableTileFilters={filterableFieldsByTileUuid}
+                        filterRule={filterRule}
+                        onTabChange={setSelectedTabId}
+                        onSave={(dashboardFilterRule) => {
+                            onUpdate(dashboardFilterRule);
+                            handleClose();
+                        }}
+                        popoverProps={{
+                            onOpened: () => openSubPopover(),
+                            onOpening: () => openSubPopover(),
+                            onClose: () => closeSubPopover(),
+                            onClosing: () => closeSubPopover(),
+                        }}
+                    />
+                </FilterModalContainer>
+            </Popover.Dropdown>
+        </Popover>
     );
 };
 
--- packages/frontend/src/components/DashboardFilter/FilterConfiguration/index.tsx
+++ packages/frontend/src/components/DashboardFilter/FilterConfiguration/index.tsx
@@ -1,5 +1,4 @@
-import { Intent, Tab, Tabs } from '@blueprintjs/core';
-import { Classes, Popover2Props, Tooltip2 } from '@blueprintjs/popover2';
+import { Classes, Popover2Props } from '@blueprintjs/popover2';
 
 import {
     applyDefaultTileTargets,
@@ -15,17 +14,12 @@ import {
     matchFieldByTypeAndName,
     matchFieldExact,
 } from '@lightdash/common';
+import { Box, Button, Flex, Group, Tabs, Tooltip } from '@mantine/core';
 import produce from 'immer';
 import { FC, useCallback, useState } from 'react';
 import FieldIcon from '../../common/Filters/FieldIcon';
 import FieldLabel from '../../common/Filters/FieldLabel';
-import SimpleButton from '../../common/SimpleButton';
-import {
-    ActionsWrapper,
-    ApplyButton,
-    ConfigureFilterWrapper,
-    FieldLabelAndIconWrapper,
-} from './FilterConfiguration.styled';
+import { ConfigureFilterWrapper } from './FilterConfiguration.styled';
 import FilterSettings from './FilterSettings';
 import TileFilterConfiguration from './TileFilterConfiguration';
 import { isFilterConfigurationApplyButtonEnabled } from './utils';
@@ -49,6 +43,7 @@ interface Props {
     filterRule?: DashboardFilterRule;
     popoverProps?: Popover2Props;
     selectedTabId?: string;
+    isActiveFilter?: boolean;
     isEditMode: boolean;
     onTabChange: (tabId: FilterTabs) => void;
     onSave: (value: DashboardFilterRule) => void;
@@ -56,6 +51,7 @@ interface Props {
 }
 
 const FilterConfiguration: FC<Props> = ({
+    isActiveFilter = false,
     isEditMode,
     selectedTabId = DEFAULT_TAB,
     tiles,
@@ -138,81 +134,79 @@ const FilterConfiguration: FC<Props> = ({
 
     return (
         <ConfigureFilterWrapper>
-            <FieldLabelAndIconWrapper>
+            <Group spacing="xs">
                 <FieldIcon item={field} />
                 <FieldLabel item={field} />
-            </FieldLabelAndIconWrapper>
+            </Group>
 
             <Tabs
-                selectedTabId={selectedTabId}
-                onChange={onTabChange}
-                renderActiveTabPanelOnly
+                value={selectedTabId}
+                onTabChange={(tabId: FilterTabs) => onTabChange(tabId)}
             >
-                <Tab
-                    id="settings"
-                    title={
-                        <Tooltip2
-                            content="Select the value you want to filter your dimension by"
-                            position="bottom"
+                {isEditMode || !isActiveFilter ? (
+                    <Tabs.List mb="md">
+                        <Tooltip
+                            label="Select the value you want to filter your dimension by"
+                            position="top-start"
                         >
-                            Settings
-                        </Tooltip2>
-                    }
-                    panel={
-                        <FilterSettings
-                            isEditMode={isEditMode}
-                            field={field}
-                            filterRule={internalFilterRule}
-                            onChangeFilterOperator={handleChangeFilterOperator}
-                            onChangeFilterRule={handleChangeFilterRule}
-                            popoverProps={popoverProps}
-                        />
-                    }
-                />
+                            <Tabs.Tab value="settings">Settings</Tabs.Tab>
+                        </Tooltip>
 
-                <Tab
-                    id="tiles"
-                    title={
-                        <Tooltip2
-                            content="Select tiles to apply filter to and which field to filter by"
-                            position="bottom"
+                        <Tooltip
+                            label="Select tiles to apply filter to and which field to filter by"
+                            position="top-start"
                         >
-                            Tiles
-                        </Tooltip2>
-                    }
-                    panel={
-                        <TileFilterConfiguration
-                            field={field}
-                            filterRule={internalFilterRule}
-                            popoverProps={popoverProps}
-                            tiles={tiles}
-                            availableTileFilters={availableTileFilters}
-                            onChange={handleChangeTileConfiguration}
-                        />
-                    }
-                />
+                            <Tabs.Tab value="tiles">Tiles</Tabs.Tab>
+                        </Tooltip>
+                    </Tabs.List>
+                ) : null}
+
+                <Tabs.Panel value="settings">
+                    <FilterSettings
+                        isEditMode={isEditMode}
+                        field={field}
+                        filterRule={internalFilterRule}
+                        onChangeFilterOperator={handleChangeFilterOperator}
+                        onChangeFilterRule={handleChangeFilterRule}
+                        popoverProps={popoverProps}
+                    />
+                </Tabs.Panel>
+
+                <Tabs.Panel value="tiles">
+                    <TileFilterConfiguration
+                        field={field}
+                        filterRule={internalFilterRule}
+                        popoverProps={popoverProps}
+                        tiles={tiles}
+                        availableTileFilters={availableTileFilters}
+                        onChange={handleChangeTileConfiguration}
+                    />
+                </Tabs.Panel>
             </Tabs>
 
-            <ActionsWrapper>
+            <Flex>
                 {onBack && (
-                    <SimpleButton small onClick={onBack}>
+                    <Button size="xs" variant="subtle" onClick={onBack}>
                         Back
-                    </SimpleButton>
+                    </Button>
                 )}
 
-                <ApplyButton
-                    type="submit"
+                <Box sx={{ flexGrow: 1 }} />
+
+                <Button
+                    size="xs"
+                    variant="filled"
                     className={Classes.POPOVER2_DISMISS}
-                    intent={Intent.PRIMARY}
-                    text="Apply"
                     disabled={
                         !isFilterConfigurationApplyButtonEnabled(
                             internalFilterRule,
                         )
                     }
                     onClick={() => onSave(internalFilterRule)}
-                />
-            </ActionsWrapper>
+                >
+                    Apply
+                </Button>
+            </Flex>
         </ConfigureFilterWrapper>
     );
 };
