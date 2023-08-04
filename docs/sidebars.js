a2f6ac415cb25f1156463c3bcbc0ee4ae081a982
oliverlaslett <olaslett@gmail.com>
2023-08-04 16:07:23 +0100

    update sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -187,6 +187,7 @@ module.exports = {
                 ,
                 'references/roles',
                 'references/user-attributes',
+                'references/sql-variables',
             ],
         },
 
