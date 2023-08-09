59bf0baf52b8495732cf142539cb0b5fa93c4a0a
Rephus <rephus@gmail.com>
2023-08-09 17:03:22 +0200

    text migration

--- packages/backend/src/database/migrations/20230809132300_add_google_oauth_token.ts
+++ packages/backend/src/database/migrations/20230809132300_add_google_oauth_token.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('openid_identities', (t) => {
-        t.string('refresh_token').nullable();
+        t.text('refresh_token').nullable();
         t.string('scope').nullable();
     });
 }
