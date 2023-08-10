0ed0e5d196c8ecd9de8a91e3a1aa841b9dcaf24e
Javier Rengel Jiménez <rephus@gmail.com>
2023-08-10 14:45:00 +0200

    Update packages/backend/src/database/migrations/20230809132300_add_google_oauth_token.ts
    Co-authored-by: Oliver Laslett <olaslett@gmail.com>

--- packages/backend/src/database/migrations/20230809132300_add_google_oauth_token.ts
+++ packages/backend/src/database/migrations/20230809132300_add_google_oauth_token.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('openid_identities', (t) => {
         t.text('refresh_token').nullable();
-        t.string('scope').nullable();
+        t.text('scope').nullable();
     });
 }
 
