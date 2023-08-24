45154a40ebe89dec8979fba2d85e4ba76b51d435
Oliver Laslett <olaslett@gmail.com>
2023-08-24 10:42:10 +0000

    chore: add gitpod configuration

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ -z ${PGPASSWORD+x} ]; then
-    echo "Can't find ENV variables (PGPASSWORD), have you loaded '.env' environments variable file?"
-    exit 1
-fi
-
 # delete public schema on db
-PGPASSWORD=$PGPASSWORD psql -p $PGPORT -h $PGHOST -d $PGDATABASE -U $PGUSER -c 'drop schema public cascade; create schema public;'
+psql -c 'drop schema public cascade; create schema public;'
 
 # migrate
 yarn workspace backend migrate
