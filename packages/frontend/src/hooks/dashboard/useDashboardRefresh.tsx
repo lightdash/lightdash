5297873e8c19a9a090ec45785cbcdb1ea498a68a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-08-29 18:25:40 +0400

    fix: dashboard refresh loader

--- packages/frontend/src/hooks/dashboard/useDashboardRefresh.tsx
+++ packages/frontend/src/hooks/dashboard/useDashboardRefresh.tsx
@@ -1,27 +1,39 @@
-import { useIsFetching, useQueryClient } from 'react-query';
+import { useCallback, useMemo } from 'react';
+import { Query, useIsFetching, useQueryClient } from 'react-query';
+
+const QUERIES_TO_REFRESH = [
+    'savedChartResults',
+    'saved_query',
+    'saved_dashboard_query',
+    'dashboards',
+];
+
+const queryPredicate = (query: Query) => {
+    return QUERIES_TO_REFRESH.some((key) => {
+        const firstQueryKey =
+            typeof query.queryKey === 'string'
+                ? query.queryKey
+                : query.queryKey[0];
+        return firstQueryKey === key;
+    });
+};
 
 export const useDashboardRefresh = () => {
     const queryClient = useQueryClient();
 
-    const queryKeysToRefresh = [
-        'savedChartResults',
-        'saved_query',
-        'saved_dashboard_query',
-        'dashboards',
-    ];
+    const isFetching = useIsFetching({ predicate: queryPredicate });
 
-    const isFetching = useIsFetching();
-
-    const invalidateDashboardRelatedQueries = () => {
-        queryKeysToRefresh.map((key) => {
-            return queryClient.invalidateQueries({
-                queryKey: [key],
-            });
+    const invalidateDashboardRelatedQueries = useCallback(() => {
+        return queryClient.invalidateQueries({
+            predicate: queryPredicate,
         });
-    };
+    }, [queryClient]);
 
-    return {
-        invalidateDashboardRelatedQueries,
-        isFetching,
-    };
+    return useMemo(
+        () => ({
+            invalidateDashboardRelatedQueries,
+            isFetching,
+        }),
+        [invalidateDashboardRelatedQueries, isFetching],
+    );
 };
