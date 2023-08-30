7ecfc450459de43b1a9409dfa87c832dce56ac8d
Joao Viana <joao@lightdash.com>
2023-08-30 15:08:05 +0100

    feat: create SyncWithGoogleSheetsModal

---
+++ packages/frontend/src/features/sync/google-sheets/sync-with-google-sheets-modal.tsx
@@ -0,0 +1,9 @@
+import { Modal } from '@mantine/core';
+
+export const SyncWithGoogleSheetsModal = () => {
+    return (
+        <Modal opened onClose={() => {}}>
+            <h1>Sync with Google Sheets</h1>
+        </Modal>
+    );
+};
