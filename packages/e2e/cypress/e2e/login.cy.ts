b1f2a2fc3a4cec673572c0917c5757270229efef
Joao Viana <joao@lightdash.com>
2023-09-01 14:26:52 +0100

    chore: merge 2 login tests into one

--- packages/e2e/cypress/e2e/login.cy.ts
+++ packages/e2e/cypress/e2e/login.cy.ts
@@ -7,6 +7,7 @@ describe('Login', () => {
     beforeEach(() => {
         cy.logout();
     });
+
     it('Should login successfully', () => {
         cy.logout();
         cy.visit('/login');
@@ -19,28 +20,17 @@ describe('Login', () => {
         cy.get('[data-cy="signin-button"]').click();
         cy.url().should('include', '/home');
     });
-    it('Should display error message when email is invalid', () => {
+
+    it('Should display error message when credentials are invalid or not recognised', () => {
         cy.logout();
         cy.visit('/login');
         cy.findByPlaceholderText('Your email address').type('test-email');
         cy.findByPlaceholderText('Your password').type('test-password');
         cy.get('[data-cy="signin-button"]').click();
         cy.findByText('Your email address is not valid').should('be.visible');
-    });
-    it('Should display error message when credentials are invalid', () => {
-        cy.logout();
-        cy.visit('/login');
         cy.findByPlaceholderText('Your email address').type('test@emaill.com');
         cy.findByPlaceholderText('Your password').type('test-password');
         cy.get('[data-cy="signin-button"]').click();
         cy.findByText('Email and password not recognized').should('be.visible');
     });
-    // FIXME: Please fill out this field is a tooltip and Cy can't find it on the UI
-    // it('Should display error message when one field is empty', () => {
-    //     cy.logout();
-    //     cy.visit('/login');
-    //     cy.findByPlaceholderText('Your email address').type('test@mail.com');
-    //     cy.get('[data-cy="signin-button"]').click();
-    //     cy.findByText('Please fill out this field.').should('be.visible');
-    // });
 });
