5d946818ec8e08a85d34ae53a7f3ad6adee729c3
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-09-04 23:51:11 +0530

    fix: add loader on hover of information icon on chart list item

--- packages/frontend/src/components/common/ResourceInfoPopup/DashboardList.tsx
+++ packages/frontend/src/components/common/ResourceInfoPopup/DashboardList.tsx
@@ -1,4 +1,4 @@
-import { Anchor, List, Text } from '@mantine/core';
+import { Anchor, List, Loader, Text } from '@mantine/core';
 import { FC } from 'react';
 import { useDashboardsContainingChart } from '../../../hooks/dashboard/useDashboards';
 
@@ -13,12 +13,14 @@ export const DashboardList: FC<Props> = ({ resourceItemId, projectUuid }) => {
         resourceItemId,
     );
     return (
-        <>
+        <>  
+            { relatedDashboards ?
             <Text fw={600} fz="xs" color="gray.6">
                 Used in {relatedDashboards?.length ?? 0} dashboard
                 {relatedDashboards?.length === 1 ? '' : 's'}
                 {relatedDashboards && relatedDashboards.length > 0 ? ':' : ''}
-            </Text>
+            </Text> : <Loader color="gray" size="xs"/>
+}
             {!!relatedDashboards?.length && (
                 <List size="xs">
                     {relatedDashboards.map(({ uuid, name }) => (
