484d2db2a4adf456b306a03e6ccfcf72f707bc70
Jose Rego <jose@lightdash.com>
2023-09-04 18:32:31 +0100

    feat: metricflow sql card

---
+++ packages/frontend/src/features/metricFlow/components/MetricFlowSqlCode.tsx
@@ -0,0 +1,31 @@
+import { ApiError } from '@lightdash/common';
+import { Loader } from '@mantine/core';
+import { Prism } from '@mantine/prism';
+import React, { FC } from 'react';
+import { useQuery } from 'react-query';
+import { EmptyState } from '../../../components/common/EmptyState';
+import ErrorState from '../../../components/common/ErrorState';
+
+interface Props {
+    status: ReturnType<typeof useQuery>['status'];
+    sql: string | null | undefined;
+    error: ReturnType<typeof useQuery<any, ApiError>>['error'];
+}
+
+const MetricFlowSqlCode: FC<Props> = ({ status, sql, error }) => {
+    if (status === 'loading') {
+        return (
+            <EmptyState title="Loading sql">
+                <Loader color="gray" />
+            </EmptyState>
+        );
+    }
+
+    if (status === 'error') {
+        return <ErrorState error={error?.error} />;
+    }
+
+    return <Prism language="sql">{sql || '# no sql available'}</Prism>;
+};
+
+export default MetricFlowSqlCode;
