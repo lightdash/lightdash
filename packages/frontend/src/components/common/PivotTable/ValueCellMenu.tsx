0ea1483ec5baf9a0861289a16cad98fbe2ef5135
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-09-20 22:25:30 +0400

    feat: light table (#7089)

--- packages/frontend/src/components/common/PivotTable/ValueCellMenu.tsx
+++ packages/frontend/src/components/common/PivotTable/ValueCellMenu.tsx
@@ -142,10 +142,10 @@ const ValueCellMenu: FC<ValueCellMenuProps> = ({
             withinPortal
             shadow="md"
             position="bottom-end"
-            radius="xs"
+            radius={0}
             offset={{
-                mainAxis: 1,
-                crossAxis: 2,
+                mainAxis: 0,
+                crossAxis: 0,
             }}
         >
             <Menu.Target>{children}</Menu.Target>
