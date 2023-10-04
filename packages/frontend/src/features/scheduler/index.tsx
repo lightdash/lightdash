0f9241e5251b6496c9be32b71b4c3d175f3918bd
magnew <mcagnew@gmail.com>
2023-10-04 11:34:08 +0200

    chore: feature flag scheduled deliveries ui (#7294)

--- packages/frontend/src/features/scheduler/index.tsx
+++ packages/frontend/src/features/scheduler/index.tsx
@@ -1,3 +1,5 @@
-export { ChartSchedulersModal } from './components/ChartSchedulersModal';
-export { DashboardSchedulersModal } from './components/DashboardSchedulersModal';
 export { SchedulerDeleteModal } from './components/SchedulerDeleteModal';
+export {
+    ChartSchedulersModal,
+    DashboardSchedulersModal,
+} from './components/SchedulerModals';
