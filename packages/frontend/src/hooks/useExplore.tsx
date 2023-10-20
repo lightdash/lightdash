5d067ed913bce3b847f8dd9f6ac9d9e7d9c270ae
Joao Viana <joao@lightdash.com>
2023-10-20 16:20:56 +0100

    chore: remove feature flag

--- packages/frontend/src/hooks/useExplore.tsx
+++ packages/frontend/src/hooks/useExplore.tsx
@@ -1,5 +1,4 @@
 import { ApiError, ApiExploreResults } from '@lightdash/common';
-import { useFeatureFlagEnabled } from 'posthog-js/react';
 import { useQuery } from 'react-query';
 import { UseQueryOptions } from 'react-query/types/react/types';
 import { useParams } from 'react-router-dom';
@@ -17,9 +16,6 @@ export const useExplore = (
     activeTableName: string | undefined,
     useQueryOptions?: UseQueryOptions<ApiExploreResults, ApiError>,
 ) => {
-    const isStaleTimeFeatureEnabled = useFeatureFlagEnabled(
-        'stale-time-on-use-explore',
-    );
     const { projectUuid } = useParams<{ projectUuid: string }>();
     const setErrorResponse = useQueryError();
     const queryKey = ['tables', activeTableName, projectUuid];
@@ -29,10 +25,8 @@ export const useExplore = (
         enabled: !!activeTableName,
         onError: (result) => setErrorResponse(result),
         retry: false,
-        ...(isStaleTimeFeatureEnabled && {
-            staleTime: 1000 * 60 * 15,
-            cacheTime: 1000 * 60 * 20,
-        }),
+        staleTime: 1000 * 60 * 15,
+        cacheTime: 1000 * 60 * 20,
         ...useQueryOptions,
     });
 };
