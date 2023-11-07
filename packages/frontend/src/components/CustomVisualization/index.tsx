48cc68b0ad22f3a23e36092da61e1a1ec300afe3
Marshall Agnew <marshall@lightdash.com>
2023-11-07 14:53:14 +0000

    thing

--- packages/frontend/src/components/CustomVisualization/index.tsx
+++ packages/frontend/src/components/CustomVisualization/index.tsx
@@ -1,13 +1,42 @@
 import { ResultRow } from '@lightdash/common';
 import EChartsReact from 'echarts-for-react';
-import { createContext, FC, useContext, useMemo } from 'react';
+import {
+    createContext,
+    FC,
+    useCallback,
+    useContext,
+    useMemo,
+    useState,
+} from 'react';
 import { useExplorerContext } from '../../providers/ExplorerProvider';
+
+const defaultValue = {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+        type: 'value',
+    },
+    series: [
+        {
+            // data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)',
+            },
+        },
+    ],
+};
 
 const CustomVisualizationContext = createContext<{
     echartsConfig: any | undefined;
-}>({ echartsConfig: undefined });
+    setChartConfig: (newConfig: any) => void;
+    rows: any;
+}>({ echartsConfig: undefined, setChartConfig: () => {}, rows: undefined });
 
-const useCustomVisualizationContext = () =>
+export const useCustomVisualizationContext = () =>
     useContext(CustomVisualizationContext);
 
 const convertRowsToSeries = (rows: ResultRow[]) => {
@@ -22,39 +51,31 @@ export const CustomVisualizationProvider: FC = ({ children }) => {
         (context) => context.queryResults.data?.rows,
     );
 
-    const echartsConfig = useMemo(() => {
-        if (!rows) {
-            return undefined;
-        }
+    const [echartsConfig, setEchartsConfig] = useState<any>(defaultValue);
 
-        return {
-            dataset: {
-                source: convertRowsToSeries(rows),
-            },
+    const setChartConfig = useCallback(
+        (newConfig: any) => {
+            // TODO: we need to figure out the data source better
+            newConfig.dataset = {
+                source: convertRowsToSeries(rows || []),
+            };
 
-            // dummy config...
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            },
-            yAxis: {
-                type: 'value',
-            },
-            series: [
-                {
-                    data: [120, 200, 150, 80, 70, 110, 130],
-                    type: 'bar',
-                    showBackground: true,
-                    backgroundStyle: {
-                        color: 'rgba(180, 180, 180, 0.2)',
-                    },
-                },
-            ],
-        };
-    }, [rows]);
+            setEchartsConfig(newConfig);
+        },
+        [rows],
+    );
+
+    const value = useMemo(
+        () => ({
+            echartsConfig,
+            setChartConfig,
+            rows,
+        }),
+        [echartsConfig, setChartConfig, rows],
+    );
 
     return (
-        <CustomVisualizationContext.Provider value={{ echartsConfig }}>
+        <CustomVisualizationContext.Provider value={value}>
             {children}
         </CustomVisualizationContext.Provider>
     );
@@ -62,6 +83,12 @@ export const CustomVisualizationProvider: FC = ({ children }) => {
 
 const CustomVisualization: FC = () => {
     const { echartsConfig } = useCustomVisualizationContext();
+
+    console.log('echartsConfig', echartsConfig);
+
+    if (!echartsConfig) {
+        return null;
+    }
 
     return <EChartsReact option={echartsConfig} />;
 };
--- packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/CustomVisConfigTabs.tsx
+++ packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/CustomVisConfigTabs.tsx
@@ -1,7 +1,18 @@
-import { JsonInput, Tabs, Text } from '@mantine/core';
+import { Button, JsonInput, Stack, Tabs, Text } from '@mantine/core';
+import React, { memo, useState } from 'react';
+import { useCustomVisualizationContext } from '../../CustomVisualization';
 
-import React, { memo } from 'react';
 const CustomVisConfigTabs: React.FC = memo(() => {
+    const { echartsConfig, setChartConfig } = useCustomVisualizationContext();
+
+    const [draftConfig, setDraftConfig] = useState<string>(
+        JSON.stringify(echartsConfig, null, 2),
+    );
+
+    const updateChart = () => {
+        setChartConfig(JSON.parse(draftConfig));
+    };
+
     return (
         <Tabs defaultValue="config" keepMounted={false}>
             <Tabs.List mb="sm">
@@ -10,13 +21,18 @@ const CustomVisConfigTabs: React.FC = memo(() => {
             </Tabs.List>
 
             <Tabs.Panel value="config">
-                <JsonInput
-                    label="Chart config"
-                    validationError="Invalid JSON"
-                    formatOnBlur
-                    autosize
-                    minRows={20}
-                />
+                <Stack>
+                    <JsonInput
+                        label="Chart config"
+                        validationError="Invalid JSON"
+                        value={draftConfig}
+                        onChange={(config) => setDraftConfig(config)}
+                        formatOnBlur
+                        autosize
+                        minRows={20}
+                    />
+                    <Button onClick={updateChart}>Make a chart</Button>
+                </Stack>
             </Tabs.Panel>
             <Tabs.Panel value="data">
                 <Text>Transform data</Text>
