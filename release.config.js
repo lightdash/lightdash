aa1c2e7aeefa24cd6e618a085f1a3fe0e3d8ecf2
oliverlaslett <11660098+owlas@users.noreply.github.com>
2023-11-08 17:02:45 +0000

    fix: do not skip ci on release

--- release.config.js
+++ release.config.js
@@ -139,7 +139,7 @@ module.exports = {
           "packages/warehouses/package.json",
         ],
         message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
       },
     ],
     ["@semantic-release/github", {}],
