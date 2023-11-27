6f8e4b5f33e76dbb38f31eaeefd1f19be80e7033
João Viana <joao@lightdash.com>
2023-11-27 14:21:41 +0000

    fix: add undefined value to posthog if no key (#8101)

--- packages/backend/src/postHog.ts
+++ packages/backend/src/postHog.ts
@@ -1,9 +1,8 @@
 import { PostHog } from 'posthog-node';
 import { lightdashConfig } from './config/lightdashConfig';
 
-export const postHogClient = new PostHog(
-    lightdashConfig.posthog.projectApiKey,
-    {
-        host: lightdashConfig.posthog.apiHost,
-    },
-);
+export const postHogClient = lightdashConfig.posthog.projectApiKey
+    ? new PostHog(lightdashConfig.posthog.projectApiKey, {
+          host: lightdashConfig.posthog.apiHost,
+      })
+    : undefined;
