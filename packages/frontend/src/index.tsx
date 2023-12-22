3a0eab9909b9c8b35bb9bcd361c66c7d5d5a0fe0
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2023-12-22 11:45:43 +0400

    chore: upgrade react and rendering

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
+const container = document.getElementById('root');
+if (!container) throw new Error('Root element not found!');
+
+const root = createRoot(container);
+
+root.render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root') as HTMLElement,
+    </StrictMode>,
 );
