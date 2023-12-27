1a9728ae26c8c7d0fd2d8beca5dd6b9eb3325de5
Joao Viana <joao@lightdash.com>
2023-12-27 15:33:08 +0000

    chore: ignore mock tracking provider react-refresh

--- packages/frontend/src/providers/__mocks__/TrackingProvider.tsx
+++ packages/frontend/src/providers/__mocks__/TrackingProvider.tsx
@@ -4,6 +4,7 @@ export const TrackingProvider: FC<any> = ({ children }) => {
     return <>{children}</>;
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useTracking() {
     return jest.fn();
 }
