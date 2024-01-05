b669a5e96aa7e124880cced9d56aa0cceb9dfae5
Joao Viana <joao@lightdash.com>
2024-01-05 17:07:55 +0000

    chore: simplify types on TrackingProvider mock

--- packages/frontend/src/providers/__mocks__/TrackingProvider.tsx
+++ packages/frontend/src/providers/__mocks__/TrackingProvider.tsx
@@ -1,21 +1,17 @@
 import { FC } from 'react';
 
-export const TrackingProvider: FC<React.PropsWithChildren<any>> = ({
-    children,
-}) => {
-    return <>{children}</>;
-};
+export const TrackingProvider: FC<React.PropsWithChildren> = ({ children }) => (
+    <>{children}</>
+);
 
 export function useTracking() {
     return jest.fn();
 }
 
-export const TrackPage: FC<React.PropsWithChildren<any>> = ({ children }) => {
-    return <>{children}</>;
-};
+export const TrackPage: FC<React.PropsWithChildren> = ({ children }) => (
+    <>{children}</>
+);
 
-export const TrackSection: FC<React.PropsWithChildren<any>> = ({
-    children,
-}) => {
-    return <>{children}</>;
-};
+export const TrackSection: FC<React.PropsWithChildren> = ({ children }) => (
+    <>{children}</>
+);
