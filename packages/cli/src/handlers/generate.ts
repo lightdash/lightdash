87ccfc4c5987999e471eaaeeba75776381b65869
oliverlaslett <11660098+owlas@users.noreply.github.com>
2024-01-18 13:59:24 +0000

    feat: allow lightdash generate to target dbt_packages

--- packages/cli/src/handlers/generate.ts
+++ packages/cli/src/handlers/generate.ts
@@ -117,6 +117,7 @@ export const generateHandler = async (options: GenerateHandlerOptions) => {
                     docs: manifest.docs,
                     includeMeta: !options.excludeMeta,
                     projectDir: absoluteProjectPath,
+                    projectName: context.projectName,
                 },
             );
             try {
