e475fec04fd783405e3757f6a33bb43ae3b37f0d
Joao Viana <joao@lightdash.com>
2024-01-19 18:14:16 +0000

    chore: improve types and restructure edit modal

--- packages/frontend/src/components/UserSettings/MyWarehouseConnectionsPanel/EditCredentialsModal.tsx
+++ packages/frontend/src/components/UserSettings/MyWarehouseConnectionsPanel/EditCredentialsModal.tsx
@@ -1,3 +1,4 @@
+import { WarehouseTypes } from '@lightdash/common';
 import {
     Button,
     Group,
@@ -8,22 +9,86 @@ import {
     TextInput,
     Title,
 } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { useForm, UseFormReturnType } from '@mantine/form';
 import { FC } from 'react';
-import { Credentials } from './types';
+import { UpdateUserCredentials, UpsertUserWarehouseCredentials } from './types';
+
+const getCredentials = (credentials: UpsertUserWarehouseCredentials) => {
+    switch (credentials.type) {
+        case WarehouseTypes.REDSHIFT:
+        case WarehouseTypes.SNOWFLAKE:
+        case WarehouseTypes.POSTGRES:
+        case WarehouseTypes.TRINO:
+            return {
+                type: credentials.type,
+                user: credentials.user,
+                password: '',
+            };
+        case WarehouseTypes.BIGQUERY:
+            return {
+                type: credentials.type,
+                keyFileContents: credentials.keyfileContents,
+            };
+        case WarehouseTypes.DATABRICKS:
+            return {
+                type: credentials.type,
+                personalAccessToken: credentials.personalAccessToken,
+            };
+        default:
+            return {};
+    }
+};
+
+const FormInputs: FC<{
+    form: UseFormReturnType<
+        Pick<UpdateUserCredentials, 'name' | 'credentials'>
+    >;
+    userCredentialsType: WarehouseTypes;
+}> = ({ form, userCredentialsType }) => {
+    switch (userCredentialsType) {
+        case WarehouseTypes.REDSHIFT:
+        case WarehouseTypes.SNOWFLAKE:
+        case WarehouseTypes.POSTGRES:
+        case WarehouseTypes.TRINO:
+            return (
+                <>
+                    <TextInput
+                        required
+                        size="xs"
+                        label="Username/email"
+                        {...form.getInputProps('username')}
+                    />
+                    <PasswordInput
+                        required
+                        size="xs"
+                        label="Password"
+                        {...form.getInputProps('password')}
+                    />
+                </>
+            );
+        case WarehouseTypes.BIGQUERY:
+            return <>{/* Add key file content input - JSON? */}</>;
+        case WarehouseTypes.DATABRICKS:
+            return <>{/* Add personal access token input */}</>;
+        default:
+            return null;
+    }
+};
 
 export const EditCredentialsModal: FC<
     Pick<ModalProps, 'opened' | 'onClose'> & {
-        credentials: Pick<Credentials, 'name' | 'username'>;
+        userCredentials: UpdateUserCredentials;
     }
-> = ({ opened, onClose, credentials }) => {
-    const form = useForm({
+> = ({ opened, onClose, userCredentials }) => {
+    const form = useForm<Pick<UpdateUserCredentials, 'name' | 'credentials'>>({
         initialValues: {
-            name: credentials.name,
-            username: credentials.username,
-            password: '', // TODO: Keep empty
+            name: userCredentials.name,
+            credentials: getCredentials(
+                userCredentials.credentials,
+            ) as UpdateUserCredentials['credentials'],
         },
     });
+
     return (
         <Modal
             title={<Title order={4}>Edit credentials</Title>}
@@ -46,18 +111,14 @@ export const EditCredentialsModal: FC<
                             label="Name"
                             {...form.getInputProps('name')}
                         />
-                        <TextInput
-                            required
-                            size="xs"
-                            label="Username/email"
-                            {...form.getInputProps('username')}
+
+                        <FormInputs
+                            form={form}
+                            userCredentialsType={
+                                userCredentials.credentials.type
+                            }
                         />
-                        <PasswordInput
-                            required
-                            size="xs"
-                            label="Password"
-                            {...form.getInputProps('password')}
-                        />
+
                         <Group position="right" spacing="xs" mt="sm">
                             <Button
                                 size="xs"
--- packages/frontend/src/components/UserSettings/MyWarehouseConnectionsPanel/types.ts
+++ packages/frontend/src/components/UserSettings/MyWarehouseConnectionsPanel/types.ts
@@ -1,6 +1,30 @@
-// TODO - to be moved to common
-export type Credentials = {
+import {
+    CreateBigqueryCredentials,
+    CreateDatabricksCredentials,
+    CreatePostgresCredentials,
+    CreateRedshiftCredentials,
+    CreateSnowflakeCredentials,
+    CreateTrinoCredentials,
+} from '@lightdash/common';
+
+export type CreateUserCredentials = {
     name: string;
-    username: string;
-    password: string;
+    credentials: UpsertUserWarehouseCredentials;
 };
+
+export type UpdateUserCredentials = {
+    uuid: string;
+    name: string;
+    credentials: UpsertUserWarehouseCredentials;
+};
+
+export type UpsertUserWarehouseCredentials =
+    | Pick<
+          | CreateRedshiftCredentials
+          | CreatePostgresCredentials
+          | CreateSnowflakeCredentials
+          | CreateTrinoCredentials,
+          'type' | 'user' | 'password'
+      >
+    | Pick<CreateBigqueryCredentials, 'type' | 'keyfileContents'>
+    | Pick<CreateDatabricksCredentials, 'type' | 'personalAccessToken'>;
