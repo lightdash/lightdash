8aea1b9128cd28e39c3648e09f030ff1be3712ce
Joao Viana <joao@lightdash.com>
2024-01-30 11:52:12 +0000

    chore: regenerate

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -737,7 +737,7 @@ const models: TsoaRoute.Models = {
         ],
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    Format: {
+    CurrencyFormat: {
         dataType: 'refEnum',
         enums: [
             'ars',
