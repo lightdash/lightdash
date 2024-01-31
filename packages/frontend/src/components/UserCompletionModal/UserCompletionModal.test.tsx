796c45e599e1baee43e4a250f14aa4d9d3d39068
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2024-01-31 15:51:11 +0400

    Update packages/frontend/src/components/UserCompletionModal/UserCompletionModal.test.tsx
    Co-authored-by: João Viana <joao@lightdash.com>

--- packages/frontend/src/components/UserCompletionModal/UserCompletionModal.test.tsx
+++ packages/frontend/src/components/UserCompletionModal/UserCompletionModal.test.tsx
@@ -34,7 +34,7 @@ describe('UserCompletionModal', () => {
             },
         });
 
-        const welcomeModel = await screen.findByRole('dialog', {
+        const welcomeModal = await screen.findByRole('dialog', {
             name: 'Nearly there... Tell us a bit more about yourself',
         });
         expect(welcomeModel).toBeInTheDocument();
