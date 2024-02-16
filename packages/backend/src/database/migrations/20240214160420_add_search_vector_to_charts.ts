cc261eac05303ec565c9e4f4a63eed51a4f2c36c
Bruno Almeida <almeidabbm@users.noreply.github.com>
2024-02-16 10:00:38 +0000

    feat: add full text search to spaces (#8935)
    * feat: adds fullText search to spaces
    
    * chore: add spaces search_vector to mocks
    
    * chore: call correct function - getFullTextSearchRankCalcSql
    
    * fix: remove code to drop index from migration. It was checking for table with index name which never exists

--- packages/backend/src/database/migrations/20240214160420_add_search_vector_to_charts.ts
+++ packages/backend/src/database/migrations/20240214160420_add_search_vector_to_charts.ts
@@ -28,13 +28,8 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
     if (await knex.schema.hasColumn(SavedChartsTableName, 'search_vector')) {
         await knex.schema.alterTable(SavedChartsTableName, (table) => {
+            // when column is dropped, index is dropped as well
             table.dropColumn('search_vector');
-        });
-    }
-
-    if (await knex.schema.hasTable('charts_search_vector_idx')) {
-        await knex.schema.alterTable(SavedChartsTableName, (table) => {
-            table.dropIndex('charts_search_vector_idx');
         });
     }
 }
