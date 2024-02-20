8811bd26f1ac690845617e1b06423c3975e2ed63
Joao Viana <joao@lightdash.com>
2024-02-20 14:08:41 +0000

    chore: add migration file

---
+++ packages/backend/src/database/migrations/20240220140829_dashboard-tile-comments.ts
@@ -0,0 +1,31 @@
+import { Knex } from 'knex';
+
+const dashboardTileCommentsTable = 'dashboard_tile_comments';
+const usersTable = 'users';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(dashboardTileCommentsTable, (table) => {
+        table
+            .uuid('comment_id')
+            .primary()
+            .defaultTo(knex.raw('uuid_generate_v4()'));
+        table
+            .timestamp('created_at', { useTz: false })
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table.text('text');
+        table.uuid('reply_to').nullable();
+        table
+            .foreign('reply_to')
+            .references(`${dashboardTileCommentsTable}.comment_id`)
+            .onDelete('SET NULL');
+        table.uuid('dashboard_tile_uuid').index();
+        table.uuid('user_uuid');
+        table.foreign('user_uuid').references(`${usersTable}.user_uuid`);
+        table.boolean('resolved').defaultTo(false);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable(dashboardTileCommentsTable);
+}
