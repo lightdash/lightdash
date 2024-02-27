a92d703060260ccec3a10d8a08e9ecefc6ee28b9
Joao Viana <joao@lightdash.com>
2024-02-27 18:48:08 +0000

    chore: update model

--- packages/backend/src/models/NotificationsModel/NotificationsModel.ts
+++ packages/backend/src/models/NotificationsModel/NotificationsModel.ts
@@ -1,6 +1,9 @@
 import { Notification } from '@lightdash/common';
 import { Knex } from 'knex';
-import { DashboardTileCommentsTableName } from '../../database/entities/comments';
+import {
+    DashboardTileCommentsTableName,
+    DbDashboardTileComments,
+} from '../../database/entities/comments';
 import { DashboardsTableName } from '../../database/entities/dashboards';
 import {
     DbNotifications,
@@ -22,13 +25,14 @@ export class NotificationsModel {
     async getDashboardCommentNotifications(
         userUuid: string,
     ): Promise<Notification[]> {
-        const notificationsWithUserAndDashboardAndAuthor: (DbNotifications & {
-            user_first_name: string;
-            user_last_name: string;
-            dashboard_name: string;
-            author_first_name: string;
-            author_last_name: string;
-        })[] = await this.database(NotificationsTableName)
+        const notificationsWithUserAndDashboardAndAuthor: (DbNotifications &
+            Pick<DbDashboardTileComments, 'dashboard_tile_uuid'> & {
+                user_first_name: string;
+                user_last_name: string;
+                dashboard_name: string;
+                author_first_name: string;
+                author_last_name: string;
+            })[] = await this.database(NotificationsTableName)
             .join(
                 UserTableName,
                 `${NotificationsTableName}.user_uuid`,
@@ -36,32 +40,29 @@ export class NotificationsModel {
                 `${UserTableName}.user_uuid`,
             )
             .join(
-                `${UserTableName} as comment_authors`,
-                `${NotificationsTableName}.comment_author_uuid`,
-                '=',
-                'comment_authors.user_uuid',
-            )
-            .join(
-                `${DashboardsTableName}`,
-                `${NotificationsTableName}.dashboard_uuid`,
-                '=',
-                `${DashboardsTableName}.dashboard_uuid`,
-            )
-            .join(
                 DashboardTileCommentsTableName,
                 `${NotificationsTableName}.comment_id`,
                 '=',
                 `${DashboardTileCommentsTableName}.comment_id`,
             )
-            .select(
-                `${NotificationsTableName}.notification_id`,
-                `${NotificationsTableName}.viewed`,
-                `${NotificationsTableName}.created_at`,
+            .join(
+                `${UserTableName} as comment_authors`,
+                `${DashboardTileCommentsTableName}.user_uuid`,
+                '=',
+                'comment_authors.user_uuid',
+            )
+            .join(
+                DashboardsTableName,
                 `${NotificationsTableName}.dashboard_uuid`,
-                `${NotificationsTableName}.dashboard_tile_uuid`,
+                '=',
+                `${DashboardsTableName}.dashboard_uuid`,
+            )
+            .select(
+                `${NotificationsTableName}.*`,
                 `${UserTableName}.first_name as user_first_name`,
                 `${UserTableName}.last_name as user_last_name`,
                 `${DashboardsTableName}.name as dashboard_name`,
+                `${DashboardTileCommentsTableName}.dashboard_tile_uuid`,
                 'comment_authors.first_name as author_first_name',
                 'comment_authors.last_name as author_last_name',
             )
@@ -113,9 +114,6 @@ export class NotificationsModel {
                             await trx(NotificationsTableName).insert({
                                 user_uuid: mentionUserUuid,
                                 comment_id: comment.comment_id,
-                                comment_author_uuid: comment.user_uuid,
-                                dashboard_tile_uuid:
-                                    comment.dashboard_tile_uuid,
                                 dashboard_uuid: dashboardUuid,
                             });
                         }
