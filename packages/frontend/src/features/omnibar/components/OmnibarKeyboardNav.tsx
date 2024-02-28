7d7322192344efab1af779bc5f990619fa9fa9e9
almeidabbm <almeidabatistam@gmail.com>
2024-02-28 17:18:43 +0000

    chore: use useCallback for arrow fns

--- packages/frontend/src/features/omnibar/components/OmnibarKeyboardNav.tsx
+++ packages/frontend/src/features/omnibar/components/OmnibarKeyboardNav.tsx
@@ -1,6 +1,6 @@
 import { SearchItemType } from '@lightdash/common';
 import { Box } from '@mantine/core';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback } from 'react';
 import { FocusedItemIndex, SearchItem } from '../types/searchItem';
 
 interface Props {
@@ -17,118 +17,134 @@ export const OmnibarKeyboardNav = ({
     onEnterPressed,
     onFocusedItemChange,
 }: PropsWithChildren<Props>) => {
-    function handleArrowDown(
-        maxGroupIndex: number,
-        maxCurrentGroupItemIndex: number,
-    ) {
-        if (!currentFocusedItemIndex) {
+    const handleArrowDown = useCallback(
+        (maxGroupIndex: number, maxCurrentGroupItemIndex: number) => {
+            if (!currentFocusedItemIndex) {
+                return {
+                    groupIndex: 0,
+                    itemIndex: 0,
+                };
+            }
+
+            if (currentFocusedItemIndex.itemIndex < maxCurrentGroupItemIndex) {
+                // move to next item in the same group
+                return {
+                    groupIndex: currentFocusedItemIndex.groupIndex,
+                    itemIndex: currentFocusedItemIndex.itemIndex + 1,
+                };
+            }
+
+            if (
+                currentFocusedItemIndex.groupIndex < maxGroupIndex &&
+                currentFocusedItemIndex.itemIndex === maxCurrentGroupItemIndex
+            ) {
+                // move to the first item in the next group
+                return {
+                    groupIndex: currentFocusedItemIndex.groupIndex + 1,
+                    itemIndex: 0,
+                };
+            }
+
             return {
                 groupIndex: 0,
                 itemIndex: 0,
             };
-        }
+        },
+        [currentFocusedItemIndex],
+    );
 
-        if (currentFocusedItemIndex.itemIndex < maxCurrentGroupItemIndex) {
-            // move to next item in the same group
-            return {
-                groupIndex: currentFocusedItemIndex.groupIndex,
-                itemIndex: currentFocusedItemIndex.itemIndex + 1,
-            };
-        }
+    const handleArrowUp = useCallback(
+        (maxGroupIndex: number, lastItemIndex: number) => {
+            if (!currentFocusedItemIndex) {
+                return {
+                    groupIndex: maxGroupIndex,
+                    itemIndex: lastItemIndex,
+                };
+            }
 
-        if (
-            currentFocusedItemIndex.groupIndex < maxGroupIndex &&
-            currentFocusedItemIndex.itemIndex === maxCurrentGroupItemIndex
-        ) {
-            // move to the first item in the next group
-            return {
-                groupIndex: currentFocusedItemIndex.groupIndex + 1,
-                itemIndex: 0,
-            };
-        }
+            if (currentFocusedItemIndex.itemIndex > 0) {
+                // move to previous item in the same group
+                return {
+                    groupIndex: currentFocusedItemIndex.groupIndex,
+                    itemIndex: currentFocusedItemIndex.itemIndex - 1,
+                };
+            }
 
-        return {
-            groupIndex: 0,
-            itemIndex: 0,
-        };
-    }
+            if (
+                currentFocusedItemIndex.groupIndex > 0 &&
+                currentFocusedItemIndex.itemIndex === 0
+            ) {
+                // move to the last item in the previous group
+                const prevGroupIndex = currentFocusedItemIndex.groupIndex - 1;
+                return {
+                    groupIndex: prevGroupIndex,
+                    itemIndex: groupedItems[prevGroupIndex][1].length - 1,
+                };
+            }
 
-    function handleArrowUp(maxGroupIndex: number, lastItemIndex: number) {
-        if (!currentFocusedItemIndex) {
             return {
                 groupIndex: maxGroupIndex,
                 itemIndex: lastItemIndex,
             };
-        }
+        },
+        [currentFocusedItemIndex, groupedItems],
+    );
 
-        if (currentFocusedItemIndex.itemIndex > 0) {
-            // move to previous item in the same group
-            return {
-                groupIndex: currentFocusedItemIndex.groupIndex,
-                itemIndex: currentFocusedItemIndex.itemIndex - 1,
-            };
-        }
+    const onKeyDown = useCallback(
+        (event: React.KeyboardEvent<HTMLDivElement>) => {
+            if (groupedItems.length < 1) {
+                return;
+            }
 
-        if (
-            currentFocusedItemIndex.groupIndex > 0 &&
-            currentFocusedItemIndex.itemIndex === 0
-        ) {
-            // move to the last item in the previous group
-            const prevGroupIndex = currentFocusedItemIndex.groupIndex - 1;
-            return {
-                groupIndex: prevGroupIndex,
-                itemIndex: groupedItems[prevGroupIndex][1].length - 1,
-            };
-        }
+            const maxGroupIndex = groupedItems.length - 1;
+            const lastItemIndex = groupedItems[maxGroupIndex][1].length - 1;
+            const maxCurrentGroupItemIndex = currentFocusedItemIndex
+                ? groupedItems[currentFocusedItemIndex.groupIndex][1].length - 1
+                : -1;
 
-        return {
-            groupIndex: maxGroupIndex,
-            itemIndex: lastItemIndex,
-        };
-    }
+            switch (event.key) {
+                case 'ArrowDown':
+                    event.preventDefault();
 
-    function onKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
-        if (groupedItems.length < 1) {
-            return;
-        }
+                    onFocusedItemChange(
+                        handleArrowDown(
+                            maxGroupIndex,
+                            maxCurrentGroupItemIndex,
+                        ),
+                    );
 
-        const maxGroupIndex = groupedItems.length - 1;
-        const lastItemIndex = groupedItems[maxGroupIndex][1].length - 1;
-        const maxCurrentGroupItemIndex = currentFocusedItemIndex
-            ? groupedItems[currentFocusedItemIndex.groupIndex][1].length - 1
-            : -1;
+                    break;
+                case 'ArrowUp':
+                    event.preventDefault();
 
-        switch (event.key) {
-            case 'ArrowDown':
-                event.preventDefault();
+                    onFocusedItemChange(
+                        handleArrowUp(maxGroupIndex, lastItemIndex),
+                    );
 
-                onFocusedItemChange(
-                    handleArrowDown(maxGroupIndex, maxCurrentGroupItemIndex),
-                );
+                    break;
+                case 'Enter':
+                    event.preventDefault();
 
-                break;
-            case 'ArrowUp':
-                event.preventDefault();
+                    if (currentFocusedItemIndex) {
+                        const item =
+                            groupedItems[currentFocusedItemIndex.groupIndex][1][
+                                currentFocusedItemIndex.itemIndex
+                            ];
+                        onEnterPressed(item);
+                    }
 
-                onFocusedItemChange(
-                    handleArrowUp(maxGroupIndex, lastItemIndex),
-                );
-
-                break;
-            case 'Enter':
-                event.preventDefault();
-
-                if (currentFocusedItemIndex) {
-                    const item =
-                        groupedItems[currentFocusedItemIndex.groupIndex][1][
-                            currentFocusedItemIndex.itemIndex
-                        ];
-                    onEnterPressed(item);
-                }
-
-                break;
-        }
-    }
+                    break;
+            }
+        },
+        [
+            currentFocusedItemIndex,
+            groupedItems,
+            handleArrowDown,
+            handleArrowUp,
+            onEnterPressed,
+            onFocusedItemChange,
+        ],
+    );
 
     return (
         <Box display="contents" onKeyDown={onKeyDown}>
