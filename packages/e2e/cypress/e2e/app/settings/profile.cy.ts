feda5a86ea7891b847602bc5023d9ca31248c618
Jose Rego <ZeRego@users.noreply.github.com>
2024-03-12 09:38:00 +0000

    chore: stabilize profile e2e test (#9329)

--- packages/e2e/cypress/e2e/app/settings/profile.cy.ts
+++ packages/e2e/cypress/e2e/app/settings/profile.cy.ts
@@ -22,8 +22,14 @@ describe('Settings - Profile', () => {
         cy.visit('/');
         cy.findByTestId('user-avatar').click();
         cy.findByRole('menuitem', { name: 'User settings' }).click();
-        cy.get('[data-cy="first-name-input"]').clear().type('Kevin');
-        cy.get('[data-cy="last-name-input"]').clear().type('Space');
+        cy.get('[data-cy="first-name-input"]').should(
+            'have.value',
+            SEED_ORG_1_ADMIN.first_name,
+        ); // wait for form to populate
+        cy.get('[data-cy="first-name-input"]').clear();
+        cy.get('[data-cy="first-name-input"]').type('Kevin');
+        cy.get('[data-cy="last-name-input"]').clear();
+        cy.get('[data-cy="last-name-input"]').type('Space');
         cy.get('[data-cy="update-profile-settings"]').click();
         cy.findByText('Success! User details were updated.').should(
             'be.visible',
