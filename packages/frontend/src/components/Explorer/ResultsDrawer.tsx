218dc392b630dc4de2b62d0d17fb0f82ac90b216
Joao Viana <joao@lightdash.com>
2024-03-12 10:54:14 +0000

    chore: add other buttons

--- packages/frontend/src/components/Explorer/ResultsDrawer.tsx
+++ packages/frontend/src/components/Explorer/ResultsDrawer.tsx
@@ -1,3 +1,4 @@
+import { subject } from '@casl/ability';
 import {
     ActionIcon,
     Affix,
@@ -5,29 +6,82 @@ import {
     Divider,
     Drawer,
     Group,
+    Popover,
     rem,
     UnstyledButton,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { IconArrowDown } from '@tabler/icons-react';
+import { IconArrowDown, IconShare2 } from '@tabler/icons-react';
 import { FC, memo, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { downloadCsv } from '../../api/csv';
+import { uploadGsheet } from '../../hooks/gdrive/useGdrive';
+import { useApp } from '../../providers/AppProvider';
 import { useExplorerContext } from '../../providers/ExplorerProvider';
+import AddColumnButton from '../AddColumnButton';
+import { Can } from '../common/Authorization';
+import {
+    COLLAPSABLE_CARD_BUTTON_PROPS,
+    COLLAPSABLE_CARD_POPOVER_PROPS,
+} from '../common/CollapsableCard';
 import MantineIcon from '../common/MantineIcon';
+import ExportSelector from '../ExportSelector';
 import SortButton from '../SortButton';
 import { ExplorerResults } from './ResultsCard/ExplorerResults';
 
 export const ResultsDrawer: FC = memo(() => {
+    const { user } = useApp();
+    const { projectUuid } = useParams<{ projectUuid: string }>();
+
     const tableName = useExplorerContext(
         (context) => context.state.unsavedChartVersion.tableName,
     );
     const isEditMode = useExplorerContext(
         (context) => context.state.isEditMode,
     );
+    const metricQuery = useExplorerContext(
+        (context) => context.state.unsavedChartVersion.metricQuery,
+    );
+    const columnOrder = useExplorerContext(
+        (context) => context.state.unsavedChartVersion.tableConfig.columnOrder,
+    );
+    const rows = useExplorerContext(
+        (context) => context.queryResults.data?.rows,
+    );
     const sorts = useExplorerContext(
         (context) => context.state.unsavedChartVersion.metricQuery.sorts,
     );
-
     const hasSorts = tableName && sorts.length > 0;
+
+    const resultsData = useExplorerContext(
+        (context) => context.queryResults.data,
+    );
+    const isExportAsCsvDisabled = !resultsData || resultsData.rows.length <= 0;
+
+    const getCsvLink = async (csvLimit: number | null, onlyRaw: boolean) => {
+        const csvResponse = await downloadCsv({
+            projectUuid,
+            tableId: tableName,
+            query: metricQuery,
+            csvLimit,
+            onlyRaw,
+            columnOrder,
+            showTableNames: true,
+        });
+        return csvResponse;
+    };
+
+    const getGsheetLink = async () => {
+        const gsheetResponse = await uploadGsheet({
+            projectUuid,
+            exploreId: tableName,
+            metricQuery,
+            columnOrder,
+            showTableNames: true,
+        });
+        return gsheetResponse;
+    };
+
     const [opened, { open, close }] = useDisclosure(false);
     const [isResizing, setIsResizing] = useState(false);
     const [height, setHeight] = useState(500);
@@ -107,7 +161,6 @@ export const ResultsDrawer: FC = memo(() => {
                         }}
                     />
                 </UnstyledButton>
-
                 <ActionIcon
                     size="xs"
                     variant="default"
@@ -118,7 +171,48 @@ export const ResultsDrawer: FC = memo(() => {
                 >
                     <MantineIcon icon={IconArrowDown} color="gray" />
                 </ActionIcon>
+                {tableName && (
+                    <Group position="right" spacing="xs">
+                        {isEditMode && <AddColumnButton />}
 
+                        <Can
+                            I="manage"
+                            this={subject('ExportCsv', {
+                                organizationUuid: user.data?.organizationUuid,
+                                projectUuid: projectUuid,
+                            })}
+                        >
+                            <Popover
+                                {...COLLAPSABLE_CARD_POPOVER_PROPS}
+                                disabled={isExportAsCsvDisabled}
+                                position="bottom-end"
+                            >
+                                <Popover.Target>
+                                    <Button
+                                        data-testid="export-csv-button"
+                                        {...COLLAPSABLE_CARD_BUTTON_PROPS}
+                                        disabled={isExportAsCsvDisabled}
+                                        px="xs"
+                                    >
+                                        <MantineIcon
+                                            icon={IconShare2}
+                                            color="gray"
+                                        />
+                                    </Button>
+                                </Popover.Target>
+
+                                <Popover.Dropdown>
+                                    <ExportSelector
+                                        projectUuid={projectUuid}
+                                        rows={rows}
+                                        getCsvLink={getCsvLink}
+                                        getGsheetLink={getGsheetLink}
+                                    />
+                                </Popover.Dropdown>
+                            </Popover>
+                        </Can>
+                    </Group>
+                )}
                 <ExplorerResults />
             </Drawer>
             <Group position="center">
