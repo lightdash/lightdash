826ae86c0903068b1a21965f6c467ca1e4729d88
Rephus <rephus@gmail.com>
2024-03-13 08:36:22 +0100

    M  packages/backend/src/controllers/rateLimiter.ts

--- packages/backend/src/controllers/rateLimiter.ts
+++ packages/backend/src/controllers/rateLimiter.ts
@@ -3,7 +3,7 @@ import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per minute).
+    limit: 100, // Limit each IP to 100 requests per `window` (per minute).
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
