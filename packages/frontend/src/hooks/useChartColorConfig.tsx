f901f11d6b9ebdada21c483032c9baef0b445423
Filipe Dobreira <filipe@lightdash.com>
2024-03-13 13:33:49 +0000

    add tweak to color mapping to ensure it iss stateful only to the current path

--- packages/frontend/src/hooks/useChartColorConfig.tsx
+++ packages/frontend/src/hooks/useChartColorConfig.tsx
@@ -1,6 +1,14 @@
 import { type Series } from '@lightdash/common';
 import { useMantineTheme } from '@mantine/core';
-import { createContext, useCallback, useContext, useRef, type FC } from 'react';
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useRef,
+    type FC,
+} from 'react';
+import { useLocation } from 'react-router-dom';
 import { type EChartSeries } from './echarts/useEchartsCartesianConfig';
 
 /**
@@ -28,12 +36,30 @@ const ChartColorMappingContext =
 export const ChartColorMappingContextProvider: FC<
     React.PropsWithChildren<{}>
 > = ({ children }) => {
+    const location = useLocation();
+
     /**
      * Changes to colorMappings are intentionally kept outside the React render loop,
      * we don't want to trigger re-renders for every mapping assignment, and we're
      * creating assignments during the render process anyway.
      */
     const colorMappings = useRef(new Map<string, Map<string, number>>());
+
+    /**
+     * Any time the path changes, if we have any color mappings in the context,
+     * we reset them completely. This prevents things like playing around with
+     * filters, or editing a chart, from 'polluting' the mappings table in
+     * unpredictable ways.
+     *
+     * This could alternatively be implemented as contexts further down the tree,
+     * but this approach ensures mappings are always shared at the highest possible
+     * level regardless of how/where a chart is being rendered.
+     */
+    useEffect(() => {
+        if (colorMappings.current.size > 0) {
+            colorMappings.current = new Map<string, Map<string, number>>();
+        }
+    }, [location.pathname]);
 
     return (
         <ChartColorMappingContext.Provider
