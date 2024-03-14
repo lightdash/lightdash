3b37c25096158eb1e069fc5263b776ba23c12c61
Filipe Dobreira <filipe@lightdash.com>
2024-03-14 11:27:14 +0000

    fix: pre/post-process comment content in backend and frontend (#9359)
    * fix: pre-process comment text on backend + frontend
    
    * fix: adjust sanitize method to allow mentions + improve tests
    
    * cleanup log statement
    
    * add sanity test for double sanitization + malformed tags

--- packages/common/src/utils/sanitizeHtml.test.ts
+++ packages/common/src/utils/sanitizeHtml.test.ts
@@ -5,33 +5,61 @@ describe('sanitizeHtml', () => {
         expect(sanitizeHtml('')).toEqual('');
     });
 
-    test('no html tags', () => {
+    test('no html tags in input', () => {
         expect(sanitizeHtml('Hello this is just some text')).toEqual(
             'Hello this is just some text',
         );
     });
 
-    test('script tag (discard)', () => {
+    test('script tag is discarded', () => {
         expect(sanitizeHtml('<script>console.log("sup");</script>')).toEqual(
             '',
         );
     });
 
-    test('script tag with neighboring text', () => {
+    test('script tag with neighboring text is discarded', () => {
         expect(
             sanitizeHtml('<script>console.log("sup");</script> Some text'),
         ).toEqual(' Some text');
     });
 
-    test('valid tag', () => {
+    test('valid tag is preserved', () => {
         expect(
             sanitizeHtml('<a href="https://www.lightdash.com/">Lightdash</a>'),
         ).toEqual('<a href="https://www.lightdash.com/">Lightdash</a>');
+    });
+
+    test('style tag in span is preserved', () => {
+        expect(
+            sanitizeHtml('<span style="color:red;font-size:24px">@Foo</span>'),
+        ).toEqual('<span style="color:red;font-size:24px">@Foo</span>');
+    });
+
+    test('style tag in paragraph is discarded', () => {
+        expect(
+            sanitizeHtml('<p style="color:red;font-size:24px">@Foo</p>'),
+        ).toEqual('<p>@Foo</p>');
     });
 
     test('valid tag with surrounding + inner text', () => {
         expect(
             sanitizeHtml('Here is some text <p>And a paragraph.</p><br />'),
         ).toEqual('Here is some text <p>And a paragraph.</p><br />');
+    });
+
+    test('double sanitization (with invalid tags)', () => {
+        expect(
+            sanitizeHtml(
+                sanitizeHtml(
+                    '<script>console.log("boo");</script><p><span style="color: red">@Foo</span></p>',
+                ),
+            ),
+        ).toEqual('<p><span style="color:red">@Foo</span></p>');
+    });
+
+    test('malformed tag', () => {
+        expect(sanitizeHtml('<span style="color:red">@Foo')).toEqual(
+            '<span style="color:red">@Foo</span>',
+        );
     });
 });
