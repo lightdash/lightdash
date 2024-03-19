274f2371d54fa2d99a0d253171195ac6789845ed
Joao Viana <joao@lightdash.com>
2024-03-19 10:50:05 +0000

    chore: remove copy-to-clipboard & react-copy-to-clipboard

--- packages/frontend/src/components/ShareLinkButton/index.tsx
+++ packages/frontend/src/components/ShareLinkButton/index.tsx
@@ -1,26 +1,28 @@
 import { ActionIcon } from '@mantine/core';
-import { IconLink } from '@tabler/icons-react';
+import { useClipboard } from '@mantine/hooks';
+import { IconCheck, IconLink } from '@tabler/icons-react';
 import { type FC } from 'react';
-import CopyToClipboard from 'react-copy-to-clipboard';
 import useToaster from '../../hooks/toaster/useToaster';
 import MantineIcon from '../common/MantineIcon';
 
 const ShareLinkButton: FC<{ url: string }> = ({ url }) => {
+    const clipboard = useClipboard({ timeout: 500 });
     const { showToastSuccess } = useToaster();
+
+    const handleCopyClick = async () => {
+        clipboard.copy(url || '');
+        showToastSuccess({
+            title: 'Link copied to clipboard',
+        });
+    };
+
     return (
-        <CopyToClipboard
-            text={url}
-            options={{ message: 'Copied' }}
-            onCopy={() =>
-                showToastSuccess({
-                    title: 'Link copied to clipboard',
-                })
-            }
-        >
-            <ActionIcon variant="default">
-                <MantineIcon icon={IconLink} />
-            </ActionIcon>
-        </CopyToClipboard>
+        <ActionIcon variant="default" onClick={handleCopyClick} color="gray">
+            <MantineIcon
+                icon={clipboard.copied ? IconCheck : IconLink}
+                color={clipboard.copied ? 'green' : undefined}
+            />
+        </ActionIcon>
     );
 };
 
