a3ecddb9f0d59a9bc7b332c0c564b094f68b933e
almeidabbm <almeidabatistam@gmail.com>
2024-03-20 15:55:28 +0000

    fix: format on save

--- packages/backend/src/models/SearchModel/index.ts
+++ packages/backend/src/models/SearchModel/index.ts
@@ -503,7 +503,6 @@ export class SearchModel {
             filters,
         );
 
-
         const explores = await this.getProjectExplores(projectUuid);
         const tableErrors = await this.searchTableErrors(
             projectUuid,
