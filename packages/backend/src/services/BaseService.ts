f9d983dea84b50eb886125e0482f199551878f72
Filipe Dobreira <filipe@lightdash.com>
2024-04-08 12:50:30 +0100

    add support for scoped service logging

--- packages/backend/src/services/BaseService.ts
+++ packages/backend/src/services/BaseService.ts
@@ -1,1 +1,19 @@
-export abstract class BaseService {}
+import Logger from '../logging/logger';
+
+export abstract class BaseService {
+    protected logger: typeof Logger = Logger;
+
+    constructor({
+        logger,
+    }: {
+        logger?: typeof Logger;
+    } = {}) {
+        /**
+         * Logger can be overriden as part of the constructor, e.g to provide a scoped
+         * logger instance.
+         */
+        if (logger) {
+            this.logger = logger;
+        }
+    }
+}
