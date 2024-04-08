dbb3a066d5e19a2b0023a0881cb28de4a60b8bce
Ayush Trivedi <ayush.trivedi@zeeve.io>
2024-04-08 18:08:28 +0530

    fix timezone select

--- packages/frontend/src/components/ReactHookForm/CronInput/TimezoneSelect.tsx
+++ packages/frontend/src/components/ReactHookForm/CronInput/TimezoneSelect.tsx
@@ -1,49 +1,25 @@
-import { Group, Input, NumberInput } from '@mantine/core';
+import { Select } from '@mantine/core';
 import React, { type FC } from 'react';
-import {
-    getMonthlyCronExpression,
-    parseCronExpression,
-} from './cronInputUtils';
-import TimePicker from './TimePicker';
 
-const MonthlyInputs: FC<{
+const TimezoneSelect: FC<{
     disabled?: boolean;
-    cronExpression: string;
-    timeZone: string;
-    onChange: (value: string) => void;
-}> = ({ disabled, cronExpression, onChange, timeZone: Timezone }) => {
-    const { minutes, hours, day } = parseCronExpression(cronExpression);
-
-    const onDayChange = (newDay: number) => {
-        if (newDay >= 1 && newDay <= 31) {
-            onChange(getMonthlyCronExpression(minutes, hours, newDay));
-        }
-    };
-
-    const onTimeChange = (newTime: { minutes: number; hours: number }) => {
-        onChange(getMonthlyCronExpression(newTime.minutes, newTime.hours, day));
-    };
+    value: number;
+    onChange: (value: number) => void;
+}> = ({ disabled, value, onChange }) => {
+    const timezones: string[] = Intl.supportedValuesOf("timeZone");
+    timezones.push('UTC');
 
     return (
-        <Group spacing="sm">
-            <Input.Label>on day</Input.Label>
-            <NumberInput
-                value={day}
-                onChange={onDayChange}
-                disabled={disabled}
-                w="6xl"
-                min={1}
-                max={31}
-            />
-            <Input.Label>at</Input.Label>
-
-            <TimePicker
-                disabled={disabled}
-                cronExpression={cronExpression}
-                onChange={onTimeChange}
-            />
-            {Timezone}
-        </Group>
+        <Select
+            data={timezones}
+            value={value}
+            disabled={disabled}
+            withinPortal
+            w={190}
+            onChange={(val) => {
+                onChange(val);
+            }}
+        />
     );
 };
-export default MonthlyInputs;
+export default TimezoneSelect;
