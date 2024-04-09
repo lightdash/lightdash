ba2f74306da9dcde7ead2fc2d2c8cf4f3f5d3dfa
Jose Rego <jose@lightdash.com>
2024-04-09 18:15:46 +0100

    chore: return user uuid in open id identities

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -5778,7 +5778,7 @@ const models: TsoaRoute.Models = {
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     OpenIdIdentityIssuerType: {
         dataType: 'refEnum',
-        enums: ['google', 'okta', 'oneLogin', 'azuread'],
+        enums: ['google', 'okta', 'oneLogin', 'azuread', 'oidc'],
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     LocalIssuerTypes: {
