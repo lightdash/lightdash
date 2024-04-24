fda95d3ded510ca9b7e552e2ff9f9e27526220c6
Rephus <rephus@gmail.com>
2024-04-24 15:47:12 +0200

    feat: new resource-slugs

---
+++ packages/backend/src/database/migrations/20240424131157_add_slugs.ts
@@ -0,0 +1,36 @@
+import { Knex } from 'knex';
+
+const chartsTable = 'saved_queries';
+const dashboards = 'dashboards';
+const spaces = 'spaces';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable(chartsTable, (table) => {
+        table.string('slug').nullable().index().unique();
+    });
+
+    await knex.schema.alterTable(dashboards, (table) => {
+        table.string('slug').nullable().index().unique();
+    });
+    await knex.schema.alterTable(spaces, (table) => {
+        table.string('slug').nullable().index().unique();
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    if (await knex.schema.hasColumn(chartsTable, 'slug')) {
+        await knex.schema.alterTable(chartsTable, (table) => {
+            table.dropColumn('slug');
+        });
+    }
+    if (await knex.schema.hasColumn(dashboards, 'slug')) {
+        await knex.schema.alterTable(dashboards, (table) => {
+            table.dropColumn('slug');
+        });
+    }
+    if (await knex.schema.hasColumn(spaces, 'slug')) {
+        await knex.schema.alterTable(spaces, (table) => {
+            table.dropColumn('slug');
+        });
+    }
+}
