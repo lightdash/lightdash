ea8e8f3c5219af2a4fc468ccdeb355a4e42b5128
Rephus <rephus@gmail.com>
2024-04-24 10:31:16 +0200

    M  packages/frontend/src/utils/sortUtils.ts

--- packages/frontend/src/utils/sortUtils.ts
+++ packages/frontend/src/utils/sortUtils.ts
@@ -62,8 +62,13 @@ export const getSortLabel = (
     item: Field | TableCalculation | CustomDimension,
     direction: SortDirection,
 ) => {
-    if (isTableCalculation(item) && item.type !== undefined) {
-        switch (item.type) {
+    if (isTableCalculation(item)) {
+        const type = item.type;
+        if (type === undefined)
+            return direction === SortDirection.ASC
+                ? NumericSortLabels.ASC
+                : NumericSortLabels.DESC;
+        switch (type) {
             case TableCalculationType.NUMBER:
                 return direction === SortDirection.ASC
                     ? NumericSortLabels.ASC
@@ -83,7 +88,7 @@ export const getSortLabel = (
                     : BooleanSortLabels.DESC;
             default:
                 return assertUnreachable(
-                    item.type,
+                    type,
                     'Unexpected dimension type when getting sort label',
                 );
         }
@@ -96,7 +101,8 @@ export const getSortLabel = (
     }
 
     if (isDimension(item)) {
-        switch (item.type) {
+        const type = item.type;
+        switch (type) {
             case DimensionType.NUMBER:
                 return direction === SortDirection.ASC
                     ? NumericSortLabels.ASC
@@ -116,12 +122,13 @@ export const getSortLabel = (
                     : BooleanSortLabels.DESC;
             default:
                 return assertUnreachable(
-                    item.type,
+                    type,
                     'Unexpected dimension type when getting sort label',
                 );
         }
     } else if (isMetric(item)) {
-        switch (item.type) {
+        const type = item.type;
+        switch (type) {
             case MetricType.PERCENTILE:
             case MetricType.MEDIAN:
             case MetricType.AVERAGE:
@@ -149,7 +156,7 @@ export const getSortLabel = (
                     : BooleanSortLabels.DESC;
             default:
                 return assertUnreachable(
-                    item.type,
+                    type,
                     'Unexpected metric type when getting sort label',
                 );
         }
