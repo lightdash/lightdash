be9d918d7a14cd58cf29cb226a4151422dad0934
Rephus <rephus@gmail.com>
2024-04-30 16:19:56 +0200

    M  packages/backend/src/database/migrations/20240424131220_populate_slugs.ts

--- packages/backend/src/database/migrations/20240424131220_populate_slugs.ts
+++ packages/backend/src/database/migrations/20240424131220_populate_slugs.ts
@@ -34,9 +34,31 @@ export async function up(knex: Knex): Promise<void> {
   UPDATE spaces s
   SET slug = ${sanitizeSlug(`s."name" `)}
 `);
+
+    // Make slugs notNullable
+    await knex.schema.alterTable('saved_queries', (table) => {
+        table.string('slug').notNullable().alter();
+    });
+    await knex.schema.alterTable('dashboards', (table) => {
+        table.string('slug').notNullable().alter();
+    });
+    await knex.schema.alterTable('spaces', (table) => {
+        table.string('slug').notNullable().alter();
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
+    // Make slugs nullable
+    await knex.schema.alterTable('saved_queries', (table) => {
+        table.string('slug').nullable().alter();
+    });
+    await knex.schema.alterTable('dashboards', (table) => {
+        table.string('slug').nullable().alter();
+    });
+    await knex.schema.alterTable('spaces', (table) => {
+        table.string('slug').nullable().alter();
+    });
+
     await knex.raw(`UPDATE saved_queries
     SET slug = NULL;`);
     await knex.raw(`UPDATE dashboards
