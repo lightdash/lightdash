526b5d54e34e629c59179cff5c4359bf2da236a0
Rephus <rephus@gmail.com>
2024-04-30 16:38:26 +0200

    M  packages/backend/src/database/migrations/20240424131220_populate_slugs.ts

--- packages/backend/src/database/migrations/20240424131220_populate_slugs.ts
+++ packages/backend/src/database/migrations/20240424131220_populate_slugs.ts
@@ -8,32 +8,19 @@ export async function up(knex: Knex): Promise<void> {
             ),
             '[^a-z0-9]+', '-', 'g'
         )`;
-    // Charts in spaces
     await knex.raw(`
         UPDATE saved_queries sq
         SET slug = ${sanitizeSlug(`sq."name"`)} 
-        FROM spaces s
-        WHERE sq.space_id = s.space_id
-        AND sq.space_id IS NOT NULL;
-  `);
-    // Charts in dashboards
+    `);
+
     await knex.raw(`
-        UPDATE saved_queries sq
-        SET slug = ${sanitizeSlug(`sq."name"`)} 
-        WHERE sq.dashboard_uuid IS NOT NULL;
+        UPDATE dashboards d
+        SET slug =  ${sanitizeSlug(`d."name"`)} 
     `);
     await knex.raw(`
-    UPDATE dashboards d
-    SET slug =  ${sanitizeSlug(`d."name"`)} 
-       
-    FROM spaces s
-    WHERE d.space_id = s.space_id
-    AND d.space_id IS NOT NULL;
-  `);
-    await knex.raw(`
-  UPDATE spaces s
-  SET slug = ${sanitizeSlug(`s."name" `)}
-`);
+        UPDATE spaces s
+        SET slug = ${sanitizeSlug(`s."name" `)}
+    `);
 
     // Make slugs notNullable
     await knex.schema.alterTable('saved_queries', (table) => {
