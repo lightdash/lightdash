475bad6c1d99071c8a8d31137e48e40488906a94
Rephus <rephus@gmail.com>
2024-05-06 09:09:52 +0200

    fix test

--- packages/backend/src/services/DashboardService/DashboardService.mock.ts
+++ packages/backend/src/services/DashboardService/DashboardService.mock.ts
@@ -200,8 +200,9 @@ export const createDashboardWithSlug = {
     slug: 'my-new-dashboard',
 };
 
-export const createDashboardWithTileIds: CreateDashboard = {
+export const createDashboardWithTileIds: CreateDashboard & { slug: string } = {
     ...createDashboard,
+    slug: 'my-new-dashboard',
     tiles: [createTileWithId],
 };
 
