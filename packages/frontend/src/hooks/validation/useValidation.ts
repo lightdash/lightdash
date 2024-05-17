d69c677c54f832b69ce5264111eea444cbec001b
Anshul Garwal <80778090+anshul45@users.noreply.github.com>
2024-05-17 14:41:53 +0530

    fix: avoid requesting validation endpoint when no permission (#10085)
    * fix: Some users are requesting validation details even though they don't have access to that feature.
    
    * refactor: simplify useValidation ability check
    
    * fix: pass user param to settings validator page hook
    
    ---------
    
    Co-authored-by: Joao Viana <joao@lightdash.com>

--- packages/frontend/src/hooks/validation/useValidation.ts
+++ packages/frontend/src/hooks/validation/useValidation.ts
@@ -4,13 +4,18 @@ import {
     type ApiJobScheduledResponse,
     type ValidationResponse,
 } from '@lightdash/common';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+    useMutation,
+    useQuery,
+    useQueryClient,
+    type UseQueryResult,
+} from '@tanstack/react-query';
 import useLocalStorageState from 'use-local-storage-state';
 import { lightdashApi } from '../../api';
 import { pollJobStatus } from '../../features/scheduler/hooks/useScheduler';
 import useToaster from '../toaster/useToaster';
 import { useProject } from '../useProject';
-import useUser from '../user/useUser';
+import useUser, { type UserWithAbility } from '../user/useUser';
 
 const LAST_VALIDATION_NOTIFICATION_KEY = 'lastValidationTimestamp';
 
@@ -26,12 +31,24 @@ const getValidation = async (
 
 export const useValidation = (
     projectUuid: string,
+    user: UseQueryResult<UserWithAbility, ApiError>,
     fromSettings: boolean = false,
 ) => {
     const [lastValidationNotification, setLastValidationNotification] =
         useLocalStorageState<string>(LAST_VALIDATION_NOTIFICATION_KEY);
+    const organizationUuid = user.data?.organizationUuid;
+
+    // Check if the user can manage validation feature
+    const canManageValidation = user.data?.ability.can(
+        'manage',
+        subject('Validation', {
+            organizationUuid,
+            projectUuid,
+        }),
+    );
 
     return useQuery<ValidationResponse[], ApiError>({
+        enabled: canManageValidation,
         queryKey: ['validation', fromSettings],
         queryFn: () => getValidation(projectUuid, fromSettings),
         retry: (_, error) => error.error.statusCode !== 403,
