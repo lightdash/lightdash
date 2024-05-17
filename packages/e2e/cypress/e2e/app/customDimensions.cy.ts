a91b2d24e2ff4057676848b9bf6ed6d391e3ad25
Javier Rengel Jiménez <rephus@gmail.com>
2024-05-17 16:39:12 +0200

    chore: custom sql dimension e2e  (#10098)
    * chore: custom dimension sql e2e
    
    * chore: custom dimension sql e2e
    
    * M  packages/e2e/cypress/e2e/app/customDimensions.cy.ts
    
    * M  packages/e2e/cypress/e2e/app/customDimensions.cy.ts
    
    * M  packages/e2e/cypress/e2e/app/customDimensions.cy.ts

--- packages/e2e/cypress/e2e/app/customDimensions.cy.ts
+++ packages/e2e/cypress/e2e/app/customDimensions.cy.ts
@@ -31,6 +31,7 @@ describe('Custom dimensions', () => {
         cy.contains('$276.98');
 
         // Show SQL
+        cy.findByTestId('Chart-card-expand').click(); // Close chart
         cy.findByTestId('Results-card-expand').click(); // Close results
         cy.findByTestId('SQL-card-expand').click();
 
@@ -45,5 +46,126 @@ describe('Custom dimensions', () => {
         sqlLines.forEach((line) => {
             cy.get('pre').contains(line);
         });
+    });
+
+    it('I can create a custom SQL dimension number', () => {
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables/payments`);
+        cy.contains('Add').click();
+
+        cy.findByPlaceholderText('Enter custom dimension label').type(
+            'random number',
+        );
+        cy.get('#ace-editor').type(`random() + 1`);
+        cy.get(`.mantine-Select-input[value='string']`).click();
+        cy.contains('number').click();
+        cy.findByText('Create').click();
+
+        // Select metric
+        cy.findByText('Total revenue').click();
+        cy.get('button').contains('Run query').click();
+
+        // Show SQL
+        cy.findByTestId('Chart-card-expand').click(); // Close chart
+        cy.findByTestId('Results-card-expand').click(); // Close results
+        cy.findByTestId('SQL-card-expand').click();
+
+        const sqlLines = [
+            `(random() + 1) AS "random_number",`,
+            `SUM("payments".amount) AS "payments_total_revenue"`,
+            `GROUP BY 1`,
+            `ORDER BY "payments_total_revenue" DESC`,
+        ];
+        sqlLines.forEach((line) => {
+            cy.get('pre').contains(line);
+        });
+    });
+
+    it('I can create a custom SQL dimension string', () => {
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables/payments`);
+        cy.contains('Add').click();
+
+        cy.findByPlaceholderText('Enter custom dimension label').type(
+            'payment method',
+        );
+        cy.get('#ace-editor').type(
+            `'payment_' || \${payments.payment_method}`,
+            { parseSpecialCharSequences: false },
+        );
+        // Defaults to string
+        cy.findByText('Create').click();
+
+        // Select metric
+        cy.findByText('Total revenue').click();
+        cy.get('button').contains('Run query').click();
+
+        // Check results
+        cy.contains('payment_credit_card');
+        cy.contains(994.16);
+        // Show SQL
+        cy.findByTestId('Chart-card-expand').click(); // Close chart
+        cy.findByTestId('Results-card-expand').click(); // Close results
+        cy.findByTestId('SQL-card-expand').click();
+
+        const sqlLines = [
+            `('payment_' || ("payments".payment_method)) AS "payment_method",`,
+            `SUM("payments".amount) AS "payments_total_revenue"`,
+            `GROUP BY 1`,
+            `ORDER BY "payments_total_revenue" DESC`,
+        ];
+        sqlLines.forEach((line) => {
+            cy.get('pre').contains(line);
+        });
+    });
+
+    it('I can create a custom metric from a custom dimension', () => {
+        cy.visit(`/projects/${SEED_PROJECT.project_uuid}/tables/payments`);
+        cy.contains('Add').click();
+
+        cy.findByPlaceholderText('Enter custom dimension label').type(
+            'discounted amount',
+        );
+        cy.get('#ace-editor').type(`(\${orders.amount}) / 10`, {
+            parseSpecialCharSequences: false,
+        });
+        cy.get(`.mantine-Select-input[value='string']`).click();
+        cy.contains('number').click();
+        cy.findByText('Create').click();
+
+        // Create custom metric
+        cy.get('.tabler-icon-dots').click();
+        cy.findByText('Max').click();
+        cy.findByText('Create').click();
+
+        // Deselect custom metric
+        cy.findByText('discounted amount').click();
+        // Select dimension
+        cy.findByText('Payment method').click();
+
+        // Select metric
+        cy.findByText('Total revenue').click();
+        cy.get('button').contains('Run query').click();
+
+        // Check results
+        cy.contains('bank_transfer');
+        cy.contains(5.8);
+        // Show SQL
+        cy.findByTestId('Chart-card-expand').click(); // Close chart
+        cy.findByTestId('Results-card-expand').click(); // Close results
+        cy.findByTestId('SQL-card-expand').click();
+
+        const sqlLines = [
+            `"payments".payment_method AS "payments_payment_method",`,
+            `MAX((("orders".amount)) / 10) AS "payments_discounted amount_max_of_discounted_amount",`,
+            `GROUP BY 1`,
+            `ORDER BY "payments_payment_method"`,
+        ];
+        sqlLines.forEach((line) => {
+            cy.get('pre').contains(line);
+        });
+
+        // We deselected the custom dimension, it should not appear in the SQL
+        cy.get('pre')
+            .contains('((("orders".amount)) / 10) AS "discounted_amount",')
+            .should('not.exist');
     });
 });
