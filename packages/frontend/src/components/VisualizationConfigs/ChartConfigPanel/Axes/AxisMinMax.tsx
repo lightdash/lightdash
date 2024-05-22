95110839f280c6802db27a3a607bbf1e69c248fe
Joao Viana <joao@lightdash.com>
2024-05-22 22:17:04 +0100

    feat: add helpful tooltips

--- packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/Axes/AxisMinMax.tsx
+++ packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/Axes/AxisMinMax.tsx
@@ -1,8 +1,10 @@
-import { Group, Switch, TextInput } from '@mantine/core';
+import { Group, Switch, Text, TextInput, Tooltip } from '@mantine/core';
+import { IconHelpCircle } from '@tabler/icons-react';
 import { useCallback, type FC } from 'react';
 import { useToggle } from 'react-use';
 import { useTracking } from '../../../../providers/TrackingProvider';
 import { EventName } from '../../../../types/Events';
+import MantineIcon from '../../../common/MantineIcon';
 
 type Props = {
     label: string;
@@ -15,6 +17,8 @@ type Props = {
     setMax: (value: string | undefined) => void;
     setMaxOffset?: (value: string | undefined) => void;
 };
+
+const DEFAULT_OFFSET_VALUE_FOR_MANUAL_RANGE = '0.5';
 
 export const AxisMinMax: FC<Props> = ({
     label,
@@ -55,8 +59,8 @@ export const AxisMinMax: FC<Props> = ({
 
                     // When setting to manual, set the default offset to 0.5
                     if (!e.target.checked) {
-                        setMinOffset?.('0.5');
-                        setMaxOffset?.('0.5');
+                        setMinOffset?.(DEFAULT_OFFSET_VALUE_FOR_MANUAL_RANGE);
+                        setMaxOffset?.(DEFAULT_OFFSET_VALUE_FOR_MANUAL_RANGE);
                     }
 
                     clearRange();
@@ -80,7 +84,20 @@ export const AxisMinMax: FC<Props> = ({
 
                         {setMinOffset && (
                             <TextInput
-                                label="Offset"
+                                label={
+                                    <Group spacing="two">
+                                        <Text>Offset</Text>
+                                        <Tooltip
+                                            multiline
+                                            variant="xs"
+                                            label="Offset is a value that is added to the min value to determine the actual min value of the axis. For example, if the min value is 0 and the offset is 0.5, the actual min value will be -0.5."
+                                        >
+                                            <MantineIcon
+                                                icon={IconHelpCircle}
+                                            />
+                                        </Tooltip>
+                                    </Group>
+                                }
                                 placeholder="Offset"
                                 defaultValue={minOffset || undefined}
                                 onBlur={(e) =>
@@ -100,7 +117,20 @@ export const AxisMinMax: FC<Props> = ({
 
                         {setMaxOffset && (
                             <TextInput
-                                label="Offset"
+                                label={
+                                    <Group spacing="two">
+                                        <Text>Offset</Text>
+                                        <Tooltip
+                                            multiline
+                                            variant="xs"
+                                            label="Offset is a value that is added to the max value to determine the actual max value of the axis. For example, if the max value is 10 and the offset is 0.5, the actual max value will be 10.5."
+                                        >
+                                            <MantineIcon
+                                                icon={IconHelpCircle}
+                                            />
+                                        </Tooltip>
+                                    </Group>
+                                }
                                 placeholder="Offset"
                                 defaultValue={maxOffset || undefined}
                                 onBlur={(e) =>
