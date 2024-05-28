5a065730c621512c441a9d8b0c2e8fc916a22e91
Joao Viana <joao@lightdash.com>
2024-05-28 15:40:49 +0100

    chore: improvements to the analytics tab

--- packages/frontend/src/features/catalog/components/CatalogAnalyticCharts.tsx
+++ packages/frontend/src/features/catalog/components/CatalogAnalyticCharts.tsx
@@ -1,7 +1,13 @@
 import { type ApiCatalogAnalyticsResults } from '@lightdash/common';
-import { Anchor, Flex, Stack, Text } from '@mantine/core';
+import { Anchor, Avatar, Flex, Group, Paper, Stack, Text } from '@mantine/core';
+import { IconFolder } from '@tabler/icons-react';
 import { useMemo, type FC } from 'react';
-import { ChartIcon } from '../../../components/common/ResourceIcon';
+import MantineIcon from '../../../components/common/MantineIcon';
+import {
+    ChartIcon,
+    getChartIcon,
+} from '../../../components/common/ResourceIcon';
+import RouterNavLink from '../../../components/common/RouterNavLink';
 
 type Props = {
     projectUuid: string;
@@ -32,28 +38,76 @@ export const CatalogAnalyticCharts: FC<React.PropsWithChildren<Props>> = ({
     }
 
     return (
-        <Stack>
+        <Stack w="100%">
+            <Paper
+                maw="50%"
+                withBorder
+                p="xs"
+                px="lg"
+                sx={(theme) => ({
+                    backgroundColor: theme.colors.gray[0],
+                })}
+            >
+                <Text c="gray.6" fw={500} fz="xs">
+                    Charts
+                </Text>
+                <Text fw={700} fz="xl">
+                    {charts.length}
+                </Text>
+            </Paper>
             {chartsInSpace.length > 0 && (
-                <Stack>
-                    <Text weight={700}>
-                        Used in {chartsInSpace.length} charts in spaces
-                    </Text>
+                <Stack w="100%">
                     {chartsInSpace.map((chart) => {
                         return (
-                            <Flex key={chart.uuid}>
-                                <ChartIcon chartKind={chart.chartKind} />
-                                <Anchor
-                                    href={`/projects/${projectUuid}/spaces/${chart.spaceUuid}`}
-                                >
-                                    {chart.spaceName}
-                                </Anchor>
-                                /
-                                <Anchor
-                                    href={`/projects/${projectUuid}/saved/${chart.uuid}`}
-                                >
-                                    {chart.name}
-                                </Anchor>
-                            </Flex>
+                            <Paper key={chart.uuid} withBorder w="100%" p="xs">
+                                <Group noWrap>
+                                    <Avatar size="sm" color="blue" radius="xl">
+                                        <MantineIcon
+                                            icon={getChartIcon(chart.chartKind)}
+                                        />
+                                    </Avatar>
+                                    <Stack spacing="xs">
+                                        <RouterNavLink
+                                            to={`/projects/${projectUuid}/saved/${chart.uuid}`}
+                                            label={
+                                                <Text fz="xs" fw={500}>
+                                                    {chart.name}
+                                                </Text>
+                                            }
+                                            sx={{
+                                                '&:hover': {
+                                                    backgroundColor:
+                                                        'transparent',
+                                                },
+                                            }}
+                                            p={0}
+                                        />
+
+                                        <Group spacing="two" noWrap>
+                                            <MantineIcon
+                                                color="gray.6"
+                                                icon={IconFolder}
+                                            />
+                                            <RouterNavLink
+                                                to={`/projects/${projectUuid}/spaces/${chart.spaceUuid}`}
+                                                label={
+                                                    <Text fz="xs">
+                                                        {chart.spaceName}
+                                                    </Text>
+                                                }
+                                                p={0}
+                                                c="gray.6"
+                                                sx={{
+                                                    '&:hover': {
+                                                        backgroundColor:
+                                                            'transparent',
+                                                    },
+                                                }}
+                                            />
+                                        </Group>
+                                    </Stack>
+                                </Group>
+                            </Paper>
                         );
                     })}
                 </Stack>
