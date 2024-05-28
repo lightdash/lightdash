f3a1ef825e7de9f329f8d1239e54f62e38d1f15b
Joao Viana <joao@lightdash.com>
2024-05-28 12:41:07 +0100

    chore: create CatalogProvider

---
+++ packages/frontend/src/features/catalog/context/CatalogProvider.tsx
@@ -0,0 +1,56 @@
+import { type CatalogMetadata } from '@lightdash/common';
+import {
+    createContext,
+    useContext,
+    useState,
+    type Dispatch,
+    type FC,
+    type SetStateAction,
+} from 'react';
+
+type CatalogContextValues = {
+    projectUuid: string;
+    metadata: CatalogMetadata | undefined;
+    setMetadata: Dispatch<SetStateAction<CatalogMetadata | undefined>>;
+    isSidebarOpen: boolean;
+    setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const CatalogContext = createContext<CatalogContextValues | undefined>(
+    undefined,
+);
+
+export const CatalogProvider: FC<
+    React.PropsWithChildren<
+        Pick<
+            CatalogContextValues,
+            'isSidebarOpen' | 'setSidebarOpen' | 'projectUuid'
+        >
+    >
+> = ({ isSidebarOpen, setSidebarOpen, projectUuid, children }) => {
+    const [metadata, setMetadata] = useState<CatalogMetadata>();
+
+    return (
+        <CatalogContext.Provider
+            value={{
+                projectUuid,
+                metadata,
+                setMetadata,
+                isSidebarOpen,
+                setSidebarOpen,
+            }}
+        >
+            {children}
+        </CatalogContext.Provider>
+    );
+};
+
+export const useCatalogContext = () => {
+    const context = useContext(CatalogContext);
+    if (!context) {
+        throw new Error(
+            'useCatalogContext must be used within a CatalogProvider',
+        );
+    }
+    return context;
+};
