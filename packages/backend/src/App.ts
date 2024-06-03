d880248e4c618a71f2c7b78c24e261ddfd0ebcc3
Joao Viana <joao@lightdash.com>
2024-06-03 13:52:59 +0100

    feat: set organization tag before each be request

--- packages/backend/src/App.ts
+++ packages/backend/src/App.ts
@@ -451,6 +451,8 @@ export default class App {
                 passportUser: { id: string; organization: string },
                 done,
             ) => {
+                // Set the organization tag so we can filter by it in Sentry
+                Sentry.setTag('organization', passportUser.organization);
                 // Convert to a full user profile
                 try {
                     done(null, await userService.findSessionUser(passportUser));
