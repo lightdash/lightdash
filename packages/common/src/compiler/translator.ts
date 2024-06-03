f1fdbdd06993586bc655d38ac9a389bd69d77c80
Honza Stepanovsky <me@janstepanovsky.cz>
2024-06-03 10:27:29 +0200

    Update packages/common/src/compiler/translator.ts
    Co-authored-by: SimonGodefroid <17337190+SimonGodefroid@users.noreply.github.com>

--- packages/common/src/compiler/translator.ts
+++ packages/common/src/compiler/translator.ts
@@ -67,7 +67,9 @@ const convertTimezone = (
             return timestampSql;
         case SupportedDbtAdapter.TRINO:
             return timestampSql;
+        case SupportedDbtAdapter.TRINO:
         case SupportedDbtAdapter.ATHENA:
+                    return timestampSql;
             return timestampSql;
         default:
             return assertUnreachable(
