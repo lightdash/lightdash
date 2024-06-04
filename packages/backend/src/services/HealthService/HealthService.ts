3b3bea464e365cf9e9de1e07fd40d6ddc2ab7242
João Viana <joao@lightdash.com>
2024-06-04 09:31:44 +0100

    chore: don't throw error for out of sync migrations (#10293)
    * chore: dont capture sentry database not migrated yet
    
    * chore: revert sentry discard
    
    * chore: remove error throwing when db hasnt been migrated yet
    
    * refactor: return status in getMigrationStatus
    
    * chore: improve migrationStatus handling in health service

--- packages/backend/src/services/HealthService/HealthService.ts
+++ packages/backend/src/services/HealthService/HealthService.ts
@@ -42,15 +42,19 @@ export class HealthService extends BaseService {
     async getHealthState(user: SessionUser | undefined): Promise<HealthState> {
         const isAuthenticated: boolean = !!user?.userUuid;
 
-        const { isComplete, currentVersion } =
+        const { status: migrationStatus, currentVersion } =
             await this.migrationModel.getMigrationStatus();
 
-        if (!isComplete) {
+        if (migrationStatus < 0) {
             throw new UnexpectedDatabaseError(
                 'Database has not been migrated yet',
                 { currentVersion },
             );
-        }
+        } else if (migrationStatus > 0) {
+            console.warn(
+                `There are more DB migrations than defined in the code (you are running old code against a newer DB). Current version: ${currentVersion}`,
+            );
+        } // else migrationStatus === 0 (all migrations are up to date)
 
         const requiresOrgRegistration =
             !(await this.organizationModel.hasOrgs());
