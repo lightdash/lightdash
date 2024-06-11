d8400dcc7a4bedb2827ab1a76d794228cdde6cbf
Jose Rego <jose@lightdash.com>
2024-06-11 14:07:50 +0100

    fix: adjust content security policy directives

--- packages/backend/src/config/parseConfig.ts
+++ packages/backend/src/config/parseConfig.ts
@@ -160,6 +160,11 @@ export type LightdashConfig = {
     version: '1.0';
     lightdashSecret: string;
     secureCookies: boolean;
+    security: {
+        contentSecurityPolicy: {
+            allowedDomains: string[];
+        };
+    };
     cookiesMaxAgeHours?: number;
     trustProxy: boolean;
     databaseConnectionUri?: string;
@@ -402,6 +407,15 @@ const mergeWithEnvironment = (config: LightdashConfigIn): LightdashConfig => {
     return {
         ...config,
         mode,
+        security: {
+            contentSecurityPolicy: {
+                allowedDomains: (
+                    process.env.LIGHTDASH_CSP_ALLOWED_DOMAINS || ''
+                )
+                    .split(',')
+                    .map((domain) => domain.trim()),
+            },
+        },
         smtp: process.env.EMAIL_SMTP_HOST
             ? {
                   host: process.env.EMAIL_SMTP_HOST,
