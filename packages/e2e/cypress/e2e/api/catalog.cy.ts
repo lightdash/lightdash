669e59ae4e4f75e911e90f8cf16a90385feac5e9
Joao Viana <joao@lightdash.com>
2024-06-24 09:23:42 +0100

    test: add labels to expected responses

--- packages/e2e/cypress/e2e/api/catalog.cy.ts
+++ packages/e2e/cypress/e2e/api/catalog.cy.ts
@@ -50,7 +50,7 @@ describe('Lightdash catalog all tables and fields', () => {
                 description: 'Method of payment used, for example credit card',
                 tableLabel: 'Payments',
                 tableName: 'payments',
-
+                label: 'Payment method',
                 fieldType: 'dimension',
                 basicType: 'string',
                 type: 'field',
@@ -69,6 +69,7 @@ describe('Lightdash catalog all tables and fields', () => {
                 tableName: 'payments',
                 fieldType: 'metric',
                 basicType: 'number',
+                label: 'Total revenue',
                 type: 'field',
                 tags: [],
             });
@@ -107,6 +108,7 @@ describe('Lightdash catalog search', () => {
 
             expect(field).to.eql({
                 name: 'customer_id',
+                label: 'Customer id',
                 tableLabel: 'Users',
                 tableName: 'users',
                 description: 'This is a unique identifier for a customer',
@@ -134,7 +136,7 @@ describe('Lightdash catalog search', () => {
                 description: 'Method of payment used, for example credit card',
                 tableLabel: 'Payments',
                 tableName: 'payments',
-
+                label: 'Payment method',
                 fieldType: 'dimension',
                 basicType: 'string',
                 type: 'field',
@@ -157,7 +159,7 @@ describe('Lightdash catalog search', () => {
                 description: 'Sum of all payments',
                 tableLabel: 'Payments',
                 tableName: 'payments',
-
+                label: 'Total revenue',
                 fieldType: 'metric',
                 basicType: 'number',
                 type: 'field',
@@ -185,7 +187,7 @@ describe('Lightdash catalog search', () => {
                 name: 'customer_id',
                 tableLabel: 'Users',
                 tableName: 'users',
-
+                label: 'Customer id',
                 description: 'This is a unique identifier for a customer',
                 type: 'field',
                 basicType: 'number',
@@ -223,7 +225,7 @@ describe('Lightdash catalog search', () => {
                 name: 'date_of_first_order',
                 tableLabel: 'Orders',
                 tableName: 'orders',
-
+                label: 'Date of first order',
                 description: 'Min of Order date',
                 type: 'field',
                 basicType: 'number',
