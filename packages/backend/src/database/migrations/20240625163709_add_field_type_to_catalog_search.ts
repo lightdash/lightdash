c924bdc66258ef5d82c76b97e917eb40bc919bc7
Joao Viana <joao@lightdash.com>
2024-06-26 09:23:47 +0100

    chore: add CatalogTableName var

--- packages/backend/src/database/migrations/20240625163709_add_field_type_to_catalog_search.ts
+++ packages/backend/src/database/migrations/20240625163709_add_field_type_to_catalog_search.ts
@@ -1,13 +1,15 @@
 import { Knex } from 'knex';
 
+const CatalogTableName = 'catalog_search';
+
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.alterTable('catalog_search', (table) => {
+    await knex.schema.alterTable(CatalogTableName, (table) => {
         table.text('field_type').nullable().index();
     });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.alterTable('catalog_search', (table) => {
+    await knex.schema.alterTable(CatalogTableName, (table) => {
         table.dropColumn('field_type');
     });
 }
