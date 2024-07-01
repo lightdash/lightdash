c84dfb3b4656fbcb7da0c4658bc92478f750be3e
Joao Viana <joao@lightdash.com>
2024-07-01 15:37:31 +0100

    chore: make same changes as rtk

--- packages/frontend/src/features/sqlRunner/components/TableFields.tsx
+++ packages/frontend/src/features/sqlRunner/components/TableFields.tsx
@@ -6,51 +6,49 @@ import {
     Text,
     UnstyledButton,
 } from '@mantine/core';
-import { useEffect, type FC } from 'react';
+import { memo, useCallback, useEffect, type FC } from 'react';
 import { useTableFields } from '../hooks/useTableFields';
 import { useSqlRunnerProvider } from '../providers/SqlRunnerProvider';
 
-const TableField: FC<{ field: string }> = ({ field }) => {
-    console.log('TableField', field, 'rendered');
+const TableField: FC<{ field: string; isActiveField: boolean }> = memo(
+    ({ field, isActiveField }) => {
+        console.log('TableField', field, 'rendered');
 
-    const { activeFields, setActiveFields } = useSqlRunnerProvider();
+        const { setActiveFields } = useSqlRunnerProvider();
 
-    return (
-        <UnstyledButton
-            fw={500}
-            p={4}
-            fz={13}
-            c={activeFields && activeFields.has(field) ? 'gray.8' : 'gray.7'}
-            bg={
-                activeFields && activeFields.has(field)
-                    ? 'gray.1'
-                    : 'transparent'
-            }
-            onClick={() => {
-                setActiveFields((prev) => {
-                    const newSet = new Set(prev);
-                    if (newSet.has(field)) {
-                        newSet.delete(field);
-                    } else {
-                        newSet.add(field);
-                    }
-                    return newSet;
-                });
-            }}
-            sx={(theme) => ({
-                borderRadius: theme.radius.sm,
-                '&:hover': {
-                    backgroundColor: theme.colors.gray[1],
-                },
-            })}
-        >
-            {field}
-        </UnstyledButton>
-    );
-};
+        return (
+            <UnstyledButton
+                fw={500}
+                p={4}
+                fz={13}
+                c={isActiveField ? 'gray.8' : 'gray.7'}
+                bg={isActiveField ? 'gray.1' : 'transparent'}
+                onClick={() => {
+                    setActiveFields((prev) => {
+                        const newSet = new Set(prev);
+                        if (newSet.has(field)) {
+                            newSet.delete(field);
+                        } else {
+                            newSet.add(field);
+                        }
+                        return newSet;
+                    });
+                }}
+                sx={(theme) => ({
+                    borderRadius: theme.radius.sm,
+                    '&:hover': {
+                        backgroundColor: theme.colors.gray[1],
+                    },
+                })}
+            >
+                {field}
+            </UnstyledButton>
+        );
+    },
+);
 
 export const TableFields: FC = () => {
-    const { activeTable, projectUuid, setActiveFields } =
+    const { activeTable, projectUuid, setActiveFields, activeFields } =
         useSqlRunnerProvider();
     const {
         data: tableFields,
@@ -66,6 +64,13 @@ export const TableFields: FC = () => {
             setActiveFields(undefined);
         }
     }, [isSuccess, setActiveFields]);
+
+    const isActiveField = useCallback(
+        (field: string) => {
+            return Boolean(activeFields && activeFields.has(field));
+        },
+        [activeFields],
+    );
 
     return (
         <Stack pt="sm" spacing="xs" h="calc(100% - 20px)" py="xs">
@@ -89,7 +94,11 @@ export const TableFields: FC = () => {
                     >
                         <Stack spacing={0}>
                             {Object.keys(tableFields).map((field) => (
-                                <TableField key={field} field={field} />
+                                <TableField
+                                    key={field}
+                                    field={field}
+                                    isActiveField={isActiveField(field)}
+                                />
                             ))}
                         </Stack>
                     </Box>
