3f09ec7d90872d70496d3d4664f565a7e4d7111a
Joao Viana <joao@lightdash.com>
2024-07-01 13:28:44 +0100

    chore: add correct width constraints

--- packages/frontend/src/components/common/Page/Sidebar.tsx
+++ packages/frontend/src/components/common/Page/Sidebar.tsx
@@ -15,8 +15,8 @@ import { TrackSection } from '../../../providers/TrackingProvider';
 import { SectionName } from '../../../types/Events';
 
 const SIDEBAR_DEFAULT_WIDTH = 400;
-const SIDEBAR_MIN_WIDTH = 300;
-const SIDEBAR_MAX_WIDTH = 600;
+export const SIDEBAR_MIN_WIDTH = 300;
+export const SIDEBAR_MAX_WIDTH = 600;
 
 const SIDEBAR_RESIZE_HANDLE_WIDTH = 6;
 
