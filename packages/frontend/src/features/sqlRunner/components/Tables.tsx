1d25ab94631e476ff6515da62de76b70ec549255
Joao Viana <joao@lightdash.com>
2024-07-02 15:29:20 +0100

    feat: allow table search in sql runner

--- packages/frontend/src/features/sqlRunner/components/Tables.tsx
+++ packages/frontend/src/features/sqlRunner/components/Tables.tsx
@@ -1,5 +1,16 @@
-import { LoadingOverlay, Stack, Text, UnstyledButton } from '@mantine/core';
-import { type Dispatch, type FC, type SetStateAction } from 'react';
+import {
+    ActionIcon,
+    Center,
+    Highlight,
+    LoadingOverlay,
+    Stack,
+    Text,
+    TextInput,
+    UnstyledButton,
+} from '@mantine/core';
+import { IconSearch, IconX } from '@tabler/icons-react';
+import { useState, type Dispatch, type FC, type SetStateAction } from 'react';
+import MantineIcon from '../../../components/common/MantineIcon';
 import { useTables } from '../hooks/useTables';
 
 type Props = {
@@ -13,51 +24,80 @@ export const Tables: FC<Props> = ({
     setActiveTable,
     projectUuid,
 }) => {
-    const { data, isLoading } = useTables({ projectUuid });
+    const [search, setSearch] = useState<string>('');
+    const isValidSearch = Boolean(search && search.trim().length > 2);
+    const { data, isLoading } = useTables({
+        projectUuid,
+        search: isValidSearch ? search : undefined,
+    });
 
     return (
         <>
             <LoadingOverlay visible={isLoading} />
-            {data &&
-                Object.entries(data).map(([, schemas]) =>
-                    Object.entries(schemas).map(([schema, tables]) => (
-                        <Stack key={schema} spacing="none">
-                            <Text p={6} fw={700} fz="md" c="gray.7">
-                                {schema}
-                            </Text>
-                            {Object.keys(tables).map((table) => (
-                                <UnstyledButton
-                                    key={table}
-                                    onClick={() => {
-                                        setActiveTable(table);
-                                    }}
-                                    fw={500}
-                                    p={4}
-                                    fz={13}
-                                    c={
-                                        activeTable === table
-                                            ? 'gray.8'
-                                            : 'gray.7'
-                                    }
-                                    bg={
-                                        activeTable === table
-                                            ? 'gray.1'
-                                            : 'transparent'
-                                    }
-                                    sx={(theme) => ({
-                                        borderRadius: theme.radius.sm,
-                                        '&:hover': {
-                                            backgroundColor:
-                                                theme.colors.gray[1],
-                                        },
-                                    })}
+            <TextInput
+                size="xs"
+                disabled={isLoading || (!data && !isValidSearch)}
+                icon={<MantineIcon icon={IconSearch} />}
+                rightSection={
+                    search ? (
+                        <ActionIcon onClick={() => setSearch('')}>
+                            <MantineIcon icon={IconX} />
+                        </ActionIcon>
+                    ) : null
+                }
+                placeholder="Search tables"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                styles={(theme) => ({
+                    input: {
+                        borderRadius: theme.radius.md,
+                        border: `1px solid ${theme.colors.gray[3]}`,
+                    },
+                })}
+            />
+            {data ? (
+                data.map(({ schema, tables }) => (
+                    <Stack key={schema} spacing="none">
+                        <Text p={6} fw={700} fz="md" c="gray.7">
+                            {schema}
+                        </Text>
+                        {Object.keys(tables).map((table) => (
+                            <UnstyledButton
+                                key={table}
+                                onClick={() => {
+                                    setActiveTable(table);
+                                }}
+                                fw={500}
+                                p={4}
+                                fz={13}
+                                c={activeTable === table ? 'gray.8' : 'gray.7'}
+                                bg={
+                                    activeTable === table
+                                        ? 'gray.1'
+                                        : 'transparent'
+                                }
+                                sx={(theme) => ({
+                                    borderRadius: theme.radius.sm,
+                                    '&:hover': {
+                                        backgroundColor: theme.colors.gray[1],
+                                    },
+                                })}
+                            >
+                                <Highlight
+                                    component={Text}
+                                    highlight={search || ''}
                                 >
                                     {table}
-                                </UnstyledButton>
-                            ))}
-                        </Stack>
-                    )),
-                )}
+                                </Highlight>
+                            </UnstyledButton>
+                        ))}
+                    </Stack>
+                ))
+            ) : (
+                <Center p="sm">
+                    <Text c="gray.4">No results found</Text>
+                </Center>
+            )}
         </>
     );
 };
