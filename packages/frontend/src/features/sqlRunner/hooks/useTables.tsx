c6a1702aa9668b4565374b78d4f5671b806680d4
João Viana <joao@lightdash.com>
2024-07-03 15:23:16 +0100

    feat: allow searching tables sql runner (#10586)
    * feat: allow table search in sql runner
    
    * chore: add search to queryKey
    
    * feat: improve search and spinner ux
    
    * chore: debounce search
    
    * chore: remove search from query key to avoid ui flickering
    
    * chore: separate rendering of no results

--- packages/frontend/src/features/sqlRunner/hooks/useTables.tsx
+++ packages/frontend/src/features/sqlRunner/hooks/useTables.tsx
@@ -1,25 +1,79 @@
 import { type ApiError, type ApiWarehouseCatalog } from '@lightdash/common';
 import { useQuery } from '@tanstack/react-query';
+import Fuse from 'fuse.js';
 import { lightdashApi } from '../../../api';
 
 export type GetTablesParams = {
     projectUuid: string;
+    search: string | undefined;
 };
 
-const fetchTables = async ({ projectUuid }: GetTablesParams) =>
+const fetchTables = async ({
+    projectUuid,
+}: Pick<GetTablesParams, 'projectUuid'>) =>
     lightdashApi<ApiWarehouseCatalog>({
         url: `/projects/${projectUuid}/sqlRunner/tables`,
         method: 'GET',
         body: undefined,
     });
 
-export const useTables = ({ projectUuid }: GetTablesParams) => {
-    return useQuery<ApiWarehouseCatalog, ApiError>({
+type TablesBySchema =
+    | {
+          schema: keyof ApiWarehouseCatalog['results'][0];
+          tables: ApiWarehouseCatalog['results'][0]['tables'];
+      }[]
+    | undefined;
+
+export const useTables = ({ projectUuid, search }: GetTablesParams) => {
+    return useQuery<ApiWarehouseCatalog, ApiError, TablesBySchema>({
         queryKey: ['sqlRunner', 'tables', projectUuid],
         queryFn: () =>
             fetchTables({
                 projectUuid,
             }),
         retry: false,
+        select(data) {
+            const tablesBySchema = Object.entries(data).flatMap(([, schemas]) =>
+                Object.entries(schemas).map(([schema, tables]) => ({
+                    schema,
+                    tables,
+                })),
+            );
+
+            if (!search) return tablesBySchema;
+
+            const searchResults: TablesBySchema = tablesBySchema
+                .map((schemaData) => {
+                    const { schema, tables } = schemaData;
+                    const tableNames = Object.keys(tables);
+
+                    const fuse = new Fuse(tableNames, {
+                        threshold: 0.3,
+                        isCaseSensitive: false,
+                    });
+
+                    const fuseResult = fuse
+                        .search(search)
+                        .map((res) => res.item);
+
+                    return {
+                        schema,
+                        tables: fuseResult.reduce<typeof tables>(
+                            (acc, tableName) => {
+                                acc[tableName] = tables[tableName];
+                                return acc;
+                            },
+                            {},
+                        ),
+                    };
+                })
+                .filter(
+                    (schemaData) => Object.keys(schemaData.tables).length > 0,
+                );
+
+            if (searchResults.length === 0) {
+                return undefined;
+            } else return searchResults;
+        },
     });
 };
