c69a5fd207d7f9e0b8916f8a6267013d9afbe8ca
João Viana <joao@lightdash.com>
2024-07-08 16:13:44 +0100

    chore: add SqlRunnerResultsTransformer class (#10641)

--- packages/common/src/transformers/ResultTransformers/index.ts
+++ packages/common/src/transformers/ResultTransformers/index.ts
@@ -1,2 +1,3 @@
 export * from './AbstractResultTransformer';
 export * from './ExplorerResultsTransformer';
+export * from './SqlRunnerResultsTransformer';
--- packages/common/src/types/sqlRunner.ts
+++ packages/common/src/types/sqlRunner.ts
@@ -1,3 +1,5 @@
+import { type ResultRow } from './results';
+
 export type SqlRunnerPayload = {
     projectUuid: string;
     sql: string;
@@ -8,5 +10,7 @@ export type SqlRunnerPayload = {
 export type SqlRunnerBody = {
     sql: string;
 };
+
+export type SqlRunnerResults = ResultRow[];
 
 export const sqlRunnerJob = 'sqlRunner';
