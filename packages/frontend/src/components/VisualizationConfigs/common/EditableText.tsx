52444b92dacaf240d2102b6615ddcad183b4244e
Joao Viana <joao@lightdash.com>
2024-07-08 09:49:08 +0100

    Revert "fixed the not able to edit input when clicked on the pen icon, solves issue #10618"
    This reverts commit b5819bef8b6fe6928de149c6d3c4acb820f3e443.

--- packages/frontend/src/components/VisualizationConfigs/common/EditableText.tsx
+++ packages/frontend/src/components/VisualizationConfigs/common/EditableText.tsx
@@ -1,34 +1,25 @@
 import { Box, TextInput, type TextInputProps } from '@mantine/core';
 import { useHover } from '@mantine/hooks';
 import { IconPencil } from '@tabler/icons-react';
-import { useRef, type FC } from 'react';
+import { type FC } from 'react';
 import MantineIcon from '../../common/MantineIcon';
 
 type Props = TextInputProps & { lighter?: boolean };
 
 export const EditableText: FC<Props> = ({ lighter, ...props }) => {
     const { hovered, ref } = useHover();
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    const handleIconClick = () => {
-        inputRef.current?.focus();
-    };
-
     return (
         <Box ref={ref}>
             <TextInput
                 size="xs"
                 {...props}
-                ref={inputRef}
                 rightSection={
                     <MantineIcon
                         style={{
                             visibility: hovered ? 'initial' : 'hidden',
-                            cursor: 'pointer',
                         }}
                         color="gray.6"
                         icon={IconPencil}
-                        onClick={handleIconClick}
                     />
                 }
                 styles={(theme) => ({
@@ -37,14 +28,17 @@ export const EditableText: FC<Props> = ({ lighter, ...props }) => {
                         background: 'transparent',
                         fontWeight: 500,
                         paddingLeft: 4,
+
                         whiteSpace: 'nowrap',
                         overflow: 'hidden',
                         textOverflow: 'ellipsis',
+
                         ':hover': {
                             whiteSpace: 'normal',
                             overflow: 'visible',
                             background: theme.colors.gray[lighter ? '1' : '2'],
                         },
+
                         '::placeholder': {
                             color: theme.colors.gray[lighter ? '5' : '6'],
                         },
