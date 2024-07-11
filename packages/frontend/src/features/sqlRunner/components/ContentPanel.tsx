688059bf696ce3881610727999b20f2b1fc6e1ec
magnew <mcagnew@gmail.com>
2024-07-11 12:17:36 +0200

    chore: fix sql runner toolitps (#10691)

--- packages/frontend/src/features/sqlRunner/components/ContentPanel.tsx
+++ packages/frontend/src/features/sqlRunner/components/ContentPanel.tsx
@@ -81,120 +81,18 @@ export const ContentPanel: FC<Props> = ({
             style={{ flex: 1, overflow: 'hidden' }}
             ref={wrapperRef}
         >
-            <Paper shadow="none" radius={0} px="md" py="sm" withBorder>
-                <Group position="apart">
-                    <Title order={5} c="gray.6">
-                        SQL panel
-                    </Title>
-                    <Group spacing="md">
-                        <Tooltip
-                            variant="xs"
-                            label={
-                                !isResultsHeightMoreThanHalf
-                                    ? 'Collapse'
-                                    : 'Expand'
-                            }
-                            position="bottom"
-                        >
-                            <ActionIcon
-                                size="xs"
-                                onClick={() =>
-                                    setResultsHeight(
-                                        isResultsHeightMoreThanHalf
-                                            ? MIN_RESULTS_HEIGHT
-                                            : maxResultsHeight,
-                                    )
-                                }
-                            >
-                                <MantineIcon
-                                    icon={
-                                        !isResultsHeightMoreThanHalf
-                                            ? IconLayoutNavbarCollapse
-                                            : IconLayoutNavbarExpand
-                                    }
-                                />
-                            </ActionIcon>
-                        </Tooltip>
-                        <Tooltip
-                            variant="xs"
-                            label="Run query"
-                            position="bottom"
-                        >
-                            <Button
-                                size="xs"
-                                leftIcon={<MantineIcon icon={IconPlayerPlay} />}
-                                loading={isLoading}
-                                onClick={() => {
-                                    if (!sql) return;
-                                    runSqlQuery({
-                                        sql,
-                                    });
-                                }}
-                            >
-                                Run query
-                            </Button>
-                        </Tooltip>
-                    </Group>
-                </Group>
-            </Paper>
-            <Paper
-                ref={inputSectionRef}
-                shadow="none"
-                radius={0}
-                p="none"
-                withBorder
-                style={{ flex: 1 }}
-            >
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        height: inputSectionHeight,
-                        width: inputSectionWidth,
-                    }}
-                >
-                    <SqlEditor sql={sql} onSqlChange={setSql} />
-                </Box>
-            </Paper>
-            <ResizableBox
-                height={resultsHeight}
-                minConstraints={[50, 50]}
-                maxConstraints={[Infinity, maxResultsHeight]}
-                resizeHandles={['n']}
-                axis="y"
-                handle={
-                    <Divider
-                        h={3}
-                        bg="gray.3"
-                        pos="absolute"
-                        top={-2}
-                        left={0}
-                        right={0}
-                        sx={{
-                            cursor: 'ns-resize',
-                        }}
-                    />
-                }
-                style={{
-                    position: 'relative',
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-                onResizeStop={(e, data) => setResultsHeight(data.size.height)}
-            >
+            <Tooltip.Group>
                 <Paper shadow="none" radius={0} px="md" py="sm" withBorder>
                     <Group position="apart">
-                        <Group spacing="xs">
-                            <Title order={5} c="gray.6">
-                                Results/Chart panel
-                            </Title>
-                            {isLoading && <Loader size="xs" />}
-                        </Group>
-
+                        <Title order={5} c="gray.6">
+                            SQL panel
+                        </Title>
                         <Group spacing="md">
                             <Tooltip
+                                key={String(isResultsHeightMoreThanHalf)}
                                 variant="xs"
                                 label={
-                                    isResultsHeightMoreThanHalf
+                                    !isResultsHeightMoreThanHalf
                                         ? 'Collapse'
                                         : 'Expand'
                                 }
@@ -212,9 +110,9 @@ export const ContentPanel: FC<Props> = ({
                                 >
                                     <MantineIcon
                                         icon={
-                                            isResultsHeightMoreThanHalf
-                                                ? IconLayoutNavbarExpand
-                                                : IconLayoutNavbarCollapse
+                                            !isResultsHeightMoreThanHalf
+                                                ? IconLayoutNavbarCollapse
+                                                : IconLayoutNavbarExpand
                                         }
                                     />
                                 </ActionIcon>
@@ -224,40 +122,152 @@ export const ContentPanel: FC<Props> = ({
                                 label="Run query"
                                 position="bottom"
                             >
-                                <ActionIcon
+                                <Button
                                     size="xs"
-                                    onClick={
-                                        isChartConfigOpen
-                                            ? closeChartConfig
-                                            : openChartConfig
+                                    leftIcon={
+                                        <MantineIcon icon={IconPlayerPlay} />
                                     }
+                                    loading={isLoading}
+                                    onClick={() => {
+                                        if (!sql) return;
+                                        runSqlQuery({
+                                            sql,
+                                        });
+                                    }}
                                 >
-                                    <MantineIcon icon={IconAdjustmentsCog} />
-                                </ActionIcon>
+                                    Run query
+                                </Button>
                             </Tooltip>
                         </Group>
                     </Group>
                 </Paper>
-
-                {queryResults && !isLoading && (
-                    <Paper
-                        shadow="none"
-                        radius={0}
-                        px="md"
-                        py="sm"
-                        withBorder
-                        sx={{ flex: 1, overflow: 'auto' }}
-                        h="100%"
+                <Paper
+                    ref={inputSectionRef}
+                    shadow="none"
+                    radius={0}
+                    p="none"
+                    withBorder
+                    style={{ flex: 1 }}
+                >
+                    <Box
+                        sx={{
+                            position: 'absolute',
+                            height: inputSectionHeight,
+                            width: inputSectionWidth,
+                        }}
                     >
-                        {selectedChartType === SqlRunnerChartType.TABLE && (
-                            <Table data={queryResults} />
-                        )}
-                        {selectedChartType === SqlRunnerChartType.BAR && (
-                            <BarChart data={queryResults} />
-                        )}
+                        <SqlEditor sql={sql} onSqlChange={setSql} />
+                    </Box>
+                </Paper>
+                <ResizableBox
+                    height={resultsHeight}
+                    minConstraints={[50, 50]}
+                    maxConstraints={[Infinity, maxResultsHeight]}
+                    resizeHandles={['n']}
+                    axis="y"
+                    handle={
+                        <Divider
+                            h={3}
+                            bg="gray.3"
+                            pos="absolute"
+                            top={-2}
+                            left={0}
+                            right={0}
+                            sx={{
+                                cursor: 'ns-resize',
+                            }}
+                        />
+                    }
+                    style={{
+                        position: 'relative',
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                    onResizeStop={(e, data) =>
+                        setResultsHeight(data.size.height)
+                    }
+                >
+                    <Paper shadow="none" radius={0} px="md" py="sm" withBorder>
+                        <Group position="apart">
+                            <Group spacing="xs">
+                                <Title order={5} c="gray.6">
+                                    Results/Chart panel
+                                </Title>
+                                {isLoading && <Loader size="xs" />}
+                            </Group>
+
+                            <Group spacing="md">
+                                <Tooltip
+                                    key={String(isResultsHeightMoreThanHalf)}
+                                    variant="xs"
+                                    label={
+                                        isResultsHeightMoreThanHalf
+                                            ? 'Collapse'
+                                            : 'Expand'
+                                    }
+                                    position="bottom"
+                                >
+                                    <ActionIcon
+                                        size="xs"
+                                        onClick={() =>
+                                            setResultsHeight(
+                                                isResultsHeightMoreThanHalf
+                                                    ? MIN_RESULTS_HEIGHT
+                                                    : maxResultsHeight,
+                                            )
+                                        }
+                                    >
+                                        <MantineIcon
+                                            icon={
+                                                isResultsHeightMoreThanHalf
+                                                    ? IconLayoutNavbarExpand
+                                                    : IconLayoutNavbarCollapse
+                                            }
+                                        />
+                                    </ActionIcon>
+                                </Tooltip>
+                                <Tooltip
+                                    variant="xs"
+                                    label="Configure"
+                                    position="bottom"
+                                >
+                                    <ActionIcon
+                                        size="xs"
+                                        onClick={
+                                            isChartConfigOpen
+                                                ? closeChartConfig
+                                                : openChartConfig
+                                        }
+                                    >
+                                        <MantineIcon
+                                            icon={IconAdjustmentsCog}
+                                        />
+                                    </ActionIcon>
+                                </Tooltip>
+                            </Group>
+                        </Group>
                     </Paper>
-                )}
-            </ResizableBox>
+
+                    {queryResults && !isLoading && (
+                        <Paper
+                            shadow="none"
+                            radius={0}
+                            px="md"
+                            py="sm"
+                            withBorder
+                            sx={{ flex: 1, overflow: 'auto' }}
+                            h="100%"
+                        >
+                            {selectedChartType === SqlRunnerChartType.TABLE && (
+                                <Table data={queryResults} />
+                            )}
+                            {selectedChartType === SqlRunnerChartType.BAR && (
+                                <BarChart data={queryResults} />
+                            )}
+                        </Paper>
+                    )}
+                </ResizableBox>
+            </Tooltip.Group>
         </Stack>
     );
 };
