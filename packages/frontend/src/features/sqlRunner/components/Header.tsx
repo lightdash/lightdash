1f9900110f9bfccfaef601e708663923523f9f05
Javier Rengel Jiménez <rephus@gmail.com>
2024-07-18 15:21:17 +0200

    feat: Share sql chart url (#10753)
    * feat: share link sql runner
    
    * fix button style
    
    * add state to url

--- packages/frontend/src/features/sqlRunner/components/Header.tsx
+++ packages/frontend/src/features/sqlRunner/components/Header.tsx
@@ -1,6 +1,6 @@
 import { ChartKind } from '@lightdash/common';
 import { ActionIcon, Group, Paper, Tooltip } from '@mantine/core';
-import { IconDeviceFloppy, IconLink } from '@tabler/icons-react';
+import { IconDeviceFloppy } from '@tabler/icons-react';
 import { useCallback, type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
 import { EditableText } from '../../../components/VisualizationConfigs/common/EditableText';
@@ -8,6 +8,7 @@ import { useUpdateSqlChartMutation } from '../hooks/useSavedSqlCharts';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { DEFAULT_NAME, toggleModal, updateName } from '../store/sqlRunnerSlice';
 import { SaveSqlChartModal } from './SaveSqlChartModal';
+import ShareSqlLinkButton from './ShareSqlLinkButton';
 
 export const Header: FC = () => {
     const dispatch = useAppDispatch();
@@ -80,9 +81,7 @@ export const Header: FC = () => {
                             label="Share URL"
                             position="bottom"
                         >
-                            <ActionIcon size="xs">
-                                <MantineIcon icon={IconLink} />
-                            </ActionIcon>
+                            <ShareSqlLinkButton />
                         </Tooltip>
                     </Group>
                 </Group>
