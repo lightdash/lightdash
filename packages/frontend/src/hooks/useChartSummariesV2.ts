0239824a3f600f52d3e929c4ea0da26fc64d5987
Rephus <rephus@gmail.com>
2024-07-19 09:29:41 +0200

    M  packages/frontend/src/hooks/useChartSummariesV2.ts

--- packages/frontend/src/hooks/useChartSummariesV2.ts
+++ packages/frontend/src/hooks/useChartSummariesV2.ts
@@ -4,11 +4,11 @@ import {
     type ApiError,
 } from '@lightdash/common';
 import { useQuery } from '@tanstack/react-query';
-import { API_PREFIX_V2, lightdashApi } from '../api';
+import { lightdashApi } from '../api';
 
 const getChartSummariesInProjectV2 = async (projectUuid: string) => {
     return lightdashApi<ApiChartContentResponse['results']>({
-        apiPrefix: API_PREFIX_V2,
+        version: 'v2',
         url: `/content?projectUuids=${projectUuid}&contentTypes=${ContentType.CHART}&pageSize=${Number.MAX_SAFE_INTEGER}`, // TODO: remove pageSize max once we have pagination
         method: 'GET',
         body: undefined,
