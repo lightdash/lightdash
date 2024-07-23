382439eb3f7ce71e52f002f60ae5d2c3b4b016d2
Javier Rengel Jiménez <rephus@gmail.com>
2024-07-23 11:51:29 +0200

    feat: Save sql chart in space and fix slug for sql charts (#10780)
    * feat: save sql chart in space and use slug on url
    
    * new space
    
    * default space
    
    * M  packages/backend/src/generated/routes.ts
    M  packages/backend/src/generated/swagger.json
    M  packages/backend/src/models/SavedChartModel.ts
    M  packages/backend/src/services/ProjectService/ProjectService.mock.ts
    M  packages/backend/src/services/PromoteService/PromoteService.mock.ts
    
    * M  packages/backend/src/models/ResourceViewItemModel.ts
    M  packages/backend/src/models/SpaceModel.ts

--- packages/frontend/src/features/sqlRunner/components/SaveSqlChartModal.tsx
+++ packages/frontend/src/features/sqlRunner/components/SaveSqlChartModal.tsx
@@ -12,17 +12,19 @@ import {
 import { useForm, zodResolver } from '@mantine/form';
 import { IconChartBar } from '@tabler/icons-react';
 import { useCallback, useEffect, type FC } from 'react';
-import { z } from 'zod';
+import { type z } from 'zod';
 import MantineIcon from '../../../components/common/MantineIcon';
-import { useSpaceSummaries } from '../../../hooks/useSpaces';
+import {
+    SaveToSpace,
+    validationSchema,
+} from '../../../components/common/modal/ChartCreateModal/SaveToSpaceOrDashboard';
+import {
+    useCreateMutation as useSpaceCreateMutation,
+    useSpaceSummaries,
+} from '../../../hooks/useSpaces';
 import { useCreateSqlChartMutation } from '../hooks/useSavedSqlCharts';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { updateName } from '../store/sqlRunnerSlice';
-
-const validationSchema = z.object({
-    name: z.string().min(1),
-    description: z.string(),
-});
 
 type FormValues = z.infer<typeof validationSchema>;
 
@@ -35,12 +37,9 @@ export const SaveSqlChartModal: FC<Props> = ({ opened, onClose }) => {
 
     const name = useAppSelector((state) => state.sqlRunner.name);
     const description = useAppSelector((state) => state.sqlRunner.description);
+    const { mutateAsync: createSpace } = useSpaceCreateMutation(projectUuid);
 
     const form = useForm<FormValues>({
-        initialValues: {
-            name: '',
-            description: '',
-        },
         validate: zodResolver(validationSchema),
     });
 
@@ -51,7 +50,10 @@ export const SaveSqlChartModal: FC<Props> = ({ opened, onClose }) => {
         if (!form.values.description && description) {
             form.setFieldValue('description', description);
         }
-    }, [name, form, description]);
+        if (!form.values.spaceUuid && spaces.length > 0) {
+            form.setFieldValue('spaceUuid', spaces[0].uuid);
+        }
+    }, [name, form, description, spaces]);
 
     const sql = useAppSelector((state) => state.sqlRunner.sql);
     const config = useAppSelector((state) =>
@@ -76,17 +78,25 @@ export const SaveSqlChartModal: FC<Props> = ({ opened, onClose }) => {
         if (spaces.length === 0) {
             return;
         }
+        let newSpace = form.values.newSpaceName
+            ? await createSpace({
+                  name: form.values.newSpaceName,
+                  access: [],
+                  isPrivate: true,
+              })
+            : undefined;
+        const spaceUuid =
+            newSpace?.uuid || form.values.spaceUuid || spaces[0].uuid;
         await createSavedSqlChart({
             name: form.values.name,
-            description: form.values.description,
+            description: form.values.description || '',
             sql: sql,
             // TODO: should initial version get generated on the BE?
             config: config || {
                 metadata: { version: 1 },
                 type: ChartKind.VERTICAL_BAR,
             },
-            // TODO: add space selection
-            spaceUuid: spaces[0].uuid,
+            spaceUuid: spaceUuid,
         });
         dispatch(updateName(form.values.name));
         onClose();
@@ -96,9 +106,12 @@ export const SaveSqlChartModal: FC<Props> = ({ opened, onClose }) => {
         dispatch,
         form.values.description,
         form.values.name,
+        form.values.spaceUuid,
+        form.values.newSpaceName,
         onClose,
         spaces,
         sql,
+        createSpace,
     ]);
 
     return (
@@ -131,6 +144,11 @@ export const SaveSqlChartModal: FC<Props> = ({ opened, onClose }) => {
                             {...form.getInputProps('description')}
                         />
                     </Stack>
+                    <SaveToSpace
+                        form={form}
+                        spaces={spaces}
+                        projectUuid={projectUuid}
+                    />
                 </Stack>
 
                 <Group
