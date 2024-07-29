a810f73bd5bbfc3218da19693fd075a91501c980
Joao Viana <joao@lightdash.com>
2024-07-29 10:46:02 +0100

    style: apply scrollarea to table config fields

--- packages/frontend/src/features/sqlRunner/components/TableVisConfiguration.tsx
+++ packages/frontend/src/features/sqlRunner/components/TableVisConfiguration.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, TextInput } from '@mantine/core';
+import { ActionIcon, ScrollArea, Stack, TextInput } from '@mantine/core';
 import { IconEye, IconEyeOff } from '@tabler/icons-react';
 import { type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
@@ -23,61 +23,71 @@ const TableVisConfiguration: FC = ({}) => {
     }
 
     return (
-        <Config>
-            <Config.Section>
-                <Config.Heading>Column labels</Config.Heading>
-                {Object.keys(tableVisConfig.columns).map((reference) => {
-                    const fieldType = sqlColumns?.find(
-                        (c) => c.reference === reference,
-                    )?.type;
+        <ScrollArea
+            offsetScrollbars
+            variant="primary"
+            className="only-vertical"
+            type="auto"
+            sx={{ flex: 1 }}
+        >
+            <Config>
+                <Stack sx={{ flex: 1, overflow: 'hidden' }}>
+                    <Config.Heading>Column labels</Config.Heading>
 
-                    return (
-                        <TextInput
-                            key={reference}
-                            radius="md"
-                            value={tableVisConfig.columns[reference].label}
-                            icon={
-                                fieldType && (
-                                    <TableFieldIcon fieldType={fieldType} />
-                                )
-                            }
-                            rightSection={
-                                <ActionIcon
-                                    onClick={() =>
-                                        dispatch(
-                                            updateColumnVisibility({
-                                                reference,
-                                                visible:
-                                                    !tableVisConfig.columns[
-                                                        reference
-                                                    ].visible,
-                                            }),
-                                        )
-                                    }
-                                >
-                                    <MantineIcon
-                                        icon={
-                                            tableVisConfig.columns[reference]
-                                                .visible
-                                                ? IconEye
-                                                : IconEyeOff
+                    {Object.keys(tableVisConfig.columns).map((reference) => {
+                        const fieldType = sqlColumns?.find(
+                            (c) => c.reference === reference,
+                        )?.type;
+
+                        return (
+                            <TextInput
+                                key={reference}
+                                radius="md"
+                                value={tableVisConfig.columns[reference].label}
+                                icon={
+                                    fieldType && (
+                                        <TableFieldIcon fieldType={fieldType} />
+                                    )
+                                }
+                                rightSection={
+                                    <ActionIcon
+                                        onClick={() =>
+                                            dispatch(
+                                                updateColumnVisibility({
+                                                    reference,
+                                                    visible:
+                                                        !tableVisConfig.columns[
+                                                            reference
+                                                        ].visible,
+                                                }),
+                                            )
                                         }
-                                    />
-                                </ActionIcon>
-                            }
-                            onChange={(e) => {
-                                dispatch(
-                                    updateFieldLabel({
-                                        reference,
-                                        label: e.target.value,
-                                    }),
-                                );
-                            }}
-                        />
-                    );
-                })}
-            </Config.Section>
-        </Config>
+                                    >
+                                        <MantineIcon
+                                            icon={
+                                                tableVisConfig.columns[
+                                                    reference
+                                                ].visible
+                                                    ? IconEye
+                                                    : IconEyeOff
+                                            }
+                                        />
+                                    </ActionIcon>
+                                }
+                                onChange={(e) => {
+                                    dispatch(
+                                        updateFieldLabel({
+                                            reference,
+                                            label: e.target.value,
+                                        }),
+                                    );
+                                }}
+                            />
+                        );
+                    })}
+                </Stack>
+            </Config>
+        </ScrollArea>
     );
 };
 
