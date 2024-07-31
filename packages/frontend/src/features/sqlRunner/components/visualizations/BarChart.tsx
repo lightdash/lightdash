79517780114e7bc14598747f7c65403cc46e4026
Oliver Laslett <11660098+owlas@users.noreply.github.com>
2024-07-31 15:34:51 +0100

    feat: improved loading state for sql runner (#10929)
    ---------
    
    Co-authored-by: João Viana <joao@lightdash.com>

--- packages/frontend/src/features/sqlRunner/components/visualizations/BarChart.tsx
+++ packages/frontend/src/features/sqlRunner/components/visualizations/BarChart.tsx
@@ -1,5 +1,5 @@
 import { type BarChartConfig } from '@lightdash/common';
-import { Center, Skeleton } from '@mantine/core';
+import { Center, LoadingOverlay } from '@mantine/core';
 import EChartsReact, { type EChartsReactProps } from 'echarts-for-react';
 import { memo, type FC } from 'react';
 import { type ResultsAndColumns } from '../../hooks/useSqlQueryRun';
@@ -26,11 +26,10 @@ const BarChart: FC<BarChartProps> = memo(
 
         return (
             <>
-                {!spec && <Skeleton h="100%" />}
+                <LoadingOverlay visible={loading || !spec} />
                 {spec && (
                     <EChartsReact
                         option={spec}
-                        showLoading={loading}
                         opts={{
                             renderer: 'svg',
                             width: 'auto',
