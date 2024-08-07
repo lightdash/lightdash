4383d471339f317273e3322e9cef59b7386e18d1
Javier Rengel Jiménez <rephus@gmail.com>
2024-08-07 15:31:01 +0200

    chore: add new client placeholder (#11046)
    * chore: add new client placeholder

---
+++ packages/backend/src/clients/cube/CubeClient.ts
@@ -0,0 +1,50 @@
+import cube, { CubeApi, Query } from '@cubejs-client/core';
+import { MissingConfigError } from '@lightdash/common';
+import { LightdashConfig } from '../../config/parseConfig';
+
+type CubeArgs = {
+    lightdashConfig: LightdashConfig;
+};
+
+export default class CubeClient {
+    // eslint-disable-next-line class-methods-use-this
+    cubeApi?: CubeApi;
+
+    constructor({ lightdashConfig }: CubeArgs) {
+        const { token, domain } = lightdashConfig.cube;
+        // In development mode, the token is not required for authorization
+
+        if (domain === undefined) {
+            console.warn(
+                'Cube token and domain are not set, CubeClient will not be initialized',
+            );
+            return;
+        }
+
+        this.cubeApi = cube(token, { apiUrl: `${domain}/cubejs-api/v1` });
+    }
+
+    async runQuery(cubeQuery: Query) {
+        /* query sample: 
+        {
+        measures: ['Stories.count'],
+        timeDimensions: [{
+            dimension: 'Stories.time',
+            dateRange: ['2015-01-01', '2015-12-31'],
+            granularity: 'month'
+        }]
+        } */
+        if (this.cubeApi === undefined)
+            throw new MissingConfigError('Cube has not been initialized');
+        const resultSet = await this.cubeApi.load(cubeQuery);
+        return resultSet;
+    }
+
+    async getSql(query: Query) {
+        if (this.cubeApi === undefined)
+            throw new MissingConfigError('Cube has not been initialized');
+
+        const sql = await this.cubeApi.sql(query);
+        return sql;
+    }
+}
--- packages/backend/src/config/lightdashConfig.mock.ts
+++ packages/backend/src/config/lightdashConfig.mock.ts
@@ -148,4 +148,7 @@ export const lightdashConfigMock: LightdashConfig = {
             allowedDomains: [],
         },
     },
+    cube: {
+        token: '',
+    },
 };
