43d092811b43190bf932e56a839c6b5794470434
Rephus <rephus@gmail.com>
2024-08-08 09:39:55 +0200

    M  packages/backend/src/services/SemanticLayerService/SemanticLayerService.ts

--- packages/backend/src/services/SemanticLayerService/SemanticLayerService.ts
+++ packages/backend/src/services/SemanticLayerService/SemanticLayerService.ts
@@ -69,9 +69,7 @@ export class SemanticLayerService extends BaseService {
         }
     }
 
-    async getSemanticLayerClient(
-        projectUuid: string,
-    ): Promise<{
+    async getSemanticLayerClient(projectUuid: string): Promise<{
         client: CubeClient;
         transformer: typeof cubeTransfomers;
     } /* | DbtCloudGraphqlClient */> {
