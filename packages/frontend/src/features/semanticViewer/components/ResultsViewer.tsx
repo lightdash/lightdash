1c70f5951d34d143aedcb631962d84748b6b4c42
Rephus <rephus@gmail.com>
2024-08-12 16:44:28 +0200

    fix . in client response

--- packages/frontend/src/features/semanticViewer/components/ResultsViewer.tsx
+++ packages/frontend/src/features/semanticViewer/components/ResultsViewer.tsx
@@ -6,6 +6,7 @@ import { useSemanticViewerQueryRun } from '../api/streamingResults';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { setResults } from '../store/semanticViewerSlice';
 
+const sanitizeFieldId = (fieldId: string) => fieldId.replace('.', '_');
 const ResultsViewer: FC = () => {
     const {
         projectUuid,
@@ -27,7 +28,7 @@ const ResultsViewer: FC = () => {
                                     const [key, resultValue] = entry;
                                     return {
                                         ...acc,
-                                        [key]: {
+                                        [sanitizeFieldId(key)]: {
                                             value: {
                                                 raw: resultValue,
                                                 formatted:
@@ -53,9 +54,9 @@ const ResultsViewer: FC = () => {
         ].reduce((acc, dimension) => {
             return {
                 ...acc,
-                [dimension]: {
+                [sanitizeFieldId(dimension)]: {
                     visible: true,
-                    reference: dimension,
+                    reference: sanitizeFieldId(dimension),
                     label: dimension,
                     frozen: false,
                     order: undefined,
