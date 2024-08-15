8cb857140812544a05d7ee4f86144480c96c88e2
João Viana <joao@lightdash.com>
2024-08-15 14:14:43 +0100

    refactor: simplify `useSqlQueryRun` hook (#11156)
    * docs: improve hooks documentation
    
    * refactor: have only one mutation in usqSqlQueryRun

--- packages/frontend/src/features/sqlRunner/hooks/useSqlQueryRun.tsx
+++ packages/frontend/src/features/sqlRunner/hooks/useSqlQueryRun.tsx
@@ -3,17 +3,16 @@ import {
     SchedulerJobStatus,
     type ApiError,
     type ApiJobScheduledResponse,
-    type ApiSqlRunnerJobStatusResponse,
     type ResultRow,
     type SqlColumn,
     type SqlRunnerBody,
 } from '@lightdash/common';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
 import { lightdashApi } from '../../../api';
 import useToaster from '../../../hooks/toaster/useToaster';
-import { getSchedulerJobStatus } from '../../scheduler/hooks/useScheduler';
 import { useAppSelector } from '../store/hooks';
+import { getResultsFromStream, getSqlRunnerCompleteJob } from './requestUtils';
 
 const scheduleSqlJob = async ({
     projectUuid,
@@ -37,11 +36,9 @@ export type ResultsAndColumns = {
 
 /**
  * Gets the SQL query results from the server
- *
- * Steps:
- * 1. Schedule the SQL query job
- * 2. Get the status of the scheduled job
- * 3. Fetch the results of the job
+ * This is a hook that is used to get the results of a SQL query - used in the SQL runner
+ * @param onSuccess - The function to call when the results are fetched.
+ * @returns The results of the SQL query
  */
 export const useSqlQueryRun = ({
     onSuccess,
@@ -50,169 +47,73 @@ export const useSqlQueryRun = ({
 }) => {
     const { showToastError } = useToaster();
     const projectUuid = useAppSelector((state) => state.sqlRunner.projectUuid);
-    const {
-        mutate,
-        isLoading: isMutating,
-        data: sqlQueryJob,
-    } = useMutation<
-        ApiJobScheduledResponse['results'],
+    const [data, setData] = useState<ResultsAndColumns | undefined>(undefined);
+    const previousDataRef = useRef<ResultsAndColumns | undefined>(undefined);
+
+    const { mutate, isLoading } = useMutation<
+        ResultsAndColumns | undefined,
         ApiError,
         {
             sql: SqlRunnerBody['sql'];
             limit: SqlRunnerBody['limit'];
         }
-    >(({ sql, limit }) => scheduleSqlJob({ projectUuid, sql, limit }), {
-        mutationKey: ['sqlRunner', 'run'],
-    });
-
-    const { data: scheduledDeliveryJobStatus, isFetching: jobIsLoading } =
-        useQuery<
-            ApiSqlRunnerJobStatusResponse['results'] | undefined,
-            ApiError
-        >(
-            ['jobStatus', sqlQueryJob?.jobId],
-            () => {
-                if (!sqlQueryJob?.jobId) return;
-                return getSchedulerJobStatus<ApiSqlRunnerJobStatusResponse>(
-                    sqlQueryJob.jobId,
-                );
-            },
-            {
-                refetchInterval: (data, query) => {
-                    if (
-                        !!query.state.error ||
-                        data?.status === SchedulerJobStatus.COMPLETED ||
-                        data?.status === SchedulerJobStatus.ERROR
-                    )
-                        return false;
-                    return 1000;
-                },
-                onSuccess: (data) => {
-                    if (data?.status === SchedulerJobStatus.ERROR) {
-                        if (isErrorDetails(data?.details)) {
-                            showToastError({
-                                title: 'Could not run SQL query',
-                                subtitle: data?.details?.error,
-                            });
-                        }
-                    }
-                },
-                enabled: Boolean(
-                    sqlQueryJob && sqlQueryJob?.jobId !== undefined,
-                ),
-            },
-        );
-
-    const { data: sqlQueryResults, isFetching: isResultsLoading } = useQuery<
-        ResultRow[] | undefined,
-        ApiError | unknown, // Error could be unknown when trying to parse the JSON
-        ResultsAndColumns | undefined
     >(
-        ['sqlQueryResults', sqlQueryJob?.jobId],
-        async () => {
-            const url =
-                !isErrorDetails(scheduledDeliveryJobStatus?.details) &&
-                scheduledDeliveryJobStatus?.details?.fileUrl;
-            if (!url) {
-                throw new Error('Missing file URL');
+        async ({ sql, limit }) => {
+            const scheduledJob = await scheduleSqlJob({
+                projectUuid,
+                sql,
+                limit,
+            });
+
+            const job = await getSqlRunnerCompleteJob(scheduledJob.jobId);
+
+            if (job.status === SchedulerJobStatus.ERROR) {
+                if (isErrorDetails(job.details)) {
+                    showToastError({
+                        title: 'Could not run SQL query',
+                        subtitle: job.details.error,
+                    });
+                }
+                return undefined;
             }
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                },
-            });
-            const rb = response.body;
-            const reader = rb?.getReader();
 
-            const stream = new ReadableStream({
-                start(controller) {
-                    function push() {
-                        void reader?.read().then(({ done, value }) => {
-                            if (done) {
-                                // Close the stream
-                                controller.close();
-                                return;
-                            }
-                            // Enqueue the next data chunk into our target stream
-                            controller.enqueue(value);
+            const url =
+                job.details && !isErrorDetails(job.details)
+                    ? job.details.fileUrl
+                    : undefined;
+            const results = await getResultsFromStream(url);
 
-                            push();
-                        });
-                    }
-
-                    push();
-                },
-            });
-
-            const responseStream = new Response(stream, {
-                headers: { 'Content-Type': 'application/json' },
-            });
-            const result = await responseStream.text();
-
-            // Split the JSON strings by newline
-            const jsonStrings = result.trim().split('\n');
-            const jsonObjects: ResultRow[] = jsonStrings
-                .map((jsonString) => {
-                    try {
-                        return JSON.parse(jsonString);
-                    } catch (e) {
-                        throw new Error('Error parsing JSON');
-                    }
-                })
-                .filter((obj) => obj !== null);
-
-            return jsonObjects;
+            return {
+                results,
+                columns:
+                    job.details && !isErrorDetails(job.details)
+                        ? job.details.columns
+                        : [],
+            };
         },
         {
+            mutationKey: ['sqlRunner', 'run'],
             onError: () => {
                 showToastError({
                     title: 'Could not fetch SQL query results',
                 });
             },
-            enabled: Boolean(
-                scheduledDeliveryJobStatus?.status ===
-                    SchedulerJobStatus.COMPLETED &&
-                    !isErrorDetails(scheduledDeliveryJobStatus?.details) &&
-                    scheduledDeliveryJobStatus?.details?.fileUrl !== undefined,
-            ),
-            select: (data) => {
-                if (
-                    !data ||
-                    isErrorDetails(scheduledDeliveryJobStatus?.details) ||
-                    !scheduledDeliveryJobStatus?.details?.columns
-                ) {
-                    return undefined;
-                }
-
-                return {
-                    results: data,
-                    columns: scheduledDeliveryJobStatus.details.columns,
-                };
+            onMutate: () => {
+                // Save the current data to the previousDataRef so we can simulate a keepPreviousData behavior
+                previousDataRef.current = data;
             },
-            onSuccess: (data) => {
-                onSuccess(data);
+            onSuccess: (newData) => {
+                setData(newData);
+                onSuccess(newData);
             },
-            keepPreviousData: true,
         },
     );
 
-    const isLoading = useMemo(
-        () =>
-            isMutating ||
-            jobIsLoading ||
-            scheduledDeliveryJobStatus?.status === SchedulerJobStatus.STARTED ||
-            isResultsLoading,
-        [
-            isMutating,
-            jobIsLoading,
-            scheduledDeliveryJobStatus?.status,
-            isResultsLoading,
-        ],
-    );
+    const currentData = isLoading ? previousDataRef.current : data;
+
     return {
         mutate,
         isLoading,
-        data: sqlQueryResults,
+        data: currentData,
     };
 };
