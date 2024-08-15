79586afab4fffc37db879e31dfb05d705884fbef
João Viana <joao@lightdash.com>
2024-08-15 13:11:36 +0100

    feat: allow formatting by percentage (#11152)
    * feat: enable ui formatting of percentage
    
    * refactor: simplify and make it clearer
    
    * perf: defer results height to improve ui painting performance
    
    * chore: rename
    
    * fix: add correct formatting when only one y axis

--- packages/frontend/src/features/sqlRunner/store/cartesianChartBaseSlice.ts
+++ packages/frontend/src/features/sqlRunner/store/cartesianChartBaseSlice.ts
@@ -1,6 +1,7 @@
 import {
     DEFAULT_AGGREGATION,
     IndexType,
+    isFormat,
     type AggregationOptions,
     type BarChartSqlConfig,
     type CartesianChartDisplay,
@@ -194,10 +195,33 @@ export const cartesianChartConfigSlice = createSlice({
             }
         },
         removeYAxisField: (state, action: PayloadAction<number>) => {
-            if (!state.config) return;
-            if (!state.config.fieldConfig) return;
+            if (!state.config?.fieldConfig?.y) return;
 
-            state.config.fieldConfig.y.splice(action.payload, 1);
+            const index = action.payload;
+            state.config.fieldConfig.y.splice(index, 1);
+
+            if (state.config.display) {
+                if (state.config.display.yAxis) {
+                    state.config.display.yAxis.splice(index, 1);
+                }
+
+                if (state.config.display.series) {
+                    Object.entries(state.config.display.series).forEach(
+                        ([key, series]) => {
+                            if (series.yAxisIndex === index) {
+                                // NOTE: Delete series from display object when it's removed from yAxis
+                                delete state.config?.display?.series?.[key];
+                            } else if (
+                                // NOTE: Decrease yAxisIndex for series that are after the removed yAxis
+                                series.yAxisIndex &&
+                                series.yAxisIndex > index
+                            ) {
+                                series.yAxisIndex--;
+                            }
+                        },
+                    );
+                }
+            }
         },
         setStacked: ({ config }, action: PayloadAction<boolean>) => {
             if (!config) return;
@@ -205,6 +229,70 @@ export const cartesianChartConfigSlice = createSlice({
             config.display = config.display || {};
 
             config.display.stack = action.payload;
+        },
+
+        setYAxisFormat: (
+            { config },
+            action: PayloadAction<{ format: string }>,
+        ) => {
+            if (!config) return;
+            config.display = config.display || {};
+
+            const validFormat = isFormat(action.payload.format)
+                ? action.payload.format
+                : undefined;
+
+            config.display.yAxis = config.display.yAxis || [];
+
+            if (config.display.yAxis.length === 0) {
+                config.display.yAxis.push({ format: validFormat });
+            } else {
+                config.display.yAxis[0].format = validFormat;
+            }
+
+            // Update the format for series with yAxisIndex 0
+            if (config.display.series) {
+                Object.values(config.display.series).forEach((series) => {
+                    if (series.yAxisIndex === 0) {
+                        series.format = validFormat;
+                    }
+                });
+            } else if (config.fieldConfig?.y[0].reference) {
+                config.display.series = {
+                    [config.fieldConfig?.y[0].reference]: {
+                        format: validFormat,
+                        yAxisIndex: 0,
+                    },
+                };
+            }
+        },
+        setSeriesFormat: (
+            { config },
+            action: PayloadAction<{
+                index: number;
+                format: string;
+                reference: string;
+            }>,
+        ) => {
+            if (!config?.display) return;
+
+            const { index, format, reference } = action.payload;
+            const validFormat = isFormat(format) ? format : undefined;
+
+            config.display.series = config.display.series || {};
+            config.display.series[reference] = {
+                ...config.display.series[reference],
+                format: validFormat,
+                yAxisIndex: index,
+            };
+
+            if (index === 0) {
+                config.display.yAxis = config.display.yAxis || [];
+                config.display.yAxis[0] = {
+                    ...config.display.yAxis[0],
+                    format: validFormat,
+                };
+            }
         },
     },
 });
