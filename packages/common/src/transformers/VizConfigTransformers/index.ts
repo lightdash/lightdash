1c1035c70a29aba612967d24677757cd4c9ea240
oliverlaslett <11660098+owlas@users.noreply.github.com>
2024-08-20 07:34:26 +0100

    chore: remove unused transformer poc

--- packages/common/src/transformers/VizConfigTransformers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AbstractVizConfigTransformer';
-export * from './Cartisean/AbstractCartiseanConfigTransformer';
-export * from './Cartisean/BarConfigTransformer';
-export * from './Table/TableConfigTransformer';
-export * from './VizConfigTransformerFactory';
