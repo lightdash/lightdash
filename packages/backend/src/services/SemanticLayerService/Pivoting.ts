5fd95d34117094c6b98306ef132b0fe3d394b2d3
almeidabbm <almeidabatistam@gmail.com>
2024-08-22 18:37:46 +0100

    chore: fix groupBy and pivot

--- packages/backend/src/services/SemanticLayerService/Pivoting.ts
+++ packages/backend/src/services/SemanticLayerService/Pivoting.ts
@@ -5,48 +5,46 @@ import pl from 'nodejs-polars';
 export function pivotResults(
     results: SemanticLayerResultRow[],
     allDimensions: string[],
-    { values, ...options }: SemanticLayerPivot,
+    pivot: SemanticLayerPivot,
 ): SemanticLayerResultRow[] {
+    const { values, ...options } = pivot;
+    const df = pl.DataFrame(results);
+
+    if (!options.index.length) {
+        const aliasedAggs: pl.Expr[] = values.map((v) =>
+            pl
+                .col(v.name)
+                [v.aggFunction]()
+                .alias(`${v.aggFunction}(${v.name})`),
+        );
+
+        return df
+            .groupBy(options.on)
+            .agg(...aliasedAggs)
+            .toRecords();
+    }
+
+    // Group by all dimensions that are not in the values
     const dimensionsToGroupBy = uniq([
         ...options.on,
         ...options.index,
         ...allDimensions,
     ]).filter((dim) => !values.map((v) => v.name).includes(dim));
 
+    // This can happen when we group by and aggregate on the same dimension
     if (dimensionsToGroupBy.length === 0) {
-        console.info(
-            'skipping pivot, no dimensions to group by -----------------------',
-        );
         return results;
     }
 
-    const df = pl.DataFrame(results);
-
-    console.info('og pivot config ----------------------');
-    console.info({ values, ...options });
-
-    console.info('OG data frame ----------------------');
-    console.info(df);
-
-    console.info({ on: options.on, index: options.index, allDimensions });
-    console.info({ dimensionsToGroupBy });
-
     const aggs: pl.Expr[] = values.map((v) => pl.col(v.name)[v.aggFunction]());
 
-    const groupedByDf = df.groupBy(dimensionsToGroupBy).agg(...aggs);
-
-    console.info('Grouped by data frame ----------------------');
-    console.info(groupedByDf);
-
-    const pivotedDf = groupedByDf
+    return df
+        .groupBy(dimensionsToGroupBy)
+        .agg(...aggs)
         .withColumns(pl.col(dimensionsToGroupBy).fillNull('zero'))
         .pivot(
             values.map((v) => v.name),
             options,
-        );
-
-    console.info('Pivoted data frame ----------------------');
-    console.info(pivotedDf);
-
-    return pivotedDf.toRecords();
+        )
+        .toRecords();
 }
