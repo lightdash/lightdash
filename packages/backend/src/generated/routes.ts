975a1c51ebce273d950cadfac1ab0e5220dfb9a5
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2024-08-22 13:45:01 +0400

    chore: code gen

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -6586,8 +6586,8 @@ const models: TsoaRoute.Models = {
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     VizAggregationOptions: {
-        dataType: 'refAlias',
-        type: { dataType: 'string', validators: {} },
+        dataType: 'refEnum',
+        enums: ['sum', 'count', 'min', 'max', 'first'],
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     VizSqlCartesianChartLayout: {
