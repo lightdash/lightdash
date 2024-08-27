33d7d03d5add28f837074e44a3d11f39fa6453bf
Tony Orciuoli <tony.orciuoli@workday.com>
2024-08-27 15:51:23 -0700

    PERFTOOL-14506: Added add upstream script.

---
+++ portal_scripts/add_upstream.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if ! test -f $PWD/yarn.lock; then
+    echo "Script must run from the repository's root directory"
+    exit 1
+fi
+
+if git remote | grep -q 'upstream'; then
+    echo "Upstream already exists"
+    exit 0
+fi
+
+git remote add upstream https://github.com/lightdash/lightdash.git
+git fetch upstreamg
