bd10dd2e9180bd6bd68450fb3356a2258bc5b85a
Joao Viana <joao@lightdash.com>
2024-08-30 10:12:22 +0100

    chore: fix type

--- packages/common/src/visualizations/CartesianChartDataModel.ts
+++ packages/common/src/visualizations/CartesianChartDataModel.ts
@@ -20,6 +20,7 @@ export class CartesianChartDataModel
         IChartDataModel<
             VizCartesianChartOptions,
             VizCartesianChartConfig,
+            CartesianChartDisplay,
             CartesianChartKind
         >
 {
