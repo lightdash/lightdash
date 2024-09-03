a4ea4fd369b8ab5128536981f4c52522f7ac9192
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2024-09-03 13:11:44 +0400

    chore: remove table forward ref

--- packages/frontend/src/components/DataViz/visualizations/Table.tsx
+++ packages/frontend/src/components/DataViz/visualizations/Table.tsx
@@ -3,9 +3,7 @@ import {
     type VizTableColumnsConfig,
 } from '@lightdash/common';
 import { Badge, Flex, Group, type FlexProps } from '@mantine/core';
-import { mergeRefs } from '@mantine/hooks';
 import { flexRender } from '@tanstack/react-table';
-import { forwardRef } from 'react';
 import { SMALL_TEXT_LENGTH } from '../../common/LightTable';
 import BodyCell from '../../common/Table/ScrollableTable/BodyCell';
 import { VirtualizedArea } from '../../common/Table/ScrollableTable/TableBody';
@@ -23,130 +21,125 @@ type TableProps<T extends ResultsRunner> = {
     flexProps?: FlexProps;
 };
 
-export const Table = forwardRef(
-    <T extends ResultsRunner>(
-        { resultsRunner, config, flexProps }: TableProps<T>,
-        ref: React.Ref<HTMLDivElement>,
-    ) => {
-        const {
-            tableWrapperRef,
-            getColumnsCount,
-            getTableData,
-            paddingTop,
-            paddingBottom,
-        } = useTableDataModel({ config, resultsRunner });
+export const Table = <T extends ResultsRunner>({
+    resultsRunner,
+    config,
+    flexProps,
+}: TableProps<T>) => {
+    const {
+        tableWrapperRef,
+        getColumnsCount,
+        getTableData,
+        paddingTop,
+        paddingBottom,
+    } = useTableDataModel({ config, resultsRunner });
 
-        const columnsCount = getColumnsCount();
-        const { headerGroups, virtualRows, rowModelRows } = getTableData();
+    const columnsCount = getColumnsCount();
+    const { headerGroups, virtualRows, rowModelRows } = getTableData();
 
-        return (
-            <Flex
-                ref={mergeRefs(ref, tableWrapperRef)}
-                direction="column"
-                miw="100%"
-                {...flexProps}
-                sx={{
-                    overflow: 'auto',
-                    fontFamily: "'Inter', sans-serif",
-                    fontFeatureSettings: "'tnum'",
-                    flexGrow: 1,
-                    ...flexProps?.sx,
-                }}
-            >
-                <TableStyled>
-                    <thead>
-                        <tr>
-                            {headerGroups.map((headerGroup) =>
-                                headerGroup.headers.map((header) => (
-                                    <th
-                                        key={header.id}
-                                        style={{
-                                            backgroundColor: TABLE_HEADER_BG,
-                                        }}
-                                    >
-                                        <Group spacing="two">
-                                            {config?.columns[header.id]
-                                                ?.aggregation && (
-                                                <Badge
-                                                    size="sm"
-                                                    color="indigo"
-                                                    radius="xs"
-                                                >
-                                                    {
-                                                        config?.columns[
-                                                            header.id
-                                                        ]?.aggregation
-                                                    }
-                                                </Badge>
-                                            )}
-                                            {/* TODO: do we need to check if it's a
+    return (
+        <Flex
+            ref={tableWrapperRef}
+            direction="column"
+            miw="100%"
+            {...flexProps}
+            sx={{
+                overflow: 'auto',
+                fontFamily: "'Inter', sans-serif",
+                fontFeatureSettings: "'tnum'",
+                flexGrow: 1,
+                ...flexProps?.sx,
+            }}
+        >
+            <TableStyled>
+                <thead>
+                    <tr>
+                        {headerGroups.map((headerGroup) =>
+                            headerGroup.headers.map((header) => (
+                                <th
+                                    key={header.id}
+                                    style={{
+                                        backgroundColor: TABLE_HEADER_BG,
+                                    }}
+                                >
+                                    <Group spacing="two">
+                                        {config?.columns[header.id]
+                                            ?.aggregation && (
+                                            <Badge
+                                                size="sm"
+                                                color="indigo"
+                                                radius="xs"
+                                            >
+                                                {
+                                                    config?.columns[header.id]
+                                                        ?.aggregation
+                                                }
+                                            </Badge>
+                                        )}
+                                        {/* TODO: do we need to check if it's a
                                             placeholder? */}
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext(),
-                                            )}
-                                        </Group>
-                                    </th>
-                                )),
-                            )}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {paddingTop > 0 && (
-                            <VirtualizedArea
-                                cellCount={columnsCount}
-                                padding={paddingTop}
-                            />
+                                        {flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext(),
+                                        )}
+                                    </Group>
+                                </th>
+                            )),
                         )}
-                        {virtualRows.map(({ index }) => {
-                            return (
-                                <Tr key={index} $index={index}>
-                                    {rowModelRows[index]
-                                        .getVisibleCells()
-                                        .map((cell) => {
-                                            const cellValue =
-                                                cell.getValue() as
-                                                    | RawResultRow[0]
-                                                    | undefined;
+                    </tr>
+                </thead>
+                <tbody>
+                    {paddingTop > 0 && (
+                        <VirtualizedArea
+                            cellCount={columnsCount}
+                            padding={paddingTop}
+                        />
+                    )}
+                    {virtualRows.map(({ index }) => {
+                        return (
+                            <Tr key={index} $index={index}>
+                                {rowModelRows[index]
+                                    .getVisibleCells()
+                                    .map((cell) => {
+                                        const cellValue = cell.getValue() as
+                                            | RawResultRow[0]
+                                            | undefined;
 
-                                            return (
-                                                <BodyCell
-                                                    key={cell.id}
-                                                    index={index}
-                                                    cell={cell}
-                                                    isNumericItem={false}
-                                                    hasData={!!cellValue}
-                                                    isLargeText={
-                                                        (
-                                                            cellValue?.toString() ||
-                                                            ''
-                                                        ).length >
-                                                        SMALL_TEXT_LENGTH
-                                                    }
-                                                >
-                                                    {cell.getIsPlaceholder()
-                                                        ? null
-                                                        : flexRender(
-                                                              cell.column
-                                                                  .columnDef
-                                                                  .cell,
-                                                              cell.getContext(),
-                                                          )}
-                                                </BodyCell>
-                                            );
-                                        })}
-                                </Tr>
-                            );
-                        })}
-                        {paddingBottom > 0 && (
-                            <VirtualizedArea
-                                cellCount={columnsCount}
-                                padding={paddingBottom}
-                            />
-                        )}
-                    </tbody>
-                </TableStyled>
-            </Flex>
-        );
-    },
-);
+                                        return (
+                                            <BodyCell
+                                                key={cell.id}
+                                                index={index}
+                                                cell={cell}
+                                                isNumericItem={false}
+                                                hasData={!!cellValue}
+                                                isLargeText={
+                                                    (
+                                                        cellValue?.toString() ||
+                                                        ''
+                                                    ).length > SMALL_TEXT_LENGTH
+                                                }
+                                            >
+                                                {cell.getIsPlaceholder()
+                                                    ? null
+                                                    : flexRender(
+                                                          cell.column.columnDef
+                                                              .cell,
+                                                          cell.getContext(),
+                                                      )}
+                                            </BodyCell>
+                                        );
+                                    })}
+                            </Tr>
+                        );
+                    })}
+                    {paddingBottom > 0 && (
+                        <VirtualizedArea
+                            cellCount={columnsCount}
+                            padding={paddingBottom}
+                        />
+                    )}
+                </tbody>
+            </TableStyled>
+        </Flex>
+    );
+};
