d8b1b3e324d9286489ff8758a498d4a4f1b1532b
almeidabbm <almeidabatistam@gmail.com>
2024-09-04 11:03:10 +0100

    chore: move forwarding to group

--- packages/frontend/src/features/semanticViewer/components/FiltersModal/FilterFieldSelectItem.tsx
+++ packages/frontend/src/features/semanticViewer/components/FiltersModal/FilterFieldSelectItem.tsx
@@ -1,21 +1,19 @@
 import type { SemanticLayerField } from '@lightdash/common';
-import { Group } from '@mantine/core';
+import { Group, type GroupProps } from '@mantine/core';
 import { forwardRef } from 'react';
 import FieldIcon from '../FieldIcon';
 
-interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
+interface ItemProps extends GroupProps {
     field: SemanticLayerField;
 }
 
 const FilterFieldSelectItem = forwardRef<HTMLDivElement, ItemProps>(
     ({ field, ...others }: ItemProps, ref) => {
         return (
-            <div ref={ref} {...others}>
-                <Group noWrap>
-                    <FieldIcon field={field} />
-                    {field.label ?? field.name}
-                </Group>
-            </div>
+            <Group noWrap ref={ref} {...others}>
+                <FieldIcon field={field} />
+                {field.label ?? field.name}
+            </Group>
         );
     },
 );
