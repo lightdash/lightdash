d086ce727b15d3f4beb30c3b8b6cd50200950caa
Rephus <rephus@gmail.com>
2024-09-05 16:29:47 +0200

    M  packages/e2e/cypress/e2e/api/sqlRunner.cy.ts

--- packages/e2e/cypress/e2e/api/sqlRunner.cy.ts
+++ packages/e2e/cypress/e2e/api/sqlRunner.cy.ts
@@ -19,8 +19,8 @@ Object.entries({ snowflake: warehouseConnections.snowflake }).forEach(
             switch (warehouseConfig.type) {
                 case WarehouseTypes.SNOWFLAKE:
                     return [
-                        warehouseConfig.database.toLowerCase(),
-                        warehouseConfig.schema.toLowerCase(),
+                        warehouseConfig.database, // On staging, these are uppercased
+                        warehouseConfig.schema,
                     ];
                 case WarehouseTypes.BIGQUERY:
                     return [warehouseConfig.project, warehouseConfig.dataset];
@@ -169,14 +169,33 @@ Object.entries({ snowflake: warehouseConnections.snowflake }).forEach(
                     expect(Object.keys(resp.body.results[database])).to.include(
                         schema,
                     );
-                    ['customers', 'orders', 'payments'].forEach((table) => {
+                    if (warehouseConfig.type === WarehouseTypes.SNOWFLAKE) {
+                        ['CUSTOMERS', 'ORDERS', 'PAYMENTS'].forEach((table) => {
+                            expect(
+                                Object.keys(
+                                    resp.body.results[database][schema],
+                                ),
+                            ).to.include(table);
+                        });
                         expect(
-                            Object.keys(resp.body.results[database][schema]),
-                        ).to.include(table);
-                    });
-                    expect(
-                        Object.keys(resp.body.results[database][schema].orders),
-                    ).to.be.deep.eq([]);
+                            Object.keys(
+                                resp.body.results[database][schema].ORDERS,
+                            ),
+                        ).to.be.deep.eq([]);
+                    } else {
+                        ['customers', 'orders', 'payments'].forEach((table) => {
+                            expect(
+                                Object.keys(
+                                    resp.body.results[database][schema],
+                                ),
+                            ).to.include(table);
+                        });
+                        expect(
+                            Object.keys(
+                                resp.body.results[database][schema].orders,
+                            ),
+                        ).to.be.deep.eq([]);
+                    }
                 });
             });
             it(`Get fields for SQL runner ${warehouseName}`, () => {
@@ -192,13 +211,24 @@ Object.entries({ snowflake: warehouseConnections.snowflake }).forEach(
                     const { results } = resp.body;
 
                     expect(Object.keys(results)).to.have.length.gt(5);
-                    ['order_id', 'status', 'amount'].forEach((table) => {
-                        expect(Object.keys(results)).to.include(table);
-                    });
-                    expect(results.amount).to.be.eq('number');
-                    expect(results.status).to.be.eq('string');
-                    expect(results.is_completed).to.be.eq('boolean');
-                    expect(results.order_date).to.be.eq('date');
+
+                    if (warehouseConfig.type === WarehouseTypes.SNOWFLAKE) {
+                        ['ORDER_ID', 'STATUS', 'AMOUNT'].forEach((table) => {
+                            expect(Object.keys(results)).to.include(table);
+                        });
+                        expect(results.AMOUNT).to.be.eq('number');
+                        expect(results.STATUS).to.be.eq('string');
+                        expect(results.IS_COMPLETED).to.be.eq('boolean');
+                        expect(results.ORDER_DATE).to.be.eq('date');
+                    } else {
+                        ['order_id', 'status', 'amount'].forEach((table) => {
+                            expect(Object.keys(results)).to.include(table);
+                        });
+                        expect(results.amount).to.be.eq('number');
+                        expect(results.status).to.be.eq('string');
+                        expect(results.is_completed).to.be.eq('boolean');
+                        expect(results.order_date).to.be.eq('date');
+                    }
                 });
             });
 
