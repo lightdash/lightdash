f574e7e8b55ea090a86fe99dc0626ed394cdad27
Andrew Dillion-Walshe <andrewdillion@gmail.com>
2024-09-10 11:12:34 +0200

    added LoadingOverlay to tables panel and passed error from useRefreshTables

--- packages/frontend/src/features/sqlRunner/components/TablesPanel.tsx
+++ packages/frontend/src/features/sqlRunner/components/TablesPanel.tsx
@@ -1,56 +1,78 @@
-import { Box } from '@mantine/core';
+import { Box, LoadingOverlay, Text } from '@mantine/core';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import { useAppSelector } from '../store/hooks';
 import { TableFields } from './TableFields';
 import { Tables } from './Tables';
 
-export const TablesPanel = () => {
+type TablesPanelProps = {
+    isLoading: boolean;
+    error: string | null;
+};
+
+export const TablesPanel: React.FC<TablesPanelProps> = ({
+    isLoading,
+    error,
+}) => {
     const initialPanelSizes = [80, 20];
     const activeTable = useAppSelector((state) => state.sqlRunner.activeTable);
+
     return (
-        <PanelGroup direction="vertical">
-            <Panel
-                id="sql-runner-tables"
-                order={1}
-                defaultSize={initialPanelSizes[0]}
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-            >
-                <Tables />
-            </Panel>
+        <Box sx={{ position: 'relative', flex: 1 }}>
+            <LoadingOverlay visible={isLoading} />
 
-            {activeTable && (
-                <>
-                    <Box
-                        component={PanelResizeHandle}
-                        bg="gray.3"
-                        h={3}
-                        sx={(theme) => ({
-                            transition: 'background-color 0.2s ease-in-out',
-                            '&[data-resize-handle-state="hover"]': {
-                                backgroundColor: theme.colors.gray[3],
-                            },
-                            '&[data-resize-handle-state="drag"]': {
-                                backgroundColor: theme.colors.gray[2],
-                            },
-                        })}
-                    />
+            {error && (
+                <Text color="red" align="center">
+                    {error}
+                </Text>
+            )}
 
+            {!isLoading && !error && (
+                <PanelGroup direction="vertical">
                     <Panel
-                        id="sql-runner-table-fields"
-                        order={2}
-                        defaultSize={initialPanelSizes[1]}
+                        id="sql-runner-tables"
+                        order={1}
+                        defaultSize={initialPanelSizes[0]}
                         style={{
                             display: 'flex',
                             flexDirection: 'column',
                         }}
                     >
-                        <TableFields />
+                        <Tables />
                     </Panel>
-                </>
+
+                    {activeTable && (
+                        <>
+                            <Box
+                                component={PanelResizeHandle}
+                                bg="gray.3"
+                                h={3}
+                                sx={(theme) => ({
+                                    transition:
+                                        'background-color 0.2s ease-in-out',
+                                    '&[data-resize-handle-state="hover"]': {
+                                        backgroundColor: theme.colors.gray[3],
+                                    },
+                                    '&[data-resize-handle-state="drag"]': {
+                                        backgroundColor: theme.colors.gray[2],
+                                    },
+                                })}
+                            />
+
+                            <Panel
+                                id="sql-runner-table-fields"
+                                order={2}
+                                defaultSize={initialPanelSizes[1]}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                }}
+                            >
+                                <TableFields />
+                            </Panel>
+                        </>
+                    )}
+                </PanelGroup>
             )}
-        </PanelGroup>
+        </Box>
     );
 };
