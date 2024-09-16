89c3e6df084682b939a0d866c3262d65e1c40139
almeidabbm <almeidabatistam@gmail.com>
2024-09-16 17:25:08 +0100

    chore: remove unused import

--- packages/backend/src/clients/dbtCloud/typeTransformers.ts
+++ packages/backend/src/clients/dbtCloud/typeTransformers.ts
@@ -8,7 +8,6 @@ import {
     isSemanticLayerBaseOperator,
     isSemanticLayerExactTimeFilter,
     isSemanticLayerRelativeTimeFilter,
-    isSemanticLayerRelativeTimeValue,
     isSemanticLayerStringFilter,
     isSemanticLayerTimeFilter,
     SemanticLayerFieldType,
