f8d2700463caace557f680e8fd79b157541fb7a2
Joao Viana <joao@lightdash.com>
2024-09-19 15:06:22 +0100

    chore: improve alert and save sql modal

--- packages/frontend/src/features/sqlRunner/components/SqlQueryBeforeSaveAlert.tsx
+++ packages/frontend/src/features/sqlRunner/components/SqlQueryBeforeSaveAlert.tsx
@@ -1,110 +1,19 @@
-import {
-    Accordion,
-    Box,
-    Button,
-    Group,
-    Text,
-    type AccordionControlProps,
-} from '@mantine/core';
-import { Editor } from '@monaco-editor/react';
-import { IconAlertTriangle } from '@tabler/icons-react';
+import { Alert, Text } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons-react';
 import { type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
-import { useAppSelector } from '../store/hooks';
 
-const AccordionControl: FC<
-    AccordionControlProps & {
-        onUse: () => void;
-    }
-> = ({ onUse, ...props }) => {
+export const SqlQueryBeforeSaveAlert: FC = () => {
     return (
-        <Box sx={{ display: 'flex', alignItems: 'center' }} pr="xs">
-            <Accordion.Control {...props} fz="xs" fw={500} />
-            <Button size="xs" compact onClick={onUse}>
-                Use
-            </Button>
-        </Box>
-    );
-};
-
-export const SqlQueryBeforeSaveAlert: FC<{
-    onUse: (value: 'unsaved' | 'previous') => void;
-}> = ({ onUse }) => {
-    const sqlToSave = useAppSelector(
-        (state) => state.sqlRunner.successfulSqlQueries.current,
-    );
-    const sql = useAppSelector((state) => state.sqlRunner.sql);
-
-    return (
-        <Box
-            sx={(theme) => ({
-                border: `1px solid ${theme.colors.gray[3]}`,
-                borderRadius: theme.radius.md,
-                overflow: 'hidden',
-            })}
+        <Alert
+            icon={<MantineIcon icon={IconAlertCircle} color="yellow" />}
+            color="yellow"
+            title="You haven't run your query yet"
         >
-            <Box bg="yellow.1" w="100%" p="xs">
-                <Group spacing="xs" noWrap>
-                    <MantineIcon icon={IconAlertTriangle} color="yellow.7" />
-                    <Text fz="xs" fw={500}>
-                        You have unsaved changes in your query. Review the
-                        changes before saving.
-                    </Text>
-                </Group>
-            </Box>
-
-            <Accordion>
-                <Accordion.Item value="unsaved-changes">
-                    <AccordionControl onUse={() => onUse('unsaved')}>
-                        Unsaved changes
-                    </AccordionControl>
-                    <Accordion.Panel>
-                        <Editor
-                            height={200}
-                            width={400}
-                            language="sql"
-                            value={sql}
-                            options={{
-                                readOnly: true,
-                                minimap: { enabled: false },
-                                scrollBeyondLastLine: false,
-                                contextmenu: false,
-                                lineNumbers: 'off',
-                                glyphMargin: false,
-                                lineDecorationsWidth: 0,
-                                revealHorizontalRightPadding: 0,
-                                roundedSelection: false,
-                            }}
-                            theme="lightdash"
-                        />
-                    </Accordion.Panel>
-                </Accordion.Item>
-                <Accordion.Item value="previous-query">
-                    <AccordionControl onUse={() => onUse('previous')}>
-                        Previous successful query
-                    </AccordionControl>
-                    <Accordion.Panel>
-                        <Editor
-                            height={200}
-                            width={400}
-                            language="sql"
-                            value={sqlToSave}
-                            options={{
-                                readOnly: true,
-                                minimap: { enabled: false },
-                                scrollBeyondLastLine: false,
-                                contextmenu: false,
-                                lineNumbers: 'off',
-                                glyphMargin: false,
-                                lineDecorationsWidth: 0,
-                                revealHorizontalRightPadding: 0,
-                                roundedSelection: false,
-                            }}
-                            theme="lightdash"
-                        />
-                    </Accordion.Panel>
-                </Accordion.Item>
-            </Accordion>
-        </Box>
+            <Text fw={500} size="xs">
+                You can verify your query before saving or you can review it
+                later.
+            </Text>
+        </Alert>
     );
 };
