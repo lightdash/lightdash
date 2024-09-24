89f6e24d9f7b7b65a5f55141828794f813666a2b
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2024-09-24 10:26:43 +0400

    feat: view mode

--- packages/frontend/src/pages/SemanticViewerView.tsx
+++ packages/frontend/src/pages/SemanticViewerView.tsx
@@ -1,8 +1,10 @@
-import { ChartKind, isVizTableConfig } from '@lightdash/common';
-import { Box } from '@mantine/core';
+import { assertUnreachable, ChartKind } from '@lightdash/common';
+import { Box, Group, SegmentedControl, Text } from '@mantine/core';
+import { IconChartHistogram, IconTable } from '@tabler/icons-react';
 import { pick } from 'lodash';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import MantineIcon from '../components/common/MantineIcon';
 import Page from '../components/common/Page/Page';
 import { useChartViz } from '../components/DataViz/hooks/useChartViz';
 import ChartView from '../components/DataViz/visualizations/ChartView';
@@ -14,7 +16,16 @@ import {
 import { HeaderView } from '../features/semanticViewer/components/Header/HeaderView';
 import { SemanticViewerResultsRunner } from '../features/semanticViewer/runners/SemanticViewerResultsRunner';
 
+enum ViewerTabs {
+    VIZ = 'viz',
+    RESULTS = 'results',
+}
+
 const SemanticViewerViewPage = () => {
+    const [activeViewerTab, setActiveViewerTab] = useState<ViewerTabs>(
+        ViewerTabs.VIZ,
+    );
+
     const { projectUuid, savedSemanticViewerChartUuid } = useParams<{
         projectUuid: string;
         savedSemanticViewerChartUuid: string;
@@ -73,19 +84,45 @@ const SemanticViewerViewPage = () => {
     const [chartVizQuery, chartSpec] = useChartViz({
         projectUuid,
         resultsRunner,
-        uuid: savedSemanticViewerChartUuid ?? undefined,
+        uuid: savedSemanticViewerChartUuid,
         config: chartQuery.data?.chart.config,
         slug: chartQuery.data?.chart.slug,
     });
 
+    const pivotResultsRunner = useMemo(() => {
+        if (!chartQuery.isSuccess) return;
+        if (!fieldsQuery.isSuccess) return;
+        if (!chartVizQuery.isSuccess) return;
+
+        if (chartQuery.data.chart.config.type === ChartKind.TABLE) return;
+
+        return new SemanticViewerResultsRunner({
+            projectUuid,
+            query: chartQuery.data.chart.semanticLayerQuery,
+            rows: chartVizQuery.data?.results ?? [],
+            columns: chartVizQuery.data?.columns ?? [],
+            fields: fieldsQuery.data,
+        });
+    }, [
+        projectUuid,
+        fieldsQuery.isSuccess,
+        fieldsQuery.data,
+        chartQuery.isSuccess,
+        chartQuery.data,
+        chartVizQuery.isSuccess,
+        chartVizQuery.data,
+    ]);
+
     if (chartQuery.isError) return null;
     if (!chartQuery.isSuccess) return null;
+
+    const chartType = chartQuery.data.chart.config.type;
 
     return (
         <Page
             title="Semantic Viewer"
             withFullHeight
-            noContentPadding
+            withPaddedContent
             header={
                 <HeaderView
                     projectUuid={projectUuid}
@@ -93,22 +130,56 @@ const SemanticViewerViewPage = () => {
                 />
             }
         >
-            {resultsRunner &&
-                chartQuery.data.chart.config.type === ChartKind.TABLE &&
-                isVizTableConfig(chartQuery.data.chart.config) && (
-                    // So that the Table tile isn't cropped by the overflow
-                    <Box w="100%" h="100%" sx={{ overflow: 'auto' }}>
-                        <Table
-                            resultsRunner={resultsRunner}
-                            columnsConfig={chartQuery.data.chart.config.columns}
-                        />
-                    </Box>
-                )}
+            {chartType === ChartKind.TABLE ? null : (
+                <Box mb="lg">
+                    <SegmentedControl
+                        styles={(theme) => ({
+                            root: {
+                                backgroundColor: theme.colors.gray[2],
+                            },
+                        })}
+                        size="sm"
+                        radius="md"
+                        data={[
+                            {
+                                value: ViewerTabs.VIZ,
+                                label: (
+                                    <Group spacing="xs" noWrap>
+                                        <MantineIcon
+                                            icon={IconChartHistogram}
+                                        />
+                                        <Text>Chart</Text>
+                                    </Group>
+                                ),
+                            },
+                            {
+                                value: ViewerTabs.RESULTS,
+                                label: (
+                                    <Group spacing="xs" noWrap>
+                                        <MantineIcon icon={IconTable} />
+                                        <Text>Results</Text>
+                                    </Group>
+                                ),
+                            },
+                        ]}
+                        disabled={
+                            !chartQuery.isSuccess ||
+                            !fieldsQuery.isSuccess ||
+                            !chartData ||
+                            !resultsRunner
+                        }
+                        value={activeViewerTab}
+                        onChange={(value: ViewerTabs) =>
+                            setActiveViewerTab(value)
+                        }
+                    />
+                </Box>
+            )}
 
-            {savedSemanticViewerChartUuid &&
-                (chartQuery.data.chart.config.type === ChartKind.VERTICAL_BAR ||
-                    chartQuery.data.chart.config.type === ChartKind.LINE ||
-                    chartQuery.data.chart.config.type === ChartKind.PIE) && (
+            {activeViewerTab === ViewerTabs.VIZ ? (
+                chartType === ChartKind.VERTICAL_BAR ||
+                chartType === ChartKind.LINE ||
+                chartType === ChartKind.PIE ? (
                     <ChartView
                         config={chartQuery.data.chart.config}
                         spec={chartSpec}
@@ -120,7 +191,49 @@ const SemanticViewerViewPage = () => {
                             width: '100%',
                         }}
                     />
-                )}
+                ) : chartType === ChartKind.TABLE ? (
+                    resultsRunner ? (
+                        <Box w="100%" h="100%" sx={{ overflow: 'auto' }}>
+                            {/* So that the Table tile isn't cropped by the overflow */}
+                            <Table
+                                resultsRunner={resultsRunner}
+                                columnsConfig={
+                                    chartQuery.data.chart.config.columns
+                                }
+                            />
+                        </Box>
+                    ) : null
+                ) : (
+                    assertUnreachable(
+                        chartType,
+                        `Unknown chart type ${chartType}`,
+                    )
+                )
+            ) : activeViewerTab === ViewerTabs.RESULTS ? (
+                pivotResultsRunner ? (
+                    <Table
+                        resultsRunner={pivotResultsRunner}
+                        columnsConfig={Object.fromEntries(
+                            chartVizQuery.data?.columns.map((field) => [
+                                field.reference,
+                                {
+                                    visible: true,
+                                    reference: field.reference,
+                                    label: field.reference,
+                                    frozen: false,
+                                    // TODO: add aggregation
+                                    // aggregation?: VizAggregationOptions;
+                                },
+                            ]) ?? [],
+                        )}
+                    />
+                ) : null
+            ) : (
+                assertUnreachable(
+                    activeViewerTab,
+                    `Unknown tab ${activeViewerTab}`,
+                )
+            )}
         </Page>
     );
 };
