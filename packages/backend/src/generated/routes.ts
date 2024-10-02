0c7e7f47ba0399c6ea8ca6ec111d3d688c6fece5
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2024-10-02 14:52:17 +0400

    feat: semantic viewer menu copy (#11757)

--- packages/backend/src/generated/routes.ts
+++ packages/backend/src/generated/routes.ts
@@ -7435,6 +7435,18 @@ const models: TsoaRoute.Models = {
         },
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    ApiGithubDbtWriteBack: {
+        dataType: 'refAlias',
+        type: {
+            dataType: 'nestedObjectLiteral',
+            nestedProperties: {
+                results: { ref: 'PullRequestCreated', required: true },
+                status: { dataType: 'enum', enums: ['ok'], required: true },
+            },
+            validators: {},
+        },
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     'Pick_SshKeyPair.publicKey_': {
         dataType: 'refAlias',
         type: {
