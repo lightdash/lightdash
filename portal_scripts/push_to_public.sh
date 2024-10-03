7416c318c993bde6d42415fa789695cb36bb9f66
Tony Orciuoli <tony.orciuoli@workday.com>
2024-10-03 16:02:19 -0700

    PERFTOOL-14506: Reworked scripts to avoid pushing internal files public.

--- portal_scripts/push_to_public.sh
+++ portal_scripts/push_to_public.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+TEMP_PATCH_LOCATION="$HOME/lightdash-public-diff-patch.tmp"
+
 get_restricted_content () {
     ALL_CONTENT=$1
     HAS_RESTRICTED=$(echo "$ALL_CONTENT" | grep -iq -e 'RK.AY' -e 'P.RFTO' && { echo 1; } || { echo 0; })
@@ -10,7 +12,16 @@ get_restricted_content () {
     fi
 }
 
-print_code_diff () {
+save_code_diff () {
+  git diff public/main ':(exclude).security_config/' ':(exclude)portal_scripts/' > $TEMP_PATCH_LOCATION
+  echo "$(cat $TEMP_PATCH_LOCATION)"
+}
+
+delete_code_diff () {
+  rm $TEMP_PATCH_LOCATION
+}
+
+print_code_diff_no_ignore () {
   echo "vvvvvvv DIFF FROM PUBLIC vvvvvvv"
   git --no-pager diff public/main
   echo "^^^^^^^ DIFF FROM PUBLIC ^^^^^^^"
@@ -41,14 +52,6 @@ git fetch public
 MODIFIED=$(git status | grep -q 'nothing to commit' && { echo 0; } || { echo 1; })
 if [ $MODIFIED = 1 ]; then
     echo "#### Add/commit your local changes before pushing to your public LD."
-    exit 1
-fi
-
-BRANCH_INFO=$(git branch -r)
-IS_TRACKING_PUBLIC=$(git branch -r | grep -qE '[[:space:]]public\/main$' && { echo 1; } || { echo 0; })
-if [ $IS_TRACKING_PUBLIC != 1 ]; then
-    echo "#### Branch is not tracking the public repo. Create a new branch with:"
-    echo "    > git checkout -b name-of-branch --track public/main"
     exit 1
 fi
 
@@ -113,28 +116,11 @@ if [ $IS_LOCAL_BEHIND_PUBLIC_INTERNAL = 1 ]; then
     exit 1;
 fi
 
-RESTRICTED_WORDS_IN_BRANCH_NAME=$(get_restricted_content "$BRANCH_INFO")
-if [[ $RESTRICTED_WORDS_IN_BRANCH_NAME ]]; then
-    echo "vvvvvvv Branch name must not use restricted words vvvvvvv"
-    echo "$RESTRICTED_WORDS_IN_BRANCH_NAME"
-    echo "^^^^^^^ Branch name must not use restricted words ^^^^^^^"
-    exit 1
-fi
-
-CODE_DIFF=$(git diff public/main)
-RESTRICTED_WORDS_IN_CODE=$(get_restricted_content "$CODE_DIFF")
-if [[ $RESTRICTED_WORDS_IN_CODE ]]; then
-    echo "vvvvvvv Code changes must not use restricted words vvvvvvv"
-    echo "$RESTRICTED_WORDS_IN_CODE"
-    echo "^^^^^^^ Code changes must not use restricted words ^^^^^^^"
-    exit 1
-fi
-
 echo "#### Has the PR been approved by the team and passed all checks? (y/n)"
 CODE_REVIEWED_AND_PASSED=""
 while true; do
     read CODE_REVIEWED_AND_PASSED
-    if [[ $CODE_REVIEWED_AND_PASSED != 'y' ]]; then
+    if [[ $CODE_REVIEWED_AND_PASSED != [yY] ]]; then
         echo "#### Please get the PR approved by the team and ensure all checks have passed."
         exit 1
     fi
@@ -145,28 +131,67 @@ echo "#### Has the Jira been QA tested and marked with status VERIFIED? (y/n)"
 QA_TESTED_AND_VERIFIED=""
 while true; do
     read QA_TESTED_AND_VERIFIED
-    if [[ $QA_TESTED_AND_VERIFIED != 'y' ]]; then
+    if [[ $QA_TESTED_AND_VERIFIED != [yY] ]]; then
         echo "#### Please get QA approval."
         exit 1
     fi
     break
 done
 
-print_code_diff
+CODE_DIFF=$(save_code_diff)
+RESTRICTED_WORDS_IN_CODE=$(get_restricted_content "$CODE_DIFF")
+if [[ $RESTRICTED_WORDS_IN_CODE ]]; then
+    echo "vvvvvvv Code changes must not use restricted words vvvvvvv"
+    echo "$RESTRICTED_WORDS_IN_CODE"
+    echo "^^^^^^^ Code changes must not use restricted words ^^^^^^^"
+    delete_code_diff
+    exit 1
+fi
+
+read -p "Enter branch name to be displayed publicly: " NEW_BRANCH_NAME
+RESTRICTED_WORDS_IN_BRANCH_NAME=$(get_restricted_content "$NEW_BRANCH_NAME")
+if [[ $RESTRICTED_WORDS_IN_BRANCH_NAME ]]; then
+    echo "vvvvvvv Branch name must not use restricted words vvvvvvv"
+    echo "$RESTRICTED_WORDS_IN_BRANCH_NAME"
+    echo "^^^^^^^ Branch name must not use restricted words ^^^^^^^"
+    exit 1
+fi
+
+read -p "Enter commit message to be displayed publicly: " COMMIT_MESSAGE
+RESTRICTED_WORDS_IN_COMMIT_MESSAGE=$(get_restricted_content "$COMMIT_MESSAGE")
+if [[ $RESTRICTED_WORDS_IN_COMMIT_MESSAGE ]]; then
+    echo "vvvvvvv Commit message must not use restricted words vvvvvvv"
+    echo "$RESTRICTED_WORDS_IN_COMMIT_MESSAGE"
+    echo "^^^^^^^ Commit message must not use restricted words ^^^^^^^"
+    exit 1
+fi
+
+git checkout -q public/main
+git checkout -b $NEW_BRANCH_NAME
+git apply $TEMP_PATCH_LOCATION
+git add .
+git commit -m "$COMMIT_MESSAGE"
+
+print_code_diff_no_ignore
 
 echo "#### Push the above changes to public GitHub? (y/n)"
 FINAL_OK=""
 while true; do
     read FINAL_OK
-    if [[ $FINAL_OK != 'y' ]]; then
+    if [[ $FINAL_OK != [yY] ]]; then
         echo "#### Changes not pushed."
+        delete_code_diff
         exit 1
     fi
     break
 done
 
-echo "#### Changes ready to be pushed public. Use the following command then create a pull request on GitHub."
+echo "#### Changes NOT pushed!"
+echo "#### Although, changes READY to be pushed public. Use the following command then create a pull request on GitHub."
 echo "    > git push -u public HEAD"
+
 # DO NOT AUTOMATICALLY PUSH AT THIS TIME
 #git push -u public HEAD
 #echo "#### Changes pushed public. Create a pull request on GitHub."
+
+delete_code_diff
