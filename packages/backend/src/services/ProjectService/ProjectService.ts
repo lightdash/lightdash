62b15c606919eaf08254ec2e00e8868faa916c7c
Javier Rengel Jiménez <rephus@gmail.com>
2024-10-30 10:28:31 +0100

    chore: add analytic events to scheduler timezone changes (#12142)
    * chore: add analytic events to scheduler timezone changes
    
    * Update packages/backend/src/services/ProjectService/ProjectService.ts
    
    Co-authored-by: Bruno Almeida <almeidabbm@users.noreply.github.com>
    
    * M  packages/backend/src/models/SchedulerModel/index.ts
    
    ---------
    
    Co-authored-by: Bruno Almeida <almeidabbm@users.noreply.github.com>

--- packages/backend/src/services/ProjectService/ProjectService.ts
+++ packages/backend/src/services/ProjectService/ProjectService.ts
@@ -53,6 +53,7 @@ import {
     getIntrinsicUserAttributes,
     getItemId,
     getMetrics,
+    getTimezoneLabel,
     hasIntersection,
     IntrinsicUserAttributes,
     isCustomSqlDimension,
@@ -4699,6 +4700,16 @@ export class ProjectService extends BaseService {
                 projectUuid,
                 schedulerTimezone,
             );
+
+        this.analytics.track({
+            event: 'default_scheduler_timezone.updated',
+            userId: user.userUuid,
+            properties: {
+                projectId: projectUuid,
+                organizationUuid: project.organizationUuid,
+                timeZone: getTimezoneLabel(schedulerTimezone),
+            },
+        });
 
         return updatedProject;
     }
