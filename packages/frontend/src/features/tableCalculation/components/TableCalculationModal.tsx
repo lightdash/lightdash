9248153064b92b76ff311e5400e5b2d210447b32
magnew <mcagnew@gmail.com>
2024-10-30 18:12:50 +0100

    feat: stacking error list (#12162)
    * feat: stacking error list
    
    * handle duplicate messages
    
    * handle api errors
    
    * lint fix
    
    * PR feedback
    
    * pr feedback

--- packages/frontend/src/features/tableCalculation/components/TableCalculationModal.tsx
+++ packages/frontend/src/features/tableCalculation/components/TableCalculationModal.tsx
@@ -52,7 +52,7 @@ const TableCalculationModal: FC<Props> = ({
     const theme = useMantineTheme();
     const [isFullscreen, toggleFullscreen] = useToggle(false);
 
-    const { showToastError } = useToaster();
+    const { addToastError } = useToaster();
 
     const tableCalculations = useExplorerContext(
         (context) =>
@@ -111,7 +111,10 @@ const TableCalculationModal: FC<Props> = ({
     const handleSubmit = form.onSubmit((data) => {
         const { name, sql } = data;
         if (sql.length === 0)
-            return showToastError({ title: 'SQL cannot be empty' });
+            return addToastError({
+                title: 'SQL cannot be empty',
+                key: 'table-calculation-modal',
+            });
 
         try {
             onSave({
@@ -122,9 +125,10 @@ const TableCalculationModal: FC<Props> = ({
                 type: data.type,
             });
         } catch (e) {
-            showToastError({
+            addToastError({
                 title: 'Error saving',
                 subtitle: e.message,
+                key: 'table-calculation-modal',
             });
         }
     });
