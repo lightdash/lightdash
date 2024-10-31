bd49516a0381fb02dd63f87e96420a9fdeddcc7c
Joao Viana <joao@lightdash.com>
2024-10-31 13:35:32 +0000

    feat: get field analytics on button click

--- packages/frontend/src/features/metricsCatalog/components/MetricChartsUsageModal.tsx
+++ packages/frontend/src/features/metricsCatalog/components/MetricChartsUsageModal.tsx
@@ -1,17 +1,32 @@
-import { Group, Modal, Stack, Text, type ModalProps } from '@mantine/core';
+import {
+    Anchor,
+    Group,
+    List,
+    Modal,
+    Stack,
+    Text,
+    type ModalProps,
+} from '@mantine/core';
 import { IconDeviceAnalytics } from '@tabler/icons-react';
 import { type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
-
+import { useAppSelector } from '../../sqlRunner/store/hooks';
+import { useMetricChartAnalytics } from '../hooks/useMetricChartAnalytics';
 type Props = ModalProps;
 
 export const MetricChartsUsageModal: FC<Props> = ({ opened, onClose }) => {
-    // const activeMetric = useAppSelector(
-    //     (state) => state.metricsCatalog.activeMetric,
-    // );
-    // const projectUuid = useAppSelector(
-    //     (state) => state.metricsCatalog.projectUuid,
-    // );
+    const activeMetric = useAppSelector(
+        (state) => state.metricsCatalog.activeMetric,
+    );
+    const projectUuid = useAppSelector(
+        (state) => state.metricsCatalog.projectUuid,
+    );
+
+    const { data: analytics, isLoading } = useMetricChartAnalytics({
+        projectUuid,
+        table: activeMetric?.tableName,
+        field: activeMetric?.name,
+    });
 
     return (
         <Modal
@@ -34,19 +49,28 @@ export const MetricChartsUsageModal: FC<Props> = ({ opened, onClose }) => {
         >
             <Stack p="md" spacing="xs">
                 <Text>This metric is used in the following charts:</Text>
-                {/* TODO: Add charts usage endpoint query data here */}
-                {/* <List pl="sm">
-                    {activeMetric?.analytics?.charts.map((chart) => (
-                        <List.Item key={chart.uuid} fz="sm">
-                            <Anchor
-                                href={`/projects/${projectUuid}/saved/${chart.uuid}`}
-                                target="_blank"
-                            >
-                                {chart.name}
-                            </Anchor>
-                        </List.Item>
-                    ))}
-                </List> */}
+                {isLoading ? (
+                    <Text size="sm" color="dimmed">
+                        Loading...
+                    </Text>
+                ) : analytics?.charts.length === 0 ? (
+                    <Text size="sm" color="dimmed">
+                        No charts found using this metric
+                    </Text>
+                ) : (
+                    <List pl="sm">
+                        {analytics?.charts.map((chart) => (
+                            <List.Item key={chart.uuid} fz="sm">
+                                <Anchor
+                                    href={`/projects/${projectUuid}/saved/${chart.uuid}`}
+                                    target="_blank"
+                                >
+                                    {chart.name}
+                                </Anchor>
+                            </List.Item>
+                        ))}
+                    </List>
+                )}
             </Stack>
         </Modal>
     );
