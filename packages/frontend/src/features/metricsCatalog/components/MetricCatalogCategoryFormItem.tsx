e7a70ffde9ef313ae7d6f4b0f5a214a25ff46fb0
Joao Viana <joao@lightdash.com>
2024-11-12 18:18:11 +0000

    feat: display color swatch picker

--- packages/frontend/src/features/metricsCatalog/components/MetricCatalogCategoryFormItem.tsx
+++ packages/frontend/src/features/metricsCatalog/components/MetricCatalogCategoryFormItem.tsx
@@ -1,8 +1,11 @@
 import { type CatalogItem } from '@lightdash/common';
 import {
     ActionIcon,
+    Box,
     Button,
     Group,
+    Popover,
+    SimpleGrid,
     Stack,
     Text,
     TextInput,
@@ -10,34 +13,41 @@ import {
     UnstyledButton,
     useMantineTheme,
 } from '@mantine/core';
-import { useHover } from '@mantine/hooks';
-import { IconDots, IconRefresh, IconTrash } from '@tabler/icons-react';
-import { useCallback, useState, type FC } from 'react';
+import { useDisclosure, useHover } from '@mantine/hooks';
+import { IconDots, IconTrash } from '@tabler/icons-react';
+import { useCallback, useRef, useState, type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
 import { useAppSelector } from '../../sqlRunner/store/hooks';
 import { useDeleteTag, useUpdateTag } from '../hooks/useProjectTags';
-import { getRandomColor } from '../utils/getRandomTagColor';
+import { getTagColorSwatches } from '../utils/getRandomTagColor';
 import { CatalogCategory } from './CatalogCategory';
 
-type Props = {
+type EditPopoverProps = {
+    hovered: boolean;
     category: CatalogItem['categories'][number];
-    onClick?: () => void;
+    onOpenChange?: (isOpen: boolean) => void;
 };
 
-export const MetricCatalogCategoryFormItem: FC<Props> = ({
+const EditPopover: FC<EditPopoverProps> = ({
+    hovered,
     category,
-    onClick,
+    onOpenChange,
 }) => {
     const projectUuid = useAppSelector(
         (state) => state.metricsCatalog.projectUuid,
     );
     const { mutate: updateTag } = useUpdateTag();
     const { mutate: deleteTag } = useDeleteTag();
-    const [isEditing, setIsEditing] = useState(false);
+    const [opened, { open, close }] = useDisclosure(false);
     const [editName, setEditName] = useState(category.name);
     const [editColor, setEditColor] = useState(category.color);
-    const { colors } = useMantineTheme();
-    const { ref: hoverRef, hovered } = useHover<HTMLDivElement>();
+    const colors = getTagColorSwatches(useMantineTheme().colors);
+    const popoverRef = useRef<HTMLDivElement>(null);
+
+    const handleClose = useCallback(() => {
+        close();
+        onOpenChange?.(false);
+    }, [close, onOpenChange]);
 
     const handleSave = useCallback(async () => {
         if (category.tagUuid && projectUuid) {
@@ -47,12 +57,12 @@ export const MetricCatalogCategoryFormItem: FC<Props> = ({
                     tagUuid: category.tagUuid,
                     data: { name: editName, color: editColor },
                 });
-                setIsEditing(false);
+                handleClose();
             } catch (error) {
                 console.error('Tag update failed:', error);
             }
         }
-    }, [editColor, editName, projectUuid, category, updateTag]);
+    }, [editColor, editName, projectUuid, category, updateTag, handleClose]);
 
     const onDelete = useCallback(async () => {
         if (category.tagUuid && projectUuid) {
@@ -64,65 +74,97 @@ export const MetricCatalogCategoryFormItem: FC<Props> = ({
         }
     }, [deleteTag, projectUuid, category]);
 
-    if (isEditing) {
-        return (
-            <Stack spacing="xs" w="100%">
-                <Group position="apart" noWrap>
-                    <Group noWrap spacing="xs">
-                        <Tooltip
-                            label={
-                                <Stack p="two" spacing="xs">
-                                    <Text fw={600}>Change color</Text>
-                                    <CatalogCategory
-                                        category={{
-                                            color: editColor,
-                                            name: editName,
-                                        }}
-                                    />
-                                </Stack>
-                            }
-                            variant="xs"
-                        >
-                            <ActionIcon
-                                size="xs"
-                                onClick={() =>
-                                    setEditColor(getRandomColor(colors))
-                                }
-                                sx={(theme) => ({
-                                    borderRadius: '90%',
-                                    backgroundColor: editColor,
-                                    '&:hover': {
-                                        backgroundColor: theme.fn.darken(
-                                            editColor,
-                                            0.1,
-                                        ),
-                                    },
-                                })}
-                            >
-                                <MantineIcon
-                                    icon={IconRefresh}
-                                    color="gray.0"
-                                    size={14}
-                                />
-                            </ActionIcon>
-                        </Tooltip>
-
+    return (
+        <Popover
+            position="top"
+            shadow="lg"
+            opened={opened}
+            trapFocus
+            radius="md"
+            closeOnClickOutside={true}
+            width={200}
+            onClose={handleClose}
+            withinPortal
+        >
+            <Popover.Target>
+                <ActionIcon
+                    sx={{
+                        visibility: hovered || opened ? 'visible' : 'hidden',
+                    }}
+                    size="xs"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        open();
+                        onOpenChange?.(true);
+                    }}
+                >
+                    <MantineIcon icon={IconDots} color="gray.6" size={14} />
+                </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown
+                onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                }}
+            >
+                <Stack spacing="xs" ref={popoverRef}>
+                    <Stack w="100%" spacing="two">
+                        <Text size="xs" weight={500} c="gray.7">
+                            Name
+                        </Text>
                         <TextInput
                             size="xs"
                             radius="md"
+                            w="100%"
                             value={editName}
                             onChange={(e) => setEditName(e.target.value)}
                         />
-                    </Group>
-                    <Group spacing={4} noWrap>
+                    </Stack>
+
+                    <Stack spacing="two">
+                        <Text size="xs" weight={500} c="gray.7">
+                            Colors
+                        </Text>
+                        <Group spacing="xs">
+                            <SimpleGrid cols={8} spacing="xs">
+                                {colors.map((color) => (
+                                    <Box
+                                        key={color}
+                                        w={14}
+                                        h={14}
+                                        bg={color}
+                                        sx={(theme) => ({
+                                            cursor: 'pointer',
+                                            borderRadius: '2px',
+                                            border:
+                                                editColor === color
+                                                    ? '1px solid black'
+                                                    : 'none',
+                                            '&:hover': {
+                                                backgroundColor:
+                                                    theme.fn.darken(color, 0.3),
+                                            },
+                                        })}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            setEditColor(color);
+                                        }}
+                                    />
+                                ))}
+                            </SimpleGrid>
+                        </Group>
+                    </Stack>
+
+                    <Group position="right" mt="xs">
                         <Tooltip
                             variant="xs"
                             label="Delete this tag permanently"
                         >
                             <ActionIcon
-                                size="xs"
-                                variant="subtle"
-                                color="red"
+                                size="sm"
+                                variant="outline"
+                                color="gray"
                                 onClick={onDelete}
                             >
                                 <MantineIcon icon={IconTrash} size={14} />
@@ -138,10 +180,24 @@ export const MetricCatalogCategoryFormItem: FC<Props> = ({
                             Save
                         </Button>
                     </Group>
-                </Group>
-            </Stack>
-        );
-    }
+                </Stack>
+            </Popover.Dropdown>
+        </Popover>
+    );
+};
+
+type Props = {
+    category: CatalogItem['categories'][number];
+    onClick?: () => void;
+    onSubPopoverChange?: (isOpen: boolean) => void;
+};
+
+export const MetricCatalogCategoryFormItem: FC<Props> = ({
+    category,
+    onClick,
+    onSubPopoverChange,
+}) => {
+    const { ref: hoverRef, hovered } = useHover<HTMLDivElement>();
 
     return (
         <Group
@@ -160,19 +216,13 @@ export const MetricCatalogCategoryFormItem: FC<Props> = ({
                 w="100%"
                 h="100%"
                 pos="absolute"
-            ></UnstyledButton>
+            />
             <CatalogCategory category={category} />
-            {hovered && (
-                <ActionIcon
-                    size="xs"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        setIsEditing(true);
-                    }}
-                >
-                    <MantineIcon icon={IconDots} color="gray.6" size={14} />
-                </ActionIcon>
-            )}
+            <EditPopover
+                hovered={hovered}
+                category={category}
+                onOpenChange={onSubPopoverChange}
+            />
         </Group>
     );
 };
