ac5f84dead0255a4b10aaebd4b4c449394c006a6
Joao Viana <joao@lightdash.com>
2024-11-15 14:16:09 +0000

    refactor: improve tag color generation and be backwards compatible

--- packages/frontend/src/features/metricsCatalog/utils/getRandomTagColor.tsx
+++ packages/frontend/src/features/metricsCatalog/utils/getRandomTagColor.tsx
@@ -1,31 +1,22 @@
-import { type useMantineTheme } from '@mantine/core';
-
-export const getTagColorSwatches = (
-    mantineColors: ReturnType<typeof useMantineTheme>['colors'],
-) => {
-    const colors = [
-        mantineColors.gray[5],
-        mantineColors.violet[4],
-        mantineColors.red[4],
-        mantineColors.orange[4],
-        mantineColors.green[4],
-        mantineColors.blue[4],
-        mantineColors.indigo[4],
-        mantineColors.pink[4],
-        mantineColors.yellow[4],
-    ];
-    return colors;
-};
+export const TAG_COLOR_SWATCHES = [
+    'gray',
+    'violet',
+    'red',
+    'orange',
+    'green',
+    'blue',
+    'indigo',
+    'pink',
+    'yellow',
+];
 
 /**
  * Get a random color from the mantine color palette
  * @param mantineColors - The mantine color palette
  * @returns A random color from the mantine color palette
  */
-export const getRandomColor = (
-    mantineColors: ReturnType<typeof useMantineTheme>['colors'],
-) => {
-    return getTagColorSwatches(mantineColors)[
-        Math.floor(Math.random() * getTagColorSwatches(mantineColors).length)
+export const getRandomColor = () => {
+    return TAG_COLOR_SWATCHES[
+        Math.floor(Math.random() * TAG_COLOR_SWATCHES.length)
     ];
 };
