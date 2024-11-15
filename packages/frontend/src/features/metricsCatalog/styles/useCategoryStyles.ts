226a8cdfd880e36d71f5d2cf7587e9e5fe067a20
Joao Viana <joao@lightdash.com>
2024-11-15 09:00:36 +0000

    style: make text color more vibrant

--- packages/frontend/src/features/metricsCatalog/styles/useCategoryStyles.ts
+++ packages/frontend/src/features/metricsCatalog/styles/useCategoryStyles.ts
@@ -4,7 +4,7 @@ export const useCategoryStyles = createStyles((theme, color: string) => ({
     base: {
         border: `1px solid ${theme.fn.lighten(color, 0.45)}`,
         backgroundColor: theme.fn.lighten(color, 0.92),
-        color: theme.fn.darken(color, 0.4),
+        color: theme.fn.darken(color, 0.2),
         cursor: 'pointer',
         boxShadow: '0px -2px 0px 0px rgba(4, 4, 4, 0.04) inset',
         '&:hover': {
