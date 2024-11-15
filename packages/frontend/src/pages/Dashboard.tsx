77e0c2c036f74bb2d9cc2a443c6597bbeecbd24c
Marshall Agnew <marshall@lightdash.com>
2024-11-15 16:33:09 +0100

    feat: remove filter warning and clear temp filters

--- packages/frontend/src/pages/Dashboard.tsx
+++ packages/frontend/src/pages/Dashboard.tsx
@@ -18,7 +18,6 @@ import MantineIcon from '../components/common/MantineIcon';
 import DashboardDeleteModal from '../components/common/modal/DashboardDeleteModal';
 import DashboardDuplicateModal from '../components/common/modal/DashboardDuplicateModal';
 import { DashboardExportModal } from '../components/common/modal/DashboardExportModal';
-import DashboardFiltersWarningModal from '../components/common/modal/DashboardFiltersWarningModal';
 import Page from '../components/common/Page/Page';
 import SuboptimalState from '../components/common/SuboptimalState/SuboptimalState';
 import DashboardFilter from '../components/DashboardFilter';
@@ -179,9 +178,6 @@ const Dashboard: FC = () => {
             (tile) => tile.type === DashboardTileTypes.SEMANTIC_VIEWER_CHART,
         );
     }, [dashboardTiles]);
-
-    const [isFilterWarningModalOpen, setIsFilterWarningModalOpen] =
-        useState(false);
 
     // tabs state
     const [activeTab, setActiveTab] = useState<DashboardTab | undefined>();
@@ -560,20 +556,14 @@ const Dashboard: FC = () => {
 
     const handleEnterEditMode = useCallback(() => {
         resetDashboardFilters();
-        setIsFilterWarningModalOpen(false);
-        history.replace({
-            pathname: `/projects/${projectUuid}/dashboards/${dashboardUuid}/edit`,
-            search: '',
+        // Defer the redirect
+        void Promise.resolve().then(() => {
+            history.replace({
+                pathname: `/projects/${projectUuid}/dashboards/${dashboardUuid}/edit`,
+                search: '',
+            });
         });
     }, [history, projectUuid, dashboardUuid, resetDashboardFilters]);
-
-    const handleEditModeClicked = useCallback(() => {
-        if (haveFiltersChanged) {
-            setIsFilterWarningModalOpen(true);
-        } else {
-            handleEnterEditMode();
-        }
-    }, [haveFiltersChanged, handleEnterEditMode]);
 
     if (dashboardError) {
         return <ErrorState error={dashboardError.error} />;
@@ -694,7 +684,7 @@ const Dashboard: FC = () => {
                         onExport={exportDashboardModalHandlers.open}
                         setAddingTab={setAddingTab}
                         onTogglePin={handleDashboardPinning}
-                        onEditClicked={handleEditModeClicked}
+                        onEditClicked={handleEnterEditMode}
                     />
                 }
                 withFullHeight={true}
@@ -755,13 +745,6 @@ const Dashboard: FC = () => {
                         uuid={dashboard.uuid}
                         onClose={duplicateModalHandlers.close}
                         onConfirm={duplicateModalHandlers.close}
-                    />
-                )}
-                {isFilterWarningModalOpen && (
-                    <DashboardFiltersWarningModal
-                        onConfirm={handleEnterEditMode}
-                        onClose={() => setIsFilterWarningModalOpen(false)}
-                        opened={isFilterWarningModalOpen}
                     />
                 )}
             </Page>
