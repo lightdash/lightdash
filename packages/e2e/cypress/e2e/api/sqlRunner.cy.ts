c5a5eae8efd2fd9e24500eab7a2da6c0e1e39c19
Javier Rengel Jiménez <rephus@gmail.com>
2024-11-19 09:44:39 +0100

    fix: disable databricks sqlrunner (#12483)

--- packages/e2e/cypress/e2e/api/sqlRunner.cy.ts
+++ packages/e2e/cypress/e2e/api/sqlRunner.cy.ts
@@ -20,7 +20,10 @@ Object.entries(warehouseConnections).forEach(
         const getDatabaseDetails = () => {
             switch (warehouseConfig.type) {
                 case WarehouseTypes.DATABRICKS:
-                    return [warehouseConfig.catalog, warehouseConfig.database];
+                    return [
+                        warehouseConfig.catalog || '',
+                        warehouseConfig.database,
+                    ];
                 case WarehouseTypes.SNOWFLAKE:
                     return [warehouseConfig.database, warehouseConfig.schema];
                 case WarehouseTypes.BIGQUERY:
@@ -498,6 +501,7 @@ describe(`Saved SQL chart`, () => {
                 sql: 'SELECT * FROM postgres.jaffle.payments',
                 limit: 21,
                 config: {
+                    display: {},
                     metadata: {
                         version: 1,
                     },
@@ -527,6 +531,7 @@ describe(`Saved SQL chart`, () => {
                         sql: 'SELECT * FROM postgres.jaffle.payments',
                         limit: 22,
                         config: {
+                            display: {},
                             metadata: {
                                 version: 1,
                             },
