7de69695959eef46ddc0070dcd61cd99423609ce
Javier Rengel Jiménez <rephus@gmail.com>
2024-11-21 17:44:56 +0100

    fix: reference line weeks (#12553)

--- packages/frontend/src/hooks/echarts/useEchartsCartesianConfig.ts
+++ packages/frontend/src/hooks/echarts/useEchartsCartesianConfig.ts
@@ -84,7 +84,10 @@ const getLabelFromField = (fields: ItemsMap, key: string | undefined) => {
     }
 };
 
-const getAxisTypeFromField = (item?: ItemsMap[string]): string => {
+const getAxisTypeFromField = (
+    item?: ItemsMap[string],
+    hasReferenceLine?: boolean,
+): string => {
     if (item && isCustomBinDimension(item)) return 'category';
     if (item && isTableCalculation(item) && !item.type) return 'value';
     if (
@@ -116,9 +119,11 @@ const getAxisTypeFromField = (item?: ItemsMap[string]): string => {
             case TableCalculationType.TIMESTAMP:
                 // Use categorical axis for weeks only. Echarts handles the
                 // other time frames well with a time axis
+                // Reference lines can only be used on time/value axes
                 if (
                     'timeInterval' in item &&
-                    item.timeInterval === TimeFrames.WEEK
+                    item.timeInterval === TimeFrames.WEEK &&
+                    !hasReferenceLine
                 ) {
                     return 'category';
                 }
@@ -148,18 +153,30 @@ const getAxisType = ({
     rightAxisYId,
     leftAxisYId,
 }: GetAxisTypeArg) => {
+    const hasReferenceLine = (axisId: string | undefined) => {
+        if (axisId === undefined) return false;
+        return validCartesianConfig.eChartsConfig.series?.some(
+            (serie) =>
+                serie.markLine !== undefined &&
+                (serie.encode.xRef.field === axisId ||
+                    serie.encode.yRef.field === axisId),
+        );
+    };
     const topAxisType = getAxisTypeFromField(
         topAxisXId ? itemsMap[topAxisXId] : undefined,
+        hasReferenceLine(topAxisXId),
     );
     const bottomAxisType =
         bottomAxisXId === EMPTY_X_AXIS
             ? 'category'
             : getAxisTypeFromField(
                   bottomAxisXId ? itemsMap[bottomAxisXId] : undefined,
+                  hasReferenceLine(bottomAxisXId),
               );
     // horizontal bar chart needs the type 'category' in the left/right axis
     const defaultRightAxisType = getAxisTypeFromField(
         rightAxisYId ? itemsMap[rightAxisYId] : undefined,
+        hasReferenceLine(rightAxisYId),
     );
     const rightAxisType =
         validCartesianConfig.layout.flipAxes &&
@@ -173,6 +190,7 @@ const getAxisType = ({
             : defaultRightAxisType;
     const defaultLeftAxisType = getAxisTypeFromField(
         leftAxisYId ? itemsMap[leftAxisYId] : undefined,
+        hasReferenceLine(leftAxisYId),
     );
     const leftAxisType =
         validCartesianConfig.layout.flipAxes &&
@@ -845,7 +863,6 @@ const getWeekAxisConfig = (
         axisField.timeInterval === TimeFrames.WEEK
     ) {
         const [minX, maxX] = getMinAndMaxValues([axisId], rows || []);
-
         const continuousWeekRange = [];
         let nextDate = dayjs.utc(minX);
         while (nextDate.isBefore(dayjs(maxX))) {
@@ -1127,7 +1144,6 @@ const getEchartAxes = ({
         validCartesianConfig.eChartsConfig.series,
         itemsMap,
     );
-
     const bottomAxisExtraConfig = getWeekAxisConfig(
         bottomAxisXId,
         bottomAxisXField,
