2cdc10717f4f86b1e2f0086900f83e11d0ef901c
magnew <mcagnew@gmail.com>
2024-11-22 16:17:49 +0100

    fix: I shouldnt be able to sort by a pivoted column (#12569)
    * fix: don't allow sorting on a pivoted column
    
    * add tooltip

--- packages/frontend/src/components/DataViz/visualizations/ChartDataTable.tsx
+++ packages/frontend/src/components/DataViz/visualizations/ChartDataTable.tsx
@@ -4,7 +4,7 @@ import {
     type VizColumnsConfig,
     type VizTableHeaderSortConfig,
 } from '@lightdash/common';
-import { Badge, Flex, Group, type FlexProps } from '@mantine/core';
+import { Badge, Flex, Group, Tooltip, type FlexProps } from '@mantine/core';
 import { IconArrowDown, IconArrowUp } from '@tabler/icons-react';
 import { flexRender } from '@tanstack/react-table';
 import { useMemo } from 'react';
@@ -62,73 +62,83 @@ export const ChartDataTable = ({
             className="sentry-block ph-no-capture"
         >
             <TableStyled>
-                <thead>
-                    <tr>
-                        {headerGroups.map((headerGroup) =>
-                            headerGroup.headers.map((header) => {
-                                const sortConfig = thSortConfig?.[header.id];
-                                const onClick =
-                                    sortConfig && onTHClick
-                                        ? () => onTHClick(header.id)
-                                        : undefined;
+                <Tooltip.Group>
+                    <thead>
+                        <tr>
+                            {headerGroups.map((headerGroup) =>
+                                headerGroup.headers.map((header) => {
+                                    const sortConfig =
+                                        thSortConfig?.[header.id];
+                                    const onClick =
+                                        sortConfig && onTHClick
+                                            ? () => onTHClick(header.id)
+                                            : undefined;
 
-                                return (
-                                    <th
-                                        key={header.id}
-                                        onClick={onClick}
-                                        style={
-                                            onClick
-                                                ? {
-                                                      cursor: 'pointer',
-                                                      backgroundColor:
-                                                          TABLE_HEADER_BG,
-                                                  }
-                                                : {
-                                                      backgroundColor:
-                                                          TABLE_HEADER_BG,
-                                                  }
-                                        }
-                                    >
-                                        <Group spacing="two" fz={13}>
-                                            {columnsConfig?.[header.id]
-                                                ?.aggregation && (
-                                                <Badge
-                                                    size="sm"
-                                                    color="indigo"
-                                                    radius="xs"
-                                                >
-                                                    {
-                                                        columnsConfig?.[
-                                                            header.id
-                                                        ]?.aggregation
-                                                    }
-                                                </Badge>
-                                            )}
-                                            {/* TODO: do we need to check if it's a
-                                      placeholder? */}
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext(),
-                                            )}
+                                    return (
+                                        <th
+                                            key={header.id}
+                                            onClick={onClick}
+                                            style={
+                                                onClick
+                                                    ? {
+                                                          cursor: 'pointer',
+                                                          backgroundColor:
+                                                              TABLE_HEADER_BG,
+                                                      }
+                                                    : {
+                                                          backgroundColor:
+                                                              TABLE_HEADER_BG,
+                                                      }
+                                            }
+                                        >
+                                            <Tooltip
+                                                label="You cannot sort by a group column"
+                                                disabled={!!onClick}
+                                                position="top"
+                                                withinPortal
+                                            >
+                                                <Group spacing="two" fz={13}>
+                                                    {columnsConfig?.[header.id]
+                                                        ?.aggregation && (
+                                                        <Badge
+                                                            size="sm"
+                                                            color="indigo"
+                                                            radius="xs"
+                                                        >
+                                                            {
+                                                                columnsConfig?.[
+                                                                    header.id
+                                                                ]?.aggregation
+                                                            }
+                                                        </Badge>
+                                                    )}
 
-                                            {onClick &&
-                                                sortConfig?.direction && (
-                                                    <MantineIcon
-                                                        icon={
-                                                            sortConfig.direction ===
-                                                            SortByDirection.ASC
-                                                                ? IconArrowUp
-                                                                : IconArrowDown
-                                                        }
-                                                    ></MantineIcon>
-                                                )}
-                                        </Group>
-                                    </th>
-                                );
-                            }),
-                        )}
-                    </tr>
-                </thead>
+                                                    {flexRender(
+                                                        header.column.columnDef
+                                                            .header,
+                                                        header.getContext(),
+                                                    )}
+
+                                                    {onClick &&
+                                                        sortConfig?.direction && (
+                                                            <MantineIcon
+                                                                icon={
+                                                                    sortConfig.direction ===
+                                                                    SortByDirection.ASC
+                                                                        ? IconArrowUp
+                                                                        : IconArrowDown
+                                                                }
+                                                            ></MantineIcon>
+                                                        )}
+                                                </Group>
+                                            </Tooltip>
+                                        </th>
+                                    );
+                                }),
+                            )}
+                        </tr>
+                    </thead>
+                </Tooltip.Group>
                 <tbody>
                     {paddingTop > 0 && (
                         <VirtualizedArea
