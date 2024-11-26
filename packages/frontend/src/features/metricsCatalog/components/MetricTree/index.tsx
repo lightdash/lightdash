e3afe6f972be74781aa0d708b3fe48734968e0e8
almeidabbm <almeidabatistam@gmail.com>
2024-11-26 17:01:39 +0000

    chore: initial code for metric tree canvas

---
+++ packages/frontend/src/features/metricsCatalog/components/MetricTree/index.tsx
@@ -0,0 +1,21 @@
+import { ReactFlow } from '@xyflow/react';
+import { type FC } from 'react';
+
+import { Box } from '@mantine/core';
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const MetricTree: FC = () => {
+    return (
+        <Box w="100%" h="calc(100dvh - 350px)">
+            <ReactFlow nodes={initialNodes} edges={initialEdges} />
+        </Box>
+    );
+};
+
+export default MetricTree;
