312f7b814ec7563d2e78428248698803cf323ab7
Joao Viana <joao@lightdash.com>
2024-11-28 16:16:59 +0000

    style: update metric peek date picker styles

--- packages/frontend/src/features/metricsCatalog/components/MetricPeekDatePicker.tsx
+++ packages/frontend/src/features/metricsCatalog/components/MetricPeekDatePicker.tsx
@@ -1,14 +1,18 @@
 import { type ApiGetMetricPeek } from '@lightdash/common';
 import {
+    Box,
     Button,
+    Divider,
     Group,
     Popover,
     Stack,
+    Text,
     TextInput,
     Tooltip,
+    UnstyledButton,
 } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
-import { IconCalendar } from '@tabler/icons-react';
+import { IconCalendar, IconChevronDown } from '@tabler/icons-react';
 import { type FC } from 'react';
 import MantineIcon from '../../../components/common/MantineIcon';
 import { useDateRangePicker } from '../hooks/useDateRangePicker';
@@ -46,100 +50,184 @@ export const MetricPeekDatePicker: FC<Props> = ({ defaultTimeDimension }) => {
                         variant="default"
                         radius="md"
                         onClick={() => handleOpen(!isOpen)}
-                        size="xs"
-                        leftIcon={<MantineIcon icon={IconCalendar} />}
+                        size="sm"
+                        px="xs"
+                        w="100%"
+                        styles={(theme) => ({
+                            root: {
+                                border: `1px solid ${theme.colors.gray[2]}`,
+                            },
+                            label: {
+                                width: '100%',
+                            },
+                        })}
                     >
-                        {buttonLabel}
+                        <Group position="apart" w="fill-available">
+                            <Group spacing="xs">
+                                <MantineIcon
+                                    color="dark.3"
+                                    icon={IconCalendar}
+                                />
+                                {buttonLabel}
+                            </Group>
+                            <MantineIcon
+                                color="dark.3"
+                                icon={IconChevronDown}
+                            />
+                        </Group>
                     </Button>
                 </Tooltip>
             </Popover.Target>
 
-            <Popover.Dropdown p="xs">
-                <Group spacing="sm" align="flex-start">
-                    <Stack spacing="xs">
+            <Popover.Dropdown p={0}>
+                <Group spacing={0} align="flex-start">
+                    <Stack spacing={2} py="xs" px="sm">
                         {presets.map((preset) => (
-                            <Button
+                            <UnstyledButton
                                 key={preset.label}
-                                variant={
-                                    tempSelectedPreset?.label === preset.label
-                                        ? 'filled'
-                                        : 'subtle'
-                                }
-                                color="dark"
-                                size="xs"
                                 onClick={() => handlePresetSelect(preset)}
-                                fullWidth
+                                sx={(theme) => ({
+                                    fontWeight: 500,
+                                    fontSize: theme.fontSizes.xs,
+                                    color: theme.colors.gray[7],
+                                    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+                                    borderRadius: theme.radius.sm,
+                                    backgroundColor:
+                                        tempSelectedPreset?.label ===
+                                        preset.label
+                                            ? theme.colors.gray[0]
+                                            : 'transparent',
+
+                                    '&:hover': {
+                                        backgroundColor: theme.colors.gray[0],
+                                    },
+                                })}
                             >
                                 {preset.label}
-                            </Button>
+                            </UnstyledButton>
                         ))}
                     </Stack>
 
-                    <Stack>
-                        <DatePicker
-                            type="range"
-                            value={tempDateRange}
-                            onChange={handleDateRangeChange}
-                            numberOfColumns={2}
-                            color="dark"
-                            size="xs"
-                            styles={(theme) => ({
-                                day: {
-                                    '&[data-in-range]': {
-                                        backgroundColor: theme.colors.gray[2],
+                    <Divider orientation="vertical" color="gray.2" />
+                    <Stack spacing={0}>
+                        <Box px="xs">
+                            <DatePicker
+                                mih={225}
+                                type="range"
+                                value={tempDateRange}
+                                onChange={handleDateRangeChange}
+                                numberOfColumns={2}
+                                color="dark"
+                                size="xs"
+                                withCellSpacing={false}
+                                styles={(theme) => ({
+                                    calendarHeaderControlIcon: {
+                                        color: theme.colors.gray[5],
                                     },
-                                    '&[data-in-range]:hover': {
-                                        backgroundColor: theme.colors.gray[4],
+                                    calendarHeaderLevel: {
+                                        color: theme.colors.gray[7],
                                     },
-                                    '&[data-selected]': {
-                                        backgroundColor: theme.colors.dark[7],
+                                    monthLevel: {
+                                        padding: theme.spacing.xs,
+                                        '&[data-month-level]:not(:last-of-type)':
+                                            {
+                                                borderRight: `1px solid ${theme.colors.gray[2]}`,
+                                                marginRight: 0,
+                                            },
                                     },
-                                    '&[data-selected]:hover': {
-                                        backgroundColor: theme.colors.dark[9],
+                                    day: {
+                                        borderRadius: theme.radius.lg,
+                                        // Revert color for weekends that are not selected
+                                        '&[data-weekend="true"]&:not([data-selected])':
+                                            {
+                                                color: theme.colors.gray[7],
+                                            },
+                                        '&[data-in-range]': {
+                                            backgroundColor:
+                                                theme.colors.gray[0],
+                                        },
+                                        '&[data-in-range]:hover': {
+                                            backgroundColor:
+                                                theme.colors.gray[0],
+                                        },
+                                        '&[data-selected]': {
+                                            backgroundColor:
+                                                theme.colors.dark[7],
+                                            borderRadius: theme.radius.lg,
+                                        },
+                                        '&[data-selected]:hover': {
+                                            backgroundColor:
+                                                theme.colors.dark[9],
+                                            borderRadius: theme.radius.lg,
+                                        },
                                     },
-                                },
-                            })}
-                        />
-                        <Group position="apart" spacing="xl">
-                            <Group spacing="xs">
-                                <TextInput
-                                    size="xs"
-                                    radius="md"
-                                    w={100}
-                                    value={formattedTempDateRange[0]}
-                                    readOnly
-                                    disabled={!formattedTempDateRange[0]}
-                                />
-                                -
-                                <TextInput
-                                    size="xs"
-                                    radius="md"
-                                    w={100}
-                                    value={formattedTempDateRange[1]}
-                                    readOnly
-                                    disabled={!formattedTempDateRange[1]}
-                                />
-                            </Group>
+                                })}
+                            />
+                        </Box>
+                        <Divider color="gray.2" />
+                        <Box p="sm">
+                            <Group position="apart" spacing="xl">
+                                <Group spacing="xs">
+                                    <TextInput
+                                        size="xs"
+                                        radius="md"
+                                        w={100}
+                                        value={formattedTempDateRange[0]}
+                                        readOnly
+                                        disabled={!formattedTempDateRange[0]}
+                                        styles={(theme) => ({
+                                            input: {
+                                                boxShadow: theme.shadows.subtle,
+                                                border: `1px solid ${theme.colors.gray[2]}`,
+                                            },
+                                        })}
+                                    />
+                                    <Text size="xs" c="gray.5">
+                                        -
+                                    </Text>
+                                    <TextInput
+                                        size="xs"
+                                        radius="md"
+                                        w={100}
+                                        value={formattedTempDateRange[1]}
+                                        readOnly
+                                        disabled={!formattedTempDateRange[1]}
+                                        styles={(theme) => ({
+                                            input: {
+                                                boxShadow: theme.shadows.subtle,
+                                                border: `1px solid ${theme.colors.gray[2]}`,
+                                            },
+                                        })}
+                                    />
+                                </Group>
 
-                            <Group spacing="xs">
-                                <Button
-                                    size="xs"
-                                    radius="md"
-                                    variant="default"
-                                    onClick={() => handleOpen(false)}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button
-                                    size="xs"
-                                    radius="md"
-                                    color="dark"
-                                    onClick={handleApply}
-                                >
-                                    Apply
-                                </Button>
+                                <Group spacing="xs">
+                                    <Button
+                                        size="xs"
+                                        radius="md"
+                                        variant="default"
+                                        onClick={() => handleOpen(false)}
+                                        sx={(theme) => ({
+                                            boxShadow: theme.shadows.subtle,
+                                            border: `1px solid ${theme.colors.gray[2]}`,
+                                        })}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        size="xs"
+                                        radius="md"
+                                        color="dark"
+                                        onClick={handleApply}
+                                        sx={(theme) => ({
+                                            boxShadow: theme.shadows.subtle,
+                                        })}
+                                    >
+                                        Apply
+                                    </Button>
+                                </Group>
                             </Group>
-                        </Group>
+                        </Box>
                     </Stack>
                 </Group>
             </Popover.Dropdown>
