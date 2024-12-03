55aab703c84ea75d327491ec7b74621b3cde10da
almeidabbm <almeidabatistam@gmail.com>
2024-12-03 10:30:05 +0000

    chore: improve migration to avoid subqueries

--- packages/backend/src/database/migrations/20241129140659_add-table-name-to-catalog-search.ts
+++ packages/backend/src/database/migrations/20241129140659_add-table-name-to-catalog-search.ts
@@ -10,16 +10,12 @@ export async function up(knex: Knex): Promise<void> {
     });
 
     // backfill table_name with data from cached_explore
-    await knex(CATALOG_SEARCH_TABLE).update({
-        // @ts-ignore - we're doing something similar in 20240604094543_add_views_columns_to_chart_and_dashboards_tables.ts
-        table_name: knex
-            .select(knex.raw(`explore->>'baseTable'`))
-            .from(CACHED_EXPLORE_TABLE)
-            .whereRaw(
-                `${CACHED_EXPLORE_TABLE}.cached_explore_uuid = ${CATALOG_SEARCH_TABLE}.cached_explore_uuid`,
-            )
-            .limit(1),
-    });
+    await knex.raw(`
+        UPDATE ${CATALOG_SEARCH_TABLE}
+        SET table_name = ${CACHED_EXPLORE_TABLE}.explore->>'baseTable'
+        FROM ${CACHED_EXPLORE_TABLE}
+        WHERE ${CACHED_EXPLORE_TABLE}.cached_explore_uuid = ${CATALOG_SEARCH_TABLE}.cached_explore_uuid
+    `);
 
     // make table_name not nullable and add unique constraint
     await knex.schema.alterTable(CATALOG_SEARCH_TABLE, (table) => {
