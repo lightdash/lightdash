4ad3799c3f9483eb0b9a4856409bbeacbe52fa20
Jose Rego <ZeRego@users.noreply.github.com>
2024-12-06 14:56:18 +0000

    fix: paginate charts in dashboard modals (#12764)

--- packages/frontend/src/hooks/useChartSummariesV2.ts
+++ packages/frontend/src/hooks/useChartSummariesV2.ts
@@ -3,28 +3,56 @@ import {
     type ApiChartContentResponse,
     type ApiError,
 } from '@lightdash/common';
-import { useQuery } from '@tanstack/react-query';
+import {
+    useInfiniteQuery,
+    type UseInfiniteQueryOptions,
+} from '@tanstack/react-query';
 import { lightdashApi } from '../api';
 
-const getChartSummariesInProjectV2 = async (projectUuid: string) => {
+type UseChartSummariesV2Args = {
+    projectUuid: string;
+    pageSize: number;
+    page: number;
+    search?: string;
+};
+
+const getChartSummariesInProjectV2 = async ({
+    projectUuid,
+    page,
+    pageSize,
+    search,
+}: UseChartSummariesV2Args) => {
     return lightdashApi<ApiChartContentResponse['results']>({
         version: 'v2',
-        url: `/content?projectUuids=${projectUuid}&contentTypes=${ContentType.CHART}&pageSize=${Number.MAX_SAFE_INTEGER}`, // TODO: remove pageSize max once we have pagination
+        url: `/content?projectUuids=${projectUuid}&contentTypes=${ContentType.CHART}&pageSize=${pageSize}&page=${page}&search=${search}`,
         method: 'GET',
         body: undefined,
     });
 };
 
-export const useChartSummariesV2 = (projectUuid: string) => {
-    return useQuery<
+export const useChartSummariesV2 = (
+    args: UseChartSummariesV2Args,
+    infinityQueryOpts: UseInfiniteQueryOptions<
         ApiChartContentResponse['results'],
-        ApiError,
-        ApiChartContentResponse['results']['data']
-    >({
-        queryKey: ['project', projectUuid, 'chart-summaries-v2'],
-        queryFn: () => getChartSummariesInProjectV2(projectUuid),
-        select(data) {
-            return data.data;
+        ApiError
+    > = {},
+) => {
+    return useInfiniteQuery<ApiChartContentResponse['results'], ApiError>({
+        queryKey: ['project', 'chart-summaries-v2', args],
+        queryFn: async ({ pageParam }) => {
+            return getChartSummariesInProjectV2({
+                ...args,
+                page: pageParam ?? 1,
+            });
         },
+        getNextPageParam: (lastPage) => {
+            if (lastPage.pagination) {
+                return lastPage.pagination.page <
+                    lastPage.pagination.totalPageCount
+                    ? lastPage.pagination.page + 1
+                    : undefined;
+            }
+        },
+        ...infinityQueryOpts,
     });
 };
