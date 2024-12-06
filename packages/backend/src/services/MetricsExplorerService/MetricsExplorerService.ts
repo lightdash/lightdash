3e8818523853df9757d4e9f67c956d21871070e1
Joao Viana <joao@lightdash.com>
2024-12-06 12:48:21 +0000

    fix: address time interval overrride on comparison of metrics

--- packages/backend/src/services/MetricsExplorerService/MetricsExplorerService.ts
+++ packages/backend/src/services/MetricsExplorerService/MetricsExplorerService.ts
@@ -192,6 +192,7 @@ export class MetricsExplorerService<
                         projectUuid,
                         compare.metricTable,
                         compare.metricName,
+                        timeDimensionOverride?.interval,
                     );
 
                     const compareTimeDimension = compareMetric.timeDimension;
@@ -202,8 +203,8 @@ export class MetricsExplorerService<
                         );
                     }
 
-                    const compareMetricDimensionGrain = dateRange
-                        ? getGrainForDateRange(dateRange)
+                    const compareMetricDimensionGrain = timeDimensionOverride
+                        ? timeDimensionOverride.interval
                         : compareTimeDimension.interval;
 
                     const differentDimensionId = getItemId({
@@ -261,6 +262,8 @@ export class MetricsExplorerService<
                     break;
             }
         }
+
+        console.log({ compareMetric });
 
         return {
             rows: currentResults,
