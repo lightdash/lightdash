b9ff2b85009667a803598d39ed98e1726f1b8771
almeidabbm <almeidabatistam@gmail.com>
2024-12-09 10:44:52 +0000

    chore: remove console.log

--- packages/frontend/src/features/metricsCatalog/hooks/useRunMetricExplorerQuery.ts
+++ packages/frontend/src/features/metricsCatalog/hooks/useRunMetricExplorerQuery.ts
@@ -72,14 +72,6 @@ export const useRunMetricExplorerQuery = ({
     timeDimensionOverride,
     options,
 }: Partial<RunMetricExplorerQueryArgs>) => {
-    console.log('useRunMetricExplorerQuery', {
-        projectUuid,
-        exploreName,
-        metricName,
-        comparison,
-        dateRange,
-        timeDimensionOverride,
-    });
     return useQuery({
         queryKey: [
             'runMetricExplorerQuery',
