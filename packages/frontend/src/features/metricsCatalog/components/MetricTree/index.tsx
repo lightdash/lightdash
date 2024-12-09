ed24eda5e08d47be17f6679e3bb050891a8ca418
almeidabbm <almeidabatistam@gmail.com>
2024-12-09 14:22:49 +0000

    chore: use const

--- packages/frontend/src/features/metricsCatalog/components/MetricTree/index.tsx
+++ packages/frontend/src/features/metricsCatalog/components/MetricTree/index.tsx
@@ -1,6 +1,6 @@
 import Dagre from '@dagrejs/dagre';
 import {
-    TimeFrames,
+    DEFAULT_METRICS_EXPLORER_TIME_INTERVAL,
     type CatalogField,
     type CatalogMetricsTreeEdge,
 } from '@lightdash/common';
@@ -54,7 +54,7 @@ enum STATIC_NODE_TYPES {
     UNCONNECTED = 'UNCONNECTED',
 }
 
-const DEFAULT_TIME_FRAME = TimeFrames.DAY;
+const DEFAULT_TIME_FRAME = DEFAULT_METRICS_EXPLORER_TIME_INTERVAL; // TODO: this should be dynamic
 
 type MetricTreeNode =
     | MetricTreeConnectedNodeData
