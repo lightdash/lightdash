67867dc2dd5075e5fb2aad3f8b8466f636e4e219
Javier Rengel <rephus@gmail.com>
2024-12-17 13:42:58 +0100

    M  packages/e2e/cypress/e2e/api/contentAsCode.cy.ts M  packages/e2e/cypress/support/mocks.ts

--- packages/e2e/cypress/e2e/api/contentAsCode.cy.ts
+++ packages/e2e/cypress/e2e/api/contentAsCode.cy.ts
@@ -1,5 +1,5 @@
-import { SEED_PROJECT } from '@lightdash/common';
-import { contentAsCodeChartMock } from '../../support/mocks';
+import { CustomBinDimension, SEED_PROJECT } from '@lightdash/common';
+import { chartAsCode } from '../../support/mocks';
 
 describe('Content as Code API', () => {
     beforeEach(() => {
@@ -14,35 +14,90 @@ describe('Content as Code API', () => {
             cy.wrap(response).its('status').should('eq', 200);
             cy.wrap(response.body.results).should('exist');
             cy.wrap(response.body.results.charts).should('be.an', 'array');
+            cy.wrap(response.body.results.charts)
+                .its('length')
+                .should('be.gt', 0);
+            const chart = response.body.results.charts.find(
+                (c: { slug: string }) => c.slug === chartAsCode.slug,
+            );
+            cy.wrap(chart).should('exist');
             cy.wrap(response.body.results.missingIds).should('be.an', 'array');
+            cy.wrap(response.body.results.missingIds)
+                .its('length')
+                .should('eq', 0);
         });
     });
 
-    it('should upload chart as code', () => {
-        const chartCode = {
-            ...contentAsCodeChartMock,
-            name: 'Test Chart Code',
-            description: 'Test chart description for content as code',
-            updatedAt: new Date().toISOString(),
-            slug: 'test-chart-code',
-        };
-
+    it('should download charts as code by slug', () => {
         cy.request({
-            method: 'POST',
-            url: `/api/v1/projects/${SEED_PROJECT.project_uuid}/charts/${chartCode.slug}/code`,
-            body: chartCode,
+            method: 'GET',
+            url: `/api/v1/projects/${SEED_PROJECT.project_uuid}/charts/code?ids=${chartAsCode.slug}`,
         }).then((response) => {
             cy.wrap(response).its('status').should('eq', 200);
             cy.wrap(response.body.results).should('exist');
             cy.wrap(response.body.results.charts).should('be.an', 'array');
-            cy.wrap(response.body.results.charts[0]).should(
-                'have.property',
-                'action',
+            cy.wrap(response.body.results.charts.length).should('eq', 1);
+            const chart = response.body.results.charts.find(
+                (c: { slug: string }) => c.slug === chartAsCode.slug,
             );
-            cy.wrap(response.body.results.charts[0].action).should('be.oneOf', [
-                'create',
+            cy.wrap(chart).should('exist');
+            cy.wrap(response.body.results.missingIds).should('be.an', 'array');
+            cy.wrap(response.body.results.missingIds)
+                .its('length')
+                .should('eq', 0);
+        });
+    });
+
+    it('should download charts as code with offset', () => {
+        cy.request({
+            method: 'GET',
+            url: `/api/v1/projects/${SEED_PROJECT.project_uuid}/charts/code?offset=5`,
+        }).then((response) => {
+            cy.wrap(response).its('status').should('eq', 200);
+            const { results } = response.body;
+            cy.wrap(results).should('exist');
+            cy.wrap(results.charts).should('be.an', 'array');
+            cy.wrap(results.length).should('be.gt', 0);
+            cy.wrap(results.total - results.charts.length).should('eq', 5);
+        });
+    });
+
+    it('should upload chart as code', () => {
+        const newDescription = `Updated description ${new Date().toISOString()}`;
+        const newBinNumber = Math.floor(Math.random() * 10) + 1;
+        const customDimension: CustomBinDimension = chartAsCode.metricQuery
+            .customDimensions![0] as CustomBinDimension;
+        const updatedChartAsCode = {
+            ...chartAsCode,
+            description: newDescription,
+            metricQuery: {
+                ...chartAsCode.metricQuery,
+                customDimensions: [
+                    {
+                        ...customDimension,
+                        binNumber: newBinNumber,
+                    },
+                ],
+            },
+        };
+
+        cy.request({
+            method: 'POST',
+            url: `/api/v1/projects/${SEED_PROJECT.project_uuid}/charts/${chartAsCode.slug}/code`,
+            body: updatedChartAsCode,
+        }).then((response) => {
+            cy.wrap(response).its('status').should('eq', 200);
+            cy.wrap(response.body.results).should('exist');
+            cy.wrap(response.body.results.charts).should('be.an', 'array');
+            cy.wrap(response.body.results.charts[0].action).should(
+                'eq',
                 'update',
-            ]);
+            );
+            const updatedChart = response.body.results.charts[0].data;
+            cy.wrap(updatedChart.description).should('eq', newDescription);
+            cy.wrap(
+                updatedChart.metricQuery.customDimensions[0].binNumber,
+            ).should('eq', newBinNumber);
         });
     });
 });
--- packages/e2e/cypress/support/mocks.ts
+++ packages/e2e/cypress/support/mocks.ts
@@ -1,8 +1,11 @@
 import {
+    BinType,
+    CartesianSeriesType,
+    ChartAsCode,
     ChartType,
     CreateDashboard,
     CreateSavedChart,
-    currentVersion,
+    CustomDimensionType,
 } from '@lightdash/common';
 
 export const chartMock: CreateSavedChart = {
@@ -25,12 +28,76 @@ export const chartMock: CreateSavedChart = {
     },
 };
 
-export const contentAsCodeChartMock = {
-    ...chartMock,
-    spaceUuid: 'test-space-uuid',
+export const chartAsCode: ChartAsCode = {
+    name: 'How do payment methods vary across different amount ranges?',
+    description: 'Payment range by amount',
+    tableName: 'payments',
+    updatedAt: new Date('2024-12-17T11:04:43.216Z'),
+    metricQuery: {
+        exploreName: 'payments',
+        dimensions: ['amount_range', 'payments_payment_method'],
+        metrics: ['orders_total_order_amount'],
+        filters: {},
+        sorts: [
+            {
+                fieldId: 'orders_total_order_amount',
+                descending: true,
+            },
+        ],
+        limit: 500,
+        tableCalculations: [],
+        additionalMetrics: [],
+        customDimensions: [
+            {
+                id: 'amount_range',
+                name: 'amount range',
+                type: CustomDimensionType.BIN,
+                dimensionId: 'payments_amount',
+                table: 'payments',
+                binType: BinType.FIXED_NUMBER,
+                binNumber: 5,
+            },
+        ],
+    },
+    chartConfig: {
+        type: ChartType.CARTESIAN,
+        config: {
+            layout: {
+                xField: 'amount_range',
+                yField: ['orders_total_order_amount'],
+                flipAxes: false,
+            },
+            eChartsConfig: {
+                series: [
+                    {
+                        type: CartesianSeriesType.BAR,
+                        encode: {
+                            xRef: {
+                                field: 'amount_range',
+                            },
+                            yRef: {
+                                field: 'orders_total_order_amount',
+                            },
+                        },
+                        yAxisIndex: 0,
+                    },
+                ],
+            },
+        },
+    },
     dashboardUuid: null,
-    version: currentVersion,
-    spaceSlug: 'test-space',
+    slug: 'how-do-payment-methods-vary-across-different-amount-ranges',
+    tableConfig: {
+        columnOrder: [
+            'amount_range',
+            'amount range',
+            'orders_total_order_amount',
+            'payments_payment_method',
+        ],
+    },
+    spaceSlug: 'jaffle-shop',
+    version: 1,
+    downloadedAt: new Date('2024-12-17T11:06:55.912Z'),
 };
 
 export const dashboardMock: CreateDashboard = {
