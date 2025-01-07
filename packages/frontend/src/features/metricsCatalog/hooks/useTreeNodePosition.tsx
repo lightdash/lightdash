d478b1cdfe149fd09c5db7da6f10950611007bcb
almeidabbm <almeidabatistam@gmail.com>
2025-01-07 16:47:21 +0000

    chore: fix drag boundaries

--- packages/frontend/src/features/metricsCatalog/hooks/useTreeNodePosition.tsx
+++ packages/frontend/src/features/metricsCatalog/hooks/useTreeNodePosition.tsx
@@ -10,26 +10,19 @@ export const useTreeNodePosition = (nodeId: string) => {
             const intersectingNode = getNode(intersectingNodeId);
             if (!intersectingNode || !node) return false;
 
-            const {
-                position: intersectingNodePosition,
-                measured: intersectingNodeMeasured,
-            } = intersectingNode;
-
-            const {
-                width: intersectingNodeWidth,
-                height: intersectingNodeHeight,
-            } = intersectingNodeMeasured ?? {};
-
+            const { position: intersectingNodePosition } = intersectingNode;
             const { position: nodePosition, measured: nodeMeasured } = node;
             const { width: nodeWidth, height: nodeHeight } = nodeMeasured ?? {};
 
-            return (
-                intersectingNodePosition.x >= nodePosition.x &&
-                intersectingNodePosition.x + (intersectingNodeWidth ?? 0) <=
-                    nodePosition.x + (nodeWidth ?? 0) &&
-                intersectingNodePosition.y >= nodePosition.y &&
-                intersectingNodePosition.y + (intersectingNodeHeight ?? 0) <=
-                    nodePosition.y + (nodeHeight ?? 0)
+            return !(
+                (
+                    intersectingNodePosition.x + 100 <= nodePosition.x || // intersecting node is to the left, here we don't use the node width because node expands to the rigth, so we use a fixed value
+                    intersectingNodePosition.x >=
+                        nodePosition.x + (nodeWidth ?? 0) || // intersecting node is to the right
+                    intersectingNodePosition.y + 20 <= nodePosition.y || // intersecting node is above, here we don't use height because node expands to the bottom, so we use a fixed value
+                    intersectingNodePosition.y >=
+                        nodePosition.y + (nodeHeight ?? 0)
+                ) // intersecting node is below
             );
         },
         [getNode, node],
