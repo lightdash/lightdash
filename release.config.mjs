a5cc8e21c14f6460baed8bb69d57b721b2d8eaeb
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-01-10 02:22:06 +0400

    testing test release

--- release.config.mjs
+++ release.config.mjs
@@ -41,15 +41,11 @@ const expectedResults = [
     },
 ];
 
-module.exports = {
-    branches: [
-        '+([0-9])?(.{+([0-9]),x}).x',
-        'main',
-        'next',
-        'next-major',
-        { name: 'beta', prerelease: true },
-        { name: 'alpha', prerelease: true },
-    ],
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ['testrelease'],
     plugins: [
         '@semantic-release/commit-analyzer',
 
@@ -76,71 +72,71 @@ module.exports = {
             },
         ],
 
-        [
-            '@amanda-mitchell/semantic-release-npm-multiple',
-            {
-                registries: {
-                    main: {
-                        npmPublish: false,
-                        pkgRoot: '.',
-                    },
-                    common: {
-                        npmPublish: true,
-                        pkgRoot: 'packages/common',
-                    },
-                    backend: {
-                        npmPublish: false,
-                        pkgRoot: 'packages/backend',
-                    },
-                    frontend: {
-                        npmPublish: false,
-                        pkgRoot: 'packages/frontend',
-                    },
-                    e2e: {
-                        npmPublish: false,
-                        pkgRoot: 'packages/e2e',
-                    },
-                    warehouses: {
-                        npmPublish: true,
-                        pkgRoot: 'packages/warehouses',
-                    },
-                    cli: {
-                        npmPublish: true,
-                        pkgRoot: 'packages/cli',
-                    },
-                },
-            },
-        ],
+        // [
+        //     '@amanda-mitchell/semantic-release-npm-multiple',
+        //     {
+        //         registries: {
+        //             main: {
+        //                 npmPublish: false,
+        //                 pkgRoot: '.',
+        //             },
+        //             common: {
+        //                 npmPublish: true,
+        //                 pkgRoot: 'packages/common',
+        //             },
+        //             backend: {
+        //                 npmPublish: false,
+        //                 pkgRoot: 'packages/backend',
+        //             },
+        //             frontend: {
+        //                 npmPublish: false,
+        //                 pkgRoot: 'packages/frontend',
+        //             },
+        //             e2e: {
+        //                 npmPublish: false,
+        //                 pkgRoot: 'packages/e2e',
+        //             },
+        //             warehouses: {
+        //                 npmPublish: true,
+        //                 pkgRoot: 'packages/warehouses',
+        //             },
+        //             cli: {
+        //                 npmPublish: true,
+        //                 pkgRoot: 'packages/cli',
+        //             },
+        //         },
+        //     },
+        // ],
 
-        [
-            '@google/semantic-release-replace-plugin',
-            {
-                replacements: [
-                    {
-                        files: packagesWithWorkspaceDependencies,
-                        from: '\\^${nextRelease.version}',
-                        to: 'workspace:*',
-                        countMatches: true,
-                        results: expectedResults,
-                    },
-                ],
-            },
-        ],
+        // [
+        //     '@google/semantic-release-replace-plugin',
+        //     {
+        //         replacements: [
+        //             {
+        //                 files: packagesWithWorkspaceDependencies,
+        //                 from: '\\^${nextRelease.version}',
+        //                 to: 'workspace:*',
+        //                 countMatches: true,
+        //                 results: expectedResults,
+        //             },
+        //         ],
+        //     },
+        // ],
 
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'CHANGELOG.md',
-                    'package.json',
-                    'packages/common/package.json',
-                    ...packagesWithWorkspaceDependencies,
-                ],
-                message:
-                    'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
-            },
-        ],
-        ['@semantic-release/github', {}],
+        // [
+        //     '@semantic-release/git',
+        //     {
+        //         assets: [
+        //             'CHANGELOG.md',
+        //             'package.json',
+        //             'packages/common/package.json',
+        //             ...packagesWithWorkspaceDependencies,
+        //         ],
+        //         message:
+        //             'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+        //     },
+        // ],
+        // ['@semantic-release/github', {}],
     ],
     tagFormat: '${version}',
 };
