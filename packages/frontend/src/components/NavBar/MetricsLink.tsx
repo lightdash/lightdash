b2d5e9b3344d3f721f9c5514969ea336757996c7
Joao Viana <joao@lightdash.com>
2025-01-15 10:29:51 +0000

    feat: render metrics link depending on metrics count

--- packages/frontend/src/components/NavBar/MetricsLink.tsx
+++ packages/frontend/src/components/NavBar/MetricsLink.tsx
@@ -1,16 +1,19 @@
-import { Button } from '@mantine/core';
+import { Button, Menu } from '@mantine/core';
+import { IconHash } from '@tabler/icons-react';
 import { useCallback, type FC } from 'react';
-import { useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { useProject } from '../../hooks/useProject';
 import useTracking from '../../providers/Tracking/useTracking';
 import { Hash } from '../../svgs/metricsCatalog';
 import { EventName } from '../../types/Events';
+import MantineIcon from '../common/MantineIcon';
 
 interface Props {
     projectUuid: string;
+    asMenu?: boolean;
 }
 
-export const MetricsLink: FC<Props> = ({ projectUuid }) => {
+export const MetricsLink: FC<Props> = ({ projectUuid, asMenu }) => {
     const navigate = useNavigate();
     const { data: project } = useProject(projectUuid);
     const { track } = useTracking();
@@ -27,6 +30,18 @@ export const MetricsLink: FC<Props> = ({ projectUuid }) => {
         }
         void navigate(`/projects/${projectUuid}/metrics`);
     }, [project, projectUuid, track, navigate]);
+
+    if (asMenu) {
+        return (
+            <Menu.Item
+                component={Link}
+                to={`/projects/${projectUuid}/metrics`}
+                icon={<MantineIcon icon={IconHash} />}
+            >
+                Metrics
+            </Menu.Item>
+        );
+    }
 
     return (
         <Button
