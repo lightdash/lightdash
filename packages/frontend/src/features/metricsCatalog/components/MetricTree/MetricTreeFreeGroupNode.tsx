81872456d53de950189b2a9a56518f28392f0979
almeidabbm <almeidabatistam@gmail.com>
2025-01-16 10:20:06 +0000

    chore: remove paper sx styles as they are default for paper in MetricsCatalog.tsx

--- packages/frontend/src/features/metricsCatalog/components/MetricTree/MetricTreeFreeGroupNode.tsx
+++ packages/frontend/src/features/metricsCatalog/components/MetricTree/MetricTreeFreeGroupNode.tsx
@@ -1,4 +1,4 @@
-import { Paper, Text, useMantineTheme } from '@mantine/core';
+import { Paper, Text } from '@mantine/core';
 import type { Node, NodeProps } from '@xyflow/react';
 import { useRef, type FC } from 'react';
 
@@ -9,22 +9,9 @@ const MetricTreeFreeGroupNode: FC<NodeProps<MetricTreeFreeGroupNodeData>> = ({
     width,
 }) => {
     const ref = useRef<HTMLDivElement>(null);
-    const theme = useMantineTheme();
 
     return (
-        <Paper
-            fz="xs"
-            p="xs"
-            h={height}
-            w={width}
-            bg="gray.0"
-            padding="sm"
-            sx={{
-                border: `1px solid ${theme.colors.gray[2]}`,
-                boxShadow: theme.shadows.subtle,
-                borderRadius: theme.radius.md,
-            }}
-        >
+        <Paper fz="xs" p="xs" h={height} w={width} bg="gray.0" padding="sm">
             <Text fz={12} fw={500} c="gray.6" ref={ref}>
                 Drag tiles from this area into the workspace to curate your
                 canvas. Use the nodes to connect metrics and create metric
