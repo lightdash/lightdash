a649b3008cbfac4278633c10678fa3fe3a1c281d
Javier Rengel <rephus@gmail.com>
2025-01-20 15:35:19 +0100

    M  packages/common/src/utils/slugs.ts

--- packages/common/src/utils/slugs.ts
+++ packages/common/src/utils/slugs.ts
@@ -11,6 +11,8 @@ const sanitizeSlug = (slug: string) =>
 export const generateSlug = (name: string) => {
     const sanitizedSlug = sanitizeSlug(name);
     if (sanitizedSlug.length === 0) {
+        // Return a random 5 character string
+        // Base-36 uses all digits (0-9) and all letters (a-z)
         return Math.random().toString(36).substring(2, 7);
     }
     return sanitizedSlug;
