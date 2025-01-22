51b9ac461878631565942ae97c93b352bd917a80
magnew <mcagnew@gmail.com>
2025-01-22 19:49:56 +0100

    feat: sql runner style grouped series (#13366)
    * feat: add pivot metadata to sql runner pivot query
    
    * cleanup, generate api
    
    * update names, use map
    
    * return metadata for agg columns
    
    * feat: sql runner pivoted series can be customized
    
    * remove comment
    
    * show series groups
    
    * routes
    
    * api
    
    * revert routes
    
    * return metadata inline
    
    * include agg in column id
    
    * comment updates
    
    * stub semantic viewer type
    
    * type cleanup
    
    * test fix
    
    * pr feedback
    
    * type update
    
    * comment update
    
    * remove series index, use reference
    
    * renames
    
    * add a fallback for the old data display object
    
    * fix grouping
    
    * pr feedback
    
    * more pr feedback
    
    * update api
    
    * test fix
    
    * fix test
    
    * test fix

---
+++ packages/frontend/src/components/DataViz/config/SingleSeriesConfiguration.tsx
@@ -0,0 +1,145 @@
+import {
+    AxisSide,
+    getEChartsChartTypeFromChartKind,
+    ValueLabelPositionOptions,
+    type CartesianChartDisplay,
+    type ChartKind,
+} from '@lightdash/common';
+import { Group, SegmentedControl, Stack, Text, TextInput } from '@mantine/core';
+import { IconAlignLeft, IconAlignRight } from '@tabler/icons-react';
+import MantineIcon from '../../common/MantineIcon';
+import ColorSelector from '../../VisualizationConfigs/ColorSelector';
+import { Config } from '../../VisualizationConfigs/common/Config';
+import { CartesianChartTypeConfig } from './CartesianChartTypeConfig';
+import { CartesianChartValueLabelConfig } from './CartesianChartValueLabelConfig';
+
+type SingleSeriesConfigurationProps = {
+    reference: string;
+    color: string | undefined;
+    colors: string[];
+    label?: string;
+    type: NonNullable<CartesianChartDisplay['series']>[number]['type'];
+    whichYAxis?: number;
+    valueLabelPosition: ValueLabelPositionOptions | undefined;
+    selectedChartType: ChartKind;
+    onColorChange: (reference: string, color: string) => void;
+    onLabelChange: (reference: string, label: string) => void;
+    onTypeChange: (
+        reference: string,
+        type: NonNullable<CartesianChartDisplay['series']>[number]['type'],
+    ) => void;
+    onAxisChange: (reference: string, value: AxisSide) => void;
+    onValueLabelPositionChange: (
+        reference: string,
+        position: ValueLabelPositionOptions,
+    ) => void;
+};
+
+export const SingleSeriesConfiguration = ({
+    reference,
+    color,
+    colors,
+    label,
+    type,
+    whichYAxis = 0,
+    valueLabelPosition,
+    selectedChartType,
+    onColorChange,
+    onLabelChange,
+    onTypeChange,
+    onAxisChange,
+    onValueLabelPositionChange,
+}: SingleSeriesConfigurationProps) => {
+    return (
+        <Stack key={reference} spacing="xs">
+            <Stack
+                pl="sm"
+                spacing="xs"
+                sx={(theme) => ({
+                    borderLeft: `1px solid ${theme.colors.gray[2]}`,
+                    backgroundColor: theme.colors.gray[1],
+                    borderRadius: theme.radius.md,
+                    padding: theme.spacing.xs,
+                })}
+            >
+                <Config.Subheading>{reference}</Config.Subheading>
+                <Config.Group>
+                    <Config.Label>Label</Config.Label>
+                    <Group spacing="xs" noWrap>
+                        <ColorSelector
+                            color={color}
+                            onColorChange={(c) => onColorChange(reference, c)}
+                            swatches={colors}
+                        />
+                        <TextInput
+                            radius="md"
+                            value={label}
+                            onChange={(e) =>
+                                onLabelChange(reference, e.target.value)
+                            }
+                        />
+                    </Group>
+                </Config.Group>
+                <Config.Group>
+                    <Config.Label>Chart Type</Config.Label>
+                    <CartesianChartTypeConfig
+                        canSelectDifferentTypeFromBaseChart={true}
+                        type={
+                            type ??
+                            getEChartsChartTypeFromChartKind(selectedChartType)
+                        }
+                        onChangeType={(value) => onTypeChange(reference, value)}
+                    />
+                </Config.Group>
+                <Config.Group>
+                    <Config.Label>Y Axis</Config.Label>
+                    <SegmentedControl
+                        sx={{ alignSelf: 'center' }}
+                        radius="md"
+                        data={[
+                            {
+                                value: 'left',
+                                label: (
+                                    <Group spacing="xs" noWrap>
+                                        <MantineIcon icon={IconAlignLeft} />
+                                        <Text>Left</Text>
+                                    </Group>
+                                ),
+                            },
+                            {
+                                value: 'right',
+                                label: (
+                                    <Group spacing="xs" noWrap>
+                                        <Text>Right</Text>
+                                        <MantineIcon icon={IconAlignRight} />
+                                    </Group>
+                                ),
+                            },
+                        ]}
+                        value={whichYAxis === AxisSide.RIGHT ? 'right' : 'left'}
+                        onChange={(value) =>
+                            onAxisChange(
+                                reference,
+                                value === 'left'
+                                    ? AxisSide.LEFT
+                                    : AxisSide.RIGHT,
+                            )
+                        }
+                    />
+                </Config.Group>
+                <Config.Group>
+                    <Config.Label>Value labels</Config.Label>
+                    <CartesianChartValueLabelConfig
+                        valueLabelPosition={
+                            valueLabelPosition ??
+                            ValueLabelPositionOptions.HIDDEN
+                        }
+                        onChangeValueLabelPosition={(position) =>
+                            onValueLabelPositionChange(reference, position)
+                        }
+                    />
+                </Config.Group>
+            </Stack>
+        </Stack>
+    );
+};
