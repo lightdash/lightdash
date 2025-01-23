3e0c4f5637a56f882667e7f226148c15502b7e08
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-01-23 09:52:11 -0500

    fix: test eslint in ci ??

--- packages/backend/src/backendApp.ts
+++ packages/backend/src/backendApp.ts
@@ -2,6 +2,22 @@ import App from './App';
 import { lightdashConfig } from './config/lightdashConfig';
 import knexConfig from './knexfile';
 
+const test = () => {
+    console.log('alalala');
+    return Promise.resolve(1);
+};
+
+const test2 = async () => {
+    console.log('alalala2');
+
+    try {
+        return await test();
+    } catch (error) {
+        console.error(error);
+        return 2;
+    }
+};
+
 const app = new App({
     lightdashConfig,
     port: process.env.PORT || 8080,
@@ -9,5 +25,7 @@ const app = new App({
         process.env.NODE_ENV === 'development' ? 'development' : 'production',
     knexConfig,
 });
+
+test2().then(console.log).catch(console.error);
 
 export default app;
