928b376e5b6909a811746f5f51ef8c80443bbf78
Marshall Agnew <marshall@lightdash.com>
2025-01-31 13:56:53 +0100

    ignore unused exports, rename cjs modules

--- packages/frontend/src/sdk/sdk.ts
+++ packages/frontend/src/sdk/sdk.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import MantineIcon from '../components/common/MantineIcon';
 import MantineProvider from '../providers/MantineProvider';
-import { TestFrontendForSdk } from './SdkTests';
 import '../styles/react-grid.css';
+import { TestFrontendForSdk } from './SdkTests';
 
 export type TestArgs = {
     name: string;
