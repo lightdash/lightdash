2340d72875929efeabe57ab4819baf4c36888b5f
almeidabbm <almeidabatistam@gmail.com>
2025-02-06 16:13:37 +0000

    chore: fix mocks

--- packages/common/src/compiler/exploreCompiler.mock.ts
+++ packages/common/src/compiler/exploreCompiler.mock.ts
@@ -104,6 +104,7 @@ export const exploreBase: Explore = {
     tables: {},
     groupLabel: undefined,
     warehouse: undefined,
+    databricksComputeHttpPath: undefined,
     sqlPath: undefined,
     ymlPath: undefined,
 };
@@ -1491,6 +1492,8 @@ export const exploreWithMetricNumberCompiled: Explore = {
     ymlPath: exploreWithMetricNumber.ymlPath,
     sqlPath: exploreWithMetricNumber.sqlPath,
     groupLabel: exploreWithMetricNumber.groupLabel,
+    databricksComputeHttpPath:
+        exploreWithMetricNumber.databricksComputeHttpPath,
     spotlight: {
         visibility: 'show',
         categories: [],
