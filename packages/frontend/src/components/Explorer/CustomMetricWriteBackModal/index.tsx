70fa2a095cc43d6d66c7a33f3a495e421792b19e
Javier Rengel <rephus@gmail.com>
2025-02-10 16:06:42 +0100

    feat: multiple custom metrics modal

--- packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx
+++ packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx
@@ -1,3 +1,4 @@
+import { type AdditionalMetric } from '@lightdash/common';
 import {
     Anchor,
     Button,
@@ -15,31 +16,22 @@ import useExplorerContext from '../../../providers/Explorer/useExplorerContext';
 import MantineIcon from '../../common/MantineIcon';
 import { useWriteBackCustomMetrics } from './hooks/useCustomMetricWriteBack';
 
-export const CustomMetricWriteBackModal = () => {
-    const { isOpen, item } = useExplorerContext(
-        (context) => context.state.modals.additionalMetricWriteBack,
-    );
-    const { projectUuid } = useParams<{
-        projectUuid: string;
-    }>();
-
-    const toggleModal = useExplorerContext(
-        (context) => context.actions.toggleAdditionalMetricWriteBackModal,
-    );
-
-    const {
-        mutate: writeBackCustomMetrics,
-        data,
-        isLoading,
-        reset,
-    } = useWriteBackCustomMetrics(projectUuid!);
-
-    const handleClose = useCallback(() => {
-        toggleModal();
-        reset();
-    }, [toggleModal, reset]);
-
-    return item ? (
+const SingleCustomMetricModalContent = ({
+    isOpen,
+    handleClose,
+    data,
+    isLoading,
+    item,
+    writeBackCustomMetrics,
+}: {
+    isOpen: boolean;
+    handleClose: () => void;
+    data: any;
+    isLoading: boolean;
+    item: AdditionalMetric;
+    writeBackCustomMetrics: (items: AdditionalMetric[]) => void;
+}) => {
+    return (
         <Modal
             size="lg"
             onClick={(e) => e.stopPropagation()}
@@ -96,7 +88,7 @@ export const CustomMetricWriteBackModal = () => {
                 ) : (
                     <>
                         <Text>
-                            Create a pull request in your dbt project’s GitHub
+                            Create a pull request in your dbt project's GitHub
                             repository for the following metric:
                         </Text>
                         <List spacing="xs" pl="xs">
@@ -147,5 +139,147 @@ export const CustomMetricWriteBackModal = () => {
                 )}
             </Group>
         </Modal>
-    ) : null;
+    );
+};
+
+const MultipleCustomMetricModalContent = ({
+    isOpen,
+    handleClose,
+    data,
+    isLoading,
+    items,
+    writeBackCustomMetrics,
+}: {
+    isOpen: boolean;
+    handleClose: () => void;
+    data: any;
+    isLoading: boolean;
+    items: AdditionalMetric[];
+    writeBackCustomMetrics: (items: AdditionalMetric[]) => void;
+}) => {
+    return (
+        <Modal
+            size="lg"
+            onClick={(e) => e.stopPropagation()}
+            opened={isOpen}
+            onClose={handleClose}
+            title={
+                <Group spacing="xs">
+                    <MantineIcon
+                        icon={IconBrandGithub}
+                        size="lg"
+                        color="gray.7"
+                    />
+                    <Text fw={500}>Write back to dbt</Text>
+                    <Tooltip
+                        variant="xs"
+                        withinPortal
+                        multiline
+                        maw={300}
+                        label="Convert this custom metric into a metric in your dbt project. This will create a new branch and start a pull request."
+                    >
+                        <MantineIcon
+                            color="gray.7"
+                            icon={IconInfoCircle}
+                            size={16}
+                        />
+                    </Tooltip>
+                </Group>
+            }
+            styles={(theme) => ({
+                header: { borderBottom: `1px solid ${theme.colors.gray[4]}` },
+                body: { padding: 0 },
+            })}
+        >
+            <p>Multiple</p>
+
+            <Group position="right" w="100%" p="md">
+                {data ? (
+                    <Button
+                        color="gray.7"
+                        onClick={handleClose}
+                        variant="outline"
+                        disabled={isLoading}
+                        size="xs"
+                    >
+                        Close
+                    </Button>
+                ) : (
+                    <>
+                        <Button
+                            color="gray.7"
+                            onClick={handleClose}
+                            variant="outline"
+                            disabled={isLoading}
+                            size="xs"
+                        >
+                            Cancel
+                        </Button>
+
+                        <Button
+                            disabled={isLoading}
+                            size="xs"
+                            onClick={() => {
+                                if (!items) return;
+                                writeBackCustomMetrics(items);
+                            }}
+                        >
+                            {isLoading
+                                ? 'Creating pull request...'
+                                : 'Open Pull Request'}
+                        </Button>
+                    </>
+                )}
+            </Group>
+        </Modal>
+    );
+};
+
+export const CustomMetricWriteBackModal = () => {
+    const { isOpen, items, multiple } = useExplorerContext(
+        (context) => context.state.modals.additionalMetricWriteBack,
+    );
+    const { projectUuid } = useParams<{
+        projectUuid: string;
+    }>();
+
+    const toggleModal = useExplorerContext(
+        (context) => context.actions.toggleAdditionalMetricWriteBackModal,
+    );
+
+    const {
+        mutate: writeBackCustomMetrics,
+        data,
+        isLoading,
+        reset,
+    } = useWriteBackCustomMetrics(projectUuid!);
+
+    const handleClose = useCallback(() => {
+        toggleModal();
+        reset();
+    }, [toggleModal, reset]);
+
+    if (items && !multiple && items.length === 1) {
+        return (
+            <SingleCustomMetricModalContent
+                isOpen={isOpen}
+                handleClose={handleClose}
+                data={data}
+                isLoading={isLoading}
+                item={items[0]}
+                writeBackCustomMetrics={writeBackCustomMetrics}
+            />
+        );
+    } else if (multiple === true) {
+        return (
+            <MultipleCustomMetricModalContent
+                isOpen={isOpen}
+                handleClose={handleClose}
+                data={data}
+                isLoading={isLoading}
+                items={items || []}
+                writeBackCustomMetrics={writeBackCustomMetrics}
+            />
+        );
+    }
 };
