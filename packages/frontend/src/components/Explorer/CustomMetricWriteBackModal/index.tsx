f18c13a5f04331cd57048b748c00f2578ddd3a32
Javier Rengel <rephus@gmail.com>
2025-02-11 08:55:09 +0100

    M  packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx

--- packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx
+++ packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx
@@ -329,6 +329,7 @@ const MultipleCustomMetricModalContent = ({
                             <Prism
                                 language="yaml"
                                 trim={false}
+                                noCopy={previewCode === ''}
                                 // all lines are additions, no need to highlight
                             >
                                 {previewCode}
