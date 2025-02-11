44af3338cbae2ae43f091021c15c327962908891
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-02-11 14:10:48 +0400

    chore: update sdk-test-app port

--- packages/sdk-test-app/vite.config.ts
+++ packages/sdk-test-app/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3001,
+        port: 3030,
         host: true,
     },
 });
