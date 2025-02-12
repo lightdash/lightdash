3a984bc40b43e597a729c8ae2b79cf2487b0dc46
Javier Rengel Jiménez <rephus@gmail.com>
2025-02-12 18:09:35 +0100

    fix: change diff and resize modal (#13634)
    * fix: add highlight and resize modal
    
    * M  packages/backend/package.json
    M  packages/backend/src/controllers/gitIntegrationController.ts
    M  packages/backend/src/generated/routes.ts
    M  packages/backend/src/generated/swagger.json
    M  packages/backend/src/services/GitIntegrationService/GitIntegrationService.ts
    M  packages/common/src/index.ts
    M  packages/common/src/types/gitIntegration.ts
    M  packages/frontend/package.json
    D  packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/hooks/usePreviewCustomMetricWriteBack.ts
    M  packages/frontend/src/components/Explorer/CustomMetricWriteBackModal/index.tsx
    M  pnpm-lock.yaml
    
    * D  packages/backend/src/services/GitIntegrationService/GitIntegrationService.test.ts

--- packages/backend/src/controllers/gitIntegrationController.ts
+++ packages/backend/src/controllers/gitIntegrationController.ts
@@ -2,7 +2,6 @@ import {
     AdditionalMetric,
     ApiErrorPayload,
     GitIntegrationConfiguration,
-    PreviewPullRequest,
     PullRequestCreated,
 } from '@lightdash/common';
 import {
@@ -75,37 +74,6 @@ export class GitIntegrationController extends BaseController {
             results: await this.services
                 .getGitIntegrationService()
                 .createPullRequestForCustomMetrics(
-                    req.user!,
-                    projectUuid,
-                    body.customMetrics,
-                    body.quoteChar || '"',
-                ),
-        };
-    }
-
-    @Middlewares([
-        allowApiKeyAuthentication,
-        isAuthenticated,
-        unauthorisedInDemo,
-    ])
-    @SuccessResponse('200', 'Success')
-    @Post('/preview/custom-metrics')
-    @OperationId('PreviewPullRequestForCustomMetrics')
-    async PreviewPullRequestForCustomMetrics(
-        @Path() projectUuid: string,
-        @Body()
-        body: {
-            customMetrics: AdditionalMetric[];
-            quoteChar?: `"` | `'`; // to be used in the yml dump options
-        },
-        @Request() req: express.Request,
-    ): Promise<{ status: 'ok'; results: PreviewPullRequest }> {
-        this.setStatus(200);
-        return {
-            status: 'ok',
-            results: await this.services
-                .getGitIntegrationService()
-                .previewPullRequestForCustomMetrics(
                     req.user!,
                     projectUuid,
                     body.customMetrics,
