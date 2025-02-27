fd08bd83c3f8d6d6b1dcc12d10dc90e7afd99977
Giorgi Bagdavadze <notgiorgi@gmail.com>
2025-02-27 17:09:00 +0400

    fix(frontend): infinite rerenders in user settings

--- packages/frontend/src/components/UserSettings/UsersAndGroupsPanel/UsersView.tsx
+++ packages/frontend/src/components/UserSettings/UsersAndGroupsPanel/UsersView.tsx
@@ -353,8 +353,11 @@ const UsersView: FC = () => {
     const { classes } = useTableStyles();
     const [page, setPage] = useState(1);
     const [search, setSearch] = useState('');
+    const debouncedValue = useMemo(() => {
+        return { search, page };
+    }, [search, page]);
     const [debouncedSearchQueryAndPage] = useDebouncedValue(
-        { search, page },
+        debouncedValue,
         300,
     );
 
