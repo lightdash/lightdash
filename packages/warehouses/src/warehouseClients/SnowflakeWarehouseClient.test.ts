79203156ea070d6d6ac1f9ed4e7dee2a372b0244
almeidabbm <almeidabatistam@gmail.com>
2025-03-06 15:37:36 +0000

    chore: add unit tests to snowflake warehouse

--- packages/warehouses/src/warehouseClients/SnowflakeWarehouseClient.test.ts
+++ packages/warehouses/src/warehouseClients/SnowflakeWarehouseClient.test.ts
@@ -24,21 +24,33 @@ const mockStreamRows = () =>
         },
     });
 
+const executeMock = jest.fn(({ sqlText, complete }) => {
+    complete(
+        undefined,
+        {
+            streamRows: mockStreamRows,
+            getColumns: () => queryColumnsMock,
+            getQueryId: () => 'queryId',
+            getSqlText: () => sqlText,
+        },
+        [],
+    );
+});
+
+const getResultsFromQueryIdMock = jest.fn(({ sqlText, queryId }) => ({
+    streamRows: mockStreamRows,
+    getColumns: () => queryColumnsMock,
+    getQueryId: () => queryId,
+    getNumRows: () => 1,
+}));
+
 jest.mock('snowflake-sdk', () => ({
     ...jest.requireActual('snowflake-sdk'),
     createConnection: jest.fn(() => ({
         connect: jest.fn((callback) => callback(null, {})),
-        execute: jest.fn(({ sqlText, complete }) => {
-            complete(
-                undefined,
-                {
-                    streamRows: mockStreamRows,
-                    getColumns: () => queryColumnsMock,
-                },
-                [],
-            );
-        }),
+        execute: executeMock,
         destroy: jest.fn((callback) => callback(null, {})),
+        getResultsFromQueryId: getResultsFromQueryIdMock,
     })),
 }));
 
@@ -66,6 +78,129 @@ describe('SnowflakeWarehouseClient', () => {
         expect(await warehouse.getCatalog(config)).toEqual(
             expectedWarehouseSchema,
         );
+    });
+    describe('getPaginatedResults', () => {
+        it('should return raw results', async () => {
+            const client = new SnowflakeWarehouseClient(credentials);
+
+            // Execute test
+            const result = await client.getPaginatedResults({
+                sql: 'SELECT * FROM test',
+                page: 1,
+                pageSize: 10,
+                tags: {},
+                timezone: 'UTC',
+                values: [],
+            });
+
+            // Assertions
+
+            expect(executeMock).toHaveBeenLastCalledWith({
+                sqlText: 'SELECT * FROM test',
+                asyncExec: true,
+                binds: undefined,
+                complete: expect.any(Function),
+            });
+
+            expect(getResultsFromQueryIdMock).toHaveBeenCalledWith({
+                sqlText: '',
+                queryId: 'queryId',
+            });
+
+            expect(result).toEqual({
+                fields: expectedFields,
+                rows: [expectedRow],
+                queryId: 'queryId',
+                pageCount: 1,
+                totalRows: 1,
+            });
+        });
+
+        it('should return formatted results when using a formatter', async () => {
+            const client = new SnowflakeWarehouseClient(credentials);
+
+            function formatter(row: Record<string, unknown>) {
+                return Object.fromEntries(
+                    Object.entries(row).map(([key, value]) => [
+                        key,
+                        {
+                            raw: value,
+                            formatted: `formatted_${value}`,
+                        },
+                    ]),
+                );
+            }
+
+            const result = await client.getPaginatedResults(
+                {
+                    sql: 'SELECT * FROM test',
+                    page: 1,
+                    pageSize: 10,
+                    tags: {},
+                    timezone: 'UTC',
+                    values: [],
+                },
+                formatter,
+            );
+
+            const expectedFormattedRow = formatter(expectedRow);
+
+            // Assertions
+            expect(executeMock).toHaveBeenLastCalledWith({
+                sqlText: 'SELECT * FROM test',
+                asyncExec: true,
+                binds: undefined,
+                complete: expect.any(Function),
+            });
+
+            expect(getResultsFromQueryIdMock).toHaveBeenCalledWith({
+                sqlText: '',
+                queryId: 'queryId',
+            });
+
+            expect(result).toEqual({
+                fields: expectedFields,
+                rows: [expectedFormattedRow],
+                queryId: 'queryId',
+                pageCount: 1,
+                totalRows: 1,
+            });
+        });
+
+        it('should not execute any query when using queryId', async () => {
+            const client = new SnowflakeWarehouseClient(credentials);
+
+            const result = await client.getPaginatedResults({
+                queryId: 'queryId',
+                page: 1,
+                pageSize: 10,
+                tags: {},
+                timezone: 'UTC',
+                values: [],
+            });
+
+            // Assertions
+
+            // Ensure that in this case we don't execute any query, we just fetch results
+            expect(executeMock).toHaveBeenLastCalledWith({
+                sqlText:
+                    'ALTER SESSION SET QUOTED_IDENTIFIERS_IGNORE_CASE = FALSE;',
+                complete: expect.any(Function),
+            });
+
+            expect(getResultsFromQueryIdMock).toHaveBeenCalledWith({
+                sqlText: '',
+                queryId: 'queryId',
+            });
+
+            expect(result).toEqual({
+                fields: expectedFields,
+                rows: [expectedRow],
+                queryId: 'queryId',
+                pageCount: 1,
+                totalRows: 1,
+            });
+        });
     });
 });
 
