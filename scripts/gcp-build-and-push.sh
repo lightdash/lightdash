1c693121b74bbb7f5b704d1c5ccc2446de8ea2fa
João Viana <joao@lightdash.com>
2025-03-13 08:58:30 +0000

    fix: sentry release and sourcemaps on both projects (#14038)
    * chore: remove sentry release action
    
    * chore: add sentry vite plugin
    
    * fix: add sentry sourcemaps correctly
    
    * chore: fix sourcemaps paths in frontend and backend
    
    * chore: some text changes

--- scripts/gcp-build-and-push.sh
+++ scripts/gcp-build-and-push.sh
@@ -17,7 +17,22 @@ echo "Building with tags: $version-commercial, beta, $timestamp"
 
 gcloud auth configure-docker us-docker.pkg.dev
 
-docker build . -t "$image:beta" -t "$image:$timestamp" -t "$image:$version-commercial"
+# Check if Sentry environment variables are set
+SENTRY_BUILD_ARGS=""
+if [ -n "$SENTRY_AUTH_TOKEN" ] && [ -n "$SENTRY_ORG" ] && [ -n "$SENTRY_FRONTEND_PROJECT" ] && [ -n "$SENTRY_BACKEND_PROJECT" ] && [ -n "$SENTRY_ENVIRONMENT" ]; then
+    echo "Sentry environment variables detected, uploading sourcemaps to sentry."
+    SENTRY_BUILD_ARGS="--build-arg SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN \
+                      --build-arg SENTRY_ORG=$SENTRY_ORG \
+                      --build-arg SENTRY_RELEASE_VERSION=$version \
+                      --build-arg SENTRY_FRONTEND_PROJECT=$SENTRY_FRONTEND_PROJECT \
+                      --build-arg SENTRY_BACKEND_PROJECT=$SENTRY_BACKEND_PROJECT \
+                      --build-arg SENTRY_ENVIRONMENT=$SENTRY_ENVIRONMENT"
+else
+    echo "Sentry environment variables not detected, skipping sourcemaps upload."
+fi
+
+# Build Docker image 
+docker build . $SENTRY_BUILD_ARGS -t "$image:beta" -t "$image:$timestamp" -t "$image:$version-commercial"
 
 for tag in "$version-commercial" "beta" "$timestamp"; do
     docker push "$image:$tag"
