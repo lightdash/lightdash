46da1c574180d7acb84290ec29ecb58e614ed0fc
Jose Rego <jose@lightdash.com>
2025-03-14 15:33:20 +0000

    chore(analytics): paginated query events

--- packages/backend/src/analytics/LightdashAnalytics.ts
+++ packages/backend/src/analytics/LightdashAnalytics.ts
@@ -202,7 +202,7 @@ export const getContextFromQueryOrHeader = (
     return getContextFromHeader(req);
 };
 
-type MetricQueryExecutionProperties = {
+export type MetricQueryExecutionProperties = {
     chartId?: string;
     metricsCount: number;
     dimensionsCount: number;
@@ -231,6 +231,12 @@ type MetricQueryExecutionProperties = {
     limit: number;
 };
 
+type PaginatedMetricQueryExecutionProperties =
+    MetricQueryExecutionProperties & {
+        queryId: string;
+        warehouseType: WarehouseTypes;
+    };
+
 type SqlExecutionProperties = {
     sqlChartId?: string;
     usingStreaming: boolean;
@@ -249,10 +255,43 @@ type QueryExecutionEvent = BaseTrack & {
         organizationId: string;
         projectId: string;
     } & (
+        | PaginatedMetricQueryExecutionProperties
         | MetricQueryExecutionProperties
         | SqlExecutionProperties
         | SemanticViewerExecutionProperties
     );
+};
+
+type QueryReadyEvent = BaseTrack & {
+    event: 'query.ready';
+    properties: {
+        queryId: string;
+        projectId: string;
+        warehouseType: WarehouseTypes;
+        warehouseExecutionTimeMs: number | null;
+        totalRowCount: number | null;
+    };
+};
+
+type QueryErrorEvent = BaseTrack & {
+    event: 'query.error';
+    properties: {
+        queryId: string;
+        projectId: string;
+        warehouseType: WarehouseTypes;
+    };
+};
+
+type QueryPageEvent = BaseTrack & {
+    event: 'query_page.fetched';
+    properties: {
+        queryId: string;
+        projectId: string;
+        warehouseType: WarehouseTypes;
+        page: number;
+        resultsPageSize: number;
+        resultsPageExecutionMs: number;
+    };
 };
 
 type SubtotalQueryEvent = BaseTrack & {
@@ -1189,6 +1228,9 @@ type TypedEvent =
     | VerifiedUserEvent
     | UserJoinOrganizationEvent
     | QueryExecutionEvent
+    | QueryReadyEvent
+    | QueryErrorEvent
+    | QueryPageEvent
     | ModeDashboardChartEvent
     | UpdateSavedChartEvent
     | DeleteSavedChartEvent
