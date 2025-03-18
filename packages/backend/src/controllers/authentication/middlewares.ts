4c529ecb511b6bfb9562e1ddccd355fbc1460890
Joao Viana <joao@lightdash.com>
2025-03-18 10:59:24 +0000

    fix: clear session on invalidUser error + invite link

--- packages/backend/src/controllers/authentication/middlewares.ts
+++ packages/backend/src/controllers/authentication/middlewares.ts
@@ -3,9 +3,10 @@
 import {
     AuthorizationError,
     DeactivatedAccountError,
+    InvalidUser,
     LightdashMode,
 } from '@lightdash/common';
-import { Request, RequestHandler } from 'express';
+import { ErrorRequestHandler, Request, RequestHandler } from 'express';
 import passport from 'passport';
 import { URL } from 'url';
 import { lightdashConfig } from '../../config/lightdashConfig';
@@ -99,3 +100,39 @@ export const getOidcRedirectURL =
         }
         return new URL('/', lightdashConfig.siteUrl).href;
     };
+
+/**
+ * This middleware is used to handle InvalidUser errors that might occur when a user tries to access an invite link.
+ * This happens when the user is not logged in and tries to access an invite link, but there is already a session for that user that has not been destroyed.
+ * It destroys the session and returns a 401 error.
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+export const inviteLinkErrorHandler: ErrorRequestHandler = (
+    err,
+    req,
+    res,
+    next,
+) => {
+    if (err instanceof InvalidUser) {
+        const inviteLinkMatch = req.path.match(
+            /^\/api\/v1\/invite-links\/([A-Za-z0-9_-]{30})$/,
+        );
+        if (req.session.id && inviteLinkMatch && req.method === 'GET') {
+            req.session.destroy((destroyErr) => {
+                if (destroyErr) {
+                    throw new AuthorizationError(
+                        `Error destroying session for invite-related path: ${destroyErr.message}`,
+                    );
+                }
+            });
+
+            return next();
+        }
+    }
+
+    return next(err);
+};
