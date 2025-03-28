012555b78f120ef0d8d134b22b068574dfeb928f
Javier Rengel <rephus@gmail.com>
2025-03-28 17:08:58 +0100

    private key

--- packages/cli/src/dbt/targets/snowflake.ts
+++ packages/cli/src/dbt/targets/snowflake.ts
@@ -133,7 +133,7 @@ export const convertSnowflakeSchema = async (
             password: target.password,
             privateKey,
             privateKeyPass: target.private_key_passphrase,
-            authenticationType: keyfilePath ? 'private_key' : 'password',
+            authenticationType: privateKey ? 'private_key' : 'password',
             role: target.role,
             warehouse: target.warehouse,
             database: target.database,
