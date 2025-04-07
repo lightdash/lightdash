f63310babffa18555c691d743831e1b6d6977dff
almeidabbm <almeidabatistam@gmail.com>
2025-04-07 10:40:42 +0100

    fix: remove sorts from subtotals queries

--- packages/backend/src/services/ProjectService/ProjectService.ts
+++ packages/backend/src/services/ProjectService/ProjectService.ts
@@ -6047,6 +6047,7 @@ export class ProjectService extends BaseService {
                             ...subtotalDimensions,
                             ...(pivotDimensions || []), // we always need to include the pivot dimensions in the subtotal query
                         ],
+                        sorts: [],
                     });
                 } catch (e) {
                     this.logger.error(
