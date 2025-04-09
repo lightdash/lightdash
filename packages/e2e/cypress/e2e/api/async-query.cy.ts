d2ea67e5025d38f776e537daf76e37be4ded6f02
Marshall Agnew <marshall@lightdash.com>
2025-04-09 12:27:40 +0200

    add BQ and more details

--- packages/e2e/cypress/e2e/api/async-query.cy.ts
+++ packages/e2e/cypress/e2e/api/async-query.cy.ts
@@ -19,6 +19,7 @@ const runQueryBody = {
 describe('Async Query API', () => {
     let pgProjectUuid: string;
     let snowflakeProjectUuid: string;
+    let bigqueryProjectUuid: string;
 
     before(() => {
         cy.login();
@@ -39,6 +40,12 @@ describe('Async Query API', () => {
                 (p: any) => p.warehouseType === 'snowflake',
             );
             snowflakeProjectUuid = snowflakeProject.projectUuid;
+
+            // Find bigquery project
+            const bigqueryProject = projects.find(
+                (p: any) => p.warehouseType === 'bigquery',
+            );
+            bigqueryProjectUuid = bigqueryProject.projectUuid;
         });
     });
 
@@ -48,6 +55,21 @@ describe('Async Query API', () => {
 
     it('Postgres: should execute async query and get all results', () => {
         const projectUuid = pgProjectUuid;
+
+        cy.log(
+            'Check that fetching the query before having a valid query id returns 404',
+        );
+        cy.request({
+            // Dummy queryUuid
+            url: `${apiUrl}/projects/${projectUuid}/query/13a00154-d590-40f2-bb27-d541f70aa8c6`,
+            method: 'GET',
+            failOnStatusCode: false,
+        }).then((resultsResp) => {
+            expect(resultsResp.status).to.eq(404);
+            expect(resultsResp.body).to.have.property('status', 'error');
+            expect(resultsResp.body).to.have.property('error');
+            expect(resultsResp.body.error.name).to.eq('NotFoundError');
+        });
 
         // First execute the async query
         cy.request({
@@ -91,11 +113,38 @@ describe('Async Query API', () => {
                     2000, // Really, 2007, but that's kinda fragile
                 );
             });
+
+            cy.log('Request page beyond available results');
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=6&pageSize=500`,
+                method: 'GET',
+                failOnStatusCode: false,
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(422);
+                expect(resultsResp.body).to.have.property('status', 'error');
+                expect(resultsResp.body).to.have.property('error');
+                expect(resultsResp.body.error.name).to.eq('PaginationError');
+            });
         });
     });
 
-    it('snowflake: should execute async query and get all results', () => {
+    it('snowflake: should execute async query and get all results paged', () => {
         const projectUuid = snowflakeProjectUuid;
+
+        cy.log(
+            'Check that fetching the query before having a valid query id returns 404',
+        );
+        cy.request({
+            // Dummy queryUuid
+            url: `${apiUrl}/projects/${projectUuid}/query/13a00154-d590-40f2-bb27-d541f70aa8c6`,
+            method: 'GET',
+            failOnStatusCode: false,
+        }).then((resultsResp) => {
+            expect(resultsResp.status).to.eq(404);
+            expect(resultsResp.body).to.have.property('status', 'error');
+            expect(resultsResp.body).to.have.property('error');
+            expect(resultsResp.body.error.name).to.eq('NotFoundError');
+        });
 
         cy.log('First execute the async query');
         cy.request({
@@ -129,13 +178,18 @@ describe('Async Query API', () => {
 
             let pageOneResults: any;
 
-            // default results are 500
+            // default results
             checkResults().then((resultsResp: any) => {
                 expect(resultsResp.status).to.eq(200);
                 expect(resultsResp.body).to.have.property('status', 'ok');
                 expect(resultsResp.body.results).to.have.property('rows');
                 expect(resultsResp.body.results.rows).to.be.an('array');
                 expect(resultsResp.body.results.rows.length).to.be.eq(500);
+                expect(resultsResp.body.results.totalResults).to.be.greaterThan(
+                    2000,
+                );
+                // ~ 2007 / 500 = 5 pages
+                expect(resultsResp.body.results.totalPageCount).to.be.eq(5);
                 pageOneResults = resultsResp.body.results.rows;
             });
 
@@ -191,7 +245,148 @@ describe('Async Query API', () => {
                 );
             });
 
-            // Add checks for over the final page
+            cy.log('Request page beyond available results');
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=6&pageSize=500`,
+                method: 'GET',
+                failOnStatusCode: false,
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(422);
+                expect(resultsResp.body).to.have.property('status', 'error');
+                expect(resultsResp.body).to.have.property('error');
+                expect(resultsResp.body.error.name).to.eq('PaginationError');
+            });
+        });
+    });
+
+    it('bigquery: should execute async query and get all results paged', () => {
+        const projectUuid = bigqueryProjectUuid;
+
+        cy.log(
+            'Check that fetching the query before having a valid query id returns 404',
+        );
+        cy.request({
+            // Dummy queryUuid
+            url: `${apiUrl}/projects/${projectUuid}/query/13a00154-d590-40f2-bb27-d541f70aa8c6`,
+            method: 'GET',
+            failOnStatusCode: false,
+        }).then((resultsResp) => {
+            expect(resultsResp.status).to.eq(404);
+            expect(resultsResp.body).to.have.property('status', 'error');
+            expect(resultsResp.body).to.have.property('error');
+            expect(resultsResp.body.error.name).to.eq('NotFoundError');
+        });
+
+        cy.log('First execute the async query');
+        cy.request({
+            url: `${apiUrl}/projects/${projectUuid}/query`,
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: runQueryBody,
+        }).then((executeResp) => {
+            expect(executeResp.status).to.eq(200);
+            expect(executeResp.body).to.have.property('status', 'ok');
+            expect(executeResp.body.results).to.have.property('queryUuid');
+
+            const { queryUuid } = executeResp.body.results;
+
+            cy.log('Poll for results until ready');
+            // Poll for results until ready
+            const checkResults = (): any =>
+                cy
+                    .request({
+                        url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}`,
+                        method: 'GET',
+                    })
+                    .then((resultsResp: any) => {
+                        if (resultsResp.body.results.status !== 'ready') {
+                            // If not ready, wait 200ms and try again
+                            cy.wait(200);
+                            return checkResults();
+                        }
+                        return resultsResp;
+                    });
+
+            let pageOneResults: any;
+
+            // default results
+            checkResults().then((resultsResp: any) => {
+                expect(resultsResp.status).to.eq(200);
+                expect(resultsResp.body).to.have.property('status', 'ok');
+                expect(resultsResp.body.results).to.have.property('rows');
+                expect(resultsResp.body.results.rows).to.be.an('array');
+                expect(resultsResp.body.results.rows.length).to.be.eq(500);
+                expect(resultsResp.body.results.totalResults).to.be.greaterThan(
+                    2000,
+                );
+                // ~ 2007 / 500 = 5 pages
+                expect(resultsResp.body.results.totalPageCount).to.be.eq(5);
+                pageOneResults = resultsResp.body.results.rows;
+            });
+
+            cy.log(
+                'Get the right number of results in a page, check the page content',
+            );
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=1&pageSize=500`,
+                method: 'GET',
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(200);
+                expect(resultsResp.body).to.have.property('status', 'ok');
+                expect(resultsResp.body.results).to.have.property('rows');
+                expect(resultsResp.body.results.rows).to.be.an('array');
+                expect(resultsResp.body.results.rows.length).to.be.eq(500);
+                // Should be the same rows as the default
+                expect(resultsResp.body.results.rows).to.deep.eq(
+                    pageOneResults,
+                );
+            });
+
+            cy.log('Get page 2');
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=2&pageSize=500`,
+                method: 'GET',
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(200);
+                expect(resultsResp.body).to.have.property('status', 'ok');
+                expect(resultsResp.body.results).to.have.property('rows');
+                expect(resultsResp.body.results.rows).to.be.an('array');
+                expect(resultsResp.body.results.rows.length).to.be.eq(500);
+                // Should NOT be the same rows as the default
+                expect(resultsResp.body.results.rows).not.to.deep.eq(
+                    pageOneResults,
+                );
+            });
+
+            cy.log(
+                'get the first 100 results and check the content against the saved 1st 100',
+            );
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=1&pageSize=100`,
+                method: 'GET',
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(200);
+                expect(resultsResp.body).to.have.property('status', 'ok');
+                expect(resultsResp.body.results).to.have.property('rows');
+                expect(resultsResp.body.results.rows).to.be.an('array');
+                expect(resultsResp.body.results.rows.length).to.be.eq(100);
+                // Should be the same as the first 100 rows from pageOneResults
+                expect(resultsResp.body.results.rows).to.deep.eq(
+                    pageOneResults.slice(0, 100),
+                );
+            });
+
+            cy.log('Request page beyond available results');
+            cy.request({
+                url: `${apiUrl}/projects/${projectUuid}/query/${queryUuid}?page=6&pageSize=500`,
+                method: 'GET',
+                failOnStatusCode: false,
+            }).then((resultsResp) => {
+                expect(resultsResp.status).to.eq(422);
+                expect(resultsResp.body).to.have.property('status', 'error');
+                expect(resultsResp.body).to.have.property('error');
+                expect(resultsResp.body.error.name).to.eq('PaginationError');
+            });
         });
     });
 });
