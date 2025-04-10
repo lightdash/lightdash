eb54ceeed8cdef6118814569dba23b162e37b433
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-04-10 05:20:25 -0400

    react sdk demo in nextjs

---
+++ packages/sdk-next-test-app/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import YourCustomDashboard from './yourCustomDashboard';
+
+export default function DashboardPage() {
+    return (
+        <>
+            <div>
+                <h1>LightdashDashboard demo in Next.js</h1>
+
+                <YourCustomDashboard />
+            </div>
+        </>
+    );
+}
