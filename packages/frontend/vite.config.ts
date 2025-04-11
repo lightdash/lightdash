04623caceef1f426fe0637dc7d95871ecd48a328
Giorgi Bagdavadze <12987425+notgiorgi@users.noreply.github.com>
2025-04-11 13:10:03 +0400

    chore: add allowed host for cloudflared tunnels in Vite config (#14363)

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -142,6 +142,7 @@ export default defineConfig({
         },
         allowedHosts: [
             'lightdash-dev', // for local development with docker
+            '.lightdash.dev', // for cloudflared tunnels
         ],
         proxy: {
             '/api': {
