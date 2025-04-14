7336a2a94468d3b0bc4028b433a3cef095477b76
almeidabbm <almeidabatistam@gmail.com>
2025-04-14 11:34:21 +0100

    chore: cleanup useDashboardChart hook

--- packages/frontend/src/hooks/dashboard/useDashboardChart.ts
+++ packages/frontend/src/hooks/dashboard/useDashboardChart.ts
@@ -1,65 +1,19 @@
 import {
     type ApiChartAndResults,
     type ApiError,
-    type DashboardFilters,
-    type DateGranularity,
-    FeatureFlags,
     QueryExecutionContext,
-    type SortField,
 } from '@lightdash/common';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useCallback, useMemo } from 'react';
-import { lightdashApi } from '../../api';
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import useDashboardContext from '../../providers/Dashboard/useDashboardContext';
 import { convertDateDashboardFilters } from '../../utils/dateFilter';
 import { useExplore } from '../useExplore';
-import { useFeatureFlag } from '../useFeatureFlagEnabled';
 import { getQueryPaginatedResults } from '../useQueryResults';
 import { useSavedQuery } from '../useSavedQuery';
 import useSearchParams from '../useSearchParams';
 import useDashboardFiltersForTile from './useDashboardFiltersForTile';
 
-const getChartAndResults = async ({
-    chartUuid,
-    dashboardUuid,
-    dashboardFilters,
-    invalidateCache,
-    dashboardSorts,
-    granularity,
-    autoRefresh,
-    context,
-}: {
-    chartUuid?: string;
-    dashboardUuid: string;
-    dashboardFilters: DashboardFilters;
-    invalidateCache?: boolean;
-    dashboardSorts: SortField[];
-    granularity?: DateGranularity;
-    autoRefresh?: boolean;
-    context?: string;
-}) => {
-    return lightdashApi<ApiChartAndResults>({
-        url: `/saved/${chartUuid}/chart-and-results${
-            context ? `?context=${context}` : ''
-        }`,
-        method: 'POST',
-        body: JSON.stringify({
-            dashboardUuid,
-            dashboardFilters,
-            dashboardSorts,
-            granularity,
-            ...(invalidateCache && { invalidateCache: true }),
-            autoRefresh,
-        }),
-    });
-};
-
 const useDashboardChart = (tileUuid: string, chartUuid: string | null) => {
-    const {
-        data: queryPaginationEnabled,
-        isFetched: isQueryPaginationFetched,
-    } = useFeatureFlag(FeatureFlags.QueryPagination);
-
     const dashboardUuid = useDashboardContext((c) => c.dashboard?.uuid);
     const invalidateCache = useDashboardContext((c) => c.invalidateCache);
     const dashboardFilters = useDashboardFiltersForTile(tileUuid);
@@ -75,69 +29,78 @@ const useDashboardChart = (tileUuid: string, chartUuid: string | null) => {
     const setChartsWithDateZoomApplied = useDashboardContext(
         (c) => c.setChartsWithDateZoomApplied,
     );
-    const queryClient = useQueryClient();
 
     const sortKey =
         dashboardSorts
             ?.map((ds) => `${ds.fieldId}.${ds.descending}`)
             ?.join(',') || '';
 
-    const queryKey = useMemo(
-        () => [
-            'savedChartResults',
-            queryPaginationEnabled,
-            chartUuid,
-            dashboardUuid,
-            dashboardFilters,
-            invalidateCache,
-            sortKey,
-            autoRefresh,
-        ],
-        [
-            queryPaginationEnabled,
-            chartUuid,
-            dashboardUuid,
-            dashboardFilters,
-            invalidateCache,
-            sortKey,
-            autoRefresh,
-        ],
-    );
-
-    const { data: chart, isFetched: isChartFetched } = useSavedQuery({
-        id: chartUuid!,
-        useQueryOptions: {
-            enabled: !!chartUuid && queryPaginationEnabled?.enabled,
-        },
+    const { data: chart } = useSavedQuery({
+        id: chartUuid ?? undefined,
     });
 
-    const { data: explore, isFetched: isExploreFetched } = useExplore(
-        chart?.metricQuery?.exploreName,
-        {
-            enabled:
-                !!chart?.metricQuery?.exploreName &&
-                queryPaginationEnabled?.enabled,
-        },
-    );
-
-    const apiChartAndResults =
-        queryClient.getQueryData<ApiChartAndResults>(queryKey);
+    const { data: explore } = useExplore(chart?.metricQuery?.exploreName);
 
     const timezoneFixFilters =
         dashboardFilters && convertDateDashboardFilters(dashboardFilters);
-    const hasADateDimension =
-        !!apiChartAndResults?.metricQuery?.metadata?.hasADateDimension;
+    const hasADateDimension = !!chart?.metricQuery?.metadata?.hasADateDimension;
 
-    const fetchChartAndResults = useCallback<
-        () => Promise<
-            ApiChartAndResults & {
-                queryUuid?: string;
-                warehouseExecutionTimeMs?: number;
-                totalTimeMs?: number;
+    setChartsWithDateZoomApplied((prev) => {
+        if (hasADateDimension) {
+            if (granularity) {
+                return (prev ?? new Set()).add(chartUuid!);
             }
-        >
-    >(async () => {
-        if (queryPaginationEnabled?.enabled && explore && chart) {
+            prev?.clear();
+            return prev;
+        }
+        return prev;
+    });
+
+    const queryKey = useMemo(
+        () => [
+            'dashboardChart',
+            [
+                chart?.projectUuid,
+                chartUuid,
+                dashboardUuid,
+                timezoneFixFilters,
+                dashboardSorts,
+                sortKey,
+                context,
+                autoRefresh,
+                invalidateCache,
+            ],
+        ],
+        [
+            chart?.projectUuid,
+            chartUuid,
+            dashboardUuid,
+            timezoneFixFilters,
+            dashboardSorts,
+            sortKey,
+            context,
+            autoRefresh,
+            invalidateCache,
+        ],
+    );
+
+    return useQuery<
+        ApiChartAndResults & {
+            queryUuid?: string;
+            warehouseExecutionTimeMs?: number;
+            totalTimeMs?: number;
+        },
+        ApiError
+    >({
+        queryKey:
+            hasADateDimension && granularity
+                ? queryKey.concat([granularity])
+                : queryKey,
+        queryFn: async () => {
+            if (!chart || !explore) {
+                throw new Error('Chart or explore is undefined');
+            }
+
             const results = await getQueryPaginatedResults(chart.projectUuid, {
                 context: autoRefresh
                     ? QueryExecutionContext.AUTOREFRESHED_DASHBOARD
@@ -162,64 +125,13 @@ const useDashboardChart = (tileUuid: string, chartUuid: string | null) => {
                 queryUuid: results.queryUuid,
                 warehouseExecutionTimeMs: results.warehouseExecutionTimeMs,
                 totalTimeMs: results.totalTimeMs,
+            } as ApiChartAndResults & {
+                queryUuid?: string;
+                warehouseExecutionTimeMs?: number;
+                totalTimeMs?: number;
             };
-        }
-        return getChartAndResults({
-            chartUuid: chartUuid!,
-            dashboardUuid: dashboardUuid!,
-            dashboardFilters: timezoneFixFilters,
-            invalidateCache,
-            dashboardSorts,
-            granularity,
-            autoRefresh,
-            context,
-        });
-    }, [
-        queryPaginationEnabled?.enabled,
-        explore,
-        chart,
-        chartUuid,
-        dashboardUuid,
-        timezoneFixFilters,
-        invalidateCache,
-        dashboardSorts,
-        granularity,
-        autoRefresh,
-        context,
-    ]);
-
-    setChartsWithDateZoomApplied((prev) => {
-        if (hasADateDimension) {
-            if (granularity) {
-                return (prev ?? new Set()).add(chartUuid!);
-            }
-            prev?.clear();
-            return prev;
-        }
-        return prev;
-    });
-
-    return useQuery<
-        ApiChartAndResults & {
-            queryUuid?: string;
-            warehouseExecutionTimeMs?: number;
-            totalTimeMs?: number;
         },
-        ApiError
-    >({
-        queryKey:
-            hasADateDimension && granularity
-                ? queryKey.concat([granularity])
-                : queryKey,
-        queryFn: fetchChartAndResults,
-        enabled:
-            !!chartUuid &&
-            !!dashboardUuid &&
-            isQueryPaginationFetched &&
-            ((isExploreFetched &&
-                isChartFetched &&
-                queryPaginationEnabled?.enabled) ||
-                !queryPaginationEnabled?.enabled),
+        enabled: Boolean(chartUuid && dashboardUuid && chart && explore),
         retry: false,
         refetchOnMount: false,
     });
