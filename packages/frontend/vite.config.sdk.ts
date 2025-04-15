28430dd0f4ef603e7fbb7a4e31803e88a12a646e
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-04-15 10:38:37 +0400

    fix: react sdk jsx runtime (#14388)
    * react sdk demo in nextjs
    
    * fix: react sdk jsx runtime
    
    * chore: reorg sdk package structure
    
    * chore: minor cleanup
    
    * chore: more cleanups
    
    * chore: better build script
    
    * sync versions

---
+++ packages/frontend/vite.config.sdk.ts
@@ -0,0 +1,43 @@
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
+
+process.env.NODE_ENV = 'production';
+
+export default defineConfig({
+    publicDir: false,
+    define: {
+        __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+    plugins: [svgrPlugin(), dts({ rollupTypes: true })],
+    mode: 'production',
+    build: {
+        outDir: 'sdk/dist',
+        emptyOutDir: true,
+        target: 'es2020',
+        // minify: 'esbuild',
+        sourcemap: true,
+        lib: {
+            entry: resolve(__dirname, 'sdk', 'index.tsx'),
+            name: 'LightdashSDK',
+            formats: ['es', 'cjs'],
+            fileName: (ext) => `sdk.${ext}.js`,
+            cssFileName: 'sdk',
+        },
+        rollupOptions: {
+            external: [
+                'react',
+                'react-dom',
+                'react/jsx-runtime',
+                'react/jsx-dev-runtime',
+            ],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+    },
+});
---
+++ packages/sdk-next-test-app/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import YourCustomDashboardOne from './yourCustomDashboardOne';
+
+export default function DashboardOne() {
+    return (
+        <>
+            <div>
+                <h1>Lightdash Dashboard</h1>
+
+                <YourCustomDashboardOne />
+            </div>
+        </>
+    );
+}
