b1063329b9c7defb265506a44ef0480d2f8c3ad9
Joao Viana <joao@lightdash.com>
2025-04-22 19:37:22 +0100

    feat: render tree in space-save view

---
+++ packages/frontend/src/components/common/SpaceSelector/SpaceCreationForm.tsx
@@ -0,0 +1,67 @@
+import { Alert, Box, Button, Stack, Text, TextInput } from '@mantine/core';
+import { IconArrowLeft, IconInfoCircle } from '@tabler/icons-react';
+import MantineIcon from '../MantineIcon';
+
+type SpaceCreationFormProps = {
+    spaceName: string;
+    onSpaceNameChange: (name: string) => void;
+    onCancel: () => void;
+    isLoading?: boolean;
+    parentSpaceName?: string;
+};
+
+const SpaceCreationForm = ({
+    spaceName,
+    onSpaceNameChange,
+    onCancel,
+    isLoading,
+    parentSpaceName,
+}: SpaceCreationFormProps) => {
+    return (
+        <Stack spacing="xs">
+            <Box>
+                <Button
+                    variant="subtle"
+                    leftIcon={<MantineIcon icon={IconArrowLeft} />}
+                    onClick={onCancel}
+                    disabled={isLoading}
+                    size="xs"
+                    compact
+                >
+                    Back
+                </Button>
+            </Box>
+
+            {parentSpaceName && (
+                <Text fz="sm" fw={500}>
+                    You are creating a new space in{' '}
+                    <Text span fw={600}>
+                        '{parentSpaceName}'
+                    </Text>
+                </Text>
+            )}
+
+            <TextInput
+                label="Name"
+                placeholder="Space name"
+                required
+                disabled={isLoading}
+                value={spaceName}
+                onChange={(e) => onSpaceNameChange(e.target.value)}
+            />
+
+            {parentSpaceName && (
+                <Alert color="blue" icon={<IconInfoCircle size={16} />}>
+                    <Text fw={500} color="blue">
+                        Permissions will be inherited from{' '}
+                        <Text span fw={600}>
+                            '{parentSpaceName}'
+                        </Text>
+                    </Text>
+                </Alert>
+            )}
+        </Stack>
+    );
+};
+
+export default SpaceCreationForm;
