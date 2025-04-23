892cd31704644589fad23743678c124c226bfc7b
Jose Rego <jose@lightdash.com>
2025-04-23 09:10:53 +0100

    fix: get cached user

--- packages/backend/src/services/UserService.ts
+++ packages/backend/src/services/UserService.ts
@@ -1502,11 +1502,18 @@ export class UserService extends BaseService {
     }
 
     async findSessionUser(passportUser: { id: string; organization: string }) {
-        return wrapSentryTransaction('Passport.deserializeUser', {}, () =>
-            this.userModel.findSessionUserAndOrgByUuid(
-                passportUser.id,
-                passportUser.organization,
-            ),
+        return wrapSentryTransaction(
+            'Passport.deserializeUser',
+            {},
+            async (span) => {
+                const { sessionUser, cacheHit } =
+                    await this.userModel.getSessionUserFromCacheOrDB(
+                        passportUser.id,
+                        passportUser.organization,
+                    );
+                span.setAttribute('cacheHit', cacheHit);
+                return sessionUser;
+            },
         );
     }
 
