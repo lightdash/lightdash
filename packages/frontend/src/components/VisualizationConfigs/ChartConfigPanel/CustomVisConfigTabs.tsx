3bc237f23ee0b69c2cb23787292d89ffdb8f17b3
Javier Rengel <rephus@gmail.com>
2025-04-23 16:10:02 +0200

    fix: custom viz UI improvements

--- packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/CustomVisConfigTabs.tsx
+++ packages/frontend/src/components/VisualizationConfigs/ChartConfigPanel/CustomVisConfigTabs.tsx
@@ -1,7 +1,18 @@
-import { Loader, Tabs } from '@mantine/core';
+import {
+    getDefaultZIndex,
+    Group,
+    Loader,
+    Select,
+    Tabs,
+    Text,
+} from '@mantine/core';
+import { useDebouncedValue } from '@mantine/hooks';
 import Editor, { type EditorProps, type Monaco } from '@monaco-editor/react';
 import merge from 'lodash/merge';
+import { type IDisposable, type languages } from 'monaco-editor';
 import React, { memo, useEffect, useRef, useState } from 'react';
+import { useDeepCompareEffect } from 'react-use';
+import DocumentationHelpButton from '../../DocumentationHelpButton';
 import { isCustomVisualizationConfig } from '../../LightdashVisualization/types';
 import { useVisualizationContext } from '../../LightdashVisualization/useVisualizationContext';
 
@@ -14,12 +25,16 @@ type Schema = {
 const MONACO_DEFAULT_OPTIONS: EditorProps['options'] = {
     cursorBlinking: 'smooth',
     folding: true,
-    lineNumbersMinChars: 1,
     minimap: { enabled: false },
     scrollBeyondLastLine: false,
     wordWrap: 'off',
     quickSuggestions: true,
     contextmenu: false,
+    lineNumbers: 'off',
+    glyphMargin: false,
+    lineDecorationsWidth: 0,
+    lineNumbersMinChars: 0,
+    fixedOverflowWidgets: true,
 };
 
 const initVegaLazySchema = async (fields: string[]) => {
@@ -64,6 +79,44 @@ const loadMonaco = (monaco: Monaco, schemas: Schema[]) => {
         diagnostics: true,
     });
 };
+let completionProviderDisposable: IDisposable | null = null;
+
+const registerCustomCompletionProvider = (
+    monaco: Monaco,
+    language: string,
+    fields: string[],
+) => {
+    if (completionProviderDisposable) {
+        console.debug('Clearing Monaco completion provider');
+        completionProviderDisposable.dispose();
+    }
+    completionProviderDisposable =
+        monaco.languages.registerCompletionItemProvider(language, {
+            provideCompletionItems: (model, position) => {
+                const wordUntilPosition = model.getWordUntilPosition(position);
+                const range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: wordUntilPosition.startColumn,
+                    endColumn: wordUntilPosition.endColumn,
+                };
+
+                const suggestions: languages.CompletionItem[] = fields.map(
+                    (field) => {
+                        return {
+                            label: field,
+                            kind: monaco.languages.CompletionItemKind.Class,
+                            insertText: field,
+                            range,
+                        };
+                    },
+                );
+
+                return { suggestions };
+            },
+            triggerCharacters: ['$'],
+        });
+};
 
 const CustomVisConfigTabs: React.FC = memo(() => {
     const { visualizationConfig } = useVisualizationContext();
@@ -85,57 +138,136 @@ const CustomVisConfigTabs: React.FC = memo(() => {
         void initVegaAsync();
     }, [isCustomConfig, visualizationConfig.chartConfig]);
 
+    const [editorConfig, setEditorConfig] = useState<string>('');
+    const [debouncedTooltipValue] = useDebouncedValue(editorConfig, 1000);
+
+    useEffect(() => {
+        if (!isCustomConfig || isLoading) return;
+        visualizationConfig.chartConfig.setVisSpec(debouncedTooltipValue ?? '');
+    }, [
+        isLoading,
+        isCustomConfig,
+        debouncedTooltipValue,
+        visualizationConfig.chartConfig,
+    ]);
+
+    const [monacoOptions, setMonacoOptions] = useState<
+        EditorProps['options'] | undefined
+    >();
+
+    useDeepCompareEffect(() => {
+        /** Creates a container that belongs to body, outside of the sidebar
+         * so we can place the autocomplete tooltip and it doesn't overflow
+         * CSS for this component is set on `monaco.css`
+         */
+        const containerId = 'monaco-overflow-container';
+        let container = document.getElementById(containerId);
+        if (!container) {
+            const wrapper = document.createElement('div');
+            wrapper.className = 'monaco-editor';
+            container = document.createElement('div');
+            container.id = containerId;
+            wrapper.appendChild(container);
+            document.getElementById('root')?.appendChild(wrapper);
+        }
+        setMonacoOptions({
+            ...MONACO_DEFAULT_OPTIONS,
+            overflowWidgetsDomNode: container,
+        });
+    }, [monacoOptions]);
+
+    if (!monacoOptions) return null; // we should not load monaco before options are set with the overflowWidgetsDomNode
+
     if (isLoading) {
         return <Loader color="gray" size="xs" />;
     }
 
     if (!isCustomConfig) return null;
-    const { visSpec, setVisSpec, series } = visualizationConfig.chartConfig;
+    const { series } = visualizationConfig.chartConfig;
 
     return (
-        <Tabs
-            defaultValue="config"
-            style={{ flexGrow: 1 }}
-            styles={{
-                root: {
-                    display: 'flex',
-                    flexDirection: 'column',
-                },
-                panel: {
-                    flexGrow: 1,
-                },
-            }}
-        >
-            <Tabs.List>
-                <Tabs.Tab value="config">Config</Tabs.Tab>
-                <Tabs.Tab value="data">Data</Tabs.Tab>
-            </Tabs.List>
+        <>
+            <Group>
+                <Text>Load template</Text>
+                <Select data={['', 'config', 'data']} defaultValue="config" />
+            </Group>
+            <Tabs
+                defaultValue="config"
+                style={{ flexGrow: 1 }}
+                styles={{
+                    root: {
+                        display: 'flex',
+                        flexDirection: 'column',
+                    },
+                    panel: {
+                        flexGrow: 1,
+                    },
+                }}
+            >
+                <Tabs.List>
+                    <Tabs.Tab value="config">Config</Tabs.Tab>
+                    <Tabs.Tab value="data">Data</Tabs.Tab>
+                    <DocumentationHelpButton
+                        ml="auto"
+                        mt="xs"
+                        href="https://docs.lightdash.com/references/custom-charts#custom-charts"
+                    />
+                </Tabs.List>
 
-            <Tabs.Panel value="config">
-                <Editor
-                    loading={<Loader color="gray" size="xs" />}
-                    beforeMount={(monaco) =>
-                        loadMonaco(monaco, schemas.current!)
-                    }
-                    defaultLanguage="json"
-                    options={{ ...MONACO_DEFAULT_OPTIONS }}
-                    value={visSpec}
-                    onChange={(config) => setVisSpec(config ?? '')}
-                />
-            </Tabs.Panel>
+                <Tabs.Panel value="config">
+                    {/* Hack to show a monaco placeholder */}
+                    {(editorConfig || '')?.length === 0 ? (
+                        <Text
+                            ml="xl"
+                            pos="absolute"
+                            w="330px"
+                            color="gray.5"
+                            sx={{
+                                pointerEvents: 'none',
+                                zIndex: getDefaultZIndex('overlay'),
+                                fontFamily: 'monospace',
+                            }}
+                        >
+                            {`Write some vega lite JSON or select a template. Check our docs for more info and examples.`}
+                        </Text>
+                    ) : null}
 
-            <Tabs.Panel value="data">
-                <Editor
-                    loading={<Loader color="gray" size="xs" />}
-                    defaultLanguage="json"
-                    options={{
-                        ...MONACO_DEFAULT_OPTIONS,
-                        readOnly: true,
-                    }}
-                    defaultValue={JSON.stringify(series, null, 2)}
-                />
-            </Tabs.Panel>
-        </Tabs>
+                    <Editor
+                        loading={<Loader color="gray" size="xs" />}
+                        beforeMount={(monaco) => {
+                            loadMonaco(monaco, schemas.current!);
+                            const fields = Object.keys(series[0] || {});
+                            registerCustomCompletionProvider(
+                                monaco,
+                                'json',
+                                fields,
+                            );
+                        }}
+                        defaultLanguage="json"
+                        options={monacoOptions}
+                        value={editorConfig}
+                        onChange={(config) => {
+                            setEditorConfig(config ?? '');
+                        }}
+                        wrapperProps={{
+                            id: 'tooltip-editor-wrapper',
+                        }}
+                    />
+                </Tabs.Panel>
+
+                <Tabs.Panel value="data">
+                    <Editor
+                        loading={<Loader color="gray" size="xs" />}
+                        defaultLanguage="json"
+                        options={{
+                            ...MONACO_DEFAULT_OPTIONS,
+                            readOnly: true,
+                        }}
+                        defaultValue={JSON.stringify(series, null, 2)}
+                    />
+                </Tabs.Panel>
+            </Tabs>
+        </>
     );
 });
 
