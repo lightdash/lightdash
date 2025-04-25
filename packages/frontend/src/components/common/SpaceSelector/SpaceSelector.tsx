751f3a58defe981af227c9c4805d6d471c510100
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-04-25 12:32:15 +0400

    feat: admin content view for space tree selector (#14542)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    Closes: https://github.com/lightdash/lightdash/issues/14500
    
    ### Description:
    
    
    [CleanShot 2025-04-24 at 20.18.36.mp4 <span class="graphite__hidden">(uploaded via Graphite)</span> <img class="graphite__hidden" src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/cOL6QbQzyaUdOs5psinD/e02edb25-b1c1-49b7-b37a-b9e16c05c438.mp4" />](https://app.graphite.dev/media/video/cOL6QbQzyaUdOs5psinD/e02edb25-b1c1-49b7-b37a-b9e16c05c438.mp4)
    
    ### Reviewer actions
    
    - [ ] I have manually tested the changes in the preview environment
    - [ ] I have reviewed the code
    - [ ] I understand that "request changes" will block this PR from merging

--- packages/frontend/src/components/common/SpaceSelector/SpaceSelector.tsx
+++ packages/frontend/src/components/common/SpaceSelector/SpaceSelector.tsx
@@ -1,20 +1,33 @@
-import { FeatureFlags } from '@lightdash/common';
+import { subject } from '@casl/ability';
+import {
+    assertUnreachable,
+    FeatureFlags,
+    type SpaceSummary,
+} from '@lightdash/common';
 import {
     Paper,
     ScrollArea,
+    Stack,
     type PaperProps,
     type ScrollAreaProps,
 } from '@mantine/core';
+import { useMemo, useState } from 'react';
 import { useFeatureFlagEnabled } from '../../../hooks/useFeatureFlagEnabled';
+import { hasDirectAccessToSpace } from '../../../hooks/useSpaces';
+import useApp from '../../../providers/App/useApp';
+import AdminContentViewFilter from '../ResourceView/AdminContentViewFilter';
 import Tree from '../Tree/Tree';
 import { type NestableItem } from '../Tree/types';
 
 type SpaceSelectorProps = {
-    spaces: Array<NestableItem> | undefined;
+    spaces:
+        | Array<Pick<SpaceSummary, 'isPrivate' | 'access'> & NestableItem>
+        | undefined;
     selectedSpaceUuid: string | null;
     scrollingContainerProps?: PaperProps & ScrollAreaProps;
     isLoading?: boolean;
     onSelectSpace: (spaceUuid: string | null) => void;
+    projectUuid: string | undefined;
 };
 
 const SpaceSelector = ({
@@ -23,27 +36,70 @@ const SpaceSelector = ({
     scrollingContainerProps,
     isLoading: _isLoading, // TODO: implement loading state for the tree.
     onSelectSpace,
+    projectUuid,
 }: SpaceSelectorProps) => {
+    const { user } = useApp();
+
+    const userCanManageProject = user.data?.ability?.can(
+        'manage',
+        subject('Project', {
+            organizationUuid: user.data?.organizationUuid,
+            projectUuid: projectUuid,
+        }),
+    );
+
     const isNestedSpacesEnabled = useFeatureFlagEnabled(
         FeatureFlags.NestedSpaces,
     );
 
+    const [selectedAdminContentType, setSelectedAdminContentType] = useState<
+        'all' | 'shared'
+    >('shared');
+
+    const filteredSpaces = useMemo(() => {
+        if (!user.data) return [];
+
+        switch (selectedAdminContentType) {
+            case 'all':
+                return spaces;
+            case 'shared':
+                return spaces.filter((space) =>
+                    hasDirectAccessToSpace(space, user.data.userUuid),
+                );
+            default:
+                return assertUnreachable(
+                    selectedAdminContentType,
+                    `Invalid admin content type when filtering spaces: ${selectedAdminContentType}`,
+                );
+        }
+    }, [user.data, selectedAdminContentType, spaces]);
+
     if (isNestedSpacesEnabled) {
         return (
-            <Paper
-                component={ScrollArea}
-                w="100%"
-                h="200px"
-                withBorder
-                {...scrollingContainerProps}
-            >
-                <Tree
-                    data={spaces}
-                    value={selectedSpaceUuid}
-                    onChange={onSelectSpace}
-                    topLevelLabel="Spaces"
-                />
-            </Paper>
+            <Stack>
+                {userCanManageProject ? (
+                    <AdminContentViewFilter
+                        value={selectedAdminContentType}
+                        onChange={setSelectedAdminContentType}
+                        withDivider={false}
+                    />
+                ) : null}
+
+                <Paper
+                    component={ScrollArea}
+                    w="100%"
+                    h="200px"
+                    withBorder
+                    {...scrollingContainerProps}
+                >
+                    <Tree
+                        data={filteredSpaces}
+                        value={selectedSpaceUuid}
+                        onChange={onSelectSpace}
+                        topLevelLabel="Spaces"
+                    />
+                </Paper>
+            </Stack>
         );
     }
 
