9d6c785d1e038f4506c2efd288b3bd6c263da62b
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-04-29 16:36:13 +0400

    chore: add mantine v8 as an alias (#14572)
    * chore: add mantine v8 as an alias
    
    * fix: pnpmfile checksum issue with docker build

--- packages/frontend/src/components/common/Tree/TreeItem.tsx
+++ packages/frontend/src/components/common/Tree/TreeItem.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Group, Paper, rem, Text } from '@lightdash/mantine-v7';
+import { ActionIcon, Group, Paper, rem, Text } from '@mantine-8/core';
 import {
     IconCheck,
     IconChevronDown,
@@ -79,13 +79,7 @@ const TreeItem: React.FC<Props> = ({
                 style={{ flexShrink: 0 }}
             />
 
-            <Text
-                inline
-                truncate="end"
-                fz={rem(13)}
-                fw={500}
-                style={{ flexGrow: 1 }}
-            >
+            <Text truncate="end" fz={rem(13)} fw={500} style={{ flexGrow: 1 }}>
                 {label}
             </Text>
 
