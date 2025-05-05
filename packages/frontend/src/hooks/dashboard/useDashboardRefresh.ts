68763f41f062288c6a924ed462c54654679fbf64
almeidabbm <almeidabatistam@gmail.com>
2025-05-05 11:47:36 +0100

    fix: dashboard double fetch on first refresh

--- packages/frontend/src/hooks/dashboard/useDashboardRefresh.ts
+++ packages/frontend/src/hooks/dashboard/useDashboardRefresh.ts
@@ -5,39 +5,70 @@ import {
 } from '@tanstack/react-query';
 import { useCallback, useMemo } from 'react';
 
-const QUERIES_TO_REFRESH = [
-    'dashboard_chart_ready_query',
+const DASHBOARD_RELATED_QUERIES = [
     'saved_query',
     'saved_dashboard_query',
     'dashboards',
 ];
 
-const queryPredicate = (query: Query) => {
-    return QUERIES_TO_REFRESH.some((key) => {
-        const firstQueryKey =
-            typeof query.queryKey === 'string'
-                ? query.queryKey
-                : query.queryKey?.[0];
-        return firstQueryKey === key;
-    });
+const DASHBOARD_RESULTS_QUERIES = ['dashboard_chart_ready_query'];
+
+const getPredicateFunction = (keyArray: string[]) => {
+    return (query: Query) => {
+        return keyArray.some((key) => {
+            const firstQueryKey =
+                typeof query.queryKey === 'string'
+                    ? query.queryKey
+                    : query.queryKey?.[0];
+            return firstQueryKey === key;
+        });
+    };
 };
+
+const dashboardRelatedQueryPredicate = getPredicateFunction(
+    DASHBOARD_RELATED_QUERIES,
+);
+
+const dashboardResultsQueryPredicate = getPredicateFunction(
+    DASHBOARD_RESULTS_QUERIES,
+);
 
 export const useDashboardRefresh = () => {
     const queryClient = useQueryClient();
 
-    const isFetching = useIsFetching({ predicate: queryPredicate });
+    const isFetchingDashboardRelatedQueries = useIsFetching({
+        predicate: dashboardRelatedQueryPredicate,
+    });
+
+    const isFetchingDashboardResultsQueries = useIsFetching({
+        predicate: dashboardResultsQueryPredicate,
+    });
 
     const invalidateDashboardRelatedQueries = useCallback(() => {
         return queryClient.invalidateQueries({
-            predicate: queryPredicate,
+            predicate: dashboardRelatedQueryPredicate,
+        });
+    }, [queryClient]);
+
+    const invalidateDashboardResultsQueries = useCallback(() => {
+        return queryClient.invalidateQueries({
+            predicate: dashboardResultsQueryPredicate,
         });
     }, [queryClient]);
 
     return useMemo(
         () => ({
             invalidateDashboardRelatedQueries,
-            isFetching,
+            invalidateDashboardResultsQueries,
+            isFetching:
+                isFetchingDashboardRelatedQueries +
+                isFetchingDashboardResultsQueries,
         }),
-        [invalidateDashboardRelatedQueries, isFetching],
+        [
+            invalidateDashboardRelatedQueries,
+            invalidateDashboardResultsQueries,
+            isFetchingDashboardRelatedQueries,
+            isFetchingDashboardResultsQueries,
+        ],
     );
 };
