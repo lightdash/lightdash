53e8776bd3635034dfd9077c926385b01e1e3bed
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-05-06 14:59:47 +0400

    fix: content as code with space path (#14663)

--- packages/common/src/utils/slugs.ts
+++ packages/common/src/utils/slugs.ts
@@ -22,37 +22,6 @@ export const generateSlug = (name: string) => {
     return sanitizedSlug;
 };
 
-/**
- * Get all slugs with hierarchy for a given slug
- * For example, "parent-space/child-space" will return ["parent-space", "parent-space/child-space"]
- * @param slug - The slug to get the hierarchy for
- * @returns An array of slugs with hierarchy
- */
-export const getSlugsWithHierarchy = (slug: string) =>
-    slug.split('/').reduce<string[]>((acc, s) => {
-        if (acc.length === 0) {
-            return [s];
-        }
-        return [...acc, `${acc[acc.length - 1]}/${s}`];
-    }, []);
-
-/**
- * Get the parent slug from a slug with hierarchy
- * For example, "parent-space/child-space" will return "parent-space"
- * @param slug - The slug to get the parent slug from
- * @returns The parent slug
- */
-export const getParentSlug = (slug: string) =>
-    slug.split('/').slice(0, -1).join('/');
-
-/**
- * Get the label from a slug with hierarchy
- * For example, "parent-space/child-space" will return "child-space"
- * @param slug - The slug to get the label from
- * @returns The label
- */
-export const getLabelFromSlug = (slug: string) => slug.split('/').pop() ?? slug;
-
 export const getLtreePathFromSlug = (slug: string) => {
     let path = slug;
     // This is for backwards compatibility with the old slug format that contained hierarchy
@@ -62,3 +31,9 @@ export const getLtreePathFromSlug = (slug: string) => {
 
     return path.replace(/-/g, '_');
 };
+
+export const getLtreePathFromContentAsCodePath = (path: string) =>
+    path.replace(/-/g, '_').replace(/\//g, '.');
+
+export const getContentAsCodePathFromLtreePath = (path: string) =>
+    path.replace(/_/g, '-').replace(/\./g, '/');
