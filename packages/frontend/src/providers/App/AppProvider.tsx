dbc5fa81aaf7bef2452cc7b4045899dd683fe816
Joao Viana <joao@lightdash.com>
2025-05-07 14:54:29 +0100

    chore: remove unused drawer in AppProvider

--- packages/frontend/src/providers/App/AppProvider.tsx
+++ packages/frontend/src/providers/App/AppProvider.tsx
@@ -1,5 +1,4 @@
-import { Drawer } from '@mantine/core';
-import { useState, type FC } from 'react';
+import { type FC } from 'react';
 import useHealth from '../../hooks/health/useHealth';
 import useUser from '../../hooks/user/useUser';
 import AppProviderContext from './context';
@@ -7,27 +6,15 @@ import AppProviderContext from './context';
 const AppProvider: FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const health = useHealth();
     const user = useUser(!!health?.data?.isAuthenticated);
-    const [isSupportDrawerOpen, setIsSupportDrawerOpen] = useState(false);
 
     const value = {
         health,
         user,
-        isSupportDrawerOpen,
-        setIsSupportDrawerOpen,
     };
 
     return (
         <AppProviderContext.Provider value={value}>
             {children}
-            <Drawer
-                opened={isSupportDrawerOpen}
-                onClose={() => setIsSupportDrawerOpen(false)}
-                title="Share with support"
-                position="right"
-                size="md"
-            >
-                moo
-            </Drawer>
         </AppProviderContext.Provider>
     );
 };
