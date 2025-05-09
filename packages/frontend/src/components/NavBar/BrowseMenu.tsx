dece140b01209ebed508230c50b5804542a7365a
oliverlaslett <11660098+owlas@users.noreply.github.com>
2025-05-09 21:50:45 +0100

    feat: simplify browse button

--- packages/frontend/src/components/NavBar/BrowseMenu.tsx
+++ packages/frontend/src/components/NavBar/BrowseMenu.tsx
@@ -1,121 +1,26 @@
-import { Box, Button, Center, Loader, Menu, ScrollArea } from '@mantine/core';
-import {
-    IconCategory,
-    IconChartAreaLine,
-    IconFolder,
-    IconFolders,
-    IconLayoutDashboard,
-} from '@tabler/icons-react';
+import { Button } from '@mantine/core';
+import { IconCategory } from '@tabler/icons-react';
 import { type FC } from 'react';
 import { Link } from 'react-router';
-import { useHasMetricsInCatalog } from '../../features/metricsCatalog/hooks/useMetricsCatalog';
-import { useSpaceSummaries } from '../../hooks/useSpaces';
 import MantineIcon from '../common/MantineIcon';
-import { MetricsLink } from './MetricsLink';
 
 interface Props {
     projectUuid: string;
 }
 
 const BrowseMenu: FC<Props> = ({ projectUuid }) => {
-    const { data: spaces, isInitialLoading } = useSpaceSummaries(
-        projectUuid,
-        true,
-        {
-            select: (data) => data.filter((space) => !space.parentSpaceUuid),
-        },
-    );
-    const { data: hasMetrics } = useHasMetricsInCatalog({
-        projectUuid,
-    });
-
     return (
-        <Menu
-            withArrow
-            withinPortal
-            shadow="lg"
-            position="bottom-start"
-            arrowOffset={16}
-            offset={-2}
+        <Button
+            component={Link}
+            to={`/projects/${projectUuid}/spaces`}
+            variant="default"
+            size="xs"
+            fz="sm"
+            leftIcon={<MantineIcon color="#adb5bd" icon={IconCategory} />}
         >
-            <Menu.Target>
-                <Button
-                    variant="default"
-                    size="xs"
-                    fz="sm"
-                    leftIcon={
-                        <MantineIcon color="#adb5bd" icon={IconCategory} />
-                    }
-                >
-                    Browse
-                </Button>
-            </Menu.Target>
-
-            <Menu.Dropdown>
-                <Menu.Item
-                    component={Link}
-                    to={`/projects/${projectUuid}/spaces`}
-                    icon={<MantineIcon icon={IconFolders} />}
-                >
-                    All Spaces
-                </Menu.Item>
-
-                <Menu.Item
-                    component={Link}
-                    to={`/projects/${projectUuid}/dashboards`}
-                    icon={<MantineIcon icon={IconLayoutDashboard} />}
-                >
-                    All dashboards
-                </Menu.Item>
-
-                <Menu.Item
-                    component={Link}
-                    to={`/projects/${projectUuid}/saved`}
-                    icon={<MantineIcon icon={IconChartAreaLine} />}
-                >
-                    All saved charts
-                </Menu.Item>
-
-                {!hasMetrics && (
-                    <MetricsLink projectUuid={projectUuid} asMenu />
-                )}
-
-                {isInitialLoading || (spaces && spaces.length > 0) ? (
-                    <>
-                        <Menu.Divider />
-                        <Menu.Label>Spaces</Menu.Label>
-
-                        {isInitialLoading ? (
-                            <Center my="sm">
-                                <Loader size="sm" color="gray" />
-                            </Center>
-                        ) : null}
-                    </>
-                ) : null}
-
-                <ScrollArea
-                    variant="primary"
-                    className="only-vertical"
-                    scrollbarSize={6}
-                    type="hover"
-                >
-                    <Box mah={300}>
-                        {spaces
-                            ?.sort((a, b) => a.name.localeCompare(b.name))
-                            .map((space) => (
-                                <Menu.Item
-                                    key={space.uuid}
-                                    component={Link}
-                                    to={`/projects/${projectUuid}/spaces/${space.uuid}`}
-                                    icon={<MantineIcon icon={IconFolder} />}
-                                >
-                                    {space.name}
-                                </Menu.Item>
-                            ))}
-                    </Box>
-                </ScrollArea>
-            </Menu.Dropdown>
-        </Menu>
+            Browse
+        </Button>
     );
 };
+
 export default BrowseMenu;
