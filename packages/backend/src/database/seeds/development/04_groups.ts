fd5a31d98afd711ba82843c8865295fea0887c3c
Giorgi Bagdavadze <notgiorgi@gmail.com>
2025-05-09 16:20:52 +0400

    chore: improve seeder to create a group with a member

--- packages/backend/src/database/seeds/development/04_groups.ts
+++ packages/backend/src/database/seeds/development/04_groups.ts
@@ -1,4 +1,10 @@
-import { SEED_GROUP, SEED_ORG_1, SEED_ORG_1_ADMIN } from '@lightdash/common';
+import {
+    SEED_GROUP,
+    SEED_GROUP_2,
+    SEED_ORG_1,
+    SEED_ORG_1_ADMIN,
+    SEED_ORG_1_EDITOR,
+} from '@lightdash/common';
 import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
@@ -12,5 +18,22 @@ export async function seed(knex: Knex): Promise<void> {
         organization_id: org.organization_id,
         created_by_user_uuid: SEED_ORG_1_ADMIN.user_uuid,
         updated_by_user_uuid: SEED_ORG_1_ADMIN.user_uuid,
+    });
+    await knex('groups').insert({
+        group_uuid: SEED_GROUP_2.groupUuid,
+        name: SEED_GROUP_2.name,
+        organization_id: org.organization_id,
+        created_by_user_uuid: SEED_ORG_1_ADMIN.user_uuid,
+        updated_by_user_uuid: SEED_ORG_1_ADMIN.user_uuid,
+    });
+
+    const [editor] = await knex('users')
+        .select('user_id')
+        .where('user_uuid', SEED_ORG_1_EDITOR.user_uuid);
+
+    await knex('group_memberships').insert({
+        group_uuid: SEED_GROUP_2.groupUuid,
+        user_id: editor.user_id,
+        organization_id: org.organization_id,
     });
 }
