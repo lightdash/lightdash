d928f638c009dc7074f8849d749d39e95df3f3bb
Jose Rego <jose@lightdash.com>
2025-05-20 17:39:19 +0100

    fix: cache invalidation after save

--- packages/frontend/src/features/sqlRunner/hooks/useSavedSqlChartResults.tsx
+++ packages/frontend/src/features/sqlRunner/hooks/useSavedSqlChartResults.tsx
@@ -78,7 +78,7 @@ export const useSavedSqlChartResults = (
         },
         Partial<ApiError>
     >(
-        ['savedSqlChartResults', args],
+        ['savedSqlChartResults', savedSqlUuid ?? slug, args], // keep uuid/slug in the key to facilitate cache invalidation
         async () => {
             // Safe to assume these are defined because of the enabled flag
             const chart = chartQuery.data!;
