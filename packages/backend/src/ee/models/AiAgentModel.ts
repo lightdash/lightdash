73e90a5a6b314803941bb86d843f89e9f8aecef0
Giorgi Bagdavadze <12987425+notgiorgi@users.noreply.github.com>
2025-05-21 16:48:06 +0400

    feat: SlackBot with ai agents (#14904)
    * feat: adds ai agent model
    
    * feat: agent configuration instead of project-slack mappings
    
    ---------
    
    Co-authored-by: Irakli Janiashvili <irakli.janiashvili@gmail.com>

--- packages/backend/src/ee/models/AiAgentModel.ts
+++ packages/backend/src/ee/models/AiAgentModel.ts
@@ -1,4 +1,11 @@
+import { type AiAgent } from '@lightdash/common';
 import { Knex } from 'knex';
+import {
+    AiAgentIntegrationTableName,
+    AiAgentSlackIntegrationTableName,
+    AiAgentTableName,
+} from '../database/entities/aiAgent';
+import { AiAgentNotFoundError } from '../services/AiService/utils/errors';
 
 type Dependencies = {
     database: Knex;
@@ -9,5 +16,54 @@ export class AiAgentModel {
 
     constructor(dependencies: Dependencies) {
         this.database = dependencies.database;
+    }
+
+    async getAgentBySlackChannelId({
+        organizationUuid,
+        slackChannelId,
+    }: {
+        organizationUuid: string;
+        slackChannelId: string;
+    }): Promise<AiAgent> {
+        // TODO: when `getAgent(uuid: string) is implemented, this should use it instead
+        const agent = await this.database(AiAgentTableName)
+            .select({
+                uuid: `${AiAgentTableName}.ai_agent_uuid`,
+                organizationUuid: `${AiAgentTableName}.organization_uuid`,
+                projectUuid: `${AiAgentTableName}.project_uuid`,
+                tags: `${AiAgentTableName}.tags`,
+                integrations: this.database.raw(`
+                    json_agg(
+                        json_build_object(
+                            'type', ${AiAgentIntegrationTableName}.integration_type,
+                            'channelId', ${AiAgentSlackIntegrationTableName}.slack_channel_id
+                        )
+                    )`),
+            } satisfies Record<keyof AiAgent, unknown>)
+            .join(
+                AiAgentIntegrationTableName,
+                `${AiAgentTableName}.ai_agent_uuid`,
+                `${AiAgentIntegrationTableName}.ai_agent_uuid`,
+            )
+            .join(
+                AiAgentSlackIntegrationTableName,
+                `${AiAgentIntegrationTableName}.ai_agent_integration_uuid`,
+                `${AiAgentSlackIntegrationTableName}.ai_agent_integration_uuid`,
+            )
+            .where({
+                [`${AiAgentTableName}.organization_uuid`]: organizationUuid,
+                [`${AiAgentSlackIntegrationTableName}.slack_channel_id`]:
+                    slackChannelId,
+            })
+            .groupBy(`${AiAgentTableName}.ai_agent_uuid`)
+            .first();
+
+        if (!agent) {
+            throw new AiAgentNotFoundError(
+                `AI agent not found for slack channel ID: ${slackChannelId}`,
+            );
+        }
+
+        return agent;
     }
 }
