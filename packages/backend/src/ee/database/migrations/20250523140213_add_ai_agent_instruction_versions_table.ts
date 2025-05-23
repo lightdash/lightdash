a1ac63f1a09bc5d3324e14c7a2abc34a22d6c660
Tatiana Inama <tatiana@lightdash.com>
2025-05-23 17:07:07 +0200

    feat: new instruction versions table

---
+++ packages/backend/src/ee/database/migrations/20250523140213_add_ai_agent_instruction_versions_table.ts
@@ -0,0 +1,44 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('ai_agent', (table) => {
+        table.timestamp('last_instruction_version_updated_at').nullable();
+        table.uuid('last_instruction_version_updated_by_user_uuid').nullable();
+    });
+
+    await knex.schema.createTable('ai_agent_instruction_versions', (table) => {
+        table
+            .uuid('ai_agent_instruction_version_uuid')
+            .primary()
+            .defaultTo(knex.raw('uuid_generate_v4()'));
+        table
+            .uuid('ai_agent_uuid')
+            .references('ai_agent_uuid')
+            .inTable('ai_agent')
+            .onDelete('CASCADE')
+            .notNullable();
+        table.text('instruction').notNullable();
+        table
+            .timestamp('created_at', { useTz: false })
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table
+            .uuid('created_by_user_uuid')
+            .references('user_uuid')
+            .inTable('users')
+            .onDelete('SET NULL')
+            .notNullable();
+
+        table.index('ai_agent_uuid');
+        table.index('created_at');
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('ai_agent_instruction_versions');
+
+    await knex.schema.alterTable('ai_agent', (table) => {
+        table.dropColumn('last_instruction_version_updated_at');
+        table.dropColumn('last_instruction_version_updated_by_user_uuid');
+    });
+}
