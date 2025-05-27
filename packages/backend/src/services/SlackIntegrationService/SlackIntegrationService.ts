26c1495a0adba0212b5c80e0365310b54242258d
Giorgi Bagdavadze <12987425+notgiorgi@users.noreply.github.com>
2025-05-27 17:18:55 +0400

    fix: Slack integration api improvements (#15011)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    ### Description:
    
    Added permission checks to Slack integration methods to ensure users have appropriate access rights

--- packages/backend/src/services/SlackIntegrationService/SlackIntegrationService.ts
+++ packages/backend/src/services/SlackIntegrationService/SlackIntegrationService.ts
@@ -32,6 +32,10 @@ export class SlackIntegrationService<
         const organizationUuid = user?.organizationUuid;
         if (!organizationUuid) throw new ForbiddenError();
 
+        if (user.ability.cannot('view', 'Organization')) {
+            throw new ForbiddenError();
+        }
+
         const installation =
             await this.slackAuthenticationModel.getInstallationFromOrganizationUuid(
                 organizationUuid,
@@ -53,6 +57,11 @@ export class SlackIntegrationService<
     async deleteInstallationFromOrganizationUuid(user: SessionUser) {
         const organizationUuid = user?.organizationUuid;
         if (!organizationUuid) throw new ForbiddenError();
+
+        if (user.ability.cannot('manage', 'Organization')) {
+            throw new ForbiddenError();
+        }
+
         await this.slackAuthenticationModel.deleteInstallationFromOrganizationUuid(
             organizationUuid,
         );
