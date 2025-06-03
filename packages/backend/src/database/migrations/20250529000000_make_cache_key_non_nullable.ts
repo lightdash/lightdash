3ebe3e1a908db270bdaf3564007c36a38d010744
Jose Rego <ZeRego@users.noreply.github.com>
2025-06-03 09:04:00 +0100

    fix: reorder migration steps to index cache_key before making it non-nullable (#15128)

--- packages/backend/src/database/migrations/20250529000000_make_cache_key_non_nullable.ts
+++ packages/backend/src/database/migrations/20250529000000_make_cache_key_non_nullable.ts
@@ -3,7 +3,12 @@ import { Knex } from 'knex';
 const QUERY_HISTORY_TABLE = 'query_history';
 
 export async function up(knex: Knex): Promise<void> {
-    // First update all null values to 'n/a'
+    // Ensure the column is indexed
+    await knex.schema.alterTable(QUERY_HISTORY_TABLE, (table) => {
+        table.index(['cache_key']);
+    });
+
+    // Second, update all null values to 'n/a'
     await knex(QUERY_HISTORY_TABLE)
         .update({
             // @ts-ignore ignore update type
@@ -14,11 +19,6 @@ export async function up(knex: Knex): Promise<void> {
     // Then alter the column to be non-nullable
     await knex.schema.alterTable(QUERY_HISTORY_TABLE, (table) => {
         table.string('cache_key').notNullable().alter();
-    });
-
-    // Ensure the column is indexed
-    await knex.schema.alterTable(QUERY_HISTORY_TABLE, (table) => {
-        table.index(['cache_key']);
     });
 }
 
