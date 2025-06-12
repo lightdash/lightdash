5efc69046cb25b849fcbc2f1947e0bdb876d2afa
Yegor Borisenco <pimmey@pimmey.com>
2025-06-12 17:39:23 +0200

    fix: change deprecation date to a Monday

--- packages/backend/src/middlewares/deprecation.ts
+++ packages/backend/src/middlewares/deprecation.ts
@@ -1,6 +1,6 @@
 import { getDeprecatedRouteMiddleware } from '../controllers/authentication';
 
 export const deprecatedDownloadCsvRoute = getDeprecatedRouteMiddleware(
-    new Date('2025-07-12'),
+    new Date('2025-07-14'),
     `Please use 'POST /api/v2/projects/{projectUuid}/query/{queryUuid}/download' instead.`,
 );
