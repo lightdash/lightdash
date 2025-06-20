b94b14d2acdcb9339d099eadd83de7983bf6ff03
Javier Rengel <rephus@gmail.com>
2025-06-20 15:57:12 +0200

    M  packages/backend/src/ee/index.ts

--- packages/backend/src/ee/index.ts
+++ packages/backend/src/ee/index.ts
@@ -244,8 +244,6 @@ export async function getEnterpriseAppArguments(): Promise<EnterpriseAppArgument
                 }),
             slackAuthenticationModel: ({ database }) =>
                 new CommercialSlackAuthenticationModel({ database }),
-            serviceAccountModel: ({ database }) =>
-                new ServiceAccountModel({ database }),
             featureFlagModel: ({ database }) =>
                 new CommercialFeatureFlagModel({ database, lightdashConfig }),
         },
