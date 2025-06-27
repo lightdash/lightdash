fa2fb14c8784b1c952fedd72c3c9d854fffe5aa1
Jose Rego <jose@lightdash.com>
2025-06-27 17:16:46 +0100

    chore: install dbt dependencies in PR env

--- examples/full-jaffle-shop-demo/renderDeployHook.sh
+++ examples/full-jaffle-shop-demo/renderDeployHook.sh
@@ -2,8 +2,9 @@
 set -e
 
 # Idempotent dbt commands
-dbt seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
-dbt run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
+dbt1.7 deps --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles
+dbt1.7 seed --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh
+dbt1.7 run --project-dir /usr/app/dbt --profiles-dir /usr/app/profiles --full-refresh --exclude fanouts_sales_targets # TODO: remove once we fix SQL in package
 
 # Rollback all migrations and seed
 pnpm -F backend rollback-all-production
