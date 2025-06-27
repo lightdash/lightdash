14ab8f868489559af2c509d8de6c480bc5b38729
Jose Rego <jose@lightdash.com>
2025-06-27 16:02:27 +0100

    chore: add saas demo to seed project

--- scripts/seed-jaffle.sh
+++ scripts/seed-jaffle.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-dbt seed \
+dbt1.7 deps \
+  --project-dir examples/full-jaffle-shop-demo/dbt \
+  --profiles-dir examples/full-jaffle-shop-demo/profiles
+
+dbt1.7 seed \
   --project-dir examples/full-jaffle-shop-demo/dbt \
   --profiles-dir examples/full-jaffle-shop-demo/profiles \
   --full-refresh
-dbt run \
+dbt1.7 run \
   --project-dir examples/full-jaffle-shop-demo/dbt \
-  --profiles-dir examples/full-jaffle-shop-demo/profiles
+  --profiles-dir examples/full-jaffle-shop-demo/profiles \
+  --full-refresh \
+  --exclude fanouts_sales_targets # TODO: remove once we fix SQL in package
