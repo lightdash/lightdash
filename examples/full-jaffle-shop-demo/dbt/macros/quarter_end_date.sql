f9e5406638a0f363251e7d2968bb5d03ee0877d6
Bruno Almeida <almeidabbm@users.noreply.github.com>
2025-07-07 13:39:43 +0100

    fix(test): create project tests were failing on redshift (#15740)

--- examples/full-jaffle-shop-demo/dbt/macros/quarter_end_date.sql
+++ examples/full-jaffle-shop-demo/dbt/macros/quarter_end_date.sql
@@ -1,7 +1,7 @@
 {% macro quarter_end_date(quarter_start_column) %}
   {% if target.type == 'bigquery' %}
     DATE_ADD(DATE_ADD({{ quarter_start_column }}, INTERVAL 3 MONTH), INTERVAL -1 DAY)
-  {% elif target.type == 'postgres' %}
+  {% elif target.type == 'postgres' or target.type == 'redshift' %}
     ({{ quarter_start_column }} + INTERVAL '3 months' - INTERVAL '1 day')::date
   {% elif target.type == 'snowflake' %}
     DATEADD(day, -1, DATEADD(month, 3, {{ quarter_start_column }}))
