85bf30e8305f08e5d4a70fb758b212fa79fb9e4b
almeidabbm <almeidabatistam@gmail.com>
2025-07-07 11:40:56 +0100

    fix(test): create project tests were failing on redshift

--- packages/e2e/cypress/e2e/app/createProjects.cy.ts
+++ packages/e2e/cypress/e2e/app/createProjects.cy.ts
@@ -169,7 +169,7 @@ const testCompile = (): Cypress.Chainable<string> => {
     cy.contains('Step 2/3', { timeout: 60000 });
     cy.contains('Successfully synced dbt project!', { timeout: 60000 });
 
-    cy.contains('selected 11 models');
+    cy.contains('selected 20 models');
     // Configure
     cy.contains('button', 'Save changes').click();
     cy.url().should('include', '/home', { timeout: 30000 });
