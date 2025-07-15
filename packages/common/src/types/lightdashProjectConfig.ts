6aacde6e962016b9f999aee83d1f8ffa5e8ea0ae
Jose Rego <jose@lightdash.com>
2025-07-15 11:17:02 +0100

    chore: parameters types

--- packages/common/src/types/lightdashProjectConfig.ts
+++ packages/common/src/types/lightdashProjectConfig.ts
@@ -10,8 +10,15 @@ type SpotlightConfig = {
     };
 };
 
+type LightdashParameter = {
+    label: string;
+    description?: string;
+    options?: string[];
+};
+
 export type LightdashProjectConfig = {
     spotlight: SpotlightConfig;
+    parameters?: Record<string, LightdashParameter>; // keys must be ^[a-zA-Z0-9_-]+$
 };
 
 export const DEFAULT_SPOTLIGHT_CONFIG: SpotlightConfig = {
