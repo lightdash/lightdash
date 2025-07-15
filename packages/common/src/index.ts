9f6d3471b5e1a0d7015cb7249aef925363700c71
almeidabbm <almeidabatistam@gmail.com>
2025-07-15 13:32:28 +0100

    chore: introduce values map type for parameters

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -239,6 +239,7 @@ export * from './types/openIdIdentity';
 export * from './types/organization';
 export * from './types/organizationMemberProfile';
 export * from './types/paginateResults';
+export * from './types/parameters';
 export * from './types/personalAccessToken';
 export * from './types/pinning';
 export * from './types/pivot';
