34830d341bc60016b33e197f42c534b1ff09f335
almeidabbm <almeidabatistam@gmail.com>
2025-07-16 11:10:39 +0100

    fix: filter replacements

--- packages/backend/src/utils/QueryBuilder/parameters.ts
+++ packages/backend/src/utils/QueryBuilder/parameters.ts
@@ -1,18 +1,18 @@
-import type { ParametersValuesMap } from '@lightdash/common';
+import type {
+    ParametersValuesMap,
+    WarehouseSqlBuilder,
+} from '@lightdash/common';
 import { parameterRegex } from '@lightdash/common/src/compiler/parameters';
 import { replaceLightdashValues } from './utils';
 
+// We need this function so that we can replace when there's no access to the sql builder (e.g. in query builder)
 export const replaceParameters = (
     sql: string,
     parameters: ParametersValuesMap,
     quoteChar: string,
     wrapChar: string = '', // ! Default to non-wrapped sql
-) => {
-    if (!quoteChar) {
-        throw new Error('Quote character is required');
-    }
-
-    return replaceLightdashValues(
+) =>
+    replaceLightdashValues(
         parameterRegex,
         sql,
         parameters,
@@ -20,4 +20,14 @@ export const replaceParameters = (
         wrapChar,
         'parameter',
     );
-};
+
+export const replaceParametersAsString = (
+    sql: string,
+    parameters: ParametersValuesMap,
+    sqlBuilder: WarehouseSqlBuilder,
+) => replaceParameters(sql, parameters, sqlBuilder.getStringQuoteChar(), '');
+
+export const replaceParametersAsRaw = (
+    sql: string,
+    parameters: ParametersValuesMap,
+) => replaceParameters(sql, parameters, '', '');
