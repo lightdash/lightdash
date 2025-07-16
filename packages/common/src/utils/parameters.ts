5fbe2195191159ca7cf7a851ca14647c75275e9d
Marshall Agnew <marshall@lightdash.com>
2025-07-16 11:22:01 +0100

    chore: add tables and joins to get params

--- packages/common/src/utils/parameters.ts
+++ packages/common/src/utils/parameters.ts
@@ -16,6 +16,7 @@ export function getParameterReferencesFromMetricQuery(
     ];
     const exploreItemsMap = getItemMap(explore);
     const result = new Set<string>();
+    // Add parameter references from active fields
     for (const fieldId of activeFields) {
         const item: Item | undefined = exploreItemsMap[fieldId];
         if (isField(item) && Array.isArray(item.parameterReferences)) {
@@ -32,6 +33,20 @@ export function getParameterReferencesFromMetricQuery(
                 item as { parameterReferences?: string[] }
             ).parameterReferences!.forEach((ref) => result.add(ref));
         }
+    }
+    // Add parameter references from base table
+    const baseTableRefs =
+        explore.tables[explore.baseTable]?.parameterReferences;
+    if (Array.isArray(baseTableRefs)) {
+        baseTableRefs.forEach((ref) => result.add(ref));
+    }
+    // Add parameter references from joined tables
+    if (Array.isArray(explore.joinedTables)) {
+        explore.joinedTables.forEach((jt) => {
+            if (Array.isArray(jt.parameterReferences)) {
+                jt.parameterReferences.forEach((ref) => result.add(ref));
+            }
+        });
     }
     return Array.from(result);
 }
