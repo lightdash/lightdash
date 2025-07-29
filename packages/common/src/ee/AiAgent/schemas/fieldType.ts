3b6c128d6b8d85cd764407fc2877426e9edf42f7
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-07-29 21:07:09 +0000

    refactor: restructure filter schemas with type literals for better validation

--- packages/common/src/ee/AiAgent/schemas/fieldType.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { DimensionType, MetricType } from '../../../types/field';
-
-const dimensionTypeSchema = z.nativeEnum(DimensionType);
-const metricTypeSchema = z.nativeEnum(MetricType);
-
-const fieldTypeSchema = z.union([dimensionTypeSchema, metricTypeSchema]);
-
-export default fieldTypeSchema;
--- packages/common/src/ee/AiAgent/schemas/filters/numberFilters.ts
+++ packages/common/src/ee/AiAgent/schemas/filters/numberFilters.ts
@@ -1,21 +1,40 @@
 import { z } from 'zod';
-import { FilterOperator } from '../../../../types/filter';
+import { DimensionType, MetricType } from '../../../../types/field';
+import { FilterOperator, FilterType } from '../../../../types/filter';
+import { getFieldIdSchema } from '../fieldId';
+
+const commonNumberFilterRuleSchema = z.object({
+    fieldId: getFieldIdSchema({ additionalDescription: null }),
+    fieldType: z.union([
+        z.literal(DimensionType.NUMBER),
+        z.literal(MetricType.NUMBER),
+        z.literal(MetricType.PERCENTILE),
+        z.literal(MetricType.MEDIAN),
+        z.literal(MetricType.AVERAGE),
+        z.literal(MetricType.COUNT),
+        z.literal(MetricType.COUNT_DISTINCT),
+        z.literal(MetricType.SUM),
+        z.literal(MetricType.MIN),
+        z.literal(MetricType.MAX),
+    ]),
+    fieldFilterType: z.literal(FilterType.NUMBER),
+});
 
 const numberFilterSchema = z.union([
-    z.object({
+    commonNumberFilterRuleSchema.extend({
         operator: z.union([
             z.literal(FilterOperator.NULL),
             z.literal(FilterOperator.NOT_NULL),
         ]),
     }),
-    z.object({
+    commonNumberFilterRuleSchema.extend({
         operator: z.union([
             z.literal(FilterOperator.EQUALS),
             z.literal(FilterOperator.NOT_EQUALS),
         ]),
         values: z.array(z.number()),
     }),
-    z.object({
+    commonNumberFilterRuleSchema.extend({
         operator: z.union([
             z.literal(FilterOperator.LESS_THAN),
             z.literal(FilterOperator.LESS_THAN_OR_EQUAL),
@@ -24,7 +43,7 @@ const numberFilterSchema = z.union([
         ]),
         values: z.array(z.number()).length(1),
     }),
-    z.object({
+    commonNumberFilterRuleSchema.extend({
         operator: z.union([
             z.literal(FilterOperator.IN_BETWEEN),
             z.literal(FilterOperator.NOT_IN_BETWEEN),
