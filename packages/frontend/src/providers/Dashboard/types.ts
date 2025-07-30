8254b4acf0695a6ea907a5bf3ed896e8ec36d5c9
Marshall Agnew <marshall@lightdash.com>
2025-07-30 17:00:14 +0200

    feat: save and load parameters on dashboards

--- packages/frontend/src/providers/Dashboard/types.ts
+++ packages/frontend/src/providers/Dashboard/types.ts
@@ -95,6 +95,8 @@ export type DashboardContextType = {
     requiredDashboardFilters: Pick<DashboardFilterRule, 'id' | 'label'>[];
     isDateZoomDisabled: boolean;
     setIsDateZoomDisabled: Dispatch<SetStateAction<boolean>>;
+    setSavedParameters: Dispatch<SetStateAction<DashboardParameters>>;
+    parametersHaveChanged: boolean;
     dashboardParameters: DashboardParameters;
     parameterValues: ParametersValuesMap;
     clearAllParameters: () => void;
