c13bdc200f1d1ca999edf40b925fe0e981995496
joesuomie@gmail.com <joesuomie@gmail.com>
2025-08-01 14:58:16 +0000

    Data Connections Components

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -15,7 +15,7 @@ import ChartHistory from './pages/ChartHistory';
 import CreateProject from './pages/CreateProject';
 import CreateProjectSettings from './pages/CreateProjectSettings';
 import Dashboard from './pages/Dashboard';
-import DataIngestion from './pages/DataIngestion';
+import DataIngestion from './pages/Connections';
 import Explorer from './pages/Explorer';
 import Home from './pages/Home';
 import Invite from './pages/Invite';
