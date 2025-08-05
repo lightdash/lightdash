ccee4bf7447e5feb70050d7d945c7e27f9ea58ab
Tatiana Inama <tatiana@lightdash.com>
2025-08-05 10:55:22 +0200

    fix: range input filter properly indicates missing values (#16152)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    Closes: #16043
    
    ### Description:
    Improved `FilterNumberRangeInput` validation. The component now validates that both min and max values are provided in addition to the current min/max validation.
    I've also improved how the error message is rendered
    
    <details><summary> screenshots 📸 </summary>
    <p>
    
    <img width="899" height="836" alt="image" src="https://github.com/user-attachments/assets/d07cac10-4b6b-4651-a6b2-a7a61dd10a7c" />
    
    <img width="899" height="836" alt="image" src="https://github.com/user-attachments/assets/e95b0092-805b-4341-a43b-a3a118a35635" />
    
    <img width="899" height="836" alt="image" src="https://github.com/user-attachments/assets/c36a10b1-3458-483f-91c3-d5a1b0e6a291" />
    
    
    </p>
    </details>

--- packages/frontend/src/components/common/Filters/FilterInputs/FilterNumberRangeInput.tsx
+++ packages/frontend/src/components/common/Filters/FilterInputs/FilterNumberRangeInput.tsx
@@ -1,11 +1,32 @@
-import { Group, Text, type TextInputProps } from '@mantine/core';
+import { Group, Stack, Text, type TextInputProps } from '@mantine/core';
+import { isNil } from 'lodash';
 import { type FC } from 'react';
+import z from 'zod';
 import FilterNumberInput from './FilterNumberInput';
 
 interface Props extends Omit<TextInputProps, 'type' | 'value' | 'onChange'> {
     value?: unknown[];
     onChange: (value: unknown[]) => void;
 }
+
+const numberRangeSchema = z
+    .tuple([z.number().nullable().optional(), z.number().nullable().optional()])
+    .superRefine(([min, max], ctx) => {
+        if (isNil(min) || isNil(max)) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: 'Both values are required',
+                fatal: true,
+            });
+            return z.NEVER;
+        }
+        if (min > max) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: 'Minimum should be less than the maximum',
+            });
+        }
+    });
 
 const FilterNumberRangeInput: FC<Props> = ({
     value,
@@ -15,44 +36,52 @@ const FilterNumberRangeInput: FC<Props> = ({
     autoFocus,
     ...rest
 }) => {
-    const isInvalid = !(
-        value?.[0] == null ||
-        value?.[1] == null ||
-        value[0] < value[1]
-    );
+    const validationResult = numberRangeSchema.safeParse(value);
+    const errorMessage = validationResult.error
+        ? validationResult.error.issues[0].message // only show one issue at a time
+        : undefined;
+
     return (
-        <Group noWrap align="start" w="100%" spacing="xs">
-            <FilterNumberInput
-                error={
-                    isInvalid
-                        ? 'Minimum should be less than the maximum'
-                        : undefined
-                }
-                disabled={disabled}
-                autoFocus={true}
-                placeholder="Min value"
-                {...rest}
-                value={value?.[0]}
-                onChange={(newValue) => {
-                    onChange([newValue, value?.[1]]);
-                }}
-            />
+        <Stack spacing={2} w="100%">
+            <Group noWrap align="start" spacing="xs">
+                <FilterNumberInput
+                    error={!!errorMessage}
+                    disabled={disabled}
+                    autoFocus={true}
+                    placeholder="Min value"
+                    {...rest}
+                    value={value?.[0]}
+                    onChange={(newValue) => {
+                        onChange([newValue, value?.[1]]);
+                    }}
+                />
 
-            <Text color="dimmed" mt={7} sx={{ whiteSpace: 'nowrap' }} size="xs">
-                –
-            </Text>
+                <Text
+                    color="dimmed"
+                    mt={7}
+                    sx={{ whiteSpace: 'nowrap' }}
+                    size="xs"
+                >
+                    –
+                </Text>
 
-            <FilterNumberInput
-                error={isInvalid}
-                disabled={disabled}
-                placeholder="Max value"
-                {...rest}
-                value={value?.[1]}
-                onChange={(newValue) => {
-                    onChange([value?.[0], newValue]);
-                }}
-            />
-        </Group>
+                <FilterNumberInput
+                    error={!!errorMessage}
+                    disabled={disabled}
+                    placeholder="Max value"
+                    {...rest}
+                    value={value?.[1]}
+                    onChange={(newValue) => {
+                        onChange([value?.[0], newValue]);
+                    }}
+                />
+            </Group>
+            {errorMessage && (
+                <Text color="red" size="xs">
+                    {errorMessage}
+                </Text>
+            )}
+        </Stack>
     );
 };
 
