e776c31a9ff0c37b45e0814cb82fa31d791ff6ed
Giorgi Bagdavadze <12987425+notgiorgi@users.noreply.github.com>
2025-08-07 12:21:04 +0400

    feat: Add SpotlightJS for local traces (#16240)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    ### Description:
    
    Using Spotlightjs you can have all the Sentry features but locally. Very helpful for debugging.
    
    To enable:
    
    ```
    SENTRY_SPOTLIGHT=1
    AI_COPILOT_TELEMETRY_ENABLED=true # optional but useful for working with AI Agents
    ```
    
    [CleanShot 2025-08-06 at 17.39.25.mp4 <span class="graphite__hidden">(uploaded via Graphite)</span> <img class="graphite__hidden" src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/SRLEqMEevAzoFwvhnfeq/ed48646e-c853-4104-aa72-a3a5d183d60f.mp4" />](https://app.graphite.dev/media/video/SRLEqMEevAzoFwvhnfeq/ed48646e-c853-4104-aa72-a3a5d183d60f.mp4)

--- packages/backend/src/ee/services/ai/types/aiAgent.ts
+++ packages/backend/src/ee/services/ai/types/aiAgent.ts
@@ -24,6 +24,7 @@ export type AiAgentArgs = {
     organizationId: string;
     userId: string;
     debugLoggingEnabled: boolean;
+    telemetryEnabled: boolean;
 
     availableExploresPageSize: number;
     findExploresPageSize: number;
