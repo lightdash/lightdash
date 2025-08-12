a1e29582de84ec1434a41e17c5b36c9cfe294548
Tatiana Inama <tatiana@lightdash.com>
2025-08-12 16:26:15 +0200

    feat: update agent edit ui (#15941)
    ### Description:
    
    Updated the Edit/Create Analyst page. 
    - Removed `delete` button from header, created a 'Danger Zone' section instead
    - Added sticky header to improve visibility when using smaller screens
    
    [Screen Recording 2025-07-22 at 16.50.24.mov <span class="graphite__hidden">(uploaded via Graphite)</span> <img class="graphite__hidden" src="https://app.graphite.dev/api/v1/graphite/video/thumbnail/HorFj8gm8QUuuIDE2vgk/46b4f78e-e777-4f2c-8c22-64c6c6acd06a.mov" />](https://app.graphite.dev/media/video/HorFj8gm8QUuuIDE2vgk/46b4f78e-e777-4f2c-8c22-64c6c6acd06a.mov)

---
+++ packages/frontend/src/ee/features/aiCopilot/components/AiAgentEditPageLayout/AiAgentEditPageLayout.tsx
@@ -0,0 +1,56 @@
+import { Box, Container } from '@mantine-8/core';
+import { useElementSize, useHeadroom } from '@mantine-8/hooks';
+import {
+    forwardRef,
+    useCallback,
+    useState,
+    type FC,
+    type PropsWithChildren,
+} from 'react';
+import { NAVBAR_HEIGHT } from '../../../../../components/common/Page/constants';
+import styles from './AiAgentEditPageLayout.module.css';
+
+const AiAgentHeader = forwardRef<HTMLDivElement, PropsWithChildren>(
+    ({ children }, ref) => {
+        const [fixed, setFixed] = useState(true);
+        const handleFix = useCallback(() => setFixed(true), []);
+        const handlePin = useCallback(() => setFixed(false), []);
+
+        const pinned = useHeadroom({
+            fixedAt: 10,
+            onFix: handleFix,
+            onPin: handlePin,
+        });
+
+        return (
+            <Box
+                ref={ref}
+                className={styles.aiAgentEditPageLayoutHeader}
+                data-pinned={pinned}
+                data-fixed={fixed}
+                style={{ top: NAVBAR_HEIGHT }}
+            >
+                <Container px="xxl" py="sm">
+                    {children}
+                </Container>
+            </Box>
+        );
+    },
+);
+
+type Props = {
+    children: React.ReactNode;
+    header?: React.ReactNode;
+};
+export const AiAgentEditPageLayout: FC<Props> = ({ header, children }) => {
+    const headerElement = useElementSize();
+
+    return (
+        <Container pb="xxl">
+            {header && (
+                <AiAgentHeader ref={headerElement.ref}>{header}</AiAgentHeader>
+            )}
+            <Box mt={header ? headerElement.height : 0}>{children}</Box>
+        </Container>
+    );
+};
