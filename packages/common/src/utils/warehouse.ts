93471d3b7cd64c2c20f58615ed348d9406265fdf
almeidabbm <almeidabatistam@gmail.com>
2025-08-22 11:33:19 +0100

    feat: move pivot sql builder to it's own class

--- packages/common/src/utils/warehouse.ts
+++ packages/common/src/utils/warehouse.ts
@@ -1,4 +1,6 @@
+import { SupportedDbtAdapter } from '../types/dbt';
 import { WarehouseTypes } from '../types/projects';
+import type { WarehouseSqlBuilder } from '../types/warehouse';
 import { VizAggregationOptions } from '../visualizations/types';
 import assertUnreachable from './assertUnreachable';
 
@@ -30,36 +32,35 @@ export const getFieldQuoteChar = (
 };
 
 export const getAggregatedField = (
-    warehouseType: WarehouseTypes,
+    warehouseSqlBuilder: WarehouseSqlBuilder,
     aggregation: VizAggregationOptions,
     reference: string,
 ): string => {
-    const q = getFieldQuoteChar(warehouseType);
-    if (warehouseType) {
-        switch (warehouseType) {
-            case WarehouseTypes.BIGQUERY:
-            case WarehouseTypes.DATABRICKS:
-            case WarehouseTypes.SNOWFLAKE:
-            case WarehouseTypes.REDSHIFT:
-            case WarehouseTypes.TRINO:
-                const aggregationFunction =
-                    aggregation === VizAggregationOptions.ANY
-                        ? 'ANY_VALUE'
-                        : aggregation;
-                return `${aggregationFunction}(${q}${reference}${q})`;
+    const q = warehouseSqlBuilder.getFieldQuoteChar();
+    const adapterType = warehouseSqlBuilder.getAdapterType();
+    switch (adapterType) {
+        case SupportedDbtAdapter.BIGQUERY:
+        case SupportedDbtAdapter.DATABRICKS:
+        case SupportedDbtAdapter.SNOWFLAKE:
+        case SupportedDbtAdapter.REDSHIFT:
+        case SupportedDbtAdapter.TRINO:
+            const aggregationFunction =
+                aggregation === VizAggregationOptions.ANY
+                    ? 'ANY_VALUE'
+                    : aggregation;
+            return `${aggregationFunction}(${q}${reference}${q})`;
 
-            case WarehouseTypes.POSTGRES:
-                if (aggregation === VizAggregationOptions.ANY) {
-                    // ANY_VALUE on Postgres is only available from version v16+
-                    return `(ARRAY_AGG(${q}${reference}${q}))[1]`;
-                }
-                break;
-            default:
-                return assertUnreachable(
-                    warehouseType,
-                    `Unknown warehouse type ${warehouseType}`,
-                );
-        }
+        case SupportedDbtAdapter.POSTGRES:
+            if (aggregation === VizAggregationOptions.ANY) {
+                // ANY_VALUE on Postgres is only available from version v16+
+                return `(ARRAY_AGG(${q}${reference}${q}))[1]`;
+            }
+            break;
+        default:
+            return assertUnreachable(
+                adapterType,
+                `Unknown warehouse type ${adapterType}`,
+            );
     }
     return `${aggregation}(${q}${reference}${q})`;
 };
