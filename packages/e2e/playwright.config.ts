f30bed00e204608fe0278b4bce2e6a44af508f16
Jose Rego <jose@lightdash.com>
2025-09-04 10:23:09 +0100

    chore: github reporter

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
+    reporter: [
+        ['html'],
+        ['@estruyf/github-actions-reporter']
+    ],
     use: {
         baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
         trace: 'on-first-retry',
