d4e5ea3455006785c8caedf6bb44274e0e2c9ac9
magnew <mcagnew@gmail.com>
2025-09-18 19:52:36 +0200

    chore: set up FE performance reporting in CI (#16937)
    * chore: set up rum performance reporting in CI
    
    * rename profiling wrapper
    
    * move flow tracking to just cypress
    
    * cleanup
    
    * add test cases
    
    * fix table
    
    * move wrapper
    
    * only run perf tests with test-perf
    
    * remove web vitals dependency
    
    * fix comments
    
    * Reset pnpm-lock.yaml to origin/main
    
    * fix workflow
    
    * comment fix
    
    * chore: add test coverage in explores
    
    * fix selector

---
+++ packages/frontend/src/perf/ProfilerWrapper.tsx
@@ -0,0 +1,33 @@
+import { type AnyType } from '@lightdash/common';
+import React, { Profiler, type PropsWithChildren } from 'react';
+
+export function ProfilerWrapper({
+    id,
+    children,
+}: PropsWithChildren<{ id: string }>) {
+    const onRender: React.ProfilerOnRenderCallback = (
+        profId,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+    ) => {
+        (window as AnyType).__profiling = (window as AnyType).__profiling || [];
+        (window as AnyType).__profiling.push({
+            id: profId,
+            phase, // "mount" | "update"
+            actualDuration, // time spent rendering this update
+            baseDuration, // estimated render cost without memoization
+            startTime,
+            commitTime,
+            ts: performance.now(),
+        });
+    };
+
+    return (
+        <Profiler id={id} onRender={onRender}>
+            {children}
+        </Profiler>
+    );
+}
