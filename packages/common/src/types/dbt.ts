f2c95e1acebcf3d5cdf947cb73d70bf7147dcb27
Jose Rego <jose@lightdash.com>
2025-09-18 14:01:01 +0100

    chore: rollback change

--- packages/common/src/types/dbt.ts
+++ packages/common/src/types/dbt.ts
@@ -202,6 +202,12 @@ export const normaliseModelDatabase = (
         case SupportedDbtAdapter.SNOWFLAKE:
         case SupportedDbtAdapter.TRINO:
         case SupportedDbtAdapter.REDSHIFT:
+            if (model.database === null) {
+                throw new ParseError(
+                    `Cannot parse dbt model '${model.unique_id}' because the database field has null value.`,
+                    {},
+                );
+            }
             return { ...model, database: model.database as string };
         case SupportedDbtAdapter.CLICKHOUSE:
         case SupportedDbtAdapter.DATABRICKS:
