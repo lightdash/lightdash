2105f49552ab805525c87ae0a90a0feb936dd282
João Viana <joao@lightdash.com>
2025-09-25 10:43:15 +0100

    style: increase preview banner's zindex (#17040)

--- packages/frontend/src/components/NavBar/PreviewBanner.tsx
+++ packages/frontend/src/components/NavBar/PreviewBanner.tsx
@@ -1,10 +1,18 @@
-import { Center, Text } from '@mantine/core';
+import { Center, getDefaultZIndex, Text } from '@mantine/core';
 import { IconTool } from '@tabler/icons-react';
 import MantineIcon from '../common/MantineIcon';
 import { BANNER_HEIGHT } from '../common/Page/constants';
 
 export const PreviewBanner = () => (
-    <Center pos="fixed" w="100%" h={BANNER_HEIGHT} bg="blue.6">
+    <Center
+        pos="fixed"
+        w="100%"
+        h={BANNER_HEIGHT}
+        bg="blue.6"
+        style={{
+            zIndex: getDefaultZIndex('app'),
+        }}
+    >
         <MantineIcon icon={IconTool} color="white" size="sm" />
         <Text color="white" fw={500} fz="xs" mx="xxs">
             This is a preview environment. Any changes you make here will not
