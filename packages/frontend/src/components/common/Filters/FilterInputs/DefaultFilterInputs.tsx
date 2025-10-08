da40cb8c880a6123467f87ce6a07b3e6cbc67734
Yegor Borisenco <pimmey@pimmey.com>
2025-10-08 10:47:21 +0400

    chore: use constant for FILTER_SELECT_LIMIT

--- packages/frontend/src/components/common/Filters/FilterInputs/DefaultFilterInputs.tsx
+++ packages/frontend/src/components/common/Filters/FilterInputs/DefaultFilterInputs.tsx
@@ -9,6 +9,7 @@ import {
 import isString from 'lodash/isString';
 import { type FilterInputsProps } from '.';
 import { TagInput } from '../../TagInput/TagInput';
+import { FILTER_SELECT_LIMIT } from '../constants';
 import useFiltersContext from '../useFiltersContext';
 import { getPlaceholderByFilterTypeAndOperator } from '../utils/getPlaceholderByFilterTypeAndOperator';
 import FilterMultiStringInput from './FilterMultiStringInput';
@@ -56,7 +57,7 @@ const DefaultFilterInputs = <T extends BaseFilterRule>({
                 case FilterType.STRING:
                     return !field || isTableCalculation(field) ? (
                         <FilterMultiStringInput
-                            limit={50}
+                            limit={FILTER_SELECT_LIMIT}
                             disabled={disabled}
                             placeholder={placeholder}
                             autoFocus={true}
@@ -73,7 +74,7 @@ const DefaultFilterInputs = <T extends BaseFilterRule>({
                         />
                     ) : (
                         <FilterStringAutoComplete
-                            limit={50}
+                            limit={FILTER_SELECT_LIMIT}
                             filterId={rule.id}
                             disabled={disabled}
                             field={field}
--- packages/frontend/src/components/common/Filters/index.tsx
+++ packages/frontend/src/components/common/Filters/index.tsx
@@ -33,6 +33,7 @@ import {
 } from '../../Explorer/FiltersCard/useFieldsWithSuggestions';
 import FieldSelect from '../FieldSelect';
 import MantineIcon from '../MantineIcon';
+import { FILTER_SELECT_LIMIT } from './constants';
 import FilterGroupForm from './FilterGroupForm';
 import SimplifiedFilterGroupForm from './SimplifiedFilterGroupForm';
 import useFiltersContext from './useFiltersContext';
@@ -277,7 +278,7 @@ const FiltersForm: FC<Props> = ({ filters, setFilters, isEditMode }) => {
                         </Group>
                     ) : (
                         <FieldSelect
-                            limit={50}
+                            limit={FILTER_SELECT_LIMIT}
                             size="xs"
                             withinPortal
                             maw={300}
