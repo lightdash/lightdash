d409d9776ff63186b80d30ba2ccd41754fd64b50
Steve Gardner <stevelikesmusic@users.noreply.github.com>
2025-10-16 08:25:52 -0700

    chore: configure sdk-test-app to resolve local modules (#17443)
    <!-- Thanks so much for your PR, your contribution is appreciated! ❤️ -->
    
    Closes: <!-- reference the related issue e.g. #150 -->
    
    ### Description:
    <!-- Add a description of the changes proposed in the pull request. -->
    
    <!-- Even better add a screenshot / gif / loom -->

--- packages/sdk-test-app/vite.config.ts
+++ packages/sdk-test-app/vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -7,4 +8,16 @@ export default defineConfig({
         port: 3030,
         host: true,
     },
+    resolve: {
+        alias: {
+            '@lightdash/sdk/sdk.css': resolve(__dirname, '../frontend/sdk/dist/sdk.css'),
+            '@lightdash/sdk': resolve(__dirname, '../frontend/sdk/index.tsx'),
+        },
+    },
+    define: {
+        __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+        REACT_SCAN_ENABLED: process.env.REACT_SCAN_ENABLED ?? false,
+        REACT_QUERY_DEVTOOLS_ENABLED:
+            process.env.REACT_QUERY_DEVTOOLS_ENABLED ?? false,
+    }
 });
