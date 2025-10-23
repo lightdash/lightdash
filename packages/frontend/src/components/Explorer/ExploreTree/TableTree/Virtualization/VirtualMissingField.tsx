ad02d9791a36b666cf396626a4dc3d7bdb918083
almeidabbm <almeidabatistam@gmail.com>
2025-10-23 16:51:41 +0100

    chore: create rendering components for virtualization

---
+++ packages/frontend/src/components/Explorer/ExploreTree/TableTree/Virtualization/VirtualMissingField.tsx
@@ -0,0 +1,44 @@
+import { Group, Text, Tooltip } from '@mantine/core';
+import { IconAlertTriangle } from '@tabler/icons-react';
+import { memo, type FC } from 'react';
+import MantineIcon from '../../../../common/MantineIcon';
+import type { MissingFieldItem } from './types';
+
+interface VirtualMissingFieldProps {
+    item: MissingFieldItem;
+}
+
+/**
+ * Renders a missing field alert in the virtualized tree
+ * TODO: Wire up click handler in integration phase (PR5)
+ */
+const VirtualMissingFieldComponent: FC<VirtualMissingFieldProps> = ({
+    item,
+}) => {
+    const { fieldId } = item.data;
+
+    return (
+        <Tooltip
+            withinPortal
+            label={`Field ${fieldId} not found on this chart. Click here to remove it.`}
+            position="bottom-start"
+            maw={700}
+        >
+            <Group ml={12} my="xs" style={{ cursor: 'pointer' }}>
+                <MantineIcon
+                    icon={IconAlertTriangle}
+                    color="yellow.9"
+                    style={{ flexShrink: 0 }}
+                />
+                <Text truncate>{fieldId}</Text>
+            </Group>
+        </Tooltip>
+    );
+};
+
+// ts-unused-exports:disable-next-line
+export const VirtualMissingField = memo(VirtualMissingFieldComponent);
+VirtualMissingField.displayName = 'VirtualMissingField';
+
+// ts-unused-exports:disable-next-line
+export default VirtualMissingField;
