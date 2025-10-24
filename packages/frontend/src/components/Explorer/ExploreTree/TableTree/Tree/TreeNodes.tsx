01df5aef1f8a97419257eb628f5bf0d919e299df
almeidabbm <almeidabatistam@gmail.com>
2025-10-24 15:54:39 +0100

    feat: add interactions to virtualized tree

--- packages/frontend/src/components/Explorer/ExploreTree/TableTree/Tree/TreeNodes.tsx
+++ packages/frontend/src/components/Explorer/ExploreTree/TableTree/Tree/TreeNodes.tsx
@@ -1,13 +1,4 @@
-import {
-    FeatureFlags,
-    OrderFieldsByStrategy,
-    isDimension,
-    sortTimeFrames,
-    type AdditionalMetric,
-    type CustomDimension,
-    type Dimension,
-    type Metric,
-} from '@lightdash/common';
+import { FeatureFlags, OrderFieldsByStrategy } from '@lightdash/common';
 import { Button } from '@mantine/core';
 import { memo, useMemo, useState, type FC } from 'react';
 import {
@@ -15,38 +6,11 @@ import {
     useExplorerSelector,
 } from '../../../../../features/explorer/store';
 import { useFeatureFlag } from '../../../../../hooks/useFeatureFlagEnabled';
+import { sortNodes } from './sortNodes';
 import TreeGroupNode from './TreeGroupNode';
 import TreeSingleNode from './TreeSingleNode';
 import { isGroupNode, type Node, type NodeMap } from './types';
 import useTableTree from './useTableTree';
-
-const sortNodes =
-    (
-        orderStrategy: OrderFieldsByStrategy,
-        itemsMap: Record<
-            string,
-            Dimension | Metric | AdditionalMetric | CustomDimension
-        >,
-    ) =>
-    (a: Node, b: Node) => {
-        if (orderStrategy === OrderFieldsByStrategy.INDEX) {
-            return a.index - b.index;
-        }
-
-        const itemA = itemsMap[a.key];
-        const itemB = itemsMap[b.key];
-
-        if (
-            isDimension(itemA) &&
-            isDimension(itemB) &&
-            itemA.timeInterval &&
-            itemB.timeInterval
-        ) {
-            return sortTimeFrames(itemA.timeInterval, itemB.timeInterval);
-        } else {
-            return a.label.localeCompare(b.label);
-        }
-    };
 
 const INITIAL_ITEMS_SHOWN = 50; // Initial number of items to show
 const LOAD_MORE_INCREMENT = 25; // Number of items to load when "Show more" is clicked
