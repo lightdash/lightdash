2989ad318515cb8a41b2b842cd59c653a6331447
Javier Rengel <rephus@gmail.com>
2025-10-27 08:29:12 +0100

    feat: add project to login on cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -186,6 +186,12 @@ ${styles.bold('Examples:')}
         'Login using OAuth2 flow (opens browser for authentication)',
         false,
     )
+    .option(
+        '--project <project uuid>',
+        'Select a project by UUID after login',
+        parseProjectArgument,
+        undefined,
+    )
     .option('--verbose', undefined, false)
     .action(login);
 
