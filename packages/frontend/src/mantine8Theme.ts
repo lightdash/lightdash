29093039e687318fc0a774aeefe0dbf7bdaba4fe
Tatiana Inama <tatiana@lightdash.com>
2025-11-24 19:14:04 +0100

    fix: custom colors in 8 theme

--- packages/frontend/src/mantine8Theme.ts
+++ packages/frontend/src/mantine8Theme.ts
@@ -60,6 +60,7 @@ export const getMantine8ThemeOverride = (
     ({
         ...legacyTheme,
         ...overrides,
+        colors,
         fontFamily: `Inter, ${legacyTheme.fontFamily}`,
         headings: {
             fontFamily: `Inter, ${legacyTheme.fontFamily}`,
