62649ec731a4cc20b77bcfa11aa6413d2c242c52
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-11-26 11:49:02 +0000

    feat: add dark mode feature flag

--- packages/frontend/src/components/NavBar/ThemeSwitcher.tsx
+++ packages/frontend/src/components/NavBar/ThemeSwitcher.tsx
@@ -1,11 +1,16 @@
+import { FeatureFlags } from '@lightdash/common';
 import { Tooltip } from '@mantine-8/core';
 import { Button, useMantineColorScheme } from '@mantine/core';
 import { IconMoonStars, IconSun } from '@tabler/icons-react';
+import { useFeatureFlagEnabled } from 'posthog-js/react';
 import { type FC } from 'react';
 import MantineIcon from '../common/MantineIcon';
 
 export const ThemeSwitcher: FC<{}> = ({}) => {
+    const isDarkModeEnabled = useFeatureFlagEnabled(FeatureFlags.DarkMode);
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+    if (!isDarkModeEnabled) return null;
 
     return (
         <Tooltip label={colorScheme === 'dark' ? 'Light mode' : 'Dark mode'}>
