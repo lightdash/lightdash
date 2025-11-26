083cefdd1af00de435807ec113810ae2ba5b6aec
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-11-26 11:32:51 +0000

    style: improve dark mode UI elements and remove unnecessary backgrounds

--- packages/frontend/src/components/common/ResourceIcon/index.tsx
+++ packages/frontend/src/components/common/ResourceIcon/index.tsx
@@ -81,7 +81,7 @@ export const ResourceIcon: FC<ResourceIconProps> = ({ item }) => {
         case ResourceViewItemType.DASHBOARD:
             return <IconBox icon={IconLayoutDashboard} color="green.6" />;
         case ResourceViewItemType.SPACE:
-            return <IconBox icon={IconFolder} color="violet.6" bg="violet.0" />;
+            return <IconBox icon={IconFolder} color="violet.6" />;
         case ResourceViewItemType.CHART:
             return <ChartIcon chartKind={item.data.chartKind} />;
         default:
