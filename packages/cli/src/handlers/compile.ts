4e12b001c3885874cd4d2d260140b6490a0b9b05
Jose Rego <ZeRego@users.noreply.github.com>
2025-11-26 12:27:13 +0000

    fix: handle empty metrics object in dbt manifest (#18355)
    * fix: handle empty metrics object in dbt manifest
    
    * fix: metrics count

--- packages/cli/src/handlers/compile.ts
+++ packages/cli/src/handlers/compile.ts
@@ -236,7 +236,7 @@ export const compile = async (options: CompileHandlerOptions) => {
                 DbtManifestVersion.V12,
             ].includes(manifestVersion)
                 ? []
-                : Object.values(manifest.metrics),
+                : Object.values(manifest.metrics || {}),
             warehouseSqlBuilder,
             lightdashProjectConfig,
         );
@@ -264,7 +264,7 @@ export const compile = async (options: CompileHandlerOptions) => {
     );
 
     const metricsCount =
-        dbtMetrics === null ? 0 : Object.values(dbtMetrics.metrics).length;
+        dbtMetrics === null ? 0 : Object.values(dbtMetrics).length;
     await LightdashAnalytics.track({
         event: 'compile.completed',
         properties: {
