5e1c57d21fa6d075f680a8f3baf4bc66ba3a47a6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2025-12-22 11:54:54 +0000

    chore: duplicate tile base component

---
+++ packages/frontend/src/components/DashboardTiles/TileBase/types.ts
@@ -0,0 +1,25 @@
+import { type DashboardTab } from '@lightdash/common';
+import { type ReactNode } from 'react';
+
+export type TileBaseProps<T> = {
+    isEditMode: boolean;
+    belongsToDashboard?: boolean;
+    title: string;
+    titleLeftIcon?: ReactNode;
+    chartName?: string;
+    titleHref?: string;
+    description?: string | null;
+    tile: T;
+    isLoading?: boolean;
+    extraMenuItems?: ReactNode;
+    onDelete: (tile: T) => void;
+    onEdit: (tile: T) => void;
+    children?: ReactNode;
+    extraHeaderElement?: ReactNode;
+    visibleHeaderElement?: ReactNode;
+    minimal?: boolean;
+    tabs?: DashboardTab[];
+    lockHeaderVisibility?: boolean;
+    transparent?: boolean;
+    fullWidth?: boolean;
+};
