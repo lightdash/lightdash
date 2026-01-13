0a039d670fb017544ff6538c0afe0216cb126cd0
almeidabbm <almeidabatistam@gmail.com>
2026-01-13 12:25:19 +0000

    chore: use block instead of line disables

--- packages/backend/src/services/UnfurlService/UnfurlService.ts
+++ packages/backend/src/services/UnfurlService/UnfurlService.ts
@@ -854,22 +854,21 @@ export class UnfurlService extends BaseService {
                             typeof crypto !== 'undefined' &&
                             !crypto.randomUUID
                         ) {
+                            /* eslint-disable no-bitwise */
                             crypto.randomUUID =
                                 (): `${string}-${string}-${string}-${string}-${string}` =>
                                     '10000000-1000-4000-8000-100000000000'.replace(
                                         /[018]/g,
                                         (c: string) =>
-                                            // eslint-disable-next-line no-bitwise
                                             (
                                                 Number(c) ^
-                                                // eslint-disable-next-line no-bitwise
                                                 (crypto.getRandomValues(
                                                     new Uint8Array(1),
                                                 )[0] &
-                                                    // eslint-disable-next-line no-bitwise
                                                     (15 >> (Number(c) / 4)))
                                             ).toString(16),
                                     ) as `${string}-${string}-${string}-${string}-${string}`;
+                            /* eslint-enable no-bitwise */
                         }
                     });
 
