89a10f3e82a993dde92f7e2bb8439a4e7838d01e
oliverlaslett <11660098+owlas@users.noreply.github.com>
2026-02-02 15:45:59 +0000

    fix: make charts schema a discriminator type instead of null

--- packages/cli/src/ajv.ts
+++ packages/cli/src/ajv.ts
@@ -4,5 +4,7 @@ import addFormats from 'ajv-formats';
 export const ajv = new Ajv({
     coerceTypes: true,
     allErrors: true, // Report all errors, not just the first one
+    allowUnionTypes: true, // Allow union types like ["string", "boolean"]
+    discriminator: true, // Enable discriminator keyword for better oneOf error messages
 });
 addFormats(ajv);
