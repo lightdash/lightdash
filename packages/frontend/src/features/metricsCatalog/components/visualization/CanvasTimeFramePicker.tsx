987a3c42ffb3c1be87f8c2bb405d410e94ef0926
Giorgi Bagdavadze <notgiorgi@gmail.com>
2026-02-05 15:00:57 +0000

    feat: redesign canvas time frame picker with improved date range display

--- packages/frontend/src/features/metricsCatalog/components/visualization/CanvasTimeFramePicker.tsx
+++ packages/frontend/src/features/metricsCatalog/components/visualization/CanvasTimeFramePicker.tsx
@@ -3,12 +3,13 @@ import {
     getDefaultMetricTreeNodeDateRange,
     getRollingPeriodDates,
 } from '@lightdash/common';
-import { Select, Tooltip } from '@mantine/core';
-import { IconChevronDown } from '@tabler/icons-react';
+import { Group, Select, Text } from '@mantine-8/core';
+import { clsx } from '@mantine/core';
+import { IconCalendar } from '@tabler/icons-react';
 import dayjs from 'dayjs';
 import { useMemo, type FC } from 'react';
 import MantineIcon from '../../../../components/common/MantineIcon';
-import { useSelectStyles } from '../../styles/useSelectStyles';
+import styles from './CanvasTimeFramePicker.module.css';
 import {
     DEFAULT_CANVAS_TIME_OPTIONS,
     type CanvasTimeOption,
@@ -16,10 +17,18 @@ import {
 
 const DATE_FORMAT = 'MMM D, YYYY';
 
-const getCanvasTimeDateLabel = (option: CanvasTimeOption): string => {
+type DateRangeInfo = {
+    currentRange: string;
+    previousRange: string;
+};
+
+const getDateRanges = (option: CanvasTimeOption): DateRangeInfo => {
     if (option.type === 'rolling') {
         const { current, previous } = getRollingPeriodDates(option.rollingDays);
-        return `${previous.start.format(DATE_FORMAT)} - ${previous.end.format(DATE_FORMAT)} / ${current.start.format(DATE_FORMAT)} - ${current.end.format(DATE_FORMAT)}`;
+        return {
+            currentRange: `${current.start.format(DATE_FORMAT)} – ${current.end.format(DATE_FORMAT)}`,
+            previousRange: `${previous.start.format(DATE_FORMAT)} – ${previous.end.format(DATE_FORMAT)}`,
+        };
     }
     const [currentStart, currentEnd] = getDefaultMetricTreeNodeDateRange(
         option.timeFrame,
@@ -27,7 +36,10 @@ const getCanvasTimeDateLabel = (option: CanvasTimeOption): string => {
     const { back } = getDateCalcUtils(option.timeFrame);
     const previousStart = back(currentStart);
     const previousEnd = back(currentEnd);
-    return `${dayjs(currentStart).format(DATE_FORMAT)} - ${dayjs(currentEnd).format(DATE_FORMAT)} / ${dayjs(previousStart).format(DATE_FORMAT)} - ${dayjs(previousEnd).format(DATE_FORMAT)}`;
+    return {
+        currentRange: `${dayjs(currentStart).format(DATE_FORMAT)} – ${dayjs(currentEnd).format(DATE_FORMAT)}`,
+        previousRange: `${dayjs(previousStart).format(DATE_FORMAT)} – ${dayjs(previousEnd).format(DATE_FORMAT)}`,
+    };
 };
 
 const serializeOption = (option: CanvasTimeOption): string =>
@@ -39,52 +51,71 @@ type Props = {
     value: CanvasTimeOption;
     onChange: (value: CanvasTimeOption) => void;
     options?: CanvasTimeOption[];
-    width?: number;
 };
 
 export const CanvasTimeFramePicker: FC<Props> = ({
     value,
     onChange,
     options = DEFAULT_CANVAS_TIME_OPTIONS,
-    width = 185,
 }) => {
-    const { classes } = useSelectStyles();
-
     const selectData = options.map((option) => ({
         value: serializeOption(option),
         label: option.label,
     }));
 
-    const tooltipLabel = useMemo(() => getCanvasTimeDateLabel(value), [value]);
+    const dateRanges = useMemo(() => getDateRanges(value), [value]);
 
     return (
-        <Tooltip label={tooltipLabel} position="bottom" withArrow>
-            <Select
-                w={width}
-                size="xs"
-                radius="md"
-                data={selectData}
-                value={serializeOption(value)}
-                onChange={(val) => {
-                    const selected = options.find(
-                        (opt) => serializeOption(opt) === val,
-                    );
-                    if (selected) onChange(selected);
-                }}
-                withinPortal
-                rightSection={
-                    <MantineIcon
-                        color="ldGray.7"
-                        icon={IconChevronDown}
-                        size={12}
-                    />
-                }
-                classNames={{
-                    input: classes.input,
-                    item: classes.item,
-                    rightSection: classes.rightSection,
-                }}
-            />
-        </Tooltip>
+        <Group gap="xs" wrap="nowrap">
+            <Group gap={0} wrap="nowrap">
+                <Select
+                    size="xs"
+                    data={selectData}
+                    value={serializeOption(value)}
+                    onChange={(val) => {
+                        const selected = options.find(
+                            (opt) => serializeOption(opt) === val,
+                        );
+                        if (selected) onChange(selected);
+                    }}
+                    leftSection={
+                        <MantineIcon
+                            icon={IconCalendar}
+                            color="ldGray.6"
+                            size={14}
+                        />
+                    }
+                    classNames={{
+                        input: styles.input,
+                        option: styles.option,
+                        dropdown: styles.dropdown,
+                        section: styles.section,
+                    }}
+                    withCheckIcon={false}
+                />
+                <Text
+                    size="sm"
+                    c="ldGray.7"
+                    fw={500}
+                    className={clsx(
+                        styles.dateDisplay,
+                        styles.dateDisplayGroupped,
+                    )}
+                >
+                    {dateRanges.currentRange}
+                </Text>
+            </Group>
+            <Text c="ldGray.6" fz={14} fw={500}>
+                compared to
+            </Text>
+            <Text
+                size="sm"
+                c="ldGray.7"
+                fw={500}
+                className={styles.dateDisplay}
+            >
+                {dateRanges.previousRange}
+            </Text>
+        </Group>
     );
 };
