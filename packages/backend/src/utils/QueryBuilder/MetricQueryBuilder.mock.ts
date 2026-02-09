a5c05c0f92525336f80976e812f54e511919222e
Jose Rego <jose@lightdash.com>
2026-02-09 11:13:57 +0000

    chore: mocks

--- packages/backend/src/utils/QueryBuilder/MetricQueryBuilder.mock.ts
+++ packages/backend/src/utils/QueryBuilder/MetricQueryBuilder.mock.ts
@@ -131,6 +131,11 @@ export const warehouseClientMock: WarehouseClient = {
         `EXTRACT(EPOCH FROM (${endTimestampSql} - ${startTimestampSql}))`,
     getMedianSql: (valueSql) =>
         `PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY ${valueSql})`,
+    buildArray: (elements) => `ARRAY[${elements.join(', ')}]`,
+    buildArrayAgg: (expression, orderBy) =>
+        orderBy
+            ? `ARRAY_AGG(${expression} ORDER BY ${orderBy})`
+            : `ARRAY_AGG(${expression})`,
 };
 
 export const bigqueryClientMock: WarehouseClient = {
@@ -213,6 +218,11 @@ export const bigqueryClientMock: WarehouseClient = {
         `TIMESTAMP_DIFF(${endTimestampSql}, ${startTimestampSql}, SECOND)`,
     getMedianSql: (valueSql) =>
         `APPROX_QUANTILES(${valueSql}, 100)[OFFSET(50)]`,
+    buildArray: (elements) => `ARRAY[${elements.join(', ')}]`,
+    buildArrayAgg: (expression, orderBy) =>
+        orderBy
+            ? `ARRAY_AGG(${expression} ORDER BY ${orderBy})`
+            : `ARRAY_AGG(${expression})`,
 };
 
 export const emptyTable = (name: string): CompiledTable => ({
