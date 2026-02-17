627c7ed23864ad26b3798b9c58faf52172f72cd0
Javier Rengel <rephus@gmail.com>
2026-02-17 13:05:33 +0100

    feat: implement required filters in model configuration

--- packages/frontend/src/components/common/Filters/FilterRuleForm.tsx
+++ packages/frontend/src/components/common/Filters/FilterRuleForm.tsx
@@ -87,7 +87,7 @@ const FilterRuleForm: FC<Props> = memo(
         );
         const isRequired = filterRule.required;
         const isRequiredLabel = isRequired
-            ? "This filter is a required filter.\n It can't be deleted, but the value can be changed."
+            ? 'This is a required filter defined in the model configuration and cannot be modified.'
             : '';
 
         if (!activeField) {
@@ -101,21 +101,31 @@ const FilterRuleForm: FC<Props> = memo(
                 spacing="xs"
                 data-testid="FilterRuleForm/filter-rule"
             >
-                <FieldSelect
-                    size="xs"
-                    disabled={!isEditMode}
-                    withinPortal={popoverProps?.withinPortal}
-                    onDropdownOpen={popoverProps?.onOpen}
-                    onDropdownClose={popoverProps?.onClose}
-                    hasGrouping
-                    item={activeField}
-                    items={fields}
-                    onChange={(field) => {
-                        if (!field) return;
-                        onFieldChange(getItemId(field));
-                    }}
-                    baseTable={baseTable}
-                />
+                <Tooltip
+                    label={isRequiredLabel}
+                    disabled={!isRequired}
+                    withinPortal
+                    variant="xs"
+                    multiline
+                >
+                    <Box>
+                        <FieldSelect
+                            size="xs"
+                            disabled={!isEditMode || isRequired}
+                            withinPortal={popoverProps?.withinPortal}
+                            onDropdownOpen={popoverProps?.onOpen}
+                            onDropdownClose={popoverProps?.onClose}
+                            hasGrouping
+                            item={activeField}
+                            items={fields}
+                            onChange={(field) => {
+                                if (!field) return;
+                                onFieldChange(getItemId(field));
+                            }}
+                            baseTable={baseTable}
+                        />
+                    </Box>
+                </Tooltip>
                 <Select
                     limit={FILTER_SELECT_LIMIT}
                     size="xs"
@@ -124,7 +134,7 @@ const FilterRuleForm: FC<Props> = memo(
                     withinPortal={popoverProps?.withinPortal}
                     onDropdownOpen={popoverProps?.onOpen}
                     onDropdownClose={popoverProps?.onClose}
-                    disabled={!isEditMode}
+                    disabled={!isEditMode || isRequired}
                     value={filterRule.operator}
                     data={filterOperatorOptions}
                     onChange={(value) => {
@@ -147,7 +157,7 @@ const FilterRuleForm: FC<Props> = memo(
                     field={activeField}
                     rule={filterRule}
                     onChange={onChange}
-                    disabled={!isEditMode}
+                    disabled={!isEditMode || isRequired}
                     popoverProps={popoverProps}
                 />
 
@@ -167,6 +177,19 @@ const FilterRuleForm: FC<Props> = memo(
                                     data-testid="delete-filter-rule-button"
                                 >
                                     <MantineIcon icon={IconX} size="sm" />
+                                </ActionIcon>
+                            </span>
+                        </Tooltip>
+                    ) : isRequired ? (
+                        <Tooltip
+                            label={isRequiredLabel}
+                            withinPortal
+                            variant="xs"
+                            multiline
+                        >
+                            <span>
+                                <ActionIcon variant="subtle" disabled>
+                                    <IconDots size="20" />
                                 </ActionIcon>
                             </span>
                         </Tooltip>
@@ -191,23 +214,9 @@ const FilterRuleForm: FC<Props> = memo(
                                 <Menu.Item onClick={onConvertToGroup}>
                                     Convert to group
                                 </Menu.Item>
-                                <Tooltip
-                                    label={isRequiredLabel}
-                                    disabled={!isRequired}
-                                    withinPortal
-                                    variant="xs"
-                                    multiline
-                                >
-                                    <span>
-                                        <Menu.Item
-                                            color="red"
-                                            disabled={isRequired}
-                                            onClick={onDelete}
-                                        >
-                                            Remove
-                                        </Menu.Item>
-                                    </span>
-                                </Tooltip>
+                                <Menu.Item color="red" onClick={onDelete}>
+                                    Remove
+                                </Menu.Item>
                             </Menu.Dropdown>
                         </Menu>
                     ))}
