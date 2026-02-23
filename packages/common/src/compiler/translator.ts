d44a661c15323b37f17aeded257423d2ede120d8
Giorgi Bagdavadze <notgiorgi@gmail.com>
2026-02-23 13:23:07 +0000

    feat: add DuckDB warehouse client with S3 support

--- packages/common/src/compiler/translator.ts
+++ packages/common/src/compiler/translator.ts
@@ -88,6 +88,8 @@ const convertTimezone = (
             // TIMESTAMP WITH TIME ZONE: stored as utc. returns in session tz. convert from session tz to target tz
             // TIMESTAMP WITHOUT TIME ZONE: no tz. assume default_source_tz. convert from default_source_tz to target_tz
             return timestampSql;
+        case SupportedDbtAdapter.DUCKDB:
+            return timestampSql;
         case SupportedDbtAdapter.DATABRICKS:
             return timestampSql;
         // Athena uses Trino SQL, timestamps return in server timezone
