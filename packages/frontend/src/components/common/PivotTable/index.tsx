afd50fd1905a466e56bb9df5c5d7e12fc293610e
Charlie Dowler <charlie@lightdash.com>
2026-02-24 19:33:27 +0000

    fix: correct pivot table columnProperties type

--- packages/frontend/src/components/common/PivotTable/index.tsx
+++ packages/frontend/src/components/common/PivotTable/index.tsx
@@ -117,7 +117,7 @@ type PivotTableProps = BoxProps & // TODO: remove this
         getFieldLabel: (fieldId: string) => string | undefined;
         getField: (fieldId: string) => ItemsMap[string] | undefined;
         showSubtotals?: boolean;
-        columnProperties?: ColumnProperties;
+        columnProperties?: Record<string, ColumnProperties>;
         isMinimal: boolean;
         isDashboard?: boolean;
     };
