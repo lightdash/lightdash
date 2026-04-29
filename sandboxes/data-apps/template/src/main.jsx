bafad075ea1956288b78d736b54f59d6fc7bfd46
Marshall Agnew <marshall@lightdash.com>
2026-04-29 15:23:33 +0200

    chore: add filter context to data app template

--- sandboxes/data-apps/template/src/main.jsx
+++ sandboxes/data-apps/template/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createClient, LightdashProvider } from '@lightdash/query-sdk';
+import { FilterProvider } from '@/lib/filters';
 import App from './App';
 import './index.css';
 import './chart-overrides.css';
@@ -21,7 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <LightdashProvider client={lightdash}>
-                <App />
+                <FilterProvider>
+                    <App />
+                </FilterProvider>
             </LightdashProvider>
         </QueryClientProvider>
     </React.StrictMode>,
