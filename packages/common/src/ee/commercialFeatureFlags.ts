e1c897b4b4e56f5131bed9df0b96ce15b3f633d5
Sandro Dolidze <sandro@lightdash.com>
2026-05-12 17:14:58 +0100

    feat: add ServiceAccountProjectMemberships feature flag (PROD-7529)

--- packages/common/src/ee/commercialFeatureFlags.ts
+++ packages/common/src/ee/commercialFeatureFlags.ts
@@ -3,6 +3,7 @@ export enum CommercialFeatureFlags {
     Scim = 'scim-token-management',
     AiCopilot = 'ai-copilot',
     ServiceAccounts = 'service-accounts',
+    ServiceAccountProjectMemberships = 'service-account-project-memberships',
     OrganizationWarehouseCredentials = 'organization-warehouse-credentials',
     CustomRoles = 'custom-roles',
 }
