8324259053fd8bd5583924a127acd47ca806a2e3
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2026-06-30 17:30:52 +0000

    fix(cli): avoid tsgo destructured export emit

--- packages/cli/src/env.ts
+++ packages/cli/src/env.ts
@@ -6,7 +6,9 @@ export const NODE_VERSION = { major: parseNodeVersion(process.version).major };
 
 export const OPTIMIZED_NODE_VERSION = 20;
 
-export const { version: CLI_VERSION } = packageJson;
+// Avoid TS7 RC CJS destructured-export bug; remove after compiler includes the fix.
+// https://github.com/microsoft/typescript-go/issues/4403
+export const CLI_VERSION = packageJson.version;
 
 export const DEFAULT_DBT_PROJECT_DIR = process.env.DBT_PROJECT_DIR || '.';
 export const DEFAULT_DBT_PROFILES_DIR: string = findDbtDefaultProfile();
--- packages/cli/src/styles.ts
+++ packages/cli/src/styles.ts
@@ -3,7 +3,10 @@ import chalk from 'chalk';
 export const error = chalk.red;
 export const title = chalk.bold.yellowBright;
 export const info = (s: string) => s;
-export const { bold } = chalk;
+// Avoid TS7 RC CJS destructured-export bug; remove after compiler includes the fix.
+// https://github.com/microsoft/typescript-go/issues/4403
+// eslint-disable-next-line prefer-destructuring
+export const bold = chalk.bold;
 export const secondary = chalk.dim;
 export const success = chalk.bold.green;
 export const warning = chalk.yellow;
