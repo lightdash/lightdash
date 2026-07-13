6560f5843ad0b4b984a3ab322e41a15f4704f39e
Joao Viana <joao@lightdash.com>
2026-07-13 22:01:04 +0100

    refactor(frontend): migrate Image to Mantine 8

--- packages/frontend/src/components/ProjectConnection/WarehouseForms/BigQueryForm.tsx
+++ packages/frontend/src/components/ProjectConnection/WarehouseForms/BigQueryForm.tsx
@@ -5,19 +5,13 @@ import {
     Button,
     FileInput,
     Group,
+    Image,
     Loader,
     Stack,
     Text,
 } from '@mantine-8/core';
 import type { SelectItem } from '@mantine/core';
-import {
-    Image,
-    NumberInput,
-    Select,
-    Switch,
-    TextInput,
-    Tooltip,
-} from '@mantine/core';
+import { NumberInput, Select, Switch, TextInput, Tooltip } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { IconCheck, IconExclamationCircle } from '@tabler/icons-react';
 import { useState, type ChangeEvent, type FC } from 'react';
@@ -93,7 +87,7 @@ const BigQuerySSOInput: FC<{
                 disabled={disabled}
                 leftSection={
                     <Image
-                        width={16}
+                        w={16}
                         src={
                             'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4='
                         }
