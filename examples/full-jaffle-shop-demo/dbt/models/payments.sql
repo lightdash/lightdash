73a1591a222ea9a70c6a9282e2fb288e8ce1cd2b
almeidabbm <almeidabatistam@gmail.com>
2026-08-28 15:03:46 +0000

    feat(demo): add payment method icon URLs to the jaffle shop demo
    Gives the demo project a dimension holding a public image URL per payment method (Tabler icons hosted on Wikimedia Commons), so custom chart types that render images have real data to show, and the docs screenshots can use it.

--- examples/full-jaffle-shop-demo/dbt/models/payments.sql
+++ examples/full-jaffle-shop-demo/dbt/models/payments.sql
@@ -1,1 +1,9 @@
-select * from {{ ref('stg_payments') }}
+select
+    *,
+    case payment_method
+        when 'credit_card' then 'https://upload.wikimedia.org/wikipedia/commons/1/16/Tabler-icons_credit-card.svg'
+        when 'bank_transfer' then 'https://upload.wikimedia.org/wikipedia/commons/0/07/Tabler-icons_building-bank.svg'
+        when 'coupon' then 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Tabler-icons_ticket.svg'
+        when 'gift_card' then 'https://upload.wikimedia.org/wikipedia/commons/9/99/Tabler-icons_gift-card.svg'
+    end as payment_method_icon_url
+from {{ ref('stg_payments') }}
