03d48e3942b33150922696043f03c9382d1ac4e5
Jose Rego <ZeRego@users.noreply.github.com>
2026-08-28 15:05:57 +0100

    feat(frontend): make login background pink

--- packages/frontend/src/pages/Login.tsx
+++ packages/frontend/src/pages/Login.tsx
@@ -8,7 +8,7 @@ import LoginLanding from '../features/users/components/LoginLanding';
 const Login: FC<{ minimal?: boolean }> = ({ minimal = false }) => {
     if (minimal) {
         return (
-            <Stack m="xl">
+            <Stack bg="pink" mih="100vh" p="xl">
                 <Box mx="auto" my="lg">
                     <LightdashLogo />
                 </Box>
@@ -35,6 +35,7 @@ const Login: FC<{ minimal?: boolean }> = ({ minimal = false }) => {
             subtitle="Welcome back — pick up where you left off."
             legacyTitle="Sign in"
             cardId={LOGIN_PAGE_ID}
+            backgroundColor="pink"
         >
             <LoginLanding />
         </AuthLayout>
